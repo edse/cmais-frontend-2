0b618108a80eea1d22db6c14ae086b38b3cbd56a
emerson <emerson.estrella@gmail.com>
2012-03-26 17:21:26 -0300

    Adding
    - /elis
    - /chicoanysio

--- apps/frontend/templates/blocks/_shortcuts.php
+++ apps/frontend/templates/blocks/_shortcuts.php
@@ -2,7 +2,7 @@
         <ul class="box-lateral posicao-lateral" id="menuFloat">         
           <li><a class="cmais" href="http://cmais.com.br" title="cmais+ O portal de conteúdo da Cultura">cmais+ O portal de conteúdo da Cultura</a></li>
           <li><a class="tvcultura" href="http://tvcultura.cmais.com.br" title="TV Cultura">TV Cultura</a></li>
-          <li><a class="tvrtb" href="http://tvratimbum.cmais.com.br" title="TV R&aacute;-Tim-Bum">TV R&aacute;-Tim-Bum</a></li>
+          <li><a class="tvrtb" href="http://tvratimbum.cmais.com.br" title="TV R&aacute; Tim Bum">TV R&aacute; Tim Bum</a></li>
           <li><a class="multicultura" href="http://multicultura.cmais.com.br" title="multiCULTURA">multiCULTURA</a></li>
           <li><a class="univesptv" href="http://univesptv.cmais.com.br/" title="Univesp TV">Univesp TV</a></li>
           <li><a class="culturabrasil" href="http://www.culturabrasil.com.br" title="culturabrasil">culturabrasil</a></li>
