295dde95ef641a35e8e53d0c7051ac46e6b161f9
unknown <renatareno@fpawks1115.fpanet.inet>
2012-03-29 10:26:07 -0300

    img6 - img3

--- apps/frontend/templates/blocks/_display1c-news.php
+++ apps/frontend/templates/blocks/_display1c-news.php
@@ -6,9 +6,9 @@
                   <p class="chapeu<?php if($displays[0]->Asset->Category->slug): ?><?php echo " ".$displays[0]->Asset->Category->getSlug() ?><?php endif; ?>"><?php echo $displays[0]->retriveLabel() ?></p>
                   <?php endif; ?>
                   
-                  <?php if($displays[0]->retriveImageUrlByImageUsage("image-6") != ""): ?>
+                  <?php if($displays[0]->retriveImageUrlByImageUsage("image-3") != ""): ?>
                   <a href="<?php echo $displays[0]->retriveUrl() ?>" title="<?php echo $displays[0]->getTitle() ?>">
-                    <img src="<?php echo $displays[0]->retriveImageUrlByImageUsage("image-6") ?>" alt="<?php echo $displays[0]->getTitle() ?>" name="<?php echo $displays[0]->getTitle() ?>" class="<?php if($displays[0]->Asset->AssetType->getSlug() == "video"):?>img-video<?php endif;?>" />
+                    <img src="<?php echo $displays[0]->retriveImageUrlByImageUsage("image-3") ?>" alt="<?php echo $displays[0]->getTitle() ?>" name="<?php echo $displays[0]->getTitle() ?>" class="<?php if($displays[0]->Asset->AssetType->getSlug() == "video"):?>img-video<?php endif;?>" />
                   </a>
                   <?php endif; ?>
 
