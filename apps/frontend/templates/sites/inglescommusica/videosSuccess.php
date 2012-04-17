d97ac9c75b8b7715eec0f52b5ac193304d5bb786
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-04-17 18:13:36 -0300

    vai

--- apps/frontend/templates/sites/inglescommusica/videosSuccess.php
+++ apps/frontend/templates/sites/inglescommusica/videosSuccess.php
@@ -12,11 +12,15 @@
 
       <!-- BARRA SITE -->
       <div id="barra-site">
-
         <div class="topo-programa">
-          
-          <h2><a href="<?php echo $site->retriveUrl() ?>"><img title="<?php echo $site->getTitle() ?>" alt="<?php echo $site->getTitle() ?>" src="/portal/univesptv/images/logo-univesptv.png" /></a></h2>
-          
+          <?php if(isset($program) && $program->id > 0): ?>
+          <h2>
+            <a href="<?php echo $program->retriveUrl() ?>">
+              <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
+            </a>
+          </h2>
+          <?php endif; ?>
+
           <?php if(isset($program) && $program->id > 0): ?>
           <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
           <?php endif; ?>
@@ -24,25 +28,26 @@
           <?php if(isset($program) && $program->id > 0): ?>
           <!-- horario -->
           <div id="horario">
-            <p>Canal digital 2.2 da multiprogramação da TV Cultura</p>
+            <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
           </div>
           <!-- /horario -->
           <?php endif; ?>
-
         </div>
 
         <?php if(isset($siteSections)): ?>
         <!-- box-topo -->
         <div class="box-topo grid3">
           
-          <?php include_partial_from_folder('blocks','global/sections-menu2', array('siteSections' => $siteSections)) ?>
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
 
-          <?php if(isset($section)): ?>
+          <?php if(isset($section->slug)): ?>
             <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
             <div class="navegacao txt-10">
               <a href="<?php echo $site->retriveUrl() ?>" title="Home">Home</a>
               <span>&gt;</span>
-              <a href="<?php echo $section->retriveUrl()?>" title="<?php echo $section->getTitle()?>"><?php echo $section->getTitle()?></a>
+              <a href="<?php echo $site->retriveUrl() ?>/videos" title="Vídeos">Vídeos</a>
+              <span>&gt;</span>
+              <a href="<?php echo $asset->retriveUrl()?>" title="<?php echo $asset->getTitle()?>"><?php echo $asset->getTitle()?></a>
             </div>
             <?php endif; ?>
           <?php endif; ?>
@@ -50,9 +55,10 @@
         </div>
         <!-- /box-topo -->
         <?php endif; ?>
-
+        
       </div>
       <!-- /BARRA SITE -->
+
       
       <!-- MIOLO -->
       <div id="miolo">
