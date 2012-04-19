f84bd30dd3e37864055111c3a78b6f33c87705b0
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-04-19 18:45:16 -0300

    template Ao Vivo (copia do cartao)

--- apps/frontend/templates/sites/inglescommusica/liveSuccess.php
+++ apps/frontend/templates/sites/inglescommusica/liveSuccess.php
@@ -1,151 +1,146 @@
-<link rel="stylesheet" href="/portal/css/tvcultura/secoes/aovivo.css" type="text/css" />
-<script type="text/javascript" src="/portal/js/mediaplayer/swfobject.js"></script>
-
-<script type="text/javascript">
-  $(function(){
-    //carrossel
-    $('.carrossel').jcarousel({
-        wrap: "both"
-    });
-    // comportamento inicial da grade
-    $('.btn-toggle:first').parent().addClass('escura');
-    $('.btn-toggle:first').parent().next().slideDown(400);
-    //hover states on the static widgets
-    $('#dialog_link, ul#icons li').hover(
-      function() { $(this).addClass('ui-state-hover'); }, 
-      function() { $(this).removeClass('ui-state-hover'); }
-    );
-    //start streaming
-    checkStreamingStart();
-    //timerStart();
-  });
-  
-  function checkStreamingStart(){
-    $.ajax({
-      url: "/ajax/streaming",
-      dataType: "text",
-      success: function(data){
-        eval(data);
-      }
-    });
-  }
-
-  function checkStreamingEnd(){
-    $.ajax({
-      url: "/ajax/streamingend",
-      dataType: "text",
-      success: function(data){
-        eval(data);
-      }
-    });
-  }
-
-  function timerStart(){
-    $.ajax({
-      url: "/ajax/timer",
-      dataType: "text",
-      success: function(data){
-        eval(data);
-      }
-    });
-  }
-</script>
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/rodaviva.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/<?php echo $site->getSlug() ?>.css" type="text/css" />
 
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
+<script type="text/javascript" src="/portal/js/mediaplayer/swfobject.js"></script>
+<script>
+function stream1() {
+  var so = new SWFObject('/portal/js/mediaplayer/player.swf','mpl','640','364','9');
+  so.addVariable('controlbar', 'bottom');
+  so.addVariable('autostart', 'true');
+  so.addVariable('streamer', 'rtmp://200.136.27.12/live');
+  so.addVariable('file', 'tv');
+  so.addVariable('type', 'video');
+  so.addParam('allowscriptaccess','always');
+  so.addParam('allowfullscreen','true');
+  so.addParam('wmode','transparent');
+  so.write('boxVideoWrapper');
+  $('.transmissaoH li a').removeClass('ativo');
+  $('#stream_tv').addClass('ativo');
+}
+
+function updateTweets() {
+  $.ajax({
+    url: "/ajax/tweetmonitor",
+    data: "monitor_id=2",
+    success: function(data) {
+      $('#twitter').html(data);
+    }
+  });
+}
+
+jQuery(document).ready(function() {
+  updateTweets();
+  var t=setInterval("updateTweets()",60000);
+  stream1();
+});
+</script>
+
+    
     <!-- CAPA SITE -->
+	<div class="bg-rodaviva">
     <div id="capa-site">
 
-      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
-
-      <!-- barra site --> 
+      <!-- BARRA SITE -->
       <div id="barra-site">
+        <div class="topo-programa">
+          <?php if(isset($program) && $program->id > 0): ?>
+          <h2>
+            <a href="<?php echo $site->retriveUrl() ?>" style="text-decoration: none;">
+              <?php if($program->getImageThumb() != ""): ?>
+                <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
+              <?php else: ?>
+                <h3 class="tit-pagina grid1"><?php echo $program->getTitle() ?></h3>
+              <?php endif; ?>
+            </a>
+          </h2>
+          <?php endif; ?>
 
-      <!-- box-topo -->
-      <div class="box-topo grid3">
-        
-        <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
-        
-        <h3 class="tit-pagina">Ao vivo</h3>
-        <div class="navegacao txt-10">
-          <a href="http://cmais.com.br" title="Home">Home</a>
-          <span>&gt;</span>
-          <a href="javascript:;" title="Ao vivo">Ao vivo</a>
+          <?php if(isset($program) && $program->id > 0): ?>
+          <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+          <?php endif; ?>
+          
+          <?php if(isset($program) && $program->id > 0): ?>
+          <!-- horario -->
+          <div id="horario">
+            <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
+          </div>
+          <!-- /horario -->
+          <?php endif; ?>
         </div>
+
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+          <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
+          <div class="navegacao txt-10">
+            <a href="../" title="Home">Home</a>
+            <span>&gt;</span>
+            <a href="<?php echo $section->retriveUrl()?>" title="<?php echo $section->getTitle()?>"><?php echo $section->getTitle()?></a>
+          </div>
+          <?php endif; ?>
+          
+          <h3 class="tit-pagina grid3"><?php echo $section->getTitle() ?></h3>
+
+        </div>
+        <!-- /box-topo -->
+        
       </div>
-      <!-- /box-topo -->
-      
-      </div>
-      <!-- /barra site -->
+      <!-- /BARRA SITE -->
 
       <!-- MIOLO -->
       <div id="miolo">
-        
+      	
         <!-- BOX LATERAL -->
         <?php include_partial_from_folder('blocks','global/shortcuts') ?>
         <!-- BOX LATERAL -->
-
+      	
         <!-- CONTEUDO PAGINA -->
-        <div id="conteudo-pagina">
-
+        <div id="conteudo-pagina exceptionn">
           <!-- CAPA -->
-          <div class="capa grid3">
-            
-            <!-- ESQUERDA -->
-            <div id="esquerda" class="grid2">
-              
-              <div id="livestream2"></div>
-
-              <!-- lista calendario -->
-              <?php include_partial_from_folder('blocks','global/display-2c-broadcast2', array('live_id' => $schedules[0]->id)) ?>
-              <!-- /lista calendario -->
-
-              <?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?>
-              
-            </div>
-            <!-- /ESQUERDA -->
-            
-            <!-- DIREITA -->
-            <div id="direita" class="grid1">
-              
-              <!-- BOX TWITTER #TAG -->
-              <!--
-              <div class="box-padrao twitter-aovivo grid1">
-
-              <ul class="abas-menu abas">
-                <li class="twitter ativo"><a href="#" title="Twitter">twitter</a><span class="decoracao"></span></li>
-              </ul>
-              <?php include_partial_from_folder('blocks','global/twitter-1c-search') ?>
-              </div>
-              -->
-              <!-- /BOX TWITTER #TAG -->
-              <?php include_partial_from_folder('blocks','global/facebook-1c', array('site' => $site)) ?>
-
-            </div>
-            <!-- /DIREITA -->
-
+          <div class="capa grid3 exceptionn">
+          	<div class="tudo-Rodaviva">
+          		<span class="bordaTopRV"></span>
+          		<div class="centroRV">
+          			<div class="transmissaoH">
+          				<div class="boxVideo">
+          					<div class="boxVideoWrapper" id="boxVideoWrapper"></div>
+		                    <span class="faixa"></span>
+		                    <h3><?php //echo $asset->getTitle() ?></h3>
+		                    <p><?php //echo $asset->getDescription() ?></p>
+		                    <br />
+		                    <?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?>
+          				</div>
+          			</div>
+          			
+          			<div class="chat">
+          				<div class="repercussao">
+          					<div class="grid1">
+          					  <a href="http://twitter.com/cartaoverde" class="twitter-follow-button" target="_blank">Siga @cartaoverde</a>	
+			                  <div id="twitter"></div>
+			            	</div>
+          				</div>
+          				<style type="text/css">
+          					.chat .repercussao {margin-top:10px;}
+          					#twitter {margin-top:0} 
+          				</style>
+          			</div>
+          		</div>
+          		<span class="bordaBottomRV"></span>
+          	</div>
           </div>
-          <!-- /CAPA -->
-
-          <!-- MENU-RODAPE -->
-          <?php if(isset($displays["videos-recentes"])) include_partial_from_folder('blocks','global/display-3c-last-videos', array('displays' => $displays["videos-recentes"])) ?>
-          <!-- /MENU-RODAPE -->
-          
-          <!-- BOX PUBLICIDADE 2 -->
-          <div class="box-publicidade pub-grd grid3">
-            <!-- univesptv-728x90 -->
-			<script type='text/javascript'>
-			GA_googleFillSlot("univesptv-728x90");
-			</script>
-          </div>
-          <!-- / BOX PUBLICIDADE 2 -->
-
+        
         </div>
         <!-- /CONTEUDO PAGINA -->
-        
+
       </div>
       <!-- /MIOLO -->
-      
+
+    </div>
     </div>
     <!-- / CAPA SITE -->
+
