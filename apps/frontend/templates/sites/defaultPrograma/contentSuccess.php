aaa98b207554894590a958a31fdaadfe9ddf8943
renatareno <asdf@fpawks0142.fpanet.inet>
2012-04-20 11:20:43 -0300

    ajuste alerta duplo cmais

--- apps/frontend/templates/sites/defaultPrograma/contentSuccess.php
+++ apps/frontend/templates/sites/defaultPrograma/contentSuccess.php
@@ -17,7 +17,7 @@
     <!-- CAPA SITE -->
     <div id="capa-site">
 
-      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
+      
 
       <!-- BARRA SITE -->
       <div id="barra-site">
