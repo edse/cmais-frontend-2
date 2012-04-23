4ed2edf51a97a56ef438035c72dad2ec2e5bbd34
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-04-23 17:35:16 -0300

    Finalização rodapé interno (apoio, realização, etc)

--- apps/frontend/templates/blocks/_support.php
+++ apps/frontend/templates/blocks/_support.php
@@ -2,11 +2,17 @@
   <?php if(count($displays) > 0): ?>
 		<div class="grid3 apoio">
 		<?php foreach($displays as $d): ?>
-  		<p>
-    		<?php echo $d->getDescription() ?>
-  		</p>
-  		  <img src="http://midia.cmais.com.br/displays/<?php echo $d->getImage() ?>" alt="<?php echo $d->getTitle() ?>">
-  	<?php endforeach; ?>
+			<?php if ($d->getDescription()): ?>
+  		<p><?php echo $d->getDescription() ?></p>
+			<?php elseif ($d->Asset->getDescription()): ?>
+  		<p><?php echo $d->Asset->getDescription() ?></p>
+			<?php endif; ?>
+			<?php if ($d->getImage()): ?>
+  		<img src="http://midia.cmais.com.br/displays/<?php echo $d->getImage() ?>" alt="<?php echo $d->getTitle() ?>">
+  		<?php elseif ($d->Asset->AssetImage->getImage()): ?>
+  		<img src="<?php echo $d->Asset->AssetImage->getImage() ?>" alt="<?php $d->getTitle() ?>">
+  		<?php endif;?>
+  	<?php endforeach; ?>    
 		</div>
   <?php endif; ?>
 <?php endif; ?>
