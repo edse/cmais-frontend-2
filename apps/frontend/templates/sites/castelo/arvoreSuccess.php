8cf680160abce2f938afb1bc8277aee3aa7305a2
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-02 10:37:52 -0300

    arvore Castelo

--- apps/frontend/templates/sites/castelo/arvoreSuccess.php
+++ apps/frontend/templates/sites/castelo/arvoreSuccess.php
@@ -17,6 +17,11 @@
       <!-- BARRA SITE -->
       <div id="barra-site">
         
+      <!--FANCYBOX-->
+      <script type="text/javascript" src="/portal/js/fancybox/jquery.fancybox-1.3.4.pack.js" ></script>
+      <link rel="stylesheet" href="/portal/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+      <!--/FANCYBOX-->
+        
         
         <div class="topo-programa">
           
