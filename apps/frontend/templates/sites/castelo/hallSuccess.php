4d8c02090d23cbd4f0d891c8ca28097292ad0f48
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-02 09:33:31 -0300

    hall Castelo

--- apps/frontend/templates/sites/castelo/hallSuccess.php
+++ apps/frontend/templates/sites/castelo/hallSuccess.php
@@ -12,7 +12,13 @@
 <div id="bg" >
   
     <!-- CAPA SITE -->
-    <div id="capa-site">      
+    <div id="capa-site">
+      
+      <!--FANCYBOX-->
+      <script type="text/javascript" src="/portal/js/fancybox/jquery.fancybox-1.3.4.pack.js" ></script>
+      <link rel="stylesheet" href="/portal/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+      <!--/FANCYBOX-->     
+   
 
       <!-- BARRA SITE -->
       <div id="barra-site">
