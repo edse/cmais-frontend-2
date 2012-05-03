73fa48918a5b64edadd7bb2cc13d00e040e76566
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-03 16:23:17 -0300

    sala de musica castelo

---
+++ apps/frontend/templates/sites/castelo/sala-de-musicaSuccess.php
@@ -0,0 +1,113 @@
+<link rel="stylesheet" href="/portal/css/tvcultura/secoes/defaultPrograma.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/castelo/geral.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/castelo/sala-de-musica.css" type="text/css" />
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+
+<div class="bg-site">
+</div>
+
+<!--CASTELO-->
+<div id="bg" >
+  
+    <!-- CAPA SITE -->
+    <div id="capa-site"> 
+      
+      <!--FANCYBOX-->
+      <script type="text/javascript" src="/portal/js/fancybox/jquery.fancybox-1.3.4.pack.js" ></script>
+      <link rel="stylesheet" href="/portal/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+      <!--/FANCYBOX-->     
+     
+
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+        
+        
+        <div class="topo-programa">
+          
+          <h2>
+            <a href="http://cmais.com.br/castelo" style="text-decoration: none;">
+          
+                <img src="/portal/images/capaPrograma/castelo/img-logo-castelo.png" class="logo" alt="Castelo Ra Tim Bum" />
+              
+            </a>
+          </h2>
+          
+
+          <?php if(isset($program) && $program->id > 0): ?>
+          <?php include_partial_from_folder('sites/castelo','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+          <?php endif; ?>
+          
+          
+        </div>
+
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+         <div class="castelo18">
+           <img src="/portal/images/capaPrograma/castelo/img-menu-hashtag.png" alt="#castelo18anos">
+           <a href="https://twitter.com/#!/search/realtime/castelo18anos" target="_blank"><img src="/portal/images/capaPrograma/castelo/btn-menu-twitter.png" alt="Twitter"></a>
+           <a href="#" target="_blank"><img src="/portal/images/capaPrograma/castelo/btn-menu-instagram.png" alt="Instangram"></a>
+
+         </div>
+         
+        </div>
+        <!-- /box-topo -->
+        
+        
+      </div>
+      <!-- /BARRA SITE -->
+
+      <!-- MIOLO -->
+      <div id="miolo">
+        
+        <?php include_partial_from_folder('blocks','global/shortcuts') ?>
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+          
+          <!-- CAPA -->
+          <div class="capa grid3">
+            
+           <!--tudo-->            
+           <div class="tudo">
+            
+                        
+            <?php if(isset($displays["penelope"])): ?>
+              <?php if(count($displays["penelope"]) > 0): ?>
+            <!--penelope-->
+              <div class="botao-penelope">
+                <div class="gif-penelope"></div>
+              </div>
+              
+              <a href="<?php echo $displays["penelope"][0]->retriveUrl()?>" title="<?php echo $displays["penelope"][0]->getTitle()?>" class="botao-penelope-over" name="over-penelope" style="display:none"></a>  
+            <!--/penelope-->
+              <?php endif; ?>  
+            <?php endif; ?>
+            
+            </div> 
+            <!--tudo-->
+
+            
+            <!-- MENU NAVEGAÇÃO-->
+            <?php include_partial_from_folder('sites/castelo','global/casteloMenuInternas') ?> 
+            <!--/MENU NAVEGAÇÃO-->
+            
+          </div>
+          <!-- /CAPA -->
+          
+        </div>
+        <!-- /CONTEUDO PAGINA -->
+        
+      </div>
+      <!-- /MIOLO -->
+      
+    </div>
+    <!-- /CAPA SITE -->
+    
+</div>   
+<!--/CASTELO-->
+
