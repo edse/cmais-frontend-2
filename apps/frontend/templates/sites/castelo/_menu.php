11d6a0c9a18e014ef115fdbb26fdf3371c0a9b36
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-08 09:05:17 -0300

    menu castelo

---
+++ apps/frontend/templates/sites/castelo/_menu.php
@@ -0,0 +1,32 @@
+          <?php if(count($siteSections) > 0): ?>
+          <!-- menu interna -->
+          <ul class="menu-interna grid3">
+            <?php foreach($siteSections as $s): ?>
+              <?php $subsections = $s->subsections(); ?>
+              <?php if(count($subsections) > 0): ?>
+                <li class="m-<?php echo $s->getSlug() ?> span"><a href="#" class="abre-menu" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?><span></span></a>
+                  <div class="submenu-interna toggle-menu" style="display:none; width: auto;">
+                    <ul style="display:block;">
+                    <?php foreach($subsections as $s): ?>
+                      <li><a href="<?php echo $s->retriveUrl()?>"><?php echo $s->getTitle()?></a></li>
+                    <?php endforeach; ?>
+                    </ul>
+                  </div>
+                </li>
+              <?php else: ?>
+                <li class="m-<?php echo $s->getSlug() ?>">
+                <?php if($section->getSlug()): ?>
+                  <?php if($s->getSlug() == $section->getSlug()): ?>
+                  <a class="selected" href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?></a>
+                  <?php else: ?>
+                  <a href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?></a>
+                  <?php endif; ?>
+                <?php else: ?>
+                  <a href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?></a>
+                <?php endif; ?>
+                </li>
+              <?php endif; ?>
+            <?php endforeach; ?>
+          </ul>
+          <!-- /menu interna -->
+          <?php endif; ?>
