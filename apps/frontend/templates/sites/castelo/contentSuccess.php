95745f43c4ca286fca560e5ae9caaadf6036e7a4
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-09 15:51:34 -0300

    content

--- apps/frontend/templates/sites/castelo/contentSuccess.php
+++ apps/frontend/templates/sites/castelo/contentSuccess.php
@@ -52,9 +52,6 @@
 <script src="/portal/js/orbit/jquery.orbit-1.2.3.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/castelo/geral.css" type="text/css" />
 
-<!--body data-twttr-rendered="true"style="background: none !important;">
-
-
 
 <div class="base">
   
