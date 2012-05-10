47b3bf86500a6101f85fb4b5b51bb54fad9336d3
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-05-10 16:38:44 -0300

    castelo

--- apps/frontend/templates/sites/castelo/_menu.php
+++ apps/frontend/templates/sites/castelo/_menu.php
@@ -35,8 +35,8 @@
           <?php endif; ?>
           
           <div class="castelo18">
-           <img src="/portal/images/capaPrograma/castelo/img-menu-hashtag.png" alt="#castelo18anos">
-           <a href="https://twitter.com/#!/search/realtime/castelo18anos" target="_blank"><img src="/portal/images/capaPrograma/castelo/btn-menu-twitter.png" alt="Twitter"></a>
-           <a href="http://statigr.am/viewer.php#/tag/castelo18anos/" target="_blank"><img src="/portal/images/capaPrograma/castelo/btn-menu-instagram.png" alt="Instangram"></a>
+
+           <a href="https://twitter.com/#!/search/realtime/castelo18anos" target="_blank"><img class="twitter" src="/portal/images/capaPrograma/castelo/btn-menu-twitter.png" alt="Twitter"></a>
+           <a href="http://statigr.am/viewer.php#/tag/castelo18anos/" target="_blank"><img class="instangram" src="/portal/images/capaPrograma/castelo/btn-menu-instagram.png" alt="Instangram"></a>
 
          </div>
