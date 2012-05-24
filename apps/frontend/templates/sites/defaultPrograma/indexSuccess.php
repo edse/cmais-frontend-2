d06730c3cd4cb65c0cfacc7321782edd896ecc10
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-05-24 18:29:57 -0300

    tweet monitor em cima do FB

--- apps/frontend/templates/sites/defaultPrograma/indexSuccess.php
+++ apps/frontend/templates/sites/defaultPrograma/indexSuccess.php
@@ -182,9 +182,9 @@
                 <!-- /BOX PADRAO + Visitados -->
               <?php endif; ?> 
 
-              <?php include_partial_from_folder('blocks','global/facebook-1c', array('site' => $site, 'uri' => $uri)) ?>
-
               <?php if(isset($displays["twitter"])) include_partial_from_folder('blocks','global/twitter-1c', array('site' => $site, 'uri' => $uri)) ?>
+              
+              <?php include_partial_from_folder('blocks','global/facebook-1c', array('site' => $site, 'uri' => $uri)) ?>
               
             </div>
             <!-- /DIREITA -->
