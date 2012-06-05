b43a086454abec689e5d7855f148164c346839de
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-05 18:12:51 -0300

    enquete

--- apps/frontend/templates/blocks/_display-1c-poll.php
+++ apps/frontend/templates/blocks/_display-1c-poll.php
@@ -11,16 +11,53 @@
                   <div class="conteudo enquete">
                     <div class="txt-padrao">
                       <p><?php echo $displays[0]->Asset->AssetQuestion->getQuestion() ?></p>
-                      <form action="" method="post">
+                      <div id="form">
+                      <form method="post" id="e<?php echo $displays[0]->Asset->getId()?>">
+                        <?php
+                         $form = new BaseForm();
+                         echo $form->renderHiddenFields() ?>
                         <?php foreach($displays[0]->Asset->AssetQuestion->Answers as $a): ?>
                         <div class="linha">
-                          <input type="radio" id="<?php echo $a->getId() ?>" name="opcao" /><label for="<?php echo $a->getId() ?>"><?php echo $a->getAnswer() ?></label>
+                          <input type="radio" id="opcao<?php echo $a->getId() ?>" name="opcao" class="required" value="<?php echo $a->getId() ?>" /><label for="opcao<?php echo $a->getId() ?>"><?php echo $a->getAnswer() ?></label>
                         </div>
                         <?php endforeach; ?>
                         <input class="votar escuro" type="submit" value="votar" id="votar" />
+                        <img src="/images/spinner_bar.gif" style="display: none" id="v_load" />
                       </form>
-                      <?php /* 
-                      <div class="resultado" style="display:none">
+                      </div>
+                      <script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
+                      <script>
+                        $(document).ready(function(){
+
+                          var validator = $('#e<?php echo $displays[0]->Asset->getId()?>').validate({
+                            submitHandler: function(form){
+                              $.ajax({
+                                type: "POST",
+                                dataType: "json",
+                                data: $("#e<?php echo $displays[0]->Asset->getId()?>").serialize(),
+                                url: "<?php //echo url_for('homepage')?>/ajax/enquetes",
+                                beforeSend: function(){
+                                  $('#votar').hide();
+                                  $('#v_load').show();
+                                },
+                                success: function(data){
+                                  $.each(data, function(key, val) {
+                                    $('#r<?php echo $displays[0]->Asset->getId()?>').append('<p>'+val.answer+'</p>');
+                                    $('#r<?php echo $displays[0]->Asset->getId()?>').append('<div class="porcentagem"><p style="width:'+val.votes+'">'+val.votes+'</p></div>');
+                                  });
+                                  //alert(data)
+                                  $('#r<?php echo $displays[0]->Asset->getId()?>').show();
+                                  $('#form').hide(); 
+                                  $('#v_load').hide();
+                                }
+                              });
+                            }
+                          });
+                        });
+                      </script>
+
+                      <div id="r<?php echo $displays[0]->Asset->getId()?>" class="resultado" style="display:none">
+                        <?php /*
                         <p>Opcao 01</p>
                         <div class="porcentagem">
                           <p class="p70">70%</p>
@@ -33,8 +70,9 @@
                         <div class="porcentagem">
                           <p class="p10">10%</p>
                         </div>
+                         */?>
                       </div>
-                      */ ?>
+
                     </div>
                   </div>
                   <div class="detalhe-borda grid1"></div>
