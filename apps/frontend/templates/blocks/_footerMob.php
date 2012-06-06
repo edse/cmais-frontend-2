1af0e81ce750900861f539509face2ea0b46c87d
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-06 17:28:46 -0300

    footer cmais mobile

---
+++ apps/frontend/templates/blocks/_footerMob.php
@@ -0,0 +1,25 @@
+	<!--FOOTER-->
+	<div class="footer programas" align="center">
+		
+		<!-- FIO LARANJA -->
+		<div class="fio"></div>
+		<!-- /FIO LARANJA -->
+		
+		<div class="footerBotoes">
+		  
+		    <p>Mobile <span>|</span> <a>Web</a></p>
+		    <p>Copyright © 1996 - <?php echo date('Y') ?> Fundação Padre Anchieta</p>
+		  
+		  
+		</div>
+		
+	</div>
+	<!--/FOOTER-->
+	
+</div>
+<!--/JQUERY MOBILE-->
+</body>
+<!--/Body-->
+
+</html>
+<!--/html-->
