1816cd970509aa2d467843e74e666262dd010ab8
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-06-06 15:10:03 -0300

    quintal

--- apps/frontend/templates/sites/quintaldacultura/videosSuccess.php
+++ apps/frontend/templates/sites/quintaldacultura/videosSuccess.php
@@ -20,7 +20,6 @@
     
     <link rel="stylesheet" href="/portal/css/geral.css" type="text/css" />
     <link rel="stylesheet" href="/portal/quintal/css/geralQuintal.css" type="text/css" />
-    <link rel="stylesheet" href="/portal/quintal/css/videosQuintal.css" type="text/css" />
        
     
     <!-- scripts -->
