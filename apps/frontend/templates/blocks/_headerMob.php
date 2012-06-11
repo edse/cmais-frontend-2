990f9398e06b2628d93d05379be40c08a3ce78b7
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-06-11 13:19:22 -0300

    mobile

--- apps/frontend/templates/blocks/_headerMob.php
+++ apps/frontend/templates/blocks/_headerMob.php
@@ -93,16 +93,13 @@
             function verificaTela(){
               //orintacao para iphone e samsung galaxy
              
-              if(orientacao == 0  && $('#cmais').width() <= 330 || orientacao == 180  && $('#cmais').width() <= 330){
+              if(orientacao == 0  && $('#cmais').width() <= 410 || orientacao == 180  && $('#cmais').width() <= 410){
                 quant = 2;
                 liga = true;
               }else if(orientacao == 90 && $('#cmais').width() <= 490 || orientacao == -90 && $('#cmais').width() <= 490){
                 quant = 3;
                 liga = true;
-              }else if($('#cmais').width() == 400) {
-                quant = 2;
-                liga = true;
-              }else if($('#cmais').width() <= 683) {
+              }else if($('#cmais').width() <= 764) {
                 quant = 4;
                 liga = true;
               }else if($('#cmais').width() >= 1024) {
