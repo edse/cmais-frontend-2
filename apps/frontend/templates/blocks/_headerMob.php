e7bda4e9cc80f022918277a0b33a67d7a253980b
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-06-11 15:53:23 -0300

    mobile

--- apps/frontend/templates/blocks/_headerMob.php
+++ apps/frontend/templates/blocks/_headerMob.php
@@ -95,11 +95,11 @@
              
               if($('#cmais').width() >= 100 ){
                 $('#destaque').css('width','320px'); 
-                quant = 3;
+                quant = 2;
                 liga = true;
               }if($('#cmais').width() >= 410){
                 $('#destaque').css('width','480px'); 
-                quant = 2;
+                quant = 3;
                 liga = true;
               }else if($('#cmais').width() >= 1024 ){
                 $('#destaque').css('width','800px'); 
