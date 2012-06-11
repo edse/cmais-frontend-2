4a3b3daed3714ab0632912fc15f48e4a1d08d221
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-06-11 11:35:52 -0300

    mobile

--- apps/frontend/templates/blocks/_headerMob.php
+++ apps/frontend/templates/blocks/_headerMob.php
@@ -92,18 +92,22 @@
            
             function verificaTela(){
               //orintacao para iphone e samsung galaxy
-              if(orientacao == 0  && $('#destaque').width() <= 450 || orientacao == 180  && $('#destaque').width() <= 450){
+              if(orientacao == 0  && $('#destaque').width() <= 300 || orientacao == 180  && $('#destaque').width() <= 300){
                 quant = 2;
                 liga = true;
-              }else if(orientacao == 90 && $('#destaque').width() >= 450 || orientacao == -90 && $('#destaque').width() >= 450){
+              }else if(orientacao == 90 && $('#destaque').width() <= 490 || orientacao == -90 && $('#destaque').width() <= 490){
                 quant = 3;
                 liga = true;
               }
               //orientacao para samsung tablet
-              /*else if($('#destaque').width() <= 800){
+              else if($('#destaque').width() <= 640){
+                quant = 4;
+                liga = false
+              }
+              else if($('#destaque').width() > 640){
                 quant = 5;
                 liga = false
-              }*/
+              }
               
               
             }
