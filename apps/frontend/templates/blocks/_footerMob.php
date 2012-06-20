63b8cfcbb1bb91b0f24e1ad05e8c0c7c252ea4a3
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-20 14:43:38 -0300

    update

--- apps/frontend/templates/blocks/_footerMob.php
+++ apps/frontend/templates/blocks/_footerMob.php
@@ -4,26 +4,34 @@
 		<!-- FIO LARANJA -->
 		<div class="fio"></div>
 		<!-- /FIO LARANJA -->
+		<?php
+			$url = '';
+			if ($site->getSlug() != 'm') {
+				if (isset($asset))
+					$url = url_for(@homepage) . $site->getSlug() . '/' . $asset->getSlug() . '?from=m';
+				else
+					$url = url_for(@homepage) . $site->getSlug() . '?from=m';
+			}
+			else {
+				if (isset($section)) {
+					$url = url_for(@homepage) . 'cmais/' . $section->getSlug() . '?from=m';
+					if ($section->getSlug() == 'culturafm' || (isset($_GET['url']) && strpos($_GET['url'], "radiofm")))
+						$url = 'http://culturafm.cmais.com.br?from=m';
+					if ($section->getSlug() == 'culturabrasil' || (isset($_GET['url']) && strpos($_GET['url'], "culturabrasil.com.br")))
+						$url = 'http://www.culturabrasil.com.br';
+					if ($section->getSlug() == 'programas')
+						$url = url_for(@homepage) . 'cmais/programas-de-a-z?from=m';
+				}
+			}
+		?>
 		
 		<div class="footerBotoes">
-		  <?php if ($site->getSlug() != 'm'): ?>
-		  	<?php if (isset($asset)): ?>
-		    <p><a href="<?php echo url_for(@homepage) . $site->getSlug() . '/' . $asset->getSlug() ?>?from=m" title="versão clássica" rel="external">Versão clássica</a></p>
-		    <?php else: ?>
-		    <p><a href="<?php echo url_for(@homepage) . $site->getSlug() ?>?from=m" title="versão clássica" rel="external">Versão clássica</a></p>
-		    <?php endif; ?>
+			<?php if ($url != ''): ?>
+		  <p><a href="<?php echo $url ?>" title="versão clássica" rel="external">Versão clássica</a></p>
 		  <?php else: ?>
-	  		<?php if (isset($section)): ?>
-		  		<?php if ($section->getSlug() == 'culturafm' || (isset($_GET['url']) && strpos($_GET['url'], "radiofm"))): ?>
-		    <p><a href="http://culturafm.cmais.com.br?from=m" title="versão clássica" rel="external">Versão clássica</a></p>
-	    		<?php elseif ($section->getSlug() == 'culturabrasil' || (isset($_GET['url']) && strpos($_GET['url'], "culturabrasil.com.br"))): ?>
-		    <p><a href="http://www.culturabrasil.com.br" title="versão clássica" rel="external">Versão clássica</a></p>
-			    <?php else: ?>
-		  	<p><a href="<?php echo url_for(@homepage) ?>cmais?from=m" title="versão clássica" rel="external">Versão clássica</a></p>
-		  		<?php endif; ?>
-		  	<?php endif; ?>
+			<p><a href="<?php echo url_for(@homepage) . $site->getSlug() . '?from=m' ?>" title="versão clássica" rel="external">Versão clássica</a></p>
 		  <?php endif; ?>
-		    <p>Copyright © 1996 - <?php echo date('Y') ?> Fundação Padre Anchieta</p>
+		  <p>Copyright © 1996 - <?php echo date('Y') ?> Fundação Padre Anchieta</p>
 		</div>
 		
 	</div>
