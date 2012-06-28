74ca26d44ccec5df17d7b960af33c08e7c13844c
edse <emerson.estrella@gmail.com>
2012-06-28 12:50:23 -0300

    like btn url change

--- apps/frontend/templates/blocks/_like.php
+++ apps/frontend/templates/blocks/_like.php
@@ -4,7 +4,7 @@
               <div style="display:block; float: left; margin-right:10px;">
               <g:plusone size="medium" count="false"></g:plusone>
               </div>
-              <fb:like href="<?php echo $uri ?>" layout="button_count" show_faces="false" send="true" width="160"></fb:like>
+              <fb:like href="<?php if($site->getFacebookUrl()): ?><?php echo $site->getFacebookUrl() ?><?php else: ?><?php echo $uri ?><?php endif; ?>" layout="button_count" show_faces="false" send="true" width="160"></fb:like>
             </div>
             <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php if($site->getTwitterAccount()): ?><?php echo $site->getTwitterAccount() ?><?php else: ?>tvcultura<?php endif; ?>">Tweet</a>
             <ul>                         
