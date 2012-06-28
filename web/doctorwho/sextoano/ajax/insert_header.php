c3fbde4b1f19f54719cb4aa5afe2f8ed0ee8ce5e
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-28 15:30:00 -0300

    test

--- web/doctorwho/sextoano/ajax/insert_header.php
+++ web/doctorwho/sextoano/ajax/insert_header.php
@@ -153,12 +153,12 @@
  * 
  */
 	$return = '
-		<link rel="stylesheet" href="/portal/css/geral.css?nocache=12345" type="text/css" />
 		<link type="text/css" rel="stylesheet" href="/doctorwho/sextoano/css/style.css" />	
     <link rel="stylesheet" href="/doctorwho/css/geral.css" type="text/css"/>
     <link rel="stylesheet" href="/portal/css/tvcultura/geral2.css" type="text/css" />
     <link type="text/css" rel="stylesheet" href="/doctorwho/sextoano/css/style.css" />
     <script type="text/javascript" src="/portal/js/portal.js"></script>
+		<link rel="stylesheet" href="/portal/css/geral.css?nocache=12345" type="text/css" />
     
 <div id="guia-topo" align="center">
   <!--topo Cmais-->
