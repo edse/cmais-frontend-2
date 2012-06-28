73da706bf542dc6b95715233c417b3ed1e2d52c2
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-28 12:27:44 -0300

    update css topo novo

--- web/vilasesamo/ajax/insert_header.php
+++ web/vilasesamo/ajax/insert_header.php
@@ -161,6 +161,7 @@
     </div>
     <!-- /TOPO PORTAL -->';*/
 	$return = '
+	<link rel="stylesheet" href="/portal/css/geral.css?nocache=1234" type="text/css" />
 <div id="guia-topo" align="center">
   <!--topo Cmais-->
   <div id="topo-cmais">
