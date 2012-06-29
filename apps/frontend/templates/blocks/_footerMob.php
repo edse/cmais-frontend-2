3fabf3ba5bf748da7176886c6a3efb6dd0e9ae1b
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-06-29 20:37:14 -0300

    update cookie expiration when browse close

--- apps/frontend/templates/blocks/_footerMob.php
+++ apps/frontend/templates/blocks/_footerMob.php
@@ -51,7 +51,7 @@
 		 */
 		?>
 		<div class="footerBotoes">
-		  <p><a href="javascript:setCookieAndRedirect('classic','yes',30,'/','cmais.com.br','http://cmais.com.br');" title="versão clássica" rel="external">Versão clássica</a></p>
+		  <p><a href="javascript:setCookieAndRedirect('classic','yes','','/','cmais.com.br','http://cmais.com.br');" title="versão clássica" rel="external">Versão clássica</a></p>
 		  <p>Copyright © 1996 - <?php echo date('Y') ?> Fundação Padre Anchieta</p>
 		</div>
 		
