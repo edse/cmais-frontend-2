762338a0783229ea13f4579f3d9971281174c137
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-02 20:22:30 -0300

    test

--- apps/frontend/templates/sites/m/transmissaoSuccess.php
+++ apps/frontend/templates/sites/m/transmissaoSuccess.php
@@ -66,8 +66,8 @@
 	<!-- /TOPO -->
 	
 	<!-- CONTEUDO -->
-	<div class="conteudoLive">
-		<embed type="application/x-shockwave-flash" src="/portal/js/mediaplayer/player.swf" width="100%" id="mpl" name="mpl" quality="high" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" flashvars="controlbar=over&amp;autostart=true&amp;streamer=rtmp://200.136.27.12/live&amp;file=tv&amp;type=video">		
+	<div class="conteudoLive" style="width:100%">
+		<embed type="application/x-shockwave-flash" src="/portal/js/mediaplayer/player.swf" width="100%" height="364" id="mpl" name="mpl" quality="high" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" flashvars="controlbar=over&amp;autostart=true&amp;streamer=rtmp://200.136.27.12/live&amp;file=tv&amp;type=video">		
 	</div>
 	<!-- /CONTEUDO -->
 		
