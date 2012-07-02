84b4d4dcb2dd73a44f628e7b922724723454cffc
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-02 19:46:53 -0300

    test

--- apps/frontend/templates/sites/m/liveSuccess.php
+++ apps/frontend/templates/sites/m/liveSuccess.php
@@ -5,7 +5,7 @@
 	  ->from('Schedule s')
 	  ->where('s.channel_id = 1')
 	  ->andWhere('s.date_start <= ? AND s.date_end > ?', array(date('Y-m-d H:i:s', time()), date('Y-m-d H:i:s', time())))
-    ->andWhere('is_live = 1')
+    ->andWhere('s.is_live = 1')
 	  ->orderBy('s.date_start desc')
 		->limit(1)
 		->fetchOne();
@@ -15,7 +15,7 @@
     ->from('Schedule s')
     ->where('s.channel_id = 1')
     ->andWhere('s.date_start >= ?', date('Y-m-d H:i:s'))
-    ->andWhere('is_live = 1')
+    ->andWhere('s.is_live = 1')
     ->orderBy('s.date_start asc')
     ->limit(4)
     ->execute();
