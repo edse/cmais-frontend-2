9b4cf5853527d00d436f947995b3ee4f16a18b57
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-02 15:30:08 -0300

    banner ra tim bum

---
+++ apps/frontend/templates/tvratimbum/_banner-300x250.php
@@ -0,0 +1,15 @@
+<!--BANNER-->          
+<div id="banner-rtb-300">
+  <!--TOPO-->
+  <div class="topo-banner"></div>
+  <!--/TOPO-->
+  <div id="banner">
+    <a href="http://www.facebook.com/ratimbum" title="fanpage">
+      <img src="/portal/tvratimbum/image/fanpagetvrtb.jpg" alt="propaganda" />
+    </a>
+  </div>
+  <!--RODAPE-->
+  <div class="rodape-banner"></div>
+  <!--/RODAPE-->
+</div>  
+<!--BANNER-->
