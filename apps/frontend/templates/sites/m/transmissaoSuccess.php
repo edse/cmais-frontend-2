134a98f96c6ed33bf73c50b05f8e91316719e5c9
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-03 17:29:17 -0300

    test

--- apps/frontend/templates/sites/m/transmissaoSuccess.php
+++ apps/frontend/templates/sites/m/transmissaoSuccess.php
@@ -29,6 +29,16 @@
   </script>
   <!--/GOOGLE ANALYTICS-->
   <script>
+	  function checkStreamingStart(){
+	    var request = $.ajax({
+	      dataType: 'jsonp',
+	      success: function(data) {
+	        eval(data);
+	      },
+	      url: '/ajax/streaming'
+	    });
+	  }
+  
 	  function checkStreamingEnd(){
 	    var request = $.ajax({
 	      //dataType: 'jsonp',
@@ -42,7 +52,7 @@
 	  }
 	  
 	  $(window).load(function(){
-	    checkStreamingEnd();
+	    checkStreamingStart();
 	    te=setInterval("checkStreamingEnd()",60000);
 	  });
   </script>
@@ -128,7 +138,8 @@
 	</script>
 	<!-- CONTEUDO -->
 	<div class="conteudoLive">
-		<embed type="application/x-shockwave-flash" src="/portal/js/mediaplayer/player.swf" width="640" height="364" id="mpl" name="mpl" quality="high" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" flashvars="controlbar=over&amp;autostart=true&amp;streamer=rtmp://200.136.27.12/live&amp;file=tv&amp;type=video">		
+		<div id="livestream2" style="display: none;"><p>Seu browser não suporta Flash.</p></div>
+		<!-- embed type="application/x-shockwave-flash" src="/portal/js/mediaplayer/player.swf" width="640" height="364" id="mpl" name="mpl" quality="high" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" flashvars="controlbar=over&amp;autostart=true&amp;streamer=rtmp://200.136.27.12/live&amp;file=tv&amp;type=video"-->		
 	</div>
 	<!-- /CONTEUDO -->
 		
