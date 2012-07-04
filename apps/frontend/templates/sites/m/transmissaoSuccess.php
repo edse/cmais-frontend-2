87466318869bf8ec9dbbf7cfa9bb1f6518f45ccb
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-04 19:43:24 -0300

    test

--- apps/frontend/templates/sites/m/transmissaoSuccess.php
+++ apps/frontend/templates/sites/m/transmissaoSuccess.php
@@ -105,12 +105,6 @@
 	<script>
 	  function verificaTela()
 	  {
-	  	alert("window.width: "+window.width);
-	  	alert("document.width: "+document.width);
-	  	alert("$(window).Width(): "+$(window).Width());
-	  	alert("$(window).width(): "+$(window).width());
-	  	alert("$(window).Width: "+$(window).Width);
-	  	alert("$(window).width: "+$(window).width);
 	  	i = 364 / 640;
 	  	mobWidth = document.width;
 	  	mobHeight = Math.round(mobWidth * i);
