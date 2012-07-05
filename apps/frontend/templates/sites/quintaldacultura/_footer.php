be8fdce087244e99bfae518cb8df62788cfc875b
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-05 18:54:49 -0300

    rodapequintal

--- apps/frontend/templates/sites/quintaldacultura/_footer.php
+++ apps/frontend/templates/sites/quintaldacultura/_footer.php
@@ -6,7 +6,7 @@
             <li><a href="/quintaldacultura/personagens">Personagens</a></li>
             <li><a href="/quintaldacultura/publico-alvo">P&uacute;blico Alvo</a></li>
             <li><a href="/quintaldacultura/diretrizes-pedagogicas">Diretrizes Pedag&oacute;gicas</a></li>
-            <li><a href="/quintaldacultura/desenhos">Desenhos</a></li>
+            <li style="margin-right:0px !important"><a href="/quintaldacultura/desenhos">Desenhos</a></li>
           </ul>
         </div>
       </div>
