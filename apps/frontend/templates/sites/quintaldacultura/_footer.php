39ed7fce0dd6c7707eba39eda586568e9badff9f
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-05 19:09:27 -0300

    quintal

--- apps/frontend/templates/sites/quintaldacultura/_footer.php
+++ apps/frontend/templates/sites/quintaldacultura/_footer.php
@@ -2,7 +2,7 @@
         <hr />
         <div class="menuRodape">
           <ul>
-            <li style="margin-right:0px !important"><a href="/quintaldacultura/sobre-o-quintal-da-cultura">Sobre o Programa</a></li>
+            <li ><a href="/quintaldacultura/sobre-o-quintal-da-cultura">Sobre o Programa</a></li>
             <li><a href="/quintaldacultura/personagens">Personagens</a></li>
             <li><a href="/quintaldacultura/publico-alvo">P&uacute;blico Alvo</a></li>
             <li><a href="/quintaldacultura/diretrizes-pedagogicas">Diretrizes Pedag&oacute;gicas</a></li>
