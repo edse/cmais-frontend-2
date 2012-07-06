498fe945651fe6907427b8132d3ce0c4dd52f10c
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-06 09:17:34 -0300

    test

--- apps/frontend/templates/sites/quintaldacultura/voce-escolheSuccess.php
+++ apps/frontend/templates/sites/quintaldacultura/voce-escolheSuccess.php
@@ -81,7 +81,7 @@ $img_b = "http://midia.cmais.com.br/assets/image/original/".$imgs[0]->AssetImage
           $('#espera').fadeIn('slow');
         },
         success: function(data){
-        	window.alert(data);
+        	//window.alert(data);
         	
           var i=1;
           $.each(data, function(key, val) {
