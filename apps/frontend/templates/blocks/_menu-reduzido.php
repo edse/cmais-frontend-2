5789e83fad926113934acad0c5902415f81100fe
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-06 18:15:34 -0300

    menu

---
+++ apps/frontend/templates/blocks/_menu-reduzido.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/menu-reduzido.css" type="text/css" />
+<!-- TOPO PORTAL -->
+<div id="menu-reduzido">
+  <!--TOPO-CONTADOR-->
+  <div id="topo-contador">
+    <a href="http://cmais.com.br" title="cmais" target="_parent"><img class="cmais" src="/portal/images/menu-reduzido/logoCMAIS.png" alt="cmais+" /></a>
+      <!--LINKS-->
+      <ul class="links">
+        <li><a href="http://tvcultura.com.br" class="barra tvcultura" target="_blank" title="TV Cultura"></a></li>
+        <li><a href="http://tvratimbum.com.br" class="barra tvratimbum" target="_blank" title="TV Rá Tim Bum"></a></li>
+        <li><a href="http://multicultura.com.br" class="barra multicultura" target="_blank" title="MultiCultura"></a></li>
+        <li><a href="http://univesp.tv.br" class="barra tvunivesp" target="_blank" title="Univesp TV"></a></li>
+        <li><a href="http://culturabrasil.com.br" class="barra culturabrasil" target="_blank" title="Cultura Brasil"></a></li>
+        <li><a href="http://www.culturafm.com.br" class="barra culturafm" target="_blank" alt="Cultura FM" title="Cultura FM"></a></li>
+        <li><a href="http://itunes.apple.com/br/app/radio-cultura/id370066053" target="_blank" class="barra itunes" title="Apple Store"></a></li>
+        <li><a href="http://facebook.com/tvcultura" class="curtir h13 facebook" target="_blank" title="Curta"></a></li>
+        <li><a href="https://plus.google.com/b/107290730774038797358/107290730774038797358" target="_blank" class="barra google" target="_blank" title="Google+"></a></li>
+        <li><a href="http://statigr.am/tvcultura" class="barra instangram" target="_blank" title="Instagram"></a></li>
+        <li><a href="http://twitter.com/tvcultura" class="curtir twitter" target="_blank" title="Siga"></a></li>
+        <li><a href="http://youtube.com/cultura" class="curtir youtube" target="_blank" title="Assista"></a></li>
+        <li><a href="http://tvcultura.cmais.com.br/feed" class="curtir rss" target="_blank" title="Assista"></a></li>
+     </ul>
+     <!--/LINKS-->
+   </div>
+   <!--/TOPO-CONTADOR-->
+</div>
+<!-- /TOPO PORTAL -->
+          
+    
+
