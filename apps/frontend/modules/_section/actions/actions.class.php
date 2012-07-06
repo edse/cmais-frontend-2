df76bce66ada705b70c5e29d2feb630ca06fde80
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-06 17:10:00 -0300

    test

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -57,24 +57,29 @@ class _sectionActions extends sfActions
 			
 	    if(($this->site->getSlug() == "quintaldacultura") && $request->getParameter('force') == "1"){
 	    	if(in_array($this->section->getSlug(), array('home-page','homepage','home'))){
-	    		/*
-	    		if(date('w') == 5 && date('H:i') >= "12:30") {
-	    			die("1");
-					}
-					 */
+					/*
 	        if(date('y-m-d H:i:s') >= "2012-07-06 12:00:00" && date('y-m-d H:i:s') <= "2012-07-06 18:00:00") {
 	          header("Location: http://cmais.com.br/quintaldacultura/voceescolhe");
 	      		die(); 
 	        }
+					*/
+	    		if(date('w') == 5 && date('H:i') >= "12:00") {
+	          header("Location: http://cmais.com.br/quintaldacultura/voceescolhe");
+	      		die(); 
+					}
 	     	}
-				/*
 	    	if($this->section->getSlug() == 'voceescolhe'){
+	    		/*
 	        if(date('y-m-d H:i:s') < "2012-07-06 12:00:00" || date('y-m-d H:i:s') > "2012-07-06 18:00:00") {
 	          header("Location: http://cmais.com.br/quintaldacultura");
 	      		die(); 
 	        }
-	     	}
-				 */
+					*/
+	    		if(date('w') == 5 && date('H:i') > "18:00") {
+	          header("Location: http://cmais.com.br/quintaldacultura");
+	      		die(); 
+					}
+				}
 	    }
 
     
