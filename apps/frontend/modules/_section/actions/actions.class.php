23e38f37366c7ac5e53692fbe83336683cb17560
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-06 16:55:18 -0300

    test

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -58,10 +58,10 @@ class _sectionActions extends sfActions
 	    if(($this->site->getSlug() == "quintaldacultura") && $request->getParameter('force') == "1"){
 	    	if(in_array($this->section->getSlug(), array('home-page','homepage','home'))){
 	    		
-	        if(date('y-m-d H:i:s') >= "2012-07-06 12:00:00" && date('y-m-d H:i:s') < "2012-07-06 18:00:00") {
+	        if(date('y-m-d H:i:s') > "2012-07-06 12:00:00" && date('y-m-d H:i:s') < "2012-07-06 18:00:00") {
 	        die(date('y-m-d H:i:s'));	
-	          header("Location: http://cmais.com.br/quintaldacultura/voceescolhe");
-	      		die(); 
+	          //header("Location: http://cmais.com.br/quintaldacultura/voceescolhe");
+	      		//die(); 
 	        }
 	     	}
 	    	if($this->section->getSlug() == 'voceescolhe'){
