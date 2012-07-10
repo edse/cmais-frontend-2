50d7f39f0afeff4d6e13f71e435a4e580a0e0002
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-10 12:55:13 -0300

    test

--- apps/frontend/templates/sites/m/_display5c.php
+++ apps/frontend/templates/sites/m/_display5c.php
@@ -17,11 +17,15 @@
 		      <a href="<?php echo url_for('homepage') . 'programa/' . $d->Asset->Site->getSlug() ?>" data-transition="slidedown" rel="external">
 		        <div class="touchcarousel-a" >
 		          <!--LOGO-->
+		          <?php if ($d->Asset->Site->getImageIcon()): ?>
 		          <img title="<?php echo $d->Asset->Site->Program->getTitle() ?>" alt="<?php echo $d->Asset->Site->Program->getTitle() ?>" src="http://midia.cmais.com.br/programs/<?php echo $d->Asset->Site->getImageIcon() ?>" width="100%">
+							<?php endif; ?>		          
 		          <!--/LOGO-->
+		          <?php if ($d->retriveImageUrlByImageUsage('image-9')): ?>
 		          <!--FOTO-->
 		          <img alt="<?php echo $d->getTitle() ?>" src="<?php echo $d->retriveImageUrlByImageUsage('image-9') ?>" width="100%">
 		          <!--/FOTO-->
+		          <?php endif; ?>
 		          <!--DESCRICAO / HORA-->
 		          	<p><?php echo $d->getTitle() ?></p>
 		          	<p><?php echo $d->getLabel() ?></p>
