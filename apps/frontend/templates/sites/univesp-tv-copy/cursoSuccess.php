58469a142ab6f4d1f8c8348a9de43044cfb9f484
edse <emerson.estrella@gmail.com>
2012-07-11 19:09:06 -0300

    univesptv copy

--- apps/frontend/templates/sites/univesp-tv-copy/cursoSuccess.php
+++ apps/frontend/templates/sites/univesp-tv-copy/cursoSuccess.php
@@ -288,7 +288,7 @@ $asset = $pager->getCurrent();
                         <?php $k=0; foreach($assets as $d): $k++; ?>
                           <li class="conteudo-lista">
                             <!-- <a href="<?php echo $site->retriveUrl(); ?>?page=<?php echo $k?>" class="episodio<?php if(($page == $k)||(!$page && $k==1)):?> ativo<?php endif; ?>">Epis&oacute;dio<span><?php echo $k; ?></span></a> -->
-                            <a href="<?php echo $section->retriveUrl(); ?>?page=<?php echo $k?>" class="titulos"><?php echo $d->getTitle(); ?></a><!-- <a href="<?php echo $d->retriveUrl(); ?>"><?php echo $d->getDescription(); ?></a> --></li>
+                            <a href="http://cmais.com.br/frontend_dev.php/univesp-tv-copy/<?php echo $site->getSlug(); ?>?page=<?php echo $k?>" class="titulos"><?php echo $d->getTitle(); ?></a><!-- <a href="<?php echo $d->retriveUrl(); ?>"><?php echo $d->getDescription(); ?></a> --></li>
                         <?php endforeach; ?>
                       </ul>
                     </div>
