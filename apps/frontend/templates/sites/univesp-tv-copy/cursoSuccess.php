4c734861190d235b6d87909d46a2d862d4f30aa7
edse <emerson.estrella@gmail.com>
2012-07-11 19:15:35 -0300

    univesptv copy

--- apps/frontend/templates/sites/univesp-tv-copy/cursoSuccess.php
+++ apps/frontend/templates/sites/univesp-tv-copy/cursoSuccess.php
@@ -111,12 +111,21 @@ $asset = $pager->getCurrent();
                       <?php
                       if($asset->AssetType->getSlug() == "video") 
                         $video = $asset;
+                      else if($asset->AssetType->getSlug() == "video-gallery") 
+                        $video = $asset;
                       else{
                         $video = $asset->retriveRelatedAssetsByAssetTypeId(6);
                         $video = $video[0];
                       }
                       ?>
                       <?php if($video->AssetVideo->getYoutubeId() != ""): ?>
+                      <div class="media grid2">
+                        <?php include_partial_from_folder('blocks','global/asset-2c-video', array('asset' => $video)) ?>
+                        <?php /* <div><?php echo $asset->getDescription() ?></div> */ ?>
+                      </div>
+                      <?php endif; ?>
+
+                      <?php if($video->AssetVideoGallery->getYoutubeId() != ""): ?>
                       <div class="media grid2">
                         <?php include_partial_from_folder('blocks','global/asset-2c-video', array('asset' => $video)) ?>
                         <?php /* <div><?php echo $asset->getDescription() ?></div> */ ?>
