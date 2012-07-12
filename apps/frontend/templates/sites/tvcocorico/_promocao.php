a6bcda92871ad0dbb216de87da28613012c9b55d
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-12 19:06:52 -0300

    form

---
+++ apps/frontend/templates/sites/tvcocorico/_promocao.php
@@ -0,0 +1,196 @@
+<script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
+<script type="text/javascript">
+      $(document).ready(function(){
+
+        //form filomena
+        $('#respFilomena .btn-enviar').click(function(){
+          $('#respFilomena').hide();
+          $('#pFilomena').show();
+        });
+        
+        var validator = $('#form-contato').validate({
+          submitHandler: function(form){
+            $.ajax({
+              type: "POST",
+              dataType: "text",
+              url:"<?php echo url_for('homepage')?>ajax/mensagem",
+              data: $("#form-contato").serialize(),
+              beforeSend: function(){
+                $('#form-contato input#enviar').hide();
+                $('img#ajax-loader').show();
+              },
+              success: function(data){
+                $('#form-contato input#enviar').show()
+                $('#pFilomena').hide();
+                $('#respFilomena').show();
+                //if(data == "1"){
+                  $("#form-contato").clearForm();
+                  $('img#ajax-loader').hide();
+                //}
+              }
+            });         
+          },
+          rules:{
+            nome:{
+              required: true,
+              minlength: 2
+            },
+            email:{
+              required: true,
+              email: true
+            },
+            cidade:{
+              required: true,
+              minlength: 2
+            },
+            idade:{
+              required: true,
+              minlength: 2
+            },
+            estado:{
+              required: true
+            },
+            mensagem:{
+              required: true,
+              minlength: 5
+            }
+          },
+          messages:{
+            nome: "*os campos em vermelho apresentam erro de preenchimento!",
+            email: "*os campos em vermelho apresentam erro de preenchimento!",
+            cidade: "*os campos em vermelho apresentam erro de preenchimento!",
+            idade: "*os campos em vermelho apresentam erro de preenchimento!",
+            estado: "*os campos em vermelho apresentam erro de preenchimento!",
+            mensagem: "*os campos em vermelho apresentam erro de preenchimento!" 
+          },
+          success: function(label){
+            // set &nbsp; as text for IE
+            label.html("&nbsp;").addClass("checked");
+          }
+        });
+        //form filomena
+      });  
+          
+      // Contador de Caracters
+      function limitText (limitField, limitNum, textCounter)
+      {
+        if (limitField.value.length > limitNum)
+          limitField.value = limitField.value.substring(0, limitNum);
+        else
+          $(textCounter).html(limitNum - limitField.value.length);
+      }
+      
+      
+    </script>
+<!--Pergunte a Filomena-->
+<div id="promocao">
+  <p>
+    na Vinheta de abertura do cocoricó qual<br/> instrumento musical o júlio toca?
+  </p>  
+  <!--FORMULARIO-->
+  <form id="form-contato" action="" method="post">
+    <?php 
+    $form = new BaseForm();
+    echo $form->renderHiddenFields();
+    ?>
+    <input type="hidden" name="formSection" id="formSection" value="pergunteParaFilomena">
+    <!--NOME-->
+    <div class="t1input">
+      <label>NOME:</label>
+      <input type="text" id="nome" name="nome" class="nome"  placeholder="Seu Nome Amiguinho"/>
+    </div>
+    <!--/NOME-->
+
+    <!--CIDADE-->
+    <div class="t2input">
+      <label>Cidade:</label>
+      <input type="text" name="cidade" id="cidade" class="cidade" />
+    </div>
+    <!--/CIDADE-->
+    
+    <!--ESTADO-->
+    <div class="t3input">
+      <label>ESTADO:</label>
+        <select class="estado required" id="estado">
+          <option value="" selected="selected">--</option>
+          <option value="Acre">AC</option>
+          <option value="Alagoas">AL</option>
+          <option value="Amazonas">AM</option>
+          <option value="Amap&aacute;">AP</option>
+          <option value="Bahia">BA</option>
+          <option value="Cear&aacute;">CE</option>
+          <option value="Distrito Federal">DF</option>
+          <option value="Espirito Santo">ES</option>
+          <option value="Goi&aacute;s">GO</option>
+          <option value="Maranh&atilde;o">MA</option>
+          <option value="Minas Gerais">MG</option>
+          <option value="Mato Grosso do Sul">MS</option>
+          <option value="Mato Grosso">MT</option>
+          <option value="Par&aacute;">PA</option>
+          <option value="Para&iacute;ba">PB</option>
+          <option value="Pernambuco">PE</option>
+          <option value="Piau&iacute;">PI</option>
+          <option value="Paran&aacute;">PR</option>
+          <option value="Rio de Janeiro">RJ</option>
+          <option value="Rio Grande do Norte">RN</option>
+          <option value="Rond&ocirc;nia">RO</option>
+          <option value="Roraima">RR</option>
+          <option value="Rio Grande do Sul">RS</option>
+          <option value="Santa Catarina">SC</option>
+          <option value="Sergipe">SE</option>
+          <option value="S&atilde;o Paulo">SP</option>
+          <option value="Tocantins">TO</option>
+      </select>
+    </div>
+    <!--/ESTADO-->
+    
+    <!--E-MAIL-->
+    <div class="t2input">
+      <label>E-MAIL:</label>
+      <input type="text" name="email" class="email"  placeholder="amiguinho@seuemail.com.br"/>
+    </div>
+    <!--/E-MAIL-->
+    
+    <!--IDADE -->
+    <div class="t3input">
+      <label>IDADE:</label>
+      <input type="text" name="idade" class="idade required"  placeholder="99"/>
+    </div>
+    <!--/E-MAIL-->
+    
+    <!--RESPOSTA-->
+    <div class="t1input">
+      <label>
+        RESPOSTA:</br>
+      </label>
+      <input type="text" name="resposta" class="resposta"  placeholder="99"/>
+    </div>
+    <!--RESPOSTA-->
+
+    <!--ENVIAR-->
+    <div id="btn-nav" align="center">
+      <img src="/portal/quintal/images/ludovicoshow/ajax-loader.gif" alt="enviando..." style="display:none;" width="16px" height="16px" id="ajax-loader" />
+      <input type="submit" name="enviar" id="enviar" class="btn-enviar" value="Enviar"/>
+    </div>
+    <!--/ENVIAR-->
+    
+  </form>
+  <!--/FORMULARIO-->
+  
+  <div id="guitarra"></div>
+</div>
+<!--/Pergunte a Filomena-->
+
+<!--RESPOSTA FORM FILOMENA-->
+<div id="respPromocao" style="display:none;">
+  <a href="javascript:;" class="troca tCha"></a>
+  <p class="recebeu">A Filomena já recebeu a sua pergunta. Fique ligado no programa ao vivo, quem sabe é a sua dúvida que ela irá solucionar.</p>
+  <p>AGUARDE!</p>
+  <a class="btn-enviar" href="javascript:;">
+    ENVIAR OUTRA PERGUNTA
+  </a>
+</div>
+<!--RESPOSTA FORM FILOMENA-->
+
+
+
---
+++ apps/frontend/templates/sites/tvcocorico/home2Success.php
@@ -0,0 +1,106 @@
+<link rel="stylesheet" href="/portal/css/tvcultura/geral.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/tvcocorico.css" type="text/css" />
+
+<?php use_helper('I18N', 'Date') ?>
+<!-- TOPO CMAIS REDUZIDO-->
+<?php include_partial_from_folder('blocks','global/menu-reduzido') ?>
+<!-- /TOPO CMAIS REDUZIDO-->
+
+<div id="palco">
+    <!-- CAPA SITE -->
+    <div id="capa-site">
+      
+      <!--LOGO TVCOCORICO -->
+      <h1>
+        <img src="/portal/images/capaPrograma/cocoricoHome/logo-tv-cocorico.png" alt="TV Cocorico" />
+      </h1>  
+      <!--LOGO TVCOCORICO -->
+      
+      <!-- MENU COCORICO -->
+      <div id="menu-tv-cocorico">
+        <!--LISTA-BOTOES-->
+        <ul>
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/turma/" target="_self" title="Turma Cocorico">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-turma.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/jogos/" target="_self" title="Jogos">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-jogos.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/videos/" target="_self" title="Vídeos">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-videos.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/imagens/" target="_self" title="Imagens">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-imagens.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/radio/" target="_self" title="Rádio">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-radio.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li>
+            <a href="http://www3.tvcultura.com.br/cocorico/extras/" target="_self" title="Extras">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-extras.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+          <!--BOTÃO-->
+          <li class="m03">
+            <a href="http://www3.tvcultura.com.br/cocorico/especiais/" target="_self" title="Especiais">
+              <img src="/portal/images/capaPrograma/cocoricoHome/btn-especiais.png" />
+            </a>
+          </li>
+          <!--/BOTÃO-->
+        </ul>
+        <!--/LISTA-BOTOES-->  
+      </div>
+      <!-- /MENU COCORICO -->  
+            
+      <!-- HORARIO -->
+      <div id="horario-tv">
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <p>AO VIVO DE 2ª a 6ª, ÀS 11H</p>
+      </div>
+      <!-- HORARIO -->
+      
+      <!--VIDEO-->
+      <div id="video-tv-cocorico">
+        <img src="/portal/images/capaPrograma/cocoricoHome/no_ar.jpg" />
+      </div>
+      <!--/VIDEO-->
+      
+      <!--PROMOCAO-->  
+      <?php include_partial_from_folder('sites/tvcocorico', 'global/promocao') ?>          
+      <!--/PROMOCAO-->
+      
+      <div id="back">
+      <!-- HOLOFOTES -->
+        <div id="holofote-01" class="holofote"></div>
+        <div id="holofote-02" class="holofote"></div>
+        <div id="holofote-03" class="holofote"></div>
+        <div id="holofote-04" class="holofote"></div>
+        <div id="holofote-05" class="holofote"></div>
+        <div id="holofote-06" class="holofote"></div>
+        <!-- /HOLOFOTES -->
+      </div>  
+    </div>
+    <!-- /CAPA SITE -->
+</div>
