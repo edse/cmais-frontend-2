48b7132a33ac1adba02bbaa081b572f5648414be
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-13 11:34:00 -0300

    cocorico

--- apps/frontend/templates/sites/tvcocorico/indexSuccess.php
+++ apps/frontend/templates/sites/tvcocorico/indexSuccess.php
@@ -6,21 +6,6 @@
 <?php include_partial_from_folder('blocks','global/menu-reduzido') ?>
 <!-- /TOPO CMAIS REDUZIDO-->
 
-  <!--GOOGLE ANALYTICS-->
-  <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-22770265-1']);
-      _gaq.push(['_setDomainName', 'cmais.com.br']);
-      _gaq.push(['_setAllowHash', 'false']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-  </script>
-  <!--/GOOGLE ANALYTICS-->  
-
 <div id="palco">
     <!-- CAPA SITE -->
     <div id="capa-site">
@@ -87,24 +72,20 @@
         </ul>
         <!--/LISTA-BOTOES-->  
       </div>
-      <!-- /MENU COCORICO -->  
-            
+      <!-- /MENU COCORICO -->
+      
       <!-- HORARIO -->
       <div id="horario-tv">
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <p>AO VIVO DE 2ª a 6ª, ÀS 11H</p>
       </div>
-      <!-- HORARIO -->
+      <!-- HORARIO -->        
       
       <!--VIDEO-->
       <div id="video-tv-cocorico">
         <img src="/portal/images/capaPrograma/cocoricoHome/no_ar.jpg" />
       </div>
       <!--/VIDEO-->
-      
-      <!--PROMOCAO-->  
-      <?php include_partial_from_folder('sites/tvcocorico', 'global/promocao') ?>          
-      <!--/PROMOCAO-->
       
       <div id="back">
       <!-- HOLOFOTES -->
