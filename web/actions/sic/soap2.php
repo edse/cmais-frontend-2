7dbce3e1062f9c7ae525b4d407b0eeb81b990637
edse <emerson.estrella@gmail.com>
2012-07-17 11:59:26 -0300

    movendo sic actions

---
+++ web/actions/sic/soap2.php
@@ -0,0 +1,89 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+//turn off WSDP caching if not in a production environment
+$ini = ini_set("soap.wsdl_cache_enabled","0");
+//instantiate the SOAP client
+$client = new SoapClient("http://intranet.tvcultura.com.br/crm_webservices/crm.asmx?WSDL");
+
+if(!isset($_REQUEST["step"]))
+  $_REQUEST["step"] = 0;
+
+if(($_REQUEST["step"]==1)&&($_REQUEST["f1_email"]!="" || $_REQUEST["email"]!="")){
+  
+  $email = (isset($_REQUEST["email"]))? $_REQUEST["email"] : $_REQUEST["f1_email"];
+  $protocolo = (isset($_REQUEST["protocolo"]))? $_REQUEST["protocolo"] : $_REQUEST["f1_protocolo"];
+  
+  //CHECK STATUS
+  $result = $client->sic_andamento(array('email'=>$email, 'protocolo'=>$protocolo));
+  $xml = simplexml_load_string($result->sic_andamentoResult->any);
+  $andamento = $xml->NewDataSet->andamento;
+  $mensagem = $xml->NewDataSet->mensagem;
+  $flag = $xml->NewDataSet->flag;
+  //var_dump($xml);
+
+  if($andamento!=NULL && $mensagem!=NULL){
+    $last = end($andamento);
+    $script = '$("#row1").hide();$("#row2").show();$("#row3").hide();$("#row5").hide();$("f1_email").val("");';
+    $html = '<table class="table table-striped table-bordered table-condensed"><thead><tr><th>Data</th><th>Previsão de entrega</th><th>Status</th><th>Justificativa</th></tr></thead><tbody>';
+    $data1 = new DateTime($andamento[0]->data_envio);
+    $data2 = new DateTime($andamento[count($andamento)-1]->data_entrega);
+    $status = $andamento[count($andamento)-1]->status;
+    foreach($andamento as $a){
+      $d1 = new DateTime($a->data_envio);
+      $d2 = new DateTime($a->data_entrega);
+      $html .= '<tr><td>'.$d1->format('Y-m-d H:i:s').'</td><td>'.$d2->format('Y-m-d H:i:s').'</td><td>'.$a->status.'</td><td>'.$a->justificativa.'</td></tr>';
+    }
+    $html .= '</tbody></table>';
+    $script .= '$("#status-html").html(\''.$html.'\');';
+    $script .= '$("#f2_email").val("'.$email.'");$("#f2_protocolo").val("'.$protocolo.'");$("#email_td").html("'.$email.'");$("#mensagem_td").html("'.$mensagem->mensagem.'");$("#protocolo_td, #protocolo2").html("'.$protocolo.'");$("#data_td").html("'.$data1->format('d/m/Y H:i:s').'");$("#entrega").html("'.$data2->format('d/m/Y H:i:s').'");$("#status_td").html("'.$status.'");';
+    //die($mensagem->mensagem);
+    if($flag->flag == "1")
+      $script .= '$("#recurso_holder").show();';
+    echo json_encode(array('script'=>$script,'title'=>'Usuário cadastrado','msg'=>'Preencha os campos abaixo.', 'form'=>'form2'));
+  }else{
+    $script = '$("#row1").hide();$("#row2").hide();$("#row3").show();$("#row5").hide();$("f1_email").val("");';
+    echo json_encode(array('script'=>$script,'title'=>'Email e/ou protocolo não conferem','msg'=>'Email e/ou protocolo não conferem', 'form'=>'form3'));
+  }
+  die();
+}
+
+if(($_REQUEST["step"]==2)&&($_REQUEST["f2_descricao"]!="")){
+  
+  if($_REQUEST["f2_email"] && $_REQUEST["f2_protocolo"]){
+    $result = $client->valida_email(array('email'=>$_REQUEST["f2_email"]));
+    $xml = simplexml_load_string($result->valida_emailResult->any);
+    $usuario = $xml->NewDataSet->usuario;
+    $arr = array(
+      'protocolo'            =>  (string)$_REQUEST["f2_protocolo"],
+      'cod_assunto'           =>  $_REQUEST["f2_cod_assunto"],
+      'mensagem'              =>  $_REQUEST["f2_descricao"]
+    );
+    $result = $client->insere_recurso_sic($arr);
+    //var_dump($arr);
+    //var_dump($result->insere_recurso_sicResult);
+    if($result->insere_recurso_sicResult){
+      $script = '$("#row1, #row2, #row3, #row4, #row5, #row7, #row8, #row9").hide();$("#protocolo_r").html("'.$_REQUEST["f2_protocolo"].'");$("#protocolo_r2").html("'.$_REQUEST["f2_protocolo"].'");$("#row5").show();';
+      $title = 'Solicitação enviada';
+      echo json_encode(array('script'=>$script,'title'=>$title,'msg'=>'Solicitação enviada', 'form'=>'form6'));
+    }else{
+      $script = '$("#row1, #row2, #row3, #row4, #row5, #row6, #row7, #row9").hide();$("#row4").show();';
+      $title = 'Erro! Solicitação não enviada';
+      echo json_encode(array('script'=>$script,'title'=>$title,'msg'=>'Erro! Solicitação não enviada', 'form'=>'form8'));
+    }
+  }
+  die();
+}
+
+if($_REQUEST["action"]=="assuntos" && $_REQUEST["cod_programa"]!=""){    //ASSUNTOS
+  //ASSUNTOS
+  $result = $client->assunto(array('cod_programa'=>$_REQUEST["cod_programa"]));
+  $assunto = simplexml_load_string($result->assuntoResult->any);
+  $assunto_options = '<option value="">--</option>';
+  foreach($assunto->NewDataSet->assunto as $item){
+    $assunto_options .= '<option value="'.$item->codigo.'">'.$item->assunto.'</option>';
+  }  
+  $script = '$("#f2_cod_assunto").children().remove();$("#f2_cod_assunto").append(\''.$assunto_options.'\');';
+  echo json_encode(array('script'=>$script));
+}
---
+++ web/actions/sic/status.php
@@ -0,0 +1,3 @@
+<?php 
+header("Location: http://172.20.17.129/esic/acompanhamento.html?protocolo=".$_REQUEST["protocolo"]);
+die();
