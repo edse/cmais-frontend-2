1f5d148a82d550acb23ffa6598a46c1370bae7d1
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-17 12:06:56 -0300

    topo Sic

--- apps/frontend/templates/sites/sic/_topo.php
+++ apps/frontend/templates/sites/sic/_topo.php
@@ -5,12 +5,12 @@
         <a href="<?php echo $site->retriveUrl() ?>" title="<?php echo $site->getTitle() ?>">
           <img src="http://midia.cmais.com.br/programs/<?php echo $site->getImageThumb() ?>" alt="<?php echo $site->getTitle() ?>" class="logo-sic"/>
         </a>
-        <div id="risco"></div>
+
         <!-- /LOGO -->
         <?php endif; ?>
 
         <?php include_partial_from_folder('sites/sic','global/sections-menu', array('siteSections' => $siteSections)) ?>
                 
-        <div id="risco"></div>
+
       </div>  
       <!-- /TOPO SIC-->
