62de56d24c6cd1a2dad35e967d862032116016ea
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-07-18 16:13:32 -0300

    ajustes gerais sic

--- apps/frontend/templates/sites/sic/solicitar-informacoesSuccess.php
+++ apps/frontend/templates/sites/sic/solicitar-informacoesSuccess.php
@@ -38,7 +38,7 @@
 				<?php if(isset($displays["conteudo-complementar"])): ?>
 					<?php if(count($displays["conteudo-complementar"]) > 0): ?>      	
         <!-- COLUNA ESQUERDA -->
-        <div class="float col-400-sic">
+        <div class="float col-400-sic font-span-14">
           <h2><?php echo $displays["conteudo-complementar"][0]->Block->getTitle() ?></h2>
           <p><?php echo html_entity_decode($displays["conteudo-complementar"][0]->Asset->AssetContent->render()) ?></p>
         </div>
