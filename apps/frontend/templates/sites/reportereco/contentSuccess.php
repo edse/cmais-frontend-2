bf5969eecd5d4700330d5879b3748ca7c24da788
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-23 20:42:33 -0300

    update

--- apps/frontend/templates/sites/reportereco/contentSuccess.php
+++ apps/frontend/templates/sites/reportereco/contentSuccess.php
@@ -108,89 +108,22 @@
                 <?php $videoRelated = $asset->retriveRelatedAssetsByAssetTypeId(6); ?>
                 
                 
-		            <?php if(isset($videoRelated)): ?>
-		              <!-- DESTAQUE 2 COLUNAS -->
-		              <div class="duas-colunas destaque grid2">
-		
-		                  <?php if($videoRelated->AssetType->getSlug() == "image"): ?>
-		                  <a class="" href="<?php echo $videoRelated->retriveUrl() ?>" title="<?php echo $asset->getTitle() ?>">
-		                  <img src="<?php echo $asset->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $videoRelated->getTitle() ?>" name="<?php echo $videoRelated->getTitle() ?>" />
-		                  
-		                  <?php elseif($videoRelated->AssetType->getSlug() == "content" || $videoRelated->AssetType->getSlug() == "image-gallery"): ?>
-		                    <?php $imgs = $videoRelated->retriveRelatedAssetsByAssetTypeId(2); ?>
-		                    <?php if(count($imgs) > 0): ?>
-		                      <img src="/uploads/assets/image/image-6/<?php echo $imgs[0]->AssetImage->getFile() ?>.jpg" alt="<?php echo $videoRelated->getTitle() ?>" name="<?php echo $videoRelated->getTitle() ?>" />
-		                    <?php endif; ?>
-		                  </a>
-		
-		                  <?php elseif($videoRelated->AssetType->getSlug() == "video"): ?>
-		                    <object style="height:390px; width: 640px">
-		                      <param name="movie" value="http://www.youtube.com/v/<?php echo $videoRelated->AssetVideo->getYoutubeId() ?>?version=3&enablejsapi=1&playerapiid=ytplayer&rel=0">
-		                      <param name="allowFullScreen" value="true">
-		                      <param name="allowScriptAccess" value="always">
-		                      <param name="wmode" value="opaque">
-		                      <embed id="ytplayer" src="http://www.youtube.com/v/<?php echo $videoRelated->AssetVideo->getYoutubeId() ?>?version=3&enablejsapi=1&playerapiid=ytplayer&rel=0" wmode="opaque" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="390"></embed>
-		                    </object>
-		
-		                  <?php elseif($videoRelated->AssetType->getSlug() == "video-gallery" || $videoRelated->AssetType->getSlug() == "episode"): ?>
-		                    <?php 
-		                    if($videoRelated->AssetType->getSlug() == "video-gallery")
-		                      $youtubeid = $videoRelated->AssetVideoGallery->getYoutubeId();
-		                    else
-		                      $youtubeid = "";
-		                    ?>
-		                    <?php if($youtubeid != ""): ?>
-		                    <iframe width="640" height="390" src="http://www.youtube.com/embed/videoseries?list=PL<?php echo $youtubeid ?>&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
-		                    <?php /*
-		                    <object height="390" width="640" style="height:390px; width: 640px">
-		                      <param name="movie" value="http://www.youtube.com/p/<?php echo $youtubeid ?>?version=3&amp;hl=en_US&amp;fs=1" />
-		                      <param name="allowFullScreen" value="true" />
-		                      <param name="allowscriptaccess" value="always" />
-		                      <param name="wmode" value="opaque">
-		                      <embed allowfullscreen="true" allowscriptaccess="always" src="http://www.youtube.com/p/<?php echo $youtubeid ?>?version=3&amp;hl=en_US&amp;fs=1" wmode="opaque" type="application/x-shockwave-flash" width="640" height="390"></embed>
-		                    </object>
-		                    */?>
-		                    <?php else: ?>
-		                    <?php $videos = $videoRelated->retriveRelatedAssetsByAssetTypeId(6); ?>
-		                    <div id="player"><iframe title="<?php echo $videos[0]->getTitle() ?>" width="640" height="390" src="http://www.youtube.com/embed/<?php echo $videos[0]->AssetVideo->getYoutubeId(); ?>?wmode=transparent" frameborder="0" allowfullscreen></iframe></div>
-		                    <script>
-		                    function changeVideo(id){
-		                      $('#player').html('<iframe width="640" height="390" src="http://www.youtube.com/embed/'+id+'?wmode=transparent" frameborder="0" allowfullscreen></iframe>');
-		                    }
-		                    </script>
-		
-		                    <?php if(count($videos) > 0): ?>
-		                      <ul class="box-playlist grid2">
-		                        <?php foreach($videos as $k=>$dd): ?>
-		                          <li style="width: 155px">
-		                            <?php if($dd->retriveImageUrlByImageUsage("image-2") != ""): ?>
-		                            <a href="javascript:changeVideo('<?php echo $dd->AssetVideo->getYoutubeId(); ?>')" class="img">
-		                              <img class="img-150x90" src="<?php echo $dd->retriveImageUrlByImageUsage("image-2") ?>" alt="<?php echo $dd->getTitle() ?>" />
-		                            </a>
-		                            <?php endif; ?>
-		                            <?php if($dd->retriveLabel() != ""): ?>
-		                            <h3 class="chapeu"><?php echo $dd->retriveLabel() ?></h3>
-		                            <?php endif; ?>
-		                            <a href="<?php echo $dd->retriveUrl() ?>"><?php echo $dd->getDescription() ?></a>
-		                          </li>
-		                        <?php endforeach; ?>
-		                      </ul>
-		                    <?php endif; ?>
-		                    <?php endif; ?>
-		                  
-		                  <?php else: ?>
-		                  <div style="width:640px; height:390px;"><h2><?php echo $videoRelated->getTitle() ?></h2><h4><?php echo $videoRelated->getDescription() ?></h4></div>
-		                  <?php endif; ?>
-		              </div>
-		              <!-- /DESTAQUE 2 COLUNAS -->
-		          <?php endif; ?>                
-                
-                
-                
-                
-                
-                
-                
+	            	<?php if(isset($videoRelated)): ?>
+	              <!-- DESTAQUE 2 COLUNAS -->
+	              <div class="duas-colunas destaque grid2">
+	
+	                  <?php if($videoRelated->AssetType->getSlug() == "video"): ?>
+	                    <object style="height:390px; width: 640px">
+	                      <param name="movie" value="http://www.youtube.com/v/<?php echo $videoRelated->AssetVideo->getYoutubeId() ?>?version=3&enablejsapi=1&playerapiid=ytplayer&rel=0">
+	                      <param name="allowFullScreen" value="true">
+	                      <param name="allowScriptAccess" value="always">
+	                      <param name="wmode" value="opaque">
+	                      <embed id="ytplayer" src="http://www.youtube.com/v/<?php echo $videoRelated->AssetVideo->getYoutubeId() ?>?version=3&enablejsapi=1&playerapiid=ytplayer&rel=0" wmode="opaque" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="390"></embed>
+	                    </object>
+	                  <?php endif; ?>
+	              </div>
+	              <!-- /DESTAQUE 2 COLUNAS -->
+	          		<?php endif; ?>                
                 
                 <div class="texto">
                   <?php if($asset->AssetType->getSlug() == "person"): ?>
