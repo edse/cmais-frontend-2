23047f616a7f0f1aba020f9d4c28b326914c9696
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-26 19:07:04 -0300

    test

--- web/portal/js/portal.js
+++ web/portal/js/portal.js
@@ -507,10 +507,7 @@ if (((screen.width * screen.height) / 600 < 800) && (window.location.href.indexO
 	if (classicVersion != "yes") // verifica se o cookie 'classic' não está setado e redireciona
 	{
   	mobileVersion = setCookie('mobile','yes','','/','cmais.com.br');
-  	if (mobileVersion == "yes")
-  	{
-  		window.location="http://m.cmais.com.br";
-  	}
+ 		window.location="http://m.cmais.com.br";
  	}
 }
 else
