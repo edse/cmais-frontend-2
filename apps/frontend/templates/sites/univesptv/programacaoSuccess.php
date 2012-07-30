79e4a94db66d7946a46e2aafb712cf0396a6efbc
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-07-30 19:21:01 -0300

    test

--- apps/frontend/templates/sites/univesptv/programacaoSuccess.php
+++ apps/frontend/templates/sites/univesptv/programacaoSuccess.php
@@ -1,4 +1,4 @@
-<!--script type="text/javascript" src="/js/jquery-ui-1.8.7/js/jquery-ui-1.8.7.custom.min.js"></script-->
+<script type="text/javascript" src="/portal/js/jquery-ui/js/jquery-ui-1.8.11.custom.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/i18n/jquery-ui-i18n.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/grade.css" type="text/css" />
 <link type="text/css" href="/portal/univesptv/css/geral.css" rel="stylesheet" /> 
