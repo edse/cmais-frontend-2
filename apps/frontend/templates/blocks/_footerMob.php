ee3a4576cb7ca4815698015b86402bcbdd59f395
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-03 13:23:16 -0300

    cross-subdomain cookie fix

--- apps/frontend/templates/blocks/_footerMob.php
+++ apps/frontend/templates/blocks/_footerMob.php
@@ -1,11 +1,6 @@
 <script>
 	function setCookieAndRedirect(urlToRedirect) {
-		//document.cookie = 'classic=yes; domain=cmais.com.br; path=/';
 		setCookie('classic', 'yes', 0, '/', 'cmais.com.br');
-		
-		//if (window.location.href.indexOf("?teste") > 0)
-		//	alert(document.cookie);
-		
 		classicVersion = getCookie('classic');
 		if (classicVersion == 'yes')
 			window.location = "http://cmais.com.br";
