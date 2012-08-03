faddf4eb1c1ec08beb10d34aa1c0b771964ef97a
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-03 13:20:34 -0300

    test

--- apps/frontend/templates/blocks/_footerMob.php
+++ apps/frontend/templates/blocks/_footerMob.php
@@ -1,7 +1,7 @@
 <script>
 	function setCookieAndRedirect(urlToRedirect) {
 		//document.cookie = 'classic=yes; domain=cmais.com.br; path=/';
-		setCookie('classic', 'yes', 0, '/', 'cmais.com.br', 'http://cmais.com.br');
+		setCookie('classic', 'yes', 0, '/', 'cmais.com.br');
 		
 		//if (window.location.href.indexOf("?teste") > 0)
 		//	alert(document.cookie);
