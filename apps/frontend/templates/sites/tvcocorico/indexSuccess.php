96548274a6d730d8ab5c63a6706d951ee262975d
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-08-07 17:19:11 -0300

    tvCocorico

--- apps/frontend/templates/sites/tvcocorico/indexSuccess.php
+++ apps/frontend/templates/sites/tvcocorico/indexSuccess.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/geral.css" type="text/css" />
-<link rel="stylesheet" href="/portal/css/tvcultura/sites/tvcocorico.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/<?php echo $site->getSlug(); ?>.css" type="text/css" />
 
 <?php use_helper('I18N', 'Date') ?>
 <!-- TOPO CMAIS REDUZIDO-->
@@ -21,14 +21,15 @@
   </script>
 
 <div id="palco">
+    <div id="back">
     <!-- CAPA SITE -->
     <div id="capa-site">
       
       <!--LOGO TVCOCORICO -->
       <h1>
-      	<a href="/tvcocorico" title="TV Cocórico" target="_self">
-	        <img src="/portal/images/capaPrograma/cocoricoHome/logo-tv-cocorico.png" alt="TV Cocorico" />
-    	</a>
+        <a href="/tvcocorico" title="TV Cocórico" target="_self">
+          <img src="/portal/images/capaPrograma/cocoricoHome/logo-tv-cocorico.png" alt="TV Cocorico" />
+      </a>
       </h1>  
       <!--LOGO TVCOCORICO -->
       
@@ -55,26 +56,14 @@
       <?php // include_partial_from_folder('sites/tvcocorico', 'global/promocao') ?>          
       <!--/PROMOCAO-->
       
-      <!--PROMOCAO->
-      <div id="respPromocao" >
-        <a href="javascript:;" class="troca tCha"></a>
-        <p class="recebeu">
-        Ganhadores da guitarra do Júlio:<br/><br/>
-        <span class="preto">Artur da Silva de Oliveira, 4 anos – São Paulo</span><br/>
-        <span class="preto">Gabrielle Antunes Rosa, 4 anos - Curitiba</span><br/>
-        <span class="preto">Mateus Ferreira Jacques, 4 anos - Florianópolis</span><br/>
-        <span class="preto">Maria Luiza Vieira Brito, 8 anos - Itapevi</span><br/>
-        <span class="preto">Deborah Genovese Fernandes, 6 anos  - São Paulo</span><br/><br/>
-
-        Parabéns!<br/>
-        <span class="obs">*Entraremos em contato por e-mail para a retirada do brinquedo.</span>
-        </p>
-
+      <!--PROMOCAO-->  
+      <?php //include_partial_from_folder('sites/tvcocorico', 'global/enquete') ?>          
+      <!--/PROMOCAO-->
         
-      </div>
-      <!--PROMOCAO-->
-      
-      <div id="back">
+    </div>
+    <!-- /CAPA SITE -->
+    
+    
       <!-- HOLOFOTES -->
         <div id="holofote-01" class="holofote"></div>
         <div id="holofote-02" class="holofote"></div>
@@ -83,7 +72,5 @@
         <div id="holofote-05" class="holofote"></div>
         <div id="holofote-06" class="holofote"></div>
         <!-- /HOLOFOTES -->
-      </div>  
-    </div>
-    <!-- /CAPA SITE -->
+      </div>
 </div>
