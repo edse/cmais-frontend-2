cdb33db337c5685ba576387a89707d7369b9f985
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-08-07 20:17:15 -0300

    cocorico

--- apps/frontend/templates/sites/tvcocorico/_enquete.php
+++ apps/frontend/templates/sites/tvcocorico/_enquete.php
@@ -16,7 +16,7 @@
     <form id="form-contato" action="" method="post" >
       
       <!--PERGUNTA-->
-      <p><?php echo $respostas[1]->Asset->AssetQuestion->getQuestion() ?></p>
+      <p><?php echo $respostas[0] ?></p>
       <!--/PERGUNTA-->
       
 
