948faa1730e04040b88623750b4e19ee68be87a7
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-08-08 14:41:13 -0300

    cocorico

--- apps/frontend/templates/sites/tvcocorico/_enquete.php
+++ apps/frontend/templates/sites/tvcocorico/_enquete.php
@@ -148,7 +148,7 @@ function sendAnswer(){
       $("#resultadoParcial").fadeIn("fast"); 
       var i=1;
       $.each(data, function(key, val) {
-        $('.resposta-'+i).html("<p>"+ Math.round (val.votes) +"</p>");
+        $('.resposta-'+i).html("<p>"+ parseInt(Math.round (val.votes)) +"</p>");
         i++;
       });
      
