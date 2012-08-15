7baacac89c606dff2c36790812d8a6d94a2bb8de
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-15 12:47:00 -0300

    site image icon

--- apps/frontend/templates/sites/univesptv-copy/cursosSuccess.php
+++ apps/frontend/templates/sites/univesptv-copy/cursosSuccess.php
@@ -48,7 +48,7 @@
     <div class="topo-programa">
       <h2>
       	<a href="<?php echo $site->retriveUrl() ?>" style="text-decoration: none;">
-      		<img src="<?php echo $site->getImageIcon() ?>" alt="<?php echo $site->getTitle() ?>" title="<?php echo $site->getTitle() ?>" />
+      		<img src="http://midia.cmais.com.br/programs/<?php echo $site->getImageIcon() ?>" alt="<?php echo $site->getTitle() ?>" title="<?php echo $site->getTitle() ?>" />
       	</a>
       </h2>
       <h2 class="cursos-livres">Cursos Livres
