a0a984f00cc701b74f7076277909d97e639fad1e
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-16 12:50:53 -0300

    update

--- apps/frontend/templates/sites/univesptv/_topo.php
+++ apps/frontend/templates/sites/univesptv/_topo.php
@@ -1,6 +1,6 @@
 <h2>
 	<a href="<?php echo $site->retriveUrl() ?>" style="text-decoration: none;">
-		<img src="http://midia.cmais.com.br/programs/<?php echo $site->getImageIcon() ?>" alt="<?php echo $site->getTitle() ?>" title="<?php echo $site->getTitle() ?>" />
+		<img src="/portal/images/capaPrograma/univesptv/logo.png" alt="<?php echo $site->getTitle() ?>" title="<?php echo $site->getTitle() ?>" />
 	</a>
 </h2>
 <h2 class="cursos-livres">
