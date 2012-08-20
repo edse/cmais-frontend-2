70c3d6c80e79c6865841ee2d667c4742b32bc239
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-08-20 16:39:10 -0300

    cocorico

--- apps/frontend/templates/sites/tvcocorico/indexSuccess.php
+++ apps/frontend/templates/sites/tvcocorico/indexSuccess.php
@@ -40,7 +40,7 @@
       <!-- HORARIO -->
       <div id="horario-tv">
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <a href="http://youtube.com.br" title="Canal TV Cocoricó">
+        <a href="http://youtube.com/tvcocorico" title="Canal TV Cocoricó">
           <img src="/portal/images/capaPrograma/cocoricoHome/youtube-tvcocorico.png" alt="TV Cocorico" />
         </a>
       </div>
