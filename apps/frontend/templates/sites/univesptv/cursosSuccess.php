18dbe2fc0d898fa8340be0c1becd8fcff8bcfb2a
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-20 12:58:18 -0300

    update

--- apps/frontend/templates/sites/univesptv/cursosSuccess.php
+++ apps/frontend/templates/sites/univesptv/cursosSuccess.php
@@ -241,8 +241,11 @@
 						$('#novos .jcarousel-next, #novos .jcarousel-prev').hide();
 					if ($('#humanidades ul li').length <= 3)
 						$('#humanidades .jcarousel-next, #humanidades .jcarousel-prev').hide();
-					if ($('#ciencia ul li').length <= 3)
-						$('#ciencia .jcarousel-next, #ciencia .jcarousel-prev').hide();
+					if ($('#ciencia ul li').length <= 3) {
+						//$('#ciencia .jcarousel-next, #ciencia .jcarousel-prev').hide();
+						$('#ciencia .jcarousel-next').addClass('jcarousel-next-disabled');
+						$('#ciencia .jcarousel-prev').addClass('jcarousel-prev-disabled');
+					}
 					if ($('#universidades ul li').length <= 3)
 						$('#universidades .jcarousel-next, #universidades .jcarousel-prev').hide();
 				});
