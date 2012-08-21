5ffa9848cf2c8c2dfec82bf0dd3d51a8d48c9523
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-08-21 16:36:30 -0300

    ideias piadas

--- apps/frontend/templates/sites/tvratimbum/piadasSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/piadasSuccess.php
@@ -249,7 +249,7 @@ $piadaAssinatura = $displays["piada-destaque-semana"][0]->getDescription();
                         $('input#enviar').show();
                         window.location.href="#";
                         if(data == "1"){
-                          $("#form-contato").clearForm();
+                          //$("#form-contato").clearForm();
                           $("#form-contato").hide();
                           $('#enviando').hide();
                           $(".caudaBarra,.pontaBarra,.msgAcerto").show();
