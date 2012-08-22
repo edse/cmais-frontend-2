f65b2825bf4b069c4df40b4043e3e1462df92bdb
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-08-22 12:27:57 -0300

    update

--- apps/frontend/templates/sites/univesptv/cursoSuccess.php
+++ apps/frontend/templates/sites/univesptv/cursoSuccess.php
@@ -94,7 +94,7 @@ if(isset($assets)){
               				<?php endif; ?>
               			<?php endif; ?>		
               <p class="bold">
-              	<?php if($d->retriveUrl()): ?>
+              	<?php if($d->getUrl()): ?>
               	<a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>"><?php echo $d->getTitle() ?></a>
               	<?php else: ?>
               	<?php echo $d->getTitle() ?>
