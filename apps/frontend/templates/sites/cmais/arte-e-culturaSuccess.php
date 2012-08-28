bd779ebe02022d0f64fb6c80fc1046a0fb742067
rafaelhcs <rhcsousa@gmail.com>
2012-08-28 08:36:16 -0300

    teste

--- apps/frontend/templates/sites/cmais/arte-e-culturaSuccess.php
+++ apps/frontend/templates/sites/cmais/arte-e-culturaSuccess.php
@@ -7,7 +7,7 @@ $(function(){
     });
 });
 </script> 
-
+    
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
