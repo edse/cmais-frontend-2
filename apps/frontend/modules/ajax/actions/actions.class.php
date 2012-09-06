8c399bb2aaea0d444a1294e6bc8aa818b69925dc
rafaelhcs <rhcsousa@gmail.com>
2012-09-06 15:13:30 -0300

    update

--- apps/frontend/modules/ajax/actions/actions.class.php
+++ apps/frontend/modules/ajax/actions/actions.class.php
@@ -975,7 +975,7 @@ class ajaxActions extends sfActions
         else{
         */
 	        foreach($assets as $a){
-	          if($request->getParameter('piadas')==1){
+	          if($request->getParameter('piadas')==1 || $request->getParameter('lista-ideias')){
 	            ?>
 	            <script>
               $(document).ready(function(){
