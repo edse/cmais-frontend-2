3e13850dc9dd84c684ad424131b2d4b3a206ec41
renatareno <asdf@fpawks0142.local>
2012-09-17 13:35:33 -0300

    banner debate

--- apps/frontend/templates/sites/cmais/assets/debateSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/debateSuccess.php
@@ -65,7 +65,7 @@
           <div class="box-publicidade grid1">
             <!-- programas-homepage-300x250 -->
             <script type='text/javascript'>
-				    GA_googleFillSlot("cmais-homepage-300x250");
+				    GA_googleFillSlot("home-geral2-300x250");
             </script>
           </div>
           <!-- / BOX PUBLICIDADE -->
