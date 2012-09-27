8743fd8c469cfc840ba706cb6c522c31f94c0bf3
rafaelhcs <rhcsousa@gmail.com>
2012-09-27 12:21:08 -0300

    update

--- apps/frontend/templates/sites/radarcultura/contentRadarSuccess.php
+++ apps/frontend/templates/sites/radarcultura/contentRadarSuccess.php
@@ -72,8 +72,8 @@
               </li>
               <li class="span12">
                 <div class="thumbnail">
-                  <link href="/portal/js/audioplayer/jplayer.Blue.Monday.2.0.0/jplayer.blue.monday.css" rel="stylesheet" type="text/css" />
-                  <script type="text/javascript" src="/portal/js/jquery.jplayer.min.js"></script>
+                  <link href="/portal/js/jPlayer/skin/jplayer.blue.monday.css" rel="stylesheet" type="text/css" />
+                  <script type="text/javascript" src="/portal/js/jPlayer/js/jquery.jplayer.min.js"></script>
                   <script type="text/javascript">
 										//<![CDATA[
 										$(document).ready(function(){
