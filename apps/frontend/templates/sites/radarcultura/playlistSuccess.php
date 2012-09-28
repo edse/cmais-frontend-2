4e3e679af352a5a8aea1875b4afd852edfa5240c
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-09-28 17:13:00 -0300

    radar

---
+++ apps/frontend/templates/sites/radarcultura/playlistSuccess.php
@@ -0,0 +1,139 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+
+    <!-- Le styles -->
+    <link href="/portal/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/portal/js/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="/portal/css/tvcultura/sites/radarcultura.css" rel="stylesheet" type="text/css" />
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script src="/portal/js/bootstrap/bootstrap.js"></script>
+    
+    <!--container-->
+    <div class="container">
+      
+        <?php include_partial_from_folder('sites/radarcultura', 'global/modal-feedback') ?>
+        
+        <!--topo menu/alert/logo-->
+        <div class="row-fluid">
+          <?php include_partial_from_folder('sites/radarcultura', 'global/alert', array('site' => $site)) ?>
+        </div>
+        <div class="row-fluid">  
+          <?php include_partial_from_folder('sites/radarcultura', 'global/menu', array('siteSections' => $siteSections, 'displays' => $displays, 'section'=>$section)) ?>
+        </div>
+        <!--topo menu/alert/logo-->
+
+     <!-- asset -->
+     <div class="span12 row">
+      
+        <?php include_partial_from_folder('sites/radarcultura', 'global/breadcrumbs', array('site' => $site, 'section' => $section, 'asset' => $asset)) ?>
+              
+        <div class="page-header">
+          <h1><?php echo $asset->getTitle() ?> <small></small></h1>
+        </div>
+     </div>
+        <div class="row-fluid span12" style="margin:0 0 0 0;">
+          <ul class="thumbnails span8">
+            <li class="span12">
+              <p><small><?php echo $asset->getTitle() ?></small></p>
+              <p><?php echo html_entity_decode($asset->AssetContent->render()) ?></p>
+             <!-- comentario facebook -->
+             <div class="container face span12">
+                <fb:comments href="http://cmais.com.br" numposts="3" width="610" publish_feed="true"></fb:comments>
+                <hr />
+              </div>
+              <!-- /comentario facebook -->
+              
+            </li>
+          </ul>
+          
+          <ul class="direita span4">
+              <li class="span12">
+                <div class="banner-radio">
+                  <script type='text/javascript'>
+                    GA_googleFillSlot("cmais-assets-300x250");
+                  </script>
+                </div>
+              </li>
+            <?php
+              $displays = array();
+              $block_sobre = Doctrine_Query::create()
+                ->select('b.*')
+                ->from('Block b, Section s')
+                ->where('b.section_id = s.id')
+                ->andWhere('s.slug = ?', 'home')
+                ->andWhere('b.slug = ?', 'sobre-o-programa')
+                ->andWhere('s.site_id = ?', $site->id)
+                ->execute();
+            
+              if(count($block_sobre) > 0){
+                $displays["sobre-o-programa"] = $block_sobre[0]->retriveDisplays();
+              }
+            ?>
+            <?php if(isset($displays['sobre-o-programa'])):?>
+              <?php if(count($displays['sobre-o-programa']) > 0): ?>
+              <li class="span12">
+                <div class="thumbnail">
+                  <div class="caption">
+                    <div class="page-header">
+                      <h4><?php echo $displays['sobre-o-programa'][0]->getTitle() ?></h4>
+                    </div>
+                    <p><?php echo $displays['sobre-o-programa'][0]->getDescription() ?></p>
+                    <p><a href="<?php echo $displays['sobre-o-programa'][0]->retriveUrl() ?>" class="btn btn-mini btn-inverse"><i class="icon-chevron-right icon-white"></i> saiba mais</a></p>
+                  </div>
+                </div>
+              </li>
+             <?php endif; ?>
+           <?php endif; ?>
+           
+            <?php
+              $displays = array();
+              $block_comoparticipar = Doctrine_Query::create()
+                ->select('b.*')
+                ->from('Block b, Section s')
+                ->where('b.section_id = s.id')
+                ->andWhere('s.slug = ?', 'home')
+                ->andWhere('b.slug = ?', 'como-participar')
+                ->andWhere('s.site_id = ?', $site->id)
+                ->execute();
+            
+              if(count($block_comoparticipar) > 0){
+                $displays["como-participar"] = $block_comoparticipar[0]->retriveDisplays();
+              }
+            ?>
+           
+           <?php if(isset($displays['como-participar'])):?>
+            <?php if(count($displays['como-participar']) > 0): ?>
+              <li class="span12">
+                <div class="thumbnail">
+                  <div class="caption">
+                    <div class="page-header">
+                      <h4><?php echo $displays['como-participar'][0]->getTitle() ?></h4>
+                    </div>
+                    <p><?php echo $displays['como-participar'][0]->getDescription() ?></p>
+                    <p><a href="<?php echo $displays['como-participar'][0]->retriveUrl() ?>" class="btn btn-mini btn-inverse"><i class="icon-chevron-right icon-white"></i> saiba mais</a></p>
+                  </div>
+                </div>
+              </li>
+             <?php endif; ?>
+           <?php endif; ?>
+            </ul>            
+        </div>
+  
+        <div class="container pull-left">
+          <div class="banner-radio horizontal">
+           <script type='text/javascript'>
+             GA_googleFillSlot("cmais-assets-728x90");
+           </script>
+          </div>
+        </div>  
+
+            
+          </div>
+
+     <!-- asset -->
+ </div>
