6fcc56ab2b5567f3d987b7b58e1c0e81876e44d4
rafaelhcs <rhcsousa@gmail.com>
2012-10-01 17:40:12 -0300

    update

--- apps/frontend/templates/sites/radarcultura/assets/playerSuccess.php
+++ apps/frontend/templates/sites/radarcultura/assets/playerSuccess.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml"
   xmlns:fb="https://www.facebook.com/2008/fbml">
-  <head>
+  <head> 
     <meta charset="utf-8">
     <title>Cultura Brasil - RadarCultura</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
