ab8edaff336c9926ccdf7f596b97a2cb20b4eb93
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-10-02 15:46:42 -0300

    update

--- apps/frontend/templates/sites/culturafm/subsectionColunistaSuccess.php
+++ apps/frontend/templates/sites/culturafm/subsectionColunistaSuccess.php
@@ -109,7 +109,7 @@ if(isset($pager)){
 						<ul class="abas-conteudo conteudo">
 							<li style="display: block; height: auto;" id="bloco1" class="filho">
 								<a class="media" href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" title="<?php echo $displays['sobre'][0]->getTitle() ?>"> 
-									<img src="<?php echo $displays['sobre'][0]->retriveImageUrlByImageUsage("image-8") ?>" alt="<?php echo $displays['sobre'][0]->getTitle() ?>">
+									<img src="<?php echo $displays['sobre'][0]->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $displays['sobre'][0]->getTitle() ?>">
 								</a>
 								<a href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" class="titulos" title="<?php echo $displays['sobre'][0]->getTitle() ?>"><?php echo $displays['sobre'][0]->getTitle() ?></a>
 									<p><?php echo $displays['sobre'][0]->getDescription() ?></p>
