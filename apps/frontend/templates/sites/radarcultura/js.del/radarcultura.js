ff99c3f8af7a286b77a7d04f32e387cb302216c3
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-02 17:04:24 -0300

    radar

---
+++ apps/frontend/templates/sites/radarcultura/js.del/radarcultura.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  /*
+  $('.nav li').hover(function(){
+    $(this).addClass('open');
+    $(this).find('.dropdown-menu').fadeIn('fast'); 
+  });
+  $('.nav li').mouseleave(function(){
+    $(this).removeClass('open');
+    $(this).find('.dropdown-menu').fadeOut('fast');
+  });
+  */
+});  
