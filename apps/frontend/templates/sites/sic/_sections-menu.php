90e1f8a0d723a105022e79bed9f61bef72f90ac4
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-04 15:58:40 -0300

    sic

--- apps/frontend/templates/sites/sic/_sections-menu.php
+++ apps/frontend/templates/sites/sic/_sections-menu.php
@@ -1,46 +1,48 @@
-        <!--menu Drop Down -->
+        <!--menu Drop Down-->
         <script>
-          $(document).ready(function(){
+        $(document).ready(function(){
           //$('.dropdown-toggle').dropdown();
           $('.dropdown-toggle').click(function(){
             $('.dropdown').removeClass('open');
-            $(this).parent().toggleClass("open");
+            $(this).parent().addClass("open");
           });
-        }); 
+        });  
+        
         </script>
         <!--/menu Drop Down -->
-        
-				<?php if(count($siteSections) > 0): ?>
+        <?php echo count($siteSections) ?>
+        <?php if(count($siteSections) > 0): ?>
         <!-- MENU SIC -->
         <div class="menu-sic">
-        	
+        
+          
           <!-- MENU PRINCIPAL -->
           <ul class="nav nav-pills">
           <?php
-          	$sectionId = 0;
-	          if(isset($section)){
-	          	$sectionId = $section->getId();
-							if ($section->getParentSectionId())
-								$sectionId = $section->getParentSectionId();
-	          }
-	          if(isset($asset)){
-	          	$assetSections = $asset->getSections();
-							$sectionId = $assetSections[0]->getId();
-							if ($assetSections[0]->getParentSectionId())
-								$sectionId = $assetSections[0]->getParentSectionId();
-	          }
+            $sectionId = 0;
+            if(isset($section)){
+              $sectionId = $section->getId();
+              if ($section->getParentSectionId())
+                $sectionId = $section->getParentSectionId();
+            }
+            if(isset($asset)){
+              $assetSections = $asset->getSections();
+              $sectionId = $assetSections[0]->getId();
+              if ($assetSections[0]->getParentSectionId())
+                $sectionId = $assetSections[0]->getParentSectionId();
+            }
           ?>
-					<?php foreach($siteSections as $s): ?>
-	          <?php $subsections = $s->subsections(); ?>
+          <?php foreach($siteSections as $s): ?>
+            <?php $subsections = $s->subsections(); ?>
             <?php if(count($subsections) > 0): ?>
             <!-- MENU BOTAO DROP DOWN -->
             <li class="dropdown <?php if($s->getId() == $sectionId): ?>active<?php endif; ?>" id="menu<?php echo $s->id ?>">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#menu<?php echo $s->id ?>" title="<?php echo $s->getTitle() ?>">
+              <a class="dropdown-toggle" href="javascript:;" title="<?php echo $s->getTitle() ?>">
                 <?php echo $s->getTitle() ?>
                 <b class="caret"></b>
               </a>
               <ul class="dropdown-menu">
-							<?php foreach($subsections as $s): ?>
+              <?php foreach($subsections as $s): ?>
                 <li><a href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle()?>"><?php echo $s->getTitle()?></a></li>
               <?php endforeach; ?>
               </ul>
