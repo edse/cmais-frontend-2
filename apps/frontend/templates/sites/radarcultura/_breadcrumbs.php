cba4fb40589afa3172d351315aec62a163f223ef
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-10-04 19:01:02 -0300

    update

--- apps/frontend/templates/sites/radarcultura/_breadcrumbs.php
+++ apps/frontend/templates/sites/radarcultura/_breadcrumbs.php
@@ -1,24 +1,29 @@
 			 <?php if($section->getParentSectionId()): ?>
 			 <?php $parentSection = Doctrine::getTable('section')->findOneById($section->getParentSectionId()); ?>
 			 <?php endif; ?>
-			 <?php if(isset($parentSection) || isset($asset)): ?>
+			 <?php if(isset($parentSection) || isset($asset) || isset($section)): ?>
 		    <!--breadcrumb-->
 		    <div class="row-fluid">
 		       <ul class="breadcrumb">
 		         <li><a href="<?php echo url_for('homepage') . $site->getSlug() ?>"><?php echo $site->getTitle() ?></a> <span class="divider">/</span></li>
-		         <?php if(isset($parentSection)): ?>
-		           <?php if(count($parentSection->getAssets()) > 0): ?>
-		         <li><a href="<?php echo $parentSection->retriveUrl()?>"><?php echo $parentSection->getTitle(); ?></a> <span class="divider">/</span></li>
-		           <?php else: ?>
-		         <li><?php echo $parentSection->getTitle(); ?> <span class="divider">/</span></li>
-		           <?php endif; ?>
-		         <?php endif; ?>
-		         <?php if(isset($asset)): ?>
-		         <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle(); ?></a> <span class="divider">/</span></li>
-		         <li><a href="<?php echo $asset->retriveUrl() ?>"><?php echo $asset->getTitle() ?></a></li>
-		         <?php else: ?>
-		         <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle(); ?></a> </li>
-		         <?php endif; ?>
+					 <?php if($section->getSlug() == "musicas"): ?>
+						 <li><a href="<?php echo url_for("homepage") . $site->getSlug() . "artistas" ?>">Artistas</a> <span class="divider">/</span></li>
+						 <li><?php echo $artist ?></li>
+					 <?php else: ?>
+			       <?php if(isset($parentSection)): ?>
+			         <?php if(count($parentSection->getAssets()) > 0): ?>
+			         <li><a href="<?php echo $parentSection->retriveUrl()?>"><?php echo $parentSection->getTitle(); ?></a> <span class="divider">/</span></li>
+			         <?php else: ?>
+			         <li><?php echo $parentSection->getTitle(); ?> <span class="divider">/</span></li>
+			         <?php endif; ?>
+			       <?php endif; ?>
+			       <?php if(isset($asset) && $parentSection->getSlug() != "sobre"): ?>
+			         <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle(); ?></a> <span class="divider">/</span></li>
+			         <li><?php echo $asset->getTitle() ?></li>
+			       <?php else: ?>
+			         <li><?php echo $section->getTitle(); ?> </li>
+			       <?php endif; ?>
+		       <?php endif; ?>
 		       </ul>
 		    </div>
 		    <!--breadcrumb-->
