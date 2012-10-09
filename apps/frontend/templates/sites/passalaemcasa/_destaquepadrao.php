5fe4cba17741d186b3b88300ce323e8db1064a4c
rafaelhcs <rhcsousa@gmail.com>
2012-10-09 17:39:07 -0300

    update

--- apps/frontend/templates/sites/passalaemcasa/_destaquepadrao.php
+++ apps/frontend/templates/sites/passalaemcasa/_destaquepadrao.php
@@ -4,5 +4,5 @@
               <p><?php echo $displays['destaque-principal'][0]->getDescription() ?><p>
               <p><?php echo html_entity_decode($displays['destaque-principal'][0]->Asset->AssetContent->render()) ?></p>
             <?php endif; ?>  
-          <?php endif; ?> 
+          <?php endif; ?>  
           
