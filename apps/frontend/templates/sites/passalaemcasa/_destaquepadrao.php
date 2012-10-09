a715375bc3d0db471bc6028ea82c380c5801ce6e
rafaelhcs <rhcsousa@gmail.com>
2012-10-09 17:35:40 -0300

    update

--- apps/frontend/templates/sites/passalaemcasa/_destaquepadrao.php
+++ apps/frontend/templates/sites/passalaemcasa/_destaquepadrao.php
@@ -1,8 +1,8 @@
-            <?php if(isset($displays['destaque-padrao-1'])):?>
-            <?php if($displays['destaque-padrao-1'][0]->Asset->AssetType->getSlug() == "content"): ?>
-              <h3><?php echo $displays['destaque-padrao-1'][0]->getTitle() ?></h3>
-              <p><?php echo $displays['destaque-padrao-1'][0]->getDescription() ?><p>
-              <p><?php echo html_entity_decode($displays['destaque-padrao-1'][0]->Asset->AssetContent->render()) ?></p>
+            <?php if(isset($displays['destaque-principal'])):?>
+            <?php if($displays['destaque-principal'][0]->Asset->AssetType->getSlug() == "content"): ?>
+              <h3><?php echo $displays['destaque-principal'][0]->getTitle() ?></h3>
+              <p><?php echo $displays['destaque-principal'][0]->getDescription() ?><p>
+              <p><?php echo html_entity_decode($displays['destaque-principal'][0]->Asset->AssetContent->render()) ?></p>
             <?php endif; ?>  
           <?php endif; ?> 
           
