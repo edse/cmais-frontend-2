1efec238860bc1b83a975aee1cbfd06721a961b8
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-10-10 12:46:44 -0300

    update maiscrianca

---
+++ apps/frontend/templates/sites/maiscrianca/_naweb.php
@@ -0,0 +1,14 @@
+    <div class="row-fluid destaques naweb">
+      <div class="span12">
+        <div class="span3 h2"><h2>Na web</h2><span></span></div>
+        <ul class="row-fluid span12">
+          <li><a href="http://cmais.com.br/quintaldacultura" class="quintal" title="Quintal da Cultura"></a></li>
+          <li><a href="http://tvratimbum.cmais.com.br/" class="tvrtb" title="TV Rá Tim Bum"></a></li>
+          <li><a href="http://tvcultura.cmais.com.br/cartaozinho" class="cartaozinho" title="Cartãozinho Verde"></a></li>
+          <li><a href="http://casteloratimbum.com.br" class="castelortb" title="Castelo Rá Tim Bum"></a></li>
+          <li class="vila"><a href="http://tvcultura.cmais.com.br/vilasesamo" class="vilasesamo" title="Vila Sésamo"></a></li>
+          <li class="last"><a href="http://tvcultura.cmais.com.br/cocorico" class="cocorico" title="Cocoricó"></a></li>
+        </ul>
+        <div class="pontilhado row-fluid span12"></div>
+      </div>
+    </div>
