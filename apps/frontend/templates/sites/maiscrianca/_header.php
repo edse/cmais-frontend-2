e86540a07f1675dbaea4d9d44641f8f498689087
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-10-11 15:10:59 -0300

    update

---
+++ apps/frontend/templates/sites/maiscrianca/_header.php
@@ -0,0 +1,7 @@
+  <div class="header">
+    <h1 class="logo span12">
+      <a href="<?php echo $site->retriveUrl() ?>" title="+Criança">
+        <img class="row-fluid span12" src="/portal/images/capaPrograma/maiscrianca2/logo.jpg" alt="+criança" />
+      </a>
+    </h1>
+  </div>
