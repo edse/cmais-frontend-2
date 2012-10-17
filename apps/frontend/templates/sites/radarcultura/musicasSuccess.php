7e20d5b3ea0d58b73f5e2c3b374b0a71a7b3fd06
Cristovam Ruiz Jr <cristovam@cristovam-G31M-S2C.(none)>
2012-10-17 19:40:32 -0300

    update

--- apps/frontend/templates/sites/radarcultura/musicasSuccess.php
+++ apps/frontend/templates/sites/radarcultura/musicasSuccess.php
@@ -122,18 +122,28 @@
                 $aux = explode(";", $d->AssetContent->getHeadlineShort());
                 ?>
                 <tr>
-                  <td><?php echo $d->getTitle(); ?></td>
-                  <td><?php echo str_ireplace("Por ", "", $d->getDescription()); ?></td>
-                  <td><?php echo $aux[4] ?></td>
+                  <td class="music-<?php echo $value ?>"><?php echo $d->getTitle(); ?></td>
+                  <td class="performer-<?php echo $value ?>"><?php echo str_ireplace("Por ", "", $d->getDescription()); ?></td>
+                  <td class="composer-<?php echo $value ?>"><?php echo $aux[4] ?></td>
                   <td class="play">
                     <a href="<?php echo url_for('@homepage') ?>musicas/<?php echo $d->getSlug(); ?>" class="btn btn-mini btn-inverse pull-right" style="margin-left: 5px;"><i class="icon-list icon-white"></i> ver detalhes </a>
-                    <a href="javascript:;" class="btn btn-mini btn-info pull-right socialBtn" id="socialBtn-<?php echo $value ?>" rel="popover" data-content='<div class="btn-toolbar"><div class="btn-group"><a class="btn" href="javascript:postTwitter();">Twitter</a><a class="btn" href="javascript:postToFeed();">Facebook</a><a class="btn" href="javascript:postGoogle();">Google+</a></div><div class="btn-group"><a class="btn btn-email" data-toggle="modal" data-target="#modal">Email</a></div></div>' data-original-title="Selecione sua rede social..."><i class="icon-share-alt icon-white"></i> Sugira esta música</a>
+                    <a href="javascript:;" class="btn btn-mini btn-info pull-right socialBtn" id="socialBtn-<?php echo $value ?>" name="<?php echo $value ?>" rel="popover" data-content='<div class="btn-toolbar"><div class="btn-group"><a class="btn" href="javascript:postTwitter();">Twitter</a><a class="btn" href="javascript:postToFeed();">Facebook</a><a class="btn" href="javascript:postGoogle();">Google+</a></div><div class="btn-group"><a class="btn btn-email" data-toggle="modal" data-target="#modal">Email</a></div></div>' data-original-title="Selecione sua rede social..."><i class="icon-share-alt icon-white"></i> Sugira esta música</a>
+                    <input type="hidden" value="<?php echo url_for('homepage') . $site->getSlug() . '/' . $section->getSlug() . '/' . $d->getSlug() ?>" />
                   </td>
                 </tr>
               <?php endforeach; ?>
             <?php endif; ?>
             </tbody>
           </table>
+          <script>
+            $(function(){
+              $('.socialBtn').click(function(){
+                $('#music').val($('.music-'+$(this).attr('name')).html());
+                $('#performer').val($('.performer-'+$(this).attr('name')).html());
+                $('#url').val($('.play input[type=hidden]').val());
+              });
+            });
+          </script>
 
 
          
