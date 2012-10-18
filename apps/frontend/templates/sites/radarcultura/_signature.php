46a6205002c4949f0dd1adca7ac9339b537377ef
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-18 19:38:25 -0300

    radar

---
+++ apps/frontend/templates/sites/radarcultura/_signature.php
@@ -0,0 +1,17 @@
+ <div class="row-fluid signature">
+    <small>Quem escreveu</small>
+    <small class="pull-right">00/00/00 00:00- Atualizado em 00/00/00 00:00</small>  
+  </div>
+  <div class="row-fluid cabecalho">
+    <div class="share content">
+        <div class="google">
+          <g:plusone size="medium" count="false"></g:plusone>
+        </div>
+        <div class="fb">
+          <fb:like href="<?php echo $uri ?>" layout="button_count" show_faces="false" send="false" width="160"></fb:like>
+        </div>
+        <div class="twt">
+          <a href="http://twitter.com/share" class="twitter-share-button" data-count="false" data-via="portalcmais" data-related="tvcultura">Tweet</a>
+        </div>
+      </div>
+  </div>
--- apps/frontend/templates/sites/radarcultura/contentRadarSuccess.php
+++ apps/frontend/templates/sites/radarcultura/contentRadarSuccess.php
@@ -37,23 +37,7 @@
             <div class="content">
               <h1><?php echo $asset->getTitle() ?></h1>
               <small><?php echo $asset->getDescription() ?></small>
-              <div class="row-fluid signature">
-                <small>Quem escreveu</small>
-                <small class="pull-right">00/00/00 00:00- Atualizado em 00/00/00 00:00</small>  
-              </div>
-              <div class="row-fluid cabecalho">
-                <div class="share content">
-                    <div class="google">
-                      <g:plusone size="medium" count="false"></g:plusone>
-                    </div>
-                    <div class="fb">
-                      <fb:like href="<?php echo $uri ?>" layout="button_count" show_faces="false" send="false" width="160"></fb:like>
-                    </div>
-                    <div class="twt">
-                      <a href="http://twitter.com/share" class="twitter-share-button" data-count="false" data-via="portalcmais" data-related="tvcultura">Tweet</a>
-                    </div>
-                  </div>
-              </div>
+              <?php include_partial_from_folder('sites/radarcultura', 'global/signature', array('uri'=>$uri)) ?>
             <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
             <?php if ($related[0]->AssetImage->getOriginalUrl()): ?>
             <p>
