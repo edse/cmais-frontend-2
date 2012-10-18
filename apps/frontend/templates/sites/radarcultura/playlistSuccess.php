2fb15c4325ddb7ce2f454fdedfbc6d31acfbfce2
Emerson <emerson.estrella@gmail.com>
2012-10-18 19:42:48 -0300

    test

--- apps/frontend/templates/sites/radarcultura/playlistSuccess.php
+++ apps/frontend/templates/sites/radarcultura/playlistSuccess.php
@@ -250,6 +250,7 @@
             data: opts,
             dataType: "text",
             success: function(data) {
+              alert(data)
               $('#socialLoading').fadeOut();
               if(data == "1"){
                 $('#socialAlert').fadeIn();
