16db36923bd22ec3dfa7153e38bfa52388bc8cfd
Emerson <emerson.estrella@gmail.com>
2012-10-22 13:36:30 -0200

    asdf

--- apps/frontend/templates/sites/radarcultura/musicasSuccess.php
+++ apps/frontend/templates/sites/radarcultura/musicasSuccess.php
@@ -49,11 +49,11 @@
                 </div>  
                 <div class="row-fluid">
                   <label class="radio inline" style="margin-left: 35px">
-                    <input type="radio" name="busca-por" id="busca-por1" value="musicas" checked>
+                    <input type="radio" name="busca-por" id="busca-por1" value="musicas" checked="checked" />
                     Por Título
                   </label>
                   <label class="radio inline">
-                    <input type="radio" name="busca-por" id="busca-por2" value="artistas">
+                    <input type="radio" name="busca-por" id="busca-por2" value="artistas" />
                     Por Artista
                   </label>
                 </div>
@@ -276,9 +276,9 @@
 
           $('#busca-radar').submit(function() {
             if($("#busca-por1:checked"))
-              self.location.href = "/"+$('#busca-por1').val()+"/busca-por/"+$('#busca-input').val();
+              self.location.href = "/musicas/busca-por/"+$('#busca-input').val();
             else if($("#busca-por2:checked"))
-              self.location.href = "/"+$('#busca-por2').val()+"/busca-por/"+$('#busca-input').val();
+              self.location.href = "/artistas/busca-por/"+$('#busca-input').val();
             return false;
           });                    
           
