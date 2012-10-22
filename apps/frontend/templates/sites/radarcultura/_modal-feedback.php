4fefe21be5d2bc6ff3515836415dc58ce5eab0a9
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-22 16:02:03 -0200

    radar

--- apps/frontend/templates/sites/radarcultura/_modal-feedback.php
+++ apps/frontend/templates/sites/radarcultura/_modal-feedback.php
@@ -43,7 +43,7 @@
     <div id="ok" class="alert alert-block alert-in">
       <span class="badge"><strong>Obrigado pela sua participação!</strong></span><br/>
       <span> Sua opinião é muito importante pra nós!</span><br/>
-      <span> Caso a janela não feche em 4 segundos <a href="#" class="close-modal">Clique aqui<a/></span>
+      <span> Caso a janela não feche em 4 segundos <a href="#" class="close-modal">Clique aqui</a></span>
       
     </div>
   </div>
@@ -53,7 +53,7 @@
     <div id="error" class="alert alert-error alert-in">
       <span class="badge"><strong>Erro!</strong></span><br/>
       <span> Sua mensagem não foi enviada, tente novamente mais tarde</span><br/>
-      <span> Caso a janela não feche em 4 segundos <a href="javascript:;" class="close-modal">Clique aqui<a/></span>
+      <span> Caso a janela não feche em 4 segundos <a href="javascript:;" class="close-modal">Clique aqui</a></span>
     </div>
   </div>
   <!-- /Modal Feedback resposta-->
