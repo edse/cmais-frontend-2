efb92bccf02b4d5c49e7a24418861214437677c2
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-23 19:54:53 -0200

    radar

--- apps/frontend/templates/sites/radarcultura/musicasSuccess.php
+++ apps/frontend/templates/sites/radarcultura/musicasSuccess.php
@@ -134,7 +134,7 @@
                   <td class="performer-<?php echo $value ?>"><?php echo str_ireplace("Por ", "", $d->getDescription()); ?></td>
                   <td class="composer-<?php echo $value ?>"><?php echo $aux[4] ?></td>
                   <td class="play">
-                    <span class="label label-important indicada"  style="display:none;">Música indicada.</span>
+                    <span id="indicada-<?php echo $value ?>" class="label label-important indicada"  style="display:none;">Música indicada.</span>
                     <a href="<?php echo url_for('@homepage') ?>musicas/<?php echo $d->getSlug(); ?>" class="btn btn-mini btn-inverse pull-right" style="margin-left: 5px;"><i class="icon-list icon-white"></i> ver detalhes </a>
                     <a href="javascript:;" class="btn btn-mini btn-info pull-right socialBtn" id="socialBtn-<?php echo $value ?>" name="<?php echo $value ?>" rel="popover" data-content='<div class="btn-toolbar"><div class="btn-group"><a class="btn" href="javascript:postTwitter();">Twitter</a><a class="btn" href="javascript:postToFeed();">Facebook</a><a class="btn" href="javascript:postGoogle();">Google+</a></div><div class="btn-group"><a class="btn btn-email" href="#" onClick="goTop();" data-toggle="modal" data-target="#modal">Email</a></div></div>' data-original-title="Selecione sua rede social..."><i class="icon-share-alt icon-white"></i> Sugira esta música</a>
                     <input class="url-<?php echo $value ?>" type="hidden" value="<?php echo "http://radarcultura.cmais.com.br" . url_for('@homepage').$section->getSlug() . '/' . $d->getSlug() ?>" />
@@ -267,7 +267,7 @@
               <div class="row-fluid">
                 <div class="modal-footer musica">
                   <!--<a data-dismiss="modal" aria-hidden="true" class="btn btn-fechar">Fechar</a>-->
-                  <input type="hidden" id="btn-pressed" value="">
+                  <input type="hidden" id="btn-pressed" value="" name="">
                   <img src="/portal/images/ajax-loader.gif" alt="carregando..." style="display:none; margin: 0 30px;" width="16px" height="16px" id="loader3" />
                   <input type="submit" class="btn btn-info btn-enviar" value="Enviar" />
                 </div>
@@ -331,7 +331,7 @@
                     $("#modal").modal('hide');
                     $('.socialBtn').popover('hide');
                     $('#'+$('#btn-pressed').attr('value')).fadeOut('fast');
-                    $('.indicada').fadeIn('fast');
+                    $('#'+$('#btn-pressed').attr('name')).fadeIn('fast');
                     $('.alert.radarIndex').hide();
                     $("#socialAlertOk").fadeIn('fast');
                     setTimeout('$("#socialAlertOk").hide();', 10000);
