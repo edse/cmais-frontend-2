b77a49d3fa3c1c81f4a8f531b7c6c80f75b1f0ec
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-10-26 20:04:59 -0200

    radar

--- apps/frontend/templates/sites/radarcultura/musicasSuccess.php
+++ apps/frontend/templates/sites/radarcultura/musicasSuccess.php
@@ -316,12 +316,10 @@
               }
             },
             highlight: function(label) {
-              $(label).closest('.control-group').addClass('error');
+              $('label').closest('.control-group').addClass('error');
             },
             success: function(label){
-              label
-                .text('OK!').addClass('valid');
-                .closest('.control-group').addClass('success');
+              $('label').text('OK!').addClass('valid').closest('.control-group').addClass('success');
             },
             submitHandler: function(form){
               $.ajax({
