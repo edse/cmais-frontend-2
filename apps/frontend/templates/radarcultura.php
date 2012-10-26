27c3cae75a35d66b84d7f2ebf75489e535890b6c
Emerson Estrella <emerson.estrella@gmail.com>
2012-10-25 22:47:12 -0200

    facebook pt-br

--- apps/frontend/templates/radarcultura.php
+++ apps/frontend/templates/radarcultura.php
@@ -133,7 +133,7 @@
          var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
          if (d.getElementById(id)) {return;}
          js = d.createElement('script'); js.id = id; js.async = true;
-         js.src = "//connect.facebook.net/en_US/all.js";
+         js.src = "//connect.facebook.net/pt_BR/all.js";
          ref.parentNode.insertBefore(js, ref);
        }(document));
     </script>
