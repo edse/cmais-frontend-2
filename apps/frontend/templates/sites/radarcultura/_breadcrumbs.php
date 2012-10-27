6c8df96c13fa7ff3da4297e1b1f77832f38cbba7
Emerson Estrella <emerson.estrella@gmail.com>
2012-10-27 01:55:49 -0200

    fix

--- apps/frontend/templates/sites/radarcultura/_breadcrumbs.php
+++ apps/frontend/templates/sites/radarcultura/_breadcrumbs.php
@@ -15,11 +15,11 @@
              <?php endif; ?>
            <?php elseif($section->getSlug() == "musicas"): ?>
              <?php if(isset($asset)): ?>
-             <li><a href="<?php echo url_for('homepage') . '/musicas' ?>">Musicas</a> <span class="divider">»</span></li>
+             <li><a href="<?php echo url_for('homepage') . 'musicas' ?>">Musicas</a> <span class="divider">»</span></li>
              <li><?php echo $asset->getTitle() ?></li>
              <?php else: ?>
                <?php if(isset($artist)): ?>
-             <li><a href="<?php echo url_for('homepage') . '/artistas' ?>">Artistas</a> <span class="divider">»</span></li>
+             <li><a href="<?php echo url_for('homepage') . 'artistas' ?>">Artistas</a> <span class="divider">»</span></li>
              <li><?php echo $artist ?></li>
                <?php else: ?>
              <li>Musicas</li>
