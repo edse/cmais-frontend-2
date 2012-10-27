29c52ec80a5a4cbc780ce85025814eb1af3d7dc1
Emerson Estrella <emerson.estrella@gmail.com>
2012-10-27 01:59:36 -0200

    test

--- apps/frontend/templates/sites/radarcultura/_menu.php
+++ apps/frontend/templates/sites/radarcultura/_menu.php
@@ -13,6 +13,7 @@
             <a href="https://twitter.com/radarcultura" class="twitter-follow-button" data-show-count="false" data-lang="pt">Seguir @radarcultura</a>
           </div>
           <div class="fb">
+            <fb:like href="https://www.facebook.com/programaradarcultura" layout="button_count" show_faces="false" send="false" width="160"></fb:like>
             <fb:like href="https://www.facebook.com/programaradarcultura" send="false" layout="button_count" width="200" show_faces="false"></fb:like>
           </div>
         </div>  
