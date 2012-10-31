4e1a6b5f6a1394a5d6583a9ad9341ea2965f64ab
rafaelhcs <rhcsousa@gmail.com>
2012-10-31 13:23:01 -0200

    update

--- apps/frontend/templates/sites/radarcultura/musicaSuccess.php
+++ apps/frontend/templates/sites/radarcultura/musicaSuccess.php
@@ -527,6 +527,13 @@
          </div>
          <!--/coluna direita-->
          
+         <?php 
+
+          $i = 0;
+          do {
+         
+          ?>
+         
          <?php $relacionados = $asset->retriveRelatedAssetsByAssetTypeId(1); ?>
          <?php if(count($relacionados) > 0 || count($relacionados) < 3): ?>
          <!--pela web-->  
@@ -564,6 +571,7 @@
           </div>
           <!--pela web-->
           <?php endif; ?>
+          <?php } while ($i > 3); ?>
           
           <!--banner horizontal-->    
           <div class="container">
