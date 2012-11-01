90d6c7aef688dd2df15ad3b1a76faab471b13594
rafaelhcs <rhcsousa@gmail.com>
2012-11-01 12:45:20 -0200

    update

--- apps/frontend/templates/sites/passalaemcasa/indexSuccess.php
+++ apps/frontend/templates/sites/passalaemcasa/indexSuccess.php
@@ -105,35 +105,7 @@
         <!-- /ESQUERDA -->
         <!-- DIREITA -->
         <div id="direita" class="grid1">
-                  <?php if(isset($displays)): ?>
-              <?php if(count($displays) > 0): ?>
-              <!-- DESTAQUE 1 COLUNA -->
-              <div class="uma-coluna destaque grid1" id="destaque">
-                <ul class="abas-conteudo conteudo">
-                  <?php foreach($displays as $k=>$d): ?>
-                  <li class="filho" id="bloco<?php echo $k+1 ?>" style="display: <?php if($k==0): ?>block<?php else: ?>none<?php endif;?>;">
-                    <?php if($d->retriveImageUrlByImageUsage("image-8") != ""): ?>
-                    <a title="<?php echo $d->getTitle() ?>" href="<?php echo $d->retriveUrl() ?>" class="media">
-                      <img alt="<?php echo $d->getTitle() ?>" src="<?php echo $d->retriveImageUrlByImageUsage("image-8-b") ?>" style="width: 310px;" />
-                    </a>
-                    <?php endif; ?>
-                    <a class="titulos" href="<?php echo $d->retriveUrl() ?>"><?php echo $d->getTitle() ?></a>
-                    <p><?php echo $d->getDescription() ?></p>
-                    <?php if($d->getLabel()!=""): ?>
-                    <p class="data"><?php echo $d->getLabel()?></p>
-                    <?php endif; ?>
-                  </li>
-                  <?php endforeach; ?>
-                </ul>
-                <ul class="abas-menu pag-bola destaque1">
-                  <?php foreach($displays as $k=>$d): ?>
-                  <li<?php if($k==0): ?> class="ativo"<?php endif; ?>><a title="<?php echo $d->getTitle() ?>" href="#bloco<?php echo $k+1 ?>"></a></li>
-                  <?php endforeach; ?>
-                </ul>
-              </div>
-              <!-- /DESTAQUE 1 COLUNA -->
-              <?php endif; ?>
-            <?php endif; ?>
+          <?php include_partial_from_folder('blocks','global/display-1c-vertical-multiple', array('displays' => $displays["destaque-secundario"])) ?>
           <?php if(isset($displays["destaque-padrao-2"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-2"]))?>
           <?php if(isset($displays["destaque-padrao-4"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-4"]))?>
         </div>
