70efbf93d4a91b5f467a438fc2d6fa0575e6fee0
renatareno <asdf@fpawks0142.local>
2012-11-07 11:55:24 -0200

    teleton

--- apps/frontend/templates/sites/teleton/index-bkpSuccess.php
+++ apps/frontend/templates/sites/teleton/index-bkpSuccess.php
@@ -5,7 +5,7 @@
       
 <script>
   // TIMER TRANSMISSAO
-  /*function timer1(){
+  function timer1(){
     var request = $.ajax({
       data: {
         asset_id: '32924',
@@ -18,7 +18,7 @@
       url: '/ajax/timer'
     });
   }
-*/
+
   // Update Twitter Statuses
   function updateTweets(){
     $.ajax({
@@ -29,12 +29,12 @@
       }
     });
   }
-  /*
+  
   $(window).load(function(){
     timer1();
     var t=setInterval("timer1()",60000);
   });
-  */
+  
 
   $(function(){ //onready
     updateTweets();
@@ -53,13 +53,50 @@
       <div id="barra-site">
 
         <div class="topo-programa">
+          <?php if(isset($program) && $program->id > 0): ?>
+          <h2>
+            <a href="<?php echo $program->retriveUrl() ?>" style="text-decoration: none;">
+              <?php if($program->getImageThumb() != ""): ?>
+                <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
+              <?php else: ?>
+                <h3 class="tit-pagina grid1"><?php echo $program->getTitle() ?></h3>
+              <?php endif; ?>
+            </a>
+          </h2>
+          <?php endif; ?>
           
-          <h2><img src="/portal/images/capaPrograma/teleton/logo.png" /></h2>
-          <a href="http://teleton.org.br/welcome.html" title="Teleton" target="_blank" class="numeros"></a>          
-              
+          <?php if(isset($program) && $program->id > 0): ?>
+          <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+          <?php endif; ?>
+
+          <?php if(isset($program) && $program->id > 0): ?>
+            <!-- horario -->
+            <div id="horario">
+              <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
+            </div>
+            <!-- /horario -->
+          <?php endif; ?>
         </div>
 
-       
+        <?php if(isset($siteSections) && $site->getType() != "Portal"): ?>
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+          <?php if(isset($section->slug)): ?>
+            <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
+            <div class="navegacao txt-10">
+              <a href="<?php echo $site->retriveUrl() ?>" title="Home">Home</a>
+              <span>&gt;</span>
+              <a href="<?php echo $section->retriveUrl()?>" title="<?php echo $section->getTitle()?>"><?php echo $section->getTitle()?></a>
+            </div>
+            <?php endif; ?>
+          <?php endif; ?>
+
+        </div>
+        <!-- /box-topo -->
+        <?php endif; ?>
 
       </div>
       <!-- /BARRA SITE -->
@@ -84,14 +121,8 @@
             <div class="btn-compartilhar">
               <p class="compartilhar">Compartilhar</p>
               <!--a class="print" href="JavaScript:window.print();"></a-->      
-              <a class="twt" href="http://twitter.com/TeletonOficial" target="_blank"></a>
-              <a class="fb" href="https://www.facebook.com/tvcultura" target="_blank"></a>
-              <div class="gplus">
-              <!-- Place this tag where you want the +1 button to render. -->
-              <div class="g-plusone" data-size="small" ></div>
-              
-             
-              </div>
+              <a class="twt" href="http://twitter.com/teletonoficial" target="_blank"></a>
+              <a class="fb" href="https://www.facebook.com/TeletonOficial" target="_blank"></a>
             </div>        
           </div>
                 </div>
@@ -100,8 +131,7 @@
             <?php if(isset($displays['destaque-principal'])): ?>
               <!-- DESTAQUE 2 COLUNAS -->
               <div class="duas-colunas destaque grid2"> 
-                  <iframe width="640" height="360" src="http://www.youtube.com/embed/9hgSRNhKPlY?rel=0&wmode=transparent#t=0m0s?version=3&amp;hl=en_US&amp;fs=1" frameborder="0" allowfullscreen></iframe>
-                  <!--
+
                   <?php if($displays['destaque-principal'][0]->Asset->AssetType->getSlug() == "image"): ?>
                   <a class="" href="<?php echo $displays[0]->retriveUrl() ?>" title="<?php echo $displays['destaque-principal'][0]->getTitle() ?>">
                   <img src="<?php echo $displays['destaque-principal'][0]->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $displays['destaque-principal'][0]->Asset->getTitle() ?>" name="<?php echo $displays['destaque-principal'][0]->Asset->getTitle() ?>" />
@@ -126,7 +156,7 @@
                   <a class="" href="<?php echo $displays[0]->retriveUrl() ?>" title="<?php echo $displays['destaque-principal'][0]->getTitle() ?>">
                   <img src="<?php echo $displays['destaque-principal'][0]->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $displays['destaque-principal'][0]->getTitle() ?>" name="<?php echo $displays['destaque-principal'][0]->getTitle() ?>" />
                   <?php endif; ?>
-                -->
+
                 <p class="titulos" style="margin-bottom:0px"><?php echo $displays['destaque-principal'][0]->getTitle() ?></p>
                 <p><?php echo $displays['destaque-principal'][0]->getDescription() ?></p>
               </div>
@@ -153,7 +183,24 @@
               <!-- BOX TWITTER -->
                 <div class="grid1">
                   <a href="http://twitter.com/teletonoficial" class="twitter-follow-button" target="_blank">Siga @teletonoficial</a>
-                 
+                  <style>
+                    #twitter {border:1px solid #666}
+                    #twitter .topo-fb { background-color:#666; overflow:hidden; padding:10px;}
+                    #twitter .avatar { margin-right:10px; float:left; }
+                    #twitter .topo-fb img { width:31px; }
+                    #twitter .topo-fb h3 {font-size:11px; color:#fff;}
+                    #twitter .topo-fb h4 a {font-size:14px; color:#fff; font-weith:bold;}
+                    #twitter ul {background:#fff; height:360px; overflow:hidden;}
+                    #twitter ul img {width:30px;}
+                    #twitter ul li {border-bottom:1px dotted #ddd; padding-top:5px;}
+                    #twitter ul .avatar {margin: 10px;}
+                    #twitter ul li a { color:#ff6633;}
+                    #twitter ul li a,
+                    #twitter ul li p {font-size:12px; line-height:16px; margin-bottom:5px;}
+                    #twitter ul li p {margin-left:50px; padding-right:10px;}
+                    #twitter ul li:last-child {border:none; margin-bottom:10px;}
+                    #twitter .respiro {background:#ffffff; height:20px;}
+                  </style>
                   <div id="twitter"></div>
                   
                 </div>
@@ -168,14 +215,4 @@
       <!-- /MIOLO -->
     </div>
     <!-- /CAPA SITE -->
-     <!-- Place this tag after the last +1 button tag. -->
-              <script type="text/javascript">
-                window.___gcfg = {lang: 'pt-BR'};
-              
-                (function() {
-                  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-                  po.src = 'https://apis.google.com/js/plusone.js';
-                  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-                })();
-              </script>
 
