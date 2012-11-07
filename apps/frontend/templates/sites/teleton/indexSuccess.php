71ae77053094b0aa5559cbc5b15627f969294e67
renatareno <asdf@fpawks0142.local>
2012-11-07 12:10:44 -0200

    topo

--- apps/frontend/templates/sites/teleton/indexSuccess.php
+++ apps/frontend/templates/sites/teleton/indexSuccess.php
@@ -26,10 +26,7 @@
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
-<!--
-<?php use_helper('I18N', 'Date') ?>
-<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
--->
+
 
     <!-- CAPA SITE -->
     <div id="capa-site">
