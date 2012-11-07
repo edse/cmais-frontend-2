d4419ec0f906eeca1c77bf54c6052efc1af5638d
renatareno <asdf@fpawks0142.local>
2012-11-07 12:12:18 -0200

    bug

--- apps/frontend/templates/sites/teleton/indexSuccess.php
+++ apps/frontend/templates/sites/teleton/indexSuccess.php
@@ -24,7 +24,8 @@
 </script>
 
 <?php use_helper('I18N', 'Date') ?>
-<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+
+
 
     <!-- CAPA SITE -->
     <div id="capa-site">
