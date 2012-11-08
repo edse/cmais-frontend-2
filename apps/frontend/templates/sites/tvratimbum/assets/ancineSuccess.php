7b9b31dcc2494076e0f969df9624fbe112bf5530
rafaelhcs <rhcsousa@gmail.com>
2012-11-08 17:15:29 -0200

    update

--- apps/frontend/templates/sites/tvratimbum/assets/ancineSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/assets/ancineSuccess.php
@@ -96,9 +96,13 @@
                 
                 <!--explicacao-->
                 <div class="explicacao">
-                  
-                  <p>Listagem Completa (Anexo I)<a href= "portal/tvratimbum/arquivos_ancine/61914891000186_TVRATIMBUM_112530001_201209_20121026.csv"> 61914891000186_TVRATIMBUM_112530001_201209_20121026.csv</a></p>
-                  <p>Acesso Público (Anexo II)<a href= "portal/tvratimbum/arquivos_ancine/PUB_61914891000186_TVRATIMBUM_112530001_201210_20121026.xlsx"> PUB_61914891000186_TVRATIMBUM_112530001_201210_20121026.xlsx</a></p>
+                  <p><b>> Novembro/2012</b></p>
+                  <p>Listagem Completa (Anexo I)<a href= "portal/tvratimbum/arquivos_ancine/11-2012/61914891000186_TVRATIMBUM_112530001_201210_20121107.CSV"> 61914891000186_TVRATIMBUM_112530001_201209_20121026.csv</a></p>
+                  <p>Acesso Público (Anexo II)<a href= "portal/tvratimbum/arquivos_ancine/11-2012/PUB_61914891000186_TVRATIMBUM_112530001_201211_20121108.xlsx"> PUB_61914891000186_TVRATIMBUM_112530001_201210_20121026.xlsx</a></p>
+                  <p><b>> Outubro/2012</b></p>
+                  <p>Listagem Completa (Anexo I)<a href= "portal/tvratimbum/arquivos_ancine/10-2012/61914891000186_TVRATIMBUM_112530001_201209_20121026.csv"> 61914891000186_TVRATIMBUM_112530001_201209_20121026.csv</a></p>
+                  <p>Acesso Público (Anexo II)<a href= "portal/tvratimbum/arquivos_ancine/10-2012/PUB_61914891000186_TVRATIMBUM_112530001_201210_20121026.xlsx"> PUB_61914891000186_TVRATIMBUM_112530001_201210_20121026.xlsx</a></p>
+                            
                 </div>
                 <!--explicacao--> 
  
