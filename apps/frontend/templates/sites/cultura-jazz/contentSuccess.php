c262e947d7a0cb4c2039a55866b09fbbe208ad37
rafaelhcs <rhcsousa@gmail.com>
2012-11-14 16:59:38 -0200

    update

--- apps/frontend/templates/sites/cultura-jazz/contentSuccess.php
+++ apps/frontend/templates/sites/cultura-jazz/contentSuccess.php
@@ -156,11 +156,11 @@ $(function(){
               <!-- BOX PADRAO -->
               <?php if(isset($displays["destaque-apresentadores"])) include_partial_from_folder('blocks','global/display-1c-hosts', array('displays' => $displays["destaque-apresentadores"])) ?>
               <!-- /BOX PADRAO -->
-                         
+                    teste1     
             <?php $relacionados_audio = $asset->retriveRelatedAssetsByRelationType(5); ?>
-            <?php print_r($relacionados_audio) ?>
+            teste2
               <?php if(count($relacionados_audio) > 0): ?>
-              teste
+            
               <!-- /BOX PLAYLISTS -->
               <div class="span4 direita">
             <link href="/portal/js/audioplayer/css/jplayer.blue.monday.css" rel="stylesheet" type="text/css" />
