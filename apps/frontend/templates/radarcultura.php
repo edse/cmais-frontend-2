da50c2fa4166ba2295641caf7b97631839f75fb9
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-11-19 16:49:25 -0200

    c

--- apps/frontend/templates/radarcultura.php
+++ apps/frontend/templates/radarcultura.php
@@ -7,18 +7,18 @@
       <link rel="stylesheet" type="text/css" href="/portal/css/ie-only.css" />
     <![endif]-->
     
-    <?php if($site->getSlug() == "radarcultura"){?>
-      <!-- Le styles--> 
-      <link href="/portal/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-      <link href="/portal/js/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-      <link href="/portal/css/tvcultura/sites/radarcultura.css" rel="stylesheet" type="text/css" />
-      
-      <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-      <!--[if lt IE 9]>
-        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <![endif]-->
-    <?php }?>
+
+    <!-- Le styles--> 
+    <link href="/portal/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/portal/js/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="/portal/css/tvcultura/sites/radarcultura.css" rel="stylesheet" type="text/css" />
     
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="Cache-Control" content="no-cache, no-store" />
     <meta http-equiv="Pragma" content="no-cache, no-store" />
