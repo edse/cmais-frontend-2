89d67ae10f5f5f36f5bf72c3b5dc971d38e2fcb1
renatareno <asdf@fpawks0142.local>
2012-11-19 15:22:19 -0200

    subsection colunista sem link destaque secundario

--- apps/frontend/templates/sites/culturafm/subsectionColunistaSuccess.php
+++ apps/frontend/templates/sites/culturafm/subsectionColunistaSuccess.php
@@ -105,12 +105,17 @@ if(isset($pager)){
 					<div id="destaque" class="uma-coluna destaque grid1">
 						<ul class="abas-conteudo conteudo">
 							<li style="display: block; height: auto;" id="bloco1" class="filho">
-								<a class="media" href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" title="<?php echo $displays['sobre'][0]->getTitle() ?>"> 
-									<img src="<?php echo $displays['sobre'][0]->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $displays['sobre'][0]->getTitle() ?>">
-								</a>
-								<a href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" class="titulos" title="<?php echo $displays['sobre'][0]->getTitle() ?>"><?php echo $displays['sobre'][0]->getTitle() ?></a>
-									<p><?php echo $displays['sobre'][0]->getDescription() ?></p>
-								</li>
+							  <img src="<?php echo $displays['sobre'][0]->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $displays['sobre'][0]->getTitle() ?>">
+								<p class="titulos"><?php echo $displays['sobre'][0]->getTitle() ?></p>
+								<p><?php echo $displays['sobre'][0]->getDescription() ?></p>
+						  </li>
+							<!--li style="display: block; height: auto;" id="bloco1" class="filho">
+               <a class="media" href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" title="<?php echo $displays['sobre'][0]->getTitle() ?>"> 
+                 <img src="<?php echo $displays['sobre'][0]->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $displays['sobre'][0]->getTitle() ?>">
+               </a>
+               <a href="<?php echo $displays['sobre'][0]->retriveUrl() ?>" class="titulos" title="<?php echo $displays['sobre'][0]->getTitle() ?>"><?php echo $displays['sobre'][0]->getTitle() ?></a>
+                 <p><?php echo $displays['sobre'][0]->getDescription() ?></p>
+              </li-->
 						</ul>
 						<?php
 						/*
