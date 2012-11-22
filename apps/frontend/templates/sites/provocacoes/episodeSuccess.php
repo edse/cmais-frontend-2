b50d1f773d579dc5cd8d0c6e7f75f6ac9b204931
rafaelhcs <rhcsousa@gmail.com>
2012-11-22 18:44:00 -0200

    update

--- apps/frontend/templates/sites/provocacoes/episodeSuccess.php
+++ apps/frontend/templates/sites/provocacoes/episodeSuccess.php
@@ -81,7 +81,7 @@
             <div class="centroRV">
               <div class="video-interna">
                 <ul>
-                  <li class="voltarJa"><a href="javascript:back()"><span>Voltar</span></a></li>
+                  <li class="voltarJa"><a href="javascript:history.back()"><span>Voltar</span></a></li>
                   
                 </ul>
                 <div class="boxVideo">
