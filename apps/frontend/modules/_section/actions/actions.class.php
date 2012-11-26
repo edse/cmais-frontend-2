954103858e2311274e711071d70a60e40416913a
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-11-26 18:25:05 -0200

    fpa

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -1103,7 +1103,9 @@ class _sectionActions extends sfActions
     elseif($this->site->slug == 'sic'){
       $this->setLayout(false);
     }
-    
+    elseif($this->site->slug == 'central-de-relacionamento'){
+      $this->setLayout(false);
+    }   
     // mail sender
     $email_site = $this->section->getContactEmail();
     if(isset($email_site)) {
