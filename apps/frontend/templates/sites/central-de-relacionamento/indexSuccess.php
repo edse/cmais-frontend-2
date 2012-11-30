a831b98e7b6ff5fe636a9ffbb0fc2071f618cf0f
rafaelhcs <rhcsousa@gmail.com>
2012-11-30 16:27:56 -0200

    update

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -37,11 +37,13 @@ $(document).ready(function(){
          
          <?php if(isset($displays['box-1'])):?>
         <?php if(count($displays['box-1']) > 0): ?>
+       	<?php foreach($displays['chamada'] as $k=>$d): ?>
           <i class="icone"></i>
           <span class="tit-perg"><?php echo $d->getTitle() ?></span>
           <span class="desc-perg"><?php echo $d->getDescription() ?></span>
         </a>
         <a href="<?php echo $d->Asset->AssetContent->getHeadline() ?>" class="btn btn-primary btn-large btn-block mais-info botoes blue" title="Perguntas Frequentes">
+           <?php endforeach; ?>
            <?php endif; ?>
         <?php endif; ?>
         
