106f6eeb02bb75d924ec4ee7f65528611ee3ff3d
rafaelhcs <rhcsousa@gmail.com>
2012-12-06 16:36:08 -0200

    update

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -72,8 +72,8 @@
   <?php if(isset($displays['destaque-principal'])):?>
   <?php if(count($displays['destaque-principal']) > 0): ?> 	
   
-  	  <?php $secao= 0; ?>
-      <?php $secao= $displays['destaque-principal'][0]-> Asset-> getSection(); ?>
+      <?php $secao= 0; ?>     
+      <?php $secao= $displays['destaque-principal'][0]-> Section ->getSlug(); ?>
       <?php $secao_destaque-> $secao ->getSlug(); ?>
       
     <div class="row-fluid conteudo">    
@@ -99,7 +99,7 @@
   	  <?php if(count($displays['receitinhas']) > 0): ?>
       
       <?php $secao= 0; ?>     
-      <?php $secao= $displays['destaque-principal'][0]-> Asset-> getSection(); ?>
+      <?php $secao= $displays['receitinhas'][0]-> Section ->getSlug(); ?>
       <?php $secao_receitinhas-> $secao ->getSlug(); ?>
                     
       <div class="span12">
