969fb01a18d897bd54a7b67646c06c0486262d3e
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2012-12-10 15:23:22 -0200

    radar

--- apps/frontend/templates/sites/radarcultura/indexSuccess.php
+++ apps/frontend/templates/sites/radarcultura/indexSuccess.php
@@ -190,20 +190,16 @@
             <fb:like-box href="https://www.facebook.com/programaradarcultura" width="319" height="675" show_faces="true" border_color="#DDDDDD" stream="true" header="true"></fb:like-box>
           </div>
 
-          <div class="span4" style="*margin:0 0 0 0px !important;">
+          <div class="span4" style="margin:0 0 0 20px\9;*margin:0 0 0 0px !important;">
             <g:plus width="319" href="https://plus.google.com/105450700645861288327" rel="publisher"></g:plus>
             <script type='text/javascript'>
               GA_googleFillSlot("home-geral300x250");
             </script>
           </div>
-          
         </div>
         
         <!--rodape-->
         <div class="row-fluid redes">
-
-          <!--/rodape-->
-        
           <!--banner horizontal-->    
           <div class="container">
             <div class="banner-radio horizontal">
