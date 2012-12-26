853509bcb9687cab33f1fb1930c48fdddcbf8beb
rafaelhcs <rhcsousa@gmail.com>
2012-12-26 14:35:04 -0200

    update

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -13,20 +13,25 @@
 <div class="container tudo">
   <!-- row-->
   <div class="row-fluid">
+  	 <?php if(isset($displays['destaque-topo'])): ?>
+      <?php if(count($displays['destaque-topo']) > 0): ?>
     <div class="span12">
       <div id="myCarousel" class="carousel slide span12">
         <!-- Carousel items -->
         <div class="carousel-inner">
-          <div class="active item ">
-            <a href="#" title="banner"><img src="/portal/images/capaPrograma/cocorico/banner.jpg" class="span12"/></a>
+        	<?php foreach($displays['destaque-topo'] as $k=>$d): ?>  
+          <div class="<?php if($k==0): ?>active <?php endif; ?>item ">
+            <a href="<?php echo $d->getHeadline() ?>" title="<?php echo $d->getTitle() ?>"><img src="<?php echo $d->Asset->retriveImageUrlByImageUsage('original') ?>" class="span12"/></a>
           </div>
-                  
+           <?php endforeach; ?>       
         </div>
         <!-- Carousel nav -->
         <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
         <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
       </div>
     </div>
+    <?php endif; ?>
+    <?php endif; ?>
     <div class="divisoria span12"></div>
   </div>
   <!-- /row-->
