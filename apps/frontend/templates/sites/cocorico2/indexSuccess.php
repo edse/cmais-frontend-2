41f118cb7e0e152101a7fe5123cd51e8723f68d4
rafaelhcs <rhcsousa@gmail.com>
2012-12-26 15:45:02 -0200

    update

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -59,7 +59,7 @@
         <?php if(count($displays['destaque-principal']) > 0): ?> 	
           
           <?php 
-          $secao = $displays['destaque-principal'] -> getSections();
+          $secao = $displays['destaque-principal'][0] -> getSections();
           $secao_destaque = $secao[0];
           ?>
           
