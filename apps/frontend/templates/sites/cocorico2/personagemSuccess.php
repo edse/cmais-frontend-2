87c9c88adfa18932979e1655d584a2f935334a1a
Emerson <emerson.estrella@gmail.com>
2013-01-02 17:08:34 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -9,75 +9,77 @@
 </script>
 <!-- container-->
 <div class="container tudo">
-  <!-- row-->
+
+ <!-- row-->
   <div class="row-fluid menu">
     <div class="navbar">
-      <div class="navbar-inner">
-        <ul class="nav">
-          <li class="personagens"><a href="#"></a></li>
-          <li class="joguinhos"><a class="icon" href="/cocorico/joguinhos" title="Joguinhos"></a><a href="/cocorico/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
-          <li class="brincadeiras"><a class="icon"  href="/cocorico/brincadeiras" title="Brincadeiras"></a><a href="/cocorico/brincadeiras" title="Brincadeiras">Brincadeiras</a><span></span></li>
-          <li class="tvcoco"><a class="icon"  href="/cocorico/tvcocorico" title="TV Cocoricó"></a><a href="/cocorico/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
-          <li class="diario"><a class="icon"  href="/cocorico/diario-do-julio" title="Diário do Júlio"></a><a href="/cocorico/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li>
-          <li class="familia"><a  href="/cocorico/em-familia" title="Em família">Em família</a></li>
-        </ul>
-      </div>
-      <div class="lista-personagens">
-        <h3>turma</h3>
-        <ul>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
-        </ul>
-      </div>
+      <!-- MENU PRINCIPAL -->
+      <?php include_partial_from_folder('sites/cocorico', 'global/menu') ?>
+      <!--/MENU PRINCIPAL -->
+      
+      <!-- PERSONAGENS -->
+      <?php include_partial_from_folder('sites/cocorico', 'global/personagens', array('site' => $site)) ?>
+      <!--/PERSONAGENS -->
+
     </div>
   </div>
   <!-- /row-->
+  
   <!-- breadcrumb-->
   <ul class="breadcrumb">
-    <li><a href="/cocorico">Cocoricó</a><span class="divider">&rsaquo;</span></li>
-    <li><a href="/cocorico/joguinhos">Joguinhos</a><span class="divider">&rsaquo;</span></li>
-    <li class="active">Nome do Joguinho</li>
+     <li><a href="/cocorico">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="/cocorico/personagens">Personagens</a> <span class="divider">&rsaquo;</span></li>
+     <li class="active"><?php echo $section->getTitle() ?></li>
   </ul>
   <!-- /breadcrumb-->
+
   <!--btn voltar-->
   <a href="#" class="voltar">voltar<span class="divisao"></span></a>
   <!-- /btn voltar-->
+
   <!--row-->
   <div class="row-fluid conteudo">
     <div class="span8 col-esq">
       <!-- titulo da pagina -->
       <div class="tit-pagina span12">
-        <h2>Alípio</h2>
+        <h2><?php echo $section->getTitle() ?></h2>
       </div>
       <!-- titulo da pagina -->
       <div class="destaque-home">
-        <a href="/cocorico/joguinhos" class="span9"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" /></a>
+        <a href="#" class="span9"><img class="span12" src="<?php echo $displays["imagens"][0]->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $displays["imagens"][0]->getTitle() ?>" /></a>
         <div class="box span3">
           <ul>
-            <li><a href="/cocorico/joguinhos-interna" title="jogo"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="nome jogo" /></a></li>
-            <li><a href="/cocorico/joguinhos-interna" title="jogo"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="nome jogo" /></a></li>
-            <li><a href="/cocorico/joguinhos-interna" title="jogo"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="nome jogo" /></a></li>
+            <li><a href="#" title="<?php echo $displays["imagens"][1]->getTitle() ?>"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="<?php echo $displays["imagens"][1]->getTitle() ?>" /></a></li>
+            <li><a href="#" title="<?php echo $displays["imagens"][2]->getTitle() ?>"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="<?php echo $displays["imagens"][2]->getTitle() ?>" /></a></li>
+            <li><a href="#" title="<?php echo $displays["imagens"][3]->getTitle() ?>"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="<?php echo $displays["imagens"][3]->getTitle() ?>" /></a></li>
           </ul>
         </div>
-        <p>É um pangaré legítimo. Grande amigo de todos, Alípio é puro e ingênuo, às vezes um tanto lento para entender o que os outro dizem. Um pouquinho preguiçoso e comilão, é um amigo fiel. Trabalha na fazenda ajudando o avô. É apaixonado por uma égua da fazenda vizinha, a Fifi, que nunca aparece na fazenda. Adora brincar com a turma.</p>
+        <p><?php echo html_entity_decode($displays["texto"][0]->Asset->AssetContent->render()) ?></p>
       </div>
       <div class="span12">
-        <a class="box destaques span6" href="/cocorico/receitinhas" title="jogo">
-        <p class="bold">
-          CLIPE
-        </p> <iframe width="301" height="182" src="http://www.youtube.com/embed/lWV1kqMJFd0?wmode=transparent#t=0m0s" frameborder="0" allowfullscreen></iframe> Nome do joguinho <span></span> </a>
-        <a class="box destaques span6 joguinho" href="/cocorico/receitinhas" title="jogo">
-         <p class="bold">
-          JOGUINHO
-        </p><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="nome jogo" />Nome do joguinho<span></span></a>
+        <?php if(isset($displays["conteudos"][0])): ?>
+          <?php $se = $displays["conteudos"][0]->Asset->Sections; ?>
+        <a class="box destaques span6" href="<?php echo $displays["conteudos"][0]->Asset->retriveUrl() ?>" title="<?php echo $displays["conteudos"][0]->getTitle() ?>">
+          <p class="bold">
+            <?php echo $se[0]->getTitle() ?>
+          </p>
+          <img src="<?php echo $displays["conteudos"][0]->retriveImageUrlByImageUsage("default") ?>" alt="<?php echo $displays["conteudos"][0]->getTitle() ?>" name="<?php echo $displays["conteudos"][0]->getTitle() ?>" />
+          <?php echo $displays["conteudos"][0]->getTitle() ?><span> </span>
+        </a>
+        <?php endif; ?>
+        <?php if(isset($displays["conteudos"][1])): ?>
+          <?php $se = $displays["conteudos"][1]->Asset->Sections; ?>
+        <a class="box destaques span6" href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>" title="<?php echo $displays["conteudos"][1]->getTitle() ?>">
+          <p class="bold">
+            <?php echo $se[0]->getTitle() ?>
+          </p>
+          <img src="<?php echo $displays["conteudos"][1]->retriveImageUrlByImageUsage("default") ?>" alt="<?php echo $displays["conteudos"][1]->getTitle() ?>" name="<?php echo $displays["conteudos"][1]->getTitle() ?>" />
+          <?php echo $displays["conteudos"][1]->getTitle() ?><span> </span>
+        </a>
+        <?php endif; ?>
       </div>
     </div>
+    <?php if(isset($displays["autografo"][0])): ?>
     <div class="span4 autografo">
       <form class="form-horizontal">
         <h2>Autografo</h2>
@@ -94,7 +96,7 @@
           </div>
         </div>
         <div class="control-group g-autografo">
-          <a href="http://172.20.17.129/portal/images/capaPrograma/cocorico/img-autografo.png" title="BAIXAR" target="_blank"><img src="/portal/images/capaPrograma/cocorico/img-autografo.png" alt="BAIXAR" /></a>
+          <a href="<?php echo $displays["autografo"][0]->retriveImageUrlByImageUsage('original') ?>" title="BAIXAR" target="_blank"><img src="<?php echo $displays["autografo"][0]->retriveImageUrlByImageUsage('original') ?>" alt="BAIXAR" /></a>
           <div class="capa-btn">
             <span></span>
             <button type="submit" class="btn">
@@ -104,74 +106,13 @@
         </div>
       </form>
     </div>
+    <?php endif; ?>
   </div>
   <!--/row-->
+  
   <!-- rodape-->
-  <div class="row-fluid  border-top"></div>
-  <div class="row-fluid rodape" >
-    <h3>2012 &copy; tv cultura - fpa</h3>
-    <div class="span2">
-      <a href="#" class="bold" title="Em família">em família</a>
-      <ul>
-        <li><a href="#" title="Na TV">Na TV</a></li>
-        <li><a href="#" title="Nas lojas">Nas lojas</a></li>
-        <li><a href="#" title="Nas Redes">Nas Redes</a></li>
-        <li><a href="#" title="Nos Teatros">Nos Teatros</a></li>
-        <li><a href="#" title="Nos Cinemas">Nos Cinemas</a></li>
-        <li><a href="#" title="Na Web">Na Web</a></li>
-        <li><a href="#" title="Agenda">Agenda</a></li>
-        <li><a href="#" title="Newsletter">Newsletter</a></li>
-        <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
-      </ul>
-    </div>
-    <div class="span2">
-      <a href="#" class="bold" title="Em família">tv cocoricó</a>
-      <ul>
-        <li><a href="#" title="Sobre o programa">Sobre o programa</a></li>
-        <li><a href="#" title="Livro de receitas">Livro de receitas</a></li>
-        <li><a href="#" title="Bastidores">Bastidores</a></li>
-        <li><a href="#" title="Tour Virtual">Tour Virtual</a></li>
-        <li><a href="#" title="Receitinhas">Receitinhas</a></li>
-        <li><a href="#" title="Envie seu vídeo">Envie seu vídeo</a></li>
-        <li><a href="#" title="Enquete">Enquete</a></li>
-      </ul>
-    </div>
-    <div class="span2">
-      <a href="#" class="bold" title="Cocoricó">cocoricó</a>
-      <ul>
-        <li><a href="#" title="Sobre a série">Sobre a série</a></li>
-        <li><a href="#" title="Diário do Júlio">Diário do Júlio</a></li>
-        <li><a href="#" title="Personagens">Personagens</a></li>
-        <li><a href="#" title="Cocoricolândia">Cocoricolândia</a></li>
-        <li><a href="#" title="Autógrafos">Autógrafos</a></li>
-      </ul>
-    </div>
-    <div class="span2 joguinhos">
-      <a href="#" class="bold" title="Jogos e Brincadeiras">Jogos e Brincadeiras</a>
-      <ul>
-        <li><a href="#" title="Joguinhos">Joguinhos</a></li>
-        <li><a href="#" title="Receitinhas">Receitinhas</a></li>
-        <li><a href="#" title="Para colorir">Para colorir</a></li>
-        <li><a href="#" title="Rádio">Rádio</a></li>
-        <li><a href="#" title="Vídeos">Vídeos</a></li>
-        <li><a href="#" title="Clipes musicais">Clipes musicais</a></li>
-        <li><a href="#" title="Papel de parede">Papel de parede</a></li>
-        <li><a href="#" title="Carinhas animadas">Carinhas animadas</a></li>
-        <li><a href="#" title="Cartões Comemorativos">Cartões Comemorativos</a></li>
-        <li><a href="#" title="Atividades para imprimir">Atividades para imprimir</a></li>
-      </ul>
-    </div>
-    <div class="span3 sites">
-      <a href="#" class="bold" title="Sites Relacionados">Sites Relacionados</a>
-      <ul>
-        <li><a href="#" class="quintal" title="Quintal da Cultura">Quintal da Cultura</a></li>
-        <li><a href="#" class="tvrtb" title="TV Rá Tim Bum!">TV Rá Tim Bum!</a></li>
-        <li class="last"><a href="#" class="cultura" title="TV Cultura">TV Cultura</a></li>
-        <li><a href="#" class="castelo" title="Castelo Rá Tim Bum">Castelo Rá Tim Bum</a></li>
-        <li><a href="#" class="vila" title="Vila Sésamo">Vila Sésamo</a></li>
-      </ul>
-    </div>
-  </div>
+  <?php include_partial_from_folder('sites/cocorico', 'global/rodape') ?>
   <!-- /rodape-->
+  
 </div>
 <!-- /container-->
