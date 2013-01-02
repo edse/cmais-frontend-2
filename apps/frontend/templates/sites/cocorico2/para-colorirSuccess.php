23f5916da11943f951994db3914b44767648c1c4
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-02 16:51:26 -0200

    para colorir

--- apps/frontend/templates/sites/cocorico2/para-colorirSuccess.php
+++ apps/frontend/templates/sites/cocorico2/para-colorirSuccess.php
@@ -87,22 +87,14 @@
   <div class="row-fluid relacionados">
     <div class="tit imprima"><span class="mais"></span><a href="/cocorico/joguinhos">Imprima e brinque</a><span></span></div>
     <ul class="destaques-small">
-      <?php echo $displays["destaques"][0]->getTitle();?>
+
             <li class="span2">
               <a href="#" title="">
                 <img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />
                 Nome do Joguinho
               </a>
             </li>
-            <?php 
-            /*
-            <li class="span2"><a href="#" title=""><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />Nome do Joguinho</a></li>
-            <li class="span2"><a href="#" title=""><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />Nome do Joguinho</a></li>
-            <li class="span2"><a href="#" title=""><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />Nome do Joguinho</a></li>
-            <li class="span2"><a href="#" title=""><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />Nome do Joguinho</a></li>
-            <li class="span2"><a href="#" title=""><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" />Nome do Joguinho</a></li>
-           */ 
-           ?>
+
     </ul>
   </div>
   <!-- /row-->
