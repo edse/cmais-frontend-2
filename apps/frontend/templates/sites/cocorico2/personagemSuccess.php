33e27ddadfed4ac4c0be7eb643afbc615691647a
Emerson <emerson.estrella@gmail.com>
2013-01-03 17:32:18 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -45,7 +45,7 @@
       </div>
       <!-- titulo da pagina -->
       <div class="destaque-home">
-        <img class="span12" src="<?php echo $displays["imagens"][0]->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $displays["imagens"][0]->getTitle() ?>" />
+        <img class="span9" src="<?php echo $displays["imagens"][0]->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $displays["imagens"][0]->getTitle() ?>" />
         <div class="box span3">
           <ul>
             <li><img class="span12" src="<?php echo $displays["imagens"][1]->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $displays["imagens"][1]->getTitle() ?>" /></li>
