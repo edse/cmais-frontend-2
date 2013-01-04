4a9a72845e58e233a80e52cab3cc505c4e6a30c6
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-04 11:02:24 -0200

    radio Cocorico

---
+++ apps/frontend/templates/sites/cocorico2/radio-cocoricoSuccess.php
@@ -0,0 +1,163 @@
+<script type="text/javascript" src="/portal/js/bootstrap/tooltip.js"></script>
+<link href="/portal/css/tvcultura/sites/cocorico/brincadeiras.css" rel="stylesheet">
+
+<!-- container-->
+<div class="container tudo">
+ <!-- row-->
+  <div class="row-fluid menu">
+    <div class="navbar">
+      <div class="navbar-inner">
+        <ul class="nav">
+          <li class="personagens"><a href="#"></a></li>
+          <li class="joguinhos"><a class="icon" href="/cocorico/joguinhos" title="Joguinhos"></a><a href="/cocorico/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
+          <li class="brincadeiras"><a class="icon"  href="/cocorico/brincadeiras" title="Brincadeiras"></a><a href="/cocorico/brincadeiras" title="Brincadeiras">Brincadeiras</a><span></span></li>
+          <li class="tvcoco"><a class="icon"  href="/cocorico/tvcocorico" title="TV Cocoricó"></a><a href="/cocorico/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
+          <li class="diario"><a class="icon"  href="/cocorico/diario-do-julio" title="Diário do Júlio"></a><a href="/cocorico/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li>
+          <li class="familia"><a  href="/cocorico/em-familia" title="Em família">Em família</a></li>
+        </ul>
+      </div>
+      <div class="lista-personagens">
+        <h3>turma</h3>
+        <ul>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+          <li><a href="#" title="Astolfo"><img src="/portal/images/capaPrograma/cocorico/menu-astolfo.png" alt="Astolfo" /></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- /row-->
+  <!-- breadcrumb-->
+  <ul class="breadcrumb">
+     <li><a href="/cocorico">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="/cocorico/joguinhos">Joguinhos</a> <span class="divider">&rsaquo;</span></li>
+     <li class="active">Nome do Joguinho</li>
+  </ul>
+  <!-- /breadcrumb-->
+  
+  <!-- titulo da pagina -->
+  <div class="tit-pagina span3">
+    <h2>rádio cocórico</h2>
+  </div>
+  <!-- titulo da pagina -->
+  
+  <!-- row conteudo radio -->
+  <div id="radio" class="row-fluid conteudo">
+    <div class="span12 radio-cocorico">
+     <img src="/portal/images/capaPrograma/cocorico/bg-radio.jpg" />      
+    </div>
+    <!-- row clipes relacionados -->
+    <div class="row-fluid relacionados">
+      
+      <!-- clipe -->
+      <div class="destaque-2 conteudo-diverso span4">
+        <h3>clipe</h3>
+        <a href="#" title="Nome do clipe">
+          <img src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="Receitinha">
+        </a>
+        <p>
+          texto corrido
+          <i class="ico-mais"></i>
+        </p>
+      </div>  
+      <!-- /clipe -->
+      
+      <!-- clipe -->
+      <div class="destaque-2 conteudo-diverso span4">
+        <h3>clipe</h3>
+        <a href="#" title="Nome do clipe">
+          <img src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="Receitinha">
+        </a>
+        <p>
+          texto corrido
+          <i class="ico-mais"></i>
+        </p>
+      </div>  
+      <!-- /clipe -->
+      
+      <!-- clipe -->
+      <div class="destaque-2 conteudo-diverso span4">
+        <h3>clipe</h3>
+        <a href="#" title="Nome do clipe">
+          <img src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="Receitinha">
+        </a>
+        <p>
+          texto corrido
+          <i class="ico-mais"></i>
+        </p>
+      </div>  
+      <!-- /clipe -->
+    </div>
+    <!-- /row clipes relacionados -->
+  </div>
+  <!-- /row conteudo radio -->
+  
+  <!-- rodape-->
+  <div class="row-fluid  border-top"></div>
+  <div class="row-fluid rodape" >
+    <h3>2012 &copy; tv cultura - fpa</h3>
+    <div class="span2">
+      <a href="#" class="bold" title="Em família">em família</a>
+      <ul>
+        <li><a href="#" title="Na TV">Na TV</a></li>
+        <li><a href="#" title="Nas lojas">Nas lojas</a></li>
+        <li><a href="#" title="Nas Redes">Nas Redes</a></li>
+        <li><a href="#" title="Nos Teatros">Nos Teatros</a></li>
+        <li><a href="#" title="Nos Cinemas">Nos Cinemas</a></li>
+        <li><a href="#" title="Na Web">Na Web</a></li>
+        <li><a href="#" title="Agenda">Agenda</a></li>
+        <li><a href="#" title="Newsletter">Newsletter</a></li>
+        <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
+      </ul>
+    </div>
+    <div class="span2"> <a href="#" class="bold" title="Em família">tv cocoricó</a>
+      <ul>
+        <li><a href="#" title="Sobre o programa">Sobre o programa</a></li>
+        <li><a href="#" title="Livro de receitas">Livro de receitas</a></li>
+        <li><a href="#" title="Bastidores">Bastidores</a></li>
+        <li><a href="#" title="Tour Virtual">Tour Virtual</a></li>
+        <li><a href="#" title="Receitinhas">Receitinhas</a></li>
+        <li><a href="#" title="Envie seu vídeo">Envie seu vídeo</a></li>
+        <li><a href="#" title="Enquete">Enquete</a></li>
+      </ul>
+    </div>
+    <div class="span2"> <a href="#" class="bold" title="Cocoricó">cocoricó</a>
+      <ul>
+        <li><a href="#" title="Sobre a série">Sobre a série</a></li>
+        <li><a href="#" title="Diário do Júlio">Diário do Júlio</a></li>
+        <li><a href="#" title="Personagens">Personagens</a></li>
+        <li><a href="#" title="Cocoricolândia">Cocoricolândia</a></li>
+        <li><a href="#" title="Autógrafos">Autógrafos</a></li>
+      </ul>
+    </div>
+    <div class="span2 joguinhos"> <a href="#" class="bold" title="Jogos e Brincadeiras">Jogos e Brincadeiras</a>
+      <ul>
+        <li><a href="#" title="Joguinhos">Joguinhos</a></li>
+        <li><a href="#" title="Receitinhas">Receitinhas</a></li>
+        <li><a href="#" title="Para colorir">Para colorir</a></li>
+        <li><a href="#" title="Rádio">Rádio</a></li>
+        <li><a href="#" title="Vídeos">Vídeos</a></li>
+        <li><a href="#" title="Clipes musicais">Clipes musicais</a></li>
+        <li><a href="#" title="Papel de parede">Papel de parede</a></li>
+        <li><a href="#" title="Carinhas animadas">Carinhas animadas</a></li>
+        <li><a href="#" title="Cartões Comemorativos">Cartões Comemorativos</a></li>
+        <li><a href="#" title="Atividades para imprimir">Atividades para imprimir</a></li>
+      </ul></div>
+    <div class="span3 sites"> <a href="#" class="bold" title="Sites Relacionados">Sites Relacionados</a>
+      <ul>
+        <li><a href="#" class="quintal" title="Quintal da Cultura">Quintal da Cultura</a></li>
+        <li><a href="#" class="tvrtb" title="TV Rá Tim Bum!">TV Rá Tim Bum!</a></li>
+        <li class="last"><a href="#" class="cultura" title="TV Cultura">TV Cultura</a></li>
+        <li><a href="#" class="castelo" title="Castelo Rá Tim Bum">Castelo Rá Tim Bum</a></li>
+        <li><a href="#" class="vila" title="Vila Sésamo">Vila Sésamo</a></li>
+      </ul></div>
+    
+  </div>
+  <!-- /rodape-->
+</div>
+<!-- /container-->
