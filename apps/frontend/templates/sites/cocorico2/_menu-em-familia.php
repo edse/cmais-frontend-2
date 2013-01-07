5f4ebbfdb0dddb20b742b72b9464cb5dd911680b
rafaelhcs <rhcsousa@gmail.com>
2013-01-07 09:01:10 -0200

    update

--- apps/frontend/templates/sites/cocorico2/_menu-em-familia.php
+++ apps/frontend/templates/sites/cocorico2/_menu-em-familia.php
@@ -2,23 +2,23 @@
     <div class="navbar">
       <div class="navbar-inner">
         <ul class="nav span12">
-          <li class="familia"><a  href="/cocorico/em-familia" title="Em família">Em família</a>
+          <li class="familia"><a  href="/cocorico2/em-familia" title="Em família">Em família</a>
             <ul class="nav" id="menu-familia">
-              <li><a class="active" href="/cocorico/natv" title="Na Tv">na tv</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/naslojas" title="Nas Lojas">nas lojas</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/noteatro" title="No Teatro">no teatro</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/nocinema" title="No Cinema">no cinema</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/naweb" title="Na Web">na web</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/agenda" title="Agenda">agenda</a></li>
+              <li><a class="active" href="/cocorico2/natv" title="Na Tv">na tv</a><span class="divider">|</span></li>
+              <li><a href="/cocorico2/naslojas" title="Nas Lojas">nas lojas</a><span class="divider">|</span></li>
+              <li><a href="/cocorico2/noteatro" title="No Teatro">no teatro</a><span class="divider">|</span></li>
+              <li><a href="/cocorico2/nocinema" title="No Cinema">no cinema</a><span class="divider">|</span></li>
+              <li><a href="/cocorico2/naweb" title="Na Web">na web</a><span class="divider">|</span></li>
+              <li><a href="/cocorico2/agenda" title="Agenda">agenda</a></li>
             </ul>
           </li>
-          <li class="joguinhos"><a class="icon" href="/cocorico/joguinhos" title="Joguinhos"></a></li>
-          <li class="brincadeiras"><a class="icon"  href="/cocorico/brincadeiras" title="Brincadeiras"></a></li>
-          <li class="tvcoco"><a class="icon"  href="/cocorico/tvcocorico" title="TV Cocoricó"></a></li>
-          <li class="diario"><a class="icon"  href="/cocorico/diario-do-julio" title="Diário do Júlio"></a></li>
-          <li class="personagens"><a href="/cocorico/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
+          <li class="joguinhos"><a class="icon" href="/cocorico2/joguinhos" title="Joguinhos"></a></li>
+          <li class="brincadeiras"><a class="icon"  href="/cocorico2/brincadeiras" title="Brincadeiras"></a></li>
+          <li class="tvcoco"><a class="icon"  href="/cocorico2/tvcocorico2" title="TV Cocoricó"></a></li>
+          <li class="diario"><a class="icon"  href="/cocorico2/diario-do-julio" title="Diário do Júlio"></a></li>
+          <li class="personagens"><a href="/cocorico2/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
         </ul>
       </div>
      
     </div>
-  </div>
+ </div> 
