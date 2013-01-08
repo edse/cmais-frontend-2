2feab8dee054980883fc6999ce46ca60b6e49b59
rafaelhcs <rhcsousa@gmail.com>
2013-01-08 10:19:00 -0200

    update

--- apps/frontend/templates/sites/tvratimbum/assets/ancineSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/assets/ancineSuccess.php
@@ -95,7 +95,7 @@
                 <!--topo-->
                 
                 <!--explicacao--> 
-                <div class="explicacao">
+                <div class="explicacao"> 
                   <p><b>> Janeiro/2013</b></p>
                   <p>Listagem Completa (Anexo I)<a href= "portal/tvratimbum/arquivos_ancine/01-2013/61914891000186_TVRATIMBUM_112530001_201212_20130107.csv"> 61914891000186_TVRATIMBUM_112530001_201212_20130107.csv</a></p>
                   <p><b>> Dezembro/2012</b></p>
