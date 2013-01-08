5cb53e139434c2a124af1425dae0ccc08269c525
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-08 15:30:54 -0200

    para colorir papel de parede

--- apps/frontend/templates/sites/cocorico2/para-colorirSuccess.php
+++ apps/frontend/templates/sites/cocorico2/para-colorirSuccess.php
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="/portal/js/bootstrap/popover.js"></script>
 <link href="/portal/css/tvcultura/sites/cocorico/brincadeiras.css" rel="stylesheet">
 
 <!-- container-->
