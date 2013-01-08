7053969d78b4d8f56d777cd9029feb6fd0733182
renatareno <asdf@fpawks0142.local>
2013-01-08 16:22:33 -0200

    bug provoca

--- apps/frontend/templates/sites/provocacoes/enforque-seSuccess.php
+++ apps/frontend/templates/sites/provocacoes/enforque-seSuccess.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/contato.css" type="text/css" />
-<script type="text/javascript" src="/js/jquery-ui-1.8.7/js/jquery-ui-1.8.7.custom.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/i18n/jquery-ui-i18n.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/<?php echo $section->Site->getSlug() ?>.css" type="text/css" />
-<!-- <link rel="stylesheet" href="/js/jquery-ui-1.8.7/css/ui-lightness/jquery-ui-1.8.7.custom.css" type="text/css" /> -->
+
+<script type="text/javascript" src="/portal/js/jquery-ui-1.8.7/js/jquery-ui-1.8.7.custom.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/i18n/jquery-ui-i18n.min.js" type="text/javascript"></script>
 <link type="text/css" href="/portal/js/jquery-ui/css/jquery-ui-1.7.2.custom.css" rel="stylesheet" />
 
 <script type="text/javascript">
