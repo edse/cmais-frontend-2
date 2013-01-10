774c0352b6239a02ec614f8f4e79da229d1ac7bc
rafaelhcs <rhcsousa@gmail.com>
2013-01-10 17:23:41 -0200

    update

--- apps/frontend/templates/sites/cocorico2/tv-cocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico2/tv-cocoricoSuccess.php
@@ -174,7 +174,7 @@
       <!-- form interatividade -->
       <div class="destaque-home-tv span9">
       	
-      	<?php echo count($displays['destaque-seu-video'] > 0 ?>
+      	<?php echo count($displays['destaque-seu-video']) ?>
       	
       	
       	<?php if(isset($displays['destaque-seu-video'])): ?>
