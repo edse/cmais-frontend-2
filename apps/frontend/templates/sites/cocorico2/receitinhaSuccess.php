b2096cdbc58b904ad649f69a9d3071625b42e0d9
rafaelhcs <rhcsousa@gmail.com>
2013-01-11 17:59:17 -0200

    update

--- apps/frontend/templates/sites/cocorico2/receitinhaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/receitinhaSuccess.php
@@ -83,7 +83,7 @@
     <ul class="destaques-small">
       <?php foreach($assets as $d): ?>
         <?php $related = $d->retriveRelatedAssetsByAssetTypeId(6); ?>
-      <li class="span2"><a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>"><img class="span12" src="http://img.youtube.com/vi/<?php echo $related[0]->AssetVideo->getYoutubeId() ?>/1.jpg" alt="<?php echo $d->getTitle() ?>" /><?php echo $d->getTitle() ?></a></li>
+      <li class="span2"><a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>"><img class="span12" src="http://img.youtube.com/vi/<?php echo $related[0]->Asset->AssetVideo->getYoutubeId() ?>/1.jpg" alt="<?php echo $d->getTitle() ?>" /><?php echo $d->getTitle() ?></a></li>
       <?php endforeach; ?>
     </ul>
   </div>
