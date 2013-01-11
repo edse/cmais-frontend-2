b760029895dfe2abd648aaf2778c888a043d4970
rafaelhcs <rhcsousa@gmail.com>
2013-01-11 10:56:39 -0200

    update

--- apps/frontend/templates/sites/cocorico2/tv-cocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico2/tv-cocoricoSuccess.php
@@ -42,23 +42,23 @@
     ->execute();
 
   if(count($blocks) > 0){
-    $displays['destaque-tv-cocorico'] = $blocks[0]->retriveDisplays();
+    $displays_tv_cocorico['destaque-tv-cocorico'] = $blocks[0]->retriveDisplays();
   }
 ?>
 
       <div class="tvcoco span12">
         <h2><i class="icon-star-empty"></i>Próximo Convidado<i class="icon-star-empty"></i></h2>
-        <?php if(isset($displays['destaque-tv-cocorico'])):?>
-          <?php if(count($displays['destaque-tv-cocorico']) > 0): ?>
+        <?php if(isset($displays_tv_cocorico['destaque-tv-cocorico'])):?>
+          <?php if(count($displays_tv_cocorico['destaque-tv-cocorico']) > 0): ?>
             <?php
-              $display_img_src = $displays['destaque-tv-cocorico'][0]->retriveImageUrlByImageUsage('original');
+              $display_img_src = $displays_tv_cocorico['destaque-tv-cocorico'][0]->retriveImageUrlByImageUsage('original');
               if ($display_img_src == '') {
-                $related = $displays['destaque-tv-cocorico'][0]->Asset->retriveRelatedAssetsByRelationType('Preview');
+                $related = $displays_tv_cocorico['destaque-tv-cocorico'][0]->Asset->retriveRelatedAssetsByRelationType('Preview');
                 $display_img_src = $related[0]->retriveImageUrlByImageUsage('original');
               }
             ?>
             
-        <a class="convidado span12" href="<?php echo $displays['destaque-tv-cocorico'][0]->retriveUrl() ?>" title="Próximo convidado: <?php echo $displays['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays['destaque-tv-cocorico'][0]->getTitle() ?>" /><?php echo $displays['destaque-tv-cocorico'][0]->getTitle() ?><span class="mais"></span></a>
+        <a class="convidado span12" href="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->retriveUrl() ?>" title="Próximo convidado: <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>" /><?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?><span class="mais"></span></a>
           <?php endif; ?>
         <?php endif; ?>
         <!-- enquete -->
