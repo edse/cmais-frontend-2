7af3f84167442ad047f63fded7d5dfcd1e8cdbee
Emerson <emerson.estrella@gmail.com>
2013-01-11 15:27:17 -0200

    test

---
+++ apps/frontend/templates/sites/cmais/assets/teste-de-streamingSuccess.php
@@ -0,0 +1,19 @@
+
+        <script src="http://cmais.com.br/portal/js/mediaplayer/swfobject.js" type="text/javascript"></script>
+        <h1>asdf asdf sa</h1>
+        <div id="radarcultura"></div>
+          <script>
+            var so = new SWFObject("http://cmais.com.br/portal/js/mediaplayer/player.swf","cam1","440","294","9");
+            so.addParam("allowscriptaccess","always");
+            so.addParam("allowfullscreen","true");
+            so.addParam("wmode","transparent");
+            so.addVariable('volume', "75");
+            so.addVariable('controlbar', "over");
+            so.addVariable('autostart', "true");
+            so.addVariable('streamer', 'rtmp://200.136.27.12/live');
+            so.addVariable('file', "tv");
+            so.addVariable('type', 'video');
+            so.write("radarcultura");
+          </script>
+          
+          <br /><br /><br />
