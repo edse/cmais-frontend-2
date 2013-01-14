b3607db5bc78076f9af3acf8814072cf340e7938
renatareno <asdf@fpawks0142.local>
2013-01-14 20:18:03 -0200

    remocao diario do julio menu

--- apps/frontend/templates/sites/cocorico2/_menu.php
+++ apps/frontend/templates/sites/cocorico2/_menu.php
@@ -4,7 +4,7 @@
     <li class="joguinhos"><a class="icon" href="cocorico2/joguinhos" title="Joguinhos"></a><a href="cocorico2/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
     <li class="brincadeiras"><a class="icon"  href="cocorico2/brincadeiras" title="Brincadeiras"></a><a href="cocorico2/brincadeiras" title="Brincadeiras">Brincadeiras</a><span></span></li>
     <li class="tvcoco"><a class="icon"  href="cocorico2/tv-cocorico" title="TV Cocoricó"></a><a href="cocorico2/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
-    <li class="diario"><a class="icon"  href="cocorico2/diario-do-julio" title="Diário do Júlio"></a><a href="cocorico2/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li>
+    <!--li class="diario"><a class="icon"  href="cocorico2/diario-do-julio" title="Diário do Júlio"></a><a href="cocorico2/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li-->
     <li class="familia"><a  href="cocorico2/em-familia" title="Em família">Em família</a></li>
   </ul>
 </div> 
