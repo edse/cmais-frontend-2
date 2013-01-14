286352220fa3f131263ed52bfa99322966148628
rafaelhcs <rhcsousa@gmail.com>
2013-01-14 13:23:38 -0200

    update

--- apps/frontend/templates/sites/cocorico2/receitinhasSuccess.php
+++ apps/frontend/templates/sites/cocorico2/receitinhasSuccess.php
@@ -79,22 +79,7 @@ $assets = $pager->getResults(); //depois tem de ordenar por ranking...
     <?php endif; ?>
   </div>
   
-  
-  <div class="row-fluid conteudo destaques">
-    <ul class="destaques-small">
-      <?php if(count($assets) > 0): ?>
-        <?php foreach($assets as $k=>$d): ?>
-          <?php// if($k > 2): ?>
-          	<?php $related = $d->retriveRelatedAssetsByAssetTypeId(6); ?>
-            <li class="span2"><a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
-              <img class="span12" src="http://img.youtube.com/vi/<?php echo $related->Asset->AssetVideo->getYoutubeId() ?>/1.jpg" alt="<?php echo $d->getTitle() ?>" />
-              <?php echo $d->getTitle() ?></a>
-            </li>
-          <?php //endif; ?>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </ul>
-  </div>
+ 
   <!-- /row-->
   <?php else: ?>
     <p>Nenhuma receitinha encontrada.</p>
