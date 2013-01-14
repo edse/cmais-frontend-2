55c571a9f4be7aa185b81424fe454c5056ea3cd7
Emerson <emerson.estrella@gmail.com>
2013-01-14 17:05:04 -0200

    coco

--- apps/frontend/templates/sites/cocorico/_menu-em-familia.php
+++ apps/frontend/templates/sites/cocorico/_menu-em-familia.php
@@ -1,22 +1,22 @@
-<div class="row-fluid menu topo-familia">
+  <div class="row-fluid menu topo-familia">
     <div class="navbar">
       <div class="navbar-inner">
         <ul class="nav span12">
-          <li class="familia"><a  href="/cocorico/em-familia" title="Em família">Em família</a>
+          <li class="familia"><a  href="<?php echo $site->retriveUrl() ?>/em-familia" title="Em família">Em família</a>
             <ul class="nav" id="menu-familia">
               <li><a class="active" href="/cocorico/natv" title="Na Tv">na tv</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/naslojas" title="Nas Lojas">nas lojas</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/noteatro" title="No Teatro">no teatro</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/nocinema" title="No Cinema">no cinema</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/naweb" title="Na Web">na web</a><span class="divider">|</span></li>
-              <li><a href="/cocorico/agenda" title="Agenda">agenda</a></li>
+              <li><a href="<?php echo $site->retriveUrl() ?>/naslojas" title="Nas Lojas">nas lojas</a><span class="divider">|</span></li>
+              <li><a href="<?php echo $site->retriveUrl() ?>/noteatro" title="No Teatro">no teatro</a><span class="divider">|</span></li>
+              <li><a href="<?php echo $site->retriveUrl() ?>/nocinema" title="No Cinema">no cinema</a><span class="divider">|</span></li>
+              <li><a href="<?php echo $site->retriveUrl() ?>/naweb" title="Na Web">na web</a><span class="divider">|</span></li>
+              <li><a href="<?php echo $site->retriveUrl() ?>/agenda" title="Agenda">agenda</a></li>
             </ul>
           </li>
-          <li class="joguinhos"><a class="icon" href="/cocorico/joguinhos" title="Joguinhos"></a></li>
-          <li class="brincadeiras"><a class="icon"  href="/cocorico/brincadeiras" title="Brincadeiras"></a></li>
-          <li class="tvcoco"><a class="icon"  href="/cocorico/tvcocorico" title="TV Cocoricó"></a></li>
-          <li class="diario"><a class="icon"  href="/cocorico/diario-do-julio" title="Diário do Júlio"></a></li>
-          <li class="personagens"><a href="/cocorico/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
+          <li class="joguinhos"><a class="icon" href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos"></a></li>
+          <li class="brincadeiras"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/brincadeiras" title="Brincadeiras"></a></li>
+          <li class="tvcoco"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó"></a></li>
+          <li class="diario"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/diario-do-julio" title="Diário do Júlio"></a></li>
+          <li class="personagens"><a href="<?php echo $site->retriveUrl() ?>/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
         </ul>
       </div>
      
