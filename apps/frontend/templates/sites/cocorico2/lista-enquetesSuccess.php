45062327184f1b5dac25f648e1e1d5a820ac66bb
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-15 13:04:17 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/lista-enquetesSuccess.php
+++ apps/frontend/templates/sites/cocorico2/lista-enquetesSuccess.php
@@ -31,6 +31,31 @@
     <h2><?php echo $section->getTitle(); ?></h2>
   </div>
   <!-- titulo da pagina -->
+  <?php
+  //$displays = array();
+  $blocks = Doctrine_Query::create()
+    ->select('b.*')
+    ->from('Block b, Section s')
+    ->where('b.section_id = s.id')
+    ->andWhere('s.slug = ?', "home")//mudar para home quando for no ar
+    ->andWhere('b.slug = ?', 'destaque-tv-cocorico') 
+    ->andWhere('s.site_id = ?', $site->id)
+    ->execute();
+
+  if(count($blocks) > 0){
+    $displays_tv_cocorico['destaque-tv-cocorico'] = $blocks[0]->retriveDisplays();
+  }
+  
+  //pergunta bloco enquete - 1º destaque
+  $q = $displays["enquete"][0]->Asset->AssetQuestion->getQuestion();
+  
+  //doctrine para respostas
+  $respostas = Doctrine_Query::create()
+    ->select('aa.*')
+    ->from('AssetAnswer aa')
+    ->where('aa.asset_question_id = ?', (int)$displays["enquete"][0]->Asset->AssetQuestion->id)
+    ->execute();
+  ?>
   <!--row lista-enquetes-->
   <div id="lista-enquetes" class="row-fluid conteudo destaques">
     <!-- lista -->
@@ -39,7 +64,7 @@
       <li class="item-lista">
         <i class="ico-confirma"></i>
         <h4>dd/mm/aaaa</h4>
-        <h3>pergunta?</h3>
+        <h3><?php echo $q; ?></h3>
         <div class="resultado">00% - alternativa1</div>
         <i class="ico-versus-enquete"></i>
         <div class="resultado verde">alternativa2 - 00%</div>
