7db8fbccb6808eccb2b8d6b65965b4bde7a99962
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-16 09:54:48 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/lista-enquetesSuccess.php
+++ apps/frontend/templates/sites/cocorico2/lista-enquetesSuccess.php
@@ -51,7 +51,7 @@
         if(count($blocks) > 0):
           $displays_home['enquete'] = $blocks[0]->retriveDisplays();
         
-        echo count($blocks[0])."teste>>>>";
+        echo count($blocks[0]->retriveDisplays())."teste>>>>";
         //doctrine para respostas
         $respostas = Doctrine_Query::create()
           ->select('aa.*')
