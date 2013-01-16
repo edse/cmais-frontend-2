cc66c0fdbcb6c0e4c0cae93db2f670867adfb0af
renatareno <asdf@fpawks0142.local>
2013-01-16 15:27:17 -0200

    menu familia diario

--- apps/frontend/templates/sites/cocorico2/_menu-em-familia.php
+++ apps/frontend/templates/sites/cocorico2/_menu-em-familia.php
@@ -15,7 +15,7 @@
           <li class="joguinhos"><a class="icon" href="/cocorico2/joguinhos" title="Joguinhos"></a></li>
           <li class="brincadeiras"><a class="icon"  href="/cocorico2/brincadeiras" title="Brincadeiras"></a></li>
           <li class="tvcoco"><a class="icon"  href="/cocorico2/tvcocorico2" title="TV Cocoricó"></a></li>
-          <li class="diario"><a class="icon"  href="/cocorico2/diario-do-julio" title="Diário do Júlio"></a></li>
+          <!--li class="diario"><a class="icon"  href="/cocorico2/diario-do-julio" title="Diário do Júlio"></a></li-->
           <li class="personagens"><a href="/cocorico2/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
         </ul>
       </div>
