7772156b3d484d7ef7013a7a833a501b56845715
rafaelhcs <rhcsousa@gmail.com>
2013-01-16 12:46:33 -0200

    update

--- apps/frontend/templates/sites/cocorico2/tour-virtualSuccess.php
+++ apps/frontend/templates/sites/cocorico2/tour-virtualSuccess.php
@@ -37,25 +37,29 @@
    
   <div class="row-fluid conteudo erros">
     <p class="tit"></p>
-    <a class="span4 destaque1" title="titulo" href="#">
-   <?php if(isset($displays['destaque-1'])):?>
+    
+  <?php if(isset($displays['destaque-1'])):?>
   	<?php if(count($displays['destaque-1']) > 0): ?>
-  	 <?php $related = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByRelationType('Preview'); ?>
-      <div class="destaque-1 conteudo-tv">
+  	 <?php $related = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByRelationType('Preview'); ?> 
+  	  <a class="span4 destaque1" title="<?php echo $displays['destaque-1'][0]->Asset->getTitle() ?>" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>">
+       <div class="destaque-1 conteudo-tv">
         <h3><?php echo $displays['destaque-1'][0]->Asset->getTitle() ?></h3>
         <img alt="Convidado" src="<?php echo $related[0]->retriveImageUrlByImageUsage('original') ?>">
         <p><?php echo $displays['destaque-1'][0]->Asset->getDescription() ?><i class="ico-mais"></i></p>
       </div>
-      <?php endif; ?>
+     </a>
+    <?php endif; ?>
    <?php endif; ?>
-    </a>
-    <a class="span4 destaque1" title="titulo" href="#">
+     
+    <a class="span4 destaque1" title="titulo" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>">
+    
       <div class="destaque-1 conteudo-tv">
         <h3>Erros de gravação</h3>
         <img alt="Convidado" src="http://midia.cmais.com.br/assets/image/image-6-b/6e0eb40f1da6a84a757b5545ac86e871d0da9ff5.jpg">
         <p> texto corrido <i class="ico-mais"></i></p>
       </div>
     </a>
+    
     <a class="span4 destaque1 last" title="titulo" href="#">
       <div class="destaque-1 conteudo-tv">
         <h3>Erros de gravação</h3>
