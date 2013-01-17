ecdde9e23574d8b0ba98147acc3aaa0c8a230932
renatareno <asdf@fpawks0142.local>
2013-01-17 19:06:43 -0200

    menu familia

--- apps/frontend/templates/sites/cocorico/_menu-em-familia.php
+++ apps/frontend/templates/sites/cocorico/_menu-em-familia.php
@@ -12,10 +12,10 @@
               <li><a href="<?php echo $site->retriveUrl() ?>/agenda" title="Agenda">agenda</a></li>
             </ul>
           </li>
-          <li class="joguinhos"><a class="icon" href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos"></a></li>
-          <li class="brincadeiras"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/brincadeiras" title="Brincadeiras"></a></li>
-          <li class="tvcoco"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó"></a></li>
-          <!--li class="diario"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/diario-do-julio" title="Diário do Júlio"></a></li-->
+          <li class="joguinhos"><a class="icon btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="Joguinhos" href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos"></a></li>
+          <li class="brincadeiras"><a class="icon btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="Brincadeiras" href="<?php echo $site->retriveUrl() ?>/brincadeiras" title="Brincadeiras"></a></li>
+          <li class="tvcoco"><a class="icon btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="TV Cocoricó"  href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó"></a></li>
+          <!--li class="diario"><a class="icon btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="Diário do Júlio" href="<?php echo $site->retriveUrl() ?>/diario-do-julio" title="Diário do Júlio"></a></li-->
           <li class="personagens"><a href="<?php echo $site->retriveUrl() ?>/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
         </ul>
       </div>
