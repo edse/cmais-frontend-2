3548bf322496ab461fb5850fe2f99150a39d2d9e
rafaelhcs <rhcsousa@gmail.com>
2013-01-17 17:52:37 -0200

    update

--- apps/frontend/templates/sites/cocorico2/atividadesSuccess.php
+++ apps/frontend/templates/sites/cocorico2/atividadesSuccess.php
@@ -47,7 +47,8 @@
         <?php if(count($related) > 0): ?>
         <a class="convidado span12" href="<?php echo $displays['destaque-receitinhas'][0]->retriveUrl() ?>" title=""><img src="<?php echo $related[0]->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $displays['destaque-receitinhas'][0]->getTitle() ?>" /> <?php echo $displays['destaque-receitinhas'][0]->getTitle() ?> <span class="mais"></span></a>
         <?php endif; ?>
-        
+        <?php endif; ?>
+        <?php endif; ?>
         
         <?php if(isset($displays['videos-receitinha'])):?>
         <?php if(count($displays['videos-receitinha']) > 0): ?>     
@@ -60,7 +61,8 @@
       </div>
       <?php endforeach; ?>
       <?php endif; ?>
-    
+     <?php endif; ?>
+     
     </div>
     <!-- /col direita -->
     <!-- col esquerda -->
