679ae68b7204327291be264087931dd1abdec08e
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-17 16:16:27 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/para-colorir-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/para-colorir-internaSuccess.php
@@ -80,7 +80,7 @@
     <?php if(count($assets) > 2): ?>
     <ul class="destaques-small">
       <?php foreach($assets as $d): ?>
-      <?php $related = $d->retriveRelatedAssetsByRelationType('original'); ?>
+      <?php $related = $d->retriveRelatedAssetsByRelationType('original');  ?>
       <li class="span2">
         <?php echo $related[0]->retriveImageUrlByImageUsage('image-6-b')."teste5" ?>
         <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
