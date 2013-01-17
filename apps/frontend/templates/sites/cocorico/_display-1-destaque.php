6172b630000c800b5d4362391fe303c9d51153de
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-17 18:03:19 -0200

    coco

--- apps/frontend/templates/sites/cocorico/_display-1-destaque.php
+++ apps/frontend/templates/sites/cocorico/_display-1-destaque.php
@@ -14,10 +14,9 @@
         <div class="carousel-inner">
           <!-- item -->
           <div class="<?php if($k==0) echo "active";?> item" name="<?php echo $k?>">
-            <?php if(($imgs)&&(count($imgs)>0)):?>
-            <a href="<?php echo $d->retriveUrl()?>"><imgs src="<?php echo $imgs[0]->retriveImageUrlByImageUsage('original') ?>"></a>
-            <?php endif; ?>
-            <a class="texto" href="<?php echo $d->retriveUrl()?>">
+
+  
+            <a class="texto" >
               <h3><?php echo $d->getTitle()?></h3>
               <p><?php echo $d->getDescription()?></p>
            </a>
