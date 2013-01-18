f9d84bd4f4fe07d36d3795be38faa787f2b487a9
Emerson <emerson.estrella@gmail.com>
2013-01-18 16:03:36 -0200

    coco

--- apps/frontend/templates/sites/cocorico/_menu.php
+++ apps/frontend/templates/sites/cocorico/_menu.php
@@ -12,13 +12,13 @@
      </div>
  */
 ?> 
-<div class="navbar-inner">
+      <div class="navbar-inner">
         <ul class="nav">
-          <li class="personagens"><a href="/cocorico2/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
+          <li class="personagens"><a href="javascript: history.go(-1);" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
           <li class="joguinhos"><a class="icon" href="/cocorico2/joguinhos" title="Joguinhos"></a><a href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
           <li class="brincadeiras"><a class="icon"  href="/cocorico2/atividades" title="Atividades"></a><a href="<?php echo $site->retriveUrl() ?>/atividades" title="Atividades">Atividades</a><span></span></li>
           <li class="tvcoco"><a class="icon"  href="/cocorico2/tv-cocorico" title="TV Cocoricó"></a><a href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
           <!--li class="diario"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/diario-do-julio" title="Diário do Júlio"></a><a href="<?php echo $site->retriveUrl() ?>/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li-->
           <li class="familia"><a  href="/cocorico2/emfamilia" title="Em família">Em família</a></li>
         </ul>
-     </div>
+      </div>
