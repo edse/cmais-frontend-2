5c453fa97af44d3c2a8b7511779166bf79cfdc5e
rafaelhcs <rhcsousa@gmail.com>
2013-01-18 12:43:10 -0200

    update

--- apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
+++ apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
@@ -52,7 +52,7 @@
         <span><?php echo $asset->AssetContent->getHeadlineShort() ?></span>
       </div>
       <!-- /titulo post -->     
-      <?php include_partial_from_folder('sites/cocorico', 'global/like') ?>
+      <?php include_partial_from_folder('sites/cocorico', 'global/like', array('site'=>$site)) ?>
       <!-- asset -->
       <div class="row-fuid asset">
         <p><?php echo html_entity_decode($asset->AssetContent->render()) ?></p>  
