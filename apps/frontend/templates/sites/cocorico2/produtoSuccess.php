51573afa9696875bd70ac2556b6c1d2fe93a3ca7
rafaelhcs <rhcsousa@gmail.com>
2013-01-18 15:40:59 -0200

    update

--- apps/frontend/templates/sites/cocorico2/produtoSuccess.php
+++ apps/frontend/templates/sites/cocorico2/produtoSuccess.php
@@ -21,7 +21,7 @@
   </div>
   <!-- /row-->
   <!-- menu-->
-  <?php include_partial_from_folder('sites/cocorico', 'global/menu-em-familia') ?>
+  <?php include_partial_from_folder('sites/cocorico', 'global/menu-em-familia', array('site'=>$site)) ?>
   <!-- /menu-->
   
   <!-- breadcrumb-->
