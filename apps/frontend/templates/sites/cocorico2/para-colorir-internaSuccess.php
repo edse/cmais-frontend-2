0fb9de982ba85663ef2ef6b5818f1c1ef7605544
Emerson <emerson.estrella@gmail.com>
2013-01-18 15:37:25 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/para-colorir-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/para-colorir-internaSuccess.php
@@ -61,24 +61,19 @@ $download = $asset->retriveRelatedAssetsByRelationType('Download');
   
   <!--row-->
   <div class="row-fluid relacionados">
-    <div class="tit imprima"><span class="mais"></span><a href="<?php $site->retriveUrl();?>/para-colorir">para colorir</a><span></span></div>
-    
+    <div class="tit imprima"><span class="mais"></span><a href="<?php echo $site->retriveUrl();?>/para-colorir">para colorir</a><span></span></div>
       <?php
       $assets = Doctrine_Query::create()
         ->select('a.*')
         ->from('Asset a, SectionAsset sa, Section s')
         ->where('a.id = sa.asset_id')
         ->andWhere('s.id = sa.section_id')
-        ->andWhere('s.slug = "para-colorir"')
+        ->andWhere('s.slug = ?', "para-colorir")
         ->andWhere('a.site_id = ?', (int)$site->id)
-        //->andWhere('a.asset_type_id = 1')
-        //->andWhere("(a.date_start IS NULL OR a.date_start <= CURRENT_TIMESTAMP)")
-        ->groupBy('sa.asset_id')
-        //->orderBy('a.id desc')
         ->limit(6)
         ->execute();
     ?>
-    <?php if(count($assets) > 2): ?>
+    <?php if(count($assets) > 0): ?>
     <ul class="destaques-small">
       <?php foreach($assets as $d): ?>
       <?php $related = $d->retriveRelatedAssetsByRelationType('Original');  ?>
