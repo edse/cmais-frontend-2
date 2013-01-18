8dd2fb23bc522b30817447080237219038d9361b
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-18 12:45:39 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
@@ -95,11 +95,12 @@
           ->andWhere('b.slug = ?', 'acontece') 
           ->andWhere('s.site_id = ?', $site->id)
           ->execute();
-      
+        echo count($blocks)."<br>";
+        echo count($blocks[0])."<br>";
         if(count($blocks) > 0){
           $displays['acontece'] = $blocks[0]->retriveDisplays();
         }
-        
+        // include_partial_from_folder('sites/cocorico', 'global/display-1-destaque',array('displays'=>$displays['acontece'])) ?>
         ?>
         <!-- destaque -->
         
