f008604ff041770e03d829978538fed9592ef51a
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-18 12:34:28 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
@@ -87,7 +87,7 @@
          </div>
        </div>
        <?php
-        /*
+        
         $displays = Doctrine_Query::create()
           ->select('a.*')
           ->from('Asset a, SectionAsset sa, Section s')
@@ -100,13 +100,91 @@
           ->orderBy('a.id desc')
           ->limit(6)
           ->execute();
-          
+        echo count($displays)."<br>";
+        echo count($displays[0])."<br>";
         if(count($displays) > 0){
           $displays['agenda'] = $displays[0]->retriveDisplays();
         }
-        */
+        
         ?>
-      <?php// include_partial_from_folder('sites/cocorico', 'global/display-1-destaque',array('displays'=>$displays['agenda'])) ?>
+        <!-- destaque -->
+        
+  <?php /*
+  <?php if(isset($displays)): ?>
+  <?php if(count($displays) >= 1): ?>
+
+  <!-- destaque -->
+   <div class="destaque">
+     <!-- carrossel -->
+     <div id="blocoCarrossel" class="carousel slide">
+      <!-- Carousel items -->
+      <div class="carousel-inner"> 
+      <?php foreach($displays as $k=>$d): ?>
+        <?php $imgs = $d->Asset->retriveRelatedAssetsByAssetTypeId(2); ?>
+        <!-- item -->
+        <div class="<?php if($k==0) echo "active";?> item bloco" name="<?php echo $k?>">
+          <?php if(($imgs)&&(count($imgs)>0)):?>
+  
+          <a href="<?php echo $d->retriveUrl()?>">
+            <img src="<?php echo $imgs[0]->retriveImageUrlByImageUsage('original') ?>">
+          </a>
+  
+          <?php endif; ?>
+          <a class="texto" href="<?php echo $d->retriveUrl()?>">
+            <h3><?php echo $d->getTitle()?></h3>
+            <p><?php echo $d->getDescription()?></p>
+         </a>
+        </div>
+        <!-- /item -->
+      <?php endforeach; ?>       
+      </div>
+      <!-- /Carousel items -->
+      <!-- base botoes troca -->
+      <div class="base-btns">
+        <!-- bloco botoes troca -->
+        <div class="bloco-nav">
+        <?php foreach($displays as $k=>$d): ?>
+          <a id="btn-nav<?php echo $k?>" class="btn-nav <?php if($k==0) echo "active";?>" href="javascript:;" name="<?php echo $k?>"></a>
+        <?php endforeach; ?>
+        </div>
+        <!-- /bloco botoes troca -->
+      </div>
+      <!-- /base botoes troca -->
+    </div>
+    <!-- /carrossel -->
+    <script>
+     //conta quantas "<a>" tem e define o tamanho para centralizar
+     var quantidade = 0;
+     $(".bloco-nav a").each(function(index){
+       quantidade ++;
+     })
+     var width = quantidade * 18;
+     $('.bloco-nav').css('width', width+'px');
+     
+     //carrossel
+     $('#blocoCarrossel').carousel({
+       interval: 8000
+     })  
+     $('.btn-nav').click(function(){
+       $('#blocoCarrossel').carousel(parseInt($(this).attr('name')));
+       $('.btn-nav').removeClass('active');
+       $(this).addClass('active');
+     });
+     $('#blocoCarrossel').on('slid',function(){
+       var pos = $(".active.item.bloco").attr('name')
+       $('.btn-nav').removeClass('active');
+       $('#btn-nav'+pos).addClass('active');
+     });
+     </script>
+   </div>
+   <!-- /destaque -->
+   
+    <?php endif; ?>
+  <?php endif; ?>
+   * 
+   */
+   ?>
+        <!-- destaque -->
       <!-- fale conosco cr-->
       <div class="cr">
         <a href="http://www2.tvcultura.com.br/faleconosco/" title="Fale Conosco" target="_blank">Fale conosco</a>
