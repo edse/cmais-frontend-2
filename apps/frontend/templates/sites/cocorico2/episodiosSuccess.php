18c4e4974decbc3a96a2746fd366354382a049d3
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-21 11:51:05 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/episodiosSuccess.php
+++ apps/frontend/templates/sites/cocorico2/episodiosSuccess.php
@@ -1,5 +1,19 @@
+<?php
+$assets = Doctrine_Query::create()
+->select('a.*')
+->from('Asset a, AssetVideo av')
+->where('a.id = av.asset_id')
+->andWhere('a.site_id = ?', 1017)
+->andWhere('a.asset_type_id = 6')
+->andWhere("av.youtube_id != ''")
+->andWhere("(a.date_start IS NULL OR a.date_start <= CURRENT_TIMESTAMP)")
+->orderBy('a.id desc')
+->limit(12)
+->fetchOne();
+?>
 <?php 
-$assets = $pager->getResults();
+ $assets = $pager->getResults($assets);
+ echo count($assets). "teste";
 ?>
 <link href="/portal/css/tvcultura/sites/cocorico/tvcocorico.css" rel="stylesheet">
 <script type="text/javascript">
