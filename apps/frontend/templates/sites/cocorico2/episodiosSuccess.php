79e8230614ce3f8fed7230244b7eef372cd4cd0a
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-21 17:20:20 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/episodiosSuccess.php
+++ apps/frontend/templates/sites/cocorico2/episodiosSuccess.php
@@ -70,7 +70,7 @@ $assets = $pager->getResults();
       <?php foreach($pager->getResults() as $d): ?>
         <li class="span4">
           <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
-            <img class="span12" src="http://img.youtube.com/vi/<?php echo $d->AssetVideo->getYoutubeId() ?>/1.jpg" alt="<?php echo $d->getTitle() ?>" />
+            <img class="span12" src="http://img.youtube.com/vi/<?php echo $d->AssetVideo->getYoutubeId() ?>/2.jpg" alt="<?php echo $d->getTitle() ?>" />
             <p><?php echo $d->getTitle() ?></p>
           </a>
         </li>
