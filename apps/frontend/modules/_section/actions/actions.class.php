4dea4481ba63d1de449d3a79fd45fff0c0e2a8ce
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-21 17:00:05 -0200

    coco

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -1342,8 +1342,8 @@ class _sectionActions extends sfActions
         if(($this->section->Site->getSlug() == "cocorico2")||($this->section->Site->getSlug() == "cocorico")){
           $pagelimit = 12;
         }
-        else if(($this->section->Site->getSlug() == "cocorico" && $this->section->getSlug() == "episodios")||($this->section->Site->getSlug() == "cocorico" && $this->section->getSlug() == "episodios")){
-          
+        else if(($this->section->Site->getSlug() == "cocorico" && $this->section->getSlug() == "episodios")||($this->section->Site->getSlug() == "cocorico2" && $this->section->getSlug() == "episodios")){
+          $pagelimit = 12;  
         }
         $this->pager = new sfDoctrinePager('Asset', $pagelimit);
         $this->pager->setQuery($this->assetsQuery);
