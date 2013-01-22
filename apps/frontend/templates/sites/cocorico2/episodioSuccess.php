372f58413ce178abf6c2ba33d1f5870c61a882e4
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-22 11:48:24 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/episodioSuccess.php
+++ apps/frontend/templates/sites/cocorico2/episodioSuccess.php
@@ -49,17 +49,15 @@
     <?php
     $assets = Doctrine_Query::create()
       ->select('a.*')
-      ->from('Asset a, SectionAsset sa, Section s')
-      ->where('a.id = sa.asset_id')
-      ->andWhere('s.id = sa.section_id')
-      ->andWhere('s.slug = "episodios"')
+      ->from('Asset a, AssetVideo av')
+      ->where('a.id = av.asset_id')
       ->andWhere('a.site_id = ?', (int)$site->id)
-      ->andWhere('a.asset_type_id = 1')
+      ->andWhere('a.asset_type_id = 6')
+      ->andWhere("av.youtube_id != ''")
       ->andWhere("(a.date_start IS NULL OR a.date_start <= CURRENT_TIMESTAMP)")
-      ->groupBy('sa.asset_id')
       ->orderBy('a.id desc')
       ->limit(6)
-      ->execute();
+      ->fetchOne();
   ?>
   <?php echo count($assets).">>>>>>>>>>" ?>
   <?php if (count($assets) > 0): ?>
