b9907a6b741622b7721fcf66d977ab2a9835eb1c
renatareno <asdf@fpawks0142.local>
2013-01-22 19:33:54 -0200

    h2 cmais

--- apps/frontend/templates/sites/cmais/image-gallerySuccess.php
+++ apps/frontend/templates/sites/cmais/image-gallerySuccess.php
@@ -39,11 +39,11 @@ $(window).load(function() {
         
         <?php if(isset($program) && $program->id > 0): ?>
         <div class="topo-programa">
-          <h2>
+          <!--h2>
             <a href="<?php echo $program->retriveUrl() ?>">
               <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
             </a>
-          </h2>
+          </h2-->
           <?php endif; ?>
           
           <?php if(isset($program) && $program->id > 0): ?>
