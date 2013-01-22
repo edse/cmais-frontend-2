2911e99f2078768df817da6a45238af65ae50c14
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-22 17:36:28 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
+++ apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
@@ -26,9 +26,9 @@
   <!-- /row-->
   <!-- breadcrumb-->
   <ul class="breadcrumb">
-     <li><a href="/cocorico">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li><a href="/cocorico">Em família</a> <span class="divider">&rsaquo;</span></li>
-     <li>Agenda</li>
+     <li><a href="<?php echo $site->retriveUrl(); ?>/cocorico2">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="<?php echo $site->retriveUrl(); ?>/cocorico2/agenda">Agenda</a> <span class="divider">&rsaquo;</span></li>
+     <li><?php echo $asset->getTitle(); ?></li>
      <li class="active"></li>
   </ul> 
   <!-- /breadcrumb-->
@@ -83,7 +83,29 @@
          </div>
        </div>
        <!-- /topo acontece -->
-       <?php include_partial_from_folder('sites/cocorico', 'global/display-1-destaque') ?>
+       <?php
+       
+       $blocks = Doctrine_Query::create()
+          ->select('b.*')
+          ->from('Block b, Section s')
+          ->where('b.section_id = s.id')
+          ->andWhere('s.slug = ?', "agenda")
+          ->andWhere('b.slug = ?', 'acontece') 
+          ->andWhere('s.site_id = ?', $site->id)
+          ->execute();
+        
+        //echo count($blocks)."<br>";
+        
+        if(count($blocks) > 0){
+          $displays_acontece['acontece'] = $blocks[0]->retriveDisplays();
+        }
+       
+        if(isset($displays_acontece['acontece'])): 
+          if(count($displays_acontece['acontece']) > 0): 
+            include_partial_from_folder('sites/cocorico', 'global/display-1-destaque', array('displays' => $displays_acontece['acontece']));
+          endif;
+        endif;
+        ?>
        <!-- banner -->
        <div class="">
          <!-- portal-cocorico-300x250 -->
