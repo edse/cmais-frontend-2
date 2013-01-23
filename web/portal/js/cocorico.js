b65f9213c6ca2d6773ab267c7c40511ba9156fd2
Jefferson de Melo Doljak <jeffersondoljak@fpawks0142.local>
2013-01-23 16:55:51 -0200

    coco

--- web/portal/js/cocorico.js
+++ web/portal/js/cocorico.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
     $('.btn-popover').popover();
 
     $('.btn-popover').click(function(){	 
-   	  $('.btn-popover span').not($(this).find("span")).removeClass('ativo').removeClass('true');  
+   	  $('.btn-popover span').removeClass('ativo').removeClass('true');  
       $('.btn-popover').not($(this)).popover('hide');
       $(this).popover({
         trigger:'click',
