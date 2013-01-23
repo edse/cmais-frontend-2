fb4dc96a4c5706fea846bf3fab35618d1ead7134
rafaelhcs <rhcsousa@gmail.com>
2013-01-23 18:43:56 -0200

    update

--- apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
@@ -77,7 +77,13 @@
         <!-- figura -->
         <?php if(count($related_preview[0])>0): ?>
         <li class="span4">
-          <a href="javascript:printDiv('div1')" class="btn-tooltip print" datasrc="<?php echo $related_download[0]->retriveImageUrlByImageUsage("original") ?>" rel="tooltip" data-placement="bottom" data-original-title="imprimir"><img src="<?php echo $related_download[0]->retriveImageUrlByImageUsage("original") ?>" alt="nome brincadeira" /><span></span></a>
+          
+          <a href="javascript:printDiv('div1')" class="btn-tooltip print" 
+          datasrc="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" 
+            rel="tooltip" data-placement="bottom" data-original-title="imprimir">
+            <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" 
+            alt="nome brincadeira" /><span></span></a>
+            
           <div id="div1" style="display: none;page-break-after:always;">
             <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" style="width:95%">
           </div>
