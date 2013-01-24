5eb44aeed50aaab70f07b399246187fbcee28ee7
renatareno <asdf@fpawks0142.local>
2013-01-24 18:23:43 -0200

    teste

--- apps/frontend/templates/sites/cocorico2/papel-de-parede-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/papel-de-parede-internaSuccess.php
@@ -101,7 +101,7 @@
         ->andWhere('s.id = sa.section_id')
         ->andWhere('s.slug = ?', "papel-de-parede")
         ->andWhere('a.site_id = ?', (int)$site->id)
-        ->limit(6)
+        ->limit(7)
         ->execute();
     ?>
     <?php if(count($assets) > 0): ?>
