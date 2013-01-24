81999b93f1f45f80c69f70d26731cf126207585c
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-24 17:59:11 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/produtoSuccess.php
+++ apps/frontend/templates/sites/cocorico2/produtoSuccess.php
@@ -90,7 +90,8 @@
     </div>
      <!-- Modal -->
       <?php endif; ?>
-     <?php endif; ?>
+
+     
       <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">Fechar</button>
