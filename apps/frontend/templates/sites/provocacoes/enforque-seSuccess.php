283124760e748791ed46a444b14d20852ae0439f
renatareno <asdf@fpawks0142.local>
2013-01-24 13:34:14 -0200

    provoca data

--- apps/frontend/templates/sites/provocacoes/enforque-seSuccess.php
+++ apps/frontend/templates/sites/provocacoes/enforque-seSuccess.php
@@ -18,9 +18,10 @@
       inline: true
     });
     //hover states on the static widgets
-    $('#dialog_link, ul#icons li').hover(
-      function() { $(this).addClass('ui-state-hover'); }, 
-      function() { $(this).removeClass('ui-state-hover'); }
+    $('#dialog_link, ul#icons li').hover(function() {
+       $(this).addClass('ui-state-hover'); 
+       } 
+      //function() { $(this).removeClass('ui-state-hover'); }
     );
   });
 </script>
