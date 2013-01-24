559fa214479b86718593bde247078798b2d4c57c
renatareno <asdf@fpawks0142.local>
2013-01-24 15:42:34 -0200

    media

--- apps/frontend/templates/cocorico.php
+++ apps/frontend/templates/cocorico.php
@@ -80,8 +80,7 @@
     <script type="text/javascript" src="/portal/js/bootstrap/tab.js"></script>
     <script type="text/javascript" src="/portal/js/cocorico.js"></script>
     <link href="/portal/css/tvcultura/sites/cocorico/geral.css" rel="stylesheet">
-    <link href="/portal/css/tvcultura/sites/cocorico/media.css" rel="stylesheet">
-   
+    
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -156,5 +155,6 @@
     
     <div id="fb-root"></div> 
  <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+ <link href="/portal/css/tvcultura/sites/cocorico2/media.css" rel="stylesheet">
   </body>
 </html>
