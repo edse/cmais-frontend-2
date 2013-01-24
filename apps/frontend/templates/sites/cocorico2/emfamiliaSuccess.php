7ccb2f4625bc32ec2512363e0ba35b3416d37572
rafaelhcs <rhcsousa@gmail.com>
2013-01-24 16:43:34 -0200

    update

--- apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
@@ -73,7 +73,7 @@
      <?php endif; ?>
     
     <?php else: ?>
-     
+      
       <?php if(isset($displays['destaque-1'])):?>
         <?php if(count($displays['destaque-1']) > 0): ?>   
           <a title="<?php echo $displays['destaque-1'][0]->getTitle() ?>" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>" class="video-ytb destaques span6"> <h2><?php echo $displays['destaque-1'][0]->getTitle() ?></h2><img alt="<?php echo $displays['destaque-1'][0]->getTitle() ?>" src="http://img.youtube.com/vi/<?php echo $related_video[0]->AssetVideo->getYoutubeId() ?>/0.jpg"><p class="bold"><?php echo $displays['destaque-1'][0]->getTitle() ?></p><p><?php echo $displays['destaque-1'][0]->getDescription() ?></p></a>
