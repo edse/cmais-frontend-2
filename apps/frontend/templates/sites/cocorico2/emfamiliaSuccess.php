136ef862b4ad270fc797da6a2a0635d927b8d3a9
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-28 16:02:17 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
@@ -64,7 +64,7 @@
   <?php $related_image = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByAssetTypeId(2); ?>
   <?php $related_video = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
   
-  <?php if(($related_image) > 0): ?> 
+  <?php if(isset($related_image) > 0): ?> 
       <?php if(isset($displays['destaque-1'])):?> 
         <?php if(count($displays['destaque-1']) > 0): ?>   
           <a title="<?php echo $displays['destaque-1'][0]->getTitle() ?>" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>" class="destaques span6"> <h2><?php echo $displays['destaque-1'][0]->getTitle() ?></h2><img alt="<?php echo $displays['destaque-1'][0]->getTitle() ?>" src="<?php echo $displays['destaque-1'][0]->Asset->retriveImageUrlByImageUsage('original') ?>"><p class="bold"><?php echo $displays['destaque-1'][0]->getTitle() ?></p><p><?php echo $displays['destaque-1'][0]->getDescription() ?></p></a>
