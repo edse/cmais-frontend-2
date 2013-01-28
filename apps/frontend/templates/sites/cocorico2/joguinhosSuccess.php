9aba4eb35a856397cd5fbcb05626f5532c22ca6f
renatareno <asdf@fpawks0142.local>
2013-01-28 18:59:47 -0200

    teste topo

--- apps/frontend/templates/sites/cocorico2/joguinhosSuccess.php
+++ apps/frontend/templates/sites/cocorico2/joguinhosSuccess.php
@@ -8,6 +8,22 @@ $assets = $pager->getResults(); //depois tem de ordenar por ranking...
 <!-- container-->
 <div class="container tudo">
   <!-- row-->
+  <div class="row-fluid">
+    <div class="topo-coco">
+      <h1 class="span3"><a href="/cocorico" title="Cocorico"><img src="/portal/images/capaPrograma/cocorico/logo-coco.png" alt="Cocoricó" /></a></h1>
+      <!-- BOX PUBLICIDADE 2 -->
+      <div class="box-publicidade span9">
+        <!-- portal-cocorico -->
+        <script type='text/javascript'>
+        GA_googleFillSlot("portal-cocorico");
+        </script>
+      </div>
+      
+    </div>
+    <div class="divisoria span12"></div>
+  </div>
+  <!-- /row-->
+  <!-- row-->
   <div class="row-fluid menu">
     <div class="navbar">
       <!--menu principal-->
