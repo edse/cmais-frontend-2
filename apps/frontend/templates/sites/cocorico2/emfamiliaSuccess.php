9b2101577f3d6f4872bc27e83f7d6c40cc52d309
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-28 16:17:42 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/emfamiliaSuccess.php
@@ -63,12 +63,11 @@
    
   <?php $related_image = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByAssetTypeId(2); ?>
   <?php $related_video = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
-  <?php echo count($related_image) . "teste >>>>"?>
   <?php if(count($related_image) > 0): ?> 
       <?php if(isset($displays['destaque-1'])):?> 
         <?php if(count($displays['destaque-1']) > 0): ?>   
           <a title="<?php echo $displays['destaque-1'][0]->getTitle() ?>" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>" class="destaques span6">
-             <h2><?php echo $displays['destaque-1'][0]->getTitle() ?> entrei 1</h2>
+             <h2><?php echo $displays['destaque-1'][0]->getTitle() ?></h2>
              <img alt="<?php echo $displays['destaque-1'][0]->getTitle() ?>" src="<?php echo $displays['destaque-1'][0]->Asset->retriveImageUrlByImageUsage('original') ?>">
              <p class="bold"><?php echo $displays['destaque-1'][0]->getTitle() ?></p><p><?php echo $displays['destaque-1'][0]->getDescription() ?></p>
           </a>
@@ -79,7 +78,7 @@
       <?php if(isset($displays['destaque-1'])):?>
         <?php if(count($displays['destaque-1']) > 0): ?>   
           <a title="<?php echo $displays['destaque-1'][0]->getTitle() ?>" href="<?php echo $displays['destaque-1'][0]->retriveUrl() ?>" class="video-ytb destaques span6">
-            <h2><?php echo $displays['destaque-1'][0]->getTitle() ?> entrei 0</h2>
+            <h2><?php echo $displays['destaque-1'][0]->getTitle() ?><?php echo $related_video[0]->AssetVideo->getYoutubeId(). "teste >>>" ?></h2>
             <img alt="<?php echo $displays['destaque-1'][0]->getTitle() ?>" src="http://img.youtube.com/vi/<?php echo $related_video[0]->AssetVideo->getYoutubeId() ?>/0.jpg">
             <p class="bold"><?php echo $displays['destaque-1'][0]->getTitle() ?></p>
             <p><?php echo $displays['destaque-1'][0]->getDescription() ?></p>
