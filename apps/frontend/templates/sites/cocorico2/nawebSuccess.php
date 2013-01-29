4a47b3d15463534fa0ea38f030177472fd2a01eb
unknown <jeffersondoljak@fpawks1115.fpanet.inet>
2013-01-29 17:34:00 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/nawebSuccess.php
+++ apps/frontend/templates/sites/cocorico2/nawebSuccess.php
@@ -119,12 +119,25 @@
                 $.fancybox.wrap.bind("contextmenu", function (e) {
                         return false; 
                 });
+                // Redes Sociais
+                if (this.title) {
+                // New line
+                this.title += '<br />';
+                // Add tweet button
+                this.title += '<a href="https://twitter.com/share" class="twitter-share-button" data-count="none" data-url="' + this.href + '">Tweet</a> ';
+                // Add FaceBook like button
+                this.title += '<iframe src="//www.facebook.com/plugins/like.php?href=' + this.href + '&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:110px; height:23px;" allowTransparency="true"></iframe>';
+                }
               },
               openEffect  : 'none',
               closeEffect : 'none',
               nextEffect  : 'none',
               prevEffect  : 'none', 
               //padding : 0,
+              afterShow: function() {
+                // Render tweet button
+                twttr.widgets.load();
+              },
               helpers : {
                 title : {
                   type : 'float'
