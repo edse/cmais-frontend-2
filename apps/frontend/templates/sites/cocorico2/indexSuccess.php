68143bdab5e0e41cee924857b827eb149e4be5ba
rafaelhcs <rhcsousa@gmail.com>
2013-01-30 11:05:06 -0200

    Link - Destaque 2

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -21,7 +21,7 @@
           <div class="carousel-inner"> 
           	<?php foreach($displays['destaque-topo'] as $k=>$d): ?>  
             <div class="<?php if($k==0): ?>active <?php endif; ?>item ">
-              <a href="<?php echo $d->getLink() ?>" title="<?php echo $d->getTitle() ?>"><img src="<?php echo $d->Asset->retriveImageUrlByImageUsage('original') ?>" class="span12"/></a>
+              <a href="<?php echo $d->getUrl() ?>" title="<?php echo $d->getTitle() ?>"><img src="<?php echo $d->Asset->retriveImageUrlByImageUsage('original') ?>" class="span12"/></a>
             </div>
              <?php endforeach; ?>       
           </div>
