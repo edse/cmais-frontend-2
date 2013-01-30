0e009b85eb41df27d8e0e055b5c3c2bd83b45eed
doljak <jedoljak@gmail.com>
2013-01-30 14:44:32 -0200

    testte

--- apps/frontend/templates/sites/cocorico2/_menu.php
+++ apps/frontend/templates/sites/cocorico2/_menu.php
@@ -5,6 +5,6 @@
     <li class="brincadeiras"><a class="icon"  href="cocorico2/brincadeiras" title="Brincadeiras"></a><a href="cocorico2/brincadeiras" title="Brincadeiras">Brincadeiras</a><span></span></li>
     <li class="tvcoco"><a class="icon"  href="cocorico2/tvcocorico" title="TV Cocoricó"></a><a href="cocorico2/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
     <!--li class="diario"><a class="icon"  href="cocorico2/diario-do-julio" title="Diário do Júlio"></a><a href="cocorico2/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li-->
-    <li class="familia"><a  href="cocorico2/em-familia" title="Em família">Em família</a></li>
+    <li class="familia"><a  href="cocorico2/em-familia" title="Em família">Em família</a></li> 
   </ul>
 </div> 
