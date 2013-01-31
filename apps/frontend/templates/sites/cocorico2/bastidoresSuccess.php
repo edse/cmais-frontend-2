29c1797a50b4c9a717164e1f25440ab827d4a115
rafaelhcs <rhcsousa@gmail.com>
2013-01-31 12:17:02 -0200

    up

--- apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
+++ apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
@@ -20,28 +20,62 @@
     </div>
   </div>
   <!-- /row--> 
-  
-    <!-- breadcrumb-->
-  <ul class="breadcrumb personagem">
-     <li><a href="<?php echo $site->retriveUrl() ?>">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li><a href="<?php echo $site->retriveUrl() ?>/tvcocorico">TV Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li class="active"><?php echo $section->getTitle() ?></li>
+  <!-- breadcrumb-->
+  <ul class="breadcrumb"> 
+    <li><a href="/cocorico">TV Cocoricó</a><span class="divider">&rsaquo;</span></li>
+    <li><a href="/cocorico">Bastidores</a><span class="divider">&rsaquo;</span></li> 
+    <li class="active">Tour virtual</li>
   </ul>
   <!-- /breadcrumb-->
-
-  <h2 class="tit-pagina"><?php echo $section->getTitle() ?></h2>
+  <h2 class="tit-pagina">Tour virtual</h2>
   
   <!--row-->
   <?php if(isset($displays['tour-virtual'])):?>
   <?php if(count($displays['tour-virtual']) > 0): ?>
-  <div class="row-fluid conteudo">  	
+  <div class="row-fluid conteudo">    
   <p><?php echo html_entity_decode($displays['tour-virtual'][0]->Asset->AssetContent->render()) ?></p>
    </div>
   <?php endif; ?>
    <?php endif; ?>
   <!-- /row-->
+  
+  
+  <?php
+    $assets = Doctrine_Query::create()
+      ->select('a.*')
+      ->from('Asset a, SectionAsset sa, Section s')
+      ->where('a.id = sa.asset_id')
+      ->andWhere('s.id = sa.section_id')
+      ->andWhere('s.slug = "bastidores"')
+      ->andWhere('a.site_id = ?', (int)$site->id)
+      ->andWhere('a.asset_type_id = 1')
+      ->andWhere("(a.date_start IS NULL OR a.date_start <= CURRENT_TIMESTAMP)")
+      ->groupBy('sa.asset_id')
+      ->orderBy('a.id desc')
+      ->limit(6)
+      ->execute();
+  ?>
+  <?php if (count($assets) > 0): ?>
   <!--row-->
-   
+  <div class="row-fluid relacionados ytb">
+    <div class="tit"><span class="mais"></span><a href="<?php echo $site->retriveUrl() ?>/receitinhas">Receitinhas</a><span></span></div>
+    <ul class="destaques-small">
+      <?php foreach($assets as $d): ?>
+        <?php $related = $d->retriveRelatedAssetsByAssetTypeId(6); ?>
+      <li class="span2">
+        <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
+          <img class="span12" src="http://img.youtube.com/vi/<?php echo $related[0]->AssetVideo->getYoutubeId() ?>/1.jpg" alt="<?php echo $d->getTitle() ?>" />
+          <p><?php echo $d->getTitle() ?></p>
+        </a>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <!-- /row-->
+  <?php endif; ?>
+  
+  
+  <!--row-->
   <div class="row-fluid conteudo erros">
     <p class="tit"></p>
    
@@ -61,8 +95,8 @@
   <!-- ** DESTAQUE 2 **-->
     
    <?php if(isset($displays['destaque-2'])):?>
-  	<?php if(count($displays['destaque-2']) > 0): ?>
-  	 
+    <?php if(count($displays['destaque-2']) > 0): ?>
+     
       <a class="span4 destaque1" title="titulo" href="<?php echo $displays['destaque-2'][0]->retriveUrl() ?>"> 
       <div class="destaque-1 conteudo-tv">
         <h3><?php echo $displays['destaque-2'][0]->Asset->getTitle() ?></h3>
@@ -76,8 +110,8 @@
      <!-- ** DESTAQUE 3 **-->
     
    <?php if(isset($displays['destaque-3'])):?>
-  	<?php if(count($displays['destaque-3']) > 0): ?>
-  	 
+    <?php if(count($displays['destaque-3']) > 0): ?>
+     
       <a class="span4 destaque1 last" title="titulo" href="<?php echo $displays['destaque-3'][0]->retriveUrl() ?>"> 
       <div class="destaque-1 conteudo-tv">
         <h3><?php echo $displays['destaque-3'][0]->Asset->getTitle() ?></h3>
