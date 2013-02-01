edae6b41c317ec76b869e6a15536582ab1e29916
Emerson Estrella <emerson.estrella@gmail.com>
2013-02-01 15:26:52 -0200

    asdf

--- web/actions/tvratimbum/ideias-mirabolantes/index.php
+++ web/actions/tvratimbum/ideias-mirabolantes/index.php
@@ -1,6 +1,6 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set('display_errors','On');
+error_reporting(E_ALL);
+ini_set('display_errors','On');
 
 $allowedExts = array("jpg", "jpeg", "gif", "png");
 
@@ -18,14 +18,14 @@ if($_FILES["arquivo"]){
       if(is_file($_FILES["arquivo"]["tmp_name"])){
         if(multi_attach_mail("emerson.estrella@gmail.com, ideiasmirabolantestvrtb@gmail.com", array($_FILES["arquivo"]["tmp_name"]), $_POST, $_FILES["arquivo"]["name"], "nao-responda@tvcultura.com.br")){
           unlink($_FILES["arquivo"]["tmp_name"]);
-          header("Location: http://tvratimbum.cmais.com.br/ideias-mirabolantes-sucesso");
-          die();
+          //header("Location: http://tvratimbum.cmais.com.br/ideias-mirabolantes-sucesso");
           echo ">>>>OK!";
+          die();
         }else{
           unlink($_FILES["arquivo"]["tmp_name"]);
-          header("Location: http://tvratimbum.cmais.com.br/ideias-mirabolantes-erro");
-          die();
+          //header("Location: http://tvratimbum.cmais.com.br/ideias-mirabolantes-erro");
           echo ">>>>ERRO!";
+          die();
         }
       }
     }
