ba7d40ef98901fd811c93fa29ad20b90e55245af
doljak <jedoljak@gmail.com>
2013-02-01 12:50:24 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/joguinhosSuccess.php
+++ apps/frontend/templates/sites/cocorico2/joguinhosSuccess.php
@@ -85,9 +85,12 @@ $assets = $pager->getResults(); //depois tem de ordenar por ranking...
         <?php foreach($assets as $k=>$d): ?>
           <?php if($k > 2): ?>
             <?php $related = $d->retriveRelatedAssetsByRelationType('Preview') ?>
-            <li class="span2"><a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
-              <img class="span12" src="<?php echo $related[0]->retriveImageUrlByImageUsage('image-7') ?>" alt="<?php echo $d->getTitle() ?>" />
-              <?php echo $d->getTitle() ?></a>
+            <li class="span2">
+              <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
+                <img class="span12" src="<?php echo $related[0]->retriveImageUrlByImageUsage('image-7') ?>" alt="<?php echo $d->getTitle() ?>" />
+                <?php //echo $d->getTitle() ?>
+                <?php $tam=18; $str=$assets[2]->getTitle(); mb_internal_encoding("UTF-8"); if(strlen($str) <= $tam) echo $str; else echo mb_substr($str, 0, $tam-1)."&hellip;" ?>
+              </a>
             </li>
           <?php endif; ?>
         <?php endforeach; ?>
