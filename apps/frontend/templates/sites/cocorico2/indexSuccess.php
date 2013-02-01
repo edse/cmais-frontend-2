6917738dc9737e0246436f28d397ef5b078f5bd0
rafaelhcs <rhcsousa@gmail.com>
2013-02-01 13:24:58 -0200

    up

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -94,9 +94,9 @@
         <?php endif; ?>
         <div class="box span3">
           <span class="mais"></span>
-          <div class="tit"><a href="<?php echo $site->retriveUrl() ?>/joguinhos">Joguinhos</a><span></span></div>
+          <div class="tit"><a href="<?php echo $site->retriveUrl() ?>/joguinhos"><?php $displays['destaque-principal-joguinhos'][0]->Section->getTitle() ?></a><span></span></div>
           <ul>
-            <?php $related = $displays['destaque-principal-joguinhos'][1]->Asset->retriveRelatedAssetsByRelationType('Preview'); ?>
+            <?php $related = $displays['destaque-principal-joguinhos'][1]->Asset->retriveRelatedAssetsByRelationType('Preview'); ?> 
             <?php if(count($related) > 0): ?>
             <li>
               <a href="<?php echo $displays['destaque-principal-joguinhos'][1]->retriveUrl() ?>" title="<?php echo $displays['destaque-principal-joguinhos'][1]->getTitle() ?>">
