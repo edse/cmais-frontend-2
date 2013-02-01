8fcc2a6a0eb2498898382b6ed4799c1f691cfe63
rafaelhcs <rhcsousa@gmail.com>
2013-02-01 17:11:53 -0200

    up

--- web/actions/cocorico/image.php
+++ web/actions/cocorico/image.php
@@ -21,7 +21,7 @@ if($_REQUEST["n"]!="")
 // Write the string
 //imagestring($im, $fontSize, $leftTextPos, $height-28, $text, $textColor);
 //imagestring($im, $fontSize, 18, 150, $text, $textColor);
-imagettftext($im, 100, 0, 340, 1175, $black, '/var/frontend/web/actions/cocorico/EmileeHandwriting.ttf', $text);
+imagettftext($im, 100, 0, 100, 340, $black, '/var/frontend/web/actions/cocorico/EmileeHandwriting.ttf', $text);
 // Output the image
 header('Content-type: image/png');
 imagepng($im);
