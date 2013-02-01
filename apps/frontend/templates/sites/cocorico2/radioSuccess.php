2907937c0efcf5f6ac86346292ac84ebc5e2f80a
renatareno <asdf@fpawks0142.local>
2013-02-01 17:24:32 -0200

    frase radio

--- apps/frontend/templates/sites/cocorico2/radioSuccess.php
+++ apps/frontend/templates/sites/cocorico2/radioSuccess.php
@@ -365,7 +365,8 @@
         </div>
       </div>
       <!-- player -->  
-      <img src="/portal/images/capaPrograma/cocorico/bg-radio.jpg" />      
+      <img src="/portal/images/capaPrograma/cocorico/bg-radio.jpg" />     
+      <p class="tit">Cante e dance com a turma do Cocoricó! Confira mais clipes aqui:</p> 
     </div>
     <!-- row clipes relacionados -->
     <div class="row-fluid relacionados">
