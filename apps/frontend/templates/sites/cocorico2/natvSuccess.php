dbb14dd4a650b69bd0d7b6d40ac574d3bd4587f3
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-04 17:09:13 -0200

    u

--- apps/frontend/templates/sites/cocorico2/natvSuccess.php
+++ apps/frontend/templates/sites/cocorico2/natvSuccess.php
@@ -50,7 +50,7 @@
    <?php foreach($displays['destaque'] as $k=>$d):?>
    	
    	<?php
-	    $horario = explode(";", $d->Asset->AssetContent->getHeadline());
+	    $horario = explode(";", $d->getHeadline());
 	  ?>
    	   	
     <ul>
