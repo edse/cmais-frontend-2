e328d2c7c77054e4925aff1e8ea9e363d52b52ba
doljak <jedoljak@gmail.com>
2013-02-04 15:33:17 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/tvcocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico2/tvcocoricoSuccess.php
@@ -76,20 +76,19 @@
         <?php endif; ?>
         <!-- enquete -->
         <?php
-        /*
-   		 $displays_home = array();
-   		 $blocks = Doctrine_Query::create()
-    		 ->select('b.*')
-    		 ->from('Block b, Section s') 
-      	 ->where('b.section_id = s.id')
-      	 ->andWhere('s.slug = ?', "home")//mudar para home quando for no ar
-        	 ->andWhere('b.slug = ?', 'enquete') 
-       	 ->andWhere('s.site_id = ?', $site->id)
-       	 ->execute();
-  
-    		if(count($blocks) > 0){
-      	 $displays_home['enquete'] = $blocks[0]->retriveDisplays();
-        }
+ 		 $displays_home = array();
+ 		 $blocks = Doctrine_Query::create()
+  		 ->select('b.*')
+  		 ->from('Block b, Section s') 
+    	 ->where('b.section_id = s.id')
+    	 ->andWhere('s.slug = ?', "home")//mudar para home quando for no ar
+      	 ->andWhere('b.slug = ?', 'enquete') 
+     	 ->andWhere('s.site_id = ?', $site->id)
+     	 ->execute();
+
+  		if(count($blocks) > 0){
+    	$displays_home['enquete'] = $blocks[0]->retriveDisplays();
+  }
 
         //pergunta bloco enquete - 1º destaque
         $q = $displays_home['enquete'][0]->Asset->AssetQuestion->getQuestion();
@@ -106,15 +105,11 @@
         $img_0 = "http://midia.cmais.com.br/assets/image/image-4-b/".$imgs[0]->AssetImage->file.".jpg";
         $imgs = $respostas[1]->Asset->retriveRelatedAssetsByAssetTypeId(2);
         $img_1 = "http://midia.cmais.com.br/assets/image/image-4-b/".$imgs[0]->AssetImage->file.".jpg";
-        */
-
-         
+    
         ?>
          <div class="enquete span12">
          	 
           <h3>enquete do dia</h3>
-          <?php
-          /*
           <p><?php echo $q;?></p>
           <!--Pergunta-->
           <form method="post" id="e<?php echo $respostas[0]->Asset->getId()?>" class="form-voto navbar-form pull-left span12" style="min-width:296px; ">
@@ -161,8 +156,6 @@
             <a href="<?php echo $site->retriveUrl();?>/enquetes" title="Ver enquetes anteriores">Ver enquetes anteriores</a>
           </form>
           <!--/Resposta-->
-          */
-          ?>
         </div>
         <!-- /enquete -->
         <!-- fale conosco cr-->
