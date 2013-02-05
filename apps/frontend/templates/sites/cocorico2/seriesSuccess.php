a1d13b7296170fa8f04d86a8306e417d326b4409
rafaelhcs <rhcsousa@gmail.com>
2013-02-05 12:44:42 -0200

    up

--- apps/frontend/templates/sites/cocorico2/seriesSuccess.php
+++ apps/frontend/templates/sites/cocorico2/seriesSuccess.php
@@ -28,7 +28,6 @@ $assets = $pager->getResults();
   <!-- breadcrumb-->
   <ul class="breadcrumb">
      <li><a href="<?php echo $site->retriveUrl() ?>">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li><a href="<?php echo $site->retriveUrl() ?>/tvcocorico">TV Cocoricó</a> <span class="divider">&rsaquo;</span></li>
      <li class="active"><?php echo $section->getTitle() ?></li>
   </ul>
   <!-- /breadcrumb-->
