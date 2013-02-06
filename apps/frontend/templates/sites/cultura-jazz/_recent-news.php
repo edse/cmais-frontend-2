cc5ed330dc58ffa5fbf823a9871a71834a8fe342
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-06 18:46:43 -0200

    u

--- apps/frontend/templates/sites/cultura-jazz/_recent-news.php
+++ apps/frontend/templates/sites/cultura-jazz/_recent-news.php
@@ -2,7 +2,7 @@
                 <?php if(count($displays) > 0): ?>
                 <ul class="conteudo">
                   <?php foreach($displays as $d): ?>
-                  <li><a class="titulos" href="<?php echo $d->retriveUrl() ?>"><?php echo $d->getTitle() ?></a>
+                  <li><a class="titulos" href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>"><?php echo $d->getTitle() ?></a>
                     <?php if($d->retriveImageUrlByImageUsage("image-3") != ""): ?>
                       <a href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>" class="img-90x54" style="width: auto">
                         <img src="<?php echo $d->retriveImageUrlByImageUsage("image-1-b") ?>" alt="<?php echo $d->getTitle() ?>" title="<?php echo $d->getTitle() ?>" style="width: auto" />
