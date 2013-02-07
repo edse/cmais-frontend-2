652eea8b0a9b06cbf559768d9cf9b29a50c39964
doljak <jedoljak@gmail.com>
2013-02-07 12:35:09 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
+++ apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
@@ -24,9 +24,9 @@
   <!-- /row-->
   <!-- breadcrumb--> 
   <ul class="breadcrumb">
-     <li><a href="<?php echo $site->retriveUrl(); ?>">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li><a href="<?php echo $site->retriveUrl(); ?>/<?php echo $section->Parent->getSlug() ?>">Em família</a> <span class="divider">&rsaquo;</span></li> 
-     <li><a href="<?php echo $site->retriveUrl(); ?>/<?php echo $section->getSlug() ?>">Agenda</a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="<?php echo $site->retriveUrl(); ?>"><?php echo $site->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle();?></a> <span class="divider">&rsaquo;</span></li>
      <li class="active"><?php echo $asset->getTitle() ?></li>
   </ul> 
   <!-- /breadcrumb-->
