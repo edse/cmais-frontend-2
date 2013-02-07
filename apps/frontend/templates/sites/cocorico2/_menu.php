986229455b78b86ee073d7c1fccb43d625bf7bf8
rafaelhcs <rhcsousa@gmail.com>
2013-02-07 14:13:38 -0200

    up

--- apps/frontend/templates/sites/cocorico2/_menu.php
+++ apps/frontend/templates/sites/cocorico2/_menu.php
@@ -1,11 +1,11 @@
 <div class="navbar-inner">
   <ul class="nav">
-    <li class="personagens"><a href="cocorico2/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
-    <li class="joguinhos"><a class="icon" href="cocorico2/joguinhos" title="Joguinhos"></a><a href="cocorico2/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
-    <li class="tvcoco"><a class="icon"  href="cocorico2/tvcocorico" title="TV Cocoricó"></a><a href="cocorico2/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
-    <li class="brincadeiras"><a class="icon"  href="cocorico2/paiol" title="Paiol"></a><a href="cocorico2/paiol" title="Paiol">Paiol</a><span></span></li>
+    <li class="personagens"><a href="<?php echo $site->retriveUrl() ?>/personagens" class="btn-tooltip" rel="tooltip" data-placement="bottom" data-original-title="ver todos"></a></li>
+    <li class="joguinhos"><a class="icon" href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos"></a><a href="<?php echo $site->retriveUrl() ?>/joguinhos" title="Joguinhos">Joguinhos</a><span></span></li>
+    <li class="tvcoco"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó"></a><a href="<?php echo $site->retriveUrl() ?>/tvcocorico" title="TV Cocoricó">TV Cocoricó</a><span></span></li>
+    <li class="brincadeiras"><a class="icon"  href="<?php echo $site->retriveUrl() ?>/paiol" title="Paiol"></a><a href="<?php echo $site->retriveUrl() ?>/paiol" title="Paiol">Paiol</a><span></span></li>
     
     <!--li class="diario"><a class="icon"  href="cocorico2/diario-do-julio" title="Diário do Júlio"></a><a href="cocorico2/diario-do-julio" title="Diário do Júlio">Diário do Júlio</a><span></span></li-->
-    <li class="familia"><a  href="cocorico2/em-familia" title="Em família">Em família</a></li>
+    <li class="familia"><a  href="<?php echo $site->retriveUrl() ?>/em-familia" title="Em família">Em família</a></li>
   </ul>
 </div> 
