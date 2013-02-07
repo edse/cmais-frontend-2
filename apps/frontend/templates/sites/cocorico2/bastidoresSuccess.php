8896dfef5577c31c5d1f6c4f8e6f321dcf40e30b
doljak <jedoljak@gmail.com>
2013-02-07 12:40:53 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
+++ apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
@@ -29,9 +29,9 @@ if(isset($pager)){
   
   <!-- breadcrumb-->
   <ul class="breadcrumb">
-     <li><a href="<?php echo $site->retriveUrl() ?>">Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li><a href="<?php echo $site->retriveUrl() ?>/tvcocorico">TV Cocoricó</a> <span class="divider">&rsaquo;</span></li>
-     <li class="active"><?php echo $section->getTitle() ?></li>
+     <li><a href="<?php echo $site->retriveUrl(); ?>"><?php echo $site->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
+     <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
+     <li class="active"><?php echo $section->getTitle(); ?> teste</li>
   </ul>
   <!-- /breadcrumb-->
   
