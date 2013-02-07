d9b286107819db95b61813f5b3042ac3c9e2ca50
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-07 19:22:22 -0200

    u

--- apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-lista-de-selecionadosSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-lista-de-selecionadosSuccess.php
@@ -31,10 +31,14 @@
                   <div class="texto">
 
                     <table>
+                      <thead>
                       <tr>
+                        
                         <th>Nome</th>
                         <th>Modalidade</th>
                       </tr>
+                      </thead>
+                      <tbody>
                         <tr>
                           <td><a href="http://cmais.com.br/cadastro-de-tutores-2013-local-de-prova?nome=Adalberto+Mesaque+Rodrigues" title="Adalberto Mesaque Rodrigues">Adalberto Mesaque Rodrigues</a></td>
                           <td>online</td>
@@ -8591,6 +8595,7 @@
                           <td><a href="http://cmais.com.br/cadastro-de-tutores-2013-local-de-prova?nome=Zuleini+Aparecida+Sabatin+do+Carmo" title="Zuleini Aparecida Sabatin do Carmo">Zuleini Aparecida Sabatin do Carmo</a></td>
                           <td>online</td>
                         </tr>
+                      </tbody>
                     </table>
                     
                   </div>
