2f839d3ca685cd5ac17141a6b98870dc1f52f27b
doljak <jedoljak@gmail.com>
2013-02-07 12:29:38 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/agendaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/agendaSuccess.php
@@ -34,7 +34,7 @@ if(isset($pager)){
   <ul class="breadcrumb">
      <li><a href="<?php echo $site->retriveUrl(); ?>"><?php echo $site->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
      <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
-     <li class="active"><?php echo $section->getTitle(); ?> teste</li>
+     <li class="active"><?php echo $section->getTitle(); ?></li>
   </ul>
   <!-- /breadcrumb-->
   <h2 class="tit-pagina">Agenda</h2>
