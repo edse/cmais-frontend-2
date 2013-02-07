6248883f79081b90603c7af29d2f4a0911e0b29d
renatareno <asdf@fpawks0142.local>
2013-02-07 14:57:19 -0200

    personagens

--- apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
+++ apps/frontend/templates/sites/cocorico2/bastidoresSuccess.php
@@ -20,7 +20,7 @@ if(isset($pager)){
       <?php include_partial_from_folder('sites/cocorico', 'global/menu', array('site'=>$site)) ?>
       <!--/menu principal-->
       <!--menu personagens -->
-      
+      <?php include_partial_from_folder('sites/cocorico', 'global/personagens', array('site'=>$site)) ?>
       <!--/menu personagens -->
       </div>
     </div>
