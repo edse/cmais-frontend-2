a3822b705bad04406128c054ba476c7f5e426dc4
doljak <jedoljak@gmail.com>
2013-02-07 12:36:36 -0200

    coco

--- apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
+++ apps/frontend/templates/sites/cocorico2/agendapostSuccess.php
@@ -27,7 +27,7 @@
      <li><a href="<?php echo $site->retriveUrl(); ?>"><?php echo $site->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
      <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
      <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle();?></a> <span class="divider">&rsaquo;</span></li>
-     <li class="active"><?php echo $asset->getTitle() ?></li>
+     <li class="active"><?php echo $asset->getTitle() ?> teste</li>
   </ul> 
   <!-- /breadcrumb-->
   <h2 class="tit-pagina">agenda</h2>
