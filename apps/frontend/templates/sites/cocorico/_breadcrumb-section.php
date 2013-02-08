6277472c2186072a633ea442534c4879579cc690
doljak <jedoljak@gmail.com>
2013-02-08 11:14:39 -0200

    commit

--- apps/frontend/templates/sites/cocorico/_breadcrumb-section.php
+++ apps/frontend/templates/sites/cocorico/_breadcrumb-section.php
@@ -5,7 +5,7 @@
    <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>  
    <?php else:?>  
    <li class="active"><?php echo $section->getTitle(); ?></li>
-   <?endif;?>
+   <?php endif;?>
    <?php if (isset($asset)):?>
    <li class="active"><?php echo $asset->getTitle() ?></li>
    <?php endif;?>
