688f33228905748a9436ac0c92a736269b1de9d2
doljak <jedoljak@gmail.com>
2013-02-08 11:16:28 -0200

    coco

--- apps/frontend/templates/sites/cocorico/_breadcrumb-section.php
+++ apps/frontend/templates/sites/cocorico/_breadcrumb-section.php
@@ -1,7 +1,7 @@
 <ul class="breadcrumb">
    <li><a href="<?php echo $site->retriveUrl(); ?>"><?php echo $site->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
-   <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
    <?php if (isset($asset)):?>
+   <li><a href="<?php echo $section->Parent->retriveUrl(); ?>"><?php echo $section->Parent->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>
    <li><a href="<?php echo $section->retriveUrl(); ?>"><?php echo $section->getTitle(); ?></a> <span class="divider">&rsaquo;</span></li>  
    <?php else:?>  
    <li class="active"><?php echo $section->getTitle(); ?></li>
