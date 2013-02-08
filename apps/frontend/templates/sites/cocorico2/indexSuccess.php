245f3f1c9c71e44ff3eb9e6ff70ee9da2e309f32
rafaelhcs <rhcsousa@gmail.com>
2013-02-08 18:06:54 -0200

    up

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -12,9 +12,9 @@
 <script>
   $(document).ready(function(){
     //alert("teste"+$(window).width()+"/"+$('body').width());
-    $('#myCarousel').carousel({
-    interval: 3000
-    })
+    //$('#myCarousel').carousel({
+    //interval: 3000
+    //})
   })
 </script>
 
