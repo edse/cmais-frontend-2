e454feb552d4b84d1f29c2d2b96089ab4f8808c5
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-08 13:57:09 -0200

    u

--- apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-segunda-etapaSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-segunda-etapaSuccess.php
@@ -1,12 +1,4 @@
 <?php if($_REQUEST['test']): ?>
-<?php
-if(isset($pager)){
-  if($pager->count() == 1){
-    header("Location: ".$pager->getCurrent()->retriveUrl());
-    die();
-  }  
-} 
-?>
 
     <link rel="stylesheet" href="/portal/css/tvcultura/sites/<?php echo $section->Site->getSlug() ?>.css" type="text/css" />
     <link rel="stylesheet" href="/portal/css/tvcultura/secoes/contato.css" type="text/css" />
