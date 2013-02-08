550f7b71e7b6d01a7c09df2e79dd864ae62617f7
doljak <jedoljak@gmail.com>
2013-02-08 14:47:49 -0200

    cocp

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -53,7 +53,7 @@
   <!-- /row-->
   
   <!-- breadcrumb-->
-  <?php include_partial_from_folder('sites/cocorico', 'global/breadcrumb-section', array('site'=>$site,'section'=>$section, 'asset'=>$asset)) ?> 
+  <?php include_partial_from_folder('sites/cocorico', 'global/breadcrumb-section', array('site'=>$site,'section'=>$section)) ?> 
   <!-- /breadcrumb-->
 
   <!--btn voltar-->
