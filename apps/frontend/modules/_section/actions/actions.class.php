7f01c44016f155be0dac2272de8fc48390d4b3f8
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-13 15:55:06 -0200

    u

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -68,13 +68,8 @@ class _sectionActions extends sfActions
       // current site
       $this->site = $this->section->Site;
       
-	    if($this->section->Site->getSlug() == "cocorico2") {
-          $this->setLayout('cocorico');
-          /*if(!$request->getParameter('test')){
-            header("Location: http://www3.tvcultura.com.br/cocorico");
-            die();
-          }*/
-      }   
+      if($this->section->Site->getSlug() == "cocorico2")
+        $this->setLayout('cocorico');   
     
       if(($this->site->getSlug() == "culturafm")&&($this->section->getSlug()=="controle-remoto")){
         $this->setLayout(false);
