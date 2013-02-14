209471d5430dac333de9e3b7f490075038ce1e14
renatareno <asdf@fpawks0142.local>
2013-02-14 08:42:07 -0200

    concurso

--- apps/frontend/templates/sites/cocorico2/concurso-culturalSuccess.php
+++ apps/frontend/templates/sites/cocorico2/concurso-culturalSuccess.php
@@ -26,96 +26,15 @@
     <a href="<?php echo $site->retriveUrl() ?>/convidados" title="Quem já passou por aqui?">Quem já passou por aqui?</a>
   </div>  
 
-  <div class="row-fluid">
-     <!-- PAGINACAO -->   
-     <?php if(isset($pager)): ?>
-     <?php if($pager->haveToPaginate()): ?>
-    <!--row-->
+ 
     
-    <div class="paginacao">
-      <a href="javascript: goToPage(<?php echo $pager->getNextPage() ?>);" class="anterior" title="Convidado anterior"><span></span>Convidado Anterior</a>
-      <a href="javascript: goToPage(<?php echo $pager->getPreviousPage() ?>);" class="proximo" title="Próximo Convidado">Próximo Convidado<span></span></a>
-    </div>
   
-  <!-- /row-->
   
-  <form id="page_form" action="" method="post">
-    <input type="hidden" name="return_url" value="<?php echo $url?>" />
-    <input type="hidden" name="page" id="page" value="" />
-    <input type="hidden" name="term" id="term" value="<?php echo $term ?>" />
-    <input type="hidden" name="filter" id="filter" value="<?php echo $filter ?>" />
-  </form>
-  
-  <script>
-     function goToPage(i){
-       $("#page").val(i);
-       $("#page_form").submit();
-     }
-  </script>
-  
-   <?php endif; ?>
-     <?php endif; ?>
-
-  </div> 
-    <!--row-->
-    <!-- /PAGINACAO -->
-    
-   <div class="row-fluid conteudo">
-   <h3><?php echo $asset->getTitle() ?></h3>
-   <span class="data"><?php echo $asset->AssetContent->getHeadlineShort() ?></span>
-   <a class="span6"><img src="<?php echo $asset->retriveImageUrlByImageUsage('original') ?>" alt="<?php echo $asset->getTitle() ?>" /></a>
-   <div class="span6">
-     <?php if ($asset->AssetContent->getHeadline()): ?>
-     <p class="frase"><span></span><?php echo $asset->AssetContent->getHeadline() ?> <span class="last"></span></p>
-     <?php endif; ?>
-     <p><?php echo html_entity_decode($asset->AssetContent->render()) ?></p>
-   </div>
-   
-  </div>
-  <!-- /row-->
-  <!--row-->
-  <div class="row-fluid conteudo convidado">
-    <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(6); ?>
-      <?php if (count($related) > 0): ?>
-        <p class="tit">Assista à participação na íntegra:</p>
-          <iframe width="940" height="529" src="http://www.youtube.com/embed/<?php echo $related[0]->AssetVideo->getYoutubeId() ?>" frameborder="0" allowfullscreen></iframe>
-       <?php endif; ?>
-  </div>
-  <!-- /row-->
+ 
 
  
-  <div class="row-fluid">
-      <!-- PAGINACAO -->  
-     <?php if(isset($pager)): ?>
-     <?php if($pager->haveToPaginate()): ?> 
-    <!--row-->
-    <div class="paginacao">
-      <a href="javascript: goToPage(<?php echo $pager->getNextPage() ?>);" class="anterior" title="Convidado anterior"><span></span>Convidado Anterior</a>
-      <a href="javascript: goToPage(<?php echo $pager->getPreviousPage() ?>);" class="proximo" title="Próximo Convidado">Próximo Convidado<span></span></a>
-    </div>
-  
-  <!-- /row-->
-  
-  <form id="page_form" action="" method="post">
-    <input type="hidden" name="return_url" value="<?php echo $url?>" />
-    <input type="hidden" name="page" id="page" value="" />
-    <input type="hidden" name="term" id="term" value="<?php echo $term ?>" />
-    <input type="hidden" name="filter" id="filter" value="<?php echo $filter ?>" />
-  </form>
-  
-  <script>
-     function goToPage(i){
-       $("#page").val(i);
-       $("#page_form").submit();
-     }
-  </script>
-  
-   <?php endif; ?>
-     <?php endif; ?>
 
-  </div>
-    <!--row-->
-    <!-- /PAGINACAO -->
+   
 
   <!-- rodapé-->
   <div class="row-fluid  border-top"></div>
