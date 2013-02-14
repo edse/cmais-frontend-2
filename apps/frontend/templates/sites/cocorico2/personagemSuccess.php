7a5390dadfc7443d894b51b646258ac5e5d3aa81
Emerson Estrella <emerson.estrella@gmail.com>
2013-02-14 19:34:06 -0200

    adsfasdf

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -1,7 +1,7 @@
  
 <?php if(isset($displays["conteudos"][0])): ?>
   <?php $related_video = $displays['conteudos'][0]->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
-  <?php echo var_dump($related_video);?>
+  <?php echo var_dump($displays['conteudos'][0]->Asset->getId());?>
   <?php echo "videos: ".count($related_video);?>
   <?php if(count($related_video) > 0): ?>   
     <?php echo $related_video[0]->AssetVideo->getYoutubeId()?>
