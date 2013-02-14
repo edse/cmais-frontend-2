0c17a73b8b036b2c3aea3b305e565c0bc8a3dcc7
renatareno <asdf@fpawks0142.local>
2013-02-14 10:19:29 -0200

    concurso

--- apps/frontend/templates/sites/cocorico2/concurso-culturalSuccess.php
+++ apps/frontend/templates/sites/cocorico2/concurso-culturalSuccess.php
@@ -45,7 +45,45 @@
     </div>
   </div>
   <!--/row-->
- 
+  <!-- paginacao -->
+  <div class="pagination pagination-centered">
+    <ul>
+      <li class="anterior"><a title="Anterior" href="javascript: goToPage(1);"></a></li>
+      <li class="active"><a href="javascript: goToPage(1);">1</a></li>
+      <li><a href="javascript: goToPage(2);">2</a></li>
+      <li><a href="javascript: goToPage(3);">3</a></li>
+      <li><a href="javascript: goToPage(4);">4</a></li>
+      <li title="Próximo" class="proximo"><a href="javascript: goToPage(2);"></a></li>
+    </ul>
+  </div>
+  <!-- paginacao -->
+  <!--row-->
+  <div class="row-fluid conteudo destaques ytb">
+    <ul id="convidados">
+      <?php foreach($pager->getResults() as $d): ?>
+        <li class="span4">
+          <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
+            <img class="span12" src="http://img.youtube.com/vi/<?php echo $d->AssetVideo->getYoutubeId() ?>/0.jpg" alt="<?php echo $d->getTitle() ?>" />
+            <p><?php $tam=33; $str=$d->getTitle(); mb_internal_encoding("UTF-8"); if(strlen($str) <= $tam) echo $str; else echo mb_substr($str, 0, $tam-1)."&hellip;" ?></p>
+          </a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <!-- /row-->
+  <!-- paginacao -->
+  <div class="pagination pagination-centered">
+    <ul>
+      <li class="anterior"><a title="Anterior" href="javascript: goToPage(1);"></a></li>
+      <li class="active"><a href="javascript: goToPage(1);">1</a></li>
+      <li><a href="javascript: goToPage(2);">2</a></li>
+      <li><a href="javascript: goToPage(3);">3</a></li>
+      <li><a href="javascript: goToPage(4);">4</a></li>
+      <li title="Próximo" class="proximo"><a href="javascript: goToPage(2);"></a></li>
+    </ul>
+  </div>
+  <!-- paginacao -->
+  
   
   <!-- rodapé-->
   <div class="row-fluid  border-top"></div>
