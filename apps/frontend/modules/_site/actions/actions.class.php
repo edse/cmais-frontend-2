380cf2d1635d6c16b8926657cb37a7860a2e2f48
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-14 13:15:06 -0200

    u

--- apps/frontend/modules/_site/actions/actions.class.php
+++ apps/frontend/modules/_site/actions/actions.class.php
@@ -36,7 +36,7 @@ class _siteActions extends sfActions
         }
       }
       elseif($this->site->slug == "cocorico2"){
-        if (preg_match("/^172\.20\.(\d+)\.(\d+)/", $_SERVER['REMOTE_ADDR']) == 1) {
+        if (preg_match("/^172\.20\.(\d+)\.(\d+)/", $_SERVER['REMOTE_ADDR']) == 0) {
           header("location: http://www3.tvcultura.com.br/cocorico");
           die();
         }
