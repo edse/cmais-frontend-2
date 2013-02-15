4c0e32eccadede9d8b5cd7dadd89a0d539e1c4e6
rafaelhcs <rhcsousa@gmail.com>
2013-02-15 15:26:28 -0200

    up

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -103,7 +103,7 @@
         
         <?php if($displays['conteudos'][1]->Asset->AssetType->getSlug() == "video"): ?>
           <h3><a href="<?php echo $site->retriveUrl().$se ?>"><?php echo $se ?></a></h3>
-          <a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $displays["conteudos"][1]->Asset->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][1]->Asset->getTitle() ?>"></a>
+          <a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $displays["conteudos"][1]->Asset->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][1]->getTitle() ?>"></a>
           <a href="<?php echo $site->retriveUrl().$se ?>"><?php echo $displays["conteudos"][1]->Asset->getTitle() ?></a>
           <a href="<?php echo $site->retriveUrl().$se ?>" class="ico-mais"></a>
 
@@ -111,14 +111,14 @@
           <?php $related_image = $displays['conteudos'][1]->Asset->retriveRelatedAssetsByAssetTypeId(2); ?>
           <?php $related_video = $displays['conteudos'][1]->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
           <?php if(count($related_image) > 0): ?> 
-            <h3><a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><?php echo $displays["conteudos"][1]->Asset->getTitle() ?></a></h3>
+            <h3><a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><?php echo $displays["conteudos"][1]->getTitle() ?></a></h3>
             <a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><img src="<?php echo $displays["conteudos"][1]->retriveImageUrlByImageUsage('image-3-b') ?>" alt="<?php echo $displays["conteudos"][1]->getTitle() ?>"></a>
             <a href="<?php echo $site->retriveUrl().$se ?>"><?php echo $se ?></a>
             <a href="<?php echo $site->retriveUrl().$se ?>" class="ico-mais"></a>
        
           <?php elseif(count($related_video) > 0): ?> 
             <h3><a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><?php echo $displays["conteudos"][1]->getTitle() ?></a></h3>
-            <a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $related_video[0]->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][1]->Asset->getTitle() ?>"></a>
+            <a href="<?php echo $displays["conteudos"][1]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $related_video[0]->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][1]->getTitle() ?>"></a>
             <a href="<?php echo $site->retriveUrl().$se ?>"><?php echo $se ?></a>
             <a href="<?php echo $site->retriveUrl().$se ?>" class="ico-mais"></a>
  
