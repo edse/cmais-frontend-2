329b7e748f1312260f07945584e952a6b9b7de02
rafaelhcs <rhcsousa@gmail.com>
2013-02-15 19:14:59 -0200

    up

--- apps/frontend/templates/sites/cocorico2/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico2/indexSuccess.php
@@ -180,9 +180,10 @@
             <div class="span6 box-destaque">
               <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $se ?></a></h3>
               <a href="<?php echo $displays["destaque-2"][0]->Asset->retriveUrl() ?>"><img src="<?php echo $displays["destaque-2"][0]->retriveImageUrlByImageUsage('image-3-b') ?>" alt="<?php echo $displays["destaque-2"][0]->getTitle() ?>"></a>
-              <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $displays["destaque-2"][0]->getTitle() ?></a>
+              <a href="<?php echo $displays["destaque-3"][0]->Asset->retriveUrl() ?>"><?php echo $displays["destaque-2"][0]->getTitle() ?></a>
              </div>
             <!-- box-destaque -->
+            </div>
           <?php endif; ?>
         <?php endif; ?>
         
@@ -205,9 +206,10 @@
             <div class="span6 box-destaque">
               <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $se ?></a></h3>
               <a href="<?php echo $displays["destaque-3"][0]->Asset->retriveUrl() ?>"><img src="<?php echo $displays["destaque-3"][0]->retriveImageUrlByImageUsage('image-3-b') ?>" alt="<?php echo $displays["destaque-3"][0]->getTitle() ?>"></a>
-              <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $displays["destaque-3"][0]->getTitle() ?></a>
+              <a href="<?php echo $displays["destaque-3"][0]->Asset->retriveUrl() ?>"><?php echo $displays["destaque-3"][0]->getTitle() ?></a>
              </div>
             <!-- box-destaque -->
+            </div>
           <?php endif; ?>
         <?php endif; ?>
       </div>
