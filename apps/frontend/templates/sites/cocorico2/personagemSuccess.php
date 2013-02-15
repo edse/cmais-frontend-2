1c3f70918e385b01bbc61db5d9c8a549173e869b
renatareno <asdf@fpawks0142.local>
2013-02-15 11:15:38 -0200

    box personagem

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -141,6 +141,26 @@
               </a>
             <?php endif; ?>
          <?php endif; ?>
+         
+        
+        <!-- box-destaque -->
+        <div class="span6 box-destaque">
+          <h3><a href="#">Nome da secao</a></h3>
+          <a href="#"><img src="http://midia.cmais.com.br/assets/image/image-6-b/6e0eb40f1da6a84a757b5545ac86e871d0da9ff5.jpg" alt="Convidado"></a>
+          <a href="#">titulo do asset</a>
+          <a href="#" class="ico-mais"></a>
+        </div>
+        <!-- box-destaque -->
+        
+        <!-- box-destaque -->
+        <div class="span6 box-destaque joguinhos">
+          <h3><a href="#">Nome da secao</a></h3>
+          <a href="#"><img src="http://midia.cmais.com.br/assets/image/image-6-b/6e0eb40f1da6a84a757b5545ac86e871d0da9ff5.jpg" alt="Convidado"></a>
+          <a href="#">titulo do asset</a>
+          <a href="#" class="ico-mais"></a>
+        </div>
+        <!-- box-destaque -->
+
 
 
      </div>
