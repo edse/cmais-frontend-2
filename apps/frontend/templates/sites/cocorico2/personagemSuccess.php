a122afef0749db9457b44385fb7ac3deda349c99
rafaelhcs <rhcsousa@gmail.com>
2013-02-18 14:20:56 -0300

    up

--- apps/frontend/templates/sites/cocorico2/personagemSuccess.php
+++ apps/frontend/templates/sites/cocorico2/personagemSuccess.php
@@ -68,14 +68,16 @@
         <?php $se_link = $displays["conteudos"][0]->Asset->Sections[0]->getSlug(); ?>
         <!-- box-destaque-0 -->
       
-        <div class="span6 box-destaque"> 
+        
         <?php if($displays['conteudos'][0]->Asset->AssetType->getSlug() == "video"): ?>
+          <div class="span6 box-destaque ytb"> 
           <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $se ?></a></h3>
           <a href="<?php echo $displays["conteudos"][0]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $displays["conteudos"][0]->Asset->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][0]->getTitle() ?>"></a>
           <a href="<?php echo $displays["conteudos"][0]->Asset->retriveUrl() ?>"><?php echo $displays["conteudos"][0]->getTitle() ?></a>
           <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="ico-mais"></a>
 
         <?php elseif($displays['conteudos'][0]->Asset->AssetType->getSlug() == "content"): ?>
+          <div class="span6 box-destaque"> 
           <?php $related_image = $displays['conteudos'][0]->Asset->retriveRelatedAssetsByAssetTypeId(2); ?>
           <?php $related_video = $displays['conteudos'][0]->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
           <?php if(count($related_image) > 0): ?> 
@@ -85,6 +87,7 @@
             <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="ico-mais"></a>
        
           <?php elseif(count($related_video) > 0): ?> 
+            <div class="span6 box-destaque"> 
             <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>>"><?php echo $se ?></a></h3>
             <a href="<?php echo $displays["conteudos"][0]->Asset->retriveUrl() ?>"><img src="http://img.youtube.com/vi/<?php echo $related_video[0]->AssetVideo->getYoutubeId()?>/0.jpg" alt="<?php echo $displays["conteudos"][0]->getTitle() ?>"></a>
             <a href="<?php echo $displays["conteudos"][0]->Asset->retriveUrl() ?>"><?php echo $displays["conteudos"][0]->getTitle() ?></a>
