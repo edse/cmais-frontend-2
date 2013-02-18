ed03433d54e06114f5066e93f636751063848ede
rafaelhcs <rhcsousa@gmail.com>
2013-02-18 18:39:28 -0300

    up

--- apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
@@ -135,7 +135,7 @@
       ->andWhere("(a.date_start IS NULL OR a.date_start <= CURRENT_TIMESTAMP)")
       ->groupBy('sa.asset_id')
       ->orderBy('a.id desc')
-      ->limit(24)
+      ->limit(6)
       ->execute();
   ?>
   <?php if (count($assets) > 0): ?>
