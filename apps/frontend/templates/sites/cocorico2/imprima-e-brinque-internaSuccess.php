1ba82204b2bfdfc20c0e844d1d46d4efd2d1dd99
rafaelhcs <rhcsousa@gmail.com>
2013-02-19 20:10:29 -0300

    up

--- apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/imprima-e-brinque-internaSuccess.php
@@ -97,7 +97,7 @@
               
       <?php if(count($related_preview)>0): ?>
        
-       <a href="javascript:printDiv('div1')" class="print grd" 
+       <a href="javascript:printDiv('div0')" class="print grd" 
        datasrc="<?php echo $related_download[0]->retriveImageUrlByImageUsage("original") ?>"
        data-original-title="imprimir">
        <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" 
