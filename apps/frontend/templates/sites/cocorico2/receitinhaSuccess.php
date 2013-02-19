8f2c526e68e0969e5503947fd1eabba14c6b9bca
rafaelhcs <rhcsousa@gmail.com>
2013-02-19 18:37:44 -0300

    up

--- apps/frontend/templates/sites/cocorico2/receitinhaSuccess.php
+++ apps/frontend/templates/sites/cocorico2/receitinhaSuccess.php
@@ -29,7 +29,7 @@
   <!-- /breadcrumb-->
   
   <!--btn voltar-->
-  <a href="<?php echo $site->retriveUrl() ?>/receitinhas" class="voltar">voltar<span class="divisao"></span></a>
+  <a href="javascript:window.history.go(-1)" class="voltar">voltar<span class="divisao"></span></a>
   <!-- /btn voltar-->
   
   <!-- titulo da pagina -->
