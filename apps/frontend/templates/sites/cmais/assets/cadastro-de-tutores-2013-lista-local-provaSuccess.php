546cda6d09106480f5d81a99a821f78fb2ac24e9
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-19 19:53:49 -0300

    u

--- apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-lista-local-provaSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/cadastro-de-tutores-2013-lista-local-provaSuccess.php
@@ -37,7 +37,7 @@
                   <!--p>Clique em seu nome para escolher o local da prova.</p-->
                   <!--p>Inscrições encerradas.</p-->
                   <div class="texto" style="margin-top:30px">
-                    <h2>Araçatuba</h2>
+                    <h5>Araçatuba</h5>
                     <p><strong>Local: </strong>UNIP (UNIVERSIDADE PAULISTA)<br />Avenida Baguaçu, 1939, Jardim Alvorada</p> 
                     <table>
                       <thead>
