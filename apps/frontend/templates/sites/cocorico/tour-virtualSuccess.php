b414e6549227a7bc61a0e53c08b7efdf93d6d238
rafaelhcs <rhcsousa@gmail.com>
2013-02-20 12:28:48 -0300

    up

--- apps/frontend/templates/sites/cocorico/tour-virtualSuccess.php
+++ apps/frontend/templates/sites/cocorico/tour-virtualSuccess.php
@@ -70,8 +70,8 @@ if(!isset($asset))
             <?php $tam=38; $str=$displays['destaques'][1]->getDescription(); mb_internal_encoding("UTF-8"); if(strlen($str) <= $tam) echo $str; else echo mb_substr($str, 0, $tam-1)."&hellip;" ?>
           </p>       
         </a>
-        <?php $se = $displays["destaques"][2]->Asset->Sections[0]->getTitle(); ?>
-        <?php $se_link = $displays["destaques"][2]->Asset->Sections[0]->getSlug(); ?>
+        <?php $se = $displays["destaques"][1]->Asset->Sections[0]->getTitle(); ?>
+        <?php $se_link = $displays["destaques"][1]->Asset->Sections[0]->getSlug(); ?>
         <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="btn-ico-mais" title="<?php echo $se ?>">
           <i class="ico-mais"></i>
         </a>
