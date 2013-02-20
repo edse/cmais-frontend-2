d7b139a34eba79e60d6f2d157b0a684030d258b6
rafaelhcs <rhcsousa@gmail.com>
2013-02-20 12:27:14 -0300

    up

--- apps/frontend/templates/sites/cocorico/tour-virtualSuccess.php
+++ apps/frontend/templates/sites/cocorico/tour-virtualSuccess.php
@@ -70,10 +70,11 @@ if(!isset($asset))
             <?php $tam=38; $str=$displays['destaques'][1]->getDescription(); mb_internal_encoding("UTF-8"); if(strlen($str) <= $tam) echo $str; else echo mb_substr($str, 0, $tam-1)."&hellip;" ?>
           </p>       
         </a>
-        <?php $se = $displays["destaques"][1]->Asset->Sections[0]->getTitle(); ?>
-        <?php $se_link = $displays["destaques"][1]->Asset->Sections[0]->getSlug(); ?>
-        <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="btn-ico-mais" title="<?php echo $se_link ?>"><i class="ico-mais"></i></a>
-      </div> 
+        <?php $se = $displays["destaques"][2]->Asset->Sections[0]->getTitle(); ?>
+        <?php $se_link = $displays["destaques"][2]->Asset->Sections[0]->getSlug(); ?>
+        <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="btn-ico-mais" title="<?php echo $se ?>">
+          <i class="ico-mais"></i>
+        </a>
       </div> 
       <!-- /clipe -->
        
@@ -88,7 +89,10 @@ if(!isset($asset))
           </p>
         </a> 
         <?php $se = $displays["destaques"][2]->Asset->Sections[0]->getTitle(); ?>
-        <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="btn-ico-mais" title="<?php echo $se_link ?>"><i class="ico-mais"></i></a>
+        <?php $se_link = $displays["destaques"][2]->Asset->Sections[0]->getSlug(); ?>
+        <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>" class="btn-ico-mais" title="<?php echo $se ?>">
+          <i class="ico-mais"></i>
+        </a>
       </div> 
       <!-- /clipe -->
        
