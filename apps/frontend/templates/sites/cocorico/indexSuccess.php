59597bd5baab2674f15f0543c2c6977a399a1a57
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-20 17:29:56 -0300

    u

--- apps/frontend/templates/sites/cocorico/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico/indexSuccess.php
@@ -54,7 +54,7 @@
   })
 </script>
 <!-- Modal -->
-<div id="modal-video" class="modal" style="top:40%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
+<div id="modal-video" class="modal hide" style="top:40%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
   <div class="modal-header">
     <button type="button" id="fechar" class="close" data-dismiss="modal" aria-hidden="true" style="margin-bottom: 20px;">
       Fechar
