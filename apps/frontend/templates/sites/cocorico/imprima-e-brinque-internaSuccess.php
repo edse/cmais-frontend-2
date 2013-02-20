56834e7308fac500b7347b57d813a006cffd5e65
rafaelhcs <rhcsousa@gmail.com>
2013-02-20 13:33:30 -0300

    up

--- apps/frontend/templates/sites/cocorico/imprima-e-brinque-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico/imprima-e-brinque-internaSuccess.php
@@ -98,12 +98,12 @@
               
       <?php if(count($related_preview)>0): ?>
        
-       <a href="javascript:printDiv('div1')" class="print grd" 
-       datasrc="<?php echo $related_download[0]->retriveImageUrlByImageUsage("original") ?>"
-       data-original-title="imprimir">
-       <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" 
-       alt="Imprimir" /><span></span></a>
+       <a href="javascript:printDiv('div1')" class="print grd" datasrc="<?php echo $related_download[0]->retriveImageUrlByImageUsage("original") ?>" data-original-title="imprimir"> 
+         <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("original") ?>" alt="Imprimir" /><span></span>
+       </a>
+       
        <ul class="imprimir"> 
+         
       <?php if(count($related_preview)>1): ?>
       <?php for($i=1; $i < count($related_preview); $i++): ?>  
         
