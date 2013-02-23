f900112b0473cb5f2f6efa4d5be8d53c5a4a427d
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-22 21:09:00 -0300

    u

--- apps/frontend/templates/sites/seminario2013/liveSuccess.php
+++ apps/frontend/templates/sites/seminario2013/liveSuccess.php
@@ -1,14 +1,11 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/defaultPrograma.css" type="text/css" />
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/<?php echo $section->Site->getSlug() ?>.css" type="text/css" />
 <script type="text/javascript" src="http://cmais.com.br/portal/js/mediaplayer/swfobject.js"></script>
-<?php use_helper('I18N', 'Date')
-?>
-<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))
-?>
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
 <div class="bg-chamada">
-  <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"]))
-  ?>
+  <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
 </div>
 <!-- CAPA SITE -->
 <div id="capa-site">
@@ -25,16 +22,13 @@
     </div>
     <!-- box-topo -->
     <div class="box-topo grid3">
-      <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections))
-      ?>
+      <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
 
-      <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))):
-      ?>
+      <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
       <div class="navegacao txt-10">
         <a href="../" title="Home">Home</a>
         <span>&gt;</span>
-        <a href="<?php echo $section->retriveUrl()?>" title="<?php echo $section->getTitle()?>"><?php echo $section->getTitle()
-        ?></a>
+        <a href="<?php echo $section->retriveUrl()?>" title="<?php echo $section->getTitle()?>"><?php echo $section->getTitle() ?></a>
       </div>
       <?php endif;?>
     </div>
@@ -43,8 +37,7 @@
   <!-- /BARRA SITE -->
   <!-- MIOLO -->
   <div id="miolo">
-    <?php include_partial_from_folder('blocks','global/shortcuts')
-    ?>
+    <?php include_partial_from_folder('blocks','global/shortcuts') ?>
 
     <!-- CONTEUDO PAGINA -->
     <div id="conteudo-pagina">
@@ -55,23 +48,22 @@
         
           
           
-           <script>
-      $(function(){
-        $('.btn-port').click(function(){
-          $('#video-port').show();
-          $('#video-esp').hide();
-          $(this).addClass('ativo');
-          $('.btn-esp').removeClass('ativo');
-        });
-        $('.btn-esp').click(function(){
-          $('#video-esp').show();
-          $('#video-port').hide();
-          $(this).addClass('ativo');
-          $('.btn-port').removeClass('ativo');
-        });
-        
-      });
-    </script>
+          <script>
+            $(function(){
+              $('.btn-port').click(function(){
+                $('#video-port').show();
+                $('#video-esp').hide();
+                $(this).addClass('ativo');
+                $('.btn-esp').removeClass('ativo');
+              });
+              $('.btn-esp').click(function(){
+                $('#video-esp').show();
+                $('#video-port').hide();
+                $(this).addClass('ativo');
+                $('.btn-port').removeClass('ativo');
+              });
+            });
+          </script>
     
           <!-- NOTICIA INTERNA -->
           <div class="box-interna ">
@@ -118,10 +110,10 @@
               url: '/ajax/scheduleddisplays'
             });
           }
-            
+            /*
           $(window).load(function(){
             var t=setInterval("timer()",5000); // a cada 5 minutos
-          }); 
+          }); */
         </script>
         <!-- DIREITA -->
         <div id="direita" class="grid1">
@@ -140,14 +132,11 @@
         <!-- /DIREITA -->
       </div>
       <!-- /CAPA -->
-      <?php if (isset($displays["rodape-interno"])):
-      ?>
+      <?php if (isset($displays["rodape-interno"])): ?>
       <!--APOIO-->
-      <?php include_partial_from_folder('blocks','global/support', array('displays' => $displays["rodape-interno"]))
-      ?>
+      <?php include_partial_from_folder('blocks','global/support', array('displays' => $displays["rodape-interno"])) ?>
       <!--/APOIO-->
-      <?php endif;
-      ?>
+      <?php endif; ?>
     </div>
     <!-- /CONTEUDO PAGINA -->
   </div>
