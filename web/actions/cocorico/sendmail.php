f2e8817ae696d0c260ef564f9a21f7138065af8b
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-02-26 18:10:15 -0300

    tvcocorico sendmail

--- web/actions/cocorico/sendmail.php
+++ web/actions/cocorico/sendmail.php
@@ -3,6 +3,56 @@
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if($_REQUEST['test'] == "1") {
     
+    // email main info 
+    //$to = "maiscriancatvcultura@gmail.com, cristovamruizjr@gmail.com";
+    $to = "cristovamruizjr@gmail.com";
+    $email = strip_tags($_REQUEST['email']);
+    $name = strip_tags($_REQUEST['nome']);
+    $subject = '[Cocoricó][TV Cocoricó] '.$name.' <'.$email.'>';
+
+    // file and mailpart pre-definitions
+    $file_name = basename($_FILES['datafile']['name']);
+    $data = file_get_contents($_FILES['datafile']['tmp_name']); 
+    $file_contents = chunk_split(base64_encode($data));
+    $bound_text = md5(time());
+    $bound = "--{$bound_text}"; 
+    $bound_last = "--{$bound_text}--";
+      
+    // header info                            
+    $header = "From: " . $name . " <" . $email . ">\r\n";
+    $header .= "Return-Path: " . $name . " <" . $email . ">\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: multipart/mixed; boundary=\"{$bound}\"\r\n";
+    $header .= "X-Priority: 3\r\n";
+    
+    // body text & html
+    $body = "Content-Type: text/html; charset=UTF-8\r\n";
+    $body .= "Content-Transfer-Encoding: 7bit\r\n";
+    $body .= "Formulário Preenchido em " . date("d/m/Y") . " as " . date("H:i:s") . ", seguem abaixo os dados:<br><br>";
+    while(list($field, $value) = each($_REQUEST)) {
+      if(!in_array(ucwords($field), array('Form_action', 'X', 'Y', 'Enviar', 'Undefinedform_action')))
+        $body .= "<b>" . ucwords($field) . ":</b> " . strip_tags($value) . "<br>";
+    }
+    $body .= "{$bound}";
+    
+    // image/jpeg attachment 
+    $body .= "Content-Type: image/jpeg; name=\"{$file_name}\"\r\n";
+    $body .= "Content-Transfer-Encoding: base64\r\n";
+    $body .= "Content-Disposition: attachment; filename=\"{$file_name}\"\r\n";
+    $body .= "{$file_contents}\r\n";
+    $body .= "{$bound_last}";
+    
+    // php mail function
+    if(mail($to, $subject, $body, $header)){
+      header("Location: http://tvcultura.cmais.com.br/cocorico/tvcocorico?success=1");
+      die();
+    }
+    else {
+      header("Location: http://tvcultura.cmais.com.br/cocorico/tvcocorico?error=1");
+      die();
+    }
+    
+    /*
     //$to = "maiscriancatvcultura@gmail.com, cristovamruizjr@gmail.com";
     $to = "cristovamruizjr@gmail.com";
     $email = strip_tags($_REQUEST['email']);
@@ -39,7 +89,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
     $body = stripslashes(nl2br($body));
     
-
+    
     
     if(mail($to, $subject, $body, implode("\r\n", $headers))){
       header("Location: http://tvcultura.cmais.com.br/cocorico/tvcocorico?success=1");
@@ -49,6 +99,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
       header("Location: http://tvcultura.cmais.com.br/cocorico/tvcocorico?error=1");
       die();
     }
+     * 
+     */
   }
   else {
     header('Location: http://tvcultura.cmais.com.br/cocorico/tvcocorico?error=1');
