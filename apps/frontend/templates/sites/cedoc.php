644eb3fca8113792729a2ef8abfca578fbb8c80f
rafaelhcs <rhcsousa@gmail.com>
2013-02-28 17:57:46 -0300

    up

---
+++ apps/frontend/templates/sites/cedoc.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CEDOC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="/portal/js/bootstrap/css/bootstrap.css" rel="stylesheet"-->
+    <link href="/portal/js/bootstrap/css/bootstrap-responsive.css" rel="stylesheet"-->
+    <link href="/portal/css/tvcultura/sites/cedoc.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <!--
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+    -->
+  </head>
+
+  <body>
+
+    <!--div class="navbar navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Fundação Padre Anchieta</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Quem Somos</a></li>
+              <li><a href="#">Acervo</a></li>
+              <li><a href="#">Central de Relacionamento</a></li>
+              <li><a href="#">Licitações</a></li>
+              <li><a href="#">Trabalhe Conosco</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Emissoras <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portais <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Sic</a></li>
+            </ul>
+            <!--form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="btn">Sign in</button>
+            </form-->
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div-->
+    
+     <?php echo $sf_content ?>
+    
+    <div id="footer">
+      <div class="container">
+        <p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+      </div>
+    </div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/transition.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/alert.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/modal.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/dropdown.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/scrollspy.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/tab.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/tooltip.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/popover.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/button.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/collapse.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/carousel.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/typeahead.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/transition.js"></script>
+    <script type="text/javascript" src="/portal/js/bootstrap/transition.js"></script>
+    
+   </body>
+</html>
