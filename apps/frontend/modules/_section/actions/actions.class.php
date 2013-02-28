9248b96d6e6c7533079739fde6c2f3e534cfe36d
rafaelhcs <rhcsousa@gmail.com>
2013-02-28 13:46:44 -0300

    up

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -77,7 +77,17 @@ class _sectionActions extends sfActions
         }
         */
       }
-    
+      
+      if(in_array($this->section->Site->getSlug(), array("belezadigital"))) {
+        $this->setLayout('belezadigital');   
+        /*
+        if (preg_match("/^172\.20\.(\d+)\.(\d+)/", $_SERVER['REMOTE_ADDR']) == 0) {
+          header("location: http://www3.tvcultura.com.br/cocorico");
+          die();
+        }
+        */
+      }
+      
       if(($this->site->getSlug() == "culturafm")&&($this->section->getSlug()=="controle-remoto")){
         $this->setLayout(false);
       }elseif($this->site->getSlug() == "prontoatendimento"){
