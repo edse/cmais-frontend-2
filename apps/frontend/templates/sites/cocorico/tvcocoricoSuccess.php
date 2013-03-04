b5e7f76c257db675dd3f330f1ebac1ef022711e8
rafaelhcs <rhcsousa@gmail.com>
2013-03-04 17:02:19 -0300

    up

--- apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
@@ -150,29 +150,12 @@ $(function(){
       <!-- destaque-home-simples -->
       <div class="destaque-home-tv span9" id="destaqueForm">
         
-            <?php if(isset($displays['destaque-principal'])): ?>
+          
+        <?php if(isset($displays['destaque-principal'])): ?>
           <?php if(count($displays['destaque-principal']) > 0): ?>
-            <h2>Concurso Cultural</h2>
-            <img class="promocao" src="/portal/images/capaPrograma/cocorico/destaque-concurso-encerrado.jpg" />
-            <?php
-            /*
-            <div class="destaque span12" style="position:relative;">
-              <span></span>
-              <!--a href="<?php echo $site->retriveUrl()?>/concurso-cultural" class="btn-destaque" title="Participe!">Participe!</a-->
-              <a href="javascript:;" class="btn-destaque btn-form" title="Participe">Participar</a> 
-              <span class="last"></span>
-            </div>
-             */
-            ?>
-          <?php endif; ?>
-        <?php endif; ?>
-        
-        
-        <?php /*if(isset($displays['destaque-seu-video'])): ?>
-          <?php if(count($displays['destaque-seu-video']) > 0): ?>
-            <h2><?php echo $displays['destaque-seu-video'][0]->getTitle(); ?></h2>
+            <h2><?php echo $displays['destaque-principal'][0]->getTitle(); ?></h2>
             
-            <iframe width="460" height="280" src="http://www.youtube.com/embed/<?php echo $displays['destaque-seu-video'][0]->Asset->AssetVideo->getYoutubeId(); ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="460" height="280" src="http://www.youtube.com/embed/<?php echo $displays['destaque-principal'][0]->Asset->AssetVideo->getYoutubeId(); ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
             
             <div class="destaque span12">
               <span></span>
@@ -180,7 +163,7 @@ $(function(){
               <span class="last"></span>
             </div>
           <?php endif; ?>
-        <?php endif;*/ ?>
+        <?php endif; ?>
         
       </div>
       <!-- /destaque-home-simples -->
