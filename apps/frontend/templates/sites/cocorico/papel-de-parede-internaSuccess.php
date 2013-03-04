a878a217ebdcdb12b52b1bbbc09d598668234c0b
renatareno <asdf@fpawks0142.local>
2013-03-04 18:50:00 -0300

    span7 coco

--- apps/frontend/templates/sites/cocorico/papel-de-parede-internaSuccess.php
+++ apps/frontend/templates/sites/cocorico/papel-de-parede-internaSuccess.php
@@ -36,7 +36,7 @@
   <!-- /btn voltar-->
   
   <!-- titulo da pagina -->
-  <div class="tit-pagina span7">
+  <div class="tit-pagina">
     <h2><?php $tam=32; $str=$asset->getDescription(); mb_internal_encoding("UTF-8"); if(strlen($str) <= $tam) echo $str; else echo mb_substr($str, 0, $tam-1)."&hellip;" ?></h2>
     <span></span>
     <!-- RANKING -->
