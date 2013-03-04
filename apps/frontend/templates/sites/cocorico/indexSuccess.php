3e33b3672fd7d20bfeb6b7e9bbe6c221e1b7710d
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-03-04 10:04:10 -0300

    u

--- apps/frontend/templates/sites/cocorico/indexSuccess.php
+++ apps/frontend/templates/sites/cocorico/indexSuccess.php
@@ -225,7 +225,9 @@
            
            <!-- box-destaque -->
             <div class="span6 box-destaque">
-              <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $se ?></a></h3>
+              
+              <?php /* <h3><a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $se ?></a></h3> */ ?>
+              <h3><a href="/cocorico/pedalando-com-o-julio">Pedalando com o Júlio</a></h3>
               <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><img src="<?php echo $displays["destaque-2"][0]->retriveImageUrlByImageUsage('image-3-b') ?>" alt="<?php echo $displays["destaque-2"][0]->getTitle() ?>"></a>
               <a href="<?php echo $site->retriveUrl() ?>/<?php echo $se_link ?>"><?php echo $displays["destaque-2"][0]->getTitle() ?></a>
              </div>
