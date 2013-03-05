2889b5d7fa19781ffc60caeb977caa3e063ae28d
renatareno <asdf@fpawks0142.local>
2013-03-05 10:00:51 -0300

    buscar

--- apps/frontend/templates/sites/cedoc2/indexSuccess.php
+++ apps/frontend/templates/sites/cedoc2/indexSuccess.php
@@ -20,7 +20,7 @@
               <div class="input-append">
                 <input type="hidden" name="output" value="search">
                 <button type="submit" class="btn"><i class="icon-search"></i></button>
-                <input type="text" name="q" class="search-query input-medium">
+                <input type="text" name="q" class="search-query input-medium" value="buscar imagens" placeholder="buscar imagens">
                 
               </div>
             </form>
