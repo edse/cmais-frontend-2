6005c865f2b2738cbdde008bd2c144a3698df561
rafaelhcs <rhcsousa@gmail.com>
2013-03-05 14:22:35 -0300

    up

--- apps/frontend/templates/sites/cedoc2/indexSuccess.php
+++ apps/frontend/templates/sites/cedoc2/indexSuccess.php
@@ -11,11 +11,10 @@
         <?php endif; ?>
       <?php endif; ?>
         <ul class="menu">
-          <li></li>
-          <li class="ativo"><span></span><a href="<?php echo $site->retriveUrl() ?>" title="Cedoc"><h3>cedoc</h3></a></li>
-          <li><span></span><a href="<?php echo $site->retriveUrl() ?>/quem-somos" title="Quem Somos">quem somos</a></li>
-          <li><span></span><a href="<?php echo $site->retriveUrl() ?>/acervo" title="Acerto">acervo</a></li>
-          <li><span></span><a href="<?php echo $site->retriveUrl() ?>/contato" title="Contato">contato</a></li>
+          <li><h3>Cedoc</h3></li>
+          <li class="ativo"><span></span><a href="#" title="Quem Somos">quem somos</a></li>
+          <li><span></span><a href="#" title="Acerto">acervo</a></li>
+          <li><span></span><a href="#" title="Contato">contato</a></li>
           <li>
             <form class="form-search pull-right" action="busca.php">
               <div class="input-append">
