206a62aba73d730e746e722382727762aaf7b690
Emerson Estrella <emersonestrella@edse.local>
2013-03-06 11:35:44 -0300

    asdf

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -588,7 +588,7 @@ class _assetActions extends sfActions
           $this->getResponse()->addMetaProp('og:video:type', 'application/x-shockwave-flash');
           $this->getResponse()->addMetaProp('og:video:width', '640');
           $this->getResponse()->addMetaProp('og:video:height', '390');
-          $this->getResponse()->addMetaProp('og:image', 'http://i4.ytimg.com/vi/'.$rel[0]->AssetVideo->getYoutubeId().'/default.jpg');
+          $this->getResponse()->addMetaProp('og:image', 'http://i4.ytimg.com/vi/'.$rel[0]->AssetVideo->getYoutubeId().'/0.jpg');
         }elseif($rel[0]->AssetType->getSlug() == "audio"){
           /*
           $this->getResponse()->addMetaProp('og:type', 'audio');
