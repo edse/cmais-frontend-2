b36f373e2176e011dd20e5cddd6a75e08e51aac1
renatareno <asdf@fpawks0142.local>
2013-03-06 10:44:10 -0300

    onfocus

--- apps/frontend/templates/sites/cedoc2/indexSuccess.php
+++ apps/frontend/templates/sites/cedoc2/indexSuccess.php
@@ -20,7 +20,7 @@
               <div class="input-append">
                 <input type="hidden" name="output" value="search">
                 <button type="submit" class="btn"><i class="icon-search"></i></button>
-                <input type="text" name="q" class="search-query input-medium" value="buscar imagens" placeholder="buscar imagens">
+                <input type="text" name="q" class="search-query input-medium" value="buscar imagens" placeholder="buscar imagens" onfocus="this.value='';">
                 </div>
             </form>
           </li>
