f9bf8a3f73a7b2eaab0d98fd03de960dc16c33dc
renatareno <asdf@fpawks0142.local>
2013-03-08 16:19:20 -0300

    ico

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -27,10 +27,12 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://cmais.com.br/portal/images/icon/cmais-favico_114.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.png">
     <link rel="SHORTCUT ICON" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.png">
-    -->
-    
     <link rel="icon" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico">
-    <!--link rel="shortcut icon" href="/portal/images/favicon.ico" type="image/x-icon" /-->
+    <link rel="shortcut icon" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico" type="image/x-icon" />
+    -->
+    <link rel="SHORTCUT ICON" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico">
+    
+    
     
 
     <!-- scripts -->
