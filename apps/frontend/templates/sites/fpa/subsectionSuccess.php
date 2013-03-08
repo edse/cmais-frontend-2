752bbf0b7bfe78e30b5ed0f313fc18825820c242
doljak <jedoljak@gmail.com>
2013-03-08 16:53:35 -0300

    fpa

--- apps/frontend/templates/sites/fpa/subsectionSuccess.php
+++ apps/frontend/templates/sites/fpa/subsectionSuccess.php
@@ -35,7 +35,7 @@ body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important
               <?php $download = $d->retriveRelatedAssetsByRelationType('Download');?>
               <?php 
               echo count($download);
-              echo $download[0]->file;
+              echo $download->file;
               ?>
             </div>
           </div>
