ee7ac5aee904c7288531b548e87884a0049c3eef
Joseval <joseval@terra.com.br>
2013-03-11 15:31:56 -0300

    Multicultura link para grade de programação

--- apps/frontend/modules/ajax/actions/actions.class.php
+++ apps/frontend/modules/ajax/actions/actions.class.php
@@ -514,7 +514,7 @@ class ajaxActions extends sfActions
         $return .= '</ul><ul class="lista">';
         foreach($pc4 as $p)
           $return .= '<li><a href="'.$p->retriveUrl().'">'.$p->getTitle().'</a></li>';
-        $return .= '</ul><div class="botoes"><a href="http://cmais.com.br/grade?c=multicultura">Grade completa</a><a href="http://cmais.com.br/programas-de-a-z">Todos os programas de A a Z</a></div>';
+        $return .= '</ul><div class="botoes"><a href="http://multicultura.cmais.com.br/programacao">Grade completa</a><!--<a href="http://cmais.com.br/programas-de-a-z">Todos os programas de A a Z</a>--></div>';
       }
       elseif($request->getParameter('content') == "tvrtb"){
         $programs = array(); $pc1 = array(); $pc2 = array(); $pc3 = array(); $pc4 = array();
@@ -800,7 +800,7 @@ class ajaxActions extends sfActions
           $return .= '<p class="titulos hora">'.format_date($important->getDateStart(), "t").'</p></div>';
         }
         unset($important); unset($coming); unset($live);
-        $return .= '<div class="botoes"><a href="/grade?c=multicultura">Grade completa</a></div>';
+        $return .= '<div class="botoes"><a href="http://multicultura.cmais.com.br/programacao">Grade completa</a></div>';
       }
       elseif($request->getParameter('content') == "no-ar-tvrtb"){
         $live = Doctrine_Query::create()
