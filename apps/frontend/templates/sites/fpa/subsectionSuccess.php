5c68f4b49e98674dde5214b04631503e0c9ed226
doljak <jedoljak@gmail.com>
2013-03-11 11:18:14 -0300

    asd

--- apps/frontend/templates/sites/fpa/subsectionSuccess.php
+++ apps/frontend/templates/sites/fpa/subsectionSuccess.php
@@ -35,9 +35,13 @@ body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important
               <?php $download = $d->retriveRelatedAssetsByRelationType('Download');?>
               <?php echo count($download) ?>
               <?php if(count($download)>0): ?>
-                <?php if(isset($download)):?>
-                  <a href="http://midia.cmais.com.br/assets/file/original/<?php echo $download[0]->AssetFile->getFile(); ?>" title="<?php echo $download[0]->AssetFile->getAsset();?>" target="_blank"><?php echo $download[0]->AssetFile->getAsset(); ?></a>
-                <?php endif; ?>
+                <?php foreach($download as $f):?>
+                  <?php if(isset($f)):?>
+                    <a href="http://midia.cmais.com.br/assets/file/original/<?php echo $f[0]->AssetFile->getFile(); ?>" title="<?php echo $f[0]->AssetFile->getAsset();?>" target="_blank">
+                      <?php echo $f[0]->AssetFile->getAsset(); ?>
+                    </a>
+                  <?php endif; ?>
+                <?php endforeach; ?>
               <?php endif; ?>
             </div>
           </div>
