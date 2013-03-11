faec94c1828f4b4142500a54917f8c0ecdb5c6fd
rafaelhcs <rhcsousa@gmail.com>
2013-03-11 18:05:47 -0300

    up

--- apps/frontend/templates/sites/cocorico/receitinhas-especiaisSuccess.php
+++ apps/frontend/templates/sites/cocorico/receitinhas-especiaisSuccess.php
@@ -111,9 +111,10 @@ $assets = $pager->getResults(); //depois tem de ordenar por ranking...
       
        <?php if(isset($displays['receitinhas-especiais'])):?>
     <?php if(count($displays['receitinhas-especiais']) > 1): ?>
+      <?php $related = $d->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
       <div class="span6">
-        <a href="/cocorico/receitinhas-interna" title="link do jogo"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" /></a>
-        <a href="/cocorico/receitinhas-interna" class="span12 btn" title="">Nome do Joguinho</a>
+        <a href="<?php echo $displays['receitinhas-especiais'][0]->retriveUrl() ?>" title="<?php echo $displays['receitinhas-especiais'][0]->getTitle() ?>"><img class="span12" src="<?php echo $displays['receitinhas-especiais'][0]->retriveUrl() ?>" alt="<?php echo $displays['receitinhas-especiais'][0]->getTitle() ?>" /></a>
+        <a href="<?php echo $displays['receitinhas-especiais'][0]->retriveUrl() ?>" class="span12 btn" title=""><?php echo $displays['receitinhas-especiais'][0]->getTitle() ?></a>
         <ul class="likes">
           <li class="ativo"></li>
           <li></li>
@@ -128,7 +129,7 @@ $assets = $pager->getResults(); //depois tem de ordenar por ranking...
       
       <?php if(isset($displays['receitinhas-especiais'])):?>
     <?php if(count($displays['receitinhas-especiais']) > 2): ?>
-      
+      <?php $related = $d->Asset->retriveRelatedAssetsByAssetTypeId(6); ?>
       <div class="span6">
         <a href="/cocorico/receitinhas-interna" title="link do jogo"><img class="span12" src="/portal/images/capaPrograma/cocorico/jogo-home.jpg" alt="jogo" /></a>
         <a href="/cocorico/receitinhas-interna" class="span12 btn" title="">Nome do Joguinho</a>
