b499f88ac83ee2b333f4f881885a321060996743
doljak <jedoljak@gmail.com>
2013-03-11 16:04:18 -0300

    asd

--- apps/frontend/templates/blocks/_display-1c-gallery.php
+++ apps/frontend/templates/blocks/_display-1c-gallery.php
@@ -16,7 +16,7 @@
                     <ul class="destaques">
                     <?php if(count($imgs) > 0): ?>
                       <?php for($i=0; $i<3; $i++): ?>
-                      <li><a href="<?php echo $displays[0]->retriveUrl() ?>"><img alt="<?php echo $imgs[$i]->getTitle() ?>" src="http://midia.cmais.com.br/assets/image/image-3-b/<?php echo $imgs[$i]->AssetImage->getFile() ?>.jpg" /></a>
+                      <li><a href="<?php echo $displays[0]->retriveUrl() ?>/teste"><img alt="<?php echo $imgs[$i]->getTitle() ?>" src="http://midia.cmais.com.br/assets/image/image-3-b/<?php echo $imgs[$i]->AssetImage->getFile() ?>.jpg" /></a>
                         <div class="tooltip">
                           <span></span>
                           <a href="<?php echo $displays[0]->retriveUrl() ?>"><?php echo $imgs[$i]->getTitle() ?></a>
