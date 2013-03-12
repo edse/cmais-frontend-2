15346e2d1c6d9f2b5671a18b12bd947b2031d97f
doljak <jedoljak@gmail.com>
2013-03-12 11:42:21 -0300

    fm e sub

--- apps/frontend/templates/sites/fpa/subsectionSuccess.php
+++ apps/frontend/templates/sites/fpa/subsectionSuccess.php
@@ -33,10 +33,12 @@ body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important
             <div class="accordion-inner">
               <?php echo html_entity_decode($d->AssetContent->render()) ?>
               <?php $download = $d->retriveRelatedAssetsByRelationType('Download');?>
-              <?php echo count($download) ?>
+              <?php for($i=0; $i<count($dowload); $i++):?>
+               <?php echo $i . "<br>"?> 
+              <?php endfor;?>
               <?php if(count($download)>0): ?>
                 <?php if(isset($download)): ?>
-                  <a href="http://midia.cmais.com.br/assets/image/original/<?php echo $d->AssetImage->getOriginalFile() ?>" title="" target="_blank">teste</a>
+                  <a href="http://midia.cmais.com.br/assets/file/original/<?php echo $download[0]->AssetFile->getFile(); ?>" title="<?php echo $download[0]->AssetFile->getAsset();?>" target="_blank"><?php echo $download[0]->AssetFile->getAsset(); ?></a>
                 <?php endif; ?>
               <?php endif; ?>
             </div>
