b505c27528961c93cfb440d3689ad61a448efc2e
Joseval <joseval@terra.com.br>
2013-03-12 19:25:07 -0300

    a

--- apps/frontend/templates/sites/quintaldacultura/_destaque-joguinhos.php
+++ apps/frontend/templates/sites/quintaldacultura/_destaque-joguinhos.php
@@ -1,8 +1,8 @@
 <?php 
-  $include_destaque = rand(0,1);
-  //$include_destaque = 1;
+  //$include_destaque = rand(0,1);
+  $include_destaque = 1;
   if($include_destaque == 1)
-    //include_partial_from_folder('sites/quintaldacultura', 'global/novos_joguinhos_peixonauta');
+    include_partial_from_folder('sites/quintaldacultura', 'global/novos_joguinhos_peixonauta');
  // else 
     //include_partial_from_folder('sites/quintaldacultura', 'global/novos-joguinhos-peixonauta');
 ?>
