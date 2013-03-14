ebdf5535e973d319401ce92a746d300a1ac3fbd1
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-03-13 21:01:10 -0300

    u

--- apps/frontend/templates/sites/cedoc2/contactSuccess.php
+++ apps/frontend/templates/sites/cedoc2/contactSuccess.php
@@ -6,10 +6,10 @@
   <!-- Main hero unit for a primary marketing message or call to action -->
   <div class="conteudo">
     <ul class="menu">
-      <li class="cedoc"><h3><a href="/cedoc/" title="Cedoc">Cedoc</a></h3></li>
-      <li><span></span><a href="/cedoc/content" title="Quem Somos">quem somos</a></li>
-      <li><span></span><a href="/cedoc/resultado-busca" title="Acervo">acervo</a></li>
-      <li class="ativo"><span></span><a href="/cedoc/contato" title="Contato">contato</a></li>
+      <li class="cedoc"><h3><a href="<?php echo $site->retriveUrl() ?>" title="Cedoc">Cedoc</a></h3></li>
+      <li><span></span><a href="<?php echo $site->retriveUrl() ?>/quem-somos" title="Quem Somos">quem somos</a></li>
+      <li><span></span><a href="<?php echo $site->retriveUrl() ?>/acervo" title="Acervo">acervo</a></li>
+      <li class="ativo"><span></span><a href="<?php echo $site->retriveUrl() ?>/contato" title="Contato">contato</a></li>
       <li>
         <?php include_partial_from_folder('sites/cedoc2', 'global/formBusca') ?>
       </li>
