51573de83c769ede4332f3b434fb7fd92d331b8a
rafaelhcs <rhcsousa@gmail.com>
2013-03-14 13:20:54 -0300

    up

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -77,7 +77,7 @@ class _sectionActions extends sfActions
         }
         */
        
-        if($this->section->getSlug() == "receitinhas" && $request->getParameter('test')){
+        if($this->section->getSlug() == "receitinhas"){
           $subsections = $this->section->subsections();
           
           foreach($subsections as $s){
