fb7e5f255100f6f02121260ae072318d13e01cd9
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-03-14 17:07:04 -0300

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -676,12 +676,15 @@ class _assetActions extends sfActions
     }
     elseif($this->site->getSlug() == "cocorico") {
       $this->setLayout('cocorico');
-      
       if($this->section->slug == "joguinhos") {
         if ($debug) print "<br>cocorico-1 >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/joguinho';
         $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/joguinho');
       }
       elseif($this->section->slug == "receitinhas") {
+        if($request->getParameter("teste") == 1) {
+          die($this->section->slug);
+        }
+        
         if ($debug) print "<br>cocorico-2-b >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/receitinha';
         $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/receitinha');
       }
