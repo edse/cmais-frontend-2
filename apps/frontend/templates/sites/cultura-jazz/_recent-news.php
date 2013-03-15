922c522ace4a112b6262ccee75ba9aa75bddd94b
Joseval <joseval@terra.com.br>
2013-03-15 16:25:35 -0300

    up

--- apps/frontend/templates/sites/cultura-jazz/_recent-news.php
+++ apps/frontend/templates/sites/cultura-jazz/_recent-news.php
@@ -2,14 +2,14 @@
                 <?php if(count($displays) > 0): ?>
                 <ul class="conteudo">
                   <?php foreach($displays as $k=>$d): ?>
-                    <?php if($k==6):?>
-                  <li><a class="titulos" href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>"><?php echo $d->getTitle() ?></a>
+                    <?php if($k<6):?>
+                      <li><a class="titulos" href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>"><?php echo $d->getTitle() ?></a>
                     <?php if($d->retriveImageUrlByImageUsage("image-3") != ""): ?>
                       <a href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>" class="img-90x54" style="width: auto">
                         <img src="<?php echo $d->retriveImageUrlByImageUsage("image-1-b") ?>" alt="<?php echo $d->getTitle() ?>" title="<?php echo $d->getTitle() ?>" style="width: auto" />
                       </a>
                       <?php endif;?>
-                    <?php endif; ?>
+                      <?php endif; ?>
                       <p><?php echo $d->getDescription() ?></p>
                   </li>
                   <?php endforeach; ?>
