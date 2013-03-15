8febe51931decd65a0a67d12d8f379e427dacb9f
rafaelhcs <rhcsousa@gmail.com>
2013-03-15 18:41:07 -0300

    up

--- apps/frontend/templates/blocks/_display-2c-broadcast2.php
+++ apps/frontend/templates/blocks/_display-2c-broadcast2.php
@@ -70,7 +70,13 @@
                     <p class="bold"><?php echo $d->retriveTitle() ?></p>
                     <p><?php echo $d->retriveDescription3() ?></p>
                     <a href="<?php echo $d->retriveUrl() ?>">ir ao site</a>
-                    <a href="http://www.google.com/calendar/event?action=TEMPLATE&text=<?php echo urlencode($d->getTitle()) ?>&dates=<?php echo DateTime::createFromFormat('Y-m-d H:i:s', $d->getDateStart())->format('Ymd\THis\Z') ?>/<?php echo DateTime::createFromFormat('Y-m-d H:i:s', $d->getDateStart())->format('Ymd\THis\Z') ?>&details=&location=<?php echo urlencode($d->Program->getTitle()) ?>&trp=false&sprop=http%3A%2F%2Fcmais.com.br&sprop=name:TV%20Cultura" target="_blank" class="google-agenda"><img src="http://www.google.com/calendar/images/ext/gc_button1.gif" border=0 style="width:100px;height:25px;" /></a>
+                    <?php $inicio_hora = strtotime(DateTime::createFromFormat('H:i:s'))?>
+                    <?php $inicio_acrescimo = "03:00:00" ?>
+                    <?php $inicio = $inicio_hora + strtotime($inicio_acrescimo) ?>
+
+                    <?php $inicio_data = $d->getDateStart()->format('Ymd\THis\Z')?>
+                    <?php $termino_get = DateTime::createFromFormat('Y-m-d H:i:s', $d->getDateStart())->format('Ymd\THis\Z') ?>
+                    <a href="http://www.google.com/calendar/event?action=TEMPLATE&text=<?php echo urlencode($d->getTitle()) ?>&dates=<?php echo $inicio, $inicio_data ?>/<?php echo $termino ?>&details=&location=<?php echo urlencode($d->Program->getTitle()) ?>&trp=false&sprop=http%3A%2F%2Fcmais.com.br&sprop=name:TV%20Cultura" target="_blank" class="google-agenda"><img src="http://www.google.com/calendar/images/ext/gc_button1.gif" border=0 style="width:100px;height:25px;" /></a>
                   </div>
                 </li>
               <?php endforeach; ?>
