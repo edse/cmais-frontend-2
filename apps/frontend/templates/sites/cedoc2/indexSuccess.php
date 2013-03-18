cc4c20bb800637bb75c38657eada6411ee76053d
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-03-18 16:04:03 -0300

    u

--- apps/frontend/templates/sites/cedoc2/indexSuccess.php
+++ apps/frontend/templates/sites/cedoc2/indexSuccess.php
@@ -7,7 +7,7 @@ function word_limiter($str,$limit=10)
     $ex_str = explode(" ",$str);
     if(count($ex_str)>$limit){
       for($i=0;$i<$limit;$i++){
-        $str_s.=$ex_str[$i]." ";
+        $str_s.=$ex_str[$i];
       }
       return $str_s;
     }else{
