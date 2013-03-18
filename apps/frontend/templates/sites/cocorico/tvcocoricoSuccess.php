81b1794454bdddd95e71948dcb157a870d5eada9
doljak <jedoljak@gmail.com>
2013-03-18 11:42:47 -0300

    coco

--- apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
@@ -111,8 +111,6 @@
       <!-- destaque-home-simples -->
       <div class="destaque-home-tv span9" id="destaqueForm" style="display:block;">
         
-        <?php if(isset($displays['destaque-principal'])): ?>
-          <?php if(count($displays['destaque-principal']) > 0): ?>
             <h2>Concurso Cultural</h2>
             <img class="promocao" src="/portal/images/capaPrograma/cocorico/10apps_destaque-galeria-concurso.jpg" />
            
@@ -122,8 +120,7 @@
               <a href="javascript:;" class="btn-destaque btn-form" title="Participe">Participar</a> 
               <span class="last"></span>
             </div>
-          <?php endif; ?>
-        <?php endif; ?>
+
         
         
         <?php /*if(isset($displays['destaque-seu-video'])): ?>
