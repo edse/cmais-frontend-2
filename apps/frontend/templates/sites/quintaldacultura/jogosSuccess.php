c811e59b3564fd34f657114d84765aef73b87414
renatareno <asdf@fpawks0142.local>
2013-03-19 15:19:48 -0300

    img 2-b

--- apps/frontend/templates/sites/quintaldacultura/jogosSuccess.php
+++ apps/frontend/templates/sites/quintaldacultura/jogosSuccess.php
@@ -109,7 +109,7 @@
                     <?php foreach($assets as $k=>$d): ?>
                       <li>
                         <a href="<?php echo $d->retriveUrl() ?>"><span class="top"><span class="text1"><?php echo $d->getTitle() ?></span></span></span>
-                        <?php if($d->retriveImageUrlByImageUsage("image-2") != ""): ?>
+                        <?php if($d->retriveImageUrlByImageUsage("image-2-b") != ""): ?>
                           <img src="<?php echo $d->retriveImageUrlByImageUsage("image-2") ?>" alt="<?php echo $d->getTitle() ?>" name="<?php echo $d->getTitle() ?>" style="width: 200px;" />
                         <?php endif; ?><span class="bottom"><span class="text2">brincar</span></span></a>
                       </li>
