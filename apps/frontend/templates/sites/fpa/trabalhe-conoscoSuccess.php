f4e22e51585dfd34c80d10ee6dcddb7ac0baf238
doljak <jedoljak@gmail.com>
2013-03-20 19:01:06 -0300

    u

---
+++ apps/frontend/templates/sites/fpa/trabalhe-conoscoSuccess.php
@@ -0,0 +1,4 @@
+<?php include_partial_from_folder('blocks', 'global/topo-fpa', array('siteSections'=>$siteSections, 'site' => $site, 'section' => $section)) ?>
+<style>
+body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important;}
+</style>
