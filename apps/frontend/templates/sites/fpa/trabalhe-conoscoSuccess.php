d1bb6c51cb95c53237c24e34cee6049f622b420b
doljak <jedoljak@gmail.com>
2013-03-20 12:47:22 -0300

    u

--- apps/frontend/templates/sites/fpa/trabalhe-conoscoSuccess.php
+++ apps/frontend/templates/sites/fpa/trabalhe-conoscoSuccess.php
@@ -129,10 +129,16 @@ body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important
           <!-- estagios -->
           <?php
             $related = $s->getAssets();
-            foreach($related as $k=>$file):
-              echo "titulo: ".$file[$k]->getTitle()."<br>";
-              echo "arquivo: ".$file[$k]->AssetFile->getFile()."<br>";
-              
+            foreach($related as $k=>$d):
+              echo "titulo: ".$related[$k]->getTitle()."<br>";
+              echo "arquivo: ".$related[$k]->AssetFile->getFile()."<br>";
+              if($related[$k]->asset_type_id==8):
+          ?>
+            <a href="http://midia.cmais.com.br/assets/file/original/<?php echo $related[$k]->AssetFile->getFile(); ?>" title="<?php echo $related[$k]->AssetFile->getAsset();?>" target="_blank">
+              <i class="icon-file icon-blue"></i> <?php echo $related[$k]->AssetFile->getAsset(); ?>
+            </a><br> 
+          <?php     
+              endif;
             endforeach;
           ?>
           <!-- estagios -->  
