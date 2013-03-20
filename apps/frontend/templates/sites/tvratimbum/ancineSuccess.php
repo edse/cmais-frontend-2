59398fe01843bb53106751b743310a1b876bf83c
rafaelhcs <rhcsousa@gmail.com>
2013-03-20 10:06:43 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/ancineSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/ancineSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N', 'Date') ?>
-<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 <link href="/portal/tvratimbum/css/geral.css" type="text/css" rel="stylesheet">
 <link href="/portal/tvratimbum/css/novoLayout-2012.css" type="text/css" rel="stylesheet">
 <link href="/portal/tvratimbum/css/institucional.css" type="text/css" rel="stylesheet">
