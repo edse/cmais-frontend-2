4aa1e846edffce96e4b7a7ef5d32679f11923245
Joseval <joseval@terra.com.br>
2013-03-21 13:42:51 -0300

    up

--- apps/frontend/templates/sites/cultura-jazz/_recent-news.php
+++ apps/frontend/templates/sites/cultura-jazz/_recent-news.php
@@ -1,8 +1,8 @@
               <?php if(isset($displays)): ?>
                 <?php if(count($displays) > 0): ?>
                 <ul class="conteudo">
-                  <?php foreach($displays as $k=>$d): ?>
-                     <?php if($k<6):?> 
+                  <?php foreach($displays as $d): ?>
+                    
                   <li><a class="titulos" href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>"><?php echo $d->getTitle() ?></a>
                     <?php if($d->retriveImageUrlByImageUsage("image-3") != ""): ?>
                       <a href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>" class="img-90x54" style="width: auto">
@@ -12,7 +12,7 @@
                      
                       <p><?php echo $d->getDescription() ?></p>
                   </li>
-                  <?php endif; ?>
+                  
                   <?php endforeach; ?>
                 </ul>
               <?php endif; ?>
