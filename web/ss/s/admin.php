61d0c911b1b33a053a07a5166052b8cd57f72189
Emerson Estrella <emersonestrella@edse.local>
2013-03-21 13:37:26 -0300

    ss

--- web/ss/s/admin.php
+++ web/ss/s/admin.php
@@ -6,7 +6,7 @@ if(isset($_REQUEST["start"])){
   //$h = isset($_REQUEST["host"])?$_REQUEST["host"]:"172.20.18.133";
   $h = isset($_REQUEST["host"])?$_REQUEST["host"]:"200.136.27.32";
   //$p = isset($_REQUEST["port"])?$_REQUEST["port"]:"443";
-  $p = isset($_REQUEST["port"])?$_REQUEST["port"]:"4442";
+  $p = isset($_REQUEST["port"])?$_REQUEST["port"]:"4443";
   $c = isset($_REQUEST["clients"])?$_REQUEST["clients"]:"2000";
   //$o = isset($_REQUEST["check"])?$_REQUEST["check"]:"1";
   $o = isset($_REQUEST["check"])?$_REQUEST["check"]:"";
