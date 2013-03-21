9f24b3722edd1fb3cb3dcd3f42c1b743e267bc2a
rafaelhcs <rhcsousa@gmail.com>
2013-03-21 19:03:09 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
@@ -56,7 +56,7 @@
               <a href="<?php echo $site->retriveUrl()?>" class="enunciado"><?php echo $site->getTitle()?></a>
             </div>
             <div class="programaEscolhido-info">
-              <img alt="<?php echo $site->retriveUrl()?>" src="http://midia.cmais.com.br/programs/<?php echo $site->Program->getSiteThumb() ?>" />
+              <img alt="<?php echo $site->retriveUrl()?>" src="http://midia.cmais.com.br/programs/<?php echo $site->Program->getImageThumb() ?>" />
               <div class="box-infos">
                 <?php /*
                 <div class="horario">
