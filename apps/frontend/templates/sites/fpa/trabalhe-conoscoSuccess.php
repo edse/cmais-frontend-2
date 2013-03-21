806a77521468766a88a39e7ae6f1532731d24162
doljak <jedoljak@gmail.com>
2013-03-21 12:51:07 -0300

    u

--- apps/frontend/templates/sites/fpa/trabalhe-conoscoSuccess.php
+++ apps/frontend/templates/sites/fpa/trabalhe-conoscoSuccess.php
@@ -25,7 +25,7 @@ body{background: url(/portal/images/capaPrograma/fpa/bkg-pattern.jpg) !important
           <!-- /Sem Vagas -->
         <?php
           elseif($s->id!=2287 && $s->id!=2296):
-            count($s->Assets->getIsActive());
+            count($s->getAssets());
             if(count($s->Assets->getIsActive())>0):
               echo "true";
             else: 
