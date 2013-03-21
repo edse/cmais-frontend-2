dc4e9d8b62985dd50d2d0003dd962567a8707301
Joseval <joseval@terra.com.br>
2013-03-21 11:04:27 -0300

    up

--- apps/frontend/templates/sites/cultura-jazz/_recent-news.php
+++ apps/frontend/templates/sites/cultura-jazz/_recent-news.php
@@ -1,7 +1,7 @@
               <?php if(isset($displays)): ?>
                 <?php if(count($displays) > 0): ?>
                 <ul class="conteudo">
-                  <?php print_r($display)?>
+                  <?php print_r($displays)?>
                   <?php foreach($displays as $k=>$d): ?>
                      <?php if($k<1):?>
                   <li><a class="titulos" href="/<?php echo $site->getSlug() ?>/<?php echo $d->getSlug() ?>"><?php echo $d->getTitle() ?></a>
