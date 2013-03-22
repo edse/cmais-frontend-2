fb13c5aa47a5eacd34462392729e17c9e07e1612
doljak <jedoljak@gmail.com>
2013-03-22 18:32:54 -0300

    u

--- apps/frontend/templates/sites/qss/indexSuccess.php
+++ apps/frontend/templates/sites/qss/indexSuccess.php
@@ -1,11 +1,10 @@
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
-  <!-- CAPA SITE -->
-  <div class="bg-provocacoes" id="home">
-teste
+<!-- CAPA SITE -->
+<div id="capa-site">
+  <img src="/portal/images/capaPrograma/qss/background-qss.jpg" alt="Quem Sabe Sabe">    
 
-  </div>
-  <!-- /capa site-->
 </div>
-<!-- /bg provocacoes-->
+<!-- /capa site-->
+
