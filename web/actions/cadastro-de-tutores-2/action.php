5d4c298228fefd17485762f5a31149124b33796a
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-03-22 16:12:43 -0300

    u

---
+++ web/actions/cadastro-de-tutores-2/action.php
@@ -0,0 +1,43 @@
+<?php
+$_REQUEST["cpf"] = str_replace(array('.','-'),"",$_REQUEST["cpf"]);
+$_REQUEST["rg"] = str_replace(array('.','-'),"",$_REQUEST["rg"]);
+
+if($_REQUEST["captcha"]) {
+  if($_REQUEST["cpf"]){
+    if(exec('grep "^'.$_REQUEST["cpf"].'$" cpf-alunos.txt')){
+      die("2");
+    }
+    elseif(exec('grep "^'.$_REQUEST["cpf"].'$" cpf-tutores.txt')){
+      die("3");
+    }
+    else{
+      $csvFile = "/var/frontend/web/actions/cadastro-de-tutores-2/cadastro-de-tutores.csv";
+      $csvContent = "";
+      while(list($campo, $valor) = each($_REQUEST)) {
+        if(!in_array($campo, array('Form_action', 'X', 'Y', 'Enviar', 'Undefinedform_action')))
+          $csvContent .= $valor . ",";
+      }
+      $csvContent .= "\n";
+      $csvFp = fopen($csvFilename, 'a+');
+      if(fwrite($csvFp, $csvContent)){
+        $txtFile = "/var/frontend/web/actions/cadastro-de-tutores-2/cpf-tutores.txt";
+        $txtFp = fopen($filename, 'a+');
+        if(fwrite($fp, $content)){
+          $filename = "/var/frontend/web/actions/cadastro-de-tutores-2/cpf-tutores.txt";
+         
+        die("0");
+      }
+      else{
+        die("1");
+      }
+    } 
+  }
+  else {
+    die("1");
+  }
+}
+else{
+  die("1");
+}
+
+?>
