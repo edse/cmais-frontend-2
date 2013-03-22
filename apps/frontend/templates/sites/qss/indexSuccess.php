1fd2f336170e0c2c45a4ef4d0854114aedccf53a
doljak <jedoljak@gmail.com>
2013-03-22 18:22:51 -0300

    qss

--- apps/frontend/templates/sites/qss/indexSuccess.php
+++ apps/frontend/templates/sites/qss/indexSuccess.php
@@ -1,1 +1,11 @@
-testeJe
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+
+  <!-- CAPA SITE -->
+  <div class="bg-provocacoes" id="home">
+teste
+
+  </div>
+  <!-- /capa site-->
+</div>
+<!-- /bg provocacoes-->
