d60536aaec9da9dba680105daf15d563ecdcf9bd
doljak <jedoljak@gmail.com>
2013-03-22 19:08:19 -0300

    q

--- apps/frontend/templates/sites/qss/indexSuccess.php
+++ apps/frontend/templates/sites/qss/indexSuccess.php
@@ -16,6 +16,28 @@
       <!--<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php if($site->getTwitterAccount()): ?><?php echo $site->getTwitterAccount() ?><?php else: ?>tvcultura<?php endif; ?>">Tweet</a>-->
     </div>
     <!-- /curtir -->
+    <div class="msgErro" style="display:none">
+      <span class="alerta"></span>
+      <div class="boxMsg">
+        <p class="aviso">Sua mensagem não pode ser enviada.</p>
+        <p>Confirme se todos os campos foram preenchidos corretamente e verifique seus dados. Você pode ter esquecido de preencher algum campo ou errado alguma informação.</p>
+      </div>
+      <hr />
+    </div>
+    <div class="msgAcerto" style="display:none">
+      <span class="alerta"></span>
+      <div class="boxMsg">
+        <p class="aviso">Mensagem enviada com sucesso!</p>
+        <p>Obrigado por entrar em contato com nosso programa. Em breve retornaremos sua mensagem.</p>
+      </div>
+      <hr />
+    </div>
+    <form id="form-contato" method="post" action="">
+      <p>Cadastre o seu e-mail para ser informado sobre a data de início das inscrições:</p>
+      <input type="text" name="cadastro" id="input" />
+      <input class="enviar" type="submit" name="enviar" id="enviar" value="enviar mensagem" style="cursor:pointer" />
+      <img src="/portal/images/ajax-loader.gif" alt="enviando..." style="display:none" width="16px" height="16px" id="ajax-loader" />
+    </form>  
 </div> 
 <!-- /capa site-->
  
