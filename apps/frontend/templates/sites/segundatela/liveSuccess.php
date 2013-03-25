287b2cc11c393dfb4ea34275285be112d2c89c9c
renatareno <asdf@fpawks0142.local>
2013-03-25 13:58:02 -0300

    links comment

--- apps/frontend/templates/sites/segundatela/liveSuccess.php
+++ apps/frontend/templates/sites/segundatela/liveSuccess.php
@@ -19,7 +19,7 @@
        <p>Atenta às mudanças na forma de se transmitir e de se consumir mídia com os novos meios de tráfego de informações ou entretenimento, a TV Cultura apresenta sua grade de programação para 2013. Projetada para oferecer programas ao vivo, interativos e exclusivos, marcados antes de tudo pela qualidade, a TV Cultura compõe sua nova programação com atrações únicas, muitas das quais ao vivo e que permitem a participação interativa do público.</p>
        <a href="http://midia.cmais.com.br/assets/file/original/bb304856eeeb8f784472dcf45959af6e7da6fe3c.pdf" title="Press Kit" target="_blank"><i class="download"></i>Faça o download do press kit</a>
       </div>
-      <div class="download">
+      <!--div class="download">
         <h3>Releases</h3>
         <ul>
           <li><a href="#" title="" target="_blank">teste</a></li>
@@ -43,6 +43,6 @@
           <li><a href="#" title="" target="_blank"></a></li>
           <li><a href="#" title="" target="_blank"></a></li>
         </ul>
-      </div>
+      </div-->
       
     </div>
