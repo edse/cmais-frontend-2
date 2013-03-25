4cdec18a7fe65dd5b19ff966899c45feb4c22cb6
renatareno <asdf@fpawks0142.local>
2013-03-25 13:42:33 -0300

    links

--- apps/frontend/templates/sites/segundatela/liveSuccess.php
+++ apps/frontend/templates/sites/segundatela/liveSuccess.php
@@ -19,5 +19,30 @@
        <p>Atenta às mudanças na forma de se transmitir e de se consumir mídia com os novos meios de tráfego de informações ou entretenimento, a TV Cultura apresenta sua grade de programação para 2013. Projetada para oferecer programas ao vivo, interativos e exclusivos, marcados antes de tudo pela qualidade, a TV Cultura compõe sua nova programação com atrações únicas, muitas das quais ao vivo e que permitem a participação interativa do público.</p>
        <a href="http://midia.cmais.com.br/assets/file/original/bb304856eeeb8f784472dcf45959af6e7da6fe3c.pdf" title="Press Kit" target="_blank"><i class="download"></i>Faça o download do press kit</a>
       </div>
+      <div class="download">
+        <h3>Downloads</h3>
+        <ul>
+          <li><a href="#" title=""></a>1</li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+          <li><a href="#" title=""></a></li>
+        </ul>
+      </div>
       
     </div>
