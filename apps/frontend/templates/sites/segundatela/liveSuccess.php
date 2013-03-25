b095347a7469f5007e85f036d191540ba31af998
renatareno <asdf@fpawks0142.local>
2013-03-25 09:09:27 -0300

    aovivo

--- apps/frontend/templates/sites/segundatela/liveSuccess.php
+++ apps/frontend/templates/sites/segundatela/liveSuccess.php
@@ -2,6 +2,7 @@
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
     <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/grade2013.css" type="text/css" />
+    <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/aovivo.css" type="text/css" />
 
     <div class="container">
 
