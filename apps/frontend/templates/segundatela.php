2c18bd2398b52861c82bf138efb10cfde4e847d4
doljak <jedoljak@gmail.com>
2013-03-25 09:45:34 -0300

    u

--- apps/frontend/templates/segundatela.php
+++ apps/frontend/templates/segundatela.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/geral.css" type="text/css" />
 
     <script src="/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+    <script src="/portal/js/bootstrap/application.js" type="text/javascript"></script>
+    <script src="/portal/js/bootstrap/collapse.js" type="text/javascript"></script>
     <script src="/portal/js/bootstrap-v2.3.1/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
