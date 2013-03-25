89a6f3c00309c3e0c2c9b7ecab0b83711dca10f4
Emerson Estrella <emersonestrella@Emerson-Estrellas-MacBook-Pro.local>
2013-03-25 03:05:22 -0300

    asfd asdf

---
+++ apps/frontend/templates/sites/segundatela/liveSuccess.php
@@ -0,0 +1,20 @@
+    <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/grade2013.css" type="text/css" />
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <div class="bgtopo"><h1><img src="/portal/images/capaPrograma/segundatela/grade2013/logo.png" alt="Cultura 2013" /></h1></div>
+        <div class="col-dir"></div>
+      </div>
+
+      <!--  row -->
+      <div class="row-fluid conteudo">
+        <div class="span12">
+          <h2>Ao Vivo</h2>
+          <br />
+          <iframe width="640" height="364" src="http://www.youtube.com/embed/<?php echo $displays["yt-live"][0]->getTitle() ?>" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      
+    </div>
