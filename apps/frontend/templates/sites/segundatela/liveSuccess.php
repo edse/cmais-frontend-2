b6fab0b482735ab8082f1eacadcce0faa6de08db
Emerson Estrella <emersonestrella@Emerson-Estrellas-MacBook-Pro.local>
2013-03-25 03:07:04 -0300

    ss

--- apps/frontend/templates/sites/segundatela/liveSuccess.php
+++ apps/frontend/templates/sites/segundatela/liveSuccess.php
@@ -5,15 +5,15 @@
       <!-- Main hero unit for a primary marketing message or call to action -->
       <div class="hero-unit">
         <div class="bgtopo"><h1><img src="/portal/images/capaPrograma/segundatela/grade2013/logo.png" alt="Cultura 2013" /></h1></div>
-        <div class="col-dir"></div>
+        <div class="col-dir">
+          <iframe width="640" height="390" src="http://www.youtube.com/embed/videoseries?list=PL0Qz-covvhxSzQaY2cuxWcwekgje5UcDd" frameborder="0" allowfullscreen></iframe>
+        </div>
       </div>
 
       <!--  row -->
       <div class="row-fluid conteudo">
         <div class="span12">
-          <h2>Ao Vivo</h2>
-          <br />
-          <iframe width="640" height="364" src="http://www.youtube.com/embed/<?php echo $displays["yt-live"][0]->getTitle() ?>" frameborder="0" allowfullscreen></iframe>
+          <iframe width="640" height="390" src="http://www.youtube.com/embed/videoseries?list=PL0Qz-covvhxSzQaY2cuxWcwekgje5UcDd" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
       
