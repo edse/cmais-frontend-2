d87ef39da9de4fe75160189d376c815639862fe4
renatareno <asdf@fpawks0142.local>
2013-03-25 11:20:44 -0300

    grade

--- apps/frontend/templates/sites/segundatela/grade2013Success.php
+++ apps/frontend/templates/sites/segundatela/grade2013Success.php
@@ -69,15 +69,15 @@
             <div class="tab-content" id="myTabContent">
               <div id="release" class="tab-pane fade active in">
                 <p>Atenta às mudanças na forma de se transmitir e de se consumir mídia com os novos meios de tráfego de informações ou entretenimento, a TV Cultura apresenta sua grade de programação para 2013. Projetada para oferecer programas ao vivo, interativos e exclusivos, marcados antes de tudo pela qualidade, a TV Cultura compõe sua nova programação com atrações únicas, muitas das quais ao vivo e que permitem a participação interativa do público.</p>
-                <p>Os arquivos estarão disponíveis para download após as 12:00</p>
+                <!--p>Os arquivos estarão disponíveis para download após as 12:00</p-->
               </div>
               <div id="presskit" class="tab-pane fade">
-                <p>O press kit estará disponível para download após as 12:00</p>
-                <!--
+                <!--p>O press kit estará disponível para download após as 12:00</p-->
+                
                 <ul>
                   <li><a href="http://midia.cmais.com.br/assets/file/original/bb304856eeeb8f784472dcf45959af6e7da6fe3c.pdf" title="Press Kit" target="_blank"><i class="download"></i>Faça o download do press kit</a></li>
                 </ul>
-                -->         
+                      
               </div>
             </div>
           </div>
