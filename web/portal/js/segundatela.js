998e743c040f219a2030a5f5a92a9d9ccd7972b8
Emerson Estrella <emersonestrella@Emerson-Estrellas-MacBook-Pro.local>
2013-03-25 06:32:31 -0300

    ss

--- web/portal/js/segundatela.js
+++ web/portal/js/segundatela.js
@@ -125,68 +125,73 @@
       return;
     };
 
-    //yotube API
-    var tag = document.createElement('script');
-    tag.src = "//www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    //arrays para players multiplos
-    var player = new Array();
-    var cont = 0;
-    var players = new Array();
-    var playing = false; 
-    function checkState(res){
-      if(res.data==1){
-        playing=players[i].attr("id");
-      }
-    }
-    function onYouTubeIframeAPIReady() {
-      $('.accordion-body iframe').each(function(i){
-        $(this).attr("id","player"+i);
-        players[i] = $("#player"+i);
-      })
-      for(var i=0; i < players.length; i++){
-        player[i] = new YT.Player(players[i].attr("id"));
-        player[i].addEventListener("onStateChange", function(res){
-          if(res.data == 1){
-            var i = res.target.a.id.substring(6,7);
-            playing = player[i];
-          }
-        });
-      }
-    }  
-
-    $('#myTab a').click(function(e) {
-      e.preventDefault();
-      $(this).tab('show');
-    });
-
-    // colocando e tirando ativo
-    $('.accordion-body').on('hidden', function() {
-      //remove barra ativa
-      $(this).prev().find('a').removeClass('ativo');
-    });
-
-    $('.accordion-body').on('show', function() {
-      //player stop
-      if(playing)
-        playing.pauseVideo();
-      //remove barra ativa
-      $(this).prev().find('a').addClass('ativo');
-      //scroll
-      var el = $(this).parent();
-      $('html, body').animate({
-        scrollTop: el.offset().top
-      }, "fast");
-    });
-
-    // padding ultimo conteudo
-    $('.accordion-body').each(function() {
-      $(this).find('p:last').css('padding-bottom', '15px');
-    });
-
   });
 }).call(this);
+
+
+$(document).ready(function() {
+  //yotube API
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  //arrays para players multiplos
+  var player = new Array();
+  var cont = 0;
+  var players = new Array();
+  var playing = false; 
+  function checkState(res){
+    if(res.data==1){
+      playing=players[i].attr("id");
+    }
+  }
+  function onYouTubeIframeAPIReady() {
+    $('.accordion-body iframe').each(function(i){
+      $(this).attr("id","player"+i);
+      players[i] = $("#player"+i);
+    })
+    for(var i=0; i < players.length; i++){
+      player[i] = new YT.Player(players[i].attr("id"));
+      player[i].addEventListener("onStateChange", function(res){
+        if(res.data == 1){
+          var i = res.target.a.id.substring(6,7);
+          playing = player[i];
+        }
+      });
+    }
+  }  
+
+  $('#myTab a').click(function(e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+  // colocando e tirando ativo
+  $('.accordion-body').on('hidden', function() {
+    //remove barra ativa
+    $(this).prev().find('a').removeClass('ativo');
+  });
+
+  $('.accordion-body').on('show', function() {
+    //player stop
+    if(playing)
+      playing.pauseVideo();
+    //remove barra ativa
+    $(this).prev().find('a').addClass('ativo');
+    //scroll
+    var el = $(this).parent();
+    $('html, body').animate({
+      scrollTop: el.offset().top
+    }, "fast");
+  });
+
+  // padding ultimo conteudo
+  $('.accordion-body').each(function() {
+    $(this).find('p:last').css('padding-bottom', '15px');
+  });
+
+});
+
 
 window.onload=function(){
   window.onbeforeunload = function(){        
