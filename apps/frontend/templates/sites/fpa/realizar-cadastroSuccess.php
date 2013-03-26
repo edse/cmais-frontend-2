35af43ac6a14d9d1209d90d2bc5a61393b972e9c
doljak <jedoljak@gmail.com>
2013-03-26 19:01:03 -0300

    u

--- apps/frontend/templates/sites/fpa/realizar-cadastroSuccess.php
+++ apps/frontend/templates/sites/fpa/realizar-cadastroSuccess.php
@@ -712,8 +712,8 @@ $('#form1').validate({
       }
     },
     messages:{
-      fpa_cpf:"teste1",
-      fpa_data:"teste2"
+      fpa_cpf:"",
+      fpa_data:""
     },
     highlight: function(label) {
       $(label).closest('.control-group').addClass('error');
