a1bc409c5a09ac09f3c2e5616cb77f78b8bbaff2
doljak <jedoljak@gmail.com>
2013-03-27 15:56:40 -0300

    u

--- web/portal/js/segundatela.js
+++ web/portal/js/segundatela.js
@@ -1,3 +1,36 @@
+//yotube API
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//arrays para players multiplos
+var player = new Array();
+var cont = 0;
+var players = new Array();
+var playing = false; 
+function checkState(res){
+  if(res.data==1){
+    playing=players[i].attr("id");
+  }
+}
+function onYouTubeIframeAPIReady() {
+  console.log("start2");
+
+  $('.accordion-body iframe').live("each",function(i){
+    $(this).attr("id","player"+i);
+    players[i] = $("#player"+i);
+  });
+  for(var i=0; i < players.length; i++){
+    player[i] = new YT.Player(players[i].attr("id"));
+    player[i].addEventListener("onStateChange", function(res){
+      if(res.data == 1){
+        var i = res.target.a.id.substring(6,7);
+        playing = player[i];
+      }
+    });
+  }
+} 
+
 (function() {
 
   $(document).ready(function() {
@@ -210,38 +243,6 @@
     
 
   });
-  //yotube API
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-//arrays para players multiplos
-var player = new Array();
-var cont = 0;
-var players = new Array();
-var playing = false; 
-function checkState(res){
-  if(res.data==1){
-    playing=players[i].attr("id");
-  }
-}
-function onYouTubeIframeAPIReady() {
-  console.log("start2");
-
-  $('.accordion-body iframe').live("each",function(i){
-    $(this).attr("id","player"+i);
-    players[i] = $("#player"+i);
-  });
-  for(var i=0; i < players.length; i++){
-    player[i] = new YT.Player(players[i].attr("id"));
-    player[i].addEventListener("onStateChange", function(res){
-      if(res.data == 1){
-        var i = res.target.a.id.substring(6,7);
-        playing = player[i];
-      }
-    });
-  }
-} 
 }).call(this);
 
 
