2a16f0cc6e10c868a054ecf32de881667ab8ae9b
Emerson Estrella <emersonestrella@edse.local>
2013-03-27 18:55:54 -0300

    ss

--- apps/frontend/templates/sites/segundatela/26-03-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-03-2013Success.php
@@ -74,4 +74,16 @@
     </div>
     <!-- /direita -->
   </div>
-
+  
+  <script type="text/javascript" src="http://cmais.com.br/portal/js/segundatela/offline.js?nocache=<?php echo time()?>"></script>
+  <script>
+  // retrive sent contents by ajax
+  $.ajax({
+    url:"/js/segundatela/log/jornaldacultura-2013-03-26.json",
+    success:function(json){
+      $.each(json, function( key, value ) {
+        contentInfo(value);
+      });
+    }
+  });
+  </script>
---
+++ web/portal/js/segundatela/offline.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+  $('#status').fadeIn('slow');
+
+  contentInfo = function(data) {
+    //console.log("<<<<<");
+    var c = 'icon-align-left';
+    if(data.type == 'people')
+      c = 'icon-user';
+    if(data.type == 'place')
+      c = 'icon-map-marker';
+    if(data.type == 'poll')
+      c = 'icon-enquete';
+    var html = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#id'+data.handler+'" rel1="'+data.id+'" rel2="'+data.source+'"><i class="'+c+' icon-white"></i>'+data.tag+'</a></div>';
+    html += '<div id="id'+data.handler+'" class="accordion-body collapse"><div class="accordion-inner">';
+    html += "";
+    html += '</div></div></div>';
+    $('#accordion2').prepend(html);
+    //console.log(data.url);
+    $('#id'+data.handler).load(data.url);
+    return;
+  };  
+  
+  $('#myTab a').click(function(e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+  
+  // colocando e tirando ativo
+  $('.accordion-body').live('hidden', function() {
+    //remove barra ativa
+    $(this).prev().find('a').removeClass('ativo');
+    console.log(playing)
+    if(playing)
+      playing.pauseVideo();
+  });
+  
+  $('.accordion-body').live('shown', function() { 
+    //remove barra ativa
+    $(this).prev().find('a').addClass('ativo');
+    //scroll
+    var el = $(this).parent();
+    $('html, body').animate({
+      scrollTop: el.offset().top
+    }, "fast");
+  });
+  
+  // padding ultimo conteudo
+  $('.accordion-body').each(function() {
+    $(this).find('p:last').css('padding-bottom', '15px');
+  });
+
+});
