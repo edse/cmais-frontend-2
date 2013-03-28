1ceeb6df125b24a589eaf7b2c5e3fd6f1abe2176
doljak <jedoljak@gmail.com>
2013-03-28 18:43:34 -0300

    u

--- apps/frontend/templates/sites/segundatela/jornaldaculturaSuccess.php
+++ apps/frontend/templates/sites/segundatela/jornaldaculturaSuccess.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/jornaldacultura.css" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/jornaldacultura.css?nocache=<?php echo time()?>" type="text/css" />
 <!-- modal-->
 <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
