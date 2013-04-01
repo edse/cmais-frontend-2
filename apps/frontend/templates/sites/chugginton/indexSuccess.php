29887d43e3c443eb0b77f4fffd236eadd38e0bf1
Joseval <joseval@terra.com.br>
2013-04-01 17:56:12 -0300

    up

--- apps/frontend/templates/sites/chugginton/indexSuccess.php
+++ apps/frontend/templates/sites/chugginton/indexSuccess.php
@@ -1,1 +1,13 @@
-Página Inicial
+  <body>
+  <!--ALLWRAPPER-->
+  <div class="allWrapper">
+
+  <?php use_helper('I18N', 'Date') ?>
+  <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+
+    <!--CONTENT WRAPPER-->
+    <div class="contentWrapper" align="center">
+
+      <!--CONTENT-->
+      <div class="content">
+        Página Inicial
