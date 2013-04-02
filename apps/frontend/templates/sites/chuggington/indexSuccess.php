90784c06729c4ef68d6ffbf077b113822e98cf54
Joseval <joseval@terra.com.br>
2013-04-02 17:25:13 -0300

    up

--- apps/frontend/templates/sites/chuggington/indexSuccess.php
+++ apps/frontend/templates/sites/chuggington/indexSuccess.php
@@ -204,6 +204,56 @@
                 <div class="grade toggle" style="display:none; width:100%; padding-bottom:25px;"> </div>
               </li>
             </ul>
+            
+          <?php if(count($pager) > 0): ?>
+              <!-- BOX LISTAO -->
+              <div class="box-listao grid2">
+                <?php if(isset($date)): ?>
+                <h3><?php echo format_date(strtotime($date),"D") ?></h3>
+                <?php endif ?>
+                <ul>
+                  <?php foreach($pager->getResults() as $d): ?>
+                    <li>
+                      <?php if(isset($date)): ?>
+                      <p class="titulos"><?php //echo format_date(strtotime($d->getCreatedAt()),"t") ?></p>
+                      <?php endif ?>
+                      <h3 class="chapeu"><?php // echo $d->retriveLabel() ?></h3>
+                      <a href="<?php echo $d->retriveUrl() ?>" class="titulos"><span class="texto"></span><?php echo $d->title ?></a>
+                      <p><?php echo $d->getDescription() ?></p>
+                      <img src="<?php echo $d->retriveImageUrlByImageUsage('image-4-b'); ?>"/>
+                    </li>
+                  <?php endforeach; ?>
+                </ul>
+              </div>
+              <!-- /BOX LISTAO -->
+            <?php endif; ?>
+               
+              
+            <?php if(isset($pager)): ?>
+              <?php if($pager->haveToPaginate()): ?>
+              <!-- PAGINACAO -->
+              <div class="paginacao pag3 grid2">
+                <p class="txt-12">P&aacute;gina <?php echo $pager->getPage() ?>/<?php echo $pager->getLastPage() ?></p>
+                <a href="javascript: goToPage(<?php echo $pager->getNextPage() ?>);" class="btn proximo"></a>
+                <a href="javascript: goToPage(<?php echo $pager->getPreviousPage() ?>);" class="btn anterior"></a>
+              </div>
+              <form id="page_form" action="" method="post">
+                <input type="hidden" name="return_url" value="<?php echo $url?>" />
+                <input type="hidden" name="page" id="page" value="" />
+              </form>
+              <script>
+                function goToPage(i){
+                  $("#page").val(i);
+                  $("#page_form").submit();
+                }
+              </script>
+              <!-- PAGINACAO -->
+              <?php endif; ?>
+            <?php endif; ?>
+            
+            
+            
+            
             <?php endif; ?>
             
             <!--?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?-->
