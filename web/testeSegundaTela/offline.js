bb6f357729a106a48904a3a1f8e5d6c58e1a3c63
doljak <jedoljak@gmail.com>
2013-04-03 16:33:42 -0300

    u

--- web/testeSegundaTela/offline.js
+++ web/testeSegundaTela/offline.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     $('#accordion2').prepend(html);
     //console.log(data.url);
     $('#id'+data.handler).load(data.url);
-    $('#id'+data.handler+' iframe').each(function(){
+    $('.accordion-body iframe').each(function(){
       console.log("fui");
     })
   };  
