d60703f16e7db0fc614df5ed8bfe7c6ca1ccc9ee
doljak <jedoljak@gmail.com>
2013-04-03 14:53:38 -0300

    u

--- web/testeSegundaTela/offline.js
+++ web/testeSegundaTela/offline.js
@@ -69,12 +69,12 @@ function checkState(res){
 }
 function onYouTubeIframeAPIReady() {
   console.log("start");
-  $("iframe").each(function(i){
-    //if($(this).attr('src').indexOf("youtube") != -1){
+  $(".accordion-body iframe").each(function(i){
+    if($(this).attr('src').indexOf("youtube") != -1){
       cont++;
       $(this).attr("id","player"+cont);
       players_ids[i] = "player"+cont;
-    //}
+    }
   });
   for(var i=0; i < players_ids.length; i++){
     player[i] = new YT.Player(players_ids[i]);
