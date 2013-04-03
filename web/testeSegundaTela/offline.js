aad42f9d267b1e4322d76dbea3a33f31824ca6b8
doljak <jedoljak@gmail.com>
2013-04-03 11:02:07 -0300

    u

--- web/testeSegundaTela/offline.js
+++ web/testeSegundaTela/offline.js
@@ -68,7 +68,7 @@ function checkState(res){
   }
 }
 function onYouTubeIframeAPIReady() {
-  console.log("start");
+  console.log("start b");
   $(".accordion-body iframe").each(function(i){
     if($(this).attr('src').indexOf("youtube") != -1){
       console.log("start each");
