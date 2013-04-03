c04c3065a94dbed6c7cee881d42ce2b0c471c16f
doljak <jedoljak@gmail.com>
2013-04-03 12:28:57 -0300

    u

--- web/testeSegundaTela/offline.js
+++ web/testeSegundaTela/offline.js
@@ -69,7 +69,7 @@ function checkState(res){
 
 function onYouTubeIframeAPIReady(el) {
   console.log(el);
-  $(".accordion-body iframe").each(function(i){
+  $(".accordion-body iframe").live("each", function(i){
     if($(this).attr('src').indexOf("youtube") != -1){
       console.log("start each");
       cont++;
