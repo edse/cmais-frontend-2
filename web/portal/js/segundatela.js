c87b803871004867746c8e4e737b4f929baf3d87
doljak <jedoljak@gmail.com>
2013-04-04 16:27:04 -0300

    u

--- web/portal/js/segundatela.js
+++ web/portal/js/segundatela.js
@@ -1,6 +1,6 @@
 //yotube API
 var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 $(document).ready(function() {
