dd68937ad239eded0126ca4f78a2ab903b3cb3fb
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-05 17:25:35 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
@@ -88,13 +88,12 @@
               </div>
             </div>
             <hr />
-            <span class="picote"></span>
+           
           </div>
-          <!--
-          <span class="alcaA"></span>
-          <span class="alcaB"></span>
-          -->
-          <hr />
+         
+          <div class="ganchos"></div>
+         
+         
           <?php
             $assets = Doctrine_Query::create()
               ->select('a.*')
@@ -109,23 +108,56 @@
           <?php include_partial_from_folder('tvratimbum','global/personagens-carrossel', array('displays' => $assets)) ?>
           <span class="picote"></span>
           <?php endif; ?>
-        </div>
-        <hr />
-            
-        <div class="coluna interna">
-          <?php if(isset($displays["imagens"])) include_partial_from_folder('tvratimbum','global/display-1c-imagens', array('displays' => $displays["imagens"])) ?>        
+        </div>       
           
-          <?php if(isset($displays["videos"])) include_partial_from_folder('tvratimbum','global/display-1c-video', array('displays' => $displays["videos"])) ?>        
-          <hr />
-          <?php if(isset($displays["jogos"])) include_partial_from_folder('tvratimbum','global/display-1c-jogos', array('displays' => $displays["jogos"])) ?>
-        </div>
+		<?php 
+			$assets = $pager->getResults();
+		?>
+                  
+        <div class="assets">
+        <?php if(count($pager) > 0): ?>
         
-        <div class="coluna interna">
-          <?php if(isset($displays["baixar"])) include_partial_from_folder('tvratimbum','global/display-1c-baixar', array('displays' => $displays["baixar"])) ?>
-          <hr />
-          <?php if(isset($displays["atividades"])) include_partial_from_folder('tvratimbum','global/display-1c-atividades', array('displays' => $displays["atividades"])) ?>
+        <?php foreach($pager->getResults() as $d): ?>	
+	       <h3><?php echo $d->getTitle() ?></h3>
+	       <p><?php echo html_entity_decode($d->AssetContent->render()) ?></p>
+        <?php endforeach; ?> 
+         
+         <span class="picote"></span>
         </div>
+       <?php endif; ?>
+       
+        <?php if($pager->haveToPaginate()): ?> 
+
+        <!-- PAGINACAO -->
+        
+        <div class="paginacao-programa">
+          <a class="btn primeira" href="javascript: goToPage(1);" title="Primeira"><span></span>Primeira</a>
+          <div class="paginas">
+            <a class="btn anterior" href="javascript: goToPage(<?php echo $pager->getPreviousPage() ?>);" title="Anterior"></a>
+            <p><span><?php echo $pager->getPage() ?></span> de <span><?php echo $pager->getLastPage() ?></span></p>
+            <a class="btn proxima" href="javascript: goToPage(<?php echo $pager->getNextPage() ?>);" title="Próxima"></a>
+          </div>
+          <a class="btn ultima" href="javascript: goToPage(<?php echo $pager->getLastPage() ?>);" title="Última"><span></span>Última</a>
+        </div>    
+        
+         <form id="page_form" action="" method="post">
+		    <input type="hidden" name="return_url" value="<?php echo $url?>" />
+		    <input type="hidden" name="page" id="page" value="" />
+		    <input type="hidden" name="term" id="term" value="<?php echo $term ?>" />
+		    <input type="hidden" name="filter" id="filter" value="<?php echo $filter ?>" />
+		 </form>
+  
+  <script>
+     function goToPage(i){
+       $("#page").val(i);
+       $("#page_form").submit();
+     }
+  </script>
+     
+       <?php endif; ?>
       
+      <!-- /PAGINACAO -->
+       
       </div>
       
       <div class="coluna">
@@ -137,9 +169,72 @@
           <?php include_partial_from_folder('tvratimbum','global/important') ?>
           <span class="picote"></span>
         </div>
-        <hr />
+        
+        <ul class="menu-secao">
+          <li class="videos"><a href="http://tvratimbum.cmais.com.br/videos" title="Vídeos" >Vídeos</a></li>
+          <li class="jogos"><a href="http://tvratimbum.cmais.com.br/jogos" title="Jogos">Jogos</a></li>
+          <li class="atividades"><a href="http://tvratimbum.cmais.com.br/atividades" title="Atividades">Atividades</a></li>
+          <li class="imagens"><a href="http://tvratimbum.cmais.com.br/imagens" title="Imagens">Imagens</a></li>
+          <li class="baixar"><a href="http://tvratimbum.cmais.com.br/baixar" title="Baixar">Baixar</a></li>
+          <li class="especial"><a href="http://tvratimbum.cmais.com.br/especial" title="Especial">Especial</a></li>
+        </ul>
+        
+        <?php if(isset($displays['concurso-cultural'])):?> 
+        <?php if(count($displays['concurso-cultural']) > 0): ?>
+        <div class="assets" id="concurso-cultural">
+          <h3><?php echo $displays['concurso-cultural'][0]->Asset->getTitle() ?></h3>
+          <p><?php echo $displays['concurso-cultural'][0]->Asset->getDescription() ?></p>
+          <!--MSG ACERTO-->
+            <div style="display:none" class="msgAcerto">
+              <p>Mensagem enviada com sucesso</p>
+            </div>  
+            <!--/MSG ACERTO-->
+            <!--MSG ERRO-->
+            <div style="display:none" class="msgErro">
+              <p>Preencha os campos abaixo.</p>
+            </div> 
+            <!--/MSG ERRO-->
+          <form id="form-contato" method="post" action="">
+            <label for="nome_crianca">Nome da criança</label>
+            <input type="text" name="nome_crianca" id="nome_crianca" />
+            
+            <label for="nome_resp">Nome dos pais ou responsável</label>
+            <input type="text" name="nome_resp" id="nome_resp" />
+            <div class="cidade">
+                <label for="cidade">Cidade</label>
+                <input type="text" name="cidade" id="cidade"/>
+            </div>
+            <div class="estado">
+              <label for="estado">Estado</label>
+              <input type="text" name="estado" id="estado" />
+            </div>
+            <label for="email" style="float:left;">Email</label>
+            <input type="text" name="email" id="email" />
+            
+            <label for="resposta">Resposta</label>
+            <textarea id="resposta" name="resposta" onKeyDown="limitText(this,400,'#textCounter');"></textarea>
+            <p class="txt-10"><span id="textCounter">400</span> caracteres restantes</p>
+            
+            <div class="termos">
+              <p class="bold">TERMOS E CONDIÇÕES</p>
+              <p><?php echo html_entity_decode($displays['concurso-cultural'][0]->Asset->AssetContent->render()) ?></p>
+            </div>
+            
+            <label for="termos"><input type="radio" class="radio" id="termos" name="termos" />Declaro que li e estou de acordo comos <a href="#">Termos e Condições</a>.</label>
+            
+            <input type="submit" value="enviar" id="enviar" class="btn-enviar"/>
+            <img src="/portal/images/ajax-loader.gif" alt="enviando..." style="display:none" width="16px" height="16px" id="ajax-loader" />
+             
+          </form>
+       
+          
+        </div>  
+           <?php endif; ?>
+        <?php endif; ?> 
+        
+        
         <?php if(isset($displays["voce-sabia"])) include_partial_from_folder('tvratimbum','global/display-1c-vocesabia', array('displays' => $displays["voce-sabia"])) ?>
-        <hr />
+        
         <?php /*
         <hr />
         <?php include_partial_from_folder('tvratimbum','global/display-1c-recadinhos', array('site_id' => $site->getId())) ?>
@@ -151,3 +246,87 @@
     <hr />
   </div>
 </div>
+
+<script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('input#enviar').click(function() {
+      $(".msgAcerto, .msgErro").hide();
+    });
+    var validator = $('#form-contato').validate({
+      submitHandler : function(form) {
+        $.ajax({
+          type : "POST",
+          dataType : "text",
+          data : $("#form-contato").serialize(),
+          beforeSend : function() {
+            $('input#enviar').attr('disabled', 'disabled');
+            $(".msgAcerto").hide();
+            $(".msgErro").hide();
+            $('img#ajax-loader').show();
+          },
+          success : function(data) {
+            $('input#enviar').removeAttr('disabled');
+            window.location.href = "#";
+            if(data == "1") {
+              $("#form-contato").clearForm();
+              $(".msgAcerto").show();
+              $('img#ajax-loader').hide();
+            } else {
+              $(".msgErro").show();
+              $('img#ajax-loader').hide();
+            }
+          }
+        });
+      },
+      rules : {
+        nome_crianca : {
+          required : true,
+          minlength : 2
+        },
+        nome_resp : {
+          required : true,
+          minlength : 2
+        },
+        email : {
+          required : true,
+          email : true
+        },
+        cidade : {
+          required : true,
+          minlength : 3
+        },
+        estado : {
+          required : true,
+          minlength : 2
+        },
+        termos : {
+          required : true
+        },
+        resposta : {
+          required : true
+        }
+      },
+      messages : {
+        nome_crianca : "Digite um nome v&aacute;lido. Este campo &eacute; obrigat&oacute;rio.",
+        nome_resp : "Digite um nome v&aacute;lido. Este campo &eacute; obrigat&oacute;rio.",
+        email : "Digite um e-mail v&aacute;lido. Este campo &eacute; obrigat&oacute;rio.",
+        cidade : "Este campo &eacute; obrigat&oacute;rio.",
+        estado : "Este campo &eacute; obrigat&oacute;rio.",
+        termos : "Este campo &eacute; obrigat&oacute;rio.",
+        resposta : "Este campo &eacute; obrigat&oacute;rio."
+      },
+      success : function(label) {
+        // set &nbsp; as text for IE
+        label.html("&nbsp;").addClass("checked");
+      }
+    });
+  });
+  // Contador de Caracters
+  function limitText(limitField, limitNum, textCounter) {
+    if(limitField.value.length > limitNum)
+      limitField.value = limitField.value.substring(0, limitNum);
+    else
+      $(textCounter).html(limitNum - limitField.value.length);
+  }
+</script>
