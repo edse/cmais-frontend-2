18cba4f72b2ff52f1f8f40d11cc579a71fbb8227
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-08 15:15:42 -0300

    up

--- apps/frontend/templates/sites/preestreia/conjunto-2013Success.php
+++ apps/frontend/templates/sites/preestreia/conjunto-2013Success.php
@@ -341,7 +341,13 @@
                   <input type="text" name="regular_1" id="regular_1" />
                 </div>
                 <!--/cursa-1-->
-
+				
+			   <!--Anexar RG--->
+               <div class="linha t5">
+               	<label>Anexar cópia do RG (se for menor de idade, anexar o RG do responsável):</label>
+               	<input type="file" name="new_photo" id="anexofoto" />
+               </div>
+               <!--/Anexar RG--->
               
                 
                 <p class="linha t7 titulo">Para candidatos menores de 18 anos favor preencher:</p>
@@ -385,6 +391,13 @@
                 <div class="linha t5">
                   <label>Nome integrante</label>
                   <input type="text" name="nome_2" id="nome_2" class="required" />
+                </div>
+                <!--/Nome-2-->
+                
+                <!--Nome-2-->
+                <div class="linha t4">
+                  <label>Nome artístico</label><br>
+                  <input type="text" name="nomeartistico_2" id="nomeartistico_2" class="required" />
                 </div>
                 <!--/Nome-2-->
                 
@@ -543,6 +556,13 @@
                   <input type="text" name="regular_2" id="regular_2" />
                 </div>
                 <!--/cursa-2-->
+                
+                  <!--Anexar RG--->
+               <div class="linha t5">
+               <label>Anexar cópia do RG (se for menor de idade, anexar o RG do responsável):</label>
+               <input type="file" name="new_photo" id="anexofoto" />
+               </div>
+               <!--/Anexar RG--->
 
               
                 
@@ -581,6 +601,13 @@
                 <div class="linha t5">
                   <label>Nome integrante</label>
                   <input type="text" name="nome_3" id="nome_3" class="required" />
+                </div>
+                <!--/Nome-3-->
+                
+                <!--Nome-3-->
+                <div class="linha t4">
+                  <label>Nome artístico</label><br>
+                  <input type="text" name="nomeartistico_3" id="nomeartistico_3" class="required" />
                 </div>
                 <!--/Nome-3-->
                 
@@ -744,7 +771,7 @@
                                     
                <!--Anexar RG--->
                <div class="linha t5">
-               <label>Anexar cópia do RG (se for menor de idade, anexar o RG do responsável) de cada integrante:</label>
+               <label>Anexar cópia do RG (se for menor de idade, anexar o RG do responsável):</label>
                <input type="file" name="new_photo" id="anexofoto" />
                </div>
                <!--/Anexar RG--->
@@ -817,6 +844,13 @@
                     new_field += '<div class="linha t5">';
                     new_field += '  <label>Nome integrante:</label>';
                     new_field += '  <input type="text" name="nome_'+i+'" id="nome_'+i+'" class="required" />';
+                    new_field += '</div>';
+                    new_field += '<!--/Nome-'+i+'-->';
+                    
+                    new_field += '<!--Nome Artistico-'+i+'-->';
+                    new_field += '<div class="linha t5">';
+                    new_field += '  <label>Nome artístico</label>';
+                    new_field += '  <input type="text" name="nomeartistico_'+i+'" id="nome_'+i+'" class="required" />';
                     new_field += '</div>';
                     new_field += '<!--/Nome-'+i+'-->';
                       
