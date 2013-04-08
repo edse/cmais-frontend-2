3e903cf97efdf3255b414b8c8f1dc2c74d4b799b
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-08 15:23:07 -0300

    up

--- apps/frontend/templates/sites/preestreia/conjunto-2013Success.php
+++ apps/frontend/templates/sites/preestreia/conjunto-2013Success.php
@@ -1285,7 +1285,7 @@
               
               <!--Sugestões Final-->
               <div class="linha t7">
-                <label>Indique abaixo 4 opções de obras contrastantes a serem executadas na final do concurso. Essas obras devem ter duração mínima de 7 minutos e máxima de 10 minutos, e serão apresentadas com acompanhamento de orquestra. É muito importante preencher os dados corretamente, conforme o exemplo.</label>
+                <label>Indique abaixo 4 opções de obras contrastantes a serem executadas na final do concurso. Essas obras devem ter duração mínima de 7 minutos e máxima de 10 minutos, e serão apresentadas  acompanhamento de orquestra. É muito importante preencher os dados corretamente, conforme o exemplo.</label>
               </div>
               
               <a class="t7 titulo laranja" >9ª Opção</a>
