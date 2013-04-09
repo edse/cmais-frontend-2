ed6f590803ef522f1b1575dec6e3e374df921ea3
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-09 16:19:06 -0300

    up

--- apps/frontend/templates/sites/preestreia/solista-2013Success.php
+++ apps/frontend/templates/sites/preestreia/solista-2013Success.php
@@ -1630,7 +1630,7 @@ $(document).ready(function(){
   	//alert("teste")
   	//}
   	$("#opcao_correspondente1, #opcao_correspondente2, #opcao_correspondente3").change(function(){
-  		console.log($($(this).attr("id"))
+  		console.log($(this).attr("id"))
   		
   	});
   		
