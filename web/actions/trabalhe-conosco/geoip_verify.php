126c4e1ecd99dd9e8fef9d35e031a1ae5c515958
Joseval <joseval@terra.com.br>
2013-04-09 12:37:50 -0300

    up

--- web/actions/trabalhe-conosco/geoip_verify.php
+++ web/actions/trabalhe-conosco/geoip_verify.php
@@ -4,12 +4,12 @@
      if($block){
           //error_reporting(E_ALL);
           //ini_set('display_errors', '1');
-          require_once sfConfig::get('sf_lib_dir').'/vendor/geoip/geoip.inc';
-          $gi = geoip_open(sfConfig::get('sf_lib_dir').'/vendor/geoip/GeoIP.dat',GEOIP_STANDARD);
-          if(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']) != "BR"){
+          //require_once sfConfig::get('sf_lib_dir').'/vendor/geoip/geoip.inc';
+          //$gi = geoip_open(sfConfig::get('sf_lib_dir').'/vendor/geoip/GeoIP.dat',GEOIP_STANDARD);
+          //if(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']) != "BR"){
             $block = true;
-          }
-          geoip_close($gi);
+          //}
+          //geoip_close($gi);
         }
        
         if($block == true){
