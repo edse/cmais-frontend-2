36f2d673cfc5bacffdcf7dd2b018a02aa3d36392
Joseval <joseval@terra.com.br>
2013-04-09 16:07:55 -0300

    up

--- web/actions/trabalhe-conosco/geoip_verify.php
+++ web/actions/trabalhe-conosco/geoip_verify.php
@@ -5,8 +5,8 @@
         $myip = "66.249.76.32";
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
-        require_once sfConfig::get('sf_lib_dir').'/vendor/geoip/geoip.inc';
-        $gi = geoip_open(sfConfig::get('sf_lib_dir').'/vendor/geoip/GeoIP.dat',GEOIP_STANDARD);
+        require_once '/lib/vendor/geoip/geoip.inc';
+        $gi = geoip_open('/lib/vendor/geoip/GeoIP.dat',GEOIP_STANDARD);
         //if(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']) != "BR"){
           $country = geoip_country_code_by_addr($gi, $myip);
           echo 'Country: '.$country;
