62678175c05168432c424aed2f2f7a4e64a8bcf2
Joseval <joseval@terra.com.br>
2013-04-09 13:08:13 -0300

    teste

--- web/actions/trabalhe-conosco/geoip_verify.php
+++ web/actions/trabalhe-conosco/geoip_verify.php
@@ -2,11 +2,13 @@
 
      $block = false;
      if($block){
+        $myip = "178.88.173.162";
           //error_reporting(E_ALL);
           //ini_set('display_errors', '1');
           require_once sfConfig::get('sf_lib_dir').'/vendor/geoip/geoip.inc';
           $gi = geoip_open(sfConfig::get('sf_lib_dir').'/vendor/geoip/GeoIP.dat',GEOIP_STANDARD);
-          if(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']) != "BR"){
+          //if(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']) != "BR"){
+            if(geoip_country_code_by_addr($gi, $myip) != "BR"){
             $block = true;
           }
           geoip_close($gi);
