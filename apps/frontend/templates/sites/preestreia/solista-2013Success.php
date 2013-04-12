0293778244cf2acf92bbbeab497ead27ea50e06e
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-12 18:32:09 -0300

    up

--- apps/frontend/templates/sites/preestreia/solista-2013Success.php
+++ apps/frontend/templates/sites/preestreia/solista-2013Success.php
@@ -789,7 +789,7 @@
               <div class="linha t7">
                 
                 <label>Anexar RG (se for menor de idade anexar o RG do responsável)(Formato JPG):</label>
-                <input type="file" name="new_photo" id="anexofoto" />
+                <input type="file" name="new_photo1" id="anexofoto1" />
                 
               </div>
               <!--/Anexar Foto-->
@@ -798,7 +798,7 @@
               <div class="linha t7">
                 
                 <label>Anexar foto(max 5mb)(Formato JPG):</label>
-                <input type="file" name="new_photo" id="anexofoto" />
+                <input type="file" name="new_photo2" id="anexofoto2" />
                 
               </div>
               <!--/Anexar Foto-->
