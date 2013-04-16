cc51333df796050fdffd894281a2ca2ed4025ad4
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-04-16 18:22:08 -0300

    u

--- apps/frontend/templates/sites/qss/inscricaoSuccess.php
+++ apps/frontend/templates/sites/qss/inscricaoSuccess.php
@@ -17,13 +17,46 @@
     </div>
     <!-- /curtir -->
     <form id="form-contato" method="post" action="">
-      <p>Cadastre o seu e-mail para ser informado sobre a data de início das inscrições:</p>
+      <p>Nome completo:</p>
       <input type="text" name="nome" id="nome" class="required" />
+
+      <p>Idade:</p>
       <input type="text" name="idade" id="idade" class="required" />
+
+      <p>Endereço:</p>
       <input type="text" name="endereco" id="endereco" class="required" />
-      <input type="text" name="bairro" id="bairro" class="required" /> 
+
+      <p>Bairro:</p>
+      <input type="text" name="bairro" id="bairro" class="required" />
+      
+      <p>CEP:</p>
       <input type="text" name="cep" id="cep" class="required" />
       
+      <p>Cidade:</p>
+      <input type="text" name="cidade" id="cidade" class="required" />
+      
+      <p>RG:</p>
+      <input type="text" name="rg" id="rg" class="required" />
+      
+      <p>CPF:</p>
+      <input type="text" name="cpf" id="cpf" class="required" />
+      
+      <p>Data de nascimento:</p>
+      <input type="text" name="nascimento" id="nascimento" class="required" />
+
+      <p>Telefone para contato:</p>
+      <input type="text" name="telefone" id="telefone" class="required" />
+
+      <p>Nome da mãe:</p>
+      <input type="text" name="nomemae" id="nomemae" class="required" />
+
+      <p>Profissão:</p>
+      <input type="text" name="profissao" id="profissao" class="required" />
+
+      <p>Grau de escolaridade:</p>
+      <input type="text" name="escolaridade" id="escolaridade" class="required" />
+      
+            
       <input class="enviar" type="submit" name="enviar" id="enviar" value="ENVIAR" style="cursor:pointer" />
       <img src="/portal/images/ajax-loader.gif" alt="enviando..." style="display:none" width="16px" height="16px" id="ajax-loader" />
       
