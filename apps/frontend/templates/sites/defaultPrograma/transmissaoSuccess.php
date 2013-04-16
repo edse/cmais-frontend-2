80884af606b3a2633ccb0fa9b69db8055c72bc89
Rafael Cruz <rafael@rafael-cruz.(none)>
2013-04-16 17:48:52 -0300

    up

--- apps/frontend/templates/sites/defaultPrograma/transmissaoSuccess.php
+++ apps/frontend/templates/sites/defaultPrograma/transmissaoSuccess.php
@@ -120,7 +120,7 @@
           </div>
           <?php endif; ?>
           <?php endif; ?>
-          <p>
+          <p></p>
           <!-- BOX PUBLICIDADE -->
           <div class="box-publicidade grid1">
             <!-- programas-homepage-300x250 -->
