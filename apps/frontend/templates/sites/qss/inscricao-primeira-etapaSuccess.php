e27533253a77a5e0dee10ef073e38945902bca21
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-04-16 21:06:01 -0300

    u

--- apps/frontend/templates/sites/qss/inscricao-primeira-etapaSuccess.php
+++ apps/frontend/templates/sites/qss/inscricao-primeira-etapaSuccess.php
@@ -17,7 +17,6 @@
       <!--<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php if($site->getTwitterAccount()): ?><?php echo $site->getTwitterAccount() ?><?php else: ?>tvcultura<?php endif; ?>">Tweet</a>-->
     </div>
     <!-- /curtir -->
-    <form id="form-contato" method="post" action="">
       <p>Faça um vídeo de no máximo 1 minuto, contando um pouco de você.</p>
       
       <div id="youtube">
@@ -47,56 +46,6 @@
         </div> 
       </div>      
             
-      <input class="enviar" type="submit" name="enviar" id="enviar" value="ENVIAR" style="cursor:pointer" />
-      <img src="/portal/images/ajax-loader.gif" alt="enviando..." style="display:none" width="16px" height="16px" id="ajax-loader" />
       
-    </form>  
 </div> 
 <!-- /capa site-->
-<script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
-<script type="text/javascript">
-  $(document).ready(function(){
-    var validator = $('#form-contato').validate({
-      submitHandler: function(form){
-        $.ajax({
-          type: "POST",
-          dataType: "text",
-          data: $("#form-contato").serialize(),
-          beforeSend: function(){
-            $('img#ajax-loader').show();
-          },
-          success: function(data){
-          $('input#enviar').removeAttr('disabled');
-            window.location.href="#";
-            if(data == "1"){
-              alert("Seu cadastro foi enviado com sucesso!");
-               $('img#ajax-loader').hide();
-               $('#form-contato').hide();
-            }
-            else {
-              alert("Sua mensagem não pode ser enviada. Tente novamente.");
-               $('img#ajax-loader').hide();
-               $("#email").val(" ");
-               
-            }
-          }
-        });         
-      },
-      rules:{
-        email:{
-          required:true,
-          email: true
-        }
-      },
-      messages:{
-        email: "Digite um e-mail válido.",
- 
-      },
-      success: function(label){
-        // set &nbsp; as text for IE
-        label.html("&nbsp;").addClass("checked");
-      }
-    });
-  });
-</script>
- 
