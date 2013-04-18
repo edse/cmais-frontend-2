0087337fbb5bbb6bb8a2f3b7e343e909e6f1872f
doljak <jedoljak@gmail.com>
2013-04-18 12:10:31 -0300

    u

--- apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
@@ -65,7 +65,7 @@
               }
             ?>
             
-        <a class="convidado span12" href="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->Asset->retriveUrl() ?>" title="Próximo convidado: <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>" />
+        <a class="convidado span12" href="http://tvcultura.cmais.com.br/cocorico/convidados/<?php echo $displays['destaque-tv-cocorico'][0]->getSlug() ?>" title="Próximo convidado: <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>" />
           <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>
         </a>
         <a href="<?php echo $site->retriveUrl(); ?>/convidados" title="Convidados">
