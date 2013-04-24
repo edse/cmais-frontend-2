79a3b60556367b953713bf6fd76aee9241d03a74
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-04-24 19:42:56 -0300

    u

---
+++ apps/frontend/templates/sites/cmais/assets/segunda-etapa-melhor-gestao-melhor-ensinoSuccess.php
@@ -0,0 +1,136 @@
+<link rel="stylesheet" href="/portal/css/tvcultura/secoes/programaBlog.css" type="text/css" />
+<script type="text/javascript">
+$(function(){
+  //hover states on the static widgets
+  $('#dialog_link, ul#icons li').hover(
+    function() { $(this).addClass('ui-state-hover'); }, 
+    function() { $(this).removeClass('ui-state-hover'); }
+  );
+  $('.comentario-fb').show();
+});
+</script> 
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+
+    <!-- CAPA SITE -->
+    <div id="capa-site">
+
+      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
+
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+        <?php if(isset($program) && $program->id > 0): ?>
+        <div class="topo-programa">
+          <!--
+          <h2>
+            <a href="<?php echo $program->retriveUrl() ?>">
+              <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
+            </a>
+          </h2>
+          -->
+          <?php endif; ?>
+          
+          <?php if(isset($program) && $program->id > 0): ?>
+            <!-- horario -->
+            <div id="horario">
+              <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
+            </div>
+            <!-- /horario -->
+        </div>
+        <?php endif; ?>
+
+        <?php if(isset($siteSections) && $site->getType() != "Portal"): ?>
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+          
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+          <?php if(isset($section)): ?>
+            <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
+            <div class="navegacao txt-10">
+              <a href="<?php echo $site->retriveUrl() ?>" title="Home">Home</a>
+              <span>&gt;</span>
+              <a href="<?php echo $asset->retriveUrl()?>" title="<?php echo $asset->getTitle()?>"><?php echo $asset->getTitle()?></a>
+            </div>
+            <?php endif; ?>
+          <?php endif; ?>
+
+        </div>
+        <!-- /box-topo -->
+        <?php endif; ?>
+
+      </div>
+      <!-- /BARRA SITE -->
+
+      <!-- MIOLO -->
+      <div id="miolo">
+      
+        <!-- BOX LATERAL -->
+        <?php include_partial_from_folder('blocks','global/shortcuts') ?>
+        <!-- BOX LATERAL -->
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+
+          <!-- CAPA -->
+          <div class="capa grid3">
+
+            <!-- ESQUERDA -->
+            <div id="esquerda" class="grid2">
+
+              <!-- NOTICIA INTERNA -->
+              <div class="box-interna grid2">
+                <h3><?php echo $asset->getTitle() ?></h3>
+                <p><?php echo $asset->getDescription() ?></p>
+                <div class="texto">
+                  <?php echo html_entity_decode($asset->AssetContent->render()) ?>
+                </div>
+                
+                <?php $relacionados = $asset->retriveRelatedAssetsByRelationType('Asset Relacionado'); ?>
+                <?php if(count($relacionados) > 0): ?>
+                  <!-- SAIBA MAIS -->
+                  <div class="box-padrao grid2" style="margin-bottom: 20px;">
+                    <div id="saibamais">                                                            
+                    <h4>saiba +</h4>                                                            
+                    <ul class="conteudo">
+                      <?php foreach($relacionados as $k=>$d): ?>
+                        <li style="width: auto;">
+                          <a class="titulos" href="<?php echo $d->retriveUrl()?>" style="width: auto;"><?php echo $d->getTitle()?></a>
+                          <!--
+                          <?php if($d->retriveImageUrlByImageUsage("image-1") != ""): ?>
+                            <a href="<?php echo $d->retriveUrl()?>" class="img-90x54" style="width: auto">
+                              <img src="<?php echo $d->retriveImageUrlByImageUsage("image-1-b") ?>" alt="<?php echo $d->getTitle() ?>" title="<?php echo $d->getTitle() ?>" style="width: auto" />
+                            </a>
+                          <?php endif; ?>
+                          -->
+                          <!--p><?php echo $d->getDescription()?></p-->
+                        </li>
+                      <?php endforeach; ?>
+                    </ul>
+                   </div>
+                  </div>
+                  <!-- SAIBA MAIS -->
+                <?php endif; ?>
+
+              </div>
+              <!-- /NOTICIA INTERNA -->
+              
+            </div>
+            <!-- /ESQUERDA -->
+            
+            <!-- DIREITA -->
+            <div id="direita" class="grid1">
+            </div>
+            <!-- /DIREITA -->
+          </div>
+          <!-- /CAPA -->
+        </div>
+        <!-- /CONTEUDO PAGINA -->
+
+      </div>
+      <!-- /MIOLO -->
+    </div>
+    <!-- / CAPA SITE -->
+    
+
