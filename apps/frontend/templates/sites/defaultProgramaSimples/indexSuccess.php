5ff0dfabeee5a53e57d710999c8b579db6bdb2ba
rafaelhcs <rhcsousa@gmail.com>
2013-05-06 19:56:54 -0300

    up

--- apps/frontend/templates/sites/defaultProgramaSimples/indexSuccess.php
+++ apps/frontend/templates/sites/defaultProgramaSimples/indexSuccess.php
@@ -43,7 +43,7 @@
 
   function dateLoading(date) { 
     var year_month = ""+ (date.getFullYear()) +"-"+ (date.getMonth()+1) +"";
-    <?php if((format_datetime($program->getDateStart(), "HH:mm") > "04:59") || (format_datetime($program->getDateStart(), "HH:mm") < "00:01")): ?>
+    <?php if(($program->getTimeStart() > "04:59") || ($program->getTimeStart() < "00:01")): ?>
     var year_month_day = ""+ year_month+"-"+ (date.getDate()-1)+"";
     <?php else: ?>
     var year_month_day = ""+ year_month+"-"+ date.getDate()+"";
