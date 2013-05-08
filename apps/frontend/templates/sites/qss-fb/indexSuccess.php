c2e40d4c3fd327eff49b04d309737604872a1795
doljak <jedoljak@gmail.com>
2013-05-08 19:41:06 -0300

    u

--- apps/frontend/templates/sites/qss-fb/indexSuccess.php
+++ apps/frontend/templates/sites/qss-fb/indexSuccess.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/qss.css" type="text/css" />
 <style type="text/css">
-#page{width:300px!important;}
 #capa-site { max-width: 780px; min-height:530px; height:1600px; overflow:scroll;}
 </style>
 <?php use_helper('I18N', 'Date') ?>
