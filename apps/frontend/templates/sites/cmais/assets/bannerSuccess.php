1ed4e6fa899855696cf79e6ea607c894c2b78948
rafaelhcs <rhcsousa@gmail.com>
2013-05-10 16:10:30 -0300

    up

--- apps/frontend/templates/sites/cmais/assets/bannerSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/bannerSuccess.php
@@ -1,9 +1,9 @@
-<?php
+<script type='text/javascript'>
 function isMobile() {
 return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|
 palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
-?>
+</script>
 
 <?php if(isMobile()): ?>
 <!-- Ipad-300x250 -->
