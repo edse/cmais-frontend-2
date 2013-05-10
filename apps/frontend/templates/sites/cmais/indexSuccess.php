8094e5911858c1d4a726e2f04d2cf831d066a05c
rafaelhcs <rhcsousa@gmail.com>
2013-05-10 16:41:34 -0300

    up

--- apps/frontend/templates/sites/cmais/indexSuccess.php
+++ apps/frontend/templates/sites/cmais/indexSuccess.php
@@ -113,9 +113,42 @@
               <!-- BOX PUBLICIDADE -->
               <div class="box-publicidade grid1">
                  <!-- home-geral300x250 -->
-				<script type='text/javascript'>
-				GA_googleFillSlot("home-geral300x250");
-				</script>
+                 <?php 
+	function detectMobile() {
+	$devices = array('android' => 'android', 'blackberry' => 'blackberry', 'iphone' => '(iphone|ipod|ipad)', 'opera' => '(opera mini|opera mobi)', 'palm' => '(avantgo|blazer|elaine|hiptop|palm|plucker|xiino)', 'windows' => 'windows ce; (iemobile|ppc|smartphone)', 'generic' => '(kindle|mobile|mmp|midp|o2|pda|pocket|psp|symbian|smartphone|treo|up.browser|up.link|vodafone|wap)');
+ 
+	$useragent = strtolower($_SERVER['HTTP_USER_AGENT']);
+	$accept = strtolower($_SERVER['HTTP_ACCEPT']);
+	$mobile = false;
+ 
+	if (isset($_SERVER['HTTP_X_WAP_PROFILE']) || isset($_SERVER['HTTP_PROFILE']) || strpos($accept, "application/vnd.wap.xhtml+xml") > 0 || strpos($accept, "text/vnd.wap.wml") > 0) {
+			$mobile = "WAP";
+	} else {
+		foreach ($devices as $device => $keys) {
+			if(preg_match("/$keys/i", $useragent)) {
+				$mobile = $device;
+			}
+		}
+	}
+ 
+	return $mobile;
+}
+ 
+if(detectMobile()) {
+?>
+<script type='text/javascript'>
+GA_googleFillSlot("Ipad-300x250");
+</script>
+<?php
+} else {
+?>
+	<script type='text/javascript'>
+	GA_googleFillSlot("home-geral300x250");
+	</script>
+<?php	
+}
+?>
+			
               </div>
               <!-- / BOX PUBLICIDADE -->
               
