528a0180ccd905503a8ec247025ace7a3484c7b4
renatareno <asdf@fpawks0142.local>
2013-05-13 14:29:58 -0300

    qss

--- apps/frontend/templates/sites/qss2/inscricaoSuccess.php
+++ apps/frontend/templates/sites/qss2/inscricaoSuccess.php
@@ -110,7 +110,7 @@ $(function(){
                 <div id="msgSuccess" style="display:none">
                   <p>Inscrição realizada com sucesso!</p>
                   <p>&nbsp;</p>
-                  <p class="bg">Atenção:No dia do programa o participante deverá, obrigatoriamente, trazer um documento original com foto.</p>
+                  <p class="bg">Atenção: No dia do programa o participante deverá, obrigatoriamente, trazer um documento original com foto.</p>
                   <p class="bg">No caso de impossibilidade da participação do amigo, a produção do programa deverá ser avisada.</p>
                 </div>
                 <div id="msgError" style="display:none">
