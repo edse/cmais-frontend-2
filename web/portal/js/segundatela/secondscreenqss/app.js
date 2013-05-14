4cb953d3ecff663f43b3eb9b4ac29eda4fc0a87f
doljak <jedoljak@gmail.com>
2013-05-14 17:19:22 -0300

    u

--- web/portal/js/segundatela/secondscreenqss/app.js
+++ web/portal/js/segundatela/secondscreenqss/app.js
@@ -240,6 +240,7 @@ $(document).ready(function() {
       if(!$(this).parent().hasClass('disabled')){
         $(this).parent().parent().find('li').each(function(index){
           $(this).css("background","#ccc");
+          $(this).die('click');
         });
         
         //$(this).removeClass('btn-primary').addClass('btn-warning');
