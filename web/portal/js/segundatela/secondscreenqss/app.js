f444fd3b73b434744aac8a6026bb2dead4fce536
doljak <jedoljak@gmail.com>
2013-05-14 15:12:22 -0300

    u

--- web/portal/js/segundatela/secondscreenqss/app.js
+++ web/portal/js/segundatela/secondscreenqss/app.js
@@ -188,7 +188,7 @@ $(document).ready(function() {
     html +=   '<!--resposta-->'
     html +=   '<div id="uid'+data.uid+'" class="accordion-body collapse">'
     html +=     '<div class="accordion-inner">' 
-    if(!clock){
+    if(clock){
       //console.log(data)
       html +=      '<ul class="answers">'
       
