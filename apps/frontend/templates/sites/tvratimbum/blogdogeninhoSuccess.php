f3ad33b581e8097969b8ae2219a473bf41240296
rafaelhcs <rhcsousa@gmail.com>
2013-05-15 18:33:30 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/blogdogeninhoSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/blogdogeninhoSuccess.php
@@ -1,9 +1,5 @@
-<?php
-  $assets = Doctrine_Query::create()
-    ->select('a.*')
-    ->from('Asset a, SectionAsset sa')
-    ->where('sa.asset_id = a.id')
-    ->execute();
+<?php 
+$assets = $pager->getResults();
 ?>
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
