af2c316432b9077c40a32aa4cd36e9ba151bdb9c
rafaelhcs <rhcsousa@gmail.com>
2013-05-15 18:55:25 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/blogdogeninhoSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/blogdogeninhoSuccess.php
@@ -1,6 +1,4 @@
-<?php 
-$assets = $pager->getResults();
-?>
+
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 <link href="/portal/tvratimbum/css/geral.css?nocache=123" type="text/css" rel="stylesheet">
