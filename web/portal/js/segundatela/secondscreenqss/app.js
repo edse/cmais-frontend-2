129967e2b8918274ed47cc61c097e3ca631393a3
doljak <jedoljak@gmail.com>
2013-05-17 16:42:32 -0300

    u

--- web/portal/js/segundatela/secondscreenqss/app.js
+++ web/portal/js/segundatela/secondscreenqss/app.js
@@ -445,7 +445,7 @@ $(document).ready(function() {
           scrollTop: el.offset().top-30
       }, "fast");
     }
-    $(this).parent().next().find('.accordion-body').collapse('toggle');
+    $(this).parent().next().find('.accordion-body:first').collapse('toggle');
    
   });
   
