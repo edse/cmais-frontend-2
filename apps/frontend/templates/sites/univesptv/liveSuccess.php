08c83de1ea4f47e209cb4a093476011a004cd900
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-17 16:23:27 -0300

    u

--- apps/frontend/templates/sites/univesptv/liveSuccess.php
+++ apps/frontend/templates/sites/univesptv/liveSuccess.php
@@ -1,11 +1,20 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/aovivo.css" type="text/css" />
 <link type="text/css" href="/portal/univesptv/css/geral.css" rel="stylesheet" /> 
 <script type="text/javascript" src="/portal/js/mediaplayer/swfobject.js"></script>  
+<script type="text/javascript" src="/portal/js/bootstrap/bootstrap-tab.js"></script>  
 <style>
 #direita { margin-top:0;}
 #esquerda .stream {float: left; margin-top: 5px;}
 #esquerda .stream a {background: none repeat scroll 0 0 #FF6633; clear: none;color: #FFFFFF; float: left;margin-right: 5px; padding: 0 3px; width: auto; }
 #esquerda .stream a.ativo, #esquerda .stream a:hover { background: none repeat scroll 0 0 #333333;text-decoration: none;}
+
+.nav-tabs > li { margin-bottom:0; }
+.nav-tabs { background:#666; border:none; border-left: 1px dotted #fff;  }
+.nav-tabs > li { border-right: 1px dotted #fff; }
+.nav-tabs > li > a { font-weight:bold; text-transform:uppercase;color:#fff; background:#666;border:none; padding:0; height:30px; line-height: 30px; margin:0; font-size:16px; min-width:100px; text-align:center; }
+.nav-tabs > li:hover > a,
+.nav-tabs > .active > a,
+.nav-tabs > .active > a:hover {  background:#333; color:#fff; border:none; }
 </style>
 
 <script type="text/javascript">
@@ -107,6 +116,27 @@
 
             <!-- DIREITA -->
             <div id="direita" class="grid1">                         
+
+              <h2>Redes Sociais</h2>
+              <!-- abas -->
+              <div class="">
+                <ul class="nav nav-tabs" id="myTab">
+                  <li class="active"><a data-toggle="tab" href="#facebook">Facebook</a></li>
+                  <li class=""><a data-toggle="tab" href="#twitter">Twitter</a></li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                  <div id="facebook" class="tab-pane fade active in">
+                    <div class="fb-comments" data-href="cmais.com.br/segundatela/jornaldacultura/<?php echo $date; ?>" data-width="300px" data-num-posts="10"></div> 
+                  </div>
+                  <div id="twitter" class="tab-pane fade">
+                    <a class="twitter-timeline" href="https://twitter.com/search?q=%40jornal_cultura" data-widget-id="316640392126808065">Tweets sobre "@jornal_cultura"</a>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                  </div>
+                </div>
+              </div>
+              <!-- /abas -->
+
+
 
               <!-- publicidade -->
               <div class="box-publicidade grid1">
