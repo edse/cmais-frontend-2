34247f4a8b16bb72df93ced2cfd54f7e4f23714c
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-23 18:44:47 -0300

    u

--- web/actions/radarcultura/retriveFacebookPost.php
+++ web/actions/radarcultura/retriveFacebookPost.php
@@ -7,7 +7,7 @@ require 'facebook-php-sdk/src/facebook.php';
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
   'appId'  => '498504570187162',
-  'secret' => '4b1e3a3e00c03267e51575259aa8cdfb',
+  'secret' => '4b1e3a3e00c03267e51575259aa8cdfb'
 ));
 
 $facebook->setExtendedAccessToken();
