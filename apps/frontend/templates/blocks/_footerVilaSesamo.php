f64b5c7d18102edf6d63939ad5f7b980f7b09b45
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-24 20:09:50 -0300

    vilasesamo

---
+++ apps/frontend/templates/blocks/_footerVilaSesamo.php
@@ -0,0 +1,39 @@
+    <footer>
+      <div class="copyright"><small>© 2013 - Vila Sésamo</small></div>
+      <div class="container row-fluid">
+      <section class="">
+        <h1><a href="vila-sesamo" title="Vila Sésamo" class="sprite-logo-sesamo-peq">Vila Sésamo</a></h1>
+        <h2><a href="/vila-sesamo/incluirbrincando" title="Incluir Brincando" class="sprite-rodape-incluir">Incluir Brincando</a></h2>
+        <nav class="span12">
+          <ul class="span4">
+            <li><h3>Jogos</h3></li>
+            <li><a href="#" title="Jogo 1">Jogo 1</a></li>
+            <li><a href="#" title="Jogo 2">Jogo 2</a></li>
+            <li><a href="#" title="Jogo 3">Jogo 3</a></li>
+            <li><a href="#" title="Jogo 4">Jogo 4</a></li>
+            <li><a href="#" title="Jogo 5">Jogo 5</a></li>
+            <li><a href="#" title="Jogo 6">Jogo 6</a></li>
+          </ul> 
+          <ul class="span4">
+            <li><h3>Jogos</h3></li>
+            <li><a href="#" title="Jogo 1">Jogo 1</a></li>
+            <li><a href="#" title="Jogo 2">Jogo 2</a></li>
+            <li><a href="#" title="Jogo 3">Jogo 3</a></li>
+            <li><a href="#" title="Jogo 4">Jogo 4</a></li>
+            <li><a href="#" title="Jogo 5">Jogo 5</a></li>
+            <li><a href="#" title="Jogo 6">Jogo 6</a></li>
+          </ul> 
+          <ul class="span4">
+            <li><h3>Jogos</h3></li>
+            <li><a href="#" title="Jogo 1">Jogo 1</a></li>
+            <li><a href="#" title="Jogo 2">Jogo 2</a></li>
+            <li><a href="#" title="Jogo 3">Jogo 3</a></li>
+            <li><a href="#" title="Jogo 4">Jogo 4</a></li>
+            <li><a href="#" title="Jogo 5">Jogo 5</a></li>
+            <li><a href="#" title="Jogo 6">Jogo 6</a></li>
+          </ul> 
+        </nav>
+      </section>
+      <aside class="span3"><h3>Sites relacionados</h3></aside>
+      </div>
+    </footer>
