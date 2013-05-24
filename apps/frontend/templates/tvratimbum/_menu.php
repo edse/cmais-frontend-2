8d8a51c68850ba08a58fc80ec2d8dcbc16de8dcd
rafaelhcs <rhcsousa@gmail.com>
2013-05-24 09:06:26 -0300

    up

--- apps/frontend/templates/tvratimbum/_menu.php
+++ apps/frontend/templates/tvratimbum/_menu.php
@@ -51,7 +51,7 @@
           </div>
         </div>
         
-        <a href="http://tvratimbum.cmais.com.br/eu-e-o-quarteto-apavorante" title="+ detalhes!"><img class="destaqueTopo" src="/portal/tvratimbum/image/quarteto.png" alt="TV Rá Tim Bum! Ideias Mirabolantes" /></a> 
+        <a href="http://tvratimbum.cmais.com.br/portal/tvratimbum/realidadeaumentada" title="+ detalhes!"><img class="destaqueTopo" src="/portal/tvratimbum/image/quarteto.png" alt="TV Rá Tim Bum! Ideias Mirabolantes" /></a> 
         
         <!--
         <div id="destaque-ferias"> 
