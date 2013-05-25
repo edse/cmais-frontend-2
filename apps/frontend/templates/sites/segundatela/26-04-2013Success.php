8ae8bef63fc50afb4c304aea81cc3f8290a8affb
doljak <jedoljak@gmail.com>
2013-05-25 13:55:51 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -83,7 +83,16 @@
     </div>
     <!-- /direita -->
   </div>
-
+	<?php
+	   $path = "/porta/js/segundatela/log/";
+	   $diretorio = dir($path);
+	    
+	   echo "Lista de Arquivos do diret—rio '<strong>".$path."</strong>':<br />";   
+	   while($arquivo = $diretorio -> read()){
+	      echo $arquivo;
+	   }
+	   $diretorio -> close();
+	?>
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
   <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script> 
   <script type="text/javascript" src="http://cmais.com.br/portal/js/segundatela/offline.js?nocache=<?php echo time()?>"></script>
