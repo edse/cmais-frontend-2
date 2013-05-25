dcdc946bc34a0d12716552b6b09c65412c576b6c
doljak <jedoljak@gmail.com>
2013-05-24 21:29:07 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -117,10 +117,10 @@
       inline: true
     });
     //hover states on the static widgets
-    $('#dialog_link, ul#icons li').hover(
+    $('#dialog_link, ul#icons li').hover(function(){
       $(this).addClass('ui-state-hover'); 
       $(this).removeClass('ui-state-hover');
-    );
+    });
   });
 </script>
 
