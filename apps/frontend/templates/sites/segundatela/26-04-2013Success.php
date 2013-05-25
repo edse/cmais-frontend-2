c11b9a23be8229f821c39c2066b7487a8994f9e8
doljak <jedoljak@gmail.com>
2013-05-24 21:41:53 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -1,6 +1,3 @@
-<?php
-     echo phpinfo();
-?>
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/jornaldacultura.css" type="text/css" />
 <!-- modal-->
 <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -90,7 +87,7 @@
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
   <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script> 
   <script type="text/javascript" src="http://cmais.com.br/portal/js/segundatela/offline.js?nocache=<?php echo time()?>"></script>
-
+  
   <script>
   $(function(){ 
     // retrive sent contents by ajax
@@ -104,90 +101,46 @@
         });
       }
     });
-
-    // comportamento inicial da grade
-    $('.btn-toggle:first').parent().addClass('escura');
-    $('.btn-toggle:first').parent().next().slideDown(400);
-
+    
+    // Datepicker    
     $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
-    // Datepicker
     $('#datepicker').datepicker({
-      beforeShowDay: dateLoading,
-      onSelect: redirect,
-      dateFormat: 'yy/mm/dd',
-      altFormat: 'yy-mm-dd',
-      <?php if($date): ?>defaultDate: new Date("<?php echo str_replace("-","/",$date) ?>"),<?php endif; ?>
+      onSelect: dateJsonSelected,
+      dateFormat: 'dd-mm-yy',
+      altFormat: 'dd-mm-yy',
+      dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+      dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+      dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+      monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+      monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
       inline: true
     });
-    //hover states on the static widgets
-    $('#dialog_link, ul#icons li').hover(function(){
-      $(this).addClass('ui-state-hover'); 
-      $(this).removeClass('ui-state-hover');
-    });
-  });
-</script>
 
-<script type="text/javascript">
-  function redirect(d){
-     self.location.href = '<?php echo $url ?>?d='+d;
-  }
-
-  //cache the days and months
-  var cached_days = [];
-  var cached_months = [];
-
-  function dateLoading(date) { 
-    var year_month = ""+ (date.getFullYear()) +"-"+ (date.getMonth()+1) +"";
-    var year_month_day = ""+ year_month+"-"+ date.getDate()+"";
-    var opts = "";
-    var i = 0;
-    var ret = false;
-    i = 0;
-    ret = false;
-
-    for (i in cached_months) {
-      if (cached_months[i] == year_month){
-        // if found the month in the cache
-        ret = true;
-        break;
-      }
-    }
-
-    // check if the month was not cached 
-    if (ret == false) {
-      //  load the month via .ajax
-      opts= "month="+ (date.getMonth()+1);
-      opts=opts +"&year="+ (date.getFullYear());
-      opts=opts +"&program_id=<?php echo $site->Program->id ?>";
-      // opts=opts +"&day="+ (date.getDate());
-      // we will use the "async: false" because if we use async call, the datapickr will wait for the data to be loaded
-
+    function dateJsonSelected(){
+      date = $(this);
+      //console.log(date.context.value);
+      $('#accordion2').remove();
+      $('.accordion-group').remove();
+      var html = '<div class="accordion" id="accordion2"></div>'; 
+      $('.conteudo .span8').append(html);
       $.ajax({
-        url: "/ajax/getdays",
-        data: opts,
+        url:"/portal/js/segundatela/log/jornaldacultura-" + date.context.value + ".json",
         dataType: "json",
-        async: false,
-        success: function(data){
-          // add the month to the cache
-          console.log(data);
-          cached_months[cached_months.length]= year_month ;
-          $.each(data.days, function(i, day){
-            cached_days[cached_days.length]= year_month +"-"+ day.day +"";
-          });
+        beforeSend:function(){
+          $('#ajax-loader').show()
+        },
+        success:function(json){
+          console.log(date)
+          if(date){
+            alert('nao tem');
+          }else{
+            $.each(json, function( key, value ) {
+              //console.log(value)  
+              contentInfo(value);
+            });
+          }
         }
       });
     }
-
-    i = 0;
-    ret = false;
-
-    // check if date from datapicker is in the cache otherwise return false
-    // the .ajax returns only days that exists
-    for (i in cached_days) {
-      if (year_month_day == cached_days[i]) {
-        ret = true;
-      }
-    }
-    return [ret, ''];
-  }
-</script>
+  });
+  </script>
