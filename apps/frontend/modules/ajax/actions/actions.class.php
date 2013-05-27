cf090ed55de27c703e5b1186d78ac6d30663d013
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-27 20:06:27 -0300

    u

--- apps/frontend/modules/ajax/actions/actions.class.php
+++ apps/frontend/modules/ajax/actions/actions.class.php
@@ -47,14 +47,31 @@ class ajaxActions extends sfActions
               ->select('s.*')
               ->from('Schedule s')
               ->where('s.program_id = ?', (int)$request->getParameter('program_id'))
-              ->andWhere('s.date_start >= ? AND s.date_start < ?', array($year.'-'.$month.'-01 04:59:59', $year.'-'.$month2.'-01 05:00:00'))
+              ->andWhere('s.date_start >= ? AND s.date_start < ?', array($year.'-'.$month.'-01 00:00:00', $year.'-'.$month2.'-01 00:00:00'))
               ->andWhere('s.channel_id = ?', 1)
               ->orderBy('s.date_start asc')
               ->execute();
           }
+          /*
           if(count($schedules)>0) {
             foreach($schedules as $s) {
               $d = substr($s->date_start, 8,2);
+              if(substr($d,0,1) == "0")
+                $d = substr($s->date_start, 9,1);
+              if(!in_array(array('day'=> $d), $output['days'])) {
+                $output['days'][] = array('day'=> $d);
+              }
+            }
+          }
+          */
+          if(count($schedules)>0) {
+            foreach($schedules as $s) {
+              $d = substr($s->date_start, 8,2);
+              $objDate = new DateTime(substr($s->date_start,0,10));
+              if ( (substr($s->date_start,11) >= "00:00:00") && (substr($s->date_start,11) <= "05:29:29") ) {
+                $objDate = $objDate->sub(date_interval_create_from_date_string('1 days'));
+                $d = $objDate->format("d");
+              }
               if(substr($d,0,1) == "0")
                 $d = substr($s->date_start, 9,1);
               if(!in_array(array('day'=> $d), $output['days'])) {
