449431292b26887cfef6daee5877f565c1c7782c
doljak <jedoljak@gmail.com>
2013-05-27 15:54:53 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -150,9 +150,8 @@
       dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
       dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
       monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-      monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
-      Prev:"a",
-      Next:"a",
+      prev:"a",
+      next:"a",
       inline: true
     });
     
