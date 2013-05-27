dd2a378af613cbbfe0ff74bd618724dfe7e8a7ef
doljak <jedoljak@gmail.com>
2013-05-27 12:33:30 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -99,7 +99,7 @@
           $arrayDate = array($programast[1], $programast[2], $programast[3]);
           $dateJson = implode("-", $arrayDate);
           $dateJson = explode(".", $dateJson);
-          echo $dateJson . "<br>"; 
+          //echo $dateJson . "<br>"; 
         } 
       }
     }
@@ -123,9 +123,9 @@
     
     <?php 
     echo count($dateJson);
-    for($i=0;$i<count($dateJson);$i++){
-      echo 'dateList.push('.$dateJson[$i].')'; 
-    }
+    //for($i=0;$i<count($dateJson);$i++){
+      //echo 'dateList.push('.$dateJson[$i].')'; 
+    //}
     ?>
     console.log(dateList);
     /*
