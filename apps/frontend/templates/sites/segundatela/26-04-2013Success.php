b9108780035b036e16626346238544aee6d0d051
doljak <jedoljak@gmail.com>
2013-05-27 10:56:48 -0300

    u

--- apps/frontend/templates/sites/segundatela/26-04-2013Success.php
+++ apps/frontend/templates/sites/segundatela/26-04-2013Success.php
@@ -3,7 +3,7 @@ if ($handle = opendir('./portal/js/segundatela/log/')) {
   while (false !== ($programast = readdir($handle))) {
     if ($programast != "." && $programast != "..") {
       explode ('-', $programast);
-      echo $programast[0] . "</br>";
+      echo $programast . "</br>";
     }
   }
   closedir($handle);
