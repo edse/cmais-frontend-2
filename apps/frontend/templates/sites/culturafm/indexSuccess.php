5352c92919d710395b8cb104af68bd24803cf02c
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-28 18:23:59 -0300

    u

--- apps/frontend/templates/sites/culturafm/indexSuccess.php
+++ apps/frontend/templates/sites/culturafm/indexSuccess.php
@@ -94,25 +94,6 @@
             </style>
  
             
-            <!-- BOX PADRAO -->
-          <div class="box-padrao grid1">
-            <div class="topo claro">
-              <span></span>
-              <div class="capa-titulo">
-                <h4>Escolha suas músicas</h4>
-              </div>
-            </div>
-            <div class="bg-cinza">
-              <a href="http://culturafm.cmais.com.br/selecao-do-ouvinte">Indique até seis composições de sua preferência e acompanhe pela Cultura FM.</a>
-              <p style="margin:20px 0 8px 0;">De segunda a sábado, às 13 horas.</p>
-            </div>
-          </div>
-          <!-- BOX PADRAO -->
-            
-            
-            
-            
-            
           </div>
           <!-- /col-esq -->
           <!-- col-dir -->
@@ -134,7 +115,20 @@
               ?>
             </div>
             <!-- /BOX RADIO -->
-           
+            <!-- BOX PADRAO -->
+            <div class="box-padrao grid1">
+              <div class="topo claro">
+                <span></span>
+                <div class="capa-titulo">
+                  <h4>Escolha suas músicas</h4>
+                </div>
+              </div>
+              <div class="bg-cinza">
+                <a href="http://culturafm.cmais.com.br/selecao-do-ouvinte">Indique até seis composições de sua preferência e acompanhe pela Cultura FM.</a>
+                <p style="margin:20px 0 8px 0;">De segunda a sábado, às 13 horas.</p>
+              </div>
+            </div>
+            <!-- BOX PADRAO -->
           </div>
           <!-- /col-dir -->
         </div>
