1dd4ad10d262e414b22ae72e8320bae02fcc3035
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-29 13:49:06 -0300

    u

--- apps/frontend/templates/sites/cartaoverde/coletivoSuccess.php
+++ apps/frontend/templates/sites/cartaoverde/coletivoSuccess.php
@@ -98,9 +98,11 @@ $(function(){ //onready
               <div class="col-esq grid1">
 	              <!-- BOX TWITTER -->
 	              <div class="grid1">
+                  <a href="http://twitter.com/cartaoverde" class="twitter-follow-button" target="_blank">Siga @cartaoverde</a>  
+                  <!--div id="twitter"></div-->
                   <a class="twitter-timeline" href="https://twitter.com/search?q=%40cartaoverde" data-widget-id="317362402159108096">Tweets about "@cartaoverde"</a>
                   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>                       
-	              </div>
+                </div>
 	              <!-- /BOX TWITTER -->
               </div>
               <!-- /col-esq -->
