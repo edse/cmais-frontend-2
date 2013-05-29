11a198e1f4b0ed21820134e69b0e9b92b2738274
Joseval <joseval@terra.com.br>
2013-05-29 15:55:56 -0300

    up

--- apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
+++ apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
@@ -119,7 +119,7 @@
     success:function(json){
       $.each(json, function( key, value ) {
         //console.log(value)
-        contentInfo(value);
+        //contentInfo(value);
       });
     }
   });
