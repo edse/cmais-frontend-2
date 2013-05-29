fb9015e116cd096282578123575a9acbb3f9e143
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-05-29 18:28:08 -0300

    u

--- apps/frontend/templates/sites/cmais/assets/cadastro-para-tutoria-presencial-do-curso-ingles-onlineSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/cadastro-para-tutoria-presencial-do-curso-ingles-onlineSuccess.php
@@ -383,10 +383,12 @@
             $("#prof_ativo_tipo, #categoria_diretoria, #prof_ativo_escola").show();
           }
           if (categoria == "pcnp") {
+            $("#categoria_diretoria > label").html("Diretoria de Ensino à qual está vinculado(a):");
             $("#categoria_diretoria").show();
             $("prof_ativo_tipo, #prof_ativo_escola").hide();
           }
           if (categoria == "professor inativo") {
+            $("#categoria_diretoria > label").html("Diretoria de Ensino da cidade/região em que reside:");
             $("#categoria_diretoria").show();
             $("prof_ativo_tipo, #prof_ativo_escola").hide();
           }
