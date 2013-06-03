e0ab5888f4395dc2c67971f575f783c14d2e1c03
Joseval <joseval@terra.com.br>
2013-06-03 15:28:49 -0300

    up

--- apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
+++ apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
@@ -179,4 +179,16 @@
     //});
   //});
   */
+   $('.accordion-body iframe').each(function() {
+      //alert('test');
+      if($(this).attr('src').indexOf('youtube') != -1){
+        //alert(cont);
+        cont++;
+        $(this).attr('id','player'+cont);
+        onYouTubeIframeAPIReadyPlayer('player'+cont , cont);
+     }
+     
+  });   
+ 
+ 
 </script>
