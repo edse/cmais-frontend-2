73a02ef8ada5bf0997411723d22880fbe074502d
Joseval <joseval@terra.com.br>
2013-06-03 15:01:33 -0300

    up

--- web/portal/js/segundatela/offline.js
+++ web/portal/js/segundatela/offline.js
@@ -81,15 +81,12 @@ $(document).ready(function() {
 
 
   $('.accordion-body iframe').each(function() {
-      alert('test');
       if($(this).attr('src').indexOf('youtube') != -1){
         alert(cont);
         cont++;
         $(this).attr('id','player'+cont);
         onYouTubeIframeAPIReadyPlayer('player'+cont , cont);
      }
-     
   });    
-
 
 });
