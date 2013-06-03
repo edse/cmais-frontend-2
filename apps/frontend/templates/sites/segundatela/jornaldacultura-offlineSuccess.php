07e4105d497f603a476b09255805077bb2415a1c
Joseval <joseval@terra.com.br>
2013-06-03 16:52:25 -0300

    up

--- apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
+++ apps/frontend/templates/sites/segundatela/jornaldacultura-offlineSuccess.php
@@ -1,7 +1,4 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/segundatela/jornaldacultura.css" type="text/css" />
-<script type="text/javascript" src="https://www.youtube.com/iframe_api"></script> 
-<script type="text/javascript" src="http://cmais.com.br/portal/js/segundatela/offline.js?nocache=<?php echo time()?>"></script>
-
 <!-- modal-->
 <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
@@ -44,7 +41,8 @@
       </div>
     </div>
   </div>
-    
+  <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script> 
+  <script type="text/javascript" src="http://cmais.com.br/portal/js/segundatela/offline.js?nocache=<?php echo time()?>"></script>    
   <!-- Example row of columns -->
   <div class="row-fluid conteudo">
     <!-- esquerda -->
--- web/portal/js/segundatela/offline.js
+++ web/portal/js/segundatela/offline.js
@@ -79,16 +79,6 @@ $(document).ready(function() {
     $(this).find('p:last').css('padding-bottom', '15px');
   });
 
-
-  $('.accordion-body iframe').each(function() {
-      if($(this).attr('src').indexOf('youtube') != -1){
-        cont++;
-        $(this).attr('id','player'+cont);
-        onYouTubeIframeAPIReadyPlayer('player'+cont , cont);
-     }
-  }); 
-
-
 /*
  *
   $('.accordion-body iframe').each(function() {
