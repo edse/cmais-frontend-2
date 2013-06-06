1d65f43e2d4933c8155de50bd11bc0fea560eb8b
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-06-05 21:07:08 -0300

    u

--- apps/frontend/templates/sites/culturafm/_display.php
+++ apps/frontend/templates/sites/culturafm/_display.php
@@ -7,10 +7,10 @@
                   <h4><?php echo $displays[0]->getLabel() ?></h4>
                 </div>
               </div>
-              <a title="<?php echo $displays[0]->getTitle() ?>" href="<?php echo $displays[0]->retriveUrl() ?>">
+              <a title="<?php echo $displays[0]->getTitle() ?>" href="<?php echo str_replace("/home/","/",$displays[0]->retriveUrl()) ?>">
                 <img name="<?php echo $displays[0]->getTitle() ?>" alt="<?php echo $displays[0]->getTitle() ?>" src="<?php echo $displays[0]->retriveImageUrlByImageUsage("image-3-b") ?>">
               </a>
-              <a href="<?php echo $displays[0]->retriveUrl() ?>" class="titulos" title="<?php echo $displays[0]->getTitle() ?>"><?php echo $displays[0]->getTitle() ?></a>
+              <a href="<?php echo str_replace("/home/","/",$displays[0]->retriveUrl()) ?>" class="titulos" title="<?php echo $displays[0]->getTitle() ?>"><?php echo $displays[0]->getTitle() ?></a>
               <p><?php echo $displays[0]->getDescription() ?></p>
             </div>
               <?php endif; ?>
