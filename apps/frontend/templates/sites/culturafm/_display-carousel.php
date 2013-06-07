b2665b49b3df683fe32ac3a7f26a701634cc19e4
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-06-07 16:30:14 -0300

    u

--- apps/frontend/templates/sites/culturafm/_display-carousel.php
+++ apps/frontend/templates/sites/culturafm/_display-carousel.php
@@ -7,7 +7,7 @@
               <li style="display: block;" id="bloco<?php echo $k; ?>" class="filho">
                 <a class="media titulos" href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
                   <?php echo $d->getLabel() ?>
-                  <img style="width: 310px;" src="<?php echo $displays[0]->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $d->getTitle() ?>">
+                  <img style="width: 310px;" src="<?php echo $d->retriveImageUrlByImageUsage("image-8-b") ?>" alt="<?php echo $d->getTitle() ?>">
                 </a>
                 <a href="<?php echo $d->retriveUrl() ?>" class="titulos" title="<?php echo $d->getTitle() ?>"><?php echo $d->getTitle() ?></a>
                 <p><?php echo $d->getDescription() ?></p>
