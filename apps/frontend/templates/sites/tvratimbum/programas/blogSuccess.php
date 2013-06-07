37f96e205296277766280d91eb6b0d43fdf3961a
rafaelhcs <rhcsousa@gmail.com>
2013-06-07 11:14:32 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/programas/blogSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/programas/blogSuccess.php
@@ -44,9 +44,9 @@
     ->where('s.id = a.site_id')
     ->andWhere('sa.asset_id = a.id')
 	->andWhere('a.is_active = 1')
-	->andWhere('a.asset_type_id = 1')
-		->andWhere('sa.asset_type_id = 1')
-    ->andWhere('sa.section_id = ?', 2475)
+	->andWhere('sa.section_id = ?', 2475)
+	->andWhereIn('a.asset_type_id = 1')
+
 
 ?>
 
