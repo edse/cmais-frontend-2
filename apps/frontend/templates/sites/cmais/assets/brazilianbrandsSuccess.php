00a433b2dfefc0a97b025e553a76d6e5f47c61d7
renatareno <asdf@fpawks0142.local>
2013-06-07 14:13:41 -0300

    brands

--- apps/frontend/templates/sites/cmais/assets/brazilianbrandsSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/brazilianbrandsSuccess.php
@@ -2,8 +2,8 @@
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
 <style type="text/css">
-  .qrcode { width:960px; margin: 0 auto; }
-  #rodape-portal { margin-top:-30px; }
+  .qrcode { width:960px; margin:30px auto 0; }
+  #rodape-portal { margin-top:20px; }
 </style>
 <div class="qrcode">
 <img src="http://tvcultura.cmais.com.br/uploads/assets/image/original/910f1b0a09e9d053a8429623f0414a9f5f14dd5c.jpg" alt="Brazilian Brands" />
