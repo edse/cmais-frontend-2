21c146d9f6cce5993a7972209ef40487ed04b865
renatareno <asdf@fpawks0142.local>
2013-06-11 14:29:47 -0300

    enquete

--- apps/frontend/templates/sites/defaultPortal/questionSuccess.php
+++ apps/frontend/templates/sites/defaultPortal/questionSuccess.php
@@ -9,11 +9,11 @@
   	->execute();
 	?>
 	
-    <link rel="stylesheet" href="/portal/css/tvcultura/secoes/contato.css" type="text/css" />
+    
 
-<link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
 
 <!--
+  <link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap-responsive.min.css">
 <link href="/portal/tvratimbum/css/geral.css" type="text/css" rel="stylesheet">
 <link href="/portal/tvratimbum/css/novoLayout-2012.css" type="text/css" rel="stylesheet">
