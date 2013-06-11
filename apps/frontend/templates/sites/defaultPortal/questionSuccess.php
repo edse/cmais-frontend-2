28588d0cff6d0cb6ed0da9c6181ebfed923c454a
renatareno <asdf@fpawks0142.local>
2013-06-11 16:47:27 -0300

    enquete

--- apps/frontend/templates/sites/defaultPortal/questionSuccess.php
+++ apps/frontend/templates/sites/defaultPortal/questionSuccess.php
@@ -46,6 +46,8 @@
 #resultado-video li span { position:absolute; right:0; top:0; }
 input.form-contato { float:left; margin-right:5px; }
 #resultado-video p { margin-bottom:0; }
+#barra-site .redes .curtir { width:auto; }
+#menu-portal-2 .abas li { float:right; }
 
 </style>
 
