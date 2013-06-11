093a07467ace5471dd0d1a84ba0a68a0b1d0a124
doljak <jedoljak@gmail.com>
2013-06-11 20:23:20 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -4,6 +4,7 @@
 <style>
 h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
 .lista-calendario .barra-grade .tit {width: 95%;padding: 5px;}
+.lista-calendario .mapa{width:100%; height:750px; border: 1px solid #333;background:#ccc;}
 </style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
@@ -15,5 +16,12 @@ h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
     <div class="grade toggle" style="background:none; padding-bottom: 25px; display: none;">
       teste de conteudo
     </div>
+    
+    <div class="mapa">
+      <form action="" method="post">
+        <input type="search" name="" id="search-map">
+        <input type="submit" id="enviar" />
+      </form>  
+    </div>  
   </div>
 </div>
