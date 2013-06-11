136285ec27ebe27b5accbe6522de3ffa08e92640
rafaelhcs <rhcsousa@gmail.com>
2013-06-11 18:46:20 -0300

    up

--- apps/frontend/templates/sites/defaultPortal/questionSuccess.php
+++ apps/frontend/templates/sites/defaultPortal/questionSuccess.php
@@ -156,7 +156,7 @@ input.form-contato { float:left; margin-right:5px; margin-left:1px; width: 14px;
                   	<?php if(count($v) > 0): ?>
                     <iframe title="<?php echo $opcao ?>" width="310" height="210" src="http://www.youtube.com/embed/<?php echo $v[0]->AssetVideo->getYoutubeId(); ?>?wmode=transparent#t=0m0s" frameborder="0" allowfullscreen></iframe>
                     <? else: ?>
-                    <img src="<?php echo $img[0]->retriveImageUrlByImageUsage('2b') ?>" >
+                    <img src="" >
                     <? endif; ?>
                     <input type="radio" name="opcao" id="opcao-<?php echo $i; ?>" class="form-contato" value="<?php echo $a[$i]->Asset->AssetAnswer->id; ?>"  />
                     <label for="opcao-<?php echo $i; ?>">
