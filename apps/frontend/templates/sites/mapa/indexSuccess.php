056618cd6e6e369f8e8fb24a28ecd6a6e256bb5f
doljak <jedoljak@gmail.com>
2013-06-11 20:15:39 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/geral2.css?a=<?php echo time() ?>" type="text/css" />
 
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+<style>
+h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
+</style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
   <div class="lista-calendario">
