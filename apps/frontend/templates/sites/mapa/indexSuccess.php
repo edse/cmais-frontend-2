ed36ef76bae52fa1bb8613f7e473f8a6574124a3
doljak <jedoljak@gmail.com>
2013-06-11 20:18:25 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -3,6 +3,7 @@
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 <style>
 h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
+.lista-calendario .barra-grade .tit {width: 95%;padding: 5px;}
 </style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
