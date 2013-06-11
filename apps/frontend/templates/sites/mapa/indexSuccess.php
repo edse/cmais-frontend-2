1aa2ed75fc2dc9ed9f81da466a202ba44f231232
doljak <jedoljak@gmail.com>
2013-06-11 19:17:37 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -1,27 +1,27 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/geral2.css?a=<?php echo time() ?>" type="text/css" />
 
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
-
-<h1>TV Cultura - Mapa de Cobertura</h1>
-
-<div class="lista-calendario">
-  <div class="barra-grade">                    
-    <p class="hora">14:30</p>
-    <a href="#" class="btn-toggle tit">Quintal da Cultura</a>
-    <a href="#" class="botao btn-toggle"></a>
-  </div>
-  <div class="grade toggle" style="width: auto; padding-bottom: 25px; display: block;">
-    <div class="capa-foto">
-      <!-- <p class="reprise">18:30 - Reprise deste episódio</p> -->
-      <img src="http://midia.cmais.com.br/programs/c6075c3e3c86909fddd816089f5bed0b2bb6bb21.jpg" alt="Quintal da Cultura">
-                          </div>
-                        <p>Ludovico, Doroteia, Osório, Filomena e Teobaldo têm cabelos coloridos e muita criatividade na cabeça; Minhoquias é uma minhoca maluca e tagarela; e Quelônio, um sábio jabuti de mais de cem anos. Juntos, eles vivem muitas aventuras, inventam brincadeiras, contam histórias, fazem bagunça  e convidam todos os dias a molecada para brincar com eles no Quintal. </p>
-    <!--
-    <p class="bold"></p>
-    <p class="bold"></p>
-    -->
-    <a class="site" href="http://cmais.com.br/quintaldacultura">ir ao site</a>
-                        <br>
-    <a href="http://www.google.com/calendar/event?action=TEMPLATE&amp;text=Quintal+da+Cultura&amp;dates=20130611T143000/20130611T143000&amp;details=&amp;location=Quintal+da+Cultura&amp;trp=false&amp;sprop=http%3A%2F%2Fcmais.com.br&amp;sprop=name:TV%20Cultura" target="_blank" class="google-agenda"><img src="http://www.google.com/calendar/images/ext/gc_button1.gif" border="0" style="width:100px;height:25px;"></a>
+<div id="capa-site">
+  <h1>TV Cultura - Mapa de Cobertura</h1>
+  <div class="lista-calendario">
+    <div class="barra-grade">                    
+      <p class="hora">14:30</p>
+      <a href="#" class="btn-toggle tit">Quintal da Cultura</a>
+      <a href="#" class="botao btn-toggle"></a>
+    </div>
+    <div class="grade toggle" style="width: auto; padding-bottom: 25px; display: block;">
+      <div class="capa-foto">
+        <!-- <p class="reprise">18:30 - Reprise deste episódio</p> -->
+        <img src="http://midia.cmais.com.br/programs/c6075c3e3c86909fddd816089f5bed0b2bb6bb21.jpg" alt="Quintal da Cultura">
+                            </div>
+                          <p>Ludovico, Doroteia, Osório, Filomena e Teobaldo têm cabelos coloridos e muita criatividade na cabeça; Minhoquias é uma minhoca maluca e tagarela; e Quelônio, um sábio jabuti de mais de cem anos. Juntos, eles vivem muitas aventuras, inventam brincadeiras, contam histórias, fazem bagunça  e convidam todos os dias a molecada para brincar com eles no Quintal. </p>
+      <!--
+      <p class="bold"></p>
+      <p class="bold"></p>
+      -->
+      <a class="site" href="http://cmais.com.br/quintaldacultura">ir ao site</a>
+                          <br>
+      <a href="http://www.google.com/calendar/event?action=TEMPLATE&amp;text=Quintal+da+Cultura&amp;dates=20130611T143000/20130611T143000&amp;details=&amp;location=Quintal+da+Cultura&amp;trp=false&amp;sprop=http%3A%2F%2Fcmais.com.br&amp;sprop=name:TV%20Cultura" target="_blank" class="google-agenda"><img src="http://www.google.com/calendar/images/ext/gc_button1.gif" border="0" style="width:100px;height:25px;"></a>
+    </div>
   </div>
 </div>
