e79838850012e252787e61df06084230e9cc8b20
doljak <jedoljak@gmail.com>
2013-06-11 20:26:20 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -5,7 +5,7 @@
 h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
 .lista-calendario .barra-grade .tit {width: 95%;padding: 5px;}
 .lista-calendario .mapa{width:100%; height:750px; border: 1px solid #333;background:#ccc;clear:both; margin-top:20px;}
-
+.lista-calendario .toggle{width:100%;}
 </style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
