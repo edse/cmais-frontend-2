8273f095d1f99a8e99a84e026c6ca1863579f523
doljak <jedoljak@gmail.com>
2013-06-12 14:50:29 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -7,7 +7,7 @@
 .lista-calendario .mapa{width:100%; height:750px; border: 1px solid #333;background:#ccc;clear:both; margin-top:20px;}
 .lista-calendario .toggle{width:100%;}
 .lista-calendario .ipt-txt {border-radius: 0px;width: 300px;float: left;height: 22px;line-height: 25px;font-size: 12px;color: #333;padding-left: 30px;border: 1px solid #c5c5c5;border-right: none;background: #fff}
-
+.lista-calendario .search-map{margin: 20px 20px -3px 5px 20px;border: none;padding: 5px 15px;width: 400px;}
 #form-map{float: left;}
 </style>
 <div id="capa-site">
@@ -24,7 +24,7 @@
     <div class="mapa">
       <form id="form-map" action="" method="post">
         <input type="text" name="" id="search-map">
-        <input type="submit" id="enviar" />
+        <input type="submit" id="enviar" value=""/>
       </form>  
     </div>  
   </div>
