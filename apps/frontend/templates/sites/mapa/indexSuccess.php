eecf5f946e1279b383674b2b1191e1c6f54aa5ff
doljak <jedoljak@gmail.com>
2013-06-12 14:29:09 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -1,7 +1,15 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/geral2.css?a=<?php echo time() ?>" type="text/css" />
 
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+<style>
+h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
+.lista-calendario .barra-grade .tit {width: 95%;padding: 0 10px;}
+.lista-calendario .mapa{width:100%; height:750px; border: 1px solid #333;background:#ccc;clear:both; margin-top:20px;}
+.lista-calendario .toggle{width:100%;}
+.lista-calendario .ipt-txt {border-radius: 0px;width: 300px;float: left;height: 22px;line-height: 25px;font-size: 12px;color: #333;padding-left: 30px;border: 1px solid #c5c5c5;border-right: none;background: #fff}
 
+#form-map{float: left;}
+</style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
   <div class="lista-calendario">
@@ -15,7 +23,7 @@
     
     <div class="mapa">
       <form id="form-map" action="" method="post">
-        <input type="search" name="" id="search-map">
+        <input type="text" name="" id="search-map">
         <input type="submit" id="enviar" />
       </form>  
     </div>  
