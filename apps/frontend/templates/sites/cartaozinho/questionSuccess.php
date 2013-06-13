402ed7e7690ec366522b323b43eb8fdd68864fdc
rafaelhcs <rhcsousa@gmail.com>
2013-06-13 16:03:27 -0300

    up

--- apps/frontend/templates/sites/cartaozinho/questionSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/questionSuccess.php
@@ -28,7 +28,7 @@
 <script type="text/javascript" src="/js/audioplayer/jplayer_min.js"></script>
 
 <link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
-<script src="/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="/portal/js/jquery-1.5.1.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
 
 
