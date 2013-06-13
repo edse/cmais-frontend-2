12a6b5523c6f5c6c00c010bd1928af600babf7a0
rafaelhcs <rhcsousa@gmail.com>
2013-06-13 16:16:54 -0300

    up

--- apps/frontend/templates/sites/defaultPortal/questionSuccess.php
+++ apps/frontend/templates/sites/defaultPortal/questionSuccess.php
@@ -1,7 +1,18 @@
 <?php $a = Doctrine_Query::create() -> select('aa.*') -> from('AssetAnswer aa, RelatedAsset ra, Asset a') -> where('aa.asset_id = a.id') -> andWhere('a.id = ra.asset_id') -> andWhere('ra.parent_asset_id = ?', 123494) -> orderBy('ra.display_order') -> execute();?>
 
 <link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
-<script src="/portal/js/jquery-1.4.4.min.js" type="text/javascript"></script>
+<link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap-responsive.min.css">
+<link href="/portal/tvratimbum/css/geral.css" type="text/css" rel="stylesheet">
+<link href="/portal/tvratimbum/css/novoLayout-2012.css" type="text/css" rel="stylesheet">
+<link href="/portal/tvratimbum/css/jquery.jcarousel.css" rel="stylesheet" type="text/css" />
+<link href="/portal/tvratimbum/css/ferias-especial.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="/portal/css/tvcultura/secoes/contato.css" type="text/css" />
+<!--CSS-->
+
+<!--SCRIPT-->
+<script src="/portal/tvratimbum/js/jquery-1.4.4.min.js" type="text/javascript"></script>
+<!--script src="/portal/tvratimbum/js/jquery-ui-1.8.9.min.js" type="text/javascript"></script-->
+<script src="/portal/tvratimbum/js/jquery.jcarousel.pack.js" type="text/javascript"></script>
 <script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
 
 <style type="text/css">
