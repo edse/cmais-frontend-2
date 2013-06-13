4f3e11b97d423b59409602e2d7e6ec0b5e33e5f8
rafaelhcs <rhcsousa@gmail.com>
2013-06-13 20:26:36 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/programas/indexSuccess.php
@@ -1,5 +1,5 @@
 <?php 
-//$assets = $pager->getResults();
+$assets = $pager->getResults();
 ?>
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
