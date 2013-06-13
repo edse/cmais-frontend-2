02a41aa39892d3e55ffaa2b22692ac74f5d5cadf
rafaelhcs <rhcsousa@gmail.com>
2013-06-13 16:33:50 -0300

    up

--- apps/frontend/templates/sites/defaultPortal/questionSuccess.php
+++ apps/frontend/templates/sites/defaultPortal/questionSuccess.php
@@ -292,7 +292,7 @@ $.ajax({
 type: "POST",
 dataType: "json",
 data: $('.form-votacao').serialize(),
-url: "<?php echo url_for('homepage')?>ajax/enquetes",
+url: "http://cmais.com.br/ajax/enquetes",
   beforeSend: function(){
   $('.btn-barra.votacao').hide();
   $('#ajax-loader-b').show();
