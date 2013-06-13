1b617acc320399c2702274de33ddb75de34a132e
rafaelhcs <rhcsousa@gmail.com>
2013-06-13 15:51:33 -0300

    up

--- apps/frontend/templates/sites/cartaozinho/questionSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/questionSuccess.php
@@ -1,9 +1,5 @@
 <?php $a = Doctrine_Query::create() -> select('aa.*') -> from('AssetAnswer aa, RelatedAsset ra, Asset a') -> where('aa.asset_id = a.id') -> andWhere('a.id = ra.asset_id') -> andWhere('ra.parent_asset_id = ?', 124025) -> orderBy('ra.display_order') -> execute();?>
 
-<link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
-<script src="/portal/tvratimbum/js/jquery-1.4.4.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
-
 <style type="text/css">
   #menuFloat {    margin-left: 0;  }
   #menu-portal-2 h1 {    margin: 0;  }
@@ -30,6 +26,9 @@
 <script type="text/javascript" src="/js/jquery-ui-1.8.7/jquery-1.4.4.min.js"></script>
 <link href="/js/audioplayer/jPlayer.Blue.Monday.2.0.0/jplayer.blue.monday.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="/js/audioplayer/jplayer_min.js"></script>
+
+<link rel="stylesheet" href="/portal/js/bootstrap/css/bootstrap.min.css">
+<script type="text/javascript" src="/portal/js/validate/jquery.validate.js"></script>
 
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
