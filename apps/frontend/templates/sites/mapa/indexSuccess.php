7224e30b066626b882b19f60eb0d0684b1e5d465
doljak <jedoljak@gmail.com>
2013-06-14 14:44:47 -0300

    u

--- apps/frontend/templates/sites/mapa/indexSuccess.php
+++ apps/frontend/templates/sites/mapa/indexSuccess.php
@@ -3,12 +3,22 @@
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 <style>
 #capa-site h1{text-align: left; margin-top: 40px; font-size: 24px; margin-bottom: 20px;}
-#form-map{float: left;width:540px;}
+#form-map{float: left;width:540px;position: relative;z-index: 1;left: 70px;top: 25px;}
 .lista-calendario .barra-grade .tit {width: 95%;padding: 0 10px;}
 .lista-calendario .mapa{height:750px; border: 1px solid #333;background:#ccc;clear:both; margin-top:20px;}
 .lista-calendario .toggle{width:100%;}
 .lista-calendario .search-map{margin: 20px -3px 5px 20px;border: none;padding: 5px 15px;width: 400px;float: left;}
 .lista-calendario #search{border: none;display:block;width: 70px;height: 30px;float: left;margin-top: 20px;background:#4a8cf6 url(/portal/images/lupa-azul-branca.jpg) no-repeat center center;float: left;}
+#map-canvas{top: -40px; z-index: 0;}
+#map-canvas, #map_canvas {height: 500px;}
+@media print {
+  html, body {
+    height: auto;
+  }
+  #map-canvas, #map_canvas {
+    height: 650px;
+  }
+}
 </style>
 <div id="capa-site">
   <h1>TV Cultura - Mapa de Cobertura</h1>
@@ -31,19 +41,6 @@
   </div>
 </div>
 
-<style>
-  #map-canvas, #map_canvas {
-    height: 500px;
-  }
-  @media print {
-    html, body {
-      height: auto;
-    }
-    #map-canvas, #map_canvas {
-      height: 650px;
-    }
-  }
-</style>
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBPYP48UMFX-s3_lrh1tRI9oLcyBM1htS4&sensor=false&language=pt" type="text/javascript"></script>
 <script>
 //980
