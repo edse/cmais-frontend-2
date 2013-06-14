f43b6b571e82c7049a48f2bfddf10a55ac3d91da
renatareno <asdf@fpawks0142.local>
2013-06-14 10:27:05 -0300

    cartaozinho

--- apps/frontend/templates/sites/cartaozinho/questionSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/questionSuccess.php
@@ -22,7 +22,7 @@
   .box-interna .texto img { padding:0; width:300px;  height:180px; overflow:hidden;}
   #lista-videos li { width:300px;  }
   #lista-videos { margin-left:5px; }
-  #lista-videos .form-contato { margin-rigth:5px; }
+  #lista-videos .form-contato { margin-right:5px; }
 </style>
 
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/defaultPrograma.css" type="text/css" />
