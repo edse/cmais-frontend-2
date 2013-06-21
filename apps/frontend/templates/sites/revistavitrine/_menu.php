a025927b5e2acf5c0029fe4bf7b22d4c1ee45678
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-06-21 17:26:23 -0300

    u

--- apps/frontend/templates/sites/revistavitrine/_menu.php
+++ apps/frontend/templates/sites/revistavitrine/_menu.php
@@ -2,7 +2,7 @@
           <div class="menu-vitrine">
             <ul>
               <?php foreach($siteSections as $s): ?>
-              <li><a href="<?php echo $s->getSlug() ?>" title="<?php echo $s->getTitle() ?>"<?php if($section->getSlug() == $s->getSlug()): ?> class="ativo"<?php endif; ?>><?php echo $s->getTitle() ?></a><span></span></li>
+              <li><a href="<?php echo $site->retriveUrl() . $s->getSlug() ?>" title="<?php echo $s->getTitle() ?>"<?php if($section->getSlug() == $s->getSlug()): ?> class="ativo"<?php endif; ?>><?php echo $s->getTitle() ?></a><span></span></li>
               <?php endforeach; ?>
               <li><a href="javascript:;" title="Sobre a Revista" class="btn-sobre">Sobre a Revista<i class="icon-chevron-down icon-white"></i></a>
                 <p class="sobre" style="display:none"><?php echo $site->getDescription() ?></p>
