6a2c395f07ed485b909410d53ee15bb3176be202
rafaelhcs <rhcsousa@gmail.com>
2013-06-24 15:58:36 -0300

    up

--- apps/frontend/templates/sites/multicultura/_videoenqueteSuccess.php
+++ apps/frontend/templates/sites/multicultura/_videoenqueteSuccess.php
@@ -10,4 +10,4 @@
   ?>
 
 
-	<iframe allowfullscreen="" frameborder="0" height="390" src="http://www.youtube.com/embed/<?php echo $asset->getTitle(); ?>?wmode=transparent&amp;rel=0#t=0m0s" title="Enquete Multicultura" width="640"></iframe>
+	
