d74b96b1a538e2135968f046500ef599b453a3eb
doljak <jedoljak@gmail.com>
2013-06-24 15:19:53 -0300

    u

--- apps/frontend/templates/sites/cocorico/_tvenquete.php
+++ apps/frontend/templates/sites/cocorico/_tvenquete.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
 $q = $assets[0]->AssetQuestion->getQuestion()."<br/>";
  
  //imagens respectivas das respostas
 $imgs = $respostas[0]->Asset->retriveRelatedAssetsByAssetTypeId(2);
 $img_0 = "http://midia.cmais.com.br/assets/image/image-4-b/".$imgs[0]->AssetImage->file.".jpg";
 $imgs = $respostas[1]->Asset->retriveRelatedAssetsByAssetTypeId(2);
-$img_1 = "http://midia.cmais.com.br/assets/image/image-4-b/".$imgs[0]->AssetImage->file.".jpg"; */
+$img_1 = "http://midia.cmais.com.br/assets/image/image-4-b/".$imgs[0]->AssetImage->file.".jpg"; 
 ?>
 <div class="enquete span12">
 
