d6bbece49e7b263773710af078df377b55259248
Joseval <joseval@terra.com.br>
2013-06-25 15:04:06 -0300

    up

--- web/panasonic/geraPanasonicJSON.php
+++ web/panasonic/geraPanasonicJSON.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 //LE JSON GERAL
-$jsonurl = "http://200.136.27.50/sony/panasonic.json";
+$jsonurl = "http://172.20.1.127/sony/panasonic.json";
 $json = file_get_contents($jsonurl);
 $json_output = json_decode($json);
 
