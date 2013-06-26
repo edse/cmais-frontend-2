a1b31dd0c0284cbabf86eba4e5935e4b53b868de
doljak <jedoljak@gmail.com>
2013-06-26 15:50:56 -0300

    u

---
+++ web/doctorwho/indexacao/ajax/insert_header.php
@@ -0,0 +1,182 @@
+<?php
+	$return = '
+		<link rel="stylesheet" href="/doctorwho/setimoano/css/geral.css?nocache='.time().'" type="text/css" />
+    <link rel="stylesheet" href="/portal/css/tvcultura/geral2.css?nocache='.time().'" type="text/css" />
+    <link type="text/css" rel="stylesheet" href="/doctorwho/setimoano/css/style.css?nocache='.time().'" />
+    <script type="text/javascript" src="/portal/js/portal.js?nocache='.time().'"></script>
+    
+     
+<div id="guia-topo" align="center">
+  <!--topo Cmais-->
+  <div id="topo-cmais">
+  
+    <!--Logo Cultura-->
+    <a href="http://tvcultura.cmais.com.br" id="logoCultura" title="Portal TV Cultura">
+      <img src="/portal/images/logos-cultura/logo-cultura-0.png" width="80" height="77">
+    </a>  
+    <!--Logo Cultura-->
+    
+    <!--menu parte 2-->
+    <div id="menu-portal-2">
+      
+      <h1>
+        <a href="http://cmais.com.br" title="cmais+ O portal de conteúdo da Cultura">cmais+ O portal de conteúdo da Cultura</a>
+      </h1>  
+      
+      <!--menu editorias-->
+      <ul class="abas">
+       <li class="m-infantil" style="float:right; margin:0 0px 0 0 !important;"><a title="+ Criança" href="http://cmais.com.br/infantil">Infantil</a></li>
+       <li class="m-musica" style="float:right"><a title="Música" href="http://cmais.com.br/musica">Música</a></li>
+       <li class="m-educacao" style="float:right"><a title="Educação" href="http://cmais.com.br/educacao">Educação</a></li>
+       <li class="m-arte-e-cultura " style="float:right"><a title="Arte &amp; Cultura" href="http://cmais.com.br/arte-e-cultura">Arte &amp; Cultura</a></li>
+       <li class="m-jornalismo" style="float:right"><a title="Jornalismo" href="http://cmais.com.br/jornalismo">Jornalismo</a></li>
+        <li class="m-cursos" style="float:right"><a title="Cursos" href="http://univesptv.cmais.com.br/cursos">Cursos</a></li>
+      </ul> 
+      <!--menu editorias-->
+      
+      <!-- Busca Portal -->
+      <form class="busca-portal" action="/busca" method="post">
+        <input type="hidden" name="site_id" id="site_id" value="">
+        <input class="ipt-txt" type="text" name="term" id="term" value="">
+                <input class="ipt-submit" type="submit" value="OK"> 
+      </form>
+      <!-- /Busca Portal -->  
+         
+    </div>
+    <!--menu parte 2-->
+    
+    <!--menu parte 1-->
+    <div id="menu-portal-1">
+      
+      <!--FACEBOOK-->
+      
+      <!--curtir-->
+      <div id="facebook-cultura">
+        
+        <div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; height: 0px; width: 0px; "><div><iframe id="fb_xdm_frame_http" name="fb_xdm_frame_http" src="http://static.ak.facebook.com/connect/xd_arbiter.php?version=8#channel=f234694b9c&amp;origin=http%3A%2F%2Fcmais.com.br&amp;channel_path=%2F%3Ffb_xd_fragment%23xd_sig%3Df101ba3388%26"></iframe><iframe id="fb_xdm_frame_https" name="fb_xdm_frame_https" src="https://s-static.ak.facebook.com/connect/xd_arbiter.php?version=8#channel=f234694b9c&amp;origin=http%3A%2F%2Fcmais.com.br&amp;channel_path=%2F%3Ffb_xd_fragment%23xd_sig%3Df101ba3388%26"></iframe></div><div></div></div><script async="" src="http://connect.facebook.net/pt_BR/all.js"></script></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, \'script\', \'facebook-jssdk\'));</script>
+        
+        <!--curtir-->
+        
+        <div class="fb-like fb_edge_widget_with_comment fb_iframe_widget" data-href="http://www.facebook.com/tvcultura" data-send="false" data-layout="button_count" data-width="110" data-show-faces="true"><span style="height: 20px; width: 110px; "><iframe id="f2741950d8" name="ff280f064" scrolling="no" style="overflow-x: hidden; overflow-y: hidden; height: 20px; width: 110px; border-top-style: none; border-right-style: none; border-bottom-style: none; border-left-style: none; border-width: initial; border-color: initial; border-image: initial; " title="Like this content on Facebook." class="fb_ltr " src="http://www.facebook.com/plugins/like.php?channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D8%23cb%3Dfaaa35764%26origin%3Dhttp%253A%252F%252Fcmais.com.br%252Ff234694b9c%26domain%3Dcmais.com.br%26relation%3Dparent.parent&amp;extended_social_context=false&amp;href=http%3A%2F%2Fwww.facebook.com%2Ftvcultura&amp;layout=button_count&amp;locale=pt_BR&amp;node_type=link&amp;sdk=joey&amp;send=false&amp;show_faces=false&amp;width=110"></iframe></span></div>
+      
+      </div>
+      <!--/FACEBOOK-->
+      
+      <!-- Menu Portal -->
+      <ul id="menu-portal">
+        <!-- Menu TV -->
+        <li class="m-tv"><a href="#" class="filho m_tv_tvcultura" title="TV">PROGRAMAS<span></span></a>
+          <div class="menu-aberto padrao tv grid3">
+            <ul class="abas-menu abas">
+              <li class="neutro">
+                <p>escolha um canal</p>
+                <span></span>
+              </li>
+              <li class="tvcultura ativo"><a href="javascript:;" title="TV Cultura" class="m_tv_tvcultura">TV Cultura</a><span class="decoracao"></span></li>
+              <li class="univesptv"><a href="javascript:;" title="Univesp TV" class="m_tv_univesptv">Univesp TV</a><span class="decoracao"></span></li>
+              <li class="multicultura"><a href="javascript:;" title="multiCULTURA" class="m_tv_multicultura">multiCULTURA</a><span class="decoracao"></span></li>
+              <li class="tvrtb"><a href="javascript:;" title="TV Rá Tim Bum" class="m_tv_tvrtb">TV Rá Tim Bum</a><span class="decoracao"></span></li>
+            </ul>
+            <!-- Abas Conteudo -->
+            <ul class="abas-conteudo">
+              <li id="tvcultura" class="filho"></li>
+              <li id="univesptv" class="filho" style="display:none;"></li>
+              <li id="multicultura" class="filho" style="display:none;"></li>
+              <li id="tvrtb" class="filho" style="display:none;"></li>
+            </ul>
+            <!-- /Abas Conteudo -->
+          </div>
+        </li>
+        <!-- /Menu TV -->
+        
+        <!-- Menu No Ar -->
+        <li class="m-noar"><a class="filho m_ar_tvcultura" href="#" title="PROGRAMAÇÃO">GRADE DE PROGRAMAÇÃO<span></span></a>
+          <div class="menu-aberto padrao ar grid3">
+            <ul class="abas-menu abas">
+              <li class="neutro">
+                <p>escolha um canal</p>
+                <span></span>
+              </li>
+              <li class="tvcultura"><a href="javascript:;" title="TV Cultura" class="m_ar_tvcultura">TV Cultura</a><span class="decoracao"></span></li>
+              <li class="univesptv"><a href="javascript:;" title="Univesp TV" class="m_ar_univesptv">Univesp TV</a><span class="decoracao"></span></li>
+              <li class="multicultura"><a href="javascript:;" title="multiCULTURA" class="m_ar_multicultura">multiCULTURA</a><span class="decoracao"></span></li>
+              <li class="tvrtb"><a href="javascript:;" title="TV Rá Tim Bum" class="m_ar_tvrtb">TV Rá Tim Bum</a><span class="decoracao"></span></li>
+            </ul>
+            <!-- Abas Conteudo -->
+            <ul class="abas-conteudo">
+              <li id="ar-tvcultura" class="filho"></li>
+              <li id="ar-univesptv" class="filho" style="display:none;"></li>
+              <li id="ar-multicultura" class="filho" style="display:none;"></li>
+              <li id="ar-tvrtb" class="filho" style="display:none;"></li>
+            </ul>
+            <!-- /Abas Conteudo -->
+          </div>
+        </li>
+        <!-- /Menu No Ar -->
+        
+        <!-- Menu Radio -->
+            <li class="m-radio"><a href="#" class="filho m_radio_am" title="RADIO">RÁDIOS<span></span></a>
+              <div class="menu-aberto padrao radio grid3">
+                <ul class="abas-menu abas">
+                  <li class="neutro">
+                    <p>escolha uma estação</p>
+                    <span></span>
+                  </li>
+                  <li class="radio-cb"><a href="javascript:;" title="Rádio Cultura Brasil" class="m_radio_am">Cultura Brasil</a><span class="decoracao"></span></li>
+                  <li class="radio-fm"><a href="javascript:;" title="Rádio FM" class="m_radio_fm">Cultura FM</a><span class="decoracao"></span></li>
+                  <li class="radio-rtb"><a href="#radio-rtb" title="Rádio Rá Tim Bum">Rádio Rá Tim Bum</a><span class="decoracao"></span></li>
+                  <li class="radio-cocorico"><a href="#radio-cocorico" title="TV Rá Tim Bum">Rádio Cocoricó</a><span class="decoracao"></span></li>
+                </ul>
+                <!-- Abas Conteudo -->
+                <ul class="abas-conteudo">
+                  <li id="radio-cb" class="filho"></li>
+                  <li id="radio-fm" class="filho" style="display:none;"></li>
+                  <li id="radio-rtb" class="filho" style="display: none; ">
+                    <a href="http://tvratimbum.cmais.com.br/radio" class="bg-Ratimbum"></a>
+                  </li>
+                  <li id="radio-cocorico" class="filho" style="display: none; ">
+                    <a href="http://www3.tvcultura.com.br/cocorico/radio" class="bg-Cocorico"></a>
+                  </li>
+                </ul>
+                <!-- /Abas Conteudo -->
+              </div>
+            </li>
+            <!-- /Menu Radio -->
+        
+        <!-- Menu Videos -->
+        <li class="m-videos"><a href="http://cmais.com.br/videos" title="VIDEOS">VÍDEOS</a></li>
+        <!-- /Menu Videos -->
+
+        <!-- Menu ao Vivo -->
+        <li class="m-aovivo"><a href="http://cmais.com.br/aovivo" title="AO VIVO">AO VIVO</a></li>
+        <!-- /Menu ao Vivo -->
+
+      </ul>
+      <!--redes sociais-->
+      <div id="redesnovo">
+        <a href="javascript:;" id="controle-remoto" class="redesB" title="controle-remoto"></a>
+        <a href="http://itunes.apple.com/br/app/radio-cultura/id370066053" id="apple" class="redesA" title=""></a>
+        <a href="https://plus.google.com/b/107290730774038797358/107290730774038797358" id="google" class="redesA" title=""></a>
+        <a href="http://statigr.am/tvcultura" id="instangram" class="redesA" title=""></a>
+        <a href="http://facebook.com/tvcultura" id="face" class="redesA" title=""></a>
+        <a href="http://twitter.com/tvcultura" id="twit" class="redesA" title=""></a>
+        <a href="http://youtube.com/cultura" id="youtube" class="redesA" title=""></a>
+        <a href="http://tvcultura.cmais.com.br/feed" id="rss" class="redesA" title=""></a>
+      </div>
+      <!--redes sociais-->
+    </div>
+    <!--menu parte 1-->
+  </div>
+  <!--/topo Cmais-->
+</div>';
+ 
+    echo $return;
+    die();
+?>
