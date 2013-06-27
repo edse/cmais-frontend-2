75fd29d3bbb99b99c2859f4dc9ea6bf9a9fafc4a
rafaelhcs <rhcsousa@gmail.com>
2013-06-27 10:48:19 -0300

    update enquete

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -697,8 +697,8 @@ class _assetActions extends sfActions
 		
 		elseif($this->site->getSlug() == "multicultura") {
         if($this->asset->getSlug()=="seumulticultura") {
-        if ($debug) print "<br>multicultura-1 >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/seumulticultura';
-        $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/seumulticultura');
+        if ($debug) print "<br>multicultura-1 >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/enquete';
+        $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/enquete');
       }
 		}
 		
