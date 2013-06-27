46defaccfb650200c9eb6db3d765ed6a26e97062
rafaelhcs <rhcsousa@gmail.com>
2013-06-27 10:21:42 -0300

    up

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -695,14 +695,14 @@ class _assetActions extends sfActions
 			}
     }
 		
-		elseif($this->site->getSlug() == "multicultura") {
+		if($this->site->getSlug() == "multicultura") {
         if($this->asset->getSlug()=="seumulticultura" && $this->asset->AssetType->getSlug()=="question") {
         if ($debug) print "<br>multicultura-1 >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/seumulticultura';
         $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/seumulticultura');
       }
 		}
 		
-		elseif($this->site->getSlug() == "multicultura") {
+		if($this->site->getSlug() == "multicultura") {
         if($this->asset->getSlug()=="seumulticultura" && $this->asset->AssetType->getSlug()=="content") {
         if ($debug) print "<br>multicultura-1 >>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/content';
         $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/content');
