81183742944eb97d3665b81f5e4d25061917d134
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-04 16:38:54 -0300

    u

--- apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
@@ -19,7 +19,8 @@
       
       <div class="span10 destaque-filtro especial">
         <?php
-          $parentSection = Doctrine::getTable('Section')->findOneById($section->getParentSectionId());
+          if($section->getParentSectionId())
+            $parentSection = Doctrine::getTable('Section')->findOneById($section->getParentSectionId());
         ?>
         
         <?php if($parentSection->subsections()): ?>
