3333b63c6fdff6ecb12bdb7744a6f899d696e68e
doljak <jedoljak@gmail.com>
2013-07-04 16:05:54 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/_bannerprincipalmobile.php
+++ apps/frontend/templates/sites/vilasesamo2/_bannerprincipalmobile.php
@@ -63,7 +63,7 @@ $('#selector-mobile li').each(function(i){
   total = width + total; 
 });
 
-$('#selector-mobile').attr('width', 'total');
+$('#selector-mobile').attr('width', total);
 
 $('#carrossel-mobile').responsiveCarousel({
     unitWidth:          'inherit',
