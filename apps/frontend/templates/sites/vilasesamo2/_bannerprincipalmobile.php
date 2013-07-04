25b156d132170d0f1afbd392d43d3c15f6fa907f
doljak <jedoljak@gmail.com>
2013-07-04 16:12:55 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/_bannerprincipalmobile.php
+++ apps/frontend/templates/sites/vilasesamo2/_bannerprincipalmobile.php
@@ -60,7 +60,7 @@
 var total=0;
 $('#selector-mobile li').each(function(i){
   var width = $(this).width();
-  total = width + total + 12; 
+  total = width + total + 13; 
 });
 
 $('#selector-mobile').css('width', total);
