047f5f785189e539c181adaa58cf7d59ea4082ec
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-03 22:20:28 -0300

    u

--- apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
@@ -5,7 +5,7 @@
 </script>
 
 <!-- HEADER -->
-<?php include_partial_from_folder('sites/vilasesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 <!-- /HEADER -->
 <div id="content">
   <section class="scroll row-fluid">
