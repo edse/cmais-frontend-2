d3ab54b2be3800260931e3f6a80e163cdc57b682
doljak <jedoljak@gmail.com>
2013-07-04 19:06:18 -0300

    u

--- apps/frontend/templates/sites/cocorico/tvcocorico1anoSuccess.php
+++ apps/frontend/templates/sites/cocorico/tvcocorico1anoSuccess.php
@@ -37,9 +37,7 @@
     <?php if(isset($displays["destaque-principal"])): ?>
       <?php if(count($displays["destaque-principal"]) > 0): ?>
     <h2><?php echo $displays["destaque-principal"][0]->Asset->getDescription() ?></h2>     
-    <a class="span6"><img alt="<?php echo $displays["destaque-principal"][0]->getTitle() ?>" src="<?php echo $displays["destaque-principal"][0]->retriveImageUrlByImageUsage("image-5-b") ?>"></a>
-    <div class="span6">
-      <?php echo $displays["destaque-principal"][0]->Asset->getDescription() ?></p><br><br>
+      <img alt="<?php echo $displays["destaque-principal"][0]->getTitle() ?>" src="<?php echo $displays["destaque-principal"][0]->retriveImageUrlByImageUsage("image-5-b") ?>">
       <?php echo (html_entity_decode($displays["destaque-principal"][0]->Asset->AssetContent->getContent())) ?>
       <p class="grd">Parabéns!!!</p>
       <p class="grd"><span><?php echo $displays["destaque-principal"][0]->getTitle() ?><br/>
