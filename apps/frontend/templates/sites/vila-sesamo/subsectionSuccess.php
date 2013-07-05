f02f973b5cd869509b1a9e9d0c59c48ed1c2bebe
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-05 17:42:19 -0300

    u

--- apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
@@ -102,7 +102,7 @@
             }
           }
         ?>
-      <li class="span4 element<?php if(is_array($assetPersonagens)) echo " " . implode(" ", $assetPersonagens); ?>">
+      <li class="span4 element<?php if(count($assetPersonagens) > 0) echo " " . implode(" ", $assetPersonagens); ?>">
         
         <?php if($d->AssetType->getSlug() == "video"): ?>
         <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>"><img src="http://img.youtube.com/vi/<?php echo $d->AssetVideo->getYoutubeId() ?>/0.jpg" alt="<?php echo $d->getTitle() ?>" /></a>
