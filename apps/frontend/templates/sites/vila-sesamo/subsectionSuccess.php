1eb7a21fb6ac9f4ae90318357cc69203fe38957c
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-05 17:16:03 -0300

    u

--- apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/subsectionSuccess.php
@@ -95,11 +95,11 @@
           $assetSections = $d->getSections();
           foreach($assetSections as $a) {
             if($a->getParentSectionId() == $personagensSection->getId()) {
-              $d->personagens[] = $a->getSlug();
+              $personagens = $a->getSlug();
             }
           }
         ?>
-      <li class="span4 element <?php echo implode(" ",$d->personagens) ?>">
+      <li class="span4 element <?php echo implode(" ",$personagens) ?>">
         
         <?php if($d->AssetType->getSlug() == "video"): ?>
         <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>"><img src="http://img.youtube.com/vi/<?php echo $d->AssetVideo->getYoutubeId() ?>/0.jpg" alt="<?php echo $d->getTitle() ?>" /></a>
