ac731bad7f7ceb2da51d8a89d8ebe8cee8bb16a2
doljak <jedoljak@gmail.com>
2013-07-10 20:24:33 -0300

    u

--- apps/frontend/templates/sites/maiscrianca/indexSuccess.php
+++ apps/frontend/templates/sites/maiscrianca/indexSuccess.php
@@ -176,7 +176,7 @@ if($feriasSection)
                 <?php if($d->retriveImageUrlByImageUsage("image-2-b") != ""): ?>
                 <img src="<?php echo $d->retriveImageUrlByImageUsage("image-2-b") ?>" alt="<?php echo $d->getTitle() ?>" />
                 <?php endif; ?>
-                <?php echo $d->getTitle() ?>
+                <p style="text-overflow: ellipsis"><?php echo $d->getTitle() ?></p>
               </a>
             </li>
             <?php endforeach; ?>
