ac845e825cf4945f8eaa3b480c0815df1843c85d
doljak <jedoljak@gmail.com>
2013-07-10 21:45:10 -0300

    u

--- web/portal/js/layer-slider/jQuery.layerSlider.js
+++ web/portal/js/layer-slider/jQuery.layerSlider.js
@@ -5,7 +5,7 @@
 		var settings = $.extend({
 			'pauseOnHover': false,
 			'slideshow' : false,
-			'delay': 4000,
+			'delay': 4800,
 			'showArrows': true,
 			'showTimer': true,
 			'uiStyle': 'style01',
