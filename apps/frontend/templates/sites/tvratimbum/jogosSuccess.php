3ffa004ce8fa84d5b79a485d624d9d82654f46f2
rafaelhcs <rhcsousa@gmail.com>
2013-07-12 09:47:54 -0300

    up

--- apps/frontend/templates/sites/tvratimbum/jogosSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/jogosSuccess.php
@@ -8,7 +8,7 @@
     ->from('Site s, SectionAsset sa, Asset a')
     ->where('s.id = a.site_id')
     ->andWhere('sa.asset_id = a.id')
-    //->andWhereIn('sa.section_id',  array(12, 28, 27, 26, 29, 25)) 
+    //->andWhereIn('sa.section_id',  array(12, 28, 27, 26, 29, 25))  
     ->limit(100)
     ->orderBy('s.title')
     ->execute();
