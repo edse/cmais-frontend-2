560ed6612403d82c144f8235887d6b2ac1ce3b27
doljak <jedoljak@gmail.com>
2013-07-15 19:06:13 -0300

    u

--- apps/frontend/templates/blocks/_topo-fpa.php
+++ apps/frontend/templates/blocks/_topo-fpa.php
@@ -3,12 +3,10 @@
 <html lang="pt">
 <!--HEAD-->
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7, user-scalable=no">
-
-  
+  <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7, user-scalable=no"> 
   <!-- SCRIPTS -->
   <script src="/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
-  <script src="/portal/js/bootstrap-v2.3.1/js/bootstrap.min.js"></script>
+  <script src="/portal/js/bootstrap/bootstrap.min.js"></script>
   <script src="/portal/js/fpa.js" type="text/javascript"></script>
   <!-- SCRIPTS -->
   
@@ -39,11 +37,10 @@
 
 
 
-  <!--CONTAINER-->
-  <div class="container principal">
+    
     <div class="navbar">
-      <div>
-        
+      <div class="navbar-inner">
+        <div class="container">
           
           <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -53,7 +50,7 @@
           </a>
      
           <!--LOGO CULTURA-->
-          <a href="/fpa" title="Fundação Padre Anchieta" class="pull-left">
+          <a href="/fpa" title="Fundação Padre Anchieta" class="brand">
             <img src="/portal/images/capaPrograma/fpa/logo-fpa.png" class="logo-fpa" alt="Fundação Padre Anchieta" />
           </a>
           <!--/LOGO CULTURA-->
@@ -62,85 +59,85 @@
           <div class="nav-collapse collapse">
             <!-- .nav, .navbar-search, .navbar-form, etc -->
             <!--LISTA DE BOTOES-->
-            <ul id="topo-fpa" class="nav-pills font-btn nav">
-              <?php if($site->getSlug()=="fpa" && $section->getSlug()=="quem-somos"){$ativo="active";}else{$ativo="";} ?>
-              <li class="<?php echo $ativo?>">
-                <a href="http://www2.tvcultura.com.br/fpa/institucional/quemsomos.aspx" class="link margin-0 <?php echo $ativo?>" title="Quem Somos">Quem Somos</a>
-                <b class="seta-hover <?php echo $ativo?>"></b>
-              </li>
-        
-              <?php if(substr($section->getSlug(), 0, 6)=="decada"){$ativo="active";}else{$ativo="";} ?>
-              <li class="dropdown <?php echo $ativo?>" id="acervo">
-                <a class="link <?php echo $ativo?>" data-toggle="dropdown" href="#" title="Acervo">
-                  Acervo
-                  <b class="caret"></b>
-                </a>
-                <b class="seta-hover <?php echo $ativo?>"></b>
-                <ul class="dropdown-menu ">
-                 <!--<li><a href="#" title="Pesquisa Audiovisual" target="_blank">Pesquisa Audiovisual</a></li>-->
-                 <li><a href="/fpa/decada-60" title="Linha do Tempo">Linha do Tempo</a></li>
-                </ul>
-              </li>
-              <?php if($site->getSlug()=="central-de-relacionamento"){$ativo="active";}else{$ativo="";} ?>
-              <li class="<?php echo $ativo?>">
-                <a href="/central-de-relacionamento" class="link <?php echo $ativo?>" title="Linha do Tempo">Central de Relacionamento</a>
-                <b class="seta-hover <?php echo $ativo?>"></b>
-              </li>
-              
-              <li class="">
-                <a href="http://www2.tvcultura.com.br/fpa/institucional/licitacoes.aspx" class="link" title="Licitações">Licitações</a>
-                <b class="seta-hover"></b>
-              </li>
-              <?php if($section->getSlug()=="trabalhe-conosco" || $section->getSlug()=="realizar-cadastro"){$ativo="active";}else{$ativo="";} ?>
-              <li class="<?php echo $ativo;?>">
-                <a href="http://www2.tvcultura.com.br/selecao/" class="link <?php echo $ativo;?>" title="Trabalhe Conosco">Trabalhe Conosco</a>
-                <b class="seta-hover <?php echo $ativo;?>"></b>
-              </li>
-              
-              <li class="dropdown " id="emissoras">
-                <a class="margin-0" class="link" data-toggle="dropdown" href="#emissoras" title="Emissoras">
-                  Emissoras
-                  <b class="caret"></b>
-                </a>
-                <b class="seta-hover"></b>
-                <ul class="dropdown-menu ">
-                 <li><a href="http://tvcultura.cmais.com.br/" title="Tv Cultura" target="_blank">TV Cultura</a></li>
-                 <li><a href="http://univesptv.cmais.com.br/" title="Univesp TV" target="_blank">Univesp TV</a></li>
-                 <li><a href="http://multicultura.cmais.com.br/" title="multiCultura" target="_blank">multiCultura</a></li>
-                 <li><a href="http://tvratimbum.cmais.com.br/" title="TV Rá Tim Bum!" target="_blank">TV Rá Tim Bum!</a></li>
-                 <li><a href="http://www.culturabrasil.com.br/" title="Cultura Brasil" target="_blank">Cultura Brasil</a></li>
-                 <li><a href="http://culturafm.cmais.com.br/" title="Cultura Fm" target="_blank">Cultura Fm</a></li>
-                 <!-- <li><a href="http://tvratimbum.cmais.com.br/radio" title="Rádio Rá Tim Bum" target="_blank">Rádio Rá Tim Bum</a></li> -->
-                 <!-- <li><a href="http://www3.tvcultura.com.br/cocorico/radio" title="Rádio Cocoricó" target="_blank">Rádio Cocoricó</a></li> -->
-                </ul>
-              </li>
-              
-              <li class="dropdown " id="portais">
-                <a class="margin-0" class="link" data-toggle="dropdown" href="portais" title="Portais">
-                  Portais
-                  <b class="caret"></b>
-                </a>
-                <b class="seta-hover"></b>
-                <ul class="dropdown-menu">
-                 <li><a href="http://cmais.com.br/" title="CMAIS+" target="_blank">CMAIS+</a></li>
-                 <li><a href="http://www.culturabrasil.com.br/" title="Cultura Brasil" target="_blank">Cultura Brasil</a></li>
-                </ul>
-              </li>
-              
-              <?php if($site->getSlug()=="sic"){$ativo="active";}else{$ativo="";} ?>
-              <li class="<?php echo $ativo?>">
-                <a href="http://fpa.com.br/sic/" class="link <?php echo $ativo?>" title="SiC">SiC</a>
-                <b class="seta-hover <?php echo $ativo?>"></b>
-              </li>
-            </ul>
-            <!--/LISTA DE BOTOES-->
+          <ul id="topo-fpa" class="nav-pills font-btn nav">
+            <?php if($site->getSlug()=="fpa" && $section->getSlug()=="quem-somos"){$ativo="active";}else{$ativo="";} ?>
+            <li class="<?php echo $ativo?>">
+              <a href="http://www2.tvcultura.com.br/fpa/institucional/quemsomos.aspx" class="link margin-0 <?php echo $ativo?>" title="Quem Somos">Quem Somos</a>
+              <b class="seta-hover <?php echo $ativo?>"></b>
+            </li>
+      
+            <?php if(substr($section->getSlug(), 0, 6)=="decada"){$ativo="active";}else{$ativo="";} ?>
+            <li class="dropdown <?php echo $ativo?>" id="acervo">
+              <a class="link <?php echo $ativo?>" data-toggle="dropdown" href="#" title="Acervo">
+                Acervo
+                <b class="caret"></b>
+              </a>
+              <b class="seta-hover <?php echo $ativo?>"></b>
+              <ul class="dropdown-menu ">
+               <!--<li><a href="#" title="Pesquisa Audiovisual" target="_blank">Pesquisa Audiovisual</a></li>-->
+               <li><a href="/fpa/decada-60" title="Linha do Tempo">Linha do Tempo</a></li>
+              </ul>
+            </li>
+            <?php if($site->getSlug()=="central-de-relacionamento"){$ativo="active";}else{$ativo="";} ?>
+            <li class="<?php echo $ativo?>">
+              <a href="/central-de-relacionamento" class="link <?php echo $ativo?>" title="Linha do Tempo">Central de Relacionamento</a>
+              <b class="seta-hover <?php echo $ativo?>"></b>
+            </li>
+            
+            <li class="">
+              <a href="http://www2.tvcultura.com.br/fpa/institucional/licitacoes.aspx" class="link" title="Licitações">Licitações</a>
+              <b class="seta-hover"></b>
+            </li>
+            <?php if($section->getSlug()=="trabalhe-conosco" || $section->getSlug()=="realizar-cadastro"){$ativo="active";}else{$ativo="";} ?>
+            <li class="<?php echo $ativo;?>">
+              <a href="http://www2.tvcultura.com.br/selecao/" class="link <?php echo $ativo;?>" title="Trabalhe Conosco">Trabalhe Conosco</a>
+              <b class="seta-hover <?php echo $ativo;?>"></b>
+            </li>
+            
+            <li class="dropdown " id="emissoras">
+              <a class="margin-0" class="link" data-toggle="dropdown" href="#emissoras" title="Emissoras">
+                Emissoras
+                <b class="caret"></b>
+              </a>
+              <b class="seta-hover"></b>
+              <ul class="dropdown-menu ">
+               <li><a href="http://tvcultura.cmais.com.br/" title="Tv Cultura" target="_blank">TV Cultura</a></li>
+               <li><a href="http://univesptv.cmais.com.br/" title="Univesp TV" target="_blank">Univesp TV</a></li>
+               <li><a href="http://multicultura.cmais.com.br/" title="multiCultura" target="_blank">multiCultura</a></li>
+               <li><a href="http://tvratimbum.cmais.com.br/" title="TV Rá Tim Bum!" target="_blank">TV Rá Tim Bum!</a></li>
+               <li><a href="http://www.culturabrasil.com.br/" title="Cultura Brasil" target="_blank">Cultura Brasil</a></li>
+               <li><a href="http://culturafm.cmais.com.br/" title="Cultura Fm" target="_blank">Cultura Fm</a></li>
+               <!-- <li><a href="http://tvratimbum.cmais.com.br/radio" title="Rádio Rá Tim Bum" target="_blank">Rádio Rá Tim Bum</a></li> -->
+               <!-- <li><a href="http://www3.tvcultura.com.br/cocorico/radio" title="Rádio Cocoricó" target="_blank">Rádio Cocoricó</a></li> -->
+              </ul>
+            </li>
+            
+            <li class="dropdown " id="portais">
+              <a class="margin-0" class="link" data-toggle="dropdown" href="portais" title="Portais">
+                Portais
+                <b class="caret"></b>
+              </a>
+              <b class="seta-hover"></b>
+              <ul class="dropdown-menu">
+               <li><a href="http://cmais.com.br/" title="CMAIS+" target="_blank">CMAIS+</a></li>
+               <li><a href="http://www.culturabrasil.com.br/" title="Cultura Brasil" target="_blank">Cultura Brasil</a></li>
+              </ul>
+            </li>
+            
+            <?php if($site->getSlug()=="sic"){$ativo="active";}else{$ativo="";} ?>
+            <li class="<?php echo $ativo?>">
+              <a href="http://fpa.com.br/sic/" class="link <?php echo $ativo?>" title="SiC">SiC</a>
+              <b class="seta-hover <?php echo $ativo?>"></b>
+            </li>
+          </ul>
+          <!--/LISTA DE BOTOES-->
             
           </div>
      
-        
+        </div>
       </div>
     </div>
-  </div>
+     
 
 
 </div> 
