ea20bf756ea47de191f52c9a47b043680cd4ef1c
doljak <jedoljak@gmail.com>
2013-07-15 18:30:59 -0300

    u

--- apps/frontend/templates/blocks/_topo-fpa.php
+++ apps/frontend/templates/blocks/_topo-fpa.php
@@ -8,8 +8,7 @@
   
   <!-- SCRIPTS -->
   <script src="/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
-  <script src="/portal/js/bootstrap/bootstrap.min.js"></script>
-  <script src="/portal/js/bootstrap/bootstrap-collapse.js"></script>
+  <script src="/portal/js/bootstrap-v2.3.1/js/bootstrap.min.js"></script>
   <script src="/portal/js/fpa.js" type="text/javascript"></script>
   <!-- SCRIPTS -->
   
