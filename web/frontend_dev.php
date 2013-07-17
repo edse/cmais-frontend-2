a7a5e07f794cf2c20a016f76a6569b859a9d6837
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-17 18:39:25 -0300

    u
    u

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -6,12 +6,12 @@
 //echo($_SERVER['REMOTE_ADDR']);
 
 //die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-/*
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '172.20.18.133', '172.20.18.103', '172.20.16.62','172.20.17.43','172.20.16.139','172.20.19.50','172.20.15.61','172.20.17.203', '172.20.80.3', '172.20.17.32', '172.20.80.2')))
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-*/
+
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
