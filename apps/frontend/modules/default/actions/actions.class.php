657efacb599a3876fa6c5cb1557f602110824f95
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-07-18 20:58:09 -0300

    u

--- apps/frontend/modules/default/actions/actions.class.php
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -57,7 +57,7 @@ class defaultActions extends sfActions
     //if(is_file('sites/culturafm/error404Success.php')){
       //print_r(get_object_vars($this));
     //if($this-> "culturafm")
-    echo $this->uri;
+    echo $this->site;
       //$this->setTemplate('sites/culturafm/error404', 'default');
    // } 
         // title
