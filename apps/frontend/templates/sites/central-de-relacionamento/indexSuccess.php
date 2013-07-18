b26bdabccc2a35e9f0dc3bcc1b733251229e4d7f
doljak <jedoljak@gmail.com>
2013-07-18 15:39:04 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -26,7 +26,7 @@ $(document).ready(function(){
             <br/> 
             <a href="/central-de-relacionamento/perguntas-frequentes" class="btn btn-success btn-large mais-info" title="Perguntas Frequentes">
               <div class="container-btn">
-                <i class="ico-perg"></i>Perguntas Frequentes
+                <i class="ico-perg"></i>Pergunta Frequente
               </div>
             </a>
           <?php endforeach; ?>
