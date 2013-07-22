17d1310962fa562611b9cc94c7b6f398d5303c36
doljak <jedoljak@gmail.com>
2013-07-22 16:15:14 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -187,14 +187,15 @@ $(document).ready(function(){
                         </label>
                       </div>
                     </div>
-                    <div class="control-group">
+                    
+                    <div id="f2_maisinfo">
+                      <div class="control-group">
                         <label class="control-label" for="f2_cep">CEP</label>
                         <div class="controls">
                           <input type="text" class="input-xlarge" id="f2_cep" name="f2_cep">
                           <p class="help-block">Não sabe seu CEP? <a href="http://www.buscacep.correios.com.br/" target="_blank" style="color:#00ccff">Clique aqui</a> e consulte o Correio.</p>
                         </div>
                       </div>
-                    <div id="f2_maisinfo">
                       <div class="control-group">
                         <label class="control-label" for="f2_endereco">Endereço</label>
                         <div class="controls">
