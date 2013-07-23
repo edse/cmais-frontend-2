1f2297b37614b47f480712a35c73c77d74527c2c
doljak <jedoljak@gmail.com>
2013-07-22 21:22:26 -0300

    u

--- apps/frontend/templates/sites/cultura-brasil/indexSuccess.php
+++ apps/frontend/templates/sites/cultura-brasil/indexSuccess.php
@@ -19,18 +19,64 @@
  
 <!--section topo--> 
 <section class="topo">
-  
-  <div class="container">
-    topo
+  <!--container topo--> 
+  <div class="container row-fluid">
+    <!--logo-->
+    <div class="logo">
+      logo
+    </div>
+    <!--/logo--> 
+    
+    <!--menu cultura brasil-->
+    <?php if(count($siteSections) > 0): ?>        
+    <ul class="nav menu-principal nav-pills">
+      <?php foreach($siteSections as $k=>$s): ?>
+        <?php $subsections = $s->subsections(); ?>
+        <?php if(count($subsections) > 0): ?>
+          <!-- botao --->
+          <li class="dropdown <?php if($section->getParentSectionId() == $s->id): ?>active<?php endif; ?>">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="javascript:;" title="<?php echo $s->getTitle()?>">
+              <?php echo $s->getTitle()?>
+              <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+              <?php foreach($subsections as $s): ?>
+              <li class="">
+                <a class="dropdown" href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle()?>"><?php echo $s->getTitle()?></a>
+              </li>
+              <?php endforeach; ?>
+            </ul>
+          </li>
+          <!-- botao --->
+            <?php else: ?>  
+          <!-- botao --->
+          <li class="<?php if($section->id == $s->id): ?>active<?php endif; ?>">
+            <a href="<?php if($s->getSlug() == "home"): ?>/<?php else: ?><?php echo $s->retriveUrl()?><?php endif; ?>" title="<?php echo $s->getTitle()?>"><?php echo $s->getTitle()?></a>
+          </li>
+          <!-- /botao --->
+        <?php endif; ?>
+      
+      <?php endforeach; ?>          
+   </ul>
+   <?php endif; ?>
+   <!--/menu cultura brasil-->
+   
+   <!-- ouca a radio -->
+   <a id="ouca" class="ouca" href="javascript:;">
+     <img src="/portal/images/capaPrograma/radarcultura/ouca-a-radio.png" alt="Ouça a rádio Cultura Brasil"/>
+   </a>
+    <!-- ouca a radio -->
+    
   </div>
- 
+ <!--/container topo-->
+  
 </section>
-<!--section topo-->
+<!--/section topo-->
 
 <!--section miolo--> 
 <section class="miolo">
 
-  <div class="container">
+  <div class="container row-fluid">
     miolo
   </div>
   
@@ -40,7 +86,7 @@
 <!--section rodape--> 
 <section class="rodape">
 
-  <div class="container">
+  <div class="container row-fluid">
     rodape
   </div>
   
