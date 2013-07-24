2f946837b71dfe980f7375d99f894c1f1d924e48
doljak <jedoljak@gmail.com>
2013-07-24 15:51:37 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/indexSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/indexSuccess.php
@@ -417,8 +417,7 @@
     
     <!-- twiter -->
     <div class="span4">
-      <a class="twitter-timeline" href="https://twitter.com/culturabrasil2" data-widget-id="360106760419287041">Tweets de @culturabrasil2</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      twitter
     </div> 
     <!-- /twiter -->
     
