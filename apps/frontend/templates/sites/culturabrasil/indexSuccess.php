30bf7d184418d4ea2532ad9d5e3169b5ed91aebf
doljak <jedoljak@gmail.com>
2013-07-24 17:34:07 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/indexSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/indexSuccess.php
@@ -5,9 +5,6 @@
 <link href="/portal/js/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="/portal/css/tvcultura/sites/culturabrasil.css" rel="stylesheet" type="text/css" />
 
-<!--fonte-->
-<!--link href='http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'-->
-<!--fonte-->
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
