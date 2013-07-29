6eae2e1e908f88efc09a0abfb36b1295513b01d6
doljak <jedoljak@gmail.com>
2013-07-29 12:34:49 -0300

    u

--- apps/frontend/templates/blocks/_topo-fpa.php
+++ apps/frontend/templates/blocks/_topo-fpa.php
@@ -115,6 +115,33 @@
       </li>
     </ul>
     <!--/LISTA DE BOTOES-->
+    
+    <div class="navbar">
+            <div class="navbar-inner">
+              <div class="container">
+           
+                <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </a>
+           
+                <!-- Be sure to leave the brand out there if you want it shown -->
+                <a class="brand" href="#">Project Central</a>
+           
+                <!-- Everything you want hidden at 940px or less, place within here -->
+                <div class="nav-collapse collapse">
+                  <!-- .nav, .navbar-search, .navbar-form, etc -->
+                  <ul>
+                    <li>teste</li>
+                    <li>teste</li>
+                  </ul>
+                </div>
+           
+              </div>
+            </div>
+          </div>
      
   </div>
   <!--/CONTAINER-->
