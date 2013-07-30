6631f794da69caafe4e68238dff12dc42ef145b4
doljak <jedoljak@gmail.com>
2013-07-30 12:59:30 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/buscaSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/buscaSuccess.php
@@ -62,7 +62,8 @@ if(isset($pager)){
     
     <!--clounaprincipal-->
     <div class="row-fluid">
-      
+      <?php
+      /*
       <!--lista assets-->
       <div class="lista-assets span8">
         <?php if(count($pager) > 0): ?>
@@ -98,7 +99,200 @@ if(isset($pager)){
         <!--paginador-->
       </div>
       <!--listaAssets>
-        
+      
+     
+       */  
+       ?>
+       <!--estatico-->
+       <div class="lista-assets span8">
+                              <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/brasileiros-da-ultima-hora-por-fabio-trummer" title=" Brasileiros da última hora, por Fábio Trummer">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Destaques 2009&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/eddietrumer_03_1279818915.jpg" alt=" Brasileiros da última hora, por Fábio Trummer" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Brasileiros da última hora, por Fábio Trummer</h2>
+                <p>
+                  Vocalista da banda Eddie elege cinco músicas de discos que se destacaram em 2009. Tem Lucas Santana, Karina Buhr, Cidadão Instigado e Céu.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/nove-integrantes-e-coisa-pouca-por-moveis-coloniais-de-acaju" title=" Nove integrantes é coisa pouca?, por Móveis Coloniais de Acaju">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Família grande&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/moveiscoloniaisdeacaju_03_1279818916.jpg" alt=" Nove integrantes é coisa pouca?, por Móveis Coloniais de Acaju" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Nove integrantes é coisa pouca?, por Móveis Coloniais de Acaju</h2>
+                <p>
+                  A banda brasiliense organizou uma seleção musical com conjuntos com, no mínimo, nove integrantes. Com Karnak, Banda Mantiqueira e Karnak.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/sons-porretas-com-a-orquestra-contemporanea-de-olinda" title=" Sons porretas com a Orquestra Contemporânea de Olinda">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Volume máximo&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/orquestracontemporanea_01_1270480433.jpg" alt=" Sons porretas com a Orquestra Contemporânea de Olinda" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Sons porretas com a Orquestra Contemporânea de Olinda</h2>
+                <p>
+                  Seleção musical de primeira tem Academia da Berlinda, Isaar e Eddie.                 </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/macumba-de-primeira" title=" Macumba de primeira">
+                                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span9">
+                <h2>Macumba de primeira</h2>
+                <p>
+                  Rodrigo Brandão e Tiago Munhoz, da Mamelo Sound System, enfileiram batucadas sangue azul.                 </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/compositores-em-feminino" title=" Compositores em feminino">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Atrás da porta&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/mallu_09_1270588633.jpg" alt=" Compositores em feminino" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Compositores em feminino</h2>
+                <p>
+                  A cantora e compositora paulistana Mallu Magalhães sugere uma playlist em que compositores criam letras a partir da alma feminina.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/brasil-latino" title=" Brasil latino">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Soy loco por ti&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/gatonegro_03_1273608504.jpg" alt=" Brasil latino" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Brasil latino</h2>
+                <p>
+                  Tango, bolero, paixão e língua espanhola temperam a seleção musical assinada por Natalia Mallo e Ramiro Murillo, ambos do gatoNegro.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/zeca-baleiro-e-as-letras-nonsenses" title=" Zeca Baleiro e as letras nonsenses">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Sem pé nem cabeça&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/zecabaleiro_02_1276118769.jpg" alt=" Zeca Baleiro e as letras nonsenses" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Zeca Baleiro e as letras nonsenses</h2>
+                <p>
+                  O cantor e compositor Zeca Baleiro seleciona alguns versos curiosos, infames ou inusitados, da música brasileira. Nem Caetano escapa!                 </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/rock-na-veia-por-nico-fornaglia" title=" Rock na veia, por Nico Fornaglia">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>Festa de arromba&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/nicofornaglia_02_1280854829.jpg" alt=" Rock na veia, por Nico Fornaglia" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Rock na veia, por Nico Fornaglia</h2>
+                <p>
+                  Diretor de TV promove viagem pela Jovem Guarda, rock psicodélico, progressivo e dos anos 1980. Tem Ronnie Cord, O Bando e Novos Baianos.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                      <a href="http://cmais.com.br/frontend_dev.php/culturabrasil/entrevistas/sambas-urbanos-sambas-gostosos" title=" Sambas urbanos, sambas gostosos">
+                                                <div class="row-fluid titulo">
+                  
+                </div>
+                            <div class="row-fluid" style="margin-left:10px">
+                                          <div class="span3" style="margin-left:0px">
+                <h6>&nbsp;</h6>
+                <img src="http://midia.cmais.com.br/assets/image/thumbnail/paulalima_01_1272907874.jpg" alt=" Sambas urbanos, sambas gostosos" class="thumb">
+              </div>
+                            <div class="span9">
+                <h2>Sambas urbanos, sambas gostosos</h2>
+                <p>
+                  A cantora Paula Lima convoca Jorge Ben, Arlindo Cruz e Martinália.                </p>  
+              </div>
+            </div>    
+           </a>
+            
+                          <!--paginador-->
+            <div class="row">
+    <div class="pagination pagination-centered">
+      <ul>
+        <li class="back-pag"><a href="javascript: goToPage(1);" class="paginacao" title="Primeira"><i class="icon-fast-backward"></i></a></li>
+        <li class="back-pag" ?=""><a href="javascript: goToPage(1);" class="paginacao" title="Anterior"><i class="icon-backward"></i></a></li>
+                <li class="back-pag active"><a href="javascript: goToPage(1);">1</a></li>
+                <li class="back-pag "><a href="javascript: goToPage(2);">2</a></li>
+                <li class="back-pag "><a href="javascript: goToPage(3);">3</a></li>
+                <li class="back-pag"><a href="javascript: goToPage(2);" class="paginacao" title="Próximo"><i class="icon-forward"></i></a></li>
+        <li class="back-pag"><a href="javascript: goToPage(3);" class="paginacao" title="Última"><i class="icon-fast-forward"></i></a></li>
+      </ul>
+    </div>
+  </div>
+  <!--form id="page_form" action="" method="post">
+    <input type="hidden" name="return_url" value="
+Notice: Undefined variable: url in /var/frontend/apps/frontend/templates/sites/culturabrasil/_paginator.php on line 17
+" />
+    <input type="hidden" name="page" id="page" value="" />
+  </form-->
+  <form id="page_form" action="" method="post">
+      <input type="hidden" name="return_url" value="
+Notice: Undefined variable: url in /var/frontend/apps/frontend/templates/sites/culturabrasil/_paginator.php on line 21
+">
+      <input type="hidden" name="page" id="page" value="">
+      <!--input type="hidden" name="letter" id="letter" value="" /-->
+    </form>
+  <script>
+  function goToPage(i){
+    $("#page").val(i);
+    $("#page_form").submit();
+  }
+  function goToLetter(i){
+    $("#letter").val(i);
+    $("#page").val("");
+    $("#page_form").submit();
+  }
+  </script>
+          <!--paginador-->
+      </div>
+      <!--estatico-->
+      
       <!--coluna direita-->
       <div class="lista-assets redes span4">
         
