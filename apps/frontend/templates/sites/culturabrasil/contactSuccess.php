6d419549aa24daa223f8380bc1e478623b893e4c
doljak <jedoljak@gmail.com>
2013-08-01 16:29:53 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/contactSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/contactSuccess.php
@@ -155,7 +155,7 @@
     <!-- /coluna esquerda -->
     
     <!--coluna direita -->
-    <div class="span4 box-direita" style="*margin-left:15px;">
+    <div class="span4 box-direita" >
       
       <!--banner -->
       <div class="banner-culturabrasil">
