c49d79eb4e7e2fb0d3d99c4236aedb9ce668b264
doljak <jedoljak@gmail.com>
2013-08-01 19:14:14 -0300

    u

--- apps/frontend/templates/sites/cartaozinho/indexSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/indexSuccess.php
@@ -142,7 +142,7 @@
             ->from('Asset a, Section s')
             ->where('s.slug = "enquetes-assets"')
             ->andWhere('a.site_id = ?', (int)$site->id)
-            //->andWhere('a.asset_type_id = 10')
+            ->andWhere('a.asset_type_id = 10')
             ->orderBy('a.id desc')
             ->execute();
           echo count($assets);  
