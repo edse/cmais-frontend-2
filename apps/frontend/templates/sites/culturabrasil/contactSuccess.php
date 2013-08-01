82a6f48047c9505695ebd8c96990f0b25583e47c
doljak <jedoljak@gmail.com>
2013-07-31 21:23:08 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/contactSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/contactSuccess.php
@@ -132,13 +132,13 @@
           </fieldset> 
           
           <fieldset>
-            <div class="codigo span2" id="captchaimage">
+            <div class="codigo span3" id="captchaimage">
               <label for="captcha">Confirmação</label>
               <a class="img" href="javascript:;" onclick="$('#captcha_image').attr('src', '/portal/js/validate/demo/captcha/images/image.php?'+new Date)" id="refreshimg" title="Clique para gerar outro código"> <img src="/portal/js/validate/demo/captcha/images/image.php?1375217158" width="132" height="46" alt="Captcha image" id="captcha_image"> </a>
             </div>
             <div class="span10" id="captchaimage">  
               <label class="msg" for="captcha">Digite no campo abaixo os caracteres que você vê na imagem:</label>
-              <input class="caracteres span12" type="text" maxlength="6" name="captcha" id="captcha">
+              <input class="caracteres span9" type="text" maxlength="6" name="captcha" id="captcha">
             </div>
           </fieldset> 
           <!-- form principal -->
