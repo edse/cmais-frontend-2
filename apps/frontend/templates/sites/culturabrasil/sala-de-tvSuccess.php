5687ab4a520fe3fd41439045631344a672904674
doljak <jedoljak@gmail.com>
2013-07-31 21:43:12 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
@@ -21,54 +21,7 @@
   <div class="row-fluid container">
   <!--transmissao ao vivo -->
   <div class="span6">  
-    <div class="page-header ao-vivo">
-      <h4>Transmiss&atilde;o ao vivo</h4>
-    </div>
-    <div id="videoPlayer">
-      <span class="falta-flash">
-        Você precisa ter o flash instalado em seu computador<br/> para acessar esse site.<br/>
-        <a href="http://get.adobe.com/br/flashplayer/"  target="blank" title="Download Flash Player">Clique aqui para fazer o download</a>.
-      </span>
-    </div>
-    <script src="http://www.culturabrasil.com.br//_libs/mediaplayer/swfobject.js" type="text/javascript"></script>
-
-    <script>
-    var so = new SWFObject("http://www.culturabrasil.com.br/_libs/mediaplayer/player.swf","cam1","450","338","9");
-    
-    so.addParam("allowscriptaccess","always");
-    so.addParam("allowfullscreen","true");
-    so.addParam("wmode","transparent");
-    so.addVariable('volume', "75");
-    so.addVariable('controlbar', "over");
-    so.addVariable('autostart', "true");
-    so.addVariable('streamer', 'rtmp://200.136.27.12/live');
-    so.addVariable('file', "galeria");
-    so.addVariable('type', 'video');
-    so.write("videoPlayer");
-    </script>
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
-   <!-- comentario facebook -->
-   <fb:comments href="<?php echo $uri?>" numposts="3" width="495" publish_feed="true" style="margin-top:30px;"></fb:comments>
-   <hr />
-   <!-- /comentario facebook -->
-   </div>      
-   <!--/transmissao ao vivo -->
-   <!--Bate papo-->
-   <div class="span6">
-     <div class="page-header ao-vivo">
-      <h4>Bate Papo</h4>
-     </div> 
-     <?php echo html_entity_decode($displays["chat"][0]->getHtml()); ?>
-   </div>
-   <!--/Bate papo-->
-   <!--banner horizontal-->    
-   <div class="container">
-     <div class="banner-radio horizontal">
-       <script type='text/javascript'>
-         GA_googleFillSlot("cmais-assets-728x90");
-       </script>
-     </div>
+   
    </div>
    <!--banner horizontal-->
    </div>
