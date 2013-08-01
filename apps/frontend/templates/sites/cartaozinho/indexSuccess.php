7afde0b9c0597cde4a166542408dec4c759ecec6
doljak <jedoljak@gmail.com>
2013-08-01 19:13:11 -0300

    u

--- apps/frontend/templates/sites/cartaozinho/indexSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/indexSuccess.php
@@ -140,8 +140,8 @@
           $assets = Doctrine_Query::create()
             ->select('a.*')
             ->from('Asset a, Section s')
-            ->where('a.site_id = ?', (int)$site->id)
-            ->andWhere('s.slug = "enquetes-assets"')
+            ->where('s.slug = "enquetes-assets"')
+            ->andWhere('a.site_id = ?', (int)$site->id)
             //->andWhere('a.asset_type_id = 10')
             ->orderBy('a.id desc')
             ->execute();
