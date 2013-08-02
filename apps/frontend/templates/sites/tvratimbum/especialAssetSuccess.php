bf866df21d42c1bccdb3db22e9dab0f01fd7f1b1
Joseval <joseval@terra.com.br>
2013-08-02 15:39:12 -0300

    u

--- apps/frontend/templates/sites/tvratimbum/especialAssetSuccess.php
+++ apps/frontend/templates/sites/tvratimbum/especialAssetSuccess.php
@@ -1,41 +1,12 @@
 <?php
-  //echo "<br><br><br><br>site> ".$site_id." section> ".$section_id;
-  if((($site_id > 0)&&($site_id != "all")&&($section_id == ""))){
-    $assets = Doctrine_Query::create()
-      ->select('a.*')
-      ->from('Asset a, SectionAsset sa')
-      ->where('sa.asset_id = a.id')
-      ->andWhere('a.site_id = ?', (int)$site_id)
-      ->andWhereIn('sa.section_id',  array(19))
-      ->orderBy('a.id desc')
-      ->execute();
-  }elseif(($section_id > 0)&&(($site_id <= 0)||($site_id == "all"))){
-    $assets = Doctrine_Query::create()
-      ->select('a.*')
-      ->from('Asset a, SectionAsset sa')
-      ->where('sa.asset_id = a.id')
-      ->andWhereIn('sa.section_id',  array($section_id))
-      ->orderBy('a.id desc')
-      ->execute();
-  }elseif((($site_id > 0)&&($site_id != "all")&&($section_id > 0))){
-    $assets = Doctrine_Query::create()
-      ->select('a.*')
-      ->from('Asset a, SectionAsset sa')
-      ->where('sa.asset_id = a.id')
-      ->andWhere('a.site_id = ?', (int)$site_id)
-      ->andWhereIn('sa.section_id',  array($section_id))
-      ->orderBy('a.id desc')
-      ->execute();
-  }
-  else{
-    $assets = Doctrine_Query::create()
-      ->select('a.*')
-      ->from('Asset a, SectionAsset sa')
-      ->where('sa.asset_id = a.id')
-      ->andWhereIn('sa.section_id',  array(19))
-      ->orderBy('a.id desc')
-      ->execute();
-  }
+  $assets = Doctrine_Query::create()
+    ->select('a.*')
+    ->from('Asset a, SectionAsset sa')
+    ->where('sa.asset_id = a.id')
+    ->andWhere('a.site_id = ?', (int)$site_id)
+    ->andWhereIn('sa.section_id',  array(19))
+    ->orderBy('a.id desc')
+    ->execute();
   
   $sites = array();
   foreach($assets as $a){
@@ -64,7 +35,6 @@
     $('.carrossel').jcarousel({
       wrap: "both"
     });
-    startclock();
   })
   
   function setSection(i){
@@ -92,7 +62,7 @@
     document.clock.face.value= timeValue
     timerID = setTimeout("showtime()",1000);
     timerRunning = true;
-  }
+  }  
 </script>
 
 <div id="bodyWrapper">
@@ -100,67 +70,57 @@
   <div class="conteudoWrapper" align="center">
     
     <?php include_partial_from_folder('tvratimbum','global/top', array('site'=> $site,'section'=>$section)) ?>
-    
+
     <div class="conteudo internas">
       <div class="colunaMaior">
         <div class="trilha">
-          <p><a href="/">TV Rá Tim Bum</a></p><span>&gt;&gt;</span><a href="/especial">Especial</a>
+          <p><a href="/">TV Rá Tim Bum</a></p><span>&gt;&gt;</span><a href="/especial">Especial</a><span>&gt;&gt;</span><a href="<?php echo $asset->retriveUrl() ?>"><?php echo $asset->getTitle() ?></a>
         </div>
         <div id="box-especial-interna">
           <div class="wrapper">
             <div class="topo-esq"></div>
-            <div class="topo">
-              <a href="/especial" class="enunciado">Especial</a>
+              <div class="topo">
+                <a href="" class="enunciado">Especial</a>
+              </div>
+              <div class="chamada-especial">
+                <p><?php echo $asset->getTitle() ?></p>
+              </div>
+              <!--div class="banner-especial">
+                <img alt="<?php echo $asset->getTitle()?>" src="<?php echo $asset->retriveImageUrlByImageUsage("image-6") ?>" />
+              </div-->
+              <div class="box-especial-infoTxt">
+                <h3><?php echo $asset->getDescription() ?></h3>
+                <p><?php echo html_entity_decode($asset->AssetContent->getContent()) ?></p>
+              </div>
+              <hr>
+              <!--div class="btn-barra">
+                <span class="pontaBarra"></span>
+                <a href="">enviar para um amigo</a>
+                <span class="caudaBarra"></span>
+              </div-->
+              <span class="picote"></span>
             </div>
-            <div class="lista-programas">
-              <ul>
-              <?php foreach($assets as $a): ?>
-              <li>
-                <a href="<?php echo $a->retriveUrl()?>" class="aImg">
-                  <img alt="<?php echo $a->getTitle()?>" src="<?php echo $a->retriveImageUrlByImageUsage("image-3-b") ?>">
-                </a>
-                <a href="<?php echo $a->retriveUrl()?>" class="aTxt">
-                  <span class="nomeRlacionado"><?php echo $a->getTitle()?></span>
-                  <?php /* <span class="nomeItem"><?php echo $a->getDescription()?></span> */?>
-                </a>
-              </li>
-              <?php endforeach; ?>
-            </ul>
           </div>
-          
-          <?php /*
-          <div class="paginacao">
-            <ul>
-              <li><a href="" class="primeira"><span>&lt;&lt;</span>primeira</a></li>
-              <li><a href="" class="anterior">anterior</a></li>
-              <li><span class="nPaginas">3 de 10</span></li>
-              <li><a href="" class="proximo">próximo</a></li>
-              <li><a href="" class="ultima">última<span>&gt;&gt;</span></a></li>
-            </ul> 
+        </div>
+        <div class="coluna">
+          <div id="box-noAr">
+            <?php include_partial_from_folder('tvratimbum','global/live') ?>
+            <span class="picote"></span>
+            <?php include_partial_from_folder('tvratimbum','global/next') ?>
+            <span class="picote"></span>
+            <?php include_partial_from_folder('tvratimbum','global/important') ?>
+            <span class="picote"></span>
           </div>
-          */ ?>
           <hr />
-          <span class="picote"></span>
+          <?php //if(isset($vocesabia)) include_partial_from_folder('tvratimbum','global/display-1c-vocesabia', array('displays' => $vocesabia)) ?>
+          <hr />
         </div>
       </div>
-    </div>
-    <div class="coluna">
-      <div id="box-noAr">
-        <?php include_partial_from_folder('tvratimbum','global/live') ?>
-        <span class="picote"></span>
-        <?php include_partial_from_folder('tvratimbum','global/next') ?>
-        <span class="picote"></span>
-        <?php include_partial_from_folder('tvratimbum','global/important') ?>
-        <span class="picote"></span>
-      </div>
-      <hr />
-      <?php //if(isset($vocesabia)) include_partial_from_folder('tvratimbum','global/display-1c-vocesabia', array('displays' => $vocesabia)) ?>
-    </div>
-  </div>
-  
+
   <?php include_partial_from_folder('tvratimbum','global/footer') ?>
   <hr />
 </div>
 
 </div>
+
 
