ff0c06e9067f08fcf8f6b9168f4ea7d31e210155
renatareno <asdf@fpawks0142.local>
2013-08-08 15:24:52 -0300

    menu

--- apps/frontend/templates/sites/cmais/indexSuccess.php
+++ apps/frontend/templates/sites/cmais/indexSuccess.php
@@ -10,9 +10,9 @@
       <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
       
       <ul class="menutv">
-        <li><a href="http://tvcultura.cmais.com.br/" title="Assista à TV Cultura">Assista à TV Cultura</a></li>
+        <li><a href="http://cmais.com.br/aovivo" title="Assista à TV Cultura">Assista à TV Cultura</a></li>
         <li class="center"><a href="http://culturafm.cmais.com.br/" title="Ouça a Cultura FM">Ouça a Cultura FM</a></li>
-        <li><a href="http://tvcultura.cmais.com.br/grade" title="Confira à Programação da TV">Confira à Programação da TV</a></li>
+        <li><a href="http://tvcultura.cmais.com.br/grade" title="Confira à Programação da TV">Confira a Programação da TV</a></li>
       </ul>
 
       <!-- MIOLO -->
