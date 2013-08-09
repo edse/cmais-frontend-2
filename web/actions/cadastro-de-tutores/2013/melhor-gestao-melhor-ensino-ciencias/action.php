dbc0c036a5ec649e7d6de1ff874715618ec60849
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-09 14:02:38 -0300

    u

--- web/actions/cadastro-de-tutores/2013/melhor-gestao-melhor-ensino-ciencias/action.php
+++ web/actions/cadastro-de-tutores/2013/melhor-gestao-melhor-ensino-ciencias/action.php
@@ -3,6 +3,7 @@
 //$cpf = ltrim($cpf, "0");
 //$rg = str_replace(array('.','-'),"",$_REQUEST["rg"]);
 
+
 if($_REQUEST["captcha"]) {
   if($_REQUEST["cpf"]){
     if(date("Y-m-d H:i:s") < "2014-01-01 00:00:00") {
@@ -10,33 +11,33 @@ if($_REQUEST["captcha"]) {
         die("3");
       }
       else {
-        $csvFile = "/var/frontend/web/tutores-2013/melhor-gestao-melhor-ensino-ciencias/cadastro.csv";
-        $csvContent = $_REQUEST["nome"] . "," .
-                      $_REQUEST["cpf"] . "," .
-                      $_REQUEST["rg"] . "," .
-                      $_REQUEST["datanasc"] . "," .
-                      $_REQUEST["email"] . "," .
-                      $_REQUEST["telefone"] . "," .
-                      $_REQUEST["celular"] . "," .
-                      $_REQUEST["sms"] . "," .
-                      $_REQUEST["diretoria"] . "," .
-                      $_REQUEST["endereco"] . "," .
-                      $_REQUEST["numero"] . "," .
-                      $_REQUEST["bairro"] . "," .
-                      $_REQUEST["complemento"] . "," .
-                      $_REQUEST["cep"] . "," .
-                      $_REQUEST["cidade"] . "," .
-                      $_REQUEST["estado"] . "," .
-                      $_REQUEST["formacao"] . "," .
-                      $_REQUEST["experiente_ava_efap"] . "," .
-                      $_REQUEST["inscrito_serra_negra"] . "," .
-                      $_REQUEST["contrato_fpa"] . "," .
-                      $_REQUEST["experiente_tutor"] . "," .
-                      $_REQUEST["curso_tutoria_profort_efap"] . "," .
-                      $_REQUEST["vinculado_rede_ensino_sp"] . "," .
-                      $_REQUEST["rede_ensino_sp_tempo"] . "," .
-                      $_REQUEST["rede_ensino_sp_funcao"] . "," .
-                      $_REQUEST["em_exercicio_ensino_fundamental"] . "," .
+        $csvFile = "/var/frontend/web/tutores-2013/melhor-gestao-melhor-ensino-ciencias/cadastro2.csv";
+        $csvContent = str_replace(","," ",$_REQUEST["nome"]) . "," .
+                      str_replace(","," ",$_REQUEST["cpf"]) . "," .
+                      str_replace(","," ",$_REQUEST["rg"]) . "," .
+                      str_replace(","," ", $_REQUEST["datanasc"]) . "," .
+                      str_replace(","," ",$_REQUEST["email"]) . "," .
+                      str_replace(","," ",$_REQUEST["telefone"]) . "," .
+                      str_replace(","," ",$_REQUEST["celular"]) . "," .
+                      str_replace(","," ",$_REQUEST["sms"]) . "," .
+                      str_replace(","," ",$_REQUEST["diretoria"]) . "," .
+                      str_replace(","," ",$_REQUEST["endereco"]) . "," .
+                      str_replace(","," ",$_REQUEST["numero"]) . "," .
+                      str_replace(","," ",$_REQUEST["bairro"]) . "," .
+                      str_replace(","," ",$_REQUEST["complemento"]) . "," .
+                      str_replace(","," ",$_REQUEST["cep"]) . "," .
+                      str_replace(","," ",$_REQUEST["cidade"]) . "," .
+                      str_replace(","," ",$_REQUEST["estado"]) . "," .
+                      str_replace(","," ",$_REQUEST["formacao"]) . "," .
+                      str_replace(","," ",$_REQUEST["experiente_ava_efap"]) . "," .
+                      str_replace(","," ",$_REQUEST["inscrito_serra_negra"]) . "," .
+                      str_replace(","," ",$_REQUEST["contrato_fpa"]) . "," .
+                      str_replace(","," ",$_REQUEST["experiente_tutor"]) . "," .
+                      str_replace(","," ",$_REQUEST["curso_tutoria_profort_efap"]) . "," .
+                      str_replace(","," ",$_REQUEST["vinculado_rede_ensino_sp"]) . "," .
+                      str_replace(","," ",$_REQUEST["rede_ensino_sp_tempo"]) . "," .
+                      str_replace(","," ",$_REQUEST["rede_ensino_sp_funcao"]) . "," .
+                      str_replace(","," ",$_REQUEST["em_exercicio_ensino_fundamental"]) . "," .
                       (($_REQUEST['concordo'] == "on") ? "sim" : "não") . "\n";
         $csvFp = fopen($csvFile, 'a+');
         if(fwrite($csvFp, $csvContent)){
