dd6b238b5f28b564649339f825f3ea9c93324af3
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-09 21:28:28 -0300

    u

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -348,10 +348,6 @@ class mainActions extends sfActions
         $this->forwardObject($parm1Object);
       }
       else{
-        if ($parm1Object->slug == "culturabrasil") {
-          $this->getRequest()->setParameter('object', $parm1Object);
-          $this->forward('_section', 'index');
-        }
         $parm2Object = $this->parseWithObject($param2, $parm1Object);
         if($request->getParameter('debug') != "")
           print "<br>parseWithObject >>".$param2." - ".$param1;
@@ -365,6 +361,11 @@ class mainActions extends sfActions
               $this->getRequest()->setParameter('object', $parm2Object);
               $this->forward('_section', 'index');
             }
+            if ($parm1Object->slug == "culturabrasil") {
+              $this->getRequest()->setParameter('object', $parm2Object);
+              $this->forward('_section', 'index');
+            }
+            
             $parm3Object = $this->parseWithObject($param3, $parm2Object);
             if($parm3Object){
               if($request->getParameter('debug') != "")
