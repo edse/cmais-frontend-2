63deb0b8c65646734c53080ae451f00d90fbed15
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-12 19:51:05 -0300

    u

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -356,11 +356,12 @@ class mainActions extends sfActions
           if($request->getParameter('debug') != "")
             print "<br>main: 2>>".get_class($parm2Object).">>".$parm2Object->id;
           if(!$param3) {
+            /*
             if($parm1Object->slug == "culturabrasil" && get_class($parm2Object) == "Site") {
               header("Location: http://culturabrasil.cmais.br/programas/".$param2);
               die();
             }
-            else
+            else*/
               $this->forwardObject($parm2Object);
           }
           else{
