851d5da004cb28a3c51b065ba39fb3b2ff9aa8cc
doljak <jedoljak@gmail.com>
2013-08-14 20:57:41 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -760,10 +760,11 @@ $(document).ready(function(){
                     beginAgain();
                   })
                   $('.change-form').click(function(){
-                    $('#row4, #f4_maisinfo, #btn5').fadeIn('fast');
-                    $('#message, #btn4, .control-group.f4_mais').hide();
-                    $('#f4_cod_programa').find('option').attr('value','--').attr('selected','selected') 
-                    $('#f4_cod_veiculo, #f4_cod_assunto, #f4_mensagem').attr('disabled','disabled');
+                    $('#f4_mais').attr('checked','checked');
+                    $('#row4, #f4_maisinfo').fadeIn('fast');
+                    //$('#message, #btn4').hide();
+                    //$('#f4_cod_programa').find('option').attr('value','--').attr('selected','selected') 
+                    //$('#f4_cod_veiculo, #f4_cod_assunto, #f4_mensagem').attr('disabled','disabled');
                   })
                   $('.backForm').click(function(){
                     $('.row').fadeOut('fast',function(){
