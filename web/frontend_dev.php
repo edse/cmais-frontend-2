834f3df769e2292a7fcb58b643e8f47dfb712186
Joseval <joseval@terra.com.br>
2013-08-14 12:18:54 -0300

    up

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -12,10 +12,10 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '172.20.18.133
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 */
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+//require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
-sfContext::createInstance($configuration)->dispatch();
+//$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+//sfContext::createInstance($configuration)->dispatch();
 
 
 
