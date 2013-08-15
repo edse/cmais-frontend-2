cba0fb30be1b1f4a7fd529684167d007de309ee1
Joseval <joseval@terra.com.br>
2013-08-15 12:04:05 -0300

    a

--- apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
@@ -40,7 +40,7 @@
       <script src="/portal/controle-remoto/js/jquery.min.js" type="text/javascript"></script>
       <script src="/portal/controle-remoto/js/main.js" type="text/javascript"></script>
       
-      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+      <!--script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script-->
       <script src="/portal/controle-remoto/js/jquery.jplayer.min.js" type="text/javascript"></script>
       <script type="text/javascript" src="/portal/controle-remoto/js/jquery.jplayer.inspector.js"></script>
       <script type="text/javascript">
