bc16a8982509483e658974fbade8532128c8c910
Joseval <joseval@terra.com.br>
2013-08-15 11:29:11 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
@@ -1,16 +1,3 @@
-<?php use_helper('I18N', 'Date') ?>
-
-<!-- Le styles--> 
-<link href="/portal/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="/portal/js/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-<link href="/portal/css/tvcultura/sites/culturabrasil.css" rel="stylesheet" type="text/css" />
-
-<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-<script src="/portal/js/bootstrap/bootstrap.js"></script>
-
 <?php include_partial_from_folder('sites/culturabrasil', 'global/menu', array('siteSections' => $siteSections, 'displays' => $displays, 'section'=>$section)) ?>
 
 <!--section miolo--> 
