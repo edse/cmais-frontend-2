7c5e7936acef607c60c888c653048b2fdb536373
Joseval <joseval@terra.com.br>
2013-08-16 18:00:10 -0300

    u

--- web/portal/controle-remoto/ajax_pulsar.php
+++ web/portal/controle-remoto/ajax_pulsar.php
@@ -7,10 +7,13 @@ foreach ($json_output as $j) {
   //echo $j->interprete->{0};
   //echo "\n".$j->titulo->{0};
   //echo "\n".$j->duracao->{0}."\n";
-  $json = array(interprete => $j->interprete->{0},titulo => $j->titulo->{0},duracao => $j->duracao->{0});
+  $json = array(interprete => $j->interprete->{0},
+                titulo => $j->titulo->{0},
+                duracao => $j->duracao->{0}
+           );
 }
 
-json_encode($json);
+echo json_encode($json);
 
  
 ?>
