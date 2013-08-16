348f5c9cc6df82ab727411bacafee59faf2ca6f0
Joseval <joseval@terra.com.br>
2013-08-16 17:56:57 -0300

    u

---
+++ web/portal/controle-remoto/ajax_pulsar.php
@@ -0,0 +1,16 @@
+<?php
+$jsonurl = "pulsar.json";
+$json = file_get_contents($jsonurl);
+$json_output = json_decode($json);
+
+foreach ($json_output as $j) {
+  //echo $j->interprete->{0};
+  //echo "\n".$j->titulo->{0};
+  //echo "\n".$j->duracao->{0}."\n";
+  $json = array(interprete => $j->interprete->{0},titulo => $j->titulo->{0},duracao => $j->duracao->{0});
+}
+
+json_encode($json);
+
+ 
+?>
