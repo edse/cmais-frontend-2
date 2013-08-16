09cd78e9f8abbdfb332504aa6e832a98b4750f08
Joseval <joseval@terra.com.br>
2013-08-16 11:15:03 -0300

    u

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -3,7 +3,7 @@
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 
-//echo($_SERVER['REMOTE_ADDR']);
+echo($_SERVER['REMOTE_ADDR']);
 
 //die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 
