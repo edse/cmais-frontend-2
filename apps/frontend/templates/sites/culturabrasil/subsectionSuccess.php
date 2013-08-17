e4dfb0cf32c780d58fc08fc8aa10348615bff3cb
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-16 21:41:45 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
@@ -70,7 +70,7 @@ if(isset($pager)){
           <ul class="nav navbar-nav">
             <?php foreach($subsections as $s): ?>
               <?php if($s->getSlug() != "home"): ?>
-            <li<?php if($s->id == $section->id): ?> class="active"<?php endif; ?>><a href="<?php echo $s->getSlug() ?>"><?php echo $s->getTitle() ?></a></li>
+            <li<?php if($s->id == $section->id): ?> class="active"<?php endif; ?>><a href="<?php $s->retriveUrl() ?>"><?php echo $s->getTitle() ?></a></li>
               <?php endif; ?>
             <?php endforeach; ?>
           </ul>
