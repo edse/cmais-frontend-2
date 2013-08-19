de06869a42def4f03d02ebbd3571f16186da8198
renatareno <asdf@fpawks0142.local>
2013-08-19 16:37:01 -0300

    cb

--- apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
@@ -34,7 +34,7 @@
       </div>
       <script src="http://www.culturabrasil.com.br//_libs/mediaplayer/swfobject.js" type="text/javascript"></script>
       <script>
-        var so = new SWFObject("http://www.culturabrasil.com.br/_libs/mediaplayer/player.swf", "cam1", "640", "480", "9");
+        var so = new SWFObject("http://www.culturabrasil.com.br/_libs/mediaplayer/player.swf", "cam1", "450", "338", "9");
 
         so.addParam("allowscriptaccess", "always");
         so.addParam("allowfullscreen", "true");
