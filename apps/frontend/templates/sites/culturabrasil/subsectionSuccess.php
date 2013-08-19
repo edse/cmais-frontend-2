4a8649e46138f46e8c22e4dbd6766fb4a3426053
doljak <jedoljak@gmail.com>
2013-08-19 18:23:56 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
@@ -55,7 +55,7 @@ if(isset($pager)){
       <!--clounaprincipal-->
       <div class="row-fluid">
         <!--lista assets-->
-        <div class="lista-assets span8" style="*margin-left: 0px;">
+        <div class="lista-assets span8" <?php echo count($section->Asset).">>>>"?> style="*margin-left: 0px;">
           <h1><?php echo $site->getTitle() ?></h1>
           <p class="horario"><?php echo nl2br($program->getSchedule()) ?></p>
           
@@ -99,7 +99,7 @@ if(isset($pager)){
       <div class="row-fluid">
         
         <!--lista assets-->
-        <div class="lista-assets span8"<?php echo count($section->Asset).">>>>"?>>
+        <div class="lista-assets span8" ?>>
       <?php endif; ?>   
          
           <?php if(count($pager) > 0): ?>
