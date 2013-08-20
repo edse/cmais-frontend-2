8a47df54a369ea2399801107297c70b3bf320119
doljak <jedoljak@gmail.com>
2013-08-20 08:22:42 -0300

    u

--- apps/frontend/templates/culturabrasil.php
+++ apps/frontend/templates/culturabrasil.php
@@ -22,8 +22,9 @@
     <meta property="fb:admins" content="100000889563712"/>
     <meta property="fb:app_id" content="498504570187162"/>
 
-    <link rel="shortcut icon" href="http://cmais.com.br/portal/images/icon/favicon.png" type="image/x-icon" />
-
+    <!--link rel="shortcut icon" href="http://cmais.com.br/portal/images/icon/favicon.png" type="image/x-icon" /-->
+    <link rel="shortcut icon" href="http://cmais.com.br/portal/images/capaPrograma/culturabrasil/ico.png" type="image/x-icon" />
+    
     <!-- scripts -->
     <script src="http://cmais.com.br/portal/js/jquery-1.7.2.min.js" type="text/javascript"></script>
     <!--script type="text/javascript" src="/portal/js/jquery-ui/js/jquery-1.5.1.min.js"></script-->
@@ -73,7 +74,7 @@
 
   </head>
   <body>
-
+    testeje
     <?php echo $sf_content ?>
 
     <?php include_partial_from_folder('blocks', 'global/footer') ?>
