db0cbd0a4846d872a77eb4d570be504850f588fc
doljak <jedoljak@gmail.com>
2013-08-20 08:40:53 -0300

    u

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -31,7 +31,7 @@
     <link rel="shortcut icon" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico" type="image/x-icon" />
     <link rel="icon" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico">
     -->
-    <?
+    <?php
       echo count($site)
     ?>
     <link rel="SHORTCUT ICON" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.ico">
