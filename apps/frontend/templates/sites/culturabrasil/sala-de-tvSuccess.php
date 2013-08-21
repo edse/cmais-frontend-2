17dc9a2e6d8dd1524d4ab1791992fc5a3eaa5fbf
renatareno <asdf@fpawks0142.local>
2013-08-21 14:37:47 -0300

    cb

--- apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
@@ -65,12 +65,7 @@
         <h4>Twitter</h4>
       </div>
       <div class="ao-vivo">
-        <!--
-        <a class="twitter-timeline" href="https://twitter.com/culturabrasil2" data-widget-id="369534168884998144">Tweets by @culturabrasil2</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-        -->
-        <a class="twitter-timeline" href="https://twitter.com/culturabrasil2" data-widget-id="369534168884998144">Tweets by @culturabrasil2</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <iframe id="iframe-twitter" width='490' height='490' frameborder='0' src='http://cmais.com.br/culturabrasil/iframe-twitter.html' style="overflow:hidden;"></iframe>
       </div>
     </div>
   </div>
@@ -108,3 +103,4 @@
   </div>
 </section>
 <!--/Linha Central-->
+
