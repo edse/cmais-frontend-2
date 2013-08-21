7b3c8d256a88f119789005a63b79ee2f077c0a36
renatareno <asdf@fpawks0142.local>
2013-08-21 14:27:18 -0300

    cb

--- apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/sala-de-tvSuccess.php
@@ -60,10 +60,51 @@
       </div>
       <!--/share-->
     </div>
-    <iframe id="iframe-twitter" width='490' height='490' frameborder='0' src='http://cmais.com.br/culturabrasil/iframe-twitter.html' style="overflow:hidden;"></iframe>
+    <div class="span6">
+      <div class="page-header ao-vivo">
+        <h4>Twitter</h4>
+      </div>
+      <div class="ao-vivo">
+        <!--
+        <a class="twitter-timeline" href="https://twitter.com/culturabrasil2" data-widget-id="369534168884998144">Tweets by @culturabrasil2</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        -->
+        <a class="twitter-timeline" href="https://twitter.com/culturabrasil2" data-widget-id="369534168884998144">Tweets by @culturabrasil2</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div>
+    </div>
   </div>
 
  
-   <iframe width='1010' height='1000' frameborder='0' src='http://cmais.com.br/culturabrasil/iframe.html' style="overflow:hidden;"></iframe>
+     <div class="row-fluid container" style="margin-top:25px;">
+    <div class="span6" style="*margin-left:0px;">    
+     
+      <div class="page-header" style="margin:0;">
+        <h4>Google+</h4>
+      </div>
+      <div id="comments"></div>
+      <script>
+      gapi.comments.render('comments', {
+          href: "http://www.culturabrasil.com.br/sala-de-tv",
+          width: '490',
+          first_party_property: 'BLOGGER',
+          view_type: 'FILTERED_POSTMOD'
+      });
+      </script>
+    </div>
+
+   
+    <div id="comments-container" class="span6" style="*margin-left:0px;">    
+      <!--share-->
+      <div class="page-header" style="margin: 0;">
+        <h4>Facebook</h4>
+      </div>
+      <!-- comentario facebook -->
+      <div class="fb-comments" data-href="http://www.culturabrasil.com.br/sala-de-tv" data-width="490px" data-num-posts="10"></div>
+      <!-- /comentario facebook -->
+      <!--/share-->
+    </div>    
+    
+  </div>
 </section>
 <!--/Linha Central-->
