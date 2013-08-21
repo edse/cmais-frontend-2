19ce3dbf3d3352daf795e9db316df454a81aa0e7
doljak <jedoljak@gmail.com>
2013-08-21 18:39:14 -0300

    u

--- apps/frontend/templates/sites/cocorico/receitinhasSuccess.php
+++ apps/frontend/templates/sites/cocorico/receitinhasSuccess.php
@@ -150,7 +150,7 @@ $assets = $pager->getResults();
                <label>Li e concordo com o regulamento.</label>
             </div>
             <?php
-            if(isset($_GET["success"])==1 || $_GET["success"]==2){
+            if(isset($_GET["success"])==1 || isset($_GET["success"])==2){
             ?>
             <p class="sucesso">Hum... Essa receitinha parece uma delícia! Obrigado!</p>
             <?php  
