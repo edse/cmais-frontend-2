c8b238117dc2e30d141ac1d3e3d1cddf929ee3d4
Joseval <joseval@terra.com.br>
2013-08-21 16:41:07 -0300

    u

--- apps/frontend/templates/sites/cocorico/receitinhasSuccess.php
+++ apps/frontend/templates/sites/cocorico/receitinhasSuccess.php
@@ -1,3 +1,63 @@
+<?php
+include("/actions/includes/functions.php");
+
+$current_time = date("Y-m-d H:i:s", time()); 
+$expiration_time = "2013-08-30 00:00:00";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
+    if ($current_time < $expiration_time) {
+    
+      //$to = "tvcocorico@gmail.com";
+      $to = "valclimaster@gmail.com,joseval@terra.com.br"; 
+      $email = strip_tags($_REQUEST['email']);
+      $name = strip_tags($_REQUEST['nome']);
+      $from = "{$name} <{$email}>";
+      $subject = '[Cocoricó][TV Cocoricó] '.$name.' <'.$email.'>';
+      
+      $message = "Formulário Preenchido em " . date("d/m/Y") . " as " . date("H:i:s") . ", seguem abaixo os dados:<br><br>";
+      while(list($field, $value) = each($_REQUEST)) {
+        if(!in_array(ucwords($field), array('Form_action', 'X', 'Y', 'Enviar', 'Undefinedform_action')))
+          $message .= "<b>" . ucwords($field) . ":</b> " . strip_tags($value) . "<br>";
+      }
+      
+      $file_name = basename($_FILES['datafile']['name']);
+      $data = file_get_contents($_FILES['datafile']['tmp_name']); 
+      $file_contents = chunk_split(base64_encode($data));
+      $file_size = $_FILES['datafile']['size'];
+      $file_mime_type = getMimeType($_FILES['datafile']['name']);
+      $attach = array();
+      $attach[] = array($_FILES['datafile']['tmp_name'], $file_mime_type);      
+            
+      //Enviar sem anexo
+      if($_FILES['datafile']['name'] == ""){
+        $headers =  'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= "From: ".$from;
+
+        if(mail($to, $subject, $message, $headers)) {
+          $sucesso = "Hum... Essa receitinha parece ser uma delícia! Obrigado!";
+        }else{
+          $sucesso = "";
+          $erro = "1";
+        }
+      }
+          
+      if(sendMailAtt($to, $from, $subject, $message, $attach)) {
+        if (unlink($_FILES['datafile']['tmp_name'])) {
+          $sucesso = "Hum... Essa receitinha parece ser uma delícia! Obrigado!!";
+        }else{
+          $sucesso = "";
+          $erro = "1";
+        }
+      }
+      
+    }
+  }
+}
+?>    
+
+
 <?php 
 $assets = $pager->getResults();
 ?>
