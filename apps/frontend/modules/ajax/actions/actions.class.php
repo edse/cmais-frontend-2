4d3b3548f337232700e2006cec65d5959fecead3
Joseval <joseval@terra.com.br>
2013-08-21 19:23:50 -0300

    u

--- apps/frontend/modules/ajax/actions/actions.class.php
+++ apps/frontend/modules/ajax/actions/actions.class.php
@@ -2054,7 +2054,7 @@ EOT;
         ->limit('1')
         ->execute();
       
-      $return["noar"] = "";
+      $return["noar"] = 0;
       
       foreach($noar as $n){
         $titulo =   $n->getProgram()->getTitle();
