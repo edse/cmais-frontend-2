bd21bb2dd2469464498a051e57e184fe62d0c907
Joseval <joseval@terra.com.br>
2013-08-21 13:21:06 -0300

    u

--- web/actions/cocorico/sendmail.php
+++ web/actions/cocorico/sendmail.php
@@ -23,7 +23,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
       }
       
       //Enviar sem anexo
-      if(!$_FILES['datafile']['tmp_name']){
+      if($_FILES['datafile']['size'] <= 0){
         $headers =  'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= "From: ".$from;
