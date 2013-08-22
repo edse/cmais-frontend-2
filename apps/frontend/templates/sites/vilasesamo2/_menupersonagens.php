092c6b2df15199f783842f561d03bff8fb8063c1
renatareno <asdf@fpawks0142.local>
2013-08-22 10:00:07 -0300

    vila

--- apps/frontend/templates/sites/vilasesamo2/_menupersonagens.php
+++ apps/frontend/templates/sites/vilasesamo2/_menupersonagens.php
@@ -13,44 +13,44 @@
             <div class="slider-mask">
               <ul class="slider-target">
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens bel">
                     <a href="/vilasesamo2/beto" title="Bel" class="btn-bel">
                       <img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="Personagem" />
                     </a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens beto">
                     <a href="/vilasesamo2/personagens/beto" title="Beto" class="btn-beto"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens comecome">
                     <a href="/vilasesamo2/personagens/come-come" title="Come-come" class="btn-comecome"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens elmo">
                     <a href="/vilasesamo2/personagens/elmo" title="Elmo" class="btn-elmo"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens enio">
                     <a href="/vilasesamo2/personagens/enio" title="Enio" class="btn-enio"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens garibaldo">
                     <a href="/vilasesamo2/personagens/garibaldo" title="Garibaldo" class="btn-garibaldo"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens grover">
                     <a href="/vilasesamo2/personagens/grover" title="Grover" class="btn-grover"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
                 <li>
-                  <div class="inner personagens">
+                  <div class="inner personagens zoe">
                     <a href="/vilasesamo2/personagens/zoe" title="Zoe" class="btn-zoe"><img src="/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="Personagem" /></a>
                   </div>
                 </li>
