b7c8ef0d38762266792f62007e86c3bd55d3c0a4
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-22 16:03:53 -0300

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -700,7 +700,7 @@ class _assetActions extends sfActions
     if($this->site->getSlug() == "castelo" && $this->asset->getSlug() != "creditos" && !isset($_REQUEST['layout']))
       $this->setLayout(false);
 		
-		if($this->site->getSlug() == "culturabrasil"){
+		if($this->site->getSlug() == "culturabrasil" || $this->site->Program->Channel->getSlug() == "culturabrasil" || $this->site->getSlug() == "especiais-1"){
       $this->setLayout('culturabrasil');
     }	
 		if ($request->getHost() == "m.cmais.com.br") {
