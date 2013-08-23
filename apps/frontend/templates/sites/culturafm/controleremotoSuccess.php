c3292fd57a551e5415b34b6fd9ffe0848f3bd1ca
Joseval <joseval@terra.com.br>
2013-08-23 15:55:57 -0300

    u

--- apps/frontend/templates/sites/culturafm/controleremotoSuccess.php
+++ apps/frontend/templates/sites/culturafm/controleremotoSuccess.php
@@ -57,10 +57,11 @@
           ready: function () {
             $(this).jPlayer("setMedia", {
               mp3: "http://midiaserver.tvcultura.com.br:8003/;stream/1"
+              m4a: "rmtp://200.136.27.12/live"
             }).jPlayer("play");
           },
           swfPath: "/portal/controle-remoto/swf",
-          supplied: "mp3",
+          supplied: "mp3,rmtpa",
           solution: 'flash',
           cssSelectorAncestor: "#jp_container_2",
           wmode: "window"
