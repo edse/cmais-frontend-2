432c9901f5821c8e2cefe664096cc4cb3fca8452
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-23 17:00:13 -0300

    u

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -842,7 +842,7 @@ class _sectionActions extends sfActions
                   }
                 }
 
-                echo "total assets:" . count($assetIds) . "<br>";
+                echo "total assets:" . count($assetIds) . "<br>"; 
                 echo "ids: " . implode(", ", $assetIds) . "<br>";
                 echo "listed sections: " . implode(", ", $listedSections) . "<br>";
                 die();
