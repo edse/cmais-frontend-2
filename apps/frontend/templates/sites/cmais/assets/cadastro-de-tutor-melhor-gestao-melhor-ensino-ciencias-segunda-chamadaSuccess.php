4c50d75e98d5b81457d631fbfd2398a06484db93
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-08-26 20:28:26 -0300

    u

--- apps/frontend/templates/sites/cmais/assets/cadastro-de-tutor-melhor-gestao-melhor-ensino-ciencias-segunda-chamadaSuccess.php
+++ apps/frontend/templates/sites/cmais/assets/cadastro-de-tutor-melhor-gestao-melhor-ensino-ciencias-segunda-chamadaSuccess.php
@@ -416,7 +416,7 @@
                     <!--span class="linhaFundo"></span-->
                     
                     <div class="linha t1 exc" style="margin-top:40px;font-size: 12px;font-weight: bold;text-transform: uppercase;line-height: 26px;">
-                      <input type="checkbox" name="concordo" id="concordo" style="width:auto;float: left;" />Declaro que li e concordo com os termos do <a href="http://cmais.com.br/cadastro-de-tutores-2013/edital-para-cadastro-formacao-e-contratacao-de-professores-tutores-online-para-curso-2-e-3-de-ciencias" title="EDITAL PARA CADASTRO, FORMAÇÃO E CONTRATAÇÃO DE  PROFESSORES TUTORES ONLINE PARA CURSO 2 E 3 DE CIÊNCIAS" target="_blank" style="font-size: 12px;color: #ff6625;">edital</a>.
+                      <input type="checkbox" name="concordo" id="concordo" style="width:auto;float: left;" />Declaro que li e concordo com os termos do <a href="http://cmais.com.br/cadastro-de-tutores-2013/segunda-chamada-do-edital-para-cadastro-formacao-e-contratacao-de-professores-tutores-online-para-curso-2-e-3-de-ciencias-1" title="SEGUNDA CHAMADA DO EDITAL PARA CADASTRO, FORMAÇÃO E CONTRATAÇÃO DE PROFESSORES TUTORES ONLINE PARA CURSO 2 E 3 DE CIÊNCIAS" target="_blank" style="font-size: 12px;color: #ff6625;">edital</a>.
                     </div>
                     
                     
