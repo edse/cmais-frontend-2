8b734c64ad167b3360f0ff91ca8320cd00a33a24
doljak <jedoljak@gmail.com>
2013-08-27 16:15:54 -0300

    u

--- apps/frontend/templates/sites/cocorico/seriesSuccess.php
+++ apps/frontend/templates/sites/cocorico/seriesSuccess.php
@@ -1,6 +1,11 @@
 <?php
-
+ if(isset($asset)){
+    echo $asset->AssetVideo->getHeadline()."<br>";
+  echo $asset->getDescription()."<br>";
+  echo $asset->AssetVideo->getYoutubeId()."<br>";
+}
 $assets = $pager->getResults();
+$assets = $assets[0];
 ?>
 
 <link href="http://cmais.com.br/portal/css/tvcultura/sites/cocorico/brincadeiras.css" rel="stylesheet">
@@ -36,13 +41,6 @@ $assets = $pager->getResults();
 
     <!--row-->
     <div class="row-fluid conteudo"> 
-     <?php
-     if(isset($asset)){
-        echo $asset->AssetVideo->getHeadline()."<br>";
-        echo $asset->getDescription()."<br>";
-        echo $asset->AssetVideo->getYoutubeId()."<br>";
-      }
-     ?>  
      <h3 class="episodio"><?php echo $assets[0]->AssetVideo->getHeadline()?></h3>  
      <?php if($assets[0]->getDescription()!="") echo "<p>".$assets[0]->getDescription()."</p>"; ?> <p>
       <iframe width="940" height="529" src="http://www.youtube.com/embed/<?php echo $assets[0]->AssetVideo->getYoutubeId() ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
