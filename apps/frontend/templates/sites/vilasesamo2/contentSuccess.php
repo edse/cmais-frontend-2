47e23908c4b7ccc4e7a82a34b463f61d599744b4
doljak <jedoljak@gmail.com>
2013-08-27 17:23:34 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/contentSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/contentSuccess.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
 <link rel="stylesheet" href="/portal/css/tvcultura/sites/vilasesamo2/assets.css" type="text/css" />
 
+<script src="/portal/js/modernizr/modernizr.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="/portal/js/responsive-carousel/script.js"></script>
 <link type="text/css" rel="stylesheet" href="/portal/js/responsive-carousel/style-vilasesamo.css"/>
-<script src="/portal/js/modernizr/modernizr.min.js" type="text/javascript"></script>
 
 <script>
   $("body").addClass("interna");
