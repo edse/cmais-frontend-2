ce8773f9a093cacab529670e60db31e4fb97d95b
doljak <jedoljak@gmail.com>
2013-08-27 16:06:43 -0300

    u

--- apps/frontend/templates/sites/cocorico/seriesSuccess.php
+++ apps/frontend/templates/sites/cocorico/seriesSuccess.php
@@ -42,7 +42,7 @@ $assets = $pager->getResults();
     <div class="row-fluid conteudo">  
      <h3 class="episodio"><?php echo $assets[0]->AssetVideo->getHeadline()?></h3>  
      <?php if($assets[0]->getDescription()!="") echo "<p>".$assets[0]->getDescription()."</p>"; ?> <p>
-      <iframe width="940" height="529" src="http://www.youtube.com/embed/<?php echo  ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
+      <iframe width="940" height="529" src="http://www.youtube.com/embed/<?php echo $assets[0]->AssetVideo->getYoutubeId() ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
     </div>
     <!-- /row-->
 
