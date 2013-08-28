43dc5e49171b83d6376bd166def957462ee9becd
doljak <jedoljak@gmail.com>
2013-08-28 19:51:16 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -145,6 +145,13 @@ $(document).ready(function(){
                         <p class="help-block">Não sabe seu CEP? <a href="http://www.buscacep.correios.com.br/" target="_blank" style="color:#00ccff">Clique aqui</a> e consulte o Correio.</p>
                       </div>
                     </div>
+                    <div class="control-group">
+                      <label class="control-label" for="f2_exterior">Reside no exterior?</label>
+                      <div class="controls">
+                        <input type="checkbox" name="f2_exterior" id="f2_exterior" value="1" onchange="toggleExterior();" />
+                        <!-- <select id="f2_exterior" name="f2_exterior" onchange="toggleExterior();"></select> -->
+                      </div>
+                    </div>
                     <div id="f2_brasil">
                       <div class="control-group">
                         <label class="control-label" for="f2_estado">Estado</label>
@@ -158,13 +165,7 @@ $(document).ready(function(){
                           <select id="f2_local" name="f2_local"></select>
                         </div>
                     </div>
-                    <div class="control-group">
-                      <label class="control-label" for="f2_exterior">Reside no exterior?</label>
-                      <div class="controls">
-                        <input type="checkbox" name="f2_exterior" id="f2_exterior" value="1" onchange="toggleExterior();" />
-                        <!-- <select id="f2_exterior" name="f2_exterior" onchange="toggleExterior();"></select> -->
-                      </div>
-                    </div>
+                    
                     <div class="control-group f2_exterior">
                       <label class="control-label" for="f2_localexterior">Pais</label>
                       <div class="controls">
