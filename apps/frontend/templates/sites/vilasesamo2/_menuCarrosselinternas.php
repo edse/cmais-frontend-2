36f17e6992b4537671a7b2a817cb4307b70a6bfd
renatareno <asdf@fpawks0142.local>
2013-08-30 13:37:38 -0300

    v

--- apps/frontend/templates/sites/vilasesamo2/_menuCarrosselinternas.php
+++ apps/frontend/templates/sites/vilasesamo2/_menuCarrosselinternas.php
@@ -3,8 +3,8 @@
       <div class="slider">
           <div class="slider-mask-wrap">
             <div class="slider-mask">
-              <ul class="slider-target row-fluid">
-                <li class="video span3">
+              <ul class="slider-target">
+                <li class="video">
                   <div class="inner personagens bel">
                     <a href="/vilasesamo2jogos/nomedojogo1" title="Nome do jogo 1" class="btn-bel">
                      <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -12,7 +12,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/jogos/nomedojogo1" title="Nome do Jogo 1">Nome do Jogo 1</a>
                 </li>
-                <li class="jogo span3">
+                <li class="jogo">
                   <div class="inner personagens beto">
                     <a href="/vilasesamo2/jogos/nomedojogo2" title="Nome do jogo 2" class="">
                      <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -20,7 +20,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/jogos/nomedojogo2" title="Nome do Jogo 2">Nome do Jogo 2</a>
                 </li>
-                <li class="atividade span3">
+                <li class="atividade">
                   <div class="inner personagens comecome">
                     <a href="/vilasesamo2/jogos/nomedojogo3" title="Nome do jogo 3" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -28,7 +28,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/jogos/nomedojogo3" title="Nome do Jogo 3">Nome do Jogo 3</a>
                 </li>
-                <li class="span3">
+                <li>
                   <div class="inner personagens elmo">
                     <a href="/vilasesamo2/jogos/nomedojogo4" title="Nome do jogo 4" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -36,7 +36,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/jogos/nomedojogo4" title="Nome do Jogo 4">Nome do Jogo 4</a>
                 </li>
-                <li class="span3">
+                <li>
                   <div class="inner personagens enio">
                     <a href="/vilasesamo2/jogos/nomedojogo5" title="Nome do Jogo 5" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -44,7 +44,7 @@
                    </div>
                    <a class="nome" href="/vilasesamo2/jogos/nomedojogo5" title="Nome do Jogo 5">Nome do Jogo 5</a>
                 </li>
-                <li class="span3">
+                <li>
                   <div class="inner personagens garibaldo">
                     <a href="/vilasesamo2/jogos/nomedojogo6" title="Nome do Jogo 6" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -52,7 +52,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/personagens/garibaldo" title="Nome do Jogo 6">Nome do Jogo 6</a>
                 </li>
-                <li class="span3">
+                <li>
                   <div class="inner personagens grover">
                     <a href="/vilasesamo2/jogos/nomedojogo7" title="Nome do Jogo 7" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
@@ -60,7 +60,7 @@
                   </div>
                   <a class="nome" href="/vilasesamo2/jogos/nomedojogo7" title="Nome do Jogo 7">Nome do Jogo 7</a>
                 </li>
-                <li class="span3"> 
+                <li>
                   <div class="inner personagens zoe">
                     <a href="/vilasesamo2/jogos/nomedojogo8" title="Nome do Jogo 8" class="">
                       <img src="/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg">
