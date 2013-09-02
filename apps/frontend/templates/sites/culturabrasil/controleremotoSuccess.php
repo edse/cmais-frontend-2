f44d5056bb2d2f3b84b61c0e082195d9843c46ca
doljak <jedoljak@gmail.com>
2013-09-02 17:18:06 -0300

    u

--- apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/controleremotoSuccess.php
@@ -292,10 +292,10 @@
         
       </section>  
       <!-- /lista a seguir -->
-      
+      <a href="https://docs.google.com/forms/d/1CWq8uyNNxMQTUpAA5dNSK-PXj6kPoF_zJ-uCAIoFDok/viewform" class="cr-problemas" title="Está com problemas? Dê a sua opinião" target="_blank">Está com problemas? Dê a sua opinião</a>
       <!-- banner -->
       <section class="cr-banner">
-        <a href="https://docs.google.com/forms/d/1CWq8uyNNxMQTUpAA5dNSK-PXj6kPoF_zJ-uCAIoFDok/viewform" class="cr-problemas" title="Está com problemas? Dê a sua opinião" target="_blank">Está com problemas? Dê a sua opinião</a>
+        
         <div class="cr-box-banner">
           
           <script type='text/javascript'>
