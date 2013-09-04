3f2e19bdd6ad537a2fa4aba248b08faf5cbc27d2
renatareno <asdf@fpawks0142.local>
2013-09-04 14:31:39 -0300

    v

--- apps/frontend/templates/sites/vilasesamo2/contentSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/contentSuccess.php
@@ -103,7 +103,7 @@
           <input type="text" id="email" placeholder="Email" value="Email">
         </div>
       </div>
-       <div class="control-group span2 idade">
+       <div class="control-group span2 idade anexo">
         <label class="control-label sprite-ico-anexo" for="anexo"></label>
         <div class="controls">
           <input id="datafile" type="file" name="datafile" size="1">
