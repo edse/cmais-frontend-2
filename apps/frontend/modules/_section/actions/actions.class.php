3a24e0ab2fb73cefd44134ca2b2a7df9bd1c79d6
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-09-05 18:02:32 -0300

    u
    u

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -832,7 +832,7 @@ class _sectionActions extends sfActions
             }
             
             elseif( ($this->site->getSlug() == 'especiais-1') && (in_array($this->section->getSlug(), array('home', 'home-page', 'homepage'))) ) {
-              
+               
               $siteAssets = Doctrine_Query::create()
                 ->select('a.*')
                 ->from('Asset a, SectionAsset sa')
