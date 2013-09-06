b072c554e8903c5a6f315a9c8a6bba30d2364437
renatareno <asdf@fpawks0142.local>
2013-09-06 11:49:48 -0300

    v

--- apps/frontend/templates/sites/vilasesamo2/content-videosSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/content-videosSuccess.php
@@ -12,10 +12,10 @@
 <!-- /HEADER -->
 <div id="content">
   <section class="scroll row-fluid">
-    <h3><i class="sprite-icon-clipes-med"></i>Atividades<i class="seta-scroll sprite-scroll-videos"></i></h3>
+    <h3><i class="sprite-icon-clipes-med"></i>Vídeos<i class="seta-scroll sprite-scroll-videos"></i></h3>
   </section>
   <section class="filtro row-fluid">
-    <h3><i class="sprite-icon-clipes-med"></i>Atividades<a class="todos-assets"><i class="sprite-btn-voltar-videos"></i><p>todos os jogos</p></a></h3>
+    <h3><i class="sprite-icon-clipes-med"></i>Vídeos<a class="todos-assets"><i class="sprite-btn-voltar-videos"></i><p>todos os jogos</p></a></h3>
     <div class="conteudo-asset">
       <h2>nome do jogo</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat massa, ullamcorper sit amet elementum sed, sollicitudin non massa. Mauris ante tortor, feugiat quis dolor id, congue pharetra nisl. Maecenas suscipit eu diam et pellentesque. Morbi fermentum libero ac diam tristique, in egestas lectus rhoncus. Proin ut pellentesque massa. Morbi metus.</p>
