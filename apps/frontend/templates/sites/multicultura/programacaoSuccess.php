bddb2f22f2e2097d826c80a963d05cdd9a682674
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-09-06 15:33:14 -0300

    u

--- apps/frontend/templates/sites/multicultura/programacaoSuccess.php
+++ apps/frontend/templates/sites/multicultura/programacaoSuccess.php
@@ -216,7 +216,11 @@ $prevDateUrl = $base_url."/".str_replace("/","-",$prevDate);
                   <div class="grade toggle" style="display:none; width:auto; padding-bottom:25px;">
                     <div class="capa-foto">
                       <!-- <p class="reprise">18:30 - Reprise deste episódio</p> -->
+                      <?php if ($d->retriveLiveImage()): ?>
                       <img src="<?php echo $d->retriveLiveImage() ?>" alt="<?php echo $d->retriveTitle() ?>" />
+                      <?php else: ?>
+                      <img src="/portal/images/capaPrograma/univesptv/univesptv-logo.jpg" alt="<?php echo $d->retriveTitle() ?>" />
+                      <?php endif; ?>
                       <?php if($d->image_source != ""): ?><p class="legenda"><?php echo $d->image_source ?></p><?php endif; ?>
                     </div>
                     <?php if($d->getTitle() != ""): ?>
