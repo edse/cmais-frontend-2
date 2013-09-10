0f6b38408e94533a978a4d37d3d3c68f1df21927
doljak <jedoljak@gmail.com>
2013-09-10 12:33:44 -0300

    u

--- web/testes/comic/index.php
+++ web/testes/comic/index.php
@@ -18,7 +18,7 @@
 
  
 
-  <link href="css/button.css?<?php echo time(); ?>" media="screen" rel="stylesheet" type="text/css" />
+  <link href="css/buttons.css?<?php echo time(); ?>" media="screen" rel="stylesheet" type="text/css" />
   <link href="css/style.css?<?php echo time(); ?>" media="screen" rel="stylesheet" type="text/css" />
   <script src="http://cmais.com.br/portal/js/jquery-1.4.4.min.js?<?php echo time(); ?>" type="text/javascript"></script>
   <script src="http://cmais.com.br/portal/js/rails.js?<?php echo time(); ?>" type="text/javascript"></script>
