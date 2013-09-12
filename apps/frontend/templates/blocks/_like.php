83cab6b36a142ea6580d6a0448db0f329b788681
doljak <jedoljak@gmail.com>
2013-09-12 16:56:44 -0300

    u

--- apps/frontend/templates/blocks/_like.php
+++ apps/frontend/templates/blocks/_like.php
@@ -9,16 +9,17 @@
               <fb:like href="<?php if($site->getFacebookUrl()): ?><?php echo $site->getFacebookUrl() ?><?php else: ?><?php echo $uri ?><?php endif; ?>" layout="button_count" show_faces="false" send="true" width="160"></fb:like>
             </div>
             <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php if($site->getTwitterAccount()): ?><?php echo $site->getTwitterAccount() ?><?php else: ?>tvcultura<?php endif; ?>">Tweet</a>
-            <ul>
+            
               <?php if($site->getSlug()=="culturalivre"):?> 
-              <li>
+              
                 <!-- Place this code where you want the badge to render. -->
                 <a href="https://plus.google.com/109467063583927278542?prsrc=3"
                   rel="publisher" target="_top" style="text-decoration:none;">
                   <img src="//ssl.gstatic.com/images/icons/gplus-16.png" alt="Google+" style="margin: 2px;border:0;width:20px;height:20px;"/>
                 </a>
-              </li> 
-              <?php endif;?>                        
+               
+              <?php endif;?> 
+            <ul>                       
               <li><a class="fb" href="<?php if($site->getFacebookUrl()): ?><?php echo $site->getFacebookUrl() ?><?php else: ?>http://facebook.com/tvcultura<?php endif; ?>" title="Facebook" target="_blank">Facebook</a></li>
               <li><a class="twt" href="<?php if($site->getTwitterUrl()): ?><?php echo $site->getTwitterUrl() ?><?php else: ?>http://twitter.com/tvcultura<?php endif; ?>" title="Twitter" target="_blank">Twitter</a></li>
               <li><a class="ytb" href="http://youtube.com/<?php if($site->getYoutubeChannel()): ?><?php echo $site->getYoutubeChannel() ?><?php else: ?>cultura<?php endif; ?>" title="YouTube" target="_blank">YouTube</a></li>
