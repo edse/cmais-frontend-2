5d824db73904120cce10449af03da3f4a1ec049a
renatareno <asdf@fpawks0142.local>
2013-09-13 11:05:41 -0300

    v

--- apps/frontend/templates/sites/vilasesamo2/incluir-brincandoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/incluir-brincandoSuccess.php
@@ -28,8 +28,8 @@
       </div>
      
       <div class="span4 dica">
-        <span class="sprite-aspa-esquerda"></span>
-        <h2><a href="#">guia do brincar inclusivo</a></h2>
+        
+        <h2><span class="sprite-aspa-esquerda"></span>guia do brincar inclusivo</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat metus ut leo interdum eleifend. Duis vel mauris et nunc posuere vehicula a id arcu. Maecenas malesuada ante ac consequat viverra. Vivamus tempor, nulla quis facilisis ullamcorper, tortor odio elementum eros, sit amet cursus felis elit vel diam. Fusce fringilla, nulla eu luctus lacinia, risus turpis varius orci, vel fringilla sem eros eu diam. Pellentesque sodales cursus elit, ac suscipit eros consectetur nec.
         Aenean at metus.</p>
         <span class="sprite-aspa-direita"></span>
