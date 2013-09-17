096b25bda16f7f00d52cccf04e15508eecb01b13
Joseval <joseval@terra.com.br>
2013-09-17 12:18:17 -0300

    fpa

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
+//require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/var/frontend/config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
