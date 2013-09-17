52e9065c56d063d5b6dd62359bfd6be0123c86ef
Joseval <joseval@terra.com.br>
2013-09-17 11:04:17 -0300

    fpa

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -28,10 +28,9 @@ class mainActions extends sfActions
     }
     
     if(($request->getHost() == "fpa.com.br")||($request->getHost() == "www.fpa.com.br")){
-      //die('123');
+      die('123');
       $this->getRequest()->setParameter('object', $this->site = Doctrine::getTable('Site')->findOneBySlug("sic"));
       $this->forward('_site', 'index');
-      die("FPA");
     }
     
     $subdomain = @current(explode(".", $request->getHost()));
