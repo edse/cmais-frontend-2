93bd52ce28df6b210fccf0071fafdcb07d160dd5
doljak <jedoljak@gmail.com>
2013-09-18 20:57:18 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
@@ -68,7 +68,7 @@
         if(number==3)contC++;
         if(number==4)contD++;
         for(var j=0; j<classes.length; j++){
-          if(classes[j] == 'q-pers-0'+ number || contA >= 2 || contB >= 2 || contC >= 2 || contD >= 2){
+          if(classes[i] == 'q-pers-0'+ number || contA >= 2 || contB >= 2 || contC >= 2 || contD >= 2){
             cont = 0; 
           }else{
             $(this).addClass('q-pers-0'+ number);
