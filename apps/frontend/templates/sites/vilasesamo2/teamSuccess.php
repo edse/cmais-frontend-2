7d4f21fdf5e0cb29a787cb4245b882bd048d2f6d
doljak <jedoljak@gmail.com>
2013-09-18 15:47:16 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
@@ -6,10 +6,10 @@
 
 </script>
 <style>
-.q-pers-01{display:block; width: 140px; height:140px; background:#000000;}
-.q-pers-02{display:block; width: 200px; height:200px; background:blue;}
-.q-pers-03{display:block; width: 300px; height:300px; background:red;}
-.q-pers-04{display:block; width: 350px; height:350px; background:yellow;}
+.variable-sizes .q-pers-01{display:block; width: 140px; height:140px; background:#000000;}
+.variable-sizes .q-pers-02{display:block; width: 200px; height:200px; background:blue;}
+.variable-sizes .q-pers-03{display:block; width: 300px; height:300px; background:red;}
+.variable-sizes .q-pers-04{display:block; width: 350px; height:350px; background:yellow;}
 </style>
 <!-- HEADER -->
 <?php include_partial_from_folder('sites/vilasesamo2', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))
@@ -23,14 +23,14 @@
       <a href="javascript:;" id="toggle-sizes">teste</a>
       
       <div id="container-personagens" class="asset">
-        <div class="element" data-order="1"></div>
-        <div class="element" data-order="2"></div>
-        <div class="element" data-order="3"></div>
-        <div class="element" data-order="4"></div>
-        <div class="element" data-order="5"></div>
-        <div class="element" data-order="6"></div>
-        <div class="element" data-order="7"></div>
-        <div class="element" data-order="8"></div>
+        <div class="element q-pers-01" data-order="1"></div>
+        <div class="element q-pers-02" data-order="2"></div>
+        <div class="element q-pers-03" data-order="3"></div>
+        <div class="element q-pers-04" data-order="4"></div>
+        <div class="element q-pers-05" data-order="5"></div>
+        <div class="element q-pers-06" data-order="6"></div>
+        <div class="element q-pers-07" data-order="7"></div>
+        <div class="element q-pers-08" data-order="8"></div>
       </div>
     </div>
   </section>
@@ -38,32 +38,14 @@
 <script src="/portal/js/isotope/jquery.isotope.min.js"></script>
 <script>
   var $container = $('#container-personagens');
-  
-  // add randomish size classes
-  $container.find('.element').each(function(){
-    var $this = $(this),
-    number = parseInt( $this.attr('data-order'));
-    if ( number == 1 || number == 5 ) {
-      $this.addClass('q-pers-01');
-    }
-    if ( number == 2 || number == 6 ) {
-      $this.addClass('q-pers-02');
-    }
-    if ( number == 3 || number == 7 ) {
-      $this.addClass('q-pers-03');
-    }
-    if ( number == 4 || number == 8 ) {
-      $this.addClass('q-pers-04');
-    }
-  });
-  
+
   $container.isotope({
     itemSelector : '.element',
   });
   
   $('#toggle-sizes').click(function(){
     console.log('clk');
-    $container.isotope('reLayout');
+    $container.toggleClass('variable-sizes').isotope('reLayout');
     return false;
   });
 </script>  
