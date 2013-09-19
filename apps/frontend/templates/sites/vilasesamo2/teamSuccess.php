71e916faa61e233623fbc21fa9e8d6a4aca178cf
doljak <jedoljak@gmail.com>
2013-09-19 16:06:12 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
@@ -6,7 +6,7 @@
 
 </script>
 <style>
- #container-personagens{width:950px;height: 750px;}
+ #container-personagens{width:950px;height:100%;}
  .q-pers-01{float:left;width: 140px; height:140px; margin:5px;background:#000000;}
  .q-pers-02{float:left; width: 200px; height:200px; margin:5px;background:blue;}
  .q-pers-03{float:left; width: 300px; height:300px; margin:5px;background:red;}
