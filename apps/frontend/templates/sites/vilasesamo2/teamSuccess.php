2a710aebb9a97f1e9a079c51dfd20fc1c08a5134
doljak <jedoljak@gmail.com>
2013-09-19 10:59:01 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
@@ -65,7 +65,7 @@
       if(number==3)contC++;
       if(number==4)contD++; 
       
-      if(contA >=2){
+      if(contA >=2 && contB >=2 && contC >=2 && contD >=2){
         contWhile--;
       }else{
         classes[vetor] = number;
