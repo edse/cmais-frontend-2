0d7662bb731a305169fb5b7149981648eff3527e
doljak <jedoljak@gmail.com>
2013-09-20 19:19:07 -0300

    u

--- apps/frontend/templates/sites/cartaozinho/indexSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/indexSuccess.php
@@ -17,13 +17,13 @@
       <div class="topo-programa">
         
         <?php 
-          if(date('d')>=20){
+          if(date('d')>=21){
             if(date("H:i") >= "14:10"){
         ?>      
               <style>
                 .menu-gabi{width:559px; float:right;}
                 .horario-gabi{margin-top:70px!important; margin-right:6px;}
-                .juiza-nova{display:block;width:312px; height:486px; position:absolute; top:0; left: 0; background:url(/portal/images/capaPrograma/cartaozinho/gabi-estreia.png) no-repeat;}
+                .juiza-nova{display:block;width:312px; height:486px; position:absolute; top:71px; left: 0; background:url(/portal/images/capaPrograma/cartaozinho/gabi-estreia.png) no-repeat;}
               </style>
               <div class='juiza-nova'></div>
         <?php      
