a9613ad818cd8d41ddb82ce52d5d5b96884aeac2
doljak <jedoljak@gmail.com>
2013-09-25 16:34:45 -0300

    u

--- apps/frontend/templates/sites/jcprimeiraedicao/participeSuccess.php
+++ apps/frontend/templates/sites/jcprimeiraedicao/participeSuccess.php
@@ -85,7 +85,7 @@
                   <div class="msgErro" style="display:none">
                     <span class="alerta"></span>
                     <div class="boxMsg">
-                      <p class="aviso">Sua mensagem não pode ser enviada.</p>
+                      <p class="aviso">Sua pergunta não pode ser enviada.</p>
                       <p>Confirme se todos os campos foram preenchidos corretamente e verifique seus dados. Você pode ter esquecido de preencher algum campo ou errado alguma informação.</p>
                     </div>
                     <hr />
@@ -93,8 +93,8 @@
                   <div class="msgAcerto" style="display:none">
                     <span class="alerta"></span>
                     <div class="boxMsg">
-                      <p class="aviso">Mensagem enviada com sucesso!</p>
-                      <p>Obrigado por entrar em contato com nosso programa. Em breve retornaremos sua mensagem.</p>
+                      <p class="aviso">Pergunta enviada com sucesso!</p>
+                      <p>Obrigado por participar do programa.</p>
                     </div>
                     <hr />
                   </div>
@@ -149,24 +149,10 @@
                       <option value="Tocantins">TO</option>
                   </select>
                   </div>
-                  <div class="linha t6">
-                    <label>assunto</label>
-                    <br />
-                    <select style="width:150px;" id="assunto" name="assunto" class="required">
-                      <option value="">- Selecione -</option>
-                      <option value="Elogio">Elogio</option>
-                      <option value="Crítica">Crítica</option>
-                      <option value="Comentário">Comentário</option>
-                      <option value="Sugestão">Sugestão</option>
-                      <option value="Pedido de Informação">Pedido de Informação</option>
-                      <?php if(in_array($site->getSlug(), array('jornaldacultura','jornaldacultura','reportereco'))): ?>
-                      <option value="Compra de DVD">Compra de DVD</option>
-                      <?php endif; ?>
-                    </select>
-                  </div>
+                  
                   <div class="linha t3">
-                    <label>mensagem</label>
-                    <textarea name="mensagem" id="mensagem" onKeyDown="limitText(this,1000,'#textCounter');"></textarea>
+                    <label>pergunta</label>
+                    <textarea name="pergunta" id="pergunta" onKeyDown="limitText(this,1000,'#textCounter');"></textarea>
                     <p class="txt-10"><span id="textCounter">1000</span> caracteres restantes</p>                                       
                   </div>
                   <div class="linha t3 codigo" id="captchaimage">
@@ -177,7 +163,7 @@
                     </a>
                     <label class="msg" for="captcha">Digite no campo abaixo os caracteres que voc&ecirc; v&ecirc; na imagem:</label>
                     <input class="caracteres" type="text" maxlength="6" name="captcha" id="captcha" />
-                    <input class="enviar" type="submit" name="enviar" id="enviar" value="enviar mensagem" style="cursor:pointer" />
+                    <input class="enviar" type="submit" name="enviar" id="enviar" value="enviar pergunta" style="cursor:pointer" />
                     <img src="http://cmais.com.br/portal/images/ajax-loader.gif" alt="enviando..." style="display:none" width="16px" height="16px" id="ajax-loader" />
                   </div>
                 </form>
@@ -267,10 +253,8 @@
               required: true,
               minlength: 2
             },
-            assunto:{
-              required: true
-            },
-            mensagem:{
+
+            pergunta:{
               required: true
             },
             captcha: {
@@ -283,8 +267,7 @@
             email: "Digite um e-mail v&aacute;lido. Este campo &eacute; Obrigat&oacute;rio.",
             cidade: "Este campo &eacute; Obrigat&oacute;rio.",
             estado: "Este campo &eacute; Obrigat&oacute;rio.",
-            assunto: "Este campo &eacute; Obrigat&oacute;rio.",
-            mensagem: "Este campo &eacute; Obrigat&oacute;rio.",
+            pergunta: "Este campo &eacute; Obrigat&oacute;rio.",
             captcha: "Digite corretamente o código que está ao lado."
           },
           success: function(label){
