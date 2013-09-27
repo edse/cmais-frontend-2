17928515fe0427e565b5ccc662f4219e2771c0c9
doljak <jedoljak@gmail.com>
2013-09-27 15:35:18 -0300

    u

--- apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
+++ apps/frontend/templates/sites/cocorico/tvcocoricoSuccess.php
@@ -65,9 +65,9 @@
               }
             ?>
             
-        <!--a class="convidado span12" href="http://tvcultura.cmais.com.br/cocorico/tvcocorico/convidados/<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->Asset->getSlug() ?>" title="Próximo convidado: <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>" />
+        <a class="convidado span12" href="http://tvcultura.cmais.com.br/cocorico/tvcocorico/convidados/<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->Asset->getSlug() ?>" title="Próximo convidado: <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>"><img src="<?php echo $display_img_src ?>" alt="<?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>" />
           <?php echo $displays_tv_cocorico['destaque-tv-cocorico'][0]->getTitle() ?>
-        </a-->
+        </a>
         <!--a href="<?php echo $site->retriveUrl(); ?>/convidados" title="Convidados">
           <span class="mais"></span>
         </a-->
