477d5e30f4afadfb031c8283e3076123b8d7574d
Joseval <Joseval@fpawks0146-2.local>
2013-10-01 18:11:58 -0300

    Vídeos Mais Cultura

---
+++ apps/frontend/templates/sites/maiscultura/videoSuccess.php
@@ -0,0 +1,145 @@
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/<?php echo $section->Site->getSlug() ?>.css" type="text/css" />
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/videos.css" type="text/css" />
+<script type="text/javascript">
+$(function(){
+  //carrossel
+    $('.carrossel').jcarousel({
+        wrap: "both",
+        scroll: 1
+    });
+});
+</script>
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+
+    <!-- / CAPA SITE -->
+    <div id="capa-site">
+
+      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
+
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+        <?php if(isset($program) && $program->id > 0): ?>
+        <div class="topo-programa">
+          <!--h2>
+            <a href="<?php echo $program->retriveUrl() ?>">
+              <img src="http://midia.cmais.com.br/programs/<?php echo $site->getImageThumb() ?>" alt="<?php echo $site->getTitle() ?>" title="<?php echo $site->getTitle() ?>" />
+            </a>
+          </h2-->
+          <?php endif; ?>
+
+          <?php if(isset($program) && $program->id > 0): ?>
+          <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+          <?php endif; ?>
+
+          <?php if(isset($program) && $program->id > 0): ?>
+          <!-- horario -->
+          <div id="horario">
+            <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
+          </div>
+          <!-- /horario -->
+        </div>
+        <?php endif; ?>
+
+        <?php if(isset($siteSections)): ?>
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+          
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+          <?php if(isset($section->slug)): ?>
+            <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
+            <div class="navegacao txt-10">
+              <a href="<?php echo $site->retriveUrl() ?>" title="Home">Home</a>
+              <span>&gt;</span>
+              <a href="<?php echo $site->retriveUrl() ?>/videos" title="Vídeos">Vídeos</a>
+              <span>&gt;</span>
+              <a href="<?php echo $asset->retriveUrl()?>" title="<?php echo $asset->getTitle()?>"><?php echo $asset->getTitle()?></a>
+            </div>
+            <?php endif; ?>
+          <?php endif; ?>
+
+        </div>
+        <!-- /box-topo -->
+        <?php endif; ?>
+        
+      </div>
+      <!-- /BARRA SITE -->
+
+      <!-- MIOLO -->
+      <div id="miolo">
+        
+        <?php include_partial_from_folder('blocks','global/shortcuts') ?>
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+
+          <!-- CAPA -->
+          <div class="capa grid3">
+
+            <!-- ESQUERDA -->
+            <div id="esquerda" class="grid2">
+
+              <?php if(isset($asset)) include_partial_from_folder('blocks','global/asset-2c-video', array('asset' => $asset, 'ipad' => $ipad)) ?>
+
+              <!--?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?-->
+
+            </div>
+            <!-- /ESQUERDA -->
+
+            <!-- DIREITA -->
+            <div id="direita" class="grid1">
+
+              <?php include_partial_from_folder('blocks','global/display-1c-list-carrossel', array('displays' => $displays["destaque-secundario"])) ?>
+              
+              <!-- BOX PUBLICIDADE -->
+              <div class="box-publicidade grid1">
+                <!-- cmais-assets-300x250 -->
+                <script type='text/javascript'>
+                GA_googleFillSlot("cmais-assets-300x250");
+                </script>
+              </div>
+              <!-- /BOX PUBLICIDADE -->
+              
+              <?php if(isset($displays["destaque-noticias"])): ?>
+              <!-- BOX PADRAO Noticia -->
+              <div class="box-padrao grid1">
+                <div class="topo claro">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4>Not&iacute;cias + recentes</h4>
+                    <!-- <a href="#" class="rss" title="rss"></a> -->
+                  </div>
+                </div>
+                <?php include_partial_from_folder('blocks','global/recent-news', array('displays' => $displays["destaque-noticias"])) ?>
+              </div>
+              <!-- /BOX PADRAO Noticia -->
+              <?php endif; ?>
+
+            </div>
+            <!-- /DIREITA -->
+          </div>
+          <!-- /CAPA -->
+
+          <!-- MENU-RODAPE -->
+          <?php if(isset($displays["ultimos-videos"])) include_partial_from_folder('blocks','global/display-3c-last-videos', array('displays' => $displays["ultimos-videos"])) ?>
+          <!-- /MENU-RODAPE -->
+
+          <!-- BOX PUBLICIDADE 2 -->
+          <div class="box-publicidade pub-grd grid3">
+            <!-- cmais-assets-728x90 -->
+            <script type='text/javascript'>
+            GA_googleFillSlot("cmais-assets-728x90");
+            </script>
+          </div>
+          <!-- / BOX PUBLICIDADE 2 -->
+
+        </div>
+        <!-- /CONTEUDO PAGINA -->
+      </div>
+      <!-- /MIOLO -->
+    </div>
+    <!-- / CAPA SITE -->
+
+
