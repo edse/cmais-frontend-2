c4aaed5d20cf2457d3478e00dbd61338437b16f8
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-04 20:37:26 -0300

    u

--- apps/frontend/templates/sites/maiscultura/videosSuccess.php
+++ apps/frontend/templates/sites/maiscultura/videosSuccess.php
@@ -138,12 +138,12 @@ $(function(){
                   </ul>
                 </div>
               </div>
-               <div class="box-publicidade" style="width: 250px; position: absolute; top:97px; left:5px;">
+               <!--div class="box-publicidade" style="width: 250px; position: absolute; top:97px; left:5px;">
                 <!-- cmais-assets-250x250 -->
-        <!--script type='text/javascript'>
+        <script type='text/javascript'>
         GA_googleFillSlot("cmais-assets-250x250");
-        </script-->
-        </div>
+        </script>
+        </div-->
               
               <!--div id="recentes" class="filho blocos" style="display:none;">
                 <div class="capa">
