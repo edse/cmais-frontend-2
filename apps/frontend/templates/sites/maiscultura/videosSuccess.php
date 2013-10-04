3d713d54f849f8ed0eb955d97b4cbbf40e09c92a
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-04 16:26:01 -0300

    u

--- apps/frontend/templates/sites/maiscultura/videosSuccess.php
+++ apps/frontend/templates/sites/maiscultura/videosSuccess.php
@@ -18,18 +18,16 @@ $(function(){
   <div class="bg-chamada">
     <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
   </div>
-  <div class="bg-site videos">
+  <div class="bg-site"></div>
 
     <!-- / CAPA SITE -->
     <div id="capa-site">
 
       <!-- BARRA SITE -->
       <div id="barra-site">
-      
-  
+        
         <?php if(isset($program) && $program->id > 0): ?>
         <div class="topo-programa">
-          
           <h2>
             <a href="<?php echo $program->retriveUrl() ?>">
               <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
@@ -40,7 +38,7 @@ $(function(){
           <?php if(isset($program) && $program->id > 0): ?>
           <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
           <?php endif; ?>
-     
+          
           <?php if(isset($program) && $program->id > 0): ?>
           <!-- horario -->
           <div id="horario">
@@ -83,35 +81,17 @@ $(function(){
           <div id="menu-rodape" class="grid3">
             
             <ul class="abas">
-              <li class="neutro"><a href="javascript:;" onclick="$('#busca-galeria #section').attr('value',''); $('#busca-galeria').submit()">Todos os V&iacute;deos</a><span></span></li>
+              <li class="neutro"><a href="videos">Todos os V&iacute;deos</a><span></span></li>
+              <!-- li class="vistos"><a title="+ Vistos" href="#vistos">+ Vistos</a><span class="decoracao"></span></li -->
+              <!--li class="recentes"><a title="+ Recentes" href="#recentes">+ Recentes</a><span class="decoracao"></span></li-->
             </ul>
             
             <div id="galeria-videos" class="abas-conteudo conteudo-rodape grid3">
               
               <div class="busca">
-                
-                <ul>
-                  <!--li><a href="javascript:;" onclick="$('#busca-galeria #section').attr('value',''); $('#busca-galeria').submit()" <?php if($_REQUEST['section'] == ''): ?>class="ativo"<?php endif; ?>><strong>Todos</strong></a></li-->
-                  <?php
-                  //echo $section->id;
-                    $subsections = Doctrine_Query::create()
-                      ->select('s.*')
-                      ->from('Section s')
-                      ->where('s.parent_section_id = ?', (int)$section->id)
-                      ->orderBy('s.display_order')
-                      ->limit(10)
-                      ->execute();
-                                    
-                  ?>
-                  <?php foreach($subsections as $s): ?>                   
-                  <li><a href="javascript:;" onclick="$('#busca-galeria #section').attr('value','<?php echo $s->getId(); ?>'); $('#busca-galeria').submit()" <?php if($_REQUEST['section'] == $s->getId()): ?>class="ativo"<?php endif; ?>><strong><?php echo $s->getTitle(); ?></strong></a></li>
-                  <?php endforeach; ?>
-                </ul>
-                
                 <form id="busca-galeria" name="busca" action="" method="post">
                   <label class="busque">Busque por <span>palavra-chave</span></label>
                   <input type="text" class="campo-busca" name="busca" id="campo-busca" value="<?php if(isset($_REQUEST['busca'])) echo $_REQUEST['busca']; ?>"/>
-                  <input type="hidden" name="section" id="section" value="<?php if(isset($_REQUEST['section'])) echo $_REQUEST['section']; ?>"/>
                   <input type="submit" class="buscar" name="buscar" id="buscar" value="buscar" style="cursor:pointer" />
                 </form>
               </div>
@@ -121,17 +101,53 @@ $(function(){
                   <ul>
                   <?php if(count($pager) > 0): ?>
                     <?php foreach($pager->getResults() as $d): ?>
-                    <li class="conteudo-lista">
-                      <a href="<?php echo $d->retriveUrl() ?>" class="bg" title="<?php echo $d->getTitle() ?>"><img class="" src="<?php echo $d->retriveImageUrlByImageUsage("image-3") ?>" alt="<?php echo $d->getTitle() ?>" /><span></span></a>
-                      <a href="<?php echo $d->retriveUrl() ?>" class="titulos" title="<?php echo $d->getTitle() ?>"><?php echo $d->getTitle() ?></a>
-                    </li>
+                  <?php 
+                        if($d->AssetType->getSlug() == "video-gallery"){
+                $videos = $d->retriveRelatedAssetsByAssetTypeId(6);
+                $youtubeid = $videos[0]->AssetVideo->getYoutubeId();
+                $url =  "http://img.youtube.com/vi/".$youtubeid."/0.jpg";
+              }else{
+                          $url = $d->retriveImageUrlByImageUsage("image-3");
+              }
+                      ?>              
+                      <li class="conteudo-lista">
+                        <a href="<?php echo $d->retriveUrl() ?>" class="bg" title="<?php echo $d->getTitle() ?>"><img class="" src="<?php echo $url ?>" alt="<?php echo $d->getTitle() ?>" /><span></span></a>
+                        <a href="<?php echo $d->retriveUrl() ?>" class="titulos" title="<?php echo $d->getTitle() ?>"><?php echo $d->getTitle() ?></a>
+                      </li>
                     <?php endforeach; ?>
                   <?php endif; ?>
                   </ul>
                 </div>
               </div>
-             
-             
+               <div class="box-publicidade" style="width: 250px; position: absolute; top:97px; left:5px;">
+                <!-- cmais-assets-250x250 -->
+        <script type='text/javascript'>
+        GA_googleFillSlot("cmais-assets-250x250");
+        </script>
+        </div>
+              
+              <!--div id="recentes" class="filho blocos" style="display:none;">
+                <div class="capa">
+                  <ul>
+                    <li class="conteudo-lista ativo">
+                      <a href="#" class="bg"><img class="" src="images/exemplo5.jpg" alt="200x120" /><span></span></a>
+                      <a href="#" class="titulos">As aventuras e dicas para curtir o show de Paul McCartney</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              
+              <div id="vistos" class="filho blocos" style="display:none;">
+                <div class="capa">
+                  <ul>
+                    <li class="conteudo-lista ativo">
+                      <a href="#" class="bg"><img class="" src="images/exemplo5.jpg" alt="200x120" /><span></span></a>
+                      <a href="#" class="titulos">As aventuras e dicas para curtir o show de Paul McCartney</a>
+                    </li>
+                  </ul>
+                </div>
+              </div -->
+              
               <?php if(isset($pager)): ?>
                 <?php if($pager->haveToPaginate()): ?>
               <!-- PAGINACAO <?php echo $pager->getPage() ?>/<?php echo $pager->getLastPage() ?> -->
@@ -152,12 +168,11 @@ $(function(){
                   <a href="javascript: goToPage(<?php echo $pager->getNextPage() ?>);" class="btn-prox"></a>
                 </div>
               </div>
-              <!-- PAGINACAO -->
+          <!-- PAGINACAO -->
               <form id="page_form" action="" method="post">
-                <input type="hidden" name="return_url" value="<?php echo $url ?>" />
+                <input type="hidden" name="return_url" value="<?php echo $url?>" />
                 <input type="hidden" name="page" id="page" value="" />
-                <input type="hidden" name="section" id="section" value="<?php if(isset($_REQUEST['section'])) echo $_REQUEST['section']; ?>" />
-                <input type="hidden" name="busca" id="busca" value="<?php if(isset($_REQUEST['busca'])) echo $_REQUEST['busca']; ?>" />
+                <input type="hidden" name="busca" id="busca" value="<?php echo $busca ?>" />
               </form>
               <script>
                 function goToPage(i){
@@ -165,7 +180,7 @@ $(function(){
                   $("#page_form").submit();
                 }
               </script>
-              <!-- PAGINACAO -->
+          <!-- PAGINACAO -->
             <?php endif; ?>
           <?php endif; ?>
          
@@ -173,7 +188,14 @@ $(function(){
       </div>
       </div>
     
-     
+      <!-- BOX PUBLICIDADE 2 -->
+      <div class="box-publicidade pub-grd grid3">
+        <!-- programas-assets-728x90 -->
+        <script type='text/javascript'>
+          GA_googleFillSlot("cmais-assets-728x90");
+        </script>
+      </div>
+      <!-- / BOX PUBLICIDADE 2 -->
 
         </div>
         <!-- /CONTEUDO PAGINA -->
@@ -182,4 +204,3 @@ $(function(){
       <!-- /MIOLO -->
     </div>
     <!-- / CAPA SITE -->
-    </div>
