f4fb4bdfd67571b9f660e4f2812cbce0f5a85986
renatareno <asdf@fpawks0142.local>
2013-10-10 08:38:41 -0300

    quintal elementos

--- apps/frontend/templates/sites/quintaldacultura/_itensBackground.php
+++ apps/frontend/templates/sites/quintaldacultura/_itensBackground.php
@@ -3,25 +3,11 @@
   <div id="nuvem1" class="pos"></div>
   <div id="nuvem2" class="pos"></div>
   <div id="nuvem3" class="pos"></div>
+  <!--
   <div id="balao" class="pos"></div>
   <div id="bola" class="pos"></div>
   <div id="bau" class="pos"></div>
   <div id="peteca" class="pos"></div>
+  -->
 </div>
 <!--/ITENS BACKGROUND -->
-
-<!--ITENS BACKGROUND
-<div id="back">
-  <div id="nuvem1" class="pos"></div>
-  <div id="nuvem2" class="pos"></div>
-  <div id="nuvem3" class="pos"></div>
-  <div id="balao" class="pos"></div>
-  
-  <div id="bau" class="pos"></div>
-  
-  
-  <div id="peteca" class="pos"></div>
-  <div id="bola" class="pos"></div>
-  <div id="niver2anos" class="pos"></div>
-</div>
-ITENS BACKGROUND-->
