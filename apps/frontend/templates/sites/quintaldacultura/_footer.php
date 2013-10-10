83fe51e9aee3899b4c3bab42fb024edf47eb877d
renatareno <asdf@fpawks0142.local>
2013-10-10 09:19:59 -0300

    rodape

--- apps/frontend/templates/sites/quintaldacultura/_footer.php
+++ apps/frontend/templates/sites/quintaldacultura/_footer.php
@@ -4,7 +4,7 @@
             <li ><a href="/quintaldacultura/sobre-o-quintal-da-cultura">Sobre o Programa</a><span></span></li>
             <li><a href="/quintaldacultura/perfil-publico">P&uacute;blico Alvo</a><span></span></li>
             <li><a href="/quintaldacultura/diretrizes-pedagogicas/qual-o-papel-da-tv-no-desenvolvimento-infantil">Diretrizes Pedag&oacute;gicas</a><span></span></li>
-            <li><a href="http://cmais.com.br/paraospais">Para os Pais</a></li>
+            <li class="last"><a href="http://cmais.com.br/paraospais">Para os Pais</a></li>
           </ul>
         </div>
       </div>
