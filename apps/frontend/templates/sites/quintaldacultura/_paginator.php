f0113f39cf1d22024f2def5563d2228b51fbe345
doljak <jedoljak@gmail.com>
2013-10-11 11:25:53 -0300

    u

--- apps/frontend/templates/sites/quintaldacultura/_paginator.php
+++ apps/frontend/templates/sites/quintaldacultura/_paginator.php
@@ -20,7 +20,7 @@
 	          $(function(){ 
   	          var i = <?php echo count($pager->getLinks()); ?>;
   	          //var width = ($("#pgNumber").width() * i) + (10 * i) - 10;
-  	          var width = $(".pgNumber").widt();
+  	          var width = $(".pgNumber").width();
   	          console.log(width);
   	          //$('#pgContainer').css({'width':width,"margin":"0 auto"})
 	          }); 
