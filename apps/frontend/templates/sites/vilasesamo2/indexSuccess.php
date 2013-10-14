736b626856d3f75a8da69bede540e66765e6e478
doljak <jedoljak@gmail.com>
2013-10-14 11:36:35 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/indexSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/indexSuccess.php
@@ -8,7 +8,7 @@
 <div id="content">
   <!--menu principal && banner promocional-->
   <?php include_partial_from_folder('sites/vilasesamo2', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)); ?>
-  <?php //include_partial_from_folder('sites/vilasesamo2', 'global/bannerprincipal'); ?>
+  <?php include_partial_from_folder('sites/vilasesamo2', 'global/bannerprincipal'); ?>
   <!--/menu principal && banner promocional-->
   
   <!--carrossel personagens-->
