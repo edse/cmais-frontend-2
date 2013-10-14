d9e3b466eb65382bd8bb32b3c5f5537b9b20026a
doljak <jedoljak@gmail.com>
2013-10-14 15:35:48 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/_bannerprincipal.php
+++ apps/frontend/templates/sites/vilasesamo2/_bannerprincipal.php
@@ -1,11 +1,3 @@
-   <!--itens acessibidade-->
-  <nav title="banner destaques">
-    <a href="#">destaque do mês exemplo 1</a>
-    <a href="#">destaque do mês exemplo 2</a>
-    <a href="#">destaque do mês exemplo 3</a>
-    <a href="#">destaque do mês exemplo 4</a>
-  </nav>
-  <!--/itens acessibildade-->
   <!-- carrossel desktop-->
   <section id="carrossel-destaque" role="banner">
     <!--Inicio-->
@@ -28,79 +20,34 @@
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/1/img_objeto_banner-topo.png" alt="Banner topo">
           </div>
           <div class="mpc_ls_slide_item total">
+            <a href="#" class="total">destaque do mês exemplo 1</a>
+          </div>
+        </li>
+        <!--/item-->
+        
+        <!--item-->
+        <li class="slide01" data-easing="easeInExpo" data-transition="slideRight" style="background: url('/portal/images/capaPrograma/vilasesamo2/banner/1/img_pattern-banner-topo.jpg') repeat;">
+          <div class="mpc_ls_slide_background total" data-style="scale">
             <a href="#" class="total"></a>
           </div>
-        </li>
-        <!--/item-->
-        <li class="slide02" data-easing="easeOutExpo" data-transition="slideRight" >
-          <div class="mpc_ls_slide_background" data-style="scale">
-            <img src="/testes/layer-slider/preview/7/slides/slider2slide02.jpg" alt="Image">
+          <div class="mpc_ls_slide_item" data-x="86" data-y="124" data-delay="1500" data-duration="500" data-easing="easeOutBounce" data-effect="slideTop" data-fade="on">
+            <h2>Para <br> colorir</h2>
           </div>
-          <div class="mpc_ls_slide_item" data-x="655" data-y="65" data-delay="750" data-duration="750" data-easing="easeOutExpo" data-effect="slideTop" data-fade="on">
-            <iframe src="http://player.vimeo.com/video/35755200?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="445" height="250" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+          <div class="mpc_ls_slide_item" data-x="350" data-y="120" data-delay="2000" data-duration="500" data-easing="easeOutBack" data-effect="slideRight" data-fade="on">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/1/img_personagem_banner-topo.png" alt="Banner topo">
           </div>
-      
-          
-          <div class="mpc_ls_slide_item" data-x="700" data-y="335" data-delay="750" data-duration="750" data-easing="easeOutExpo" data-effect="slideBottom" data-fade="on">
-            <p>Behind the scenes video of Lana Del Rey's shoot for the February issue of Interview Russia by Sean & Seng. Image copyright <a href="http://www.screened.com/lana-del-rey/14-885806/all-images/132-2597697/lana_del_rey_complex_2/131-603732/" target="_blank">Complex Magazine</a></p>
+          <div class="mpc_ls_slide_item" data-x="180" data-y="340" data-delay="2000" data-duration="500" data-easing="easeOutElastic" data-effect="slideLeft" data-fade="on">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/1/img_objeto_banner-topo.png" alt="Banner topo">
           </div>
-
-        </li>
-
-        <!--item-->
-        <li class="slide04" data-easing="easeInExpo" data-transition="slideRight">
-          <div class="mpc_ls_slide_background" data-style="scale">
-            <img src="/testes/layer-slider/preview/6/slides/slide04bg.jpg" alt="Banner topo">
-          </div>
-          <div class="mpc_ls_slide_item" data-x="796" data-y="70" data-delay="1000" data-duration="1000" data-easing="swing" data-effect="slideTop" data-fade="on">
-            <h2>LOREM IPSUM</h2>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="560" data-y="90" data-delay="1500" data-duration="1000" data-easing="swing" data-effect="slideTop" data-fade="on">
-            <h3>CONSECTETUR ADIPISCING EIUSMOD TEMPOR</h3>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="510" data-y="150" data-delay="2500" data-duration="1000" data-easing="easeOutElastic" data-effect="slideRight" data-fade="on">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="510" data-y="220" data-delay="2750" data-duration="1000" data-easing="easeOutElastic" data-effect="slideRight" data-fade="on">
-            <p>Duis aute irure dolor this is link inside the text voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="510" data-y="290" data-delay="3000" data-duration="1000" data-easing="easeOutElastic" data-effect="slideRight" data-fade="on">
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi sint occaecat cupidatat non proident, sunt inut aliquip.</p>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="70" data-y="60" data-delay="2000" data-duration="500" data-easing="swing" data-effect="none" data-fade="on">
-            <img src="/testes/layer-slider/preview/6/slides/slide04img1.png" alt="Image">
-          </div>
-          <div class="mpc_ls_slide_item" data-x="360" data-y="190" data-delay="1500" data-duration="500" data-easing="swing" data-effect="none" data-fade="on">
-            <img src="/testes/layer-slider/preview/6/slides/slide04img2.png" alt="Image">
-          </div>
-          <div class="mpc_ls_slide_item" data-x="420" data-y="195" data-delay="1000" data-duration="500" data-easing="swing" data-effect="none" data-fade="on">
-            <img src="/testes/layer-slider/preview/6/slides/slide04img3.png" alt="Image">
-          </div>
-          <div class="mpc_ls_slide_item" data-x="854" data-y="360" data-delay="3000" data-duration="1000" data-easing="easeOutBack" data-effect="slideBottom" data-fade="on">
-            <a href="#" class="read_more">READ MORE</a>
+          <div class="mpc_ls_slide_item total">
+            <a href="#" class="total">destaque do mês exemplo 2</a>
           </div>
         </li>
         <!--/item-->
+        
 
-        <!--item-->
-        <li class="slide02" data-easing="easeInExpo" data-transition="slideRight">
-          <div class="mpc_ls_slide_background" data-style="center">
-            <img src="/testes/layer-slider/preview/6/slides/slide02bg.jpg" alt="Banner topo" >
-          </div>
-          <div class="mpc_ls_slide_item" data-x="248" data-y="124" data-delay="1000" data-duration="1000" data-easing="easeOutBack" data-effect="slideLeft" data-fade="on">
-            <h2>LOREM IPSUM DOLOR</h2>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="177" data-y="190" data-delay="1500" data-duration="1000" data-easing="easeOutBack" data-effect="slideRight" data-fade="on">
-            <h3>CONSECTETUR ADIPISCING EIUSMOD TEMPOR</h3>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="80" data-y="260" data-delay="2000" data-duration="1000" data-easing="easeOutBack" data-effect="slideBottom" data-fade="on">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor <a href="#">this is link inside the text</a> voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="mpc_ls_slide_item" data-x="820" data-y="410" data-delay="3000" data-duration="1000" data-easing="easeOutBack" data-effect="slideBottom" data-fade="on">
-            <a href="#" class="read_more">READ MORE </a>
-          </div>
-        </li>
-        <!--/item-->
+        
+
       </ul>
       <!--/lista-->
     </div>
@@ -121,6 +68,7 @@
     'pauseOnHover' : false,
     'slideshow' : true,
     'uiStyle': 'style02',
+    'delay': 10800,
     'showThumbTooltip':false, 
     'arrowsOffset': -35,
     'showControlsOnHover': true,
