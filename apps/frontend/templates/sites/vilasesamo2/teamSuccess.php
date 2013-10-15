511d76854a159a602b446c4e47a29991a0461ec7
doljak <jedoljak@gmail.com>
2013-10-15 12:55:20 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/teamSuccess.php
@@ -8,10 +8,10 @@
 <style>
  #container-personagens{width:950px;height:750px;}
  .conteudo-asset {padding: 0;}
- .q-pers-01{float:left; width: 110px; height:110px; margin:5px;background:#000000;}
- .q-pers-02{float:left; width: 180px; height:180px; margin:5px;background:blue;}
- .q-pers-03{float:left; width: 290px; height:290px; margin:5px;background:red;}
- .q-pers-04{float:left; width: 410px; height:410px; margin:5px;background:yellow;}
+ .q-pers-01{float:left; width: 200px; height:200px; margin:5px;background:#000000;}
+ .q-pers-02{float:left; width: 200px; height:200px; margin:5px;background:blue;}
+ .q-pers-03{float:left; width: 200px; height:200px; margin:5px;background:red;}
+ .q-pers-04{float:left; width: 200px; height:200px; margin:5px;background:yellow;}
 </style>
 <!-- HEADER -->
 <?php include_partial_from_folder('sites/vilasesamo2', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))
