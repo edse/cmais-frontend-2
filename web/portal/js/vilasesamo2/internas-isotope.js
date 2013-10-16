caa9d9f52ebb890113de54967f029ece4f2d9263
doljak <jedoljak@gmail.com>
2013-10-16 17:12:49 -0300

    u

--- web/portal/js/vilasesamo2/internas-isotope.js
+++ web/portal/js/vilasesamo2/internas-isotope.js
@@ -1,5 +1,16 @@
   $(function(){
-
+    /*lista destaque small*/
+     $('.todos-itens li').each(function(i){
+       el = $(this);
+       if(i%3==0){
+         $(el).css('margin-left', '0');
+         //$(el).css('clear', 'both');
+         i = 0;
+       }else{
+         i++;
+       }
+     });
+     
     var $container = $('#container');
     
     $container.isotope({
