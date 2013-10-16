cd802f10329d30a56be5ba05b47a79ae0bfde10c
doljak <jedoljak@gmail.com>
2013-10-16 11:53:06 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/atividadesSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/atividadesSuccess.php
@@ -45,51 +45,64 @@
     </div>
     <!--destaque-filtro-->
     
-    <nav class="span2">
+    <nav role="navigation" class="span2">
       <h3>escolha por personagem</h3>
       
       <ul class="filtro-personagem">
        
        <li>
         <div class="inner bel">
-          <a href="/vilasesamo2/personagens/bel" title="Bel" class="btn-bel" data-filter=".bel">
-            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="Personagem" />
-           </a>
+          <a href="/vilasesamo2/personagens/bel" class="btn-bel" data-filter=".bel">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="filtro bel" />
+          </a>
         </div>
        </li>
         <li>
           <div class="inner beto">
-            <a href="/vilasesamo2/personagens/beto" title="Beto" class="btn-beto" data-filter=".beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/beto" class="btn-beto" data-filter=".beto">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="filtro beto" />
+            </a>
           </div>
         </li>
         <li>
           <div class="inner comecome">
-            <a href="/vilasesamo2/personagens/come-come" title="Come-come" class="btn-comecome" data-filter=".come-come"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/come-come" class="btn-comecome" data-filter=".come-come">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="filtro Come-come" />
+            </a>
           </div>
         </li>
         <li>
           <div class="inner elmo">
-            <a href="/vilasesamo2/personagens/elmo" title="Elmo" class="btn-elmo" data-filter=".elmo"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/elmo" class="btn-elmo" data-filter=".elmo">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="filtro Elmo" />
+            </a>
           </div>
         </li>
         <li>
           <div class="inner enio">
-            <a href="/vilasesamo2/personagens/enio" title="Ênio" class="btn-enio" data-filter=".enio"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/enio" class="btn-enio" data-filter=".enio">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="filtro Enio" /></a>
            </div>
         </li>
         <li>
           <div class="inner garibaldo">
-            <a href="/vilasesamo2/personagens/garibaldo" title="Garibaldo" class="btn-garibaldo" data-filter=".garibaldo"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/garibaldo" class="btn-garibaldo" data-filter=".garibaldo">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="filtro Garibaldo" />
+            </a>
           </div>
         </li>
         <li>
           <div class="inner grover">
-            <a href="/vilasesamo2/personagens/grover" title="Grover" class="btn-grover" data-filter=".grover"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/grover" class="btn-grover" data-filter=".grover">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="filtro Grover" />
+            </a>
           </div>
         </li>
         <li>
           <div class="inner zoe">
-            <a href="/vilasesamo2/personagens/zoe" title="Zoe" class="btn-zoe" data-filter=".zoe"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="Personagem" /></a>
+            <a href="/vilasesamo2/personagens/zoe" class="btn-zoe" data-filter=".zoe">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="filtro Zoe" />
+            </a>
            </div>
         </li>
       </ul>
