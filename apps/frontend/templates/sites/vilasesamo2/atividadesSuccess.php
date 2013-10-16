ca8a580d817672ed245285f9faf069acb6feea54
doljak <jedoljak@gmail.com>
2013-10-16 16:41:52 -0300

    u

--- apps/frontend/templates/sites/vilasesamo2/atividadesSuccess.php
+++ apps/frontend/templates/sites/vilasesamo2/atividadesSuccess.php
@@ -135,7 +135,7 @@
       <a href="#" title="">
         <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="" />
         <i class="sprite-icons-new sprite-icone_atividade"></i>
-        <p class="nav-atividades">Nome jogo 2 <br> quebra de linha</p>
+        <p class="nav-atividades">Nome jogo 2</p>
       </a>
     </li>
     <li class="span4 element bel"> 
