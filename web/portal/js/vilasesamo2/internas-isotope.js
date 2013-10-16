43b30c8f8f34b16548489211f3a3d1907d569a04
doljak <jedoljak@gmail.com>
2013-10-16 17:34:35 -0300

    u

--- web/portal/js/vilasesamo2/internas-isotope.js
+++ web/portal/js/vilasesamo2/internas-isotope.js
@@ -3,11 +3,11 @@
      $('.todos-itens li').each(function(i){
        el = $(this);
        if(i%2==0){
-         $(el).css('left', '20px');
+         $(el).css('margin-left', '0px');
          //$(el).css('clear', 'both');
          //i = 0;
-       }else if(i%3==0){
-         $(el).css('left', '20px');
+       }else{
+         $(el).css('margin-left', '20px');
          i++;
        }
      });
