27cc594f5f21716dcace0016a48843539d5fc881
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-17 14:16:49 -0300

    u

--- apps/frontend/templates/sites/vilasesamo/_sidebar-personagens.php
+++ apps/frontend/templates/sites/vilasesamo/_sidebar-personagens.php
@@ -5,48 +5,48 @@
       <ul class="filtro-personagem">
        <li>
         <div class="inner bel">
-          <a href="#" class="btn-bel" data-filter=".bel">
+          <a href="#" class="btn-bel" data-filter=".bel" title="Bel">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="filtro bel" />
           </a>
         </div>
        </li>
         <li>
           <div class="inner beto">
-            <a href="#" class="btn-beto" data-filter=".beto">
+            <a href="#" class="btn-beto" data-filter=".beto" title="Beto">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="filtro beto" />
             </a>
           </div>
         </li>
         <li>
           <div class="inner comecome">
-            <a href="#" class="btn-comecome" data-filter=".come-come">
+            <a href="#" class="btn-comecome" data-filter=".come-come" title="Come-come">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="filtro Come-come" />
             </a>
           </div>
         </li>
         <li>
           <div class="inner elmo">
-            <a href="#" class="btn-elmo" data-filter=".elmo">
+            <a href="#" class="btn-elmo" data-filter=".elmo" title="Elmo">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="filtro Elmo" />
             </a>
           </div>
         </li>
         <li>
           <div class="inner enio">
-            <a href="#" class="btn-enio" data-filter=".enio">
+            <a href="#" class="btn-enio" data-filter=".enio" title="Ênio">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="filtro Enio" /></a>
            </div>
         </li>
         <li>
           <div class="inner garibaldo">
-            <a href="#" class="btn-garibaldo" data-filter=".garibaldo">
+            <a href="#" class="btn-garibaldo" data-filter=".garibaldo" title="Garibaldo">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="filtro Garibaldo" />
             </a>
           </div>
         </li>
         <li>
           <div class="inner grover">
-            <a href="#" class="btn-grover" data-filter=".grover">
+            <a href="#" class="btn-grover" data-filter=".grover" title="Grover">
               <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="filtro Grover" />
             </a>
           </div>
