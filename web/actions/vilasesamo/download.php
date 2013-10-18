129ff103d5ebd390fe25c332c7ec119d6b647b6a
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-18 19:51:09 -0300

    u

---
+++ web/actions/vilasesamo/download.php
@@ -0,0 +1,11 @@
+<?php
+  if($_REQUEST["file"]) {
+    $file = $_REQUEST['file'];
+    header("Content-disposition: attachment; filename={$file}");
+    header("Content-type: image/jpg");
+    readfile("{$file}");
+  }
+  else {
+    die("parameter 'file' needed!");
+  }
+?>
