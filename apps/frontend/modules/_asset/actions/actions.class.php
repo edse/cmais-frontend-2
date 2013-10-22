7c4b48094c96c6d89ee9a702e65f70d6a3ebeb80
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-22 17:57:14 -0200

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -1063,15 +1063,13 @@ class _assetActions extends sfActions
 			      }
 			    }
           if ($this->site->getSlug() == "vila-sesamo") {
-            /*
             $sections = $this->asset->getSections();
             foreach($sections as $s) {
-              if(in_array($s->getSlug(), array("atividades", "jogos", "videos"))) {
+              if(in_array($s->getSlug(), array("atividades", "jogos", "videos","para-os-pais"))) {
                 $this->section = $s;
                 break;
               }
             }
-            
             if($this->section->getSlug() == "atividades") {
               if($debug) print "<br>2-3-1>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/atividade';
               $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/atividade');
@@ -1084,19 +1082,7 @@ class _assetActions extends sfActions
               if($debug) print "<br>2-3-3>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/video';
               $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/video');
             }
-            else {
-              if($debug) print "<br>2-3-5>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug();
-              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug());
-            }
-             */
-            $sections = $this->asset->getSections();
-            foreach($sections as $s) {
-              if(in_array($s->getSlug(), array("para-os-pais"))) {
-                $this->section = $s;
-                break;
-              }
-            }
-            if($this->section->getSlug() == "para-os-pais") {
+            elseif($this->section->getSlug() == "para-os-pais") {
               if($debug) print "<br>2-3-4>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/article';
               $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/article');
             }
