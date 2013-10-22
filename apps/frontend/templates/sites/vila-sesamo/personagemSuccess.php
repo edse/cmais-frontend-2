78d1a8e8f8d76106affa00e262e98b685b79dbfc
doljak <jedoljak@gmail.com>
2013-10-22 15:57:34 -0200

    u

---
+++ apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
@@ -0,0 +1,102 @@
+<link type="text/css" rel="stylesheet" href="http:/cmais.com.br/portal/js/responsive-carousel/style-vilasesamo.css"/>
+
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lt IE 8]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
+
+<script>
+  $("body").addClass("interna personagens");
+</script>
+
+<!-- HEADER -->
+<?php include_partial_from_folder('sites/vilasesamo2', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+<!-- /HEADER -->
+
+<!--content-->
+<div id="content">
+  
+  <!--section-->
+  <section class="filtro row-fluid">
+    
+    <!--span12-->
+    <div class="span12" role="main">
+      
+      <!--h3><i class="sprite-icon-colorir-med"></i>Atividades</h3-->
+      <h1><i class="sprite-icon-personagens-med"></i>Personagens</h1>
+      
+      <!--nav filtro personagem-->
+    <nav role="navigation" class="span2">
+      <h3>escolha por personagem</h3>
+      <h3 aria-live="polite" id="filtro-descricao">todas as atividades estão para selecionar</h3>
+      <ul class="filtro-personagem">
+       
+       <li>
+        <div class="inner bel">
+          <a href="#" class="btn-bel ">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="Personagem bel" />
+          </a>
+          
+        </div>
+       </li>
+        <li>
+          <div class="inner beto">
+            <a href="#" class="btn-beto ">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="Personagem beto" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="inner comecome">
+            <a href="javascript:;" class="btn-comecome" >
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="Personagem Come-come" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="inner elmo">
+            <a href="javascript:;" class="btn-elmo ">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="Personagem Elmo" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="inner enio">
+            <a href="javascript:;" class="btn-enio ">
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="Personagem Enio" /></a>
+           </div>
+        </li>
+        <li>
+          <div class="inner garibaldo">
+            <a href="javascript:;" class="btn-garibaldo " >
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="Personagem Garibaldo" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="inner grover">
+            <a href="javascript:;" class="btn-grover " >
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="Personagem Grover" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="inner zoe">
+            <a href="javascript:;" class="btn-zoe " >
+              <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="Personagem Zoe" />
+            </a>
+           </div>
+        </li>
+      </ul>
+    </nav>
+    <!--/nav filtro personagem-->
+    
+    </div>
+    <!--/span12-->
+
+
+  </section>
+  <!--/content-->
+  
+</div>
+<!--/section-->
