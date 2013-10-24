1161afc608248c391f1fcacb97fb5130b95dcdd0
doljak <jedoljak@gmail.com>
2013-10-24 11:59:38 -0200

    u

---
+++ apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
@@ -0,0 +1,98 @@
+<!--inicio carrossel--> 
+    <div id="carrossel-interna-artigo">
+      <!--slider-->
+      <div class="slider">
+        <!--slider-mask-wrap-->
+        <div class="slider-mask-wrap">
+          <!--slider-mask-->
+          <div class="slider-mask">
+            <!--slider-mask-wrap--> 
+            <ul class="slider-target">
+              <!--item-->
+              <li>
+                <div class="pull-left videoorimage">
+                  <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/interna-carrosel-bel.jpg" alt="foto da bel" />
+                </div>
+                <div class="descritivo">
+                  <h3>BEL</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspen disse pretium tortor felis, quis egestas arcu facilisis a.<br><br> Aliquam commodo lectus volutpat nisl faucibus hendrerit.Nulla facilisi. Donec porta, leo at aliquet mattis, erat justo sodales tellus, at aliquet lacus. massa eu purus cras amet.</p>
+                </div>
+              </li>
+              <!--/item-->
+              
+              <!--item-->
+              <li>
+                <div class="pull-left videoorimage">
+                  <iframe width="420" height="315" src="//www.youtube.com/embed/52GWILDxOcA" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="descritivo">
+                  <h3>BEL</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspen disse pretium tortor felis, quis egestas arcu facilisis a.<br><br> Aliquam commodo lectus volutpat nisl faucibus hendrerit.Nulla facilisi. Donec porta, leo at aliquet mattis, erat justo sodales tellus, at aliquet lacus. massa eu purus cras amet.</p>
+                </div>
+              </li>
+              <!--/item-->
+              
+              <!--item-->
+              <li>
+                <div class="pull-left videoorimage">
+                  <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/interna-carrosel-bel.jpg" alt="foto da bel" />
+                </div>
+                <div class="descritivo">
+                  <h3>BEL</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspen disse pretium tortor felis, quis egestas arcu facilisis a.<br><br> Aliquam commodo lectus volutpat nisl faucibus hendrerit.Nulla facilisi. Donec porta, leo at aliquet mattis, erat justo sodales tellus, at aliquet lacus. massa eu purus cras amet.</p>
+                </div>
+              </li>
+              <!--/item-->
+              
+              <!--item-->
+              <li>
+                <div class="pull-left videoorimage">
+                  <iframe width="420" height="315" src="//www.youtube.com/embed/52GWILDxOcA" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="descritivo">
+                  <h3>BEL</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspen disse pretium tortor felis, quis egestas arcu facilisis a.<br><br> Aliquam commodo lectus volutpat nisl faucibus hendrerit.Nulla facilisi. Donec porta, leo at aliquet mattis, erat justo sodales tellus, at aliquet lacus. massa eu purus cras amet.</p>
+                </div>
+              </li>
+              <!--/item-->
+              
+              <!--item-->
+              <li>
+                <div class="pull-left videoorimage">
+                  <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/interna-carrosel-bel.jpg" alt="foto da bel" />
+                </div>
+                <div class="descritivo">
+                  <h3>BEL</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspen disse pretium tortor felis, quis egestas arcu facilisis a.<br><br> Aliquam commodo lectus volutpat nisl faucibus hendrerit.Nulla facilisi. Donec porta, leo at aliquet mattis, erat justo sodales tellus, at aliquet lacus. massa eu purus cras amet.</p>
+                </div>
+              </li>
+              <!--/item-->
+              
+              
+            </ul>
+            <!--slider-mask-->
+            <div class="clearit"></div>
+          </div>
+        </div>
+        <!--slider-mask-wrap--> 
+        <!--slider-nav>
+        <div class="slider-nav">
+          <div class="arrow-left arrow"><span title="Anterior" class="back"></span></div>
+          <div class="arrow-right arrow"><span title="Proximo" class="next"></span></div>
+        </div> 
+        <!--slider-nav-->
+      </div>
+      <!--/slider-->
+      <!--seletor carrossel-->
+      <div class="container-itens"> 
+        <ul id="selector-interna-personagem">
+          <li><a href="#" rel="frame_0"></a></li>
+          <li><a href="#" rel="frame_1"></a></li>
+          <li><a href="#" rel="frame_2"></a></li>
+          <li><a href="#" rel="frame_3"></a></li>
+          <li><a href="#" rel="frame_4"></a></li>
+        </ul>
+      </div>
+      <!--/seletor carrossel--> 
+    </div>
+    <!--/inicio carrossel--> 
