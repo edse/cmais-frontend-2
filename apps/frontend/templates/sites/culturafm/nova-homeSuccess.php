689808adb21364013e3aee1bc7a03a8c5f94118c
Joseval <Joseval@fpawks0146-2.local>
2013-10-24 15:24:47 -0200

    Nova Home

--- apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
+++ apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
@@ -162,25 +162,27 @@
     <div class="grid4 marginLeft10">
       
       <!--carrossel-->
-      <div id="myCarousel" class="carousel slide">
+      <!--div id="myCarousel" class="carousel slide">
         <!-- Carousel items -->
-        <div class="carousel-inner">
-			<?php if(isset($displays["destaque-carrossel"])): ?>
-				<?php foreach ($displays["destaque-carrossel"] as $k => $d): ?>          
-			      <!--item-->
-			      <div class="<?php if($k == 0): ?>active<?php endif; ?> item">
-			      	<h2><?php echo $d->getTitle() ?></h2>
-			        <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
-			          <img width="424" src="<?php echo $d->retriveImageUrlByImageUsage("image-4-b") ?>" alt="<?php echo $d->getTitle() ?>">
-			          <p> <?php echo $d->getDescription() ?></p>
-			        </a>
-			      </div>
-			      <!--/item-->
-			    <?php endforeach; ?>
-		  	<?php endif; ?>          
-        </div>
+        <!--div class="carousel-inner"-->
+        	<div class="container-destaque">
+				<?php if(isset($displays["destaque-carrossel"])): ?>
+					<?php foreach ($displays["destaque-carrossel"] as $k => $d): ?>          
+				      <!--item-->
+				      <div class="<?php if($k == 0): ?>active<?php endif; ?> item">
+				      	<h2><?php echo $d->getTitle() ?></h2>
+				        <a href="<?php echo $d->retriveUrl() ?>" title="<?php echo $d->getTitle() ?>">
+				          <img width="424" src="<?php echo $d->retriveImageUrlByImageUsage("image-4-b") ?>" alt="<?php echo $d->getTitle() ?>">
+				          <p> <?php echo $d->getDescription() ?></p>
+				        </a>
+				      </div>
+				      <!--/item-->
+				    <?php endforeach; ?>
+			  	 <?php endif; ?>         
+		  	 </div>
+        <!--/div>
         <!-- Carousel nav -->
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev"></a>
+        <!--a class="carousel-control left" href="#myCarousel" data-slide="prev"></a>
         <a class="carousel-control right" href="#myCarousel" data-slide="next"></a>
       </div>
       <!--carrossel-->
