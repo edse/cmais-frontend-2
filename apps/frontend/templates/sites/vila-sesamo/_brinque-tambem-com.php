ecb385128d64ba6b7a4f8cb8136b1dc22d442ec9
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-25 18:00:51 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_brinque-tambem-com.php
+++ apps/frontend/templates/sites/vila-sesamo/_brinque-tambem-com.php
@@ -33,11 +33,11 @@
   else { // senão, prioriza assets com a mesma tag, concatenando em seguida com assets da mesma categoria e por último com assets da mesma seção, juntando tudo isso em um só carrossel.
     $tags = array();
     if(count($asset->getTags())>0){
-      foreach($asset->getTags() as $t)
+      foreach($asset->getTags() as $t) {
         $tags[] = $t;
+      }
     }
     if(count($tags) > 0) {
-      die("1");
       $see_also_by_tags = Doctrine_Query::create()
         ->select('a.*')
         ->from('Asset a, SectionAsset sa, tag t, tagging tg')
