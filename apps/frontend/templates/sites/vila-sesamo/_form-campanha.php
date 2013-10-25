6ff2849bf756b2169470a256ceebc61f8b63b2e9
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-25 20:13:01 -0200

    uu

--- apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
+++ apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
@@ -120,7 +120,7 @@
         <!--concorda-->
         <div class="control-group span11">
           <label class="radio">
-            <input type="radio" name="concordo" id="concordo" value="concordo" checked>
+            <input type="radio" name="concordo" id="concordo" value="concordo">
             Declaro que li e estou de acordo com os Termos e Condições acima.
           </label>
           <button type="submit" class="btn">enviar minha brincadeira</button>
