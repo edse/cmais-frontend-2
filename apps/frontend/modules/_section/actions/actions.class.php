13d2cb176d9337a0d0e2740b7f929d80075410de
Joseval <Joseval@fpawks0146-2.local>
2013-10-25 18:10:21 -0200

    Agendamento de Assets

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -879,8 +879,8 @@ class _sectionActions extends sfActions
                 ->from('Asset a, SectionAsset sa')
                 ->where('sa.section_id = ?', $this->section->id)
                 ->andWhere('sa.asset_id = a.id')
-                ->andWhere("(a.date_start IS NULL OR a.date_start >= CURRENT_TIMESTAMP)")
-				->andWhere("(a.date_end IS NULL OR a.date_end <= CURRENT_TIMESTAMP)")
+                ->andWhere("a.date_start IS NULL OR a.date_start >= CURRENT_TIMESTAMP")
+				//->andWhere("a.date_end IS NULL OR a.date_end <= CURRENT_TIMESTAMP")
                 ->andWhere('a.is_active = ?', 1);
               if($request->getParameter('busca') != '')
                 $this->assetsQuery->andWhere("a.title like '%".$request->getParameter('busca')."%' OR a.description like '%".$request->getParameter('busca')."%'");               
