ed6cc388ccfa06f007b12416b04dc80d863c20a9
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-25 20:51:06 -0200

    u

--- web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
+++ web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
@@ -43,7 +43,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     //$file_mime_type = "image/jpg";
     $attach = array();
     $attach[] = array($_FILES['datafile']['tmp_name'], $file_mime_type);
-    
+    echo "1";
     if (!in_array($file_mime_type, array("image/gif", "image/png", "image/jpg"))) {
       if (unlink($_FILES['datafile']['tmp_name'])) {
         //header("Location: http://tvcultura.cmais.com.br/cocorico/receitinhas?error=2");
