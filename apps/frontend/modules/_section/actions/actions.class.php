34be548543af611a085a9a857703194776e9f0b9
Joseval <Joseval@fpawks0146-2.local>
2013-10-25 12:21:50 -0200

    Pedro e Bianca

--- apps/frontend/modules/_section/actions/actions.class.php
+++ apps/frontend/modules/_section/actions/actions.class.php
@@ -1233,7 +1233,7 @@ class _sectionActions extends sfActions
         $this->assetsQuery->andWhere("sa.section_id = ?", (int)$request->getParameter('section'));
       else
         $this->assetsQuery->andWhere('sa.section_id = ?', $this->section->id);
-      $this->assetsQuery->orderBy('sa.display_order');
+      $this->assetsQuery->orderBy('a.created_at asc');
       $test = $this->assetsQuery->execute();
     }    
 
