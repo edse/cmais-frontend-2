8369aaebe939443f7435139c2f78121ab7850b9a
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-28 20:20:01 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/jogoSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/jogoSuccess.php
@@ -19,7 +19,6 @@
       }
     }
   }
-  echo $section->getTitle();
 ?>
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/assets.css" type="text/css" />
@@ -93,7 +92,7 @@
   </section>
   <!--/section-->
   
-  <?php //include_partial_from_folder('sites/vila-sesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
+  <?php include_partial_from_folder('sites/vila-sesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
   
   <?php include_partial_from_folder('sites/vila-sesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
 
