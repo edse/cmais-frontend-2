540cd56e6f6885217f94e066e220d7252df56f30
doljak <jedoljak@gmail.com>
2013-10-28 10:54:04 -0200

    u

--- apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
+++ apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
@@ -5,7 +5,7 @@
 
 
 <?php use_helper('I18N', 'Date') ?>
-<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => @$asset, 'section' => $section)) ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
 
 <!-- container -->
