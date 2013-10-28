401e28534905d498dcb827bc76d6c5e55b46c7c4
doljak <jedoljak@gmail.com>
2013-10-28 15:58:41 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
+++ apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
@@ -126,12 +126,12 @@
         <div class="control-group span11">
           <label class="radio">
             <input type="radio" name="concordo" id="concordo" value="concordo">
-            Declaro que li e estou de acordo com os Termos e Condições acima.
+            Declaro que li e estou de acordo com os Termos e Condições acima 2.
           </label>
           <button type="submit" class="btn">enviar minha brincadeira</button>
         </div>
         <!--/concorda-->
-        
+         
         <!--Termos e condições-->
         <div class="span12 termo">
           <?php include_partial_from_folder('sites/vila-sesamo', 'global/termos') ?>
