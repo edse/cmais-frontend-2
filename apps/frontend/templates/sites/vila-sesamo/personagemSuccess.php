e9fd6995aeed08c920866ad2c892cf29801a9035
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-28 18:34:32 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
@@ -41,7 +41,7 @@
                     <?php if($d->Asset->AssetType->getSlug() == "video"): ?>
                       <iframe width="351" height="263" src="http://www.youtube.com/embed/<?php echo $d->Asset->AssetVideo->getYoutubeId() ?>?wmode=transparent&rel=0" frameborder="0" allowfullscreen></iframe>
                     <?php elseif($d->Asset->AssetType->getSlug() == "image"): ?>
-                      <img src="<?php $d->Asset->AssetImage->retriveImageUrlByImageUsage("image-13-b"); ?>" alt="<?php echo $d->getTitle() ?>" />
+                      <img src="<?php echo $d->retriveImageUrlByImageUsage('image-13-b'); ?>" alt="<?php echo $d->getTitle() ?>" />
                     <?php endif; ?>
                     </div>
                   </li>
