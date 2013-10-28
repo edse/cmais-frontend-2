497172b5ebdf2e019e68c2b2379c238d5dfa6d9b
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-28 21:08:12 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/jogoSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/jogoSuccess.php
@@ -116,14 +116,9 @@
   </section>
   <!--/section-->
   
-  <?php if(isset($campaign)): ?>
-  <?php include_partial_from_folder('sites/vila-sesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
-  <?php include_partial_from_folder('sites/vila-sesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
-  <?php else: ?>
-  <?php include_partial_from_folder('sites/vila-sesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "categories" => $categories)) ?>
-  <?php include_partial_from_folder('sites/vila-sesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "categories" => $categories)) ?>
-  <?php endif; ?>
+  <?php //include_partial_from_folder('sites/vila-sesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
   
+  <?php include_partial_from_folder('sites/vila-sesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
 
   <?php include_partial_from_folder('sites/vila-sesamo', 'global/para-os-pais', array("site" => $site, "asset" => $asset, "categories" => $categories, "uri" => $uri)) ?>
 
