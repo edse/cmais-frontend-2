bd287e82293271e8227c5a2a7e05a7ee290d6b51
doljak <jedoljak@gmail.com>
2013-10-28 10:57:41 -0200

    u

--- apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
+++ apps/frontend/templates/sites/culturafm/nova-homeSuccess.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="http://cmais.com.br/portal/js/culturafm.js"></script>
- 
+<link rel="stylesheet" href="/portal/css/tvcultura/sites/culturafm-home2013.css" type="text/css" />
 <script type="text/javascript" src="http://cmais.com.br/portal/js/swfobject/swfobject.js"></script>
 <script type="text/javascript" src="http://cmais.com.br/portal/js/bootstrap/bootstrap.js"></script>
 
@@ -11,8 +11,42 @@
 <!-- container -->
 <div role="container">
   
-  <?php include_partial_from_folder('sites/culturafm','global/newheader', array('site' => $site, 'uri' => $uri, 'program' => $program, 'siteSections'=>$siteSections)) ?>
-  
+  <?php// include_partial_from_folder('sites/culturafm','global/newheader', array('site' => $site, 'uri' => $uri, 'program' => $program, 'siteSections'=>$siteSections)) ?>
+  <!--header principal-->
+  <header id="main-header" role="main" role="banner" >
+    
+    <!--logo-->
+    <h1>
+      <a href="http://culturafm.cmais.com.br" title="">
+        <img title="<?php echo $site->getTitle() ?>" alt="<?php echo $site->getTitle() ?>" src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>">
+      </a>
+    </h1>
+    <!--/logo-->
+    
+    <!-- network -->
+    <?php if(isset($program) && $program->id > 0): ?>
+    <?php include_partial_from_folder('sites/culturafm','global/social-network', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+    <?php endif;?>
+    <!-- network -->
+    
+    <!--box ouça as rádios -->
+    <div id="listen">
+      <h1>Ouça as rádios</h1>
+      <a href="javascript: window.open('http://culturabrasil.cmais.com.br/controleremoto','controle','width=400,height=600,scrollbars=no');void(0);" title="Rádio Cultura Brasil" class="link-radio cbrasil">Cultura brasil</a>
+      <a href="javascript: window.open('http://culturafm.cmais.com.br/controleremoto','controle','width=400,height=600,scrollbars=no');void(0);" title="Rádio Cultura FM" class="link-radio cfm">Cultura FM</a>
+    </div>  
+    <!--/box ouça as rádios -->
+    
+    <?php if(isset($siteSections)): ?>
+    <!-- main-nav -->    
+    <nav role="navigation" id="main-nav">
+      <?php include_partial_from_folder('sites/culturafm','global/menuNovo', array('siteSections' => $siteSections))?>
+    </nav>
+    <!-- /main-nav -->
+    <?php endif;?>
+    
+  </header>
+  <!--/header principal-->
 
   <!--content-holder-->
   <div id="content-holder">
