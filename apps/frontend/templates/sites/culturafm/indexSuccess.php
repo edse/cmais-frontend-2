361aa457ee5ce226c6ffad83263420c368029bf6
Joseval <Joseval@fpawks0146-2.local>
2013-10-28 16:56:30 -0200

    Link

--- apps/frontend/templates/sites/culturafm/indexSuccess.php
+++ apps/frontend/templates/sites/culturafm/indexSuccess.php
@@ -22,7 +22,7 @@
          
       <!--destaque cultura agora-->
       <div class="destaque c-agora">
-       <a href="http://culturafm.cmais.com.br/cultura-agora" title="Cultura Agora"><h1>Cultura Agora</a><i class="seta"></i></h1></a>
+        <h1>Cultura Agora</a><i class="seta"></i></h1>
 			<?php if(isset($displays["destaque-cultura-agora"])): ?>
 				<?php foreach ($displays["destaque-cultura-agora"] as $k => $d): ?>
 			        <!--item-->
