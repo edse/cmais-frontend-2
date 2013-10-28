90b89608f8138b9184c4c163d4b36b1d4b649eb2
Joseval <Joseval@fpawks0146-2.local>
2013-10-28 15:33:56 -0200

    Nova Home

--- apps/frontend/templates/sites/culturafm/indexSuccess.php
+++ apps/frontend/templates/sites/culturafm/indexSuccess.php
@@ -268,3 +268,59 @@
 </div>
 <!-- /container-->
 
+  <script type="text/javascript" src="http://cmais.com.br/portal/js/validate/jquery.validate.js"></script>
+<script>
+$(document).ready(function(){
+  $('.c-radio .chapeu').remove();
+  $('.c-radio').find('.titulos').remove();
+  $('#search_field').focus(function(){
+    $(this).val('');
+  });
+  $('#search_field').focusout(function(){
+    if($(this).val() == ''){
+      $(this).val('BUSCA');   
+    }
+  });
+  
+  //$('.c-radio').append('<div class="seta2" style="top: 14px;left: 10px;"></div>')
+  var validator = $('#form-email').validate({
+        
+      submitHandler: function(form){
+        //form.submit();
+        $.ajax({
+          type: "POST",
+          dataType: "text",
+          //url: "http://app.cmais.com.br/actions/culturafm/newsletter.php",
+          url: "/actions/culturafm/newsletter.php",
+          data: $("#form-email").serialize(),
+          beforeSend: function(){
+            $('#send_news, #news, #lbl_news').hide();
+            $('#ajax-loader').show();
+          },
+          success: function(data){
+            //window.location.href="#";
+            if(data == "0"){
+              $('#ajax-loader, #news').hide();
+              $('#msgAcerto').show();
+            }
+            else {
+              $('#ajax-loader, #news').hide();
+              $('#msgErro').show();
+            }
+          }
+        });         
+      },
+      rules:{
+        email_newsletter:{
+          required:true,
+          email: true
+        }
+      },
+      success: function(label){
+        label.addClass("checked");
+        $("label.error.checked").css("display","none");
+        label.html("&nbsp;");
+      }
+    });
+});
+</script>
