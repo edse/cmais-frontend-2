c902f1b72352014f396c69ab74c31a924e5e20dc
doljak <jedoljak@gmail.com>
2013-10-29 15:54:51 -0200

    y

--- apps/frontend/templates/sites/vila-sesamo/paraospaisSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/paraospaisSuccess.php
@@ -160,6 +160,48 @@
         <?php endif; ?>
         <!-- /selecione uma categoria--> 
         
+        <!--/section-->
+        <section class="todos-itens ">
+          <!--lista-->
+          <ul id="container" class="row-fluid">
+            
+            <li class="span4 element"> 
+              <a href="#" title="title">
+                <img src="http://midia.cmais.com.br/assets/image/image-13-b/255f75fd3506598980f287c10fa1ddc925872e96.jpg" alt="titulo/descrição" />
+                <i class="sprite-icons-new sprite-icone_atividades"></i>
+                <div>
+                  <img class="altura" src="/portal/images/capaPrograma/vilasesamo2/altura.png"/>
+                  Nome do Artigo
+                </div>
+              </a>
+            </li>
+    
+            <li class="span4 element"> 
+              <a href="#" title="title">
+                <img src="http://midia.cmais.com.br/assets/image/image-13-b/255f75fd3506598980f287c10fa1ddc925872e96.jpg" alt="titulo/descrição" />
+                <i class="sprite-icons-new sprite-icone_atividades"></i>
+                <div>
+                  <img class="altura" src="/portal/images/capaPrograma/vilasesamo2/altura.png"/>
+                  Nome do Artigo
+                </div>
+              </a>
+            </li>
+            
+            <li class="span4 element"> 
+              <a href="#" title="title">
+                <img src="http://midia.cmais.com.br/assets/image/image-13-b/255f75fd3506598980f287c10fa1ddc925872e96.jpg" alt="titulo/descrição" />
+                <i class="sprite-icons-new sprite-icone_atividades"></i>
+                <div>
+                  <img class="altura" src="/portal/images/capaPrograma/vilasesamo2/altura.png"/>
+                  Nome do Artigo
+                </div>
+              </a>
+            </li>
+            
+          </ul> 
+          <!--lista-->  
+        </section>
+        <!--/section-->
       </div>
       <!--/selecione-->
     </div>  
