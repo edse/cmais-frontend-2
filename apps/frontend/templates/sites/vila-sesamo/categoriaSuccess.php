e3b34069957d79032eca22ee29339f886a3d3dae
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-29 18:16:48 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
@@ -99,7 +99,10 @@
   
   <?php
     // Pega o bloco "parceiros" da seção "para os pais"
+    $forParents = Doctrine::getTable('Section')->findOneById(2399);
     $block = Doctrine::getTable('Block')->findOneBySectionIdAndSlug($forParents->getId(), "parceiros");
+    if ($block)
+      $displays["parceiros"] = $block->retriveDisplays(); // Pega os destaques do bloco "parceiros"    
   ?>
 
   <?php if(isset($displays['parceiros']) > 0): ?>
