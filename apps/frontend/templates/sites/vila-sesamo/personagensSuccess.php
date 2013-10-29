b3c056f558428435198e276563a9af4cc3a5c701
doljak <jedoljak@gmail.com>
2013-10-29 20:26:37 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/personagensSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/personagensSuccess.php
@@ -14,49 +14,49 @@
     <div class="conteudo-asset">
             
       <div id="container-personagens" class="asset">
-        <div class="element q-pers" data-order="1">
-          <a href="#">
+        <div class="element q-pers"  >
+          <a href="#" >
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_bel.png" alt="Bel" />
             <p>Bel</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_beto.png" alt="Beto" />
             <p>Beto</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_comecome.png" alt="Come-Come" />
             <p>Come-Come</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_elmo.png" alt="Elmo" />
             <p>Elmo</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_enio.png" alt="Enio" />
             <p>Enio</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_garibaldo.png" alt="Garibaldo" />
             <p>Garibaldo</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_grover.png" alt="Grover" />
             <p>Grover</p>
           </a>
         </div>
-        <div class="element q-pers" data-order="1">
+        <div class="element q-pers" >
           <a href="#">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_zoe.png" alt="Zoe" />
             <p>Zoe</p>
