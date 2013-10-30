e3ce65ce81e59fd39a5cf5827d522759a86a2863
doljak <jedoljak@gmail.com>
2013-10-30 19:58:18 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/indexSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/indexSuccess.php
@@ -1,10 +1,10 @@
 <link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/js/responsive-carousel/style-vilasesamo.css"/>
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/home.css" type="text/css" />
-
-<!--content-->
-<div id="content">
   <!--menu principal && banner promocional-->
   <?php include_partial_from_folder('sites/vila-sesamo', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)); ?>
+<!--content-->
+<div id="content">
+
   <?php include_partial_from_folder('sites/vila-sesamo', 'global/bannerprincipal'); ?>
   <!--/menu principal && banner promocional-->
   
