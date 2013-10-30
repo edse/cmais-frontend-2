312e281fcfcf6b4b326f83f73f326831227b9d01
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-30 21:42:27 -0200

    u

---
+++ apps/frontend/templates/sites/vila-sesamo/natvSuccess.php
@@ -0,0 +1,45 @@
+<!-- HEADER -->
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))?>
+<!-- /HEADER -->
+
+<!-- barra do titulo da seção -->
+<div>
+  <span><?php echo $section->getTitle() ?></span>
+</div>
+<!--/barra do titulo da seção -->
+
+<!-- destaques -->
+<div>
+  <?php if(isset($displays['historia'])): ?>
+    <?php if(count($displays['historia']) > 0): ?>
+  <div>
+      <?php echo html_entity_decode($displays['historia']->Asset->AssetContent->render()) ?>
+  </div>
+
+      <?php if(isset($displays['programacao-na-tv'])): ?>
+        <?php if(count($displays['programacao-na-tv']) > 0): ?>
+  <div>
+    <a href="<?php echo $displays['programacao-na-tv'][0]->retriveUrl() ?>" title="<?php echo $displays['programacao-na-tv'][0]->getTitle() ?>">
+      <img src="<?php echo $displays['programacao-na-tv'][0]->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $displays['programacao-na-tv'][0]->getTitle() ?>" />
+      <span><?php echo $displays['programacao-na-tv'][0]->getTitle() ?></span>
+      <span><?php echo $displays['programacao-na-tv'][0]->getHtml() ?></span>
+    </a>
+  </div>
+  
+  <div>
+    <a href="<?php echo $displays['programacao-na-tv'][1]->retriveUrl() ?>" title="<?php echo $displays['programacao-na-tv'][1]->getTitle() ?>">
+      <img src="<?php echo $displays['programacao-na-tv'][1]->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $displays['programacao-na-tv'][1]->getTitle() ?>" />
+      <span><?php echo $displays['programacao-na-tv'][1]->getTitle() ?></span>
+      <span><?php echo $displays['programacao-na-tv'][1]->getHtml() ?></span>
+    </a>
+  </div>          
+        <?php endif; ?>  
+      <?php endif; ?>  
+
+    <?php endif; ?>  
+  <?php endif; ?>  
+</div>
+<!--/destaques-->
+
+
+
--- apps/frontend/templates/sites/vila-sesamo/personagensSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/personagensSuccess.php
@@ -15,49 +15,49 @@
             
       <div id="container-personagens" class="asset">
         <div class="element q-pers inner bel"  >
-          <a href="#" >
+          <a href="/<?php echo $site->getSlug() ?>/personagens/bel" >
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_bel.png" alt="Bel" />
             <p>Bel</p>
           </a>
         </div>
         <div class="element q-pers inner beto" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/beto">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_beto.png" alt="Beto" />
             <p>Beto</p>
           </a>
         </div>
         <div class="element q-pers inner come-come" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/come-come">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_comecome.png" alt="Come-Come" />
             <p>Come-Come</p>
           </a>
         </div>
         <div class="element q-pers inner elmo" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/elmo">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_elmo.png" alt="Elmo" />
             <p>Elmo</p>
           </a>
         </div>
         <div class="element q-pers inner enio" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/enio">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_enio.png" alt="Enio" />
             <p>Enio</p>
           </a>
         </div>
         <div class="element q-pers inner garibaldo" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/garibaldo">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_garibaldo.png" alt="Garibaldo" />
             <p>Garibaldo</p>
           </a>
         </div>
         <div class="element q-pers inner grover" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/grover">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_grover.png" alt="Grover" />
             <p>Grover</p>
           </a>
         </div>
         <div class="element q-pers inner zoe" >
-          <a href="#">
+          <a href="/<?php echo $site->getSlug() ?>/personagens/zoe">
             <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/personagens/vs_personagem_zoe.png" alt="Zoe" />
             <p>Zoe</p>
           </a>
