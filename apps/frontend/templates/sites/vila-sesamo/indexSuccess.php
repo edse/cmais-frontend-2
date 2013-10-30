fa17c29f2b1e9f2002962e2e5b4669b4ec13504c
doljak <jedoljak@gmail.com>
2013-10-30 18:16:33 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/indexSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/indexSuccess.php
@@ -1,243 +1,183 @@
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/js/layer-slider/model06/jquery.layerSlider.css">
+<link rel="stylesheet" href="http://cmais.com.br/portal/js/layer-slider/model06/main.css">
+
+<link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/js/responsive-carousel/style-vilasesamo.css"/>
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/home.css" type="text/css" />
-<script src="http://cmais.com.br/portal/js/modernizr/modernizr.min.js" type="text/javascript"></script>
-<link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/js/responsive-carousel/style.css"/>
+
 
 <div id="content">
-  <nav class="header-bar">
-    <h1><a href="/vila-sesamo" class="sprite-logo-sesamo" title="Vila Sésamo">Vila Sésamo</a></h1>
-    <ul>
-      <li class="btn-jogos" data-width="160" data-time="400" data-back="200">
-        <a href="/vila-sesamo/jogos" title="Jogos">
-          <span class="texto">Jogos</span>
-          <i class="sprite-btn-jogos"></i>
-          <span class="fundo fundo-jogos"></span>
-          <span class="borda borda-jogos"></span>
-        </a>
-      </li>
-      <li class="btn-videos" data-width="170" data-time="400" data-back="200">
-        <a href="/vila-sesamo/videos"  title="Vídeos">
-          <span class="texto">Vídeos</span>
-          <i class="sprite-btn-videos"></i>
-          <span class="fundo fundo-videos"></span>
-          <span class="borda borda-videos"></span>
-        </a>
-      </li>
-      <li class="btn-atividades" data-width="225" data-time="800" data-back="500">
-        <a href="/vila-sesamo/atividades"  title="Atividades">
-          <span class="texto">Atividades</span>
-          <i class="sprite-btn-atividades"></i>
-          <span class="fundo fundo-atividades"></span>
-          <span class="borda borda-atividades"></span>
-        </a>
-     </li>
-      <li class="btn-personagens" data-width="260" data-time="1000" data-back="500">
-        <a href="/vila-sesamo/personagens"  title="Personagens">
-          <span class="texto">Personagens</span>
-          <i class="sprite-btn-personagens"></i>
-          <span class="fundo fundo-personagens"></span>
-          <span class="borda borda-personagens"></span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <section id="carrossel-destaque">
-    <h1 style="position:relative; top:200px; color:#fff;">destaque carrossel</h1>
-  </section>
-  
-  <section class="bgtotal">
-    <div id="carrossel-personagem">
-    <span class="divisa1"></span>
-    <div class="carrossel-p" id="carrossel-p">
-      <div class="slider">
-        <div class="header-carrossel-personagens">
-          <span class="topo-p"></span>
-          <i class="sprite-icon-personagens-peq"></i>
-          <h3><a href="/vila-sesamo/personagens">Personagens</a></h3>
-          
-          <div class="slider-mask-wrap">
-            <div class="slider-mask">
-              <ul class="slider-target">
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                <li>
-                  <div class="inner">
-                    <a href="/vila-sesamo/beto" title="Beto"><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/personagem.jpg" alt="Personagem" /></a>
-                  </div>
-                </li>
-                
-              </ul>
-              <div class="clearit"></div>
-            </div>
-          </div>
-        </div>  
-        <div class="slider-nav">
-          <div class="arrow-left arrow">
-            <span title="Back" class="sprite-seta-esquerda"></span>
-          </div>
-          <div class="arrow-right arrow">
-            <span title="Next" class="sprite-seta-direita"></span>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-  </section>
+
+  <?php include_partial_from_folder('sites/vilasesamo2', 'global/menuprincipal', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)); ?>
+  <?php include_partial_from_folder('sites/vilasesamo2', 'global/bannerprincipal'); ?>
+  <?php include_partial_from_folder('sites/vilasesamo2', 'global/bannerprincipalmobile'); ?> 
+
+
+  <!--carrossel personagens-->
+  <?php include_partial_from_folder('sites/vilasesamo2', 'global/menupersonagens', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section));?>
+  <!--carrossel personagens-->
+
+  <!-- link seções -->
   <section class="bgtotal">
     <span class="divisa1"></span>
     <div class="destaques row-fluid container">
-    <section class="span8">
-      <article class="span6 jogo">
-        <a href="/vila-sesamo/jogos" title="Jogo">
-          <img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/escola-pra-cachorro.jpg" alt="Jogos" />
-        </a> 
-        <h3><a href="/vila-sesamo/jogos" title="Jogos">Jogo</a></h3>       
-      </article>
-      <article class="span6 video">
-        <a href="/vila-sesamo/clipes" title="Clipes">
-          <img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/escola-pra-cachorro.jpg" alt="Clipes" />
-        </a> 
-        <h3><a href="/vila-sesamo/clipes" title="Clipes">Clipe</a></h3>       
-      </article>
-      <article class="span6 atividade">
-        <a href="/vila-sesamo/atividades" title="Para Colorir">
-          <img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/escola-pra-cachorro.jpg" alt="Para Colorir" />
-        </a> 
-        <h3><a href="/vila-sesamo/atividades" title="Para Colorir">Para Colorir</a></h3>       
-      </article>
-    
-    </section>
-    <div class="span4 banner" >
-      <a href="#" title=""><img src="http://cmais.com.br/portal/images/capaPrograma/vila-sesamo/escola-pra-cachorro.jpg" alt="" /></a>
-    </div>
+      <section class="span8">
+        <article class="span6 jogo">
+          <a href="/vilasesamo2/jogos" title="Jogo">
+            <i class="sprite-icon-jogos-peq"></i>Jogos
+            
+          </a>
+          <a href="/vilasesamo2/jogos" class="img" ><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="Jogos" /></a> 
+          <a class="asset" href="/vilasesamo2/jogos-interna" title="Jogos">Nome do Jogo</a>  
+        </article>
+        <article class="span6 video">
+          <a href="/vilasesamo2/clipes" title="Vídeos">
+            <i class="sprite-icon-videos-peq"></i>Vídeos
+            
+          </a>
+          <a href="/vilasesamo2/clipes" class="img"><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="Clipes" /></a> 
+          <a class="asset" href="/vilasesamo2/videos-interna" title="Clipes">Nome do Video</a>      
+        </article>
+        <article class="span6 atividade">
+          <a href="/vilasesamo2/atividades" title="Atividades">
+            <i class="sprite-icon-atividades-peq"></i>atividades
+            
+          </a> 
+          <a href="/vilasesamo2/atividades" class="img" ><img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="Atividades" /></a>
+          <a class="asset" href="/vilasesamo2/atividades-interna" title="Para Colorir">Nome da atividade</a>       
+        </article>
+      
+
+      <div class="span4 banner" >
+        <a href="#" title="Incluir Brincando" class="sprite-btn-incluir"></a>
+        <a href="#" title="Hábitos para uma vida saudável" class="sprite-btn-habitos"></a>
+        <a href="#" title="O que achou do novo site?" class="sprite-btn-contato"></a>
+      </div>
     </div>
   </section>
+  <!-- link seções -->
+  
 </div>
-<script src="http://cmais.com.br/portal/js/modernizr/modernizr.min.js" type="text/javascript"></script>
-<link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/js/responsive-carousel/style-vilasesamo.css"/>
-<script src="http://cmais.com.br/portal/js/jquery-ui/js/jquery-ui.min.js"></script>
-<script src="http://cmais.com.br/portal/js/hammer.min.js"></script>
-<script src="http://cmais.com.br/portal/js/responsive-carousel/script.js"></script>
-<script>
-  $(document).ready(function() {
-    //carrossel personagens
-    $('#carrossel-p').responsiveCarousel({
-      target:'#carrossel-p .slider-target',
-      unitElement:'#carrossel-p .slider-target > li',
-      mask:'#carrossel-p .slider-mask',
-      easing:'linear',
-      dragEvents:true,
-      speed:200,
-      slideSpeed:1000,
-      responsiveUnitSize : function() {
-        return 2;
-      },
-      step : -1,
-      onShift : function(i) {
-        var $current = $('#selector li a[rel=frame_' + i + ']');
-        $('#selector li a').removeClass('current');
-        $current.addClass('current');
-      }
-    });
-    
-    if(screen.width > 980){
-      $('#carrossel-p').mouseenter(function(){
-        $('.arrow').fadeIn('fast');
-      });
-      $('#carrossel-p').mouseleave(function(){
-         $('.arrow').fadeOut('fast');
-      });
-    };
-    if(navigator.appName!='Microsoft Internet Explorer')
-    {
-      //carrossel personagens redraw pra tablet e celular
-      window.addEventListener('load', function() {
-        $('.carrossel-p').responsiveCarousel('redraw');
-      });
-      window.addEventListener("orientationchange", function() {
-        $('.carrossel-p').responsiveCarousel('redraw');
-      }, false);
-      window.addEventListener("resize", function() {
-        $('.carrossel-p').responsiveCarousel('redraw');
-      }, false);
-      //carrossel personagens redraw pra tablet e celular
-    }
-    //carrossel personagens
-    
-    //menu principal
-    var w_default = 62;
-    $('.header-bar ul li').mouseenter(function() {
-      var el = $("." + $(this).attr("class") + " .fundo");
-      var elBorda = $("." + $(this).attr("class") + " .borda");
-      var w_time = $(this).attr("data-time");
-      var w_button = $(this).attr("data-width");
-      
-      el.stop();
-      $(this).stop();
-      
-      el.show();
-      elBorda.show();
-      $(this).animate({
-        width: w_button
-      }, w_time);
-      el.animate({
-        width:  w_button
-      }, w_time);
-    });
-    
-    $('.header-bar ul li').mouseleave(function() {
-      var el = $("." + $(this).attr("class") + " .fundo");
-      var elBorda = $("." + $(this).attr("class") + " .borda");
-      var w_back = $(this).attr("data-back")
-      
-      $(this).stop();
-      el.stop();
-      
-      $(this).animate({
-        width: w_default
-      }, w_back);
-      el.animate({
-        width: w_default
-      }, w_back, function(){
-        if(el.width() == w_default){
-          el.hide();
-          elBorda.hide();
-        }
-      });
-      //menu principal
-    });
 
+<!--scripts e css banner-->
+<script type="text/javascript" src="http://cmais.com.br/portal/js/layer-slider/jQuery.layerSlider.js"></script>
+<script src="http://cmais.com.br/portal/js/jquery-ui/js/jquery-ui-1.8.11.custom.min.js"></script>
+<script type="text/javascript" src="http://cmais.com.br/portal/js/modernizr/modernizr.min.js"></script>
+<script type="text/javascript" src="http://cmais.com.br/portal/js/hammer.min.js"></script>
+<script type="text/javascript" src="http://cmais.com.br/portal/js/responsive-carousel/script.js"></script>
+
+<script>
+//verificação de tela
+
+
+//carrossel personagens home
+$('#carrossel-p').responsiveCarousel({
+  arrowLeft: '.arrow-left span.personagens',
+  arrowRight: '.arrow-right span.personagens',
+  target:'#carrossel-p .slider-target',
+  unitElement:'#carrossel-p .slider-target > li',
+  mask:'#carrossel-p .slider-mask',
+  easing:'linear',
+  dragEvents:true,
+  speed:200,
+  slideSpeed:1000,
+  responsiveUnitSize : function() {
+    return 2;
+  },
+  step : -1
+});
+
+if(screen.width > 1024){
+  $('#carrossel-p').mouseenter(function(){
+    $('.arrow.personagem').fadeIn('fast');
   });
+  
+  $('#carrossel-mobile').mouseenter(function(){
+    $('.arrow.destaque-mobile').fadeIn('fast');
+  });
+};
+if(navigator.appName!='Microsoft Internet Explorer'){
+  //carrossel personagens redraw pra tablet e celular home
+  window.addEventListener('load', function() {
+    $('.carrossel-p, #carrossel-mobile').responsiveCarousel('redraw');
+    machineScreenSize();
+  });
+  window.addEventListener("orientationchange", function() {
+    $('.carrossel-p, #carrossel-mobile').responsiveCarousel('redraw');
+    machineScreenSize();
+  }, false);
+  window.addEventListener("resize", function() {
+    $('.carrossel-p, #carrossel-mobile').responsiveCarousel('redraw');
+    machineScreenSize();
+  }, false);
+  //carrossel personagens redraw pra tablet e celular home
+}
+/*
+var alturaImg;
+var alturaBox;
+var altura;
+$(".inner a img").each(function(i){
+  if(i==0){
+    $(this).attr("src", $(this).attr("src")).load(function() {
+      alert('rodei load');
+      alturaImg = this.height;
+      alturaBox = $('.inner').height();
+      altura = alturaBox - alturaImg;
+    });
+  }
+});
+*/
+$('.inner.personagens a').mouseenter(function(){
+  $(this).find('img').animate({top:-70, easing:"swing"},'fast');
+});
+$('.inner.personagens a').mouseleave(function(){
+  $(this).find('img').stop();
+  $(this).find('img').animate({top:0, easing:"swing"},'fast');  
+});
+
+//carrossel personagens
+function windowSize(){
+  if(screen.width >= 640){
+    $('#carrossel-destaque').css('display','block');
+    $('#carrossel-destaque-mobile').css('display','none');
+  }else{
+    $('#carrossel-destaque').css('display','none');
+    $('#carrossel-destaque-mobile').css('display','block');
+  }
+}
+function machineScreenSize(){
+  //alert(window.innerWidth);
+  //alert(window.screen.width);
+  var ua = navigator.userAgent.toLowerCase();
+  //alert(ua);
+
+  if(ua.indexOf("mobile") > -1 && (ua.indexOf("iphone") > -1 || ua.indexOf("android") > -1)) {
+    //alert("Aplicar Versão Mobile");
+    botoesPersonagensMobile();
+    //Verificar Windows Phone
+  }else{
+    if(window.screen.width <= 640){
+      //alert("Aplicar Versão Mobile");
+      botoesPersonagensMobile();
+      
+    }else if(window.screen.width <= 1024){
+      //alert("Aplicar Versão TABLET");
+      botoesPersonagensTablet();
+    }else{
+      //alert("Aplicar Versão Desktop");
+
+    }
+  }
+}
+function botoesPersonagensMobile(){
+  $('.inner.personagens a').each(function(i){
+    $(this).find('img').delay(1000 + (i*400)).animate({top:-50},'fast');
+  });
+}
+
+function botoesPersonagensTablet(){
+  $('.inner.personagens a').each(function(i){
+    $(this).find('img').delay(1000 + (i*400)).animate({top:60},'fast');
+  });
+}
 </script>
