e75c82585f69d8e33b234a31e924be34fcb0fd65
doljak <jedoljak@gmail.com>
2013-10-31 16:55:26 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
@@ -1,6 +1,37 @@
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lt IE 8]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
+
+<script>
+  $("body").addClass("na-tv");
+</script>
+
 <!-- HEADER -->
-<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))?>
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 <!-- /HEADER -->
+
+<!--content-->
+<div id="content">
+  
+  <!--section -->
+  <section class="filtro row-fluid">
+    
+    <!--container conteudo-->
+    <div class="b-verde borda-arredonda">
+      <h1>
+        <span class="icones-sprite-interna icone-na-tv-grande"></span>
+        <?php echo $section->getTitle() ?>
+      </h1>
+      
+    </div>
+    <!--/container conteudo-->
+    
+  </section>
+  <!--/section -->
+  
+</div>
+<!--/content-->      
 <?php 
 /**
  * programado
