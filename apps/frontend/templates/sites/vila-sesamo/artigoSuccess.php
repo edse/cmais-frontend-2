33cfc0ca65b90707f822a1e09d46ab053f16c55d
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-10-31 17:17:28 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
@@ -47,8 +47,8 @@
     <p>Sobre os autores:</p>
     <?php foreach($colaborador as $c): ?>
     <div>
-      <?php if($c->retriveImageUrlByImageUsage("image-13-b")): ?>
-      <img src="<?php echo $c->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $c->getTitle() ?>" />
+      <?php if($c->retriveImageUrlByImageUsage("image-13")): ?>
+      <img src="<?php echo $c->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $c->getTitle() ?>" />
       <?php endif; ?>
       <p><?php echo $c->getTitle() ?></p>
       <p><?php echo $c->AssetImage->getSource() ?></p>
