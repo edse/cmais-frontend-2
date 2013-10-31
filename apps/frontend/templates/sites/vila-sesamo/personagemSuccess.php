16a216e85271230b83316df827ac592f83a431eb
Joseval <Joseval@fpawks0146-2.local>
2013-10-31 15:29:23 -0200

    Carrossel

--- apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
@@ -210,10 +210,44 @@
   <link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/js/responsive-carousel/style-vilasesamo.css"/>
   
   <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script> 
-  <script type="text/javascript" src="http://cmais.com.br/portal/js/vilasesamo2/youtubeapi.js"></script> 
+  <!-- script type="text/javascript" src="http://cmais.com.br/portal/js/vilasesamo2/youtubeapi.js"></script --> 
+  
   
   <script>
-  $(document).ready(function() {  
+  
+	 $(document).ready(function() {
+	  //arrays para players multiplos
+	  var cont = 0;
+	  var player = new Array();
+	  var players_ids = new Array();
+	  var playing;
+	  var playing_id = false;
+	  
+	  onYouTubeIframeAPIReadyPlayer = function(obj, cont) {
+	    //console.log("start"+cont);
+	    //console.log("obj:"+obj);
+	    //console.log("contador:"+cont);
+	    player[cont] = new YT.Player(obj);
+	    //console.log("player:"+player[cont]);
+	    player[cont].addEventListener("onStateChange", function(res){
+	      if(res.data == 1){
+	        $('#carrossel-interna-personagem').responsiveCarousel('stopSlideShow');
+	        playing = res.target;
+	        //console.log('playing:'+playing.pauseVideo());
+	      }
+	      if(res.data == 0){
+	        $('#carrossel-interna-personagem').responsiveCarousel('toggleSlideShow');
+	      }
+	    });
+	  }
+	  
+	  $('.videoorimage iframe').each(function(i){
+	    if($(this).attr('src').indexOf("youtube") != -1){
+	      cont++;
+	      $(this).attr("id","player"+cont);
+	      onYouTubeIframeAPIReadyPlayer("player"+cont , cont)
+	    }
+	  });
 
   //carrossel
   var total=0;
