b6e85e0e4bff9c5211fd91d2a03ea3d645d4fb39
doljak <jedoljak@gmail.com>
2013-10-31 20:36:17 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
@@ -122,7 +122,7 @@
   
   function goTop(){
     $('html, body').animate({
-      scrollTop: parseInt($('.divisa.top').offset().top)
+      scrollTop: parseInt($('.divisa.top').offset().top)+300
     }, "fast");
   }
 </script>    
