436e51a39b37da7ed80f0a5b4050258082d3d9f7
doljak <jedoljak@gmail.com>
2013-10-31 17:31:51 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
@@ -112,7 +112,14 @@
   <!--/section-->
   
 </div>
-<!--/content-->      
+<!--/content--> 
+
+<input type="hidden" id="filter-choice" value="">
+
+<nav id="page_nav">
+  <a href="/testes/vilasesamo2/pages/2.html" class="mais">Carregar mais<i class="icones-sprite-interna icone-carregar-br-grande"></i></a>
+</nav>
+     
 <?php 
 /**
  * programado
