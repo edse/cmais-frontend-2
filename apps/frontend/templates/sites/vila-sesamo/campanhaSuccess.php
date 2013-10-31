b05cf842609d9c59b0abb44e31cf561415c65b0b
doljak <jedoljak@gmail.com>
2013-10-31 16:46:00 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/campanhaSuccess.php
@@ -1,7 +1,10 @@
 <!-- HEADER -->
 <?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section))?>
 <!-- /HEADER -->
-
+<?php 
+/**
+ * programado
+ 
 <h1><?php echo $section->getTitle() ?></h1>
 
 <?php
@@ -82,5 +85,5 @@
   }
 </script>
 
-
+*/?>
 
