d2c674992e00d4945c527745732b6c8d121e59b2
doljak <jedoljak@gmail.com>
2013-11-01 19:49:26 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
@@ -44,17 +44,24 @@
                
               <div class="overflow">
                 <span class="data">DD/MM/AAAA - por</span>
-                <span class"nome">Nome do Autor</span>
+                <span class="nome">Nome do Autor</span>
               </div>
               
               <!--compartilhar redes-->
-              <div class="redes-artigo">
+              <div class="redes-artigo redes overflow">
+                
                 <div class="divisa"></div>
+                
                 <p>compartihe este artigo</p>
                 <div class="redes">
-                   redes  
+                  <g:plusone size="medium" count="false"></g:plusone>
+                  <a href="//pinterest.com/pin/create/button/" data-pin-do="buttonBookmark" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" alt="Pinterest" style="margin-top:-10px;" /></a>
+                  <fb:like href="<?php echo $uri ?>" layout="button_count" show_faces="false" send="false"></fb:like>
+                  <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="portalcmais" data-related="tvcultura">Tweet</a> 
                 </div>
-                <div class="divisa"></div>
+                
+                <div class="divisa clearboth"></div>
+                
               </div>
               <!--/compartilhar redes-->
               
