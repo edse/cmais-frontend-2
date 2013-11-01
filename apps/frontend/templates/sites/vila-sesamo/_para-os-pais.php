14fe8a333dff0bd6f08ac05fe802214715183798
Cristovam Ruiz Jr <contato@cristovamjr.com.br>
2013-11-01 19:54:19 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_para-os-pais.php
+++ apps/frontend/templates/sites/vila-sesamo/_para-os-pais.php
@@ -5,7 +5,6 @@
      * Se o asset chamado pertencer a uma categoria especial (seção filha de "categorias" e marcada como "is homepage") as dicas e artigos serão destaques dos blocos "dicas" e "artigos", respectivamente, da seção dessa categoria.
      * Senão busca assets com semelhança de tags
      */
-    $dicaRelacionada = "";
     if(isset($asset))
       $dicaRelacionada = $asset->retriveRelatedAssetsByRelationType("Asset Relacionado");
     
