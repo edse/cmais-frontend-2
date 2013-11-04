bd3ccf4e83637ed0029b070fc549010fbe15262d
doljak <jedoljak@gmail.com>
2013-11-04 17:28:32 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
@@ -89,22 +89,31 @@
           <button type="submit" class="btn">baixar</button>
         </div>
         
-        <div class="span4 box-select">
-          <a href="#" title=""> <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="thumb do jogo" /> </a>
-          <h2><a>Nome jogo</a></h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat metus ut leo interdum eleifend.</p>
-        </div>
+      <div class="span4 box-select">
+        <a href="#" title=""> <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/escola-pra-cachorro.jpg" alt="thumb do jogo" /> </a>
+        <h2><a>Nome jogo</a></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat metus ut leo interdum eleifend.</p>
+      </div>
+      <div class="span4">
+        <p>Conheça nossos parceiros:</p>
+        <a class="publicidade" href="#" title="Publicidade">
+          <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner-sesc.png" alt="Sesc" />
+        </a>
+        <a class="publicidade" href="#" title="Publicidade">
+          <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner-mapa.png" alt="Mapa do Brincar" />
+        </a>
         
-        <div class="span4">
-          <p>Conheça nossos parceiros:</p>
-          <a class="publicidade" href="#" title="Publicidade">
-            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner-sesc.png" alt="Sesc" />
-          </a>
-          <a class="publicidade" href="#" title="Publicidade">
-            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner-mapa.png" alt="Mapa do Brincar" />
-          </a>
+        <p>Você também pode escolher o jogo de acordo com as preferências da criança:</p>
+        <div class="btn-group">
+          <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> Selecione a categoria <span class="caret sprite-seta-down-amarela"></span> </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">categoria 01</a></li>
+            <li><a href="#">categoria 02</a></li>
+            <li><a href="#">categoria 03</a></li>
+            <li><a href="#">categoria 04</a></li>
+          </ul>
         </div>
-        
+      </div>
       </div> 
     </div> 
        
