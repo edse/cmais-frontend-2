d1153722f9ae5d9c7f9377af4ade80eb1a2b0f4d
doljak <jedoljak@gmail.com>
2013-11-05 16:22:49 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/buscaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/buscaSuccess.php
@@ -1,1 +1,10 @@
-teeste
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lt IE 8]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
+
+
+
+<!-- HEADER -->
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+<!-- /HEADER -->
