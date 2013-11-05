b5c1e8a4fdfc71496a3f41b25a9d17978672dc53
doljak <jedoljak@gmail.com>
2013-11-05 20:37:18 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_menupersonagens.php
+++ apps/frontend/templates/sites/vila-sesamo/_menupersonagens.php
@@ -14,68 +14,67 @@
               <ul class="slider-target">
                 <li>
                   <div class="inner personagens bel">
-                    <a class="nome" href="/vilasesamo2/personagens/bel" title="Bel">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="Personagem" />
-                      
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/bel" title="Bel" class="btn-bel" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/bel_personagem.png" alt="" />
                     </a>
-                    <span class="nome">Bel</span>
                   </div>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/bel" title="Bel">Bel</a>
                 </li>
                 <li>
                   <div class="inner personagens beto">
                     <a href="/<?php echo $site->getSlug(); ?>/personagens/beto" title="Beto" class="btn-beto" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="Personagem" />
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="" />
                     </a>
                   </div>
-                  <a class="nome" href="/vilasesamo2/personagens/beto" title="Bel">Beto</a>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/beto" title="Bel">Beto</a>
                 </li>
                 <li>
                   <div class="inner personagens comecome">
-                    <a href="/vilasesamo2/personagens/come-come" title="Come-come" class="btn-comecome" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="Personagem" />
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/come-come" title="Come-come" class="btn-comecome" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="" />
                     </a>
                   </div>
-                  <a class="nome" href="/vilasesamo2/personagens/comecome" title="Come-come">Come-come</a>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/comecome" title="Come-come">Come-come</a>
                 </li>
                 <li>
                   <div class="inner personagens elmo">
-                    <a href="/vilasesamo2/personagens/elmo" title="Elmo" class="btn-elmo" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="Personagem" />
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/elmo" title="Elmo" class="btn-elmo" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="" />
                     </a>
                   </div>
-                  <a class="nome" href="/vilasesamo2/personagens/elmo" title="Elmo">Elmo</a>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/elmo" title="Elmo">Elmo</a>
                 </li>
                 <li>
                   <div class="inner personagens enio">
-                   <a href="/vilasesamo2/personagens/enio" title="Ênio" class="btn-enio" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="Personagem" />
+                   <a href="/<?php echo $site->getSlug(); ?>/personagens/enio" title="Ênio" class="btn-enio" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="" />
                    </a>
                    </div>
-                   <a class="nome" href="/vilasesamo2/personagens/enio" title="Ênio">Ênio</a>
+                   <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/enio" title="Ênio">Ênio</a>
                 </li>
                 <li>
                   <div class="inner personagens garibaldo">
-                    <a href="/vilasesamo2/personagens/garibaldo" title="Garibaldo" class="btn-garibaldo" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="Personagem" />
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/garibaldo" title="Garibaldo" class="btn-garibaldo" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="" />
                     </a>
                   </div>
-                  <a class="nome" href="/vilasesamo2/personagens/garibaldo" title="Garibaldo">Garibaldo</a>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/garibaldo" title="Garibaldo">Garibaldo</a>
                 </li>
                 <li>
                   <div class="inner personagens grover">
-                    <a href="/vilasesamo2/personagens/grover" title="Grover" class="btn-grover" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="Personagem" />
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/grover" title="Grover" class="btn-grover" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="" />
                     </a>
                   </div>
-                  <a class="nome" href="/vilasesamo2/personagens/grover" title="Grover">Grover</a>
+                  <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/grover" title="Grover">Grover</a>
                 </li>
                 <li>
                   <div class="inner personagens zoe">
-                    <a href="/vilasesamo2/personagens/zoe" title="Zoe" class="btn-zoe" role="presentation" aria-hidden="true">
-                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="Personagem" />
+                    <a href="/<?php echo $site->getSlug(); ?>/personagens/zoe" title="Zoe" class="btn-zoe" role="presentation" aria-hidden="true">
+                      <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="" />
                    </a>
                    </div>
-                   <a class="nome" href="/vilasesamo2/personagens/zoe" title="Zoe">Zoe</a>
+                   <a class="nome" href="/<?php echo $site->getSlug(); ?>/personagens/zoe" title="Zoe">Zoe</a>
                 </li>
                 
               </ul>
