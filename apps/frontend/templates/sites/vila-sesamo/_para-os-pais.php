c9c8adc1db25f14464e5649635b6f1089cc72b9e
doljak <jedoljak@gmail.com>
2013-11-05 16:01:00 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_para-os-pais.php
+++ apps/frontend/templates/sites/vila-sesamo/_para-os-pais.php
@@ -1,7 +1,7 @@
 
   <section class="pais">
     <span class="divisa"></span>
-    <h2>Cuidadores <i class="icones-setas icone-cuidadores-abrir"></i></h2>
+    <h2 class="tit-box">Cuidadores <i class="icones-setas icone-cuidadores-abrir"></i></h2>
     <div class="content span12 row-fluid">
       
       <div class="redes">
