5750bebd48223989d757573a365ef94ca0c02a3d
doljak <jedoljak@gmail.com>
2013-11-08 16:37:43 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_menu-personagens.php
+++ apps/frontend/templates/sites/vila-sesamo/_menu-personagens.php
@@ -3,7 +3,8 @@
         <h3>escolha o personagem</h3>
         <h3 aria-live="polite" id="filtro-descricao">todas as atividades estão para selecionar</h3>
         <ul class="filtro-personagem">
-         
+          <?php $particularSection = Doctrine::getTable('Section')->findOneBySiteIdAndSlug($site->getId(),"personagens"); ?>
+          <?php echo $particularSection->subsections()?>
           <li>
             <div class="inner bel">
               <a href="javascript:;" class="btn-bel " data-filter=".bel">
@@ -11,54 +12,7 @@
               </a>
             </div>
           </li>
-          <li>
-            <div class="inner beto">
-              <a href="javascript:;" class="btn-beto " data-filter=".beto">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/beto_personagem.png" alt="filtro beto" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <div class="inner comecome">
-              <a href="javascript:;" class="btn-comecome " data-filter=".come-come">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/comecome_personagem.png" alt="filtro Come-come" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <div class="inner elmo">
-              <a href="javascript:;" class="btn-elmo " data-filter=".elmo">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/elmo_personagem.png" alt="filtro Elmo" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <div class="inner enio">
-              <a href="javascript:;" class="btn-enio " data-filter=".enio">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/enio_personagem.png" alt="filtro Enio" /></a>
-             </div>
-          </li>
-          <li>
-            <div class="inner garibaldo">
-              <a href="javascript:;" class="btn-garibaldo " data-filter=".garibaldo">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/garibaldo_personagem.png" alt="filtro Garibaldo" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <div class="inner grover">
-              <a href="javascript:;" class="btn-grover " data-filter=".grover">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/grover_personagem.png" alt="filtro Grover" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <div class="inner zoe">
-              <a href="javascript:;" class="btn-zoe " data-filter=".zoe">
-                <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/zoe_personagem.png" alt="filtro Zoe" />
-              </a>
-             </div>
-          </li>
+          
         </ul>
       </nav>
       <!--/nav filtro personagem-->
