fad1f691fe9cdaba51df20a866ded8d22f44cf06
doljak <jedoljak@gmail.com>
2013-11-08 12:08:18 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
@@ -212,18 +212,8 @@
               }
             }
           ?>
+          
           <li class="span4 element <?php echo $a->getSlug(); ?>"> 
-            <a href="/<?php echo $site->getSlug() ?>/<?php echo $a->getSlug(); ?>/<?php echo $d->getSlug() ?>" title="<?php echo $d->getTitle() ?>">
-              <?php $related = $d->retriveRelatedAssetsByRelationType("Preview") ?>
-              <img src="<?php echo $related[0]->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $d->getTitle() ?>" />
-              <i class="sprite-icons-new sprite-icone_<?php echo $assetSection->getSlug() ?>"></i>
-              <div>
-                <img src="/portal/images/capaPrograma/vilasesamo2/altura.png"/>
-                <?php echo $d->getTitle() ?>
-              </div>
-            </a>
-          </li>
-          <li class="span4 element <?php if(count($assetPersonagens) > 0) echo " " . implode(" ", $assetPersonagens); ?> <?php echo $a->getSlug(); ?>"> 
             <a href="/<?php echo $site->getSlug() ?>/<?php echo $a->getSlug(); ?>/<?php echo $d->getSlug() ?>" title="<?php echo $d->getTitle() ?>">
               <img src="<?php echo $related[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $d->getTitle() ?>"/>
               <i class="icones-sprite-interna icone-<?php echo $a->getSlug(); ?>-pequeno"></i>
