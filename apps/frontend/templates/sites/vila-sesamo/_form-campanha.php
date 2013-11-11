e7220864c40d333a7ec86ce570f87c012fa9b8e5
doljak <jedoljak@gmail.com>
2013-11-11 15:24:58 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
+++ apps/frontend/templates/sites/vila-sesamo/_form-campanha.php
@@ -284,25 +284,27 @@
       verifyKey();
     });
     
-  });
-  var SITE = SITE || {};
+    var SITE = SITE || {};
 
-  SITE.fileInputs = function() {
-    var $this = $(this),
-        $val = $this.val(),
-        valArray = $val.split('\\'),
-        newVal = valArray[valArray.length-1],
-        $button = $this.siblings('.button'),
-        $fakeFile = $this.siblings('.file-holder');
-    if(newVal !== '') {
-      $button.text('Anexar');
-      if($fakeFile.length === 0) {
-        $button.after('<span class="file-holder">' + newVal + '</span>');
-      } else {
-        $fakeFile.text('Anexo: 'newVal);
+    SITE.fileInputs = function() {
+      var $this = $(this),
+          $val = $this.val(),
+          valArray = $val.split('\\'),
+          newVal = valArray[valArray.length-1],
+          $button = $this.siblings('.button'),
+          $fakeFile = $this.siblings('.file-holder');
+      if(newVal !== '') {
+        $button.text('Anexar');
+        if($fakeFile.length === 0) {
+          $button.after('<span class="file-holder">' + newVal + '</span>');
+        } else {
+          $fakeFile.text('Anexo: '+ newVal);
+        }
       }
     }
-  }
+    
+  });
+  
 
 
   function validate(obj){
