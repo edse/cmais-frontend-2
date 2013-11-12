35d63ed697d3858263bca895f8fd849aca43fdc1
doljak <jedoljak@gmail.com>
2013-11-12 16:30:39 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/personagemSuccess.php
@@ -143,7 +143,7 @@
       <!--lista-->
       <ul role="contentinfo" id="container" class="row-fluid">
         <?php foreach($pager->getResults() as $k=>$d): ?>
-          <?php echo $d->Parent . ">>>?"?>
+          <?php echo $d->getParent() . ">>>?"?>
           <?php
             $assetPersonagens = array();
             $personagensSection = Doctrine::getTable('Section')->findOneBySiteIdAndSlug($site->id, 'personagens');
