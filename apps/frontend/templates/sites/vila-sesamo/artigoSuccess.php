96933a2c1a99157ecf40b250cc9cba0ed9270dc8
doljak <jedoljak@gmail.com>
2013-11-12 18:44:59 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/artigoSuccess.php
@@ -143,10 +143,8 @@
               
               <div id="comente-este-artigo">              
                 <div class="divisa"></div>
-                <div class="center-title">
-                  <i class="icones-form icone-fale-conosco-ve"></i>
-                  <h3>Gostou desse artigo? Comente!</h3>
-                </div>
+                <i class="icones-form icone-fale-conosco-ve"></i>
+                <h3>Gostou desse artigo? Comente!</h3>
               </div>
               
               <fb:comments href="<?php echo $uri ?>" numposts="3" width="580" publish_feed="true"></fb:comments>
