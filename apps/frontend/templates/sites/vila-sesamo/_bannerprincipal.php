f21e38223c56e2abf84d48a5e4c5ec5bdce863c6
doljak <jedoljak@gmail.com>
2013-11-13 17:15:40 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_bannerprincipal.php
+++ apps/frontend/templates/sites/vila-sesamo/_bannerprincipal.php
@@ -28,10 +28,29 @@
         </li>
         <!--/item-->
         
+        <!--item-->
+        <li class="slide01" data-easing="easeInExpo" data-transition="slideRight" style="background: url('/portal/images/capaPrograma/vilasesamo2/banner/1/img-pattern_banner_1-boasvindas.jpg') repeat;" role="presentation" aria-hidden="true">
+          <div class="mpc_ls_slide_background total" data-style="scale">
+            <a href="#" class="total"></a>
+          </div>
+          <div class="mpc_ls_slide_item" data-x="86" data-y="124" data-delay="1500" data-duration="500" data-easing="easeOutBounce" data-effect="slideTop" data-fade="on">
+            <h2>Olá</h2>
+          </div>
+          <div class="mpc_ls_slide_item" data-x="86" data-y="124" data-delay="1500" data-duration="500" data-easing="easeOutBounce" data-effect="slideTop" data-fade="on">
+            <p>Bem-vindo ao nosso novo site!</p>
+          </div>
+          <div class="mpc_ls_slide_item" data-x="350" data-y="120" data-delay="2000" data-duration="500" data-easing="easeOutBack" data-effect="slideRight" data-fade="on">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/_1/img_personagem_banner-topo.png" alt="">
+          </div>
+          <div class="mpc_ls_slide_item" data-x="86" data-y="124" data-delay="1500" data-duration="500" data-easing="easeOutBounce" data-effect="slideTop" data-fade="on">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/_1/img_personagem_banner-topo.png" alt="">
+          </div>
+          <div class="mpc_ls_slide_item" data-x="180" data-y="340" data-delay="2000" data-duration="500" data-easing="easeOutElastic" data-effect="slideLeft" data-fade="on">
+            <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/banner/_1/img_objeto_banner-topo.png" alt="">
+          </div>
+        </li>
+        <!--/item-->
         
-
-        
-
       </ul>
       <!--/lista-->
     </div>
