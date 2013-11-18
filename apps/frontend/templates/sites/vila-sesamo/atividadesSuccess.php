661e1a2eda858cd01ba41569a64bcfee714c7d30
doljak <jedoljak@gmail.com>
2013-11-18 12:07:51 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
@@ -146,7 +146,7 @@
         if (data != "") {
           //console.log(data);
           var $newEls = data;
-          $('#container').isotope().isotope('appended',$newEls).isotope('reloadItems');
+          $('#container').isotope().append($newEls).isotope('appended',$newEls).isotope('reloadItems');
           //$('#container').isotope().append($newEls).isotope('reloadItems').isotope({ sortBy:'original-order'});
           contentPage++;
         }else{
