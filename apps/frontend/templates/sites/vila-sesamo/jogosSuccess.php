26fd0ba89365eb5113d5358771ae37668f84e70f
doljak <jedoljak@gmail.com>
2013-11-18 16:51:29 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/jogosSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/jogosSuccess.php
@@ -115,11 +115,9 @@
   
 
 
-<input type="hidden" id="filter-choice" value="">
-
-<nav id="page_nav">
-  <a href="/testes/vilasesamo2/pages/2.html" class="mais">Carregar mais<i class="icones-sprite-interna icone-carregar-br-grande"></i></a>
-</nav>
+<!--paginacao-->
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/pagination', array('site' => $site, 'section' => $section)) ?>
+<!--/paginacao-->
 
 
 <!--scripts-->
