19b308ce5d112a02b84f6dec0350c977bd18febd
doljak <jedoljak@gmail.com>
2013-11-18 16:49:23 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
@@ -121,45 +121,7 @@
 
 
 <!--paginacao-->
-<nav id="page_nav">
-  <div class="container-ajax-loader">
-    <img id="ajax-loader" src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/sprites/ajax-loader.gif" alt="" style="display:none;">
-  </div>
-  <a href="javascript:vilaSesamoGetContents();" class="mais">Carregar mais<i class="icones-sprite-interna icone-carregar-br-grande"></i></a>
-</nav>
-<script src="http://cmais.com.br/portal/js/isotope/jquery.isotope.min.js"></script>
-<!--script src="http://cmais.com.br/portal/js/isotope/jquery.infinitescroll.min.js"></script-->
-<script src="http://cmais.com.br/portal/js/vilasesamo2/internas-isotope.js"></script>
-<script>
-  videoPage = 1;
-  contentPage = 1;
-  function vilaSesamoGetContents() {
-    $.ajax({
-      url: "<?php echo url_for("@homepage") ?>ajax/vilasesamogetcontents",
-      data: "page="+contentPage+"&items=9&site=<?php echo $site->getSlug(); ?>&siteId=<?php echo (int)$site->id ?>&sectionId=<?php echo $section->getId(); ?>&section=<?php echo $section->getSlug(); ?>",
-      beforeSend: function(){
-          $('#page-nav a.mais').hide();
-          $('#page-nav #ajax-loader').show();
-        },
-      success: function(data){
-        $('#page-nav #ajax-loader').hide();
-        if (data != "") {
-          //console.log(data);
-          //$('#container').isotope().append($newEls).isotope('reloadItems').isotope({ sortBy:'original-order'});
-          var newEls = $(data).appendTo('#container');
-          $("#container").isotope().isotope('appended',newEls);
-          contentPage++;
-        }else{
-          $('#page_nav').html('<span class="mais">fim da listagem.</span>')
-          //console.log("fim da listagem");
-        }
-      }
-    });
-  }
-  $(document).ready(function(){
-   vilaSesamoGetContents();
-  });
-</script>
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/pagination', array('site' => $site, 'section' => $section)) ?>
 <!--/paginacao-->
 
 <!--scripts-->
