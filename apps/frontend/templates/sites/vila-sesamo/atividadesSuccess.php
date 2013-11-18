99360deeba828b51bd9549e5db922698e7441bea
doljak <jedoljak@gmail.com>
2013-11-18 11:12:35 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
@@ -158,5 +158,5 @@
 
 <!--scripts-->
 <script src="http://cmais.com.br/portal/js/isotope/jquery.isotope.min.js"></script>
-<?php /*<script src="http://cmais.com.br/portal/js/isotope/jquery.infinitescroll.min.js"></script> */ ?>
+<script src="http://cmais.com.br/portal/js/isotope/jquery.infinitescroll.min.js"></script>
 <script src="http://cmais.com.br/portal/js/vilasesamo2/internas-isotope.js"></script>
