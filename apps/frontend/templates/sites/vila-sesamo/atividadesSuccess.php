f5f7b04e5c734066d1cc358dc976ece46de4fb98
doljak <jedoljak@gmail.com>
2013-11-18 11:47:36 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
@@ -136,7 +136,7 @@
   function vilaSesamoGetContents() {
     $.ajax({
       url: "<?php echo url_for("@homepage") ?>ajax/vilasesamogetcontents",
-      data: "page="+contentPage+"&items=3&site=<?php echo (int)$site->id ?>&section=<?php echo $section->getId(); ?>",
+      data: "page="+contentPage+"&items=9&site=<?php echo (int)$site->id ?>&section=<?php echo $section->getId(); ?>",
       beforeSend: function(){
           $('#page-nav a.mais').hide();
           $('#page-nav #ajax-loader').show();
@@ -146,7 +146,7 @@
         if (data != "") {
           //console.log(data);
           var $newEls = data;
-          $('#container').append($newEls).isotope('appended',$newEls);
+          $('#container').append($newEls).isotope('appended',$newEls).isotope();
           //$('#container').append(data).isotope('reloadItems').isotope({ sortBy:'original-order'});
           contentPage++;
         }else{
