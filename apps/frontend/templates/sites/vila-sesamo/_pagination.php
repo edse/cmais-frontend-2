2e58809d19452e7bf96ab8469df43fe2e01a5711
doljak <jedoljak@gmail.com>
2013-11-19 19:43:16 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_pagination.php
+++ apps/frontend/templates/sites/vila-sesamo/_pagination.php
@@ -1,4 +1,5 @@
 
+<?php if(intval($pager2) <= intval(count($pager)/9)):?>
 <nav id="page_nav">
   <div class="container-ajax-loader">
     <img id="ajax-loader" src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/sprites/ajax-loader.gif" alt="" style="display:none;">
@@ -12,7 +13,7 @@
   ?>
   <a href="javascript:vilaSesamoGetContents();" class="mais">Carregar mais<i class="icones-sprite-interna <?php echo $icone ?>"></i></a>
 </nav>
-
+<?php endif; ?>
 <script src="http://cmais.com.br/portal/js/isotope/jquery.isotope.min.js"></script>
 <script src="http://cmais.com.br/portal/js/vilasesamo2/internas-isotope.js"></script>
 <script>
