58d4af34202192801a047be8b42aee778fb35e43
doljak <jedoljak@gmail.com>
2013-11-20 15:52:51 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/_menu-personagens.php
+++ apps/frontend/templates/sites/vila-sesamo/_menu-personagens.php
@@ -23,7 +23,11 @@
                   else:
                     $href = "javascript:;";
                   ?> 
-                    <?php echo count($p->getAssets()) ?>   
+                    <?php
+                    foreach($p->getAssets() as $ap):
+                      echo $ap->getSlug();
+                    endforeach;  
+                    ?>   
                     <div class="inner <?php echo $p->getSlug() ?>">
                       <a href="<?php echo $href; ?>" title="<?php echo $p->getTitle() ?>" target="_self" class="btn-<?php echo $p->getSlug() ?> <?php if($section->getSlug() == $p->getSlug()) echo "active"?>" data-filter=".<?php echo $p->getSlug() ?>">
                         <img src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/botoes-carrossel/<?php echo $p->getSlug() ?>_personagem.png" alt="filtro <?php echo $p->getTitle() ?>" />
