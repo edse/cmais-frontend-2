5bb8a96a81daa8970758fc46c5ce402f805cd051
doljak <jedoljak@gmail.com>
2013-11-20 16:25:50 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/atividadesSuccess.php
@@ -73,7 +73,7 @@
           $assetPersonagens[] = $a->getSlug();
         }
       }
-      $contador = $pager->getResults();
+      $contador = $assetPersonagens;
         
     endforeach;   
     ?>
