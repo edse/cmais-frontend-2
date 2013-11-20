2053c6cb920eb5335a73258b3176c87941b26e50
doljak <jedoljak@gmail.com>
2013-11-20 13:56:39 -0200

    u

--- apps/frontend/templates/vilasesamo.php
+++ apps/frontend/templates/vilasesamo.php
@@ -79,7 +79,7 @@
       })();
     </script>
     
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <!--meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"-->
     <script>
     /*
       window.onload = function() { 
