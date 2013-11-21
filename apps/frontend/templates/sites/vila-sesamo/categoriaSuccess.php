01a36e340fce480f1bd55c5594290b57aa4d6449
doljak <jedoljak@gmail.com>
2013-11-21 18:06:25 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/categoriaSuccess.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
 
 <script>
-  $("body").addClass("interna campanhas");
+  $("body").addClass("interna campanhas categorias");
   $('.ellipsis').dotdotdot();
 </script>
 
