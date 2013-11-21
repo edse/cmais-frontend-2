898e6372bec745e900a48a8da92eb2b26df1bc3d
Joseval <Joseval@fpawks0146-2.local>
2013-11-21 13:02:12 -0200

    Busca

--- apps/frontend/templates/sites/vila-sesamo/buscaSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/buscaSuccess.php
@@ -26,18 +26,18 @@
       
       <!--container-->
       <div class="container-busca">
-          <script type="text/javascript">
-            (function() {
-              var cx = '005232987476052626260:gkqgz9ihn4w';
-              var gcse = document.createElement('script');
-              gcse.type = 'text/javascript';
-              gcse.async = true;
-              gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-                  '//www.google.com/cse/cse.js?cx=' + cx;
-              var s = document.getElementsByTagName('script')[0];
-              s.parentNode.insertBefore(gcse, s);
-            })();
-          </script>
+					<script>
+					  (function() {
+					    var cx = '005232987476052626260:fifvz-rfcl0';
+					    var gcse = document.createElement('script');
+					    gcse.type = 'text/javascript';
+					    gcse.async = true;
+					    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+					        '//www.google.com/cse/cse.js?cx=' + cx;
+					    var s = document.getElementsByTagName('script')[0];
+					    s.parentNode.insertBefore(gcse, s);
+					  })();
+					</script>
           <gcse:searchresults-only>Buscando...</gcse:searchresults-only>
       </div>
       <!--container-->
