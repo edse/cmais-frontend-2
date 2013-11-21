efa6e834ac14991db9c1b80c109dea7c37d6fdaf
doljak <jedoljak@gmail.com>
2013-11-21 10:42:26 -0200

    u

--- web/portal/js/vilasesamo2/internas-isotope.js
+++ web/portal/js/vilasesamo2/internas-isotope.js
@@ -6,7 +6,7 @@
       itemSelector : '.element',
       layoutMode:'fitRows'
     }); 
-    $('.count').each(function(){
+    $($('.filtro-personagem li).each(function(){
       console.log("oi")
     });
     var filter_selected;
