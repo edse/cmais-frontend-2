ca34bb1538e06f6928a84bab7920e56c7af15617
doljak <jedoljak@gmail.com>
2013-11-22 15:43:13 -0200

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -1066,6 +1066,41 @@ class _assetActions extends sfActions
 				}
       }
       else{
+        if ($this->site->getSlug() == "vila-sesamo") {
+            //if($this->asset->id != 148169) // id do asset somente para teste, retirar assim que puderem!
+              $this->setLayout("vilasesamo");
+            $sections = $this->asset->getSections();
+            foreach($sections as $s) {
+              if(in_array($s->getSlug(), array("atividades", "jogos", "videos","cuidadores"))) {
+                $this->section = $s;
+                break;
+              }
+            }
+            if($this->section->getSlug() == "atividades") {
+              if($debug) print "<br>2-3-1>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/atividade';
+              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/atividade');
+            }
+            elseif($this->section->getSlug() == "jogos") {
+              if($debug) print "<br>2-3-2>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/jogo';
+              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/jogo');
+            }
+            elseif($this->section->getSlug() == "videos") {
+              if($debug) print "<br>2-3-3>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/video';
+              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/video');
+            }
+            elseif($this->section->getSlug() == "cuidadores") {
+              if($debug) print "<br>2-3-4>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/artigo';
+              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/artigo');
+            }
+            else {
+              if($debug) print "<br>2-3-5>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug();
+              $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug());
+            }
+          }
+          else {
+            if($debug) print "<br>2-4>>".sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug();
+            $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug());
+          }
         if($this->site->getType() == "Hotsite" || $this->site->getType() == 1){
           
           if(in_array($this->site->getSlug(), array("revistavitrine","revistavitrine2"))) {
