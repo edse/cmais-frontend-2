641c522e1303fbe32f7801f55a8fa4d344e49a68
doljak <jedoljak@gmail.com>
2013-11-22 10:42:38 -0200

    u

---
+++ apps/frontend/templates/sites/vila-sesamo/acessibilidade.php
@@ -0,0 +1,48 @@
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lt IE 8]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
+
+<script>
+  $("body").addClass("na-tv");
+</script>
+
+<!-- HEADER -->
+<?php include_partial_from_folder('sites/vila-sesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+<!-- /HEADER -->
+
+<!--content-->
+<div id="content">
+  
+  <!--section -->
+  <section class="filtro row-fluid">
+    
+    <!--container conteudo-->
+    <div class="b-verde borda-arredonda">
+      <h1>
+        <span class="icones-sprite-interna icone-na-tv-grande"></span>
+        <?php echo $section->getTitle() ?>
+      </h1>
+      
+      <!--container-->
+      <div class="container-na-tv">
+        <h2 class="ola"><?php echo $section->getDescription() ?></h2>
+        
+        
+            
+                  
+         
+      </div>
+      <!--/container-->
+      <span class="fundo-na-tv"></span> 
+    </div>
+    <!--/container conteudo-->
+    
+    
+    
+       
+
+  </section>
+  <!--/section -->
+</div>
+<!--div-->      
