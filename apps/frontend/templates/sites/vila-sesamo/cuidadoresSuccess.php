3f56c14f225b924a8645c0898fe965207f18572b
doljak <jedoljak@gmail.com>
2013-11-22 13:30:45 -0200

    u

--- apps/frontend/templates/sites/vila-sesamo/cuidadoresSuccess.php
+++ apps/frontend/templates/sites/vila-sesamo/cuidadoresSuccess.php
@@ -44,7 +44,7 @@
                 <?php foreach($displays['destaque-principal'] as $d): ?>
                 <!--item-->
                 <li>
-                  <a href="<?php echo $site->getSlug() ?>/<?php echo $section->getSlug() ?>/<?php echo $d->Asset->getSlug() ?>">
+                  <a href="<?php echo $site->getSlug() ?>/<?php echo $section->getSlug() ?>/<?php echo $d->Asset->getSlug() ?>" title="<?php echo $d->getTitle() ?>" target="_self">
                     <div class="pull-left videoorimage">
                       <div class="imagem-destaque-carrossel">
                         <img src="<?php echo $d->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $d->getTitle() ?>" />
