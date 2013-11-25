4c5fee55698f571562254be5d6b16a5bda044a86
doljak <jedoljak@gmail.com>
2013-11-25 10:39:23 -0200

    u

--- apps/frontend/templates/vilasesamo.php
+++ apps/frontend/templates/vilasesamo.php
@@ -104,6 +104,8 @@
     googletag.enableServices();
     });
     </script>
+    <!--/banner vila sesamo 2013-->
+    
     <!--meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"-->
     <script>
     /*
