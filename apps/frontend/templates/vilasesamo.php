41dc5985ff0082efaf0d5b22e616efac5c9c7858
doljak <jedoljak@gmail.com>
2013-11-28 16:46:47 -0200

    u

--- apps/frontend/templates/vilasesamo.php
+++ apps/frontend/templates/vilasesamo.php
@@ -27,7 +27,7 @@
      <!--[if lte IE 8]>
        <link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/cocorico/ie.css" />
      <![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <meta name="viewport" content="initial-scale=0.5">
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="Cache-Control" content="no-cache, no-store" />
     <meta http-equiv="Pragma" content="no-cache, no-store" />
