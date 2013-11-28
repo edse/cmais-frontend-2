dd033ecb8e3d6537b52be3e1cd1c26bf890e8c8e
Joseval <Joseval@fpawks0146-2.local>
2013-11-28 10:44:16 -0200

    Widget

--- web/widgets/transito/index.php
+++ web/widgets/transito/index.php
@@ -1,8 +1,8 @@
-<html>
-<head>
 <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+
 <style>
   ul{margin:0;padding:0;}
+  #legendas-ruas{width:640px;height:100%;background: url(http://cmais.com.br/portal/images/capaPrograma/transito/mapa-sao-paulo-legendas.png) no-repeat -99px -68px;}
   #mapa-sp-legenda{display:block;width: 194px;height:314px;position:absolute;left:0;top:0;background: #dfdfdf;}
   #mapa-sp-legenda li{list-style: none;height: 42px;padding-top: 3px;width: 150px; border-radius: 2px;text-align:left;margin-left: 12px;padding-left: 15px;margin-bottom: 5px}
   #mapa-sp-legenda .norte {background-color: #fc724b !important; top:114px;}
@@ -28,27 +28,26 @@
   #mapa-sp-legenda h4 {font-family:Arial, Helvetica, sans-serif;margin-top: 0px;color: #fff;font-size: 14px;text-transform: uppercase;padding-top: 3px;}
   #mapa-sp-legenda p {font-family:Arial, Helvetica, sans-serif;color: #FFF !important;font-size: 12px;text-transform: capitalize;font-weight: normal;}
 </style>
-</head>
-<body>
-	<?php
-	  $xmlTransitoAgoraCET = file_get_contents('http://cetsp1.cetsp.com.br/monitransmapa/xmltransitoagora.asp');
-	  try {
-	    $transitoagora = new SimpleXMLElement($xmlTransitoAgoraCET);
-	  } catch (Exception $e) {}
-	?>
-	    
-	<div id="mapa-sp-legenda"> 
-		<ul>
-			<?php foreach($transitoagora->regiao as $k=>$r): ?>
-			<li class="<?php echo strtolower($r->Attributes()->ID); ?>">
-				<h4><?php echo $r->Attributes()->ID; ?></h4>
-				<p>Lentidão: <?php echo $r->lentidao; ?> Km (<?php echo $r->percent; ?>%)</p>
-				<span class="<?php echo strtolower($r->tendencia) ?> setatamanho"></span>  
-				<!--span class="seta pos-<?php echo strtolower($r->Attributes()->ID); ?>"></span-->  
-			</li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-	<a class="pos02" href="http://cmais.com.br/transito" name="modal" title="Lentidão dos Corredores">Lentidão dos Corredores</a>
-</body>
-</html>
+
+
+<?php
+  $xmlTransitoAgoraCET = file_get_contents('http://cetsp1.cetsp.com.br/monitransmapa/xmltransitoagora.asp');
+  try {
+    $transitoagora = new SimpleXMLElement($xmlTransitoAgoraCET);
+  } catch (Exception $e) {}
+?>
+    
+ <div id="mapa-sp-legenda"> 
+  <ul>
+    <?php foreach($transitoagora->regiao as $k=>$r): ?>
+    <li class="<?php echo strtolower($r->Attributes()->ID); ?>">
+      <h4><?php echo $r->Attributes()->ID; ?></h4>
+      <p>Lentidão: <?php echo $r->lentidao; ?> Km (<?php echo $r->percent; ?>%)</p>
+      
+      <span class="<?php echo strtolower($r->tendencia) ?> setatamanho"></span>  
+      <!--span class="seta pos-<?php echo strtolower($r->Attributes()->ID); ?>"></span-->  
+    </li>
+    <?php endforeach; ?>
+  </ul>
+<a class="pos02" href="http://cmais.com.br/transito" name="modal" title="Lentidão dos Corredores">Lentidão dos Corredores</a>                              
+
