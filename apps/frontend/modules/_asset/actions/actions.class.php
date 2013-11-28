87221e8db705c66819e0396a7eaa56216b7255e6
doljak <jedoljak@gmail.com>
2013-11-28 12:57:39 -0200

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -1062,7 +1062,7 @@ class _assetActions extends sfActions
 			        $this->setTemplate(sfConfig::get('sf_app_template_dir').DIRECTORY_SEPARATOR.'sites/'.$this->site->getSlug().'/'.$this->asset->AssetType->getSlug());
 			      }
 			    }
-          if ($this->site->getSlug() == "vila-sesamo") {
+          if ($this->site->getSlug() == "vilasesamo") {
             //if($this->asset->id != 148169) // id do asset somente para teste, retirar assim que puderem!
               $this->setLayout("vilasesamo");
             $sections = $this->asset->getSections();
