8612b474f7b235b817db5284717f9f1e1d6ad467
doljak <jedoljak@gmail.com>
2013-11-29 16:24:58 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/_banner300x250.php
+++ apps/frontend/templates/sites/vilasesamo/_banner300x250.php
@@ -1,5 +1,5 @@
 <!-- vilasesamo -->
-<div id='div-gpt-ad-1385748525978-0' style='width:300px; height:250px;'>
+<div id='div-gpt-ad-1385748525978-0' class="google-banner" style='width: 287px;height: 250px;margin-bottom: 20px;'>
   <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1385748525978-0'); });
   </script>
