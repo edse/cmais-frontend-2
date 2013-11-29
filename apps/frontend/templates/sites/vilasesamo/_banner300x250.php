8d3300fa7ccf2aee0a177d3bfe87884d45fedaa8
doljak <jedoljak@gmail.com>
2013-11-29 17:23:24 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/_banner300x250.php
+++ apps/frontend/templates/sites/vilasesamo/_banner300x250.php
@@ -6,7 +6,7 @@ if(isset($asset)){
   $class = "google-banner-home";
 }
 ?>
-<div id='div-gpt-ad-1385748525978-0' class="<?php if(isset($asset)){echo "google-banner";}else{echo "google-banner-home";}?>">
+<div id='div-gpt-ad-1385748525978-0' class="<?php echo $class ?>">
   <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1385748525978-0'); });
   </script>
