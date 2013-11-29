d1316eb33d03398aa50915948c9570bf357558b8
doljak <jedoljak@gmail.com>
2013-11-29 12:15:50 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/_para-os-pais.php
+++ apps/frontend/templates/sites/vilasesamo/_para-os-pais.php
@@ -305,7 +305,7 @@ else { // senão traz pela semelhança de tags com o asset em questão
         ?>
         <?php if(isset($displays['parceiros'])): ?>
           <?php if(count($displays['parceiros']) > 0): ?>
-          <p>Conheça nossos parceiros:</p>
+          <p style="margin: 20px 0 0 0;">Conheça nossos parceiros:</p>
           <a class="publicidade" href="<?php echo $displays['parceiros'][0]->retriveUrl() ?>" title="<?php echo $displays['parceiros'][0]->getTitle() ?>">
             <img src="<?php echo $displays['parceiros'][0]->retriveImageUrlByImageUsage("image-13-b") ?>" alt="<?php echo $displays['parceiros'][0]->getTitle() ?>" />
           </a>
