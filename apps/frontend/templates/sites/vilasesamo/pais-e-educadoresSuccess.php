7c214b99ac9f551544c613e70d53372a6323ce01
doljak <jedoljak@gmail.com>
2013-11-29 17:10:04 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/pais-e-educadoresSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/pais-e-educadoresSuccess.php
@@ -1,5 +1,4 @@
-<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<!--[if lt IE 8]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
 
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
 
