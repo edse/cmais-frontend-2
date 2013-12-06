9dab5c201e0cc7f7cdef269786f168605a8670bf
doljak <jedoljak@gmail.com>
2013-12-06 19:01:49 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/videoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/videoSuccess.php
@@ -105,7 +105,7 @@
   </section>
   <!--/section-->
   
-  <?php include_partial_from_folder('sites/vilasesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
+  <?php include_partial_from_folder('sites/vilasesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories, 'assetSection' => $assetSection)) ?>
   
   <?php //include_partial_from_folder('sites/vilasesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
 
