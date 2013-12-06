3a1acb32fbc5fa6533b14de101d20557b6b397d7
doljak <jedoljak@gmail.com>
2013-12-06 16:31:55 -0200

    u

--- apps/frontend/templates/sites/cmais/novo-jornalismoSuccess.php
+++ apps/frontend/templates/sites/cmais/novo-jornalismoSuccess.php
@@ -280,7 +280,7 @@ $btn_live = '<span class="live"><i class="ico-setas ico-seta-cima"></i>AO VIVO</
 	       <!--previsão do tempo-->
 	       <div style="margin-bottom: 10px;">
 	       	<h2>Previsão do Tempo</h2>
-	       	  <iframe src="http://www.tempoagora.com.br/selos_iframe/sky_SaoPaulo-SP.html" height="200" width="150" frameborder="0" allowtransparency="yes" scrolling="no"></iframe>
+	       	  <iframe src="http://www.tempoagora.com.br/selos_iframe/sky_SaoPaulo-SP.html" height="200" width="200" frameborder="0" allowtransparency="yes" scrolling="no"></iframe>
 	       </div>
 	       <!--previsão do tempo-->
       </div>
