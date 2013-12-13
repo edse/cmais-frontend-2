f020491c87fe6cdb59ff03905afac1e66d970276
doljak <jedoljak@gmail.com>
2013-12-13 18:46:31 -0200

    u

--- apps/frontend/templates/cocorico.php
+++ apps/frontend/templates/cocorico.php
@@ -82,65 +82,7 @@
   
   </head>
   <body>
-    <!-- Navbar
-    ================================================== -->
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container" style="width: 414px;">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="http://cmais.com.br/">Cmais</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="tvcultura">
-                <a href="http://tvcultura.com.br" title="TV Cultura" target="_blank">TV Cultura</a>
-              </li>
-              <li class="tvratimbum">
-                <a href="http://tvratimbum.com.br" title="TV Rá Tim Bum" target="_blank">TV Rá Tim Bum</a>
-              </li>
-              <li class="multicultura">
-                <a href="http://multicultura.com.br" title="Multicultura" target="_blank">Multicultura</a>
-              </li>
-              <li class="univesp">
-                <a href="http://univesp.tv.br" title="Univesp TV" target="_blank">Univesp TV</a>
-              </li>
-              <li class="active culturabrasil">
-                <a href="http://culturabrasil.com.br" title="Cultura Brasil" target="_blank">Cultura Brasil</a>
-              </li>
-              <li class="culturafm">
-                <a href="http://www.culturafm.com.br" title="Cultura FM" target="_blank">Cultura FM</a>
-              </li>
-              <!--
-              <li class="itunes">
-                <a href="http://itunes.apple.com/br/app/radio-cultura/id370066053" title="Itunes" target="_blank">Itunes</a>
-              </li>
-              <li class="facebook">
-                <a href="http://facebook.com/tvcultura" title="Facebook" target="_blank">Facebook</a>
-              </li>
-              <li class="google">
-                <a href="https://google.com/+tvcultura" title="Google+" target="_blank">Google+</a>
-              </li>
-              <li class="instagram">
-                <a href="http://instagram.com/tvcultura" title="Instagram" target="_blank">Instagram</a>
-              </li>
-              <li class="twitter">
-                <a href="http://twitter.com/tvcultura" title="Twitter" target="_blank">Twitter</a>
-              </li>
-              <li class="youtube">
-                <a href="http://youtube.com/tvcultura" title="Youtube" target="_blank">Youtube</a>
-              </li>
-              <li class="feed">
-                <a href="http://tvcultura.cmais.com.br/feed" title="Feed" target="_blank">Feed</a>
-              </li>
-              -->
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php include_partial_from_folder('blocks', 'global/menu_reduzido_abrace') ?>
     
     <?php echo $sf_content ?>
     
