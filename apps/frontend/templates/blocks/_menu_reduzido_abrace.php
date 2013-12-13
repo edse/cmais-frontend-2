b66b8437a387df073447b7532c5b4fea6d23e73b
doljak <jedoljak@gmail.com>
2013-12-13 18:57:08 -0200

    u

--- apps/frontend/templates/blocks/_menu_reduzido_abrace.php
+++ apps/frontend/templates/blocks/_menu_reduzido_abrace.php
@@ -1,36 +1,37 @@
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/topo_minimizado.css" type="text/css" />
 <!-- Navbar -->
-    <div class="navbar navbar-inverse">
-      <div class="navbar-inner">
-        <div class="container" style="width: 414px;">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="http://cmais.com.br/">Cmais</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="tvcultura">
-                <a href="http://tvcultura.com.br" title="TV Cultura" target="_blank">TV Cultura</a>
-              </li>
-              <li class="tvratimbum">
-                <a href="http://tvratimbum.com.br" title="TV Rá Tim Bum" target="_blank">TV Rá Tim Bum</a>
-              </li>
-              <li class="multicultura">
-                <a href="http://multicultura.com.br" title="Multicultura" target="_blank">Multicultura</a>
-              </li>
-              <li class="univesp">
-                <a href="http://univesp.tv.br" title="Univesp TV" target="_blank">Univesp TV</a>
-              </li>
-              <li class="active culturabrasil">
-                <a href="http://culturabrasil.com.br" title="Cultura Brasil" target="_blank">Cultura Brasil</a>
-              </li>
-              <li class="culturafm">
-                <a href="http://www.culturafm.com.br" title="Cultura FM" target="_blank">Cultura FM</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+<div class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container" style="width: 414px;">
+      <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand" href="http://cmais.com.br/">Cmais</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li class="tvcultura">
+            <a href="http://tvcultura.com.br" title="TV Cultura" target="_blank">TV Cultura</a>
+          </li>
+          <li class="tvratimbum">
+            <a href="http://tvratimbum.com.br" title="TV Rá Tim Bum" target="_blank">TV Rá Tim Bum</a>
+          </li>
+          <li class="multicultura">
+            <a href="http://multicultura.com.br" title="Multicultura" target="_blank">Multicultura</a>
+          </li>
+          <li class="univesp">
+            <a href="http://univesp.tv.br" title="Univesp TV" target="_blank">Univesp TV</a>
+          </li>
+          <li class="active culturabrasil">
+            <a href="http://culturabrasil.com.br" title="Cultura Brasil" target="_blank">Cultura Brasil</a>
+          </li>
+          <li class="culturafm">
+            <a href="http://www.culturafm.com.br" title="Cultura FM" target="_blank">Cultura FM</a>
+          </li>
+        </ul>
       </div>
-    </div> 
-    <!--topo cmais-->
+    </div>
+  </div>
+</div> 
+<!--topo cmais-->
