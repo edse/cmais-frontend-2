6639b48464539b226191c1600af63d04fe91a293
doljak <jedoljak@gmail.com>
2013-12-16 15:29:39 -0200

    u

--- apps/frontend/templates/sites/culturabrasil/indexSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/indexSuccess.php
@@ -11,6 +11,8 @@
 <![endif]-->
 <script src="http://cmais.com.br/portal/js/bootstrap/bootstrap.js"></script>
 
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'asset' => $asset, 'section' => $section)) ?>
+
 <?php include_partial_from_folder('sites/culturabrasil', 'global/menu', array('siteSections' => $siteSections, 'displays' => $displays, 'section'=>$section, 'site'=>$site)) ?>
 
  
