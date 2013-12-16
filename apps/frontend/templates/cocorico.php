b76ed996d01a7fb559cbe416997461b782e886a9
doljak <jedoljak@gmail.com>
2013-12-16 14:50:47 -0200

    i

--- apps/frontend/templates/cocorico.php
+++ apps/frontend/templates/cocorico.php
@@ -83,7 +83,7 @@
   </head>
   <body>
     <?php //include_partial_from_folder('blocks', 'global/menu_reduzido_abrace') ?>
-    <?php include_partial_from_folder('blocks', 'global/menu', array('section'=>$section,'channels' => $channels, 'live' => $live, 'editorials' => $editorials, 'site' => $site, 'mainSite' => $mainSite, 'coming' => $coming, 'important' => $important)) ?>
+    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
     
     <?php echo $sf_content ?>
     
--- apps/frontend/templates/culturabrasil.php
+++ apps/frontend/templates/culturabrasil.php
@@ -128,7 +128,7 @@
 
   </head>
   <body>
-    <?php include_partial_from_folder('blocks', 'global/menu', array('section'=>$section,'channels' => $channels, 'live' => $live, 'editorials' => $editorials, 'site' => $site, 'mainSite' => $mainSite, 'coming' => $coming, 'important' => $important)) ?>
+    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
     <?php //include_partial_from_folder('blocks', 'global/menu_reduzido_abrace')?>
     <?php echo $sf_content ?>
     <?php include_partial_from_folder('blocks', 'global/footer') ?>
