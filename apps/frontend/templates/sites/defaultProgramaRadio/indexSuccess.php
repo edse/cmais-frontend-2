beaf2a28afabde1286638df15ed0a5c8024bc94d
doljak <jedoljak@gmail.com>
2013-12-23 09:29:24 -0200

    u

--- apps/frontend/templates/sites/defaultProgramaRadio/indexSuccess.php
+++ apps/frontend/templates/sites/defaultProgramaRadio/indexSuccess.php
@@ -16,7 +16,8 @@
     <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/culturafm.css" type="text/css" />
 
     <?php use_helper('I18N', 'Date') ?>
-    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section, 'asset' => $asset)) ?>
+    <?php // include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section, 'asset' => $asset)) ?>
+    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
    <div id="bg-site"></div>
     <!-- CAPA SITE -->
