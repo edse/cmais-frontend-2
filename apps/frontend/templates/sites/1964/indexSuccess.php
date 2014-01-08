837b9c6eea0bda43b0ee79e261fe1f2ff1afd207
Emerson Estrella <emersonestrella@motu.fpanet.inet>
2014-01-08 16:46:57 -0200

    1964

--- apps/frontend/templates/sites/1964/indexSuccess.php
+++ apps/frontend/templates/sites/1964/indexSuccess.php
@@ -57,7 +57,7 @@
 			              var timeline_config = {
 			               width: "100%",
 			               height: "100%",
-			               source: '/actions/1964/json.php',
+			               source: '/actions/1964/json.jsonp',
 			               start_at_slide: 0,
 			               start_zoom_adjust: 2,
 			               embed_id: "tvcultura-embed",
