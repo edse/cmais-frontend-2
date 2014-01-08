d4f4421ace5e30c77c7f9ec49ea7e0d4144038af
Emerson Estrella <emersonestrella@motu.fpanet.inet>
2014-01-08 16:32:53 -0200

    1964

--- apps/frontend/templates/sites/1964/linha-do-tempoSuccess.php
+++ apps/frontend/templates/sites/1964/linha-do-tempoSuccess.php
@@ -2,6 +2,210 @@ storyjs_jsonp_data = {
     "timeline":
     {
         "date": [
+            {
+                "startDate":"2013,1,12,7,0",
+                "endDate":"2013,1,12,8,0",
+                "headline":"Mosaicos",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/f038729dc5bab0c07138e386a19ae7e2353f7c50.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/f038729dc5bab0c07138e386a19ae7e2353f7c50.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Mosaicos"
+                }
+            },
+            {
+                "startDate":"2013,1,12,8,0",
+                "endDate":"2013,1,12,9,0",
+                "headline":"Missa de Aparecida",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/0e3ab178db7352d30b22b4f8d7c27bdd738fbb23.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/0e3ab178db7352d30b22b4f8d7c27bdd738fbb23.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Missa de Aparecida"
+                }
+            },
+            {
+                "startDate":"2013,1,12,9,0",
+                "endDate":"2013,1,12,10,15",
+                "headline":"Viola, Minha Viola",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/4876f375d7a91651bbfe1a3818d31ba189dd4243.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/4876f375d7a91651bbfe1a3818d31ba189dd4243.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Viola, Minha Viola"
+                }
+            },
+            {
+                "startDate":"2013,1,12,10,15",
+                "endDate":"2013,1,12,11,15",
+                "headline":"Sr. Brasil",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/f42630dca8f13b9b5331be1655d2df7c0c64250d.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/f42630dca8f13b9b5331be1655d2df7c0c64250d.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Sr. Brasil"
+                }
+            },
+            {
+                "startDate":"2013,1,12,11,15",
+                "endDate":"2013,1,12,11,45",
+                "headline":"O Milagre de Santa Luzia",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/214c0848f0979ed5f30c1b77647914dc451fe3ea.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/214c0848f0979ed5f30c1b77647914dc451fe3ea.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"O Milagre de Santa Luzia"
+                }
+            },
+            {
+                "startDate":"2013,1,12,11,45",
+                "endDate":"2013,1,12,13,30",
+                "headline":"Mestres do Riso",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/ca7d49c99aec0c88b32036fe1457448c99b7bb5c.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/ca7d49c99aec0c88b32036fe1457448c99b7bb5c.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Mestres do Riso"
+                }
+            },
+            {
+                "startDate":"2013,1,12,13,30",
+                "endDate":"2013,1,12,14,0",
+                "headline":"Eu e Os Monstros",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/c8f56f458cad1519768aedb7476a5b2f809bea41.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/c8f56f458cad1519768aedb7476a5b2f809bea41.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Eu e Os Monstros"
+                }
+            },
+            {
+                "startDate":"2013,1,12,14,0",
+                "endDate":"2013,1,12,15,0",
+                "headline":"As Aventuras de Tintim",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/78edcd81118874f1e6c0d58261733b07f011f25d.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/78edcd81118874f1e6c0d58261733b07f011f25d.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"As Aventuras de Tintim"
+                }
+            },
+            {
+                "startDate":"2013,1,13,8,0",
+                "endDate":"2013,1,13,9,0",
+                "headline":"Missa de Aparecida2",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/0e3ab178db7352d30b22b4f8d7c27bdd738fbb23.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/0e3ab178db7352d30b22b4f8d7c27bdd738fbb23.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Missa de Aparecida"
+                }
+            },
+            {
+                "startDate":"2013,1,13,9,0",
+                "endDate":"2013,1,13,10,15",
+                "headline":"Viola, Minha Viola",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/4876f375d7a91651bbfe1a3818d31ba189dd4243.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/4876f375d7a91651bbfe1a3818d31ba189dd4243.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Viola, Minha Viola"
+                }
+            },
+            {
+                "startDate":"2013,1,13,10,15",
+                "endDate":"2013,1,13,11,15",
+                "headline":"Sr. Brasil",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/f42630dca8f13b9b5331be1655d2df7c0c64250d.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/f42630dca8f13b9b5331be1655d2df7c0c64250d.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Sr. Brasil"
+                }
+            },
+            {
+                "startDate":"2013,1,13,11,15",
+                "endDate":"2013,1,13,11,45",
+                "headline":"O Milagre de Santa Luzia",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/displays/214c0848f0979ed5f30c1b77647914dc451fe3ea.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/displays/214c0848f0979ed5f30c1b77647914dc451fe3ea.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"O Milagre de Santa Luzia"
+                }
+            },
+            {
+                "startDate":"2013,1,13,11,45",
+                "endDate":"2013,1,13,13,30",
+                "headline":"Mestres do Riso",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/ca7d49c99aec0c88b32036fe1457448c99b7bb5c.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/ca7d49c99aec0c88b32036fe1457448c99b7bb5c.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Mestres do Riso"
+                }
+            },
+            {
+                "startDate":"2013,1,13,13,30",
+                "endDate":"2013,1,13,14,0",
+                "headline":"Eu e Os Monstros",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/c8f56f458cad1519768aedb7476a5b2f809bea41.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/c8f56f458cad1519768aedb7476a5b2f809bea41.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"Eu e Os Monstros"
+                }
+            },
+            {
+                "startDate":"2013,1,13,14,0",
+                "endDate":"2013,1,13,15,0",
+                "headline":"As Aventuras de Tintim",
+                "text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p><button class='btn btn-large btn-primary' type='button' onclick='self.location.href=\"interatividade.html\"'>Interatividade  &raquo;</button>",
+                "asset":
+                {
+                    "media":"http://midia.cmais.com.br/programs/78edcd81118874f1e6c0d58261733b07f011f25d.jpg",
+                    "thumbnail":"http://midia.cmais.com.br/programs/78edcd81118874f1e6c0d58261733b07f011f25d.jpg",
+                    "credit":"cmais+ o portal de conteÃºdo da Cultura",
+                    "caption":"As Aventuras de Tintim"
+                }
+            }            
+        ]
+    }
+}
+<?php /*
+
+storyjs_jsonp_data = {
+    "timeline":
+    {
+        "date": [
           <?php foreach($pager->getResults() as $d): ?>
             <?php
             $date1 = implode(",", explode("/",$d->AssetContent->getSource()));
@@ -23,3 +227,5 @@ storyjs_jsonp_data = {
         ]
     }
 }
+ */
+?>
