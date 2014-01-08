db2b90c26db85d2120b1f81a82c5e4457c6e1767
georgiacatarina <georgia.catarina@gmail.com>
2014-01-08 11:42:18 -0200

    Site 1964

---
+++ apps/frontend/templates/sites/1964/image-gallerySuccess.php
@@ -0,0 +1,229 @@
+<link type="text/css" href="http://cmais.com.br/portal/js/orbit/orbit-1.2.3.css" rel="stylesheet" />
+<script type="text/javascript" src="http://cmais.com.br/portal/js/orbit/jquery.orbit-1.2.3.min.js"></script>
+
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/<?php echo $site->getSlug() ?>.css" type="text/css" />
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/programaVideosInterna.css" type="text/css" />
+<script type="text/javascript">
+//carrossel
+$(function(){
+  $('.carrossel').jcarousel({
+      wrap: "both"
+  });
+
+  //hover states on the static widgets
+  $('#dialog_link, ul#icons li').hover(
+    function() { $(this).addClass('ui-state-hover'); }, 
+    function() { $(this).removeClass('ui-state-hover'); }
+  );
+  
+});
+
+$(window).load(function() {
+  $('#featured').orbit({
+    'bullets' : true,   
+    'bulletThumbs': true
+  });
+});
+</script>
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+
+    <!-- CAPA SITE -->
+    <div id="capa-site">
+      
+      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
+
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+        
+        <div class="topo-programa">
+          <?php if(isset($program) && $program->id > 0): ?>
+          <h2>
+            <a href="<?php echo $program->retriveUrl() ?>">
+              <img src="http://midia.cmais.com.br/programs/<?php echo $program->getImageThumb() ?>" alt="<?php echo $program->getTitle() ?>" title="<?php echo $program->getTitle() ?>" />
+            </a>
+          </h2>
+          <?php endif; ?>
+          
+          <?php if(isset($program) && $program->id > 0): ?>
+          <?php include_partial_from_folder('blocks','global/like', array('site' => $site, 'uri' => $uri, 'program' => $program)) ?>
+          <?php endif; ?>
+          
+          <?php if(isset($program) && $program->id > 0): ?>
+            <!-- horario -->
+            <div id="horario">
+              <p><?php echo html_entity_decode($program->getSchedule()) ?></p>
+            </div>
+            <!-- /horario -->
+          <?php endif; ?>
+        </div>
+
+        <?php if(isset($siteSections)): ?>
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+
+          <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
+
+          <?php if(isset($section->slug)): ?>
+            <?php if(!in_array(strtolower($section->getSlug()), array('home','homepage','home-page','index'))): ?>
+            <div class="navegacao txt-10">
+              <a href="<?php echo $site->retriveUrl() ?>" title="Home">Home</a>
+              <span>&gt;</span>
+              <a href="<?php echo $site->retriveUrl() ?>/fotos" title="Fotos">Fotos</a>
+              <span>&gt;</span>
+              <a href="<?php echo $asset->retriveUrl()?>" title="<?php echo $asset->getTitle()?>"><?php echo $asset->getTitle()?></a>
+            </div>
+            <?php endif; ?>
+          <?php endif; ?>
+
+          
+
+        </div>
+        <!-- /box-topo -->
+        <?php endif; ?>
+        
+      </div>
+      <!-- /BARRA SITE -->
+
+      <!-- MIOLO -->
+      <div id="miolo">
+        
+        <!-- BOX LATERAL -->
+        <?php include_partial_from_folder('blocks','global/shortcuts') ?>
+        <!-- BOX LATERAL -->
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+          
+          <!-- CAPA -->
+          <div class="capa grid3">
+            <h3 class="tit-pagina grid3"><?php echo $asset->getTitle()?></h3>
+            <div id="esquerda" class="grid2">
+              
+              <div class="box-interna grid2" style="margin-bottom:0px;">
+                
+                <a href="#" class="txt-16"><?php echo $asset->getDescription() ?></a>
+                <p><?php echo $asset->AssetImageGallery->getText()?></p>
+                <!-- 
+                <div class="assinatura grid2">
+                  <p class="sup">Cristina Hallogallo <span>Nome da categoria</span></p>
+                  <p class="inf">10/11/2010 09h30 - Atualizado em 10/11/2010 11h29</p>
+                  <div class="acessibilidade">
+                    <a href="#" class="zoom">+A</a>
+                    <a href="#" class="zoom">-A</a>
+                  </div>
+                </div>
+                -->
+              </div>
+
+              <!-- GALERIA DE FOTOS -->
+              <div class="container galeriaNew" style="float: left; margin-bottom: 10px; width: 640px;">
+                <div id="featured">
+                <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
+                <?php if(count($related)>0): ?>
+                  <?php foreach($related as $d): ?>
+                  <img src="<?php echo $d->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $d->getTitle() ?>" data-thumb="<?php echo $d->retriveImageUrlByImageUsage('image-1') ?>" data-caption="#html<?php echo $d->getSlug() ?>" />
+                  <?php endforeach; ?>
+                <?php endif; ?>
+                </div>
+
+                <!-- THUMBNAILS -->
+              <?php if(count($related)>0): ?>
+                <?php foreach($related as $d): ?>
+                <?php $related_content = $d->retriveRelatedAssetsByAssetTypeId(1); ?>
+                <span class="orbit-caption" id="html<?php echo $d->getSlug() ?>">
+                  <span class="espaco">
+                    <?php echo $d->getDescription() ?><?php if($d->AssetImage->getHeadline()!="") echo "<br>".$d->AssetImage->getHeadline() ?><?php if($d->AssetImage->getAuthor()!="") echo "<br>Foto: ".$d->AssetImage->getAuthor() ?>
+                    <?php if(count($related_content)>0): ?>
+                    <br /><a href="<?php echo $related_content[0]->retriveUrl()?>" target="_blank">Saiba mais</a>
+                    <?php endif; ?>
+                  </span>
+                </span>
+                <?php endforeach; ?>
+              <?php endif; ?>
+              </div>
+              <!-- /GALERIA DE FOTOS -->
+              
+              <!-- barra compartilhar -->
+              <?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?>
+              <!-- /barra compartilhar -->
+
+            </div>
+            
+            <div id="direita" class="grid1 direitaMais">
+              
+              <?php $people = $asset->retriveRelatedAssetsByAssetTypeId(20); ?>
+              <?php if(count($people)>0): ?>
+                <?php $author = $people[0]; ?>
+                <!-- BOX PADRAO INFORMACOES -->
+                <div class="box-padrao grid1 informacao">
+                  <div class="topo claro">
+                    <span></span>
+                    <div class="capa-titulo">
+                      <h4>informa&ccedil;&otilde;es do artista</h4>
+                    </div>
+                  </div>
+                  <div class="conteudo">
+                    <p class="titulos"><?php echo $author->AssetPerson->getName() ?></p>
+                    <p class="txt-12 bold"><?php echo $author->AssetPerson->getHeadline() ?></p>
+                    <!-- <p class="txt-12 bold">Nasceu em: <span>30/10/1980, S&atilde;o Paulo.</span></p> -->
+                    <p class="txt-16 bold">Biografia</p>
+                    <p class="txt"><?php echo $author->AssetPerson->getBio() ?></p>  
+                  </div>
+                </div>
+                <!-- BOX PADRAO INFORMACOES -->
+              <?php endif; ?>
+
+              <!-- BOX PUBLICIDADE -->
+              <div class="box-publicidade grid1">
+                <!-- programas-assets-300x250 -->
+                <script type='text/javascript'>
+                GA_googleFillSlot("cmais-assets-300x250");
+                </script>
+              </div>
+              <!-- / BOX PUBLICIDADE -->
+
+              <?php if(isset($displays["destaque-noticias"])): ?>
+              <!-- BOX PADRAO Noticia -->
+              <div class="box-padrao grid1">
+                <div class="topo claro">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4>Not&iacute;cias + recentes</h4>
+                    <!-- <a href="#" class="rss" title="rss"></a> -->
+                  </div>
+                </div>
+                <?php include_partial_from_folder('blocks','global/recent-news', array('displays' => $displays["destaque-noticias"])) ?>
+              </div>
+              <!-- /BOX PADRAO Noticia -->
+              <?php endif; ?>
+
+            </div>
+
+          </div>
+          <!-- CAPA -->
+
+          <!-- MENU-RODAPE -->
+          <?php if(isset($displays["galerias-recentes"])) include_partial_from_folder('blocks','global/display-3c-last-galleries', array('displays' => $displays["galerias-recentes"])) ?>
+          <!-- /MENU-RODAPE -->
+
+          <!-- BOX PUBLICIDADE 2 -->
+          <div class="box-publicidade pub-grd grid3">
+            <!-- programas-assets-728x90 -->
+            <script type='text/javascript'>
+            GA_googleFillSlot("cmais-assets-728x90");
+            </script>
+          </div>
+          <!-- / BOX PUBLICIDADE 2 -->
+          
+          </div>
+          <!-- /CONTEUDO PAGINA -->
+        
+        </div>
+        <!-- /MIOLO -->
+      
+      </div>
+      <!-- / CAPA SITE -->
+
+
---
+++ apps/frontend/templates/sites/1964/indexSuccess.php
@@ -0,0 +1,98 @@
+<link type="text/css" href="/portal/univesptv/css/geral.css" rel="stylesheet" />
+<script type="text/javascript" src="/portal/js/mediaplayer/swfobject.js"></script>
+
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+
+    <!-- CAPA SITE -->
+    <div id="capa-site">
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+       <div class="topo-programa">
+          <h2><a href="http://univesptv.cmais.com.br"><img title="Univesp TV" alt="Univesp TV" src="http://midia.cmais.com.br/programs/43cfb180f75e0cbc2c2823f4cfb603643151ab5a.png"></a></h2>
+          
+                              <!-- curtir -->
+          <div class="redes">
+            <div class="curtir">
+              <div style="display:block; float: left; margin-right:10px;">
+                
+                <div id="___plusone_0" style="text-indent: 0px; margin: 0px; padding: 0px; background-color: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 32px; height: 20px; background-position: initial initial; background-repeat: initial initial;"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 32px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 20px;" tabindex="0" vspace="0" width="100%" id="I0_1389127431741" name="I0_1389127431741" src="https://apis.google.com/u/0/_/+1/fastbutton?usegapi=1&amp;bsv=o&amp;size=medium&amp;count=false&amp;hl=pt-BR&amp;origin=http%3A%2F%2Funivesptv.cmais.com.br&amp;url=http%3A%2F%2Funivesptv.cmais.com.br%2F&amp;gsrc=3p&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.pt_BR.u8A1venuJ-0.O%2Fm%3D__features__%2Fam%3DIQ%2Frt%3Dj%2Fd%3D1%2Frs%3DAItRSTPTCw51hZ2UipDUMXmbkRuPEsc1NA#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh&amp;id=I0_1389127431741&amp;parent=http%3A%2F%2Funivesptv.cmais.com.br&amp;pfname=&amp;rpctoken=38916840" data-gapiattached="true" title="+1"></iframe></div>
+                            </div>
+              <fb:like href="https://www.facebook.com/pages/Univesp-TV/288255214535686" layout="button_count" show_faces="false" send="true" width="160" fb-xfbml-state="rendered" class="fb_edge_widget_with_comment fb_iframe_widget"><span style="height: 20px; width: 174px;"><iframe id="f177646c2" name="f1b589709" scrolling="no" title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=124792594261614&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D28%23cb%3Dfe8953304%26domain%3Dunivesptv.cmais.com.br%26origin%3Dhttp%253A%252F%252Funivesptv.cmais.com.br%252Ff17cd6a578%26relation%3Dparent.parent&amp;colorscheme=light&amp;extended_social_context=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FUnivesp-TV%2F288255214535686&amp;layout=button_count&amp;locale=pt_BR&amp;node_type=link&amp;sdk=joey&amp;send=true&amp;show_faces=false&amp;width=160" style="border: none; overflow: hidden; height: 20px; width: 174px;"></iframe></span></fb:like>
+            </div>
+            <iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" src="http://platform.twitter.com/widgets/tweet_button.1387492107.html#_=1389127431727&amp;count=horizontal&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=http%3A%2F%2Funivesptv.cmais.com.br%2F&amp;size=m&amp;text=Univesp%20TV%20-%20cmais%2B%20O%20portal%20de%20conte%C3%BAdo%20da%20Cultura&amp;url=http%3A%2F%2Funivesptv.cmais.com.br%2F&amp;via=Univesp" class="twitter-share-button twitter-tweet-button twitter-count-horizontal" title="Twitter Tweet Button" data-twttr-rendered="true" style="width: 108px; height: 20px;"></iframe>
+            
+               
+            <ul>                       
+              <li><a class="fb" href="https://www.facebook.com/pages/Univesp-TV/288255214535686" title="Facebook" target="_blank">Facebook</a></li>
+              <li><a class="twt" href="http://twitter.com/Univesptv" title="Twitter" target="_blank">Twitter</a></li>
+              <li><a class="ytb" href="http://youtube.com/univesptv" title="YouTube" target="_blank">YouTube</a></li>
+              <li><a class="rss" href="/univesptv/feed" title="RSS" target="_blank">RSS</a></li>
+            </ul>
+          </div>
+          <!-- /curtir -->
+                    
+                    <!-- horario -->
+          <div id="horario">
+            <p>Canal digital 2.2 da multiprogramação da TV Cultura</p>
+          </div>
+          <!-- /horario -->
+          
+        </div>
+			<!-- box-topo -->
+	        <div class="box-topo grid3">
+	          <!-- menu interna -->
+	          <ul class="menu-interna grid3">
+				<li><a href="" title="">Opção1</a></li>
+				<li><a href="" title="">Opção2</a></li>
+				<li><a href="" title="">Opção3</a></li>
+				<li><a href="" title="">Opção4</a></li>
+				<li><a href="" title="">Opção5</a></li>
+			 </ul>
+	          <!-- /menu interna -->                 
+        </div>
+        <!-- /box-topo -->
+	  </div>	
+      <!-- /BARRA SITE -->
+      
+      <!-- MIOLO -->
+   	   <section id="miolo">
+   	   	<div class="box-interna grid2">
+   	   		<h3>Timeline 1964 - 50 anos do Golpe</h3>
+   	   		<p>Aqui vai uma breve descrição se precisar, bla bla bla bla<br>
+   	   			bla bla bla
+   	   		</p>
+   	   	</div>
+          <!-- INICIO TIMELINE -->
+          <div class="" style="height: 500px;">
+            <div id="tvcultura-embed"></div>
+            <script type="text/javascript">
+              var timeline_config = {
+               width: "100%",
+               height: "100%",
+               source: 'http://172.20.1.79:8080/tela2/example_jsonp.jsonp',
+               start_at_slide: 0,
+               start_zoom_adjust: 2,
+               embed_id: "tvcultura-embed",
+               css: "/portal/css/tvcultura/sites/1964.css",
+              /* css: "http://172.20.1.79:8080/tela2/js/timelinejs/css/timeline.css",*/
+               js: "/portal/js/timeline/timeline-min.js"
+              }
+            </script>
+            <script type="text/javascript" src="/portal/js/timeline/storyjs-embed.js"></script>
+            <!-- /FIM TIMELINE -->
+            <ul id="apoio" class="grid3">
+	              <li><a href="http://www.desenvolvimento.sp.gov.br" class="governoSp"><img src="http://cmais.com.br/portal/univesptv/images/logo-goversoSp.jpg" alt="Governo do Estado de São Paulo"></a></li>
+	              <li><a href="http://www.fapesp.br" class="fapesp"><img src="http://cmais.com.br/portal/univesptv/images/logo-fapesp.png" alt="FAPESP"></a></li>
+	              <li><a href="http://www.unicamp.br" class="unicamp"><img src="http://cmais.com.br/portal/univesptv/images/logo-unicamp.png" alt="UNICAMP"></a></li>
+	              <li><a href="http://www.unesp.br" class="unesp"><img src="http://cmais.com.br/portal/univesptv/images/logo-unesp.png" alt="UNESP"></a></li>
+	              <li><a href="http://www.usp.br" class="usp"><img src="http://cmais.com.br/portal/univesptv/images/logo-usp.png" alt="USP"></a></li>
+	              <li><a href="http://www.fundap.sp.gov.br" class="fundap"><img src="http://cmais.com.br/portal/univesptv/images/logo-fundap.jpg" alt="FUNDAP"></a></li>
+	              <li><a href="http://www.centropaulasouza.sp.gov.br" class="cps"><img src="http://cmais.com.br/portal/univesptv/images/logo-cps.png" alt="Centro Paula Souza"></a></li>
+	          </ul>
+          </div>
+       </section><!--/MIOLO -->
+    </div><!-- /CAPA SITE -->
+
+
