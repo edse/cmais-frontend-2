b2c2bec90bc5beec05c94eb949b3b4de4a5d3c06
doljak <jedoljak@gmail.com>
2014-01-09 19:57:03 -0200

    acessibilidade Vila Sésamo

---
+++ apps/frontend/templates/vilasesamo.sem.acessibilidade.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html lang="pt-br">
+<html xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/"> 
+  <head>
+    <?php include_title() ?>
+    <?php include_metas() ?>
+    <?php include_meta_props() ?>
+    
+    <!--META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE"-->
+     
+    
+    <!-- Le styles -->  
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <!--script type="text/jscript" src="http://cmais.com.br/portal/js/jquery-1.7.2.min.js"></script-->
+    <link href="http://cmais.com.br/portal/js/bootstrap-v2.3.1/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <!--link href="http://cmais.com.br/portal/js/bootstrap-v2.3.1/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"--> 
+    <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/geral.css" type="text/css" />
+    
+
+    <script src="http://cmais.com.br/portal/js/bootstrap-v2.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="http://cmais.com.br/portal/js/dotdotdot-1.6.7/jquery.dotdotdot.min.js" type="text/javascript"></script>
+
+     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+     <!--[if lt IE 9]>
+       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+        
+     <!--[if lte IE 8]>
+       <link type="text/css" rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/cocorico/ie.css" />
+     <![endif]-->
+
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store" />
+    <meta http-equiv="Pragma" content="no-cache, no-store" />
+    <meta http-equiv="expires" content="Mon, 06 Jan 1990 00:00:01 GMT" />
+    
+    <meta name="google-site-verification" content="sPxYSUnxlnoyUdly_hNwIHma64gh9iosgNcOBrZBYdo" />
+
+    <meta property="fb:admins" content="100000889563712"/>
+    <meta property="fb:app_id" content="124792594261614"/>
+    
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://cmais.com.br/portal/images/icon/cmais-favico_144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://cmais.com.br/portal/images/icon/cmais-favico_114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.png">
+    
+    <link rel="apple-touch-icon" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.png" />
+    <link rel="image_src" href="http://cmais.com.br/portal/images/icon/cmais-favico_72.png" />
+    <link rel="SHORTCUT ICON" href="http://cmais.com.br/portal/images/icon/favicon.ico" type="image/x-icon">
+    
+    <script type="text/javascript" src="http://apis.google.com/js/plusone.js">
+      {lang: 'pt-BR'}
+    </script>
+    <?php
+    /*
+    <!-- DFP -->        
+    <script type='text/javascript' src='http://partner.googleadservices.com/gampad/google_service.js'>
+    </script>
+    <script type='text/javascript'>
+    GS_googleAddAdSenseService("ca-pub-6681834746443470");
+    GS_googleEnableAllServices();
+    </script>
+    <script type='text/javascript'>
+    GA_googleAddSlot("ca-pub-6681834746443470", "vilasesamo");
+    </script>
+    <script type='text/javascript'>
+    GA_googleFetchAds();
+    </script> 
+    <!-- /DFP -->
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-22770265-1']);
+      _gaq.push(['_setDomainName', 'cmais.com.br']);
+      _gaq.push(['_setAllowHash', 'false']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+    */
+    ?>
+    <!--banner vila sesamo 2013-->
+    <script type='text/javascript'>
+      var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') + 
+      '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+      })();
+    </script>
+    
+    <script type='text/javascript'>
+      googletag.cmd.push(function() {
+      googletag.defineSlot('/4079539/vilasesamo', [300, 250], 'div-gpt-ad-1385748525978-0').addService(googletag.pubads());
+      googletag.pubads().enableSingleRequest();
+      googletag.enableServices();
+      });
+    </script>
+    <!--/banner vila sesamo 2013-->
+    
+    <!--meta name="viewport" content="initial-scale=0.5, minimum-scale=0.5, maximum-scale=0.5, user-scalable=no"--> 
+
+      
+  </head>
+  <body>
+    <!-- header -->
+    <header class="navbar navbar-inverse navbar-fixed-top" aria-label="Menu topo Vila Sésamo">
+      <!--div class="navbar-inner topo-cmais">
+        <div class="container">
+          <h2><a href="http://tvcultura.cmais.com.br/" title="TV Cultura" class="sprite-logo-cultura"></a></h2>
+          <h1><a class="sprite-logo-cmais brand" href="http://cmais.com.br/" title="Cmais"></a></h1>     
+        </div>
+      </div-->
+      <!--topo-vila-->
+      <div class="navbar-inner topo-vila">
+        <!--/container-->
+        <div class="container">
+          <!--button-->          
+          <!--button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <i class="sprite-seta-up-menu"></i>
+            <h1>Menu</h1>
+          </button-->
+          <!--button-->
+          
+          <!--nav--> 
+          <nav class="nav-collapse collapse" title="menu principal">
+            
+            <!--ul.nav-->
+            <ul class="nav">
+              <li>
+                <i class="icones-sprite-menu icone-cuidadores"></i>
+                <a class="btn-cuidadores-topo" href="http://cmais.com.br/vilasesamo/pais-e-educadores" title="Pais e Educadores" target="_self">Pais e Educadores</a>
+                <span class="sprite-menu-ball"></span>
+              </li>
+              <li>
+                <i class="icones-sprite-menu icone-natv-peqno-verde"></i> 
+                <a class="btn-na-tv-topo" href="http://cmais.com.br/vilasesamo/na-tv" title="Na TV" target="_self">Na TV</a>
+                <span class="sprite-menu-ball"></span>
+              </li>
+              <li>  
+                <i class="icones-sprite-menu icone-acessibilidade-pqno-verde"></i>
+                <a class="btn-acessibilidade-topo" href="http://cmais.com.br/vilasesamo/acessibilidade" title="Acessibilidade" target="_self">Acessibilidade</a>
+                <!--span class="sprite-menu-ball"></span-->
+              </li>  
+              <!--li>
+                <a href="/vilasesamo2/historia" title="História" target="_self">História</a>
+              </li-->            
+            </ul> 
+            <!--ul.nav-->
+            
+            <!--form-->
+            <form class="form-search" action="http://cmais.com.br/vilasesamo/busca" method="get">
+              <input type="text" class="input-medium search-query" value="Encontre no site" onfocus="this.value='';" title="Encontre no site" name="term" aria-label="Digite aqui o que gostaria de encontrar no site Vila Sésamo.">
+              <button type="submit" class="icones-sprite-menu icone-busca" title="buscar no site vila sesamo"></button>
+            </form>
+            <!--/form-->
+          </nav>
+          <!--/nav-->
+        </div>
+        <!--/container-->
+      </div>
+      <!--/topo-vila-->
+    </header>
+    <!-- /header -->
+    
+    <?php echo $sf_content ?>
+    
+    <!--footer-->
+    <footer>
+      
+      <div class="copyright" role="presentation" aria-hidden="true" >
+        <small>© 2013 - Vila Sésamo</small>
+      </div>
+      
+      <!--container-->
+      <div class="container row-fluid">
+        
+        <!--section-->  
+        <section aria-label="Você está entrando no rodapé da página">
+          
+          <!--logos-->
+          <ul class="logos">
+            <li class="col-esq">
+              <a href="http://cmais.com.br/vilasesamo" title="Site Vila Sésamo" target="_self">
+                <i class="icones-rodape icone-logo-internas-rodape"></i>
+              </a>
+            </li>
+            <li class="col-central">
+              <a href="http://cmais.com.br/vilasesamo/categorias/habitos-saudaveis" title="Hábitos Saudáveis" target="_self">
+                <i class="icones-rodape icone-habitos-saudaveis-rodape"></i>
+              </a>
+            </li>
+            <li class="col-dir">
+              <a href="http://cmais.com.br/vilasesamo/categorias/incluir-brincando" title="Incluir Brincando" target="_self">
+                <i class="icones-rodape icone-incluir-brincando-rodape"></i>
+              </a>
+            </li>
+            <img class="sombra" src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/sombra-rodape.jpg" alt="" />
+          </ul>
+          <!--/logos-->
+          
+          <!--mapa-->
+          <div class="mapa">
+            
+            <!--nav sites Vila Sesamo-->
+            <nav class="span12" title="Sites Vila Sésamo" aria-disabled="true">
+              <a href="http://cmais.com.br/vilasesamo/jogos" class="col-esq" title="Jogos" target="_self">Jogos</a>
+              <a href="http://cmais.com.br/vilasesamo/videos" class="col-central" title="Jogos" target="_self"><i class="icones-rodape icone-triangulo-claro"></i>Vídeos</a>
+              <a href="http://cmais.com.br/vilasesamo/atividades" class="col-dir" title="Jogos" target="_self"><i class="icones-rodape icone-triangulo-medio"></i>Atividades</a>
+            </nav>
+            <!--nav sites Vila Sesamo-->
+            
+            <!--nav Personagens-->
+            <nav class="span12 personagens-rodape" title="Menu Personagens">
+              <h3>PERSONAGENS:</h3>
+              <ul>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/garibaldo" title="Garibaldo" target="_self">Garibaldo</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/bel" title="Bel" target="_self">Bel</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/elmo" title="Elmo" target="_self">Elmo</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/grover" title="Grover" target="_self">Grover</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/beto" title="Beto" target="_self">Beto</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/enio" title="Ênio" target="_self">Ênio</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/comecome" title="Come-Come" target="_self">Come-Come</a><span>,</span>
+                </li>
+                <li>
+                  <a href="http://cmais.com.br/vilasesamo/personagens/zoe" title="Zoe" target="_self">Zoe</a>
+                </li>
+              </ul>
+            </nav> 
+            <!--/nav Personagens-->
+            
+            <!--nav-->
+            <nav class="span12" aria-disabled="true">
+              <a href="/vilasesamo/pais-e-educadores" class="col-esq " title="">Pais e Educadores</a><!--cuidadores-footer - classe criada para quando tiver somente dois itens na lista -->
+              <a href="/vilasesamo/na-tv" class="col-central" title=""><i class="icones-rodape icone-triangulo-claro"></i>Na TV</a><!-- natv-footer - cuidadores-footer - classe criada para quando tiver somente dois itens na lista -->
+              <a href="/vilasesamo/acessibilidade" class="col-dir" title=""><i class="icones-rodape icone-triangulo-medio"></i>Acessibilidade</a>
+              <!--a href="/vilasesamo2/atividades" class="col-dir" title=""><span class="sprite-destalhe-amarelo1"></span>História</a>
+            </nav>
+            <!--/nav-->
+                  
+          </div>
+          <!--/mapa-->
+          
+          <img class="sombra" src="http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/sombra-rodape.jpg" alt="" />
+        </section>
+        <!--/section-->
+      <section class="relacionados" aria-label="sites relacionados">
+        <h3>Sites relacionados</h3>
+        <ul>
+          <li><a href="http://tvcultura.cmais.com.br/" target="_self" class="sprite-logo-cultura2" title="TV Cultura"></a></li>
+          <li><a href="http://tvratimbum.cmais.com.br/" target="_self" class="sprite-logo-rtb" title="TV Rá Tim Bum"></a></li>
+          <li><a href="http://cmais.com.br/maiscrianca" target="_self" class="sprite-logo-maiscrianca" title="Mais Criança"></a></li>
+          <li><a href="http://tvcultura.cmais.com.br/cocorico" target="_self" class="sprite-logo-cocorico"  title="TV Cocoricó"></a></li>
+          <li><a href="http://tvcultura.cmais.com.br/castelo" target="_self" class="sprite-logo-castelo" title="Castelo Rá Tim Bum"></a></li>
+          <li><a href="http://tvcultura.cmais.com.br/cartaozinho" target="_self" class="sprite-logo-cartaozinho" title="Cartãozinho"></a></li>
+          <li class="last"><a href="http://tvcultura.cmais.com.br/quintaldacultura" target="_self" class="sprite-logo-quintal" title="Quintal da Cultura"></a></li>
+          
+        </ul>
+      </section>
+      </div>
+      <!--/container-->
+      
+    </footer>
+    <!--/footer-->
+    
+    <!-- scripts -->
+    <script type="text/javascript" src="http://cmais.com.br/portal/js/vilasesamo2/vilasesamo.js"></script>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-22770265-1']);
+      _gaq.push(['_setDomainName', 'cmais.com.br']);
+      _gaq.push(['_setAllowHash', 'false']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+    <!-- /scripts -->
+    
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({appId: '124792594261614', status: true, cookie: true, xfbml: true});
+      };
+      (function() {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol +
+          '//connect.facebook.net/pt_BR/all.js';
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script type="text/javascript" src="http://apis.google.com/js/plusone.js">
+      {lang: 'pt-BR'}
+    </script>
+    <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/media.css" type="text/css" />
+       
+  </body>
+</html>
