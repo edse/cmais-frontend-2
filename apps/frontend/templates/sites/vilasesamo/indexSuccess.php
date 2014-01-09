3ab91d62edfaea7e7a0715e8cbef46a032af18dc
doljak <jedoljak@gmail.com>
2014-01-09 20:18:07 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/indexSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/indexSuccess.php
@@ -40,7 +40,7 @@
           <a href="/<?php echo $site->getSlug() ?>/<?php echo $assetSection->getSlug() ?>/<?php echo $d->Asset->getSlug() ?>" title="">
             <?php if($d->Asset->AssetType->getSlug() == "video"): ?>
             <div class="yt-menu">  
-              <img class="destaque" src="http://img.youtube.com/vi/<?php echo $d->Asset->AssetVideo->getYoutubeId() ?>/0.jpg" alt="/>
+              <img class="destaque" src="http://img.youtube.com/vi/<?php echo $d->Asset->AssetVideo->getYoutubeId() ?>/0.jpg" alt=""/>
             </div>
             <?php else: ?>
               <img class="destaque" src="<?php echo $preview[0]->retriveImageUrlByImageUsage("image-13") ?>" alt=""/>
