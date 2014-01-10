ca9db9c33593b5076fd0b9f7f6bcfb34789426e0
Joseval <Joseval@fpawks0146-2.local>
2014-01-10 18:16:15 -0200

    Meta Refresh Jornalismo

--- apps/frontend/templates/sites/cmais/jornalismoSuccess.php
+++ apps/frontend/templates/sites/cmais/jornalismoSuccess.php
@@ -36,7 +36,11 @@ if(count($bs) > 0){
 } 	
 	
 ?>
-<meta http-equiv="refresh" content="18000">
+<noscript>
+	<meta http-equiv="refresh" content="250" />
+</noscript>
+<meta http-equiv="refresh" content="18000" />
+		
 <link rel="stylesheet" href="/portal/css/tvcultura/secoes/jornalismo-novo2013.css" type="text/css" />
 
 <?php use_helper('I18N', 'Date') ?>
