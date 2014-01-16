7232134c0e3ec262d63c0a085c2d81c012715c51
doljak <jedoljak@gmail.com>
2014-01-16 12:45:21 -0200

    jogos e videos com acessibilidade vila sésamo

--- apps/frontend/templates/sites/vilasesamo/jogosSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/jogosSuccess.php
@@ -1,12 +1,17 @@
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
-
+<link rel="stylesheet" href="http://172.20.16.219/portal/css/tvcultura/sites/vilasesamo2/internas.css" type="text/css" />
+<?php  $noscript = "  <noscript>Desculpe mas no seu navegador não esta habilitado o Javascript, habilite-o e recarregue a página para o banner aparecer.</noscript>" ?>
 <script>
   $("body").addClass("interna jogos");
 </script>
-
+<?php echo $noscript; ?>
 <!-- HEADER -->
 <?php include_partial_from_folder('sites/vilasesamo', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 <!-- /HEADER -->
+
+<!--Explicação acessibilidade-->
+<h1 tabindex="0" class="ac-explicacao">
+  <?php echo $section->getDescription(); ?>
+</h1>
 
 <!--content-->
 <div id="content">
@@ -15,8 +20,11 @@
   <!--span12-->
   <div class="span12" role="main">
     
-    <!--h3><i class="sprite-icon-colorir-med"></i>Atividades</h3-->
-    <h1><i class="icones-sprite-interna icone-jogos-grande"></i>Jogos</h1>
+    <!--h3><i class="sprite-icon-colorir-med"></i>jogos</h3-->
+    <h1>
+      <i class="icones-sprite-interna icone-jogos-grande"></i>
+      JOGOS
+   </h1>
     
     <?php if(isset($displays['destaque-1']) || isset($displays['destaque-2'])): ?>
       <?php if(count($displays['destaque-1']) > 0 || count($displays['destaque-2']) > 0): ?>
@@ -27,29 +35,47 @@
         <?php if(isset($displays['destaque-1'])): ?>
           <?php if(count($displays['destaque-1']) > 0): ?>
             <?php $related_preview = $displays['destaque-1'][0]->Asset->retriveRelatedAssetsByRelationType("Preview"); ?>
-        <h2 aria-describedby="Novidade">
-          <article class="span6 clipes">
-            <a class="img-destaque" href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $displays['destaque-1'][0]->Asset->getSlug() ?>">
-              <span class="sprite-selo">Novidade!</span>
-              <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $displays['destaque-1'][0]->getTitle() ?>" />
-              <p><?php echo $displays['destaque-1'][0]->getTitle() ?></p> 
-            </a> 
-          </article>
-        </h2>
+            <?php if($related_preview):?>
+              <?php
+                if(count($related_preview)>0):
+                  $decricaoImagem = $related_preview[0]->AssetImage->getHeadline(); 
+                else:
+                  $decricaoImagem = "Desculpe, a imagem esta sem descrição mas o Jogo é bem legal, jogue-o e divirta-se!";  
+                endif; 
+              ?>
+            <?php endif; ?>
+            <h2>
+              <div class="span6 clipes">
+                <a class="img-destaque" href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $displays['destaque-1'][0]->Asset->getSlug() ?>" title="" aria-label="Novidade destaque Jogo - Título: <?php echo $displays['destaque-1'][0]->getTitle().". Descrição: ".$displays['destaque-1'][0]->Asset->getDescription(). ". Descrição do thumbnail:". $decricaoImagem; ?>" >
+                  <span class="sprite-selo">Novidade!</span>
+                  <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="" />
+                  <p><?php echo $displays['destaque-1'][0]->getTitle() ?></p>
+                </a> 
+              </div>
+            </h2>
           <?php endif; ?>
         <?php endif; ?>
         <?php if(isset($displays['destaque-2'])): ?>
           <?php if(count($displays['destaque-2']) > 0): ?>
             <?php $related_preview = $displays['destaque-2'][0]->Asset->retriveRelatedAssetsByRelationType("Preview"); ?>
-        <h2 aria-describedby="Novidade">
-          <article class="span6 clipes  semmargem">
-            <a class="img-destaque" href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $displays['destaque-2'][0]->Asset->getSlug() ?>">
-              <span class="sprite-selo">Novidade!</span>
-              <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $displays['destaque-2'][0]->getTitle() ?>" />
-              <p><?php echo $displays['destaque-2'][0]->getTitle() ?></p> 
-            </a> 
-          </article>
-        </h2>
+            <?php if($related_preview):?>
+              <?php
+                if(count($related_preview)>0):
+                  $decricaoImagem = $related_preview[0]->AssetImage->getHeadline(); 
+                else:
+                  $decricaoImagem = "Desculpe, a imagem esta sem descrição.";  
+                endif; 
+              ?>
+            <?php endif; ?>
+            <h2>
+              <div class="span6 clipes semmargem">
+                <a class="img-destaque" href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $displays['destaque-2'][0]->Asset->getSlug() ?>" title="" aria-label="Novidade destaque Jogo - Título:<?php echo $displays['destaque-2'][0]->getTitle().". Descrição: ".$displays['destaque-2'][0]->Asset->getDescription(). ". Descrição do thumbnail:". $decricaoImagem; ?>" >
+                  <span class="sprite-selo">Novidade!</span>
+                  <img src="<?php echo $related_preview[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="" />
+                  <p><?php echo $displays['destaque-2'][0]->getTitle() ?></p>
+                </a> 
+              </div>
+            </h2>
           <?php endif; ?>
         <?php endif; ?>
       </div>
@@ -63,8 +89,7 @@
     <?php $particularSection = Doctrine::getTable('Section')->findOneBySiteIdAndSlug($site->getId(),"personagens"); ?>
     <?php $personagens = $particularSection->subsections()?>
     
-     
-    
+       
     <?php include_partial_from_folder('sites/vilasesamo', 'global/menu-personagens', array('site'=>$site ,'section' => $section,'personagens' => $personagens)) ?>
     <!--/menu filtro persoagem-->
         
@@ -76,15 +101,16 @@
 
 <?php if(isset($pager)): ?>
   <?php if(count($pager) > 0): ?>
-    <?php $pager2 = count($pager)/9; ?>    
+  <?php $pager2 = count($pager)/9; ?>
+    <?php $parent = $section->Parent->getSlug(); ?>
+ 
 <span class="divisa"></span>
 
 <!--/section-->
 <section class="todos-itens ">
   <!--lista-->
-  <ul role="contentinfo" id="container" class="row-fluid">
-    <?php
-    /*
+  <ul id="container" class="row-fluid">
+    <?php /*
     <?php foreach($pager->getResults() as $k=>$d): ?>
     <?php
       $assetPersonagens = array();
@@ -96,10 +122,10 @@
         }
       }
     ?>
-    <?php $related = $d->retriveRelatedAssetsByRelationType("Preview") ?>
     <li class="span4 element<?php if(count($assetPersonagens) > 0) echo " " . implode(" ", $assetPersonagens); ?> jogos"> 
-      <a href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $d->getSlug() ?>" title="<?php echo $d->getTitle() ?>" aria-label="<?php echo $d->getTitle().$d->getDescription().".Descrição do Thumbnail:".$related[0]->AssetImage->getHeadline() ?>">
-        <img src="<?php echo $related[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $d->getDescription() .". ".$related[0]->AssetImage->getHeadline() ?>" />
+      <a href="/<?php echo $site->getSlug() ?>/jogos/<?php echo $d->getSlug() ?>" title="<?php echo $d->getTitle() ?>">
+        <?php $related = $d->retriveRelatedAssetsByRelationType("Preview") ?>
+        <img src="<?php echo $related[0]->retriveImageUrlByImageUsage("image-13") ?>" alt="<?php echo $d->getTitle() ?>" />
         <i class="icones-sprite-interna icone-jogos-pequeno"></i>
         <div>
           <img class="altura" src="/portal/images/capaPrograma/vilasesamo2/altura.png"/>
@@ -109,7 +135,7 @@
     </li>
     <?php endforeach; ?>
      */
-     ?> 
+     ?>
   </ul> 
   <!--lista-->  
 </section>
@@ -122,9 +148,10 @@
   
 
 
+
 <!--paginacao-->
-<?php include_partial_from_folder('sites/vilasesamo', 'global/pagination', array('site' => $site, 'section' => $section, 'pager'=>$pager , 'pager2'=>$pager2)) ?>
+<?php include_partial_from_folder('sites/vilasesamo', 'global/pagination', array('site' => $site, 'section' => $section,'pager'=>$pager , 'pager2'=>$pager2, 'parent' => $parent)) ?>
 <!--/paginacao-->
 
-
+<!--scripts-->
 
