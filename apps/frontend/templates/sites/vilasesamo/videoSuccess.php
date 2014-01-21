7d44c8a5ec9fa35126acea3c9998eea70569a956
doljak <jedoljak@gmail.com>
2014-01-21 17:30:22 -0200

    u

--- apps/frontend/templates/sites/vilasesamo/videoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/videoSuccess.php
@@ -167,8 +167,22 @@
         player.pauseVideo();
     }
     $('.play').click(function(){playVideo()});
-    $('.stop').click(function(){stopVideo()});
-    $('.pause').click(function(){pauseVideo()});
+    $('.stop').click(function(){
+      stopVideo()
+      #('.stop').before('<span class="stopado" aria-label="Você parou a reprodução amiguinho, para iniciar novamente aperte enter no link Iniciar o vídeo">')
+      setTimeout(function(){
+        $('.stopado').remove();
+        $('.stop').focus();
+      },3000);
+    });
+    $('.pause').click(function(){
+      pauseVideo()
+      #('.pause').before('<span class="pausado" aria-label="Você pausou a reprodução amiguinho, para iniciar novamente aperte novamente o link pausar o vídeo">')
+      setTimeout(function(){
+        $('.pausado').remove();
+        $('.pause').focus();
+      },3000);
+    });
 </script>
 <?php echo $noscript ?>
   
