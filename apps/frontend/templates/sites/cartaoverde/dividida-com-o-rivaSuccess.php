1299048bd7a4237b2bc0b58c86126c8ba382db03
Emerson Estrella <emersonestrella@edse.local>
2014-02-03 17:12:12 -0200

    ...

--- apps/frontend/templates/sites/cartaoverde/dividida-com-o-rivaSuccess.php
+++ apps/frontend/templates/sites/cartaoverde/dividida-com-o-rivaSuccess.php
@@ -1,13 +1,32 @@
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/defaultPrograma.css" type="text/css" />
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/rodaviva.css?nocache=<?php echo time(); ?>" type="text/css" />
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/<?php echo $section->Site->getSlug() ?>.css?nocache=<?php echo time(); ?>" type="text/css" />
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/contato.css" type="text/css" />
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/cartaoverde.css" type="text/css" />
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/rodaviva.css" type="text/css" />
 
-    <?php use_helper('I18N', 'Date') ?>
-    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
+<script>
+/*
+function updateTweets(){
+  $.ajax({
+    url: "/index.php/ajax/tweetmonitor",
+    data: "monitor_id=2",
+    success: function(data) {
+      $('#twitter').html(data);
+    }
+  });
+}
+$(function(){ //onready
+  updateTweets();
+  var t=setTimeout("updateTweets()",10000);
+});
+*/
+</script>
+
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
     <!-- CAPA SITE -->
     <div id="capa-site">
+
+      <?php if(isset($displays["alerta"])) include_partial_from_folder('blocks','global/breakingnews', array('displays' => $displays["alerta"])) ?>
 
       <!-- BARRA SITE -->
       <div id="barra-site">
@@ -50,8 +69,7 @@
           </div>
           <?php endif; ?>
           
-          <h3 class="tit-pagina grid3"><?php echo $section->getTitle() ?></h3>  
-          <p><?php echo html_entity_decode($section->getDescription())?></p>
+          <h3 class="tit-pagina grid3"><?php echo $section->getTitle() ?></h3>
 
         </div>
         <!-- /box-topo -->
@@ -62,14 +80,14 @@
       <!-- MIOLO -->
       <div id="miolo">
         
-        <!-- BOX LATERAL -->
         <?php include_partial_from_folder('blocks','global/shortcuts') ?>
-        <!-- BOX LATERAL -->
 
         <!-- CONTEUDO PAGINA -->
         <div id="conteudo-pagina">
+          
           <!-- CAPA -->
           <div class="capa grid3">
+            
             <!-- ESQUERDA -->
             <div id="esquerda" class="grid2">
               <div class="contato grid2">
@@ -166,15 +184,18 @@
             <!-- /ESQUERDA -->
             
             <!-- DIREITA -->
-            <div id="direita" class="grid1">
+            <div id="direita" class="grid1">                          
+
               <!-- BOX PUBLICIDADE -->
               <div class="box-publicidade grid1">
-                <!-- programas-assets-300x250 -->
+                <!-- programas-homepage-300x250 -->
                 <script type='text/javascript'>
                 GA_googleFillSlot("cmais-assets-300x250");
                 </script>
-              </div>
+              </div>                                        
               <!-- / BOX PUBLICIDADE -->
+             
+              
             </div>
             <!-- /DIREITA -->
             
@@ -186,9 +207,9 @@
         
       </div>
       <!-- /MIOLO -->
-
+      
     </div>
-    <!-- / CAPA SITE -->
+    <!-- /CAPA SITE -->
 
     <script type="text/javascript" src="http://cmais.com.br/portal/js/validate/jquery.validate.js"></script>
     <script type="text/javascript">
