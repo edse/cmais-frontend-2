437bd236181eebef10e5c0c8d9d38425594d9a16
georgiacatarina <georgia.catarina@gmail.com>
2014-02-03 11:38:41 -0200

    dicas

--- apps/frontend/templates/sites/1964/contentSuccess.php
+++ apps/frontend/templates/sites/1964/contentSuccess.php
@@ -118,8 +118,7 @@ $(function(){
               </div>
               <!-- / BOX PUBLICIDADE -->
               
-              <!--BOX DICAS DE COMPRA-->
-              <?php include_partial_from_folder('blocks','global/box-dicas', array('section'=> $section)) ?>
+             
 
               <?php $relacionados = array(); if($asset) $relacionados = $asset->retriveRelatedAssets2(); ?>
               <?php if(count($relacionados) > 0): ?>
