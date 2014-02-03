46709e3bf7c806700435cff3b9838dd6ab15fee9
Emerson Estrella <emersonestrella@edse.local>
2014-02-03 17:18:21 -0200

    ...

--- web/actions/cartao/sendmail.php
+++ web/actions/cartao/sendmail.php
@@ -5,7 +5,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
   
     //$to = "julianagaldeano@tvcultura.com.br";
-    $to = "emerson.estrella@gmail.com";
+    $to = "julianagaldeano@tvcultura.com.br, emerson.estrella@gmail.com";
     $email = strip_tags($_REQUEST['email']);
     $name = strip_tags($_REQUEST['nome']);
     $from = "{$name} <{$email}>";
