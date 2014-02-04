785d97b3b0256a17709189059293743cd59d4fd1
Joseval <Joseval@fpawks0146-2.local>
2014-02-04 10:20:56 -0200

    Linha do Tempo

--- apps/frontend/templates/sites/qualidade/linha-do-tempoSuccess.php
+++ apps/frontend/templates/sites/qualidade/linha-do-tempoSuccess.php
@@ -17,28 +17,27 @@ storyjs_jsonp_data = {
             }
 						$assets = null;
             $assets = $d->retriveRelatedAssets();
-            if($assets[0]->getAssetTypeId() == 2){
-              //IMG
-              $src = $assets[0]->AssetImage->getOriginalUrl();
-              $thumb = "http://midia.cmais.com.br/assets/image/thumbnail/".$assets[0]->AssetImage->getFile().".jpg";
-            }else if($assets[0]->getAssetTypeId() == 4){
-              //AUDIO
-              $src = "http://midia.cmais.com.br/assets/audio/default/".$assets[0]->AssetAudio->getFile().".mp3";
-              $thumb = "";
-            }else if($assets[0]->getAssetTypeId() == 6){
-              //VIDEO
-              $src = "http://www.youtube.com/watch?v=".$assets[0]->AssetVideo->getYoutubeId();
-              $thumb = "http://img.youtube.com/vi/".$assets[0]->AssetVideo->getYoutubeId()."/default.jpg";
-            }
-						echo count($assets);
-						if(count($assets) < 1) {
-							
+
+						//echo count($assets);
+						if(count($assets) == 0) {
 							$src = "http://midia.cmais.com.br/assets/image/default/f734c02e984e7059a94276178adb725acc71ffbf.jpg";	
 							$thumb = "http://midia.cmais.com.br/assets/image/thumbnail/f734c02e984e7059a94276178adb725acc71ffbf.jpg";
-							echo "<br>".$src.$thumb."<br>";	
+							//echo "<br>".$src.$thumb."<br>";	
+						}else{ 
+							if($assets[0]->getAssetTypeId() == 2){
+	              //IMG
+	              $src = $assets[0]->AssetImage->getOriginalUrl();
+	              $thumb = "http://midia.cmais.com.br/assets/image/thumbnail/".$assets[0]->AssetImage->getFile().".jpg";
+	            }else if($assets[0]->getAssetTypeId() == 4){
+	              //AUDIO
+	              $src = "http://midia.cmais.com.br/assets/audio/default/".$assets[0]->AssetAudio->getFile().".mp3";
+	              $thumb = "";
+	            }else if($assets[0]->getAssetTypeId() == 6){
+	              //VIDEO
+	              $src = "http://www.youtube.com/watch?v=".$assets[0]->AssetVideo->getYoutubeId();
+	              $thumb = "http://img.youtube.com/vi/".$assets[0]->AssetVideo->getYoutubeId()."/default.jpg";
+	            }
 						}
-						
-						
 						
             if(isset($date1)):
 							$description = str_replace("\r\n",";",trim($d->getDescription()));
