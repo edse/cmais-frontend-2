eb242783258a692360555391a774e18ae33d7120
georgiacatarina <georgia.catarina@gmail.com>
2014-02-06 14:13:35 -0200

    qualidade

--- apps/frontend/templates/sites/qualidade/indexSuccess.php
+++ apps/frontend/templates/sites/qualidade/indexSuccess.php
@@ -4,24 +4,24 @@
 <link rel="stylesheet" href="http://cmais.com.br/portal/univesptv/css/cursos.css" /-->
 <script type="text/javascript" src="http://cmais.com.br/portal/js/mediaplayer/swfobject.js"></script>
 
-
+<script type="text/javascript">
+    $(function(){
+      $('.carrossel').jcarousel({
+        wrap: "both"
+      });
+    })
+    </script>
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
     <!-- CAPA SITE -->
 	<div id="capa-site" class="a1964">
+     	
      	<!-- BARRA SITE -->
-  		<div id="barra-site" onclick=location="home" title="<?php echo $section->getTitle() . "  ". $section->getDescription() ?>">
+  		<div id="barra-site" title="<?php echo $section->getTitle() . "  ". $section->getDescription() ?>">
+					<a href="#"><img src="http://cmais.com.br/portal/images/qualidade/topo.jpg"></a>
 				
-				<!-- TOPO -->
-		    <div class="topo-programa">
-		    	
-	    		<!-- MENU -->
-					<?php include_partial_from_folder('blocks','global/sections-menu2', array('siteSections' => $siteSections))?>
-					<!--/ MENU -->
-					
-		    <!-- / TOPO -->  
-		    </div>
+		
 		  <!-- /BARRA SITE -->  
       </div>
        
@@ -44,63 +44,77 @@
 								<p><?php echo $asset_quality->getDescription();?></p>
 								<div class="duas-colunas destaque grid2">
 								
+								<!--Players-->
 								<div id="player"><iframe title="Washington Olivetto: “A TV Cultura tem uma busca de qualidade histórica”" width="640" height="390" src="http://www.youtube.com/embed/DD3hR-qhz_c?wmode=transparent&amp;rel=0" frameborder="0" allowfullscreen=""></iframe></div>
 									<script>
 										function changeVideo(id){
 										  $('#player').html('<iframe width="640" height="390" src="http://www.youtube.com/embed/'+id+'?wmode=transparent" frameborder="0" allowfullscreen></iframe>');
 										}
 									</script>
-								
-									<ul class="box-playlist grid2">
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										<li>
-											<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
-												<img src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
-											</a>
-										</li>
-										
-									</ul>
+									<div class="box-thumbs">
+										<ul class="box-playlist grid2">
+											<h4>Assista todos os vídeos</h4>
+											<li>
+												<a href="javascript:changeVideo('NnxykRieC34')" class="img">
+                          <img class="img-150x90" src="http://img.youtube.com/vi/NnxykRieC34/1.jpg" alt="Prêmio Ford 2003 premia TV Cultura">
+                        </a>
+											</li>
+											<li>
+												<a href="javascript:changeVideo('PRFlzQ8OXw4')" class="img">
+                          <img class="img-150x90" src="http://img.youtube.com/vi/PRFlzQ8OXw4/1.jpg" alt="Relembre: TV Cultura conquista prêmio Emy pela programação infantil">
+                        </a>
+											</li>
+											<li>
+												<a href="javascript:changeVideo('NnxykRieC34')" class="img">
+                          <img class="img-150x90" src="http://img.youtube.com/vi/NnxykRieC34/1.jpg" alt="Prêmio Ford 2003 premia TV Cultura">
+                        </a>
+											</li>
+											<li>
+												<a href="javascript:changeVideo('RllwhNmqqcg')" class="img">
+                          <img class="img-150x90" src="http://img.youtube.com/vi/RllwhNmqqcg/1.jpg" alt="Tom Zé: “A TV Cultura é aconselhável”">
+                        </a>
+											</li>
+											<li>
+												<a href="javascript:changeVideo('qf5fJG0RfNA')" class="img">
+                          <img class="img-150x90" src="http://img.youtube.com/vi/qf5fJG0RfNA/1.jpg" alt="Maurício de Sousa: “Eu fico muito feliz ter a TV Cultura entre as melhores do mundo”">
+                        </a>
+											</li>
+											
+										</ul>
+									</div>
+									<div class="jcarousel-prev jcarousel-prev-horizontal jcarousel-prev-disabled jcarousel-prev-disabled-horizontal" disabled="disabled" style="display: block;"></div>
+									<div class="jcarousel-next jcarousel-next-horizontal" style="display: block;"></div>
+									<!--Players-->
 								</div>
 								
 		   	   	 </div>
 		   	   		<div id="direita"class="grid1">
-									<?php	echo $asset_quality->AssetContent->render(); ?>
+								<?php	//echo $asset_quality->AssetContent->render(); ?>
+									
+								<p>De acordo com um levantamento feito pelo Instituto Populus de pesquisa, do Reino Unido, a TV Cultura foi classificada como a segunda emissora com maior qualidade no mundo, ficando atrás apenas da britânica BBC One, que teve 79% da preferência do público. Outra brasileira que também classificada no ranking foi a Rede Globo, na 28ª posição.</p><br>
+								<p>A pesquisa encomendada pela BBC foi realizada entre 30 de setembro e 18 de outubro de 2013, em 14 países (Austrália, Brasil, Dinamarca, França, Alemanha, Itália, Japão, Holanda, Espanha, Suécia, Portugal, Reino Unido, Emirados Árabes e Estados Unidos). Um total de 500 adultos de cada país, com mais de 18 anos, foram submetidos a um questionário para dar nota à qualidade da TV, de um modo geral, bem como às emissoras de seu país.</p><br>
+								<p>Também participaram do levantamento as emissoras com Market Share (participação de mercado) de 5% ou mais em seus países. O Market Share serve para avaliar os canais que tiveram, no mesmo período, a preferência do telespectador. A referência é o número de pessoas com a televisão ligada naquele instante.</p><br>
+								
+								<div class="box-confira">
+									<h2><strong>Confira a apresentação da pesquisa de qualidade da TV mundial na íntegra</strong></h2>
+									<p class="btn-confira"><a href="http://midia.cmais.com.br/assets/file/original/d736f2af1a073e7645f1db9583bf2ebd052232fc.pdf" target="_blank">Português</a></p>
+									<p class="btn-confira"><a href="http://midia.cmais.com.br/assets/file/original/481a3dbb48c883c9ac04eb47268f73afd3d931fb.pdf" target="_blank">Inglês</a></p>
+									<h2><strong>Confira as tabelas de avaliação por país</strong></h2>
+									<p class="btn-confira"><a href="http://midia.cmais.com.br/assets/file/original/3c537556968f9e1f714fa51df6f1e7aefda011c2.pdf" target="_blank">Português (Brasil)</a></p>
+									<p class="btn-confira"><a href="http://midia.cmais.com.br/assets/file/original/2b57ba2dc91af7fb2222b4a9940587abdf2469b1.pdf" target="_blank">Inglês (íntegra)</a></p>
+								</div>		
 							</div>
 							
 		          <!-- INICIO TIMELINE -->
-		          <div class="timeline">
+		          <div class="timeline" style="display: none">
 			            <div id="tvcultura-embed"></div>
 			            <script type="text/javascript">
 			              var timeline_config = {
 			               width: "100%",
 			               height: "100%",
 			               source: "http://cmais.com.br<?php echo url_for('homepage')?>qualidade/linha-do-tempo.jsonp",
-			               start_at_slide: 0,
-			               start_zoom_adjust: 2,
+			               start_at_slide: 67,
+			               start_zoom_adjust: -1,
 			               embed_id: "tvcultura-embed",
 			               css: "http://cmais.com.br/portal/js/qualidade/qualidade.css",
 			               lang: "http://cmais.com.br/portal/js/qualidade/pt-br.js",
@@ -111,15 +125,25 @@
 		            </div>
 		            <!-- /FIM TIMELINE -->
 	            		         
-     		</div><!--/CAPA-->
+     				</div><!--/CAPA-->
 
-	         <!-- APOIO -->
-         	
         </div><!--/CONTEUDO-->
         
       </div><!--/MIOLO -->
       
     </div><!-- /CAPA SITE -->
 
-
+<script>
+$(document).ready(function(){
+	function getURLParameter(name){
+		return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
+	}
+	
+	if(getURLParameter("timeline") != "null"){
+		$(".timeline").show();
+	}
+	
+});
+</script>
+ 
  
