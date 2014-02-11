f68bb912be29f4e56caa3cc936bff474bd7092a6
Joseval <Joseval@fpawks0146-2.local>
2014-02-11 16:53:19 -0200

    Jsonp

--- apps/frontend/templates/sites/qualidade/indexSuccess.php
+++ apps/frontend/templates/sites/qualidade/indexSuccess.php
@@ -103,7 +103,7 @@
 			              var timeline_config = {
 			               width: "100%",
 			               height: "100%",
-			               source: "http://cmais.com.br/qualidade/linha-do-tempo/",
+			               source: "http://cmais.com.br/qualidade/linha-do-tempo.jsonp",
 			               start_at_slide: 94, //para começar do último slide
 			               start_zoom_adjust: -1,
 			               embed_id: "tvcultura-embed",
