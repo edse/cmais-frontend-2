12b9b3f40c8da0b4fd97cd2aac3b5ff0449be35c
Emerson Estrella <emersonestrella@edse.local>
2014-02-13 13:49:06 -0200

    ...

---
+++ apps/frontend/templates/sites/culturabrasil/audioSuccess.php
@@ -0,0 +1,158 @@
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/contato.css" type="text/css" />
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/culturafm.css" type="text/css" />
+
+    <?php use_helper('I18N', 'Date') ?>
+    <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
+
+	 <div id="bg-site"></div>
+
+    <!-- CAPA SITE -->
+    <div id="capa-site" class="audio">
+
+           <?php include_partial_from_folder('sites/culturafm','global/newheader', array('site' => $site,'section'=>$section, 'uri' => $uri, 'program' => $program, 'siteSections'=>$siteSections)) ?>
+
+      <!-- MIOLO -->
+      <div id="miolo">
+      
+        <!-- BOX LATERAL -->
+        <?php include_partial_from_folder('blocks','global/shortcuts') ?>
+        <!-- BOX LATERAL -->
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+
+          <!-- CAPA -->
+          <div class="capa grid3">
+
+            <!-- ESQUERDA -->
+            <div id="esquerda" class="grid2">
+
+              <!-- NOTICIA INTERNA -->
+              <div class="box-interna grid2">
+                <h3><?php echo $asset->getTitle() ?></h3>
+                
+                <div class="assinatura grid2">
+                  <p class="sup"><?php echo $asset->AssetContent->getAuthor() ?> <span><?php echo $asset->retriveLabel() ?></span></p>
+                  <p class="inf"><?php echo format_date($asset->getCreatedAt(), "g") ?> - Atualizado em <?php echo format_date($asset->getUpdatedAt(), "g") ?></p>
+                  <!--
+                  <div class="acessibilidade">
+                    <a href="#" class="zoom">+A</a>
+                    <a href="#" class="zoom">-A</a>
+                  </div>
+                  -->
+
+                  <?php include_partial_from_folder('blocks','global/share-small', array('site' => $site, 'uri' => $uri)) ?>
+
+                </div>
+                
+                <div class="texto bg-cinza">
+                  <div class="grid1">
+                  <?php if($asset->AssetType->getSlug() == "person"): ?>
+                    <?php echo html_entity_decode($asset->AssetPerson->getBio()) ?>
+                  <?php elseif($asset->AssetType->getSlug() == "audio"): ?>
+                    <?php echo html_entity_decode($asset->AssetAudio->render()) ?>
+                  <?php else: ?>
+                    <?php echo html_entity_decode($asset->AssetContent->render()) ?>
+                  <?php endif; ?>
+                  </div>
+                  <p><?php echo nl2br($asset->getDescription()) ?> </p>  
+                </div>
+                
+                <?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri, 'asset' => $asset)) ?>
+
+              </div>
+              <!-- /NOTICIA INTERNA -->
+              
+            </div>
+            <!-- /ESQUERDA -->
+            
+            <!-- DIREITA -->
+            <div id="direita" class="grid1">
+
+              <!-- BOX PADRAO -->
+              <?php if(isset($displays["destaque-apresentadores"])) include_partial_from_folder('blocks','global/display-1c-hosts', array('displays' => $displays["destaque-apresentadores"])) ?>
+              <!-- /BOX PADRAO -->
+              
+              <!-- BOX PUBLICIDADE -->
+              <div class="box-publicidade grid1">
+                <!-- programas-assets-300x250 -->
+                <script type='text/javascript'>
+                GA_googleFillSlot("culturafm-300x250");
+                </script>
+              </div>
+              <!-- / BOX PUBLICIDADE -->
+
+              <?php $relacionados = array(); if($asset) $relacionados = $asset->retriveRelatedAssets2(); ?>
+              <?php if(count($relacionados) > 0): ?>
+              <!-- BOX PADRAO Mais recentes -->
+              <div class="box-padrao grid1">
+                <div class="topo claro">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4>relacionadas</h4>
+                    <a href="#" class="rss" title="rss"></a>
+                  </div>
+                </div>
+                <?php if(count($relacionados) > 0) include_partial_from_folder('blocks','global/recent-news', array('displays' => $relacionados)) ?>
+              </div>
+              <!-- BOX PADRAO Mais recentes -->
+              <?php endif; ?>
+
+              <?php if(isset($displays["destaque-noticias-recentes"])): ?>
+              <!-- BOX PADRAO Mais recentes -->
+              <div class="box-padrao grid1">
+                <div class="topo claro">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4>+ recentes</h4>
+                    <a href="#" class="rss" title="rss"></a>
+                  </div>
+                </div>
+                <?php if(isset($displays["destaque-noticias-recentes"])) include_partial_from_folder('blocks','global/recent-news', array('displays' => $displays["destaque-noticias-recentes"])) ?>
+              </div>
+              <!-- BOX PADRAO Mais recentes -->
+              <?php endif; ?>
+
+              <?php if(isset($displays["destaque-categorias"])): ?>
+              <!-- BORDA 02 -->
+              <div class="box-padrao box-borda grid1">
+                <div class="topo claro">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4><?php if(isset($displays["destaque-categorias"])) echo $displays["destaque-categorias"][0]->Block->getTitle() ?></h4>
+                  </div>
+                </div>
+                <div class="conteudo top tipo2">
+                  <?php if(isset($displays["destaque-categorias"])) include_partial_from_folder('blocks','global/popular-news', array('displays' => $displays["destaque-categorias"])) ?>
+                </div>
+                <div class="detalhe-borda grid1"></div>
+              </div>
+              <!-- /BORDA 02 -->
+              <?php endif; ?>
+              
+              <?php if(isset($displays["destaque-links"])): ?>
+              <!-- BOX PADRAO + Visitados -->
+              <div class="box-padrao mais-visitados grid1">
+                <div class="topo">
+                  <span></span>
+                  <div class="capa-titulo">
+                    <h4><?php if(isset($displays["destaque-links"])) echo $displays["destaque-links"][0]->Block->getTitle() ?></h4>
+                  </div>
+                </div>
+                <?php if(isset($displays["destaque-links"])) include_partial_from_folder('blocks','global/popular-news', array('displays' => $displays["destaque-links"])) ?>
+              </div>
+              <!-- /BOX PADRAO + Visitados -->
+              <?php endif; ?>
+
+            </div>
+            <!-- /DIREITA -->
+          </div>
+          <!-- /CAPA -->
+        </div>
+        <!-- /CONTEUDO PAGINA -->
+
+      </div>
+      <!-- /MIOLO -->
+    </div>
+    <!-- / CAPA SITE -->
+
