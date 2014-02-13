e65fe6f64621a0a16b3d81b549f9dd9b5f863535
Joseval <Joseval@fpawks0146-2.local>
2014-02-13 11:18:07 -0200

    Caminho Url

--- apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/subsectionSuccess.php
@@ -134,11 +134,11 @@ $uri = str_replace('/index.php', '', $uri);
           <?php if(count($pager) > 0): ?>
             <?php foreach($pager->getResults() as $d): ?>
               <?php if( ($section->Site->Program->Channel->getSlug() == "culturabrasil") && ($section->getSlug() == "arquivo") ): ?>
-              <a href="<?php echo $uri . '/arquivo/' . $d->getSlug(); ?>" title=" <?php echo $d->getTitle(); ?>">
+              <a href="http://culturabrasil.cmais.com.br/arquivo/<?php echo $d->getSlug(); ?>" title=" <?php echo $d->getTitle(); ?>">
               <?php elseif($section->getSlug() == "entrevistas"): ?>
               <a href="<?php echo $d->retriveUrl(); ?>" title=" <?php echo $d->getTitle(); ?>">
               <?php else: ?>
-              <a href="<?php echo $uri . '/' . $d->getSlug(); ?>" title=" <?php echo $d->getTitle(); ?>">
+              <a href="http://culturabrasil.cmais.com.br/arquivo/<?php echo $d->getSlug(); ?>" title=" <?php echo $d->getTitle(); ?>">
               <?php endif; ?>
                   <?php $related = $d->retriveRelatedAssetsByAssetTypeId(2); ?>
                   <?php if ($related[0]->retriveImageUrlByImageUsage("culturabrasil-thumb1")): ?>
