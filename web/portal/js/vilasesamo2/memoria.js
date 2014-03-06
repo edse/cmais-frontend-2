629a0b5ebe4e79ff6bbe3524b56962c35755970a
doljak <jedoljak@gmail.com>
2014-03-06 12:57:28 -0300

    u

--- web/portal/js/vilasesamo2/memoria.js
+++ web/portal/js/vilasesamo2/memoria.js
@@ -306,6 +306,9 @@ function closebox(ev) {
 function playSound(soundFileName) {
   if(mediaSupport('audio/ogg; codecs=vorbis', 'audio') || mediaSupport('audio/mpeg', 'audio')) {
     //$(".tampa").css("z-index", "10");
+    var audio = document.getElementByTag("audio");
+    audio.creatElement("source");
+    audio.creatElement("source");
     $(".audio source").attr("src", "http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/memoria/audio/"+soundFileName+".mp3").attr("type", "audio/mp3");
     $(".audio source:last-child").attr("src", "http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/memoria/audio/"+soundFileName+".ogg").attr("type", "audio/ogg");
     $(".audio").trigger('load');
