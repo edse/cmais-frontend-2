6db1ad5ad3a50fc3b305b3bb7dd2d435001deef7
georgiacatarina <georgia.catarina@gmail.com>
2014-03-10 17:22:48 -0300

    as

--- apps/frontend/templates/sites/culturabrasil/indexSuccess.php
+++ apps/frontend/templates/sites/culturabrasil/indexSuccess.php
@@ -243,7 +243,7 @@ $(document).ready(function(){
     $('.carousel-control').fadeOut("fast");
   });
   $('#carrossel-radar').jcarousel({
-  	autostart: true
+  	 wrap: 'circular'
   })
 });
 </script>  
