835d37751cb462a7036837d981a74ae1db71f2b5
doljak <jedoljak@gmail.com>
2014-03-13 12:02:58 -0300

    u

--- web/portal/js/vilasesamo2/vilasesamo.js
+++ web/portal/js/vilasesamo2/vilasesamo.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   //setInterval(function(){
    //$('#header-tablet .logo-mobile').addClass('animated').addClass('tada'); 
   //},7000);
-  var obj = document.getElementById("content");
+  var obj = document.getElementById("content").pageYOffset;
   setInterval(function(){
 
     if(window.innerWidth < 980 && window.pageYOffset > 80){
