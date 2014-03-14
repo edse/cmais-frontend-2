cb61e302bbab42294e1eef02202a32c74e9ee295
georgiacatarina <georgia.catarina@gmail.com>
2014-03-14 16:07:47 -0300

    form icones responsavel

--- apps/frontend/templates/sites/vilasesamo/_form-campanha.php
+++ apps/frontend/templates/sites/vilasesamo/_form-campanha.php
@@ -62,6 +62,13 @@
         </div>
         <!--/Nome-->
         
+        <!--Nome Responsável-->
+        <div class="control-group span8">
+          <label class="control-label icones-form icone-form-nome-resp" for="nome-resp"></label>
+          <input type="text" id="nome-resp" value="Nome do Responsável" name="nome-resp" data-default="Nome do responsável"  placeholder="Nome do responsável">
+        </div>
+        <!--/Nome Responsável-->
+        
         <!--Idade-->
         <div class="control-group idade span2">
           <label class="control-label icones-form icone-form-idade" for="idade"></label>
