71d802e0bcd683f68c674fab07f440d5885ac483
georgiacatarina <georgia.catarina@gmail.com>
2014-03-14 17:01:15 -0300

    termos

--- apps/frontend/templates/sites/vilasesamo/_termos.php
+++ apps/frontend/templates/sites/vilasesamo/_termos.php
@@ -1,1 +1,47 @@
-Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições v Termos e condições Termos e condições Termos e condições v v Termos e condiçõesTermos e condições Termos e condições Termos e condições Termos e condiçõesTermos e condiçõesTermos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições Termos e condições v Termos e condições Termos e condições Termos e condições v v Termos e condiçõesTermos e condições Termos e condições Termos e condições Termos e condiçõesTermos e condiçõesTermos e condições Termos e condições
+Participação:<br/>
+<br/>
+  
+Este é um programa de caráter exclusivamente cultural, sem qualquer modalidade de sorteio ou pagamento, nem vinculado à aquisição ou uso de qualquer bem, direito ou serviço, nos termos da Lei 5.768/71 e do Decreto n° 70.951/72, e que é realizado pela Fundação Padre Anchieta Centro Paulista de Rádio e TVs Educativas. A participação é aberta a crianças representadas por seus pais ou responsáveis legais.
+<br/>
+ 
+Para participar, o interessado (com autorização de pais ou responsáveis) deve enviar um desenho de sua brincadeira favorita. Não há restrições temáticas, desde que o texto seja livre de preconceitos, palavras obscenas ou conteúdo inadequado ao público infantil. O desenho deve ser encaminhado pelo site: http://cmais.com.br/vilasesamo
+<br/>
+ 
+1.1 Os desenhos deverão ser enviados até o dia 31/03/2014, acompanhados dos dados pessoais do participante, utilizando o formulário da página. Caso o participante seja menor de 18 (dezoito) anos, deverá necessariamente ter autorização dos seus pais ou responsáveis, bem como preencher os dados dos seus representantes legais: nome e e-mail. Cada pessoa pode participar com quantos desenhos desejar.
+<br/>
+ 
+1.2. A Fundação Padre Anchieta não se responsabiliza por eventuais falhas no envio do material.
+<br/>
+ 
+2. Critérios de Seleção:
+<br/>
+ 
+2.1 A seleção dos desenhos será feita pela equipe de Produção da TV Cultura e será baseada na observação dos seguintescritérios e pela ordem: criatividade, originalidade e adequação à faixa etária.
+ <br/>
+ 
+2.2 Serão desconsiderados os textos com dados incorretos; os que fujam da adequação à faixa etária (público infantil); que tenham conteúdo inadequado.
+ <br/>
+ 
+2.3 Cada criança poderá participar enviando quantos desenhos desejar.
+ <br/>
+ 
+3. Considerações Gerais:
+ <br/>
+ 
+3.1 Os participantes representados por seus pais ou responsáveis legais, declaram, desde já, a autorização de seu nome e cidade onde moram para publicação na programação da TV Cultura e Sesame Workshop e transferem à Fundação Padre Anchieta Centro Paulista de Rádio e TV Educativas/ Sesame Workshop, sem qualquer ônus para esta e, em caráter definitivo, plena e totalmente, todos os direitos autorais sobre o referido trabalho, para qualquer tipo de utilização, publicação, reprodução por qualquer meio ou técnica, especialmente na divulgação do resultado.
+ <br/>
+ 
+3.2 A FPA não aceitará qualquer desenho que contenha exposição de pessoas em situações vexatórias, incitando o preconceito, violência e que contenham apelo sexual ou ao consumismo exacerbado.
+ <br/>
+ 
+3.3 Quaisquer dúvidas, divergências ou situações não previstas neste regulamento serão apreciadas e decididas pela Produção da TV Cultura referida no item 2.1 deste Regulamento.
+ <br/>
+ 
+3.4 A simples participação neste evento de incentivo à criatividade implica no total conhecimento e aceitação irrestrita deste regulamento.
+ <br/>
+ 
+3.5 Os desenhos poderão ser publicados no site http://cmais.com.br/vilasesamo e os melhores poderão ser exibidos na programação da TV Cultura/Sesame Workshop.
+<br/>
+ 
+
+
