eef55281a1b112bb234b3f740a2558f92aae60bf
doljak <jedoljak@gmail.com>
2014-03-17 16:13:46 -0300

    u

--- apps/frontend/templates/sites/vilasesamo/colecaoincluirbrincandoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/colecaoincluirbrincandoSuccess.php
@@ -80,17 +80,17 @@ $noscript = "  <noscript>Desculpe mas no seu navegador não esta habilitado o Ja
 								</a>
 							</div>
 							
-            	<!--div class="pdf audio-caderno">
-								<a href="">
+            	<div class="pdf audio-caderno">
+								<a href="http://cmais.com.br/portal/audio/vilasesamo/audio_book_dicas.zip">
 								<p>Audiobook Dicas + Caderno de Formação</p>
 								</a>
 							</div>
 							
             	<div class="pdf audio-descricao">
-								<a href="">
+								<a href="http://cmais.com.br/portal/audio/vilasesamo/audio_descricao_personagem.zip">
 								<p>Áudiodescrição dos Personagens: Bel, Garibaldo e Sivan</p>
 								</a>
-							</div-->
+							</div>
 							
 						</div>
               
