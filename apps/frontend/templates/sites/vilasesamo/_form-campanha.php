2264e7944a1b5e6cebc2895329008a3f5f6529e4
georgiacatarina <georgia.catarina@gmail.com>
2014-03-17 12:49:59 -0300

    scrol to msgs form

--- apps/frontend/templates/sites/vilasesamo/_form-campanha.php
+++ apps/frontend/templates/sites/vilasesamo/_form-campanha.php
@@ -312,16 +312,20 @@
 	  if(success == 2){
 	    $(".msgAcerto").show();
 	    $("#form-contato").hide();
-	    $(".msgAcerto").html("<p> Sua brincadeira foi enviada com sucesso<br/> e em breve estará em nossa galeria de brincadeiras!</p>").scrollTo('#statusMsg_0');
+	    $(".msgAcerto").html("<p> Sua brincadeira foi enviada com sucesso<br/> e em breve estará em nossa galeria de brincadeiras!</p>");
+	    $(".msgAcerto").scrollTo('#statusMsg_0');
 	  }else if(error == 1){
 	    $(".msgErro").show();
 	    $(".msgErro").html("<p>Erro inesperado<br/>Por favor, tente mais tarde!</p>");
+	    $(".msgErro").scrollTo("statusMsg_1");
 	  }else if(error == 2){
 	    $(".msgErro").show();
 	    $(".msgErro").html("<p>Formato de imagem inválido<br/> Por favor, tente com JPG, PNG ou GIF!</p>");
+	    $(".msgErro").scrollTo("statusMsg_1");
 	  }else if(error == 3){
 	    $(".msgErro").show();
 	    $(".msgErro").html("<p>Arquivo muito grande<br/> Por favor, tente com um arquivo de até 15 MB!</p>");
+	    $(".msgErro").scrollTo("statusMsg_1");
 	  }
 
   });
