cb235f51c95b88b41949672a5421272e05306e3b
doljak <jedoljak@gmail.com>
2014-03-18 10:52:51 -0300

    u

--- apps/frontend/templates/sites/vilasesamo/_bannerprincipal.php
+++ apps/frontend/templates/sites/vilasesamo/_bannerprincipal.php
@@ -96,13 +96,10 @@
             <span>Atividades</span>
           </div>
           <div class="mpc_ls_slide_item par" data-x="40" data-y="270" data-delay="1300" data-duration="2000" data-easing="easeInOutElastic" data-effect="slideLeft" data-fade="on"> 
-            <span>Veja as novas</span>
+            <span>Qual é a sua brincadeira favorita? </span>
           </div>
           <div class="mpc_ls_slide_item par" data-x="40" data-y="325" data-delay="1400" data-duration="2200" data-easing="easeInOutElastic" data-effect="slideLeft" data-fade="on"> 
-            <span>brincadeiras para  </span>
-          </div>
-          <div class="mpc_ls_slide_item par" data-x="40" data-y="370" data-delay="1600" data-duration="2400" data-easing="easeInOutElastic" data-effect="slideLeft" data-fade="on"> 
-            <span>você imprimir! </span>
+            <span>Faça um desenho e envie pra gente!</span>
           </div>
           <div class="mpc_ls_slide_item total" data-fade="on">
             <a href="http://cmais.com.br/vilasesamo/atividades/brincando-no-parque-1#form-campanha" class="total" title="Novidade chegando"></a>
@@ -193,7 +190,7 @@
           
           switch(i){
             case 0:
-              $(this).attr("aria-label", "Incluir Brincando - Garibaldo, Bel e Sivan estão acenando para o povo.").attr('data-href','http://cmais.com.br/vilasesamo/categorias/incluir-brincando');
+              $(this).attr("aria-label", "Incluir Brincando - Garibaldo, Bel e Sivan estão acenando para o povo.").attr('data-href','http://cmais.com.br/<?php echo $site->getSlug() ?>/categorias/incluir-brincando');
             break;
             case 1:
               $(this).attr("aria-label", "Novo Jogo Teste a sua memória e divirta-se - Groover esta pensativo com uma bolacha na mão.").attr('data-href','http://cmais.com.br/<?php echo $site->getSlug() ?>/jogos/jogo-da-memoria-vila-sesamo');
@@ -202,7 +199,7 @@
               $(this).attr("aria-label", "Bel está lhe perguntando se você sabe o que é direito de brincar.").attr('data-href','http://cmais.com.br/<?php echo $site->getSlug() ?>/videos/brincar-e-um-direito-da-crianca')
             break;
             case 3:
-              $(this).attr("aria-label", "elmo está convidando você a conhecer as novas atividades de imprimir'. ").attr('data-href','http://cmais.com.br/<?php echo $site->getSlug() ?>/atividades')
+              $(this).attr("aria-label", "elmo está perguntando: Qual é a sua brincadeira favorita? Faça um desenho e envie pra gente!'. ").attr('data-href','hhttp://cmais.com.br/<?php echo $site->getSlug(); ?>/atividades/brincando-no-parque-1')
             break;
             case 4:
               $(this).attr("aria-label", "Persanagens - Você já conhece a Sivan?").attr('data-href','http://cmais.com.br/<?php echo $site->getSlug() ?>/personagens/sivan');
