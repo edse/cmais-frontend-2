49764033fdc846149332037f50fc1c67494ff456
Joseval <Joseval@fpawks0146-2.local>
2014-03-19 17:03:30 -0300

    Arte e Cultura

--- apps/frontend/templates/sites/cmais/especiaisSuccess.php
+++ apps/frontend/templates/sites/cmais/especiaisSuccess.php
@@ -1,11 +1,12 @@
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/especiais.css" type="text/css" />
+<!-- link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/arte-e-cultura.css" type="text/css" / -->
+<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/especiais.css" type="text/css">
 <script type="text/javascript">
-//carrossel
 $(function(){
+  //carrossel
     $('.carrossel').jcarousel({
         wrap: "both"
     });
-})
+});
 </script> 
 
 <?php use_helper('I18N', 'Date') ?>
@@ -18,30 +19,31 @@ $(function(){
 
       <!-- BARRA SITE -->
       <div id="barra-site">
-
+        
         <!-- box-topo -->
         <div class="box-topo grid3">
-
-          <h3 class="tit-pagina"><a href="http://cmais.com.br/especiais" class="especiais">Especiais</a></h3>
-
+          
+          <h3 class="tit-pagina"><a href="http://cmais.com.br/especiais" class="arte">Especiais</a></h3>
+          
           <!-- menu interna -->
           <?php include_partial_from_folder('blocks','global/sections-menu', array('siteSections' => $siteSections)) ?>
           <!-- /menu interna -->
 
-          <!-- /menu interna -->
-          <div class="navegacao txt-10">
+          <div class="navegacao grid3">
             <a href="http://cmais.com.br" title="Home">Home</a>
-            <span>&gt;</span><a href="http://cmais.com.br/especiais" title="Especiais">Especiais</a>
+            <span>&gt;</span>
+            <a href="http://cmais.com.br/especiais" title="Especiais">Especiais</a>
           </div>
-          <!-- /box-topo -->
-
+          
         </div>
+        <!-- /box-topo -->
+        
       </div>
       <!-- /BARRA SITE -->
-      
+
       <!-- MIOLO -->
       <div id="miolo">
-
+        
         <!-- BOX LATERAL -->
         <?php include_partial_from_folder('blocks','global/shortcuts') ?>
         <!-- BOX LATERAL -->
@@ -51,90 +53,83 @@ $(function(){
           
           <!-- CAPA -->
           <div class="capa grid3">
-
+            
             <!-- ESQUERDA -->
             <div id="esquerda" class="grid2">
-              
-              <!-- col-esq -->
+
+              <!-- DESTAQUE 2 COLUNAS -->
+              <?php if(isset($displays["destaque-principal"])) include_partial_from_folder('blocks','global/display-2c', array('displays' => $displays["destaque-principal"])) ?>
+              <!-- /DESTAQUE 2 COLUNAS -->
+
+              <!-- barra compartilhar -->
+              <!--?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri)) ?-->
+              <!-- /barra compartilhar -->
+
+              <!-- PLAYLIST -->
+              <?php if(isset($displays["destaque-playlist"])) include_partial_from_folder('blocks','global/display-2c-playlist', array('displays' => $displays["destaque-playlist"])) ?>
+              <!-- PLAYLIST -->
+
               <div class="col-esq grid1">
                 
-                <!-- NOTICIA INTERNA SEM FOTO -->
-                <?php if(isset($displays["destaque-texto"])) include_partial_from_folder('blocks','global/display-1c-text2', array('displays' => $displays["destaque-texto"])) ?>
-                <!-- /NOTICIA INTERNA SEM FOTO -->
-                
-                <!-- BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 1 -->
                 <?php if(isset($displays["destaque-padrao-1"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-1"])) ?>
-                <!-- /BOX NOTICIA -->
-                
-                <!-- BOX NOTICIA -->
-                <?php if(isset($displays["destaque-padrao-2"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-2"])) ?>
-                <!-- /BOX NOTICIA -->
-                
-                <!-- BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 1 -->
+
+                <!-- BOX NOTICIA DESTAQUE PADRAO 3 -->
                 <?php if(isset($displays["destaque-padrao-3"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-3"])) ?>
-                <!-- /BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 3 -->
+
+                <!-- BOX NOTICIA DESTAQUE PADRAO 5 -->
+                <?php if(isset($displays["destaque-padrao-5"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-5"])) ?>
+                <!-- BOX NOTICIA DESTAQUE PADRAO 5 -->                
 
               </div>
-              <!-- /col-esq -->
-              
-              <!-- col-dir -->
+
               <div class="col-dir grid1">
 
-                <!-- BOX NOTICIA -->
+
+                <!-- BOX NOTICIA DESTAQUE PADRAO 2 -->
+                <?php if(isset($displays["destaque-padrao-2"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-2"])) ?>
+                <!-- BOX NOTICIA DESTAQUE PADRAO 2 -->
+
+                <!-- BOX NOTICIA DESTAQUE PADRAO 4 -->
                 <?php if(isset($displays["destaque-padrao-4"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-4"])) ?>
-                <!-- /BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 4 -->
 
-                <!-- BOX NOTICIA -->
-                <?php if(isset($displays["destaque-padrao-5"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-5"])) ?>
-                <!-- /BOX NOTICIA -->
-
-                <!-- BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 6 -->
                 <?php if(isset($displays["destaque-padrao-6"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-6"])) ?>
-                <!-- /BOX NOTICIA -->
-
-                <!-- BOX NOTICIA -->
-                <?php if(isset($displays["destaque-padrao-7"])) include_partial_from_folder('blocks','global/display1c-news', array('displays' => $displays["destaque-padrao-7"])) ?>
-                <!-- /BOX NOTICIA -->
+                <!-- BOX NOTICIA DESTAQUE PADRAO 6 -->
 
               </div>
-              <!-- /col-dir -->
-
             </div>
             <!-- /ESQUERDA -->
             
             <!-- DIREITA -->
             <div id="direita" class="grid1">
               
-              <!-- BOX NOTICIA VIDEO -->
-              <?php if(isset($displays["destaque-videos"])) include_partial_from_folder('blocks','global/display-1c-videos-carrossel', array('displays' => $displays["destaque-videos"])) ?>
-              <!-- /BOX NOTICIA VIDEO --> 
-              
-              <!-- BOX PUBLICIDADE -->
-              <?php if(isset($displays["publicidade-300x250"])) include_partial_from_folder('blocks','global/banner-300x250', array('displays' => $displays["publicidade-300x250"])) ?>
-              <!-- / BOX PUBLICIDADE -->
+              <!-- DESTAQUE 1 COLUNA -->
+              <?php if(isset($displays["destaque-secundario"])) include_partial_from_folder('blocks','global/display-1c-vertical-multiple', array('displays' => $displays["destaque-secundario"])) ?>
+              <!-- /DESTAQUE 1 COLUNA -->
 
-              <!-- BOX PADRAO + Visitados -->
-              <div class="box-padrao mais-visitados grid1">
-                <div class="topo">
-                  <span></span>
-                  <div class="capa-titulo">
-                    <h4><?php if(isset($displays["destaque-links"])) echo $displays["destaque-links"][0]->Block->getTitle() ?></h4>
-                  </div>
-                </div>
-                <?php if(isset($displays["destaque-links"])) include_partial_from_folder('blocks','global/popular-news', array('displays' => $displays["destaque-links"])) ?>
-              </div>
-              <!-- /BOX PADRAO + Visitados -->
+              <!-- BOX PUBLICIDADE -->
+              <div class="box-publicidade grid1">
+                <!-- cmais-arteecultura-300x250 -->
+                <script type='text/javascript'>
+                GA_googleFillSlot("cmais-assets-300x250");
+                </script>
+              </div>                           
+              <!-- / BOX PUBLICIDADE -->
 
               <!-- BOX FACEBOOK -->
               <?php include_partial_from_folder('blocks','global/facebook-1c', array('site' => $site, 'uri' => $uri)) ?>
               <!-- /BOX FACEBOOK -->
-              
+
             </div>
             <!-- /DIREITA -->
-            
+
           </div>
           <!-- /CAPA -->
-          
+
         </div>
         <!-- CONTEUDO PAGINA -->
         
@@ -143,4 +138,4 @@ $(function(){
       
     </div>
     <!-- / CAPA SITE -->
-
+    
