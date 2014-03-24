5487c2835a2469b85de744b18d0ca8104049cf0d
georgiacatarina <georgia.catarina@gmail.com>
2014-03-24 14:06:17 -0300

    troca ordem brinque com e form

--- apps/frontend/templates/sites/vilasesamo/jogoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/jogoSuccess.php
@@ -128,11 +128,13 @@ object{height:460px!important;}
   </section>
   <!--/section-->
   
-  <?php include_partial_from_folder('sites/vilasesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
-  
   <?php if(isset($_GET["m"]) && $_GET["m"] == 2143):?> 
     <?php include_partial_from_folder('sites/vilasesamo', 'global/form-campanha', array("site" => $site, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
   <?php endif; ?>
+  
+  <?php include_partial_from_folder('sites/vilasesamo', 'global/brinque-tambem-com', array("site" => $site, "section" => $section, "asset" => $asset, "campaign" => $campaign, "categories" => $categories)) ?>
+  
+  
   <?php include_partial_from_folder('sites/vilasesamo', 'global/para-os-pais', array("site" => $site, "asset" => $asset, "categories" => $categories, "uri" => $uri)) ?>
 
 </div>
