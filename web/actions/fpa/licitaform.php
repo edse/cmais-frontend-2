faf528f519d5e130b3e2094685ccd03199323244
georgiacatarina <georgia.catarina@gmail.com>
2014-03-25 18:14:40 -0300

    email

--- web/actions/fpa/licitaform.php
+++ web/actions/fpa/licitaform.php
@@ -5,12 +5,12 @@ include("/var/frontend/web/actions/includes/functions.php");
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   //if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
     //$to = "jedoljak@gmail.com"; //"vilasesamooficial@gmail.com";
-    $to = "vilasesamooficial@gmail.com";
+    $to = "georgia.catarina@gmail.com";
     $email = strip_tags($_REQUEST['email']);
     $name = strip_tags($_REQUEST['nome']);
     $campaign = strip_tags($_REQUEST['campanha']);
     $from = "{$name} <{$email}>";
-    $subject = '[Vila Sésamo][' . $campaign . '] '.$name.' <'.$email.'>';
+    $subject = '[FPA][' . $campaign . '] '.$name.' <'.$email.'>';
     
     $message = "Formulário Preenchido em " . date("d/m/Y") . " as " . date("H:i:s") . ", seguem abaixo os dados:<br><br>";
     while(list($field, $value) = each($_REQUEST)) {
