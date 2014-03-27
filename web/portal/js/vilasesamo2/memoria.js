4c9681d68c1c930fc325d38b700d8c68b8596b5b
doljak <jedoljak@gmail.com>
2014-03-27 11:54:12 -0300

    u

--- web/portal/js/vilasesamo2/memoria.js
+++ web/portal/js/vilasesamo2/memoria.js
@@ -63,7 +63,7 @@ matchingGame.clone = $.extend(true, [], matchingGame.deck);
 
 //on document load the lazy way
 $(function(){
-
+  $('gamePlay').attr("aria-label", "você está no meio da página, no botão Jogar, aperte "+enterHelp+" pra começar o jogo da memória amiguinho!");
   var loader = new PxLoader();
   
   loader.addImage('http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/memoria/acerto.png');
