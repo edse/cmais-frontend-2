f3c4c87a16b4a82cff603a35c18db11cbdc78182
georgiacatarina <georgia.catarina@gmail.com>
2014-04-01 16:58:56 -0300

    ancora

--- apps/frontend/templates/sites/anchieta/indexSuccess.php
+++ apps/frontend/templates/sites/anchieta/indexSuccess.php
@@ -57,24 +57,9 @@
          <?php if(count($siteSections) > 0): ?>
           <!-- menu interna -->
           <ul class="menu-interna grid3 menu-gabi">
-            <?php foreach($siteSections as $s): ?>
-              <?php $subsections = $s->subsections(); ?>
-              <?php if(count($subsections) > 0): ?>
-                <li class="m-<?php echo $s->getSlug() ?> span"><a href="#" class="abre-menu" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?><span></span></a>
-                  <div class="submenu-interna toggle-menu" style="display:none; width: auto;">
-                    <ul style="display:block;">
-                    <?php foreach($subsections as $s): ?>
-                      <li><a href="<?php echo $s->retriveUrl()?>"><?php echo $s->getTitle()?></a></li>
-                       <li class="m-ancora"><a href="#timeline" title="Linha do tempo">Linha do Tempo</a></li>
-                    <?php endforeach; ?>
-                    </ul>
-                  </div>
-                </li>
-              <?php else: ?>
-                <li class="m-<?php echo $s->getSlug() ?>"><a href="<?php echo $s->retriveUrl()?>" title="<?php echo $s->getTitle() ?>"><?php echo $s->getTitle() ?></a></li>
-              <?php endif; ?>
-            <?php endforeach; ?>
-          </ul>
+					  <li class="m-videos"><a href="http://cmais.com.br/anchieta/videos" title="Vídeos">Vídeos</a></li>
+					  <li class="m-videos"><a href="#timeline" title="Vídeos">Vídeos</a></li>
+					</ul>
           <!-- /menu interna -->
           <?php endif; ?>
 
