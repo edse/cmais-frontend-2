3f18f20066435e5f91f9a60db14fbff29d4507ed
doljak <jedoljak@gmail.com>
2014-04-07 10:28:24 -0300

    u

--- apps/frontend/modules/_asset/actions/actions.class.php
+++ apps/frontend/modules/_asset/actions/actions.class.php
@@ -1063,7 +1063,7 @@ class _assetActions extends sfActions
 			    }
           if ($this->site->getSlug() == "vilasesamo" || $this->asset->id == 180586) {// para o shape puzzle
             //if($this->asset->id != 148169) // id do asset somente para teste, retirar assim que puderem!
-            
+              die("1");
               $this->setLayout("vilasesamo");
             $sections = $this->asset->getSections();
             foreach($sections as $s) {
