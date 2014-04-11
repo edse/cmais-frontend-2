42470b2176b014fe2a8633f1bf6b76bfe2d59c38
doljak <jedoljak@gmail.com>
2014-04-11 16:34:38 -0300

    u

--- web/portal/js/vilasesamo2/puzzle/piece.js
+++ web/portal/js/vilasesamo2/puzzle/piece.js
@@ -122,10 +122,10 @@ Piece.prototype.draw = function() {
       this.position.y = 0;
     }
     if(this.position.x+this.img.width > window.innerWidth){
-      this.position.x = (this.position.x+this.img.width) + window.innerWidth
+      this.position.x = window.innerWid - this.img.width;
     }
     if(this.position.y+this.img.height > window.innerHeight){ 
-      this.position.y = (this.position.y+this.img.height) + window.innerHeight
+      this.position.y = window.innerHeight - this.img.height;
     }
     this.game.context.drawImage(this.img, this.position.x, this.position.y);
     
