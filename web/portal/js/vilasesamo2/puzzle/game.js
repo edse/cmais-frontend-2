c56719aad2847ff7a6ad7ceba33d66eae1b266f1
georgiacatarina <georgia.catarina@gmail.com>
2014-04-14 20:55:02 -0300

    teste inserir logo no canvas

--- web/portal/js/vilasesamo2/puzzle/game.js
+++ web/portal/js/vilasesamo2/puzzle/game.js
@@ -131,6 +131,7 @@ Game.prototype.init = function(){
 Game.prototype.render = function() {
   
   this.draw_bg();
+  this.draw_logo();
     
   //LOADING
   if(!this.loaded){
@@ -213,6 +214,20 @@ Game.prototype.draw_bg = function() {
     this.context.restore();
   }
   
+  Game.prototype.draw_logo = function() {
+	 var img = new Image();
+	  img.src = 'http://cmais.com.br/portal/images/capaPrograma/vilasesamo2/puzzle/icones/logo.png';
+  if(!this.scale) this.scale = 1;
+  this.context.fillStyle = "rgba(1, 114, 249, 1)";
+	this.context.beginPath();
+	this.context.rect(1580, 50, 200, 100); // desenha o retângulo
+	this.context.fill();
+	this.context.lineWidth = 5;
+	this.context.strokeStyle = 'black';
+	this.context.drawImage(img, 1580, 50, 200, 100);
+	
+	
+  }
   /*
   var width = this.canvas.width, 
     height = this.canvas.height, 
