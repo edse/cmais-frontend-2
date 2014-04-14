41fc6f9922cce8f6b88ab4ffca9c13fdd53b5cbc
doljak <jedoljak@gmail.com>
2014-04-14 18:25:30 -0300

    u

--- web/portal/js/vilasesamo2/puzzle/game.js
+++ web/portal/js/vilasesamo2/puzzle/game.js
@@ -117,7 +117,7 @@ Game.prototype.init = function(){
       new Point2D(-323,293),
       new Point2D(-12,226),
       new Point2D(277,214),
-      new Point2D(346,42),
+      new Point2D(346,44),
       new Point2D(87,60),
       new Point2D(66,-148),
       new Point2D(380,-148)))
