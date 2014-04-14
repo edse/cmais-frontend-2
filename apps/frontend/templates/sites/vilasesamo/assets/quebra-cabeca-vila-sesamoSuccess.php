1c1f92657284652c22043f008237e90ab90d2628
georgiacatarina <georgia.catarina@gmail.com>
2014-04-14 16:50:27 -0300

    comenta alert

--- apps/frontend/templates/sites/vilasesamo/assets/quebra-cabeca-vila-sesamoSuccess.php
+++ apps/frontend/templates/sites/vilasesamo/assets/quebra-cabeca-vila-sesamoSuccess.php
@@ -303,7 +303,7 @@
     function updateOrientation(){  
       var screenWidth = screen.width;
       var windowWidth = window.innerWidth;
-      alert(windowWidth);
+      //alert(windowWidth);
       //console.log("oiiiii");
       if (windowWidth > 470 || (screenWidth > 470 && windowWidth > 470 && window.orientation == 90) || (screenWidth > 470 && windowWidth > 470 && window.orientation == -90) || navigator.platform == "Win32" || navigator.platform == "Win64") {  
         $('.asset').fadeIn('fast');
