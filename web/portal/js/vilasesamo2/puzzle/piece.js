2a15e71c9d03b3e5c80619169fe3f99a36f0c45b
doljak <jedoljak@gmail.com>
2014-04-15 17:59:48 -0300

    u

--- web/portal/js/vilasesamo2/puzzle/piece.js
+++ web/portal/js/vilasesamo2/puzzle/piece.js
@@ -121,6 +121,10 @@ Piece.prototype.draw = function() {
     if(this.position.y<0){
       this.position.y = 0;
     }
+    if(this.position.x>$("#grid-size").width()){
+      this.position.x = 0;
+      console.log("passei")
+    }
     //console.log(this.position.y);
     //console.log($("#grid-size").height());
 
