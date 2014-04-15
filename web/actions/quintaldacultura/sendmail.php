a20ac98208b836cec786519406c7f63692e22130
georgiacatarina <georgia.catarina@gmail.com>
2014-04-15 17:21:11 -0300

    email do quintal

--- web/actions/quintaldacultura/sendmail.php
+++ web/actions/quintaldacultura/sendmail.php
@@ -8,7 +8,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
     if ($current_time < $expiration_time) {
     
-      $to = "georgia.catarina@gmail.com, jedoljak@gmail.com";
+      $to = "georgia.catarina@gmail.com, jedoljak@gmail.com";//quintal.tv@gmail.com
       //$to = "maiscriancatvcultura@gmail.com";
       $email = strip_tags($_REQUEST['email']);
       $name = strip_tags($_REQUEST['nome']);
