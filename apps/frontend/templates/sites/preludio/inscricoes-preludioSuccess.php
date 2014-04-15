92ed97b763d0482b3b4796945499f00ef5acf7ac
georgiacatarina <georgia.catarina@gmail.com>
2014-04-15 19:43:41 -0300

    coloca rodape = home preludio

--- apps/frontend/templates/sites/preludio/inscricoes-preludioSuccess.php
+++ apps/frontend/templates/sites/preludio/inscricoes-preludioSuccess.php
@@ -582,7 +582,7 @@
         <!-- /DIREITA -->
         <!-- rodape preestreia-->
         <div class="grid3 apoio">
-          <img src="http://cmais.com.br/portal/images/capaPrograma/preestreia/rodape_preestreia.jpg" />  
+          <img src="http://midia.cmais.com.br/displays/d79e3a6ca8284083bd5aaec356dbf2f6e81783fe.jpg" alt="cartela de logos">
          </div>
          <!-- /rodape preestreia-->
         
