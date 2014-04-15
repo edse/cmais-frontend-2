195df25318584e5c67685975cc6d1f31376d1583
doljak <jedoljak@gmail.com>
2014-04-15 17:02:51 -0300

    u

--- web/portal/js/vilasesamo2/puzzle/puzzle.js
+++ web/portal/js/vilasesamo2/puzzle/puzzle.js
@@ -118,8 +118,8 @@ Puzzle.prototype.init = function(){
 Puzzle.prototype.placePiece = function(id, img, holder){
   //x = Math.floor(Math.random()*(this.game.canvas.width/this.game.scale-img.width));
   //y = Math.floor(Math.random()*(this.game.canvas.height/this.game.scale-img.height));
-  x = Math.floor(Math.random()*(this.game.canvas.width/this.game.scale-img.width));
-  y = Math.floor(Math.random()*(this.game.canvas.height/this.game.scale-img.height));
+  x = Math.floor(this.game.canvas.width/this.game.scale-img.width));
+  y = Math.floor(this.game.canvas.height/this.game.scale-img.height));
   /*
   if(y<80) y += 80;
   if(y>this.game.canvas.height-20) y -= 80;
