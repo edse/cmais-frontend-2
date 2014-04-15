d4c7cc93afe1c4122b8062e3306719a5dda146d8
doljak <jedoljak@gmail.com>
2014-04-15 17:31:32 -0300

    u

--- web/portal/js/vilasesamo2/puzzle/puzzle.js
+++ web/portal/js/vilasesamo2/puzzle/puzzle.js
@@ -121,7 +121,7 @@ Puzzle.prototype.placePiece = function(id, img, holder){
   for(var i = 0; i<this.holders.length; i++){
     console.log(i);
     console.log(this.holders.length);
-    x = 300 * i;
+    x = 500 * i;
     y = 30;
   }
   
