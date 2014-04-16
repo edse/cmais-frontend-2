3b491bbe9fdb57865dfb1430ae2770b9ff9c89d8
georgiacatarina <georgia.catarina@gmail.com>
2014-04-16 19:47:31 -0300

    pullaaa

--- web/portal/js/vilasesamo2/puzzle/game.js
+++ web/portal/js/vilasesamo2/puzzle/game.js
@@ -287,8 +287,8 @@ Game.prototype.draw_remaining = function() {
   this.context.font = "bold "+this.font_size+"px Arial";
   this.context.textBaseline = 'top';
   this.context.textAlign = 'left';
-  this.context.strokeText("Tempo" .html("<br> ") +this.puzzle.remaining_time, 10, 150);
-  this.context.fillText("Tempo" .html("<br> ") +this.puzzle.remaining_time, 10, 150);
+  this.context.strokeText.html("Tempo <br> "+this.puzzle.remaining_time, 10, 150);
+  this.context.fillText.html("Tempo <br> "+this.puzzle.remaining_time, 10, 150);
   var metrics = this.context.measureText(this.stage+"/"+this.puzzles.length+" ");
   this.context.strokeText(this.stage+"/"+this.puzzles.length, this.canvas.width/this.scale-metrics.width, 80,80);
   this.context.fillText(this.stage+"/"+this.puzzles.length, this.canvas.width/this.scale-metrics.width, 80,80);
