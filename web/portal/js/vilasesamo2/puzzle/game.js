2fe159bf7b847d7391e8e4c2000dd7df2974028e
georgiacatarina <georgia.catarina@gmail.com>
2014-04-17 11:32:56 -0300

    margens

--- web/portal/js/vilasesamo2/puzzle/game.js
+++ web/portal/js/vilasesamo2/puzzle/game.js
@@ -227,7 +227,7 @@ Game.prototype.draw_logo = function() {
 	this.context.rect(this.canvas.width/this.scale - 200, 50, 200, 100); 
 	this.context.lineWidth = 5;
 	this.context.strokeStyle = 'black';
-	this.context.drawImage(img, this.canvas.width/this.scale - 200, 50, 200, 100);
+	this.context.drawImage(img, this.canvas.width/this.scale - 400, 50, 200, 100);
 	
 	
 };
@@ -287,10 +287,10 @@ Game.prototype.draw_remaining = function() {
   this.context.font = "bold "+this.font_size+"px Arial";
   this.context.textBaseline = 'top';
   this.context.textAlign = 'left';
-  this.context.strokeText("Tempo", 10, 100);
-  this.context.fillText("Tempo", 10, 100);
-  this.context.strokeText(this.puzzle.remaining_time, 10, 150);
-  this.context.fillText(this.puzzle.remaining_time, 10, 150);
+  this.context.strokeText("Tempo", 10, 50);
+  this.context.fillText("Tempo", 10, 50);
+  this.context.strokeText(this.puzzle.remaining_time, 30, 100);
+  this.context.fillText(this.puzzle.remaining_time, 30, 100);
   var metrics = this.context.measureText(this.stage+"/"+this.puzzles.length+" ");
   this.context.strokeText(this.stage+"/"+this.puzzles.length,50,1150 );
   this.context.fillText(this.stage+"/"+this.puzzles.length, 50,1150);
