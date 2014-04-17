2b6e3358664ea0ce14add992e07106192bd60346
Joseval <Joseval@fpawks0146-2.local>
2014-04-17 14:22:03 -0300

    Rodape

--- apps/frontend/templates/sites/preludio/videoSuccess.php
+++ apps/frontend/templates/sites/preludio/videoSuccess.php
@@ -161,6 +161,12 @@ $(function(){
 
             </div>
             <!-- /DIREITA -->
+        <!-- rodape preludio-->
+        <div class="grid3 apoio">
+          <img src="http://midia.cmais.com.br/displays/d79e3a6ca8284083bd5aaec356dbf2f6e81783fe.jpg" alt="cartela de logos">
+         </div>
+        <!-- /rodape preludio-->            
+            
           </div>
           <!-- /CAPA -->
 
