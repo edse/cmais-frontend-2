4ff3dc5aafcc2cd925a6e4ac520d69f1a4c3f160
doljak <jedoljak@gmail.com>
2014-04-22 16:01:23 -0300

    u

--- web/actions/quintaldacultura/sendmail.php
+++ web/actions/quintaldacultura/sendmail.php
@@ -7,7 +7,7 @@ $expiration_time = "2014-08-30 00:00:00";
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
     if ($current_time < $expiration_time) {
-      
+      die("1");
       $to = "georgia.catarina@gmail.com, jedoljak@gmail.com";//quintal.tv@gmail.com
       //$to = "maiscriancatvcultura@gmail.com";
       $email = strip_tags($_REQUEST['email']);
