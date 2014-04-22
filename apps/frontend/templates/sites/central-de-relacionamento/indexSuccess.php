7acf6f9bb026956145e6b7a8473425718e4410ee
doljak <jedoljak@gmail.com>
2014-04-22 18:07:13 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -1394,7 +1394,7 @@ $(document).ready(function(){
                     success: function(data){
                       if(data.script != ""){
                         eval(data.script);
-                        console.log($(this));
+                        console.log$('#f4_cod_veiculo :selected').val());
                         if($(this).val() == "Institucional"){
                           $('#f4_cod_veiculo').parent().prev().html('Área');  
                         }else{
