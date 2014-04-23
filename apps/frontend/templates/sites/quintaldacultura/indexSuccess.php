74cf8a045433e9404898cca3d5d772d7f0fa8773
georgiacatarina <georgia.catarina@gmail.com>
2014-04-23 16:05:34 -0300

    MASG

--- apps/frontend/templates/sites/quintaldacultura/indexSuccess.php
+++ apps/frontend/templates/sites/quintaldacultura/indexSuccess.php
@@ -360,7 +360,7 @@ Para participar, o interessado (com autorização de pais ou responsáveis) deve
             cidade:'*TODOS OS CAMPOS SÃO DE PREENCHIMENTO OBRIGATÓRIO!',
             uf:'*TODOS OS CAMPOS SÃO DE PREENCHIMENTO OBRIGATÓRIO!',
             concordo:'*TODOS OS CAMPOS SÃO DE PREENCHIMENTO OBRIGATÓRIO!',
-            datafile:'POR FAVOR INSIRA UM ARQUIVO DE IMAGEM VÁLIDO:JPEG,GIF OU PNG'
+            datafile:'FORMATO INVÁLIDO'
           }, 
           
           success: function(label){
