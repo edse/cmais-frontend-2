e4f0e3c8195e64f78d8ef9b543244eddf5e35f75
georgiacatarina <georgia.catarina@gmail.com>
2014-04-30 15:31:24 -0300

    volta

--- web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
+++ web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
@@ -19,52 +19,55 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $message .= "<b>" . ucwords($field) . ":</b> " . strip_tags($value) . "<br>";
     }
 		
-		if(sendMailAtt($to, $from, $subject, $message)) {
-        header("Location: ".$_REQUEST['urlElement']."?success=2#carrossel-interna");
-		}else{
-    	header("Location: ".$_REQUEST['urlElement']."?error=1#carrossel-interna");
-        //die("3");
-      
-    }
-    
-    $file_name = basename($_FILES['datafile']['name']);
-    $data = file_get_contents($_FILES['datafile']['tmp_name']); 
-    $file_contents = chunk_split(base64_encode($data));
-    $file_size = $_FILES['datafile']['size'];
-    $file_mime_type = getMimeType($_FILES['datafile']['name']);
-    //$file_mime_type = "image/jpg";
-    //$extension = strtolower(end(explode('.',$_FILES['datafile']['name'])));
-    //$file_mime_type = "image/".$extension;
-    $attach = array();
-    $attach[] = array($_FILES['datafile']['tmp_name'], $file_mime_type);
-    //die($file_mime_type);
-    if (!in_array($file_mime_type, array("image/gif", "image/png", "image/jpg"))) {
-      if (unlink($_FILES['datafile']['tmp_name'])) {
-        header("Location: ".$_REQUEST['urlElement']."?error=2#carrossel-interna");
-        //die("1");
-      }
-    }
-    else if ($file_size > 15728640) { // 15MB
-      if (unlink($_FILES['datafile']['tmp_name'])) {
-        header("Location: ".$_REQUEST['urlElement']."?error=3#carrossel-interna");
-        //die("2");
-      }
-    }
-    else {
-      
-      if(sendMailAtt($to, $from, $subject, $message, $attach)) {
-        if (unlink($_FILES['datafile']['tmp_name'])) {
-          header("Location: ".$_REQUEST['urlElement']."?success=2#carrossel-interna");
-          //die("0");
-        }
-      }
-      else{
-        if (unlink($_FILES['datafile']['tmp_name'])) {
-          header("Location: ".$_REQUEST['urlElement']."?error=1#carrossel-interna");
-          //die("3");
-        }
-      }
-    }
+		if(isset($_FILES['datafile']['tmp_name'])) {
+			
+	    $file_name = basename($_FILES['datafile']['name']);
+	    $data = file_get_contents($_FILES['datafile']['tmp_name']); 
+	    $file_contents = chunk_split(base64_encode($data));
+	    $file_size = $_FILES['datafile']['size'];
+	    $file_mime_type = getMimeType($_FILES['datafile']['name']);
+	    //$file_mime_type = "image/jpg";
+	    //$extension = strtolower(end(explode('.',$_FILES['datafile']['name'])));
+	    //$file_mime_type = "image/".$extension;
+	    $attach = array();
+	    $attach[] = array($_FILES['datafile']['tmp_name'], $file_mime_type);
+	    //die($file_mime_type);
+	    if (!in_array($file_mime_type, array("image/gif", "image/png", "image/jpg"))) {
+	      if (unlink($_FILES['datafile']['tmp_name'])) {
+	        header("Location: ".$_REQUEST['urlElement']."?error=2#carrossel-interna");
+	        //die("1");
+	      }
+	    }
+	    else if ($file_size > 15728640) { // 15MB
+	      if (unlink($_FILES['datafile']['tmp_name'])) {
+	        header("Location: ".$_REQUEST['urlElement']."?error=3#carrossel-interna");
+	        //die("2");
+	      }
+	    }
+	    else {
+	      
+	      if(sendMailAtt($to, $from, $subject, $message, $attach)) {
+	        if (unlink($_FILES['datafile']['tmp_name'])) {
+	          header("Location: ".$_REQUEST['urlElement']."?success=2#carrossel-interna");
+	          //die("0");
+	        }
+	      }
+	      else{
+	        if (unlink($_FILES['datafile']['tmp_name'])) {
+	          header("Location: ".$_REQUEST['urlElement']."?error=1#carrossel-interna");
+	          //die("3");
+	        }
+	      }
+	    }
+	  }else{
+	   	if(sendMailAtt($to, $from, $subject, $message)) {
+	        header("Location: ".$_REQUEST['urlElement']."?success=2#carrossel-interna");
+			}else{
+	    	header("Location: ".$_REQUEST['urlElement']."?error=1#carrossel-interna");
+	        //die("3");
+	      
+	    }
+	  }
   //}
 }
 
