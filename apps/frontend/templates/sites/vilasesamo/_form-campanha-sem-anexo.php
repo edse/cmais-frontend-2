981464a3d7867138f5a4f2447c1b25a0a6b657ff
georgiacatarina <georgia.catarina@gmail.com>
2014-04-30 13:48:18 -0300

    limita

--- apps/frontend/templates/sites/vilasesamo/_form-campanha-sem-anexo.php
+++ apps/frontend/templates/sites/vilasesamo/_form-campanha-sem-anexo.php
@@ -307,6 +307,17 @@
 			else
 				$(textCounter).html(limitNum - limitField.value.length);
 		}
+		  function getVar(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++){
+          var pair = vars[i].split("=");
+          if (pair[0] == variable) {
+            return pair[1];
+          }
+        }
+      }
+      
     function getURLParameter(name) {
       return decodeURI(
             (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
