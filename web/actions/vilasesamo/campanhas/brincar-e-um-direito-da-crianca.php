7c6464366bfb800c308c46b0e828397fff4a885e
georgiacatarina <georgia.catarina@gmail.com>
2014-04-30 12:47:27 -0300

    testa 2

--- web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
+++ web/actions/vilasesamo/campanhas/brincar-e-um-direito-da-crianca.php
@@ -3,7 +3,7 @@ include("/var/frontend/web/actions/includes/functions.php");
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	die("1");
+	
   //if(strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) > 0) {
     $to = "jedoljak@gmail.com"; "georgia.catarina@gmail.com";
     //$to = "vilasesamooficial@gmail.com";
@@ -19,32 +19,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $message .= "<b>" . ucwords($field) . ":</b> " . strip_tags($value) . "<br>";
     }
     
-    $file_name = basename($_FILES['datafile']['name']);
-    $data = file_get_contents($_FILES['datafile']['tmp_name']); 
-    $file_contents = chunk_split(base64_encode($data));
-    $file_size = $_FILES['datafile']['size'];
-    $file_mime_type = getMimeType($_FILES['datafile']['name']);
-    //$file_mime_type = "image/jpg";
-    //$extension = strtolower(end(explode('.',$_FILES['datafile']['name'])));
-    //$file_mime_type = "image/".$extension;
-    $attach = array();
-    $attach[] = array($_FILES['datafile']['tmp_name'], $file_mime_type);
-    //die($file_mime_type);
-    if (!in_array($file_mime_type, array("image/gif", "image/png", "image/jpg"))) {
-      if (unlink($_FILES['datafile']['tmp_name'])) {
-        header("Location: ".$_REQUEST['urlElement']."?error=2#carrossel-interna");
-        //die("1");
-      }
-    }
-    else if ($file_size > 15728640) { // 15MB
-      if (unlink($_FILES['datafile']['tmp_name'])) {
-        header("Location: ".$_REQUEST['urlElement']."?error=3#carrossel-interna");
-        //die("2");
-      }
-    }
-    else {
+   
       
-      if(sendMailAtt($to, $from, $subject, $message, $attach)) {
+      if(sendMailAtt($to, $from, $subject, $message)) {
         if (unlink($_FILES['datafile']['tmp_name'])) {
           header("Location: ".$_REQUEST['urlElement']."?success=2#carrossel-interna");
           //die("0");
@@ -56,7 +33,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
           //die("3");
         }
       }
-    }
+    
   //}
 }
 
