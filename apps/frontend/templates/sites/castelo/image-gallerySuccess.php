1eba31c6e2c09e8ebdecc62052a526d0ccf4e56a
georgiacatarina <georgia.catarina@gmail.com>
2014-05-09 10:17:54 -0300

    hjhh

--- apps/frontend/templates/sites/castelo/image-gallerySuccess.php
+++ apps/frontend/templates/sites/castelo/image-gallerySuccess.php
@@ -1,155 +1,106 @@
-<!DOCTYPE html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/"> 
-  <head>
-    <link href="/feed" type="application/atom+xml" rel="alternate" title="cmais+ feed" />
-    <link rel="stylesheet" href="http://cmais.com.br/portal/css/geral.css?nocache=1234" type="text/css" />
-    <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/geral2.css" type="text/css" />
-    
-    <!--[if IE]>
-      <link rel="stylesheet" type="text/css" href="http://cmais.com.br/portal/css/ie-only.css" />
-    <![endif]-->
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta http-equiv="Cache-Control" content="no-cache, no-store" />
-    <meta http-equiv="Pragma" content="no-cache, no-store" />
-    <meta http-equiv="expires" content="Mon, 06 Jan 1990 00:00:01 GMT" />
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
 
-    <?php include_title() ?>
-    <?php include_metas() ?>
-    <meta property="og:title" content="<?php echo $asset->getTitle(); ?> - <?php echo $site->getTitle(); ?> - cmais+ O portal de conteúdo da Cultura" />
-    <meta property="og:type" content="article" />
-    <meta property="og:description" content="<?php echo $asset->getDescription(); ?>" />
-    <meta property="og:url" content="<?php echo $asset->retriveUrl(); ?>" />
-    <meta property="og:site_name" content="<?php echo $site->getTitle(); ?>" />
-    <meta property="og:image" content="http://cmais.com.br/portal/images/capaPrograma/castelo/ratimbum-icon.jpg"/>
-    
-    <meta name="google-site-verification" content="sPxYSUnxlnoyUdly_hNwIHma64gh9iosgNcOBrZBYdo" />
-    <meta property="fb:admins" content="100000889563712"/>
-    <meta property="fb:app_id" content="124792594261614"/>
-    <link rel="shortcut icon" href="http://cmais.com.br/portal/images/icon/favicon.png" type="image/x-icon" />
-    
-    <!-- scripts -->
-    <script type="text/javascript" src="http://cmais.com.br/portal/js/jquery-ui/js/jquery-1.5.1.min.js"></script>
-    <script type="text/javascript" src="http://cmais.com.br/portal/js/portal.js"></script>
-    <script type="text/javascript" src="http://cmais.com.br/portal/js/jcarousel/lib/jquery.jcarousel.min.js"></script>
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script type="text/javascript" src="http://apis.google.com/js/plusone.js">
-      {lang: 'pt-BR'}
-    </script>
-    <script>
-    /*
-    if(window.location == parent.window.location){
-    	self.location.href+='&layout=1';
-    }
-    */
-    </script>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-22770265-1']);
-      _gaq.push(['_setDomainName', 'cmais.com.br']);
-      _gaq.push(['_setAllowHash', 'false']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
-    <!-- /scripts -->
-
-  </head>
-  <body>
-<?php use_helper('I18N','Date') ?>
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/defaultPrograma.css" type="text/css" />
-<script src="http://cmais.com.br/portal/js/orbit/jquery.orbit-1.2.3.min.js" type="text/javascript"></script>
-<link type="text/css" href="http://cmais.com.br/portal/js/orbit/orbit-1.2.3.css" rel="stylesheet">
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/castelo/geral.css" type="text/css" />
-<link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/castelo/interna.css" type="text/css" />
-
-<script type="text/javascript">
-$(window).load(function() {
-	$('#featured').orbit({
-		'bullets' : true,   
-		'bulletThumbs': true
-	});
-});
-</script>
-
-<div class="base">  	
-		<!--CONTAINER ASSSET -->
-		<div class="container">
-			
-			<!-- GALERIA DE FOTOS -->
-			<div class="container galeriaNew" style="float: left; margin-bottom: 10px; width: 640px;">
-			  <div id="featured">
-	      <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
-	      <?php if(count($related)>0): ?>
-	      	<?php foreach($related as $d): ?>
-			    <img src="<?php echo $d->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $d->getTitle() ?>" data-thumb="<?php echo $d->retriveImageUrlByImageUsage('image-1') ?>" data-caption="#html<?php echo $d->getSlug() ?>" />
-          <?php endforeach; ?>
-        <?php endif; ?>
-			  </div>
-			
-			  <!-- THUMBNAILS -->
-	      <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
-	      <?php if(count($related)>0): ?>
-	      	<?php foreach($related as $d): ?>
-	      		<?php $related_content = $d->retriveRelatedAssetsByAssetTypeId(1); ?>
-			  <span class="orbit-caption" id="html<?php echo $d->getSlug() ?>">
-			    <span class="espaco">
-	          <?php echo $d->getDescription() ?><?php if($d->AssetImage->getHeadline()!="") echo "<br>".$d->AssetImage->getHeadline() ?><?php if($d->AssetImage->getAuthor()!="") echo "<br>Foto: ".$d->AssetImage->getAuthor() ?>
-	          <?php if(count($related_content)>0): ?>
-	          <br /><a href="<?php echo $related_content[0]->retriveUrl()?>" target="_blank">Saiba mais</a>
-	          <?php endif; ?>
-			    </span>
-			  </span>
-          <?php endforeach; ?>
-        <?php endif; ?>
-			  <!-- THUMBNAILS -->
-			</div>
-			<!-- /GALERIA DE FOTOS -->
-			
-      <?php $relacionados = $asset->retriveRelatedAssetsbyRelationType('Asset Relacionado'); ?>
-      <?php if(count($relacionados) > 0): ?>
-      <!-- SAIBA MAIS -->
-      <div class="box-padrao grid2" style="margin-bottom: 20px;">
-        <div id="saibamais">                                                            
-        <h4>Veja +</h4>                                                            
-        <ul class="conteudo">
-				<?php foreach($relacionados as $k=>$d): ?>        
-          <li style="width: auto;">
-            <a class="titulos" href="<?php echo $d->retriveUrl()?>" title="<?php echo $d->getTitle()?>" style="width: auto;"><?php echo $d->getTitle()?></a>
-          </li>
-        <?php endforeach; ?>
-          
-        </ul>
-       </div>
-      </div>
-      <!-- SAIBA MAIS -->
-      <?php endif; ?>
-			
-			
- 	    <div id="fb-root"></div>
-	    <script>
-	      window.fbAsyncInit = function() {
-	        FB.init({appId: '124792594261614', status: true, cookie: true, xfbml: true});
-	      };
-	      (function() {
-	        var e = document.createElement('script'); e.async = true;
-	        e.src = document.location.protocol +
-	          '//connect.facebook.net/pt_BR/all.js';
-	        document.getElementById('fb-root').appendChild(e);
-	      }());
-	    </script>
-
-			<!-- barra compartilhar -->
-			<?php include_partial_from_folder('blocks','global/share-2c', array('site' => $site, 'uri' => $uri, 'asset' => $asset)) ?>
-			<!-- /barra compartilhar -->
-
-		</div>
-		<!--CONTAINER ASSSET -->
-
-
-  
+<div class="bg-site">
 </div>
-</body>
-</html>
+
+<!--CASTELO-->
+<div id="bg" >
+  
+    <!-- CAPA SITE -->
+    <div id="capa-site"> 
+      
+      <!--FANCYBOX-->
+      <script type="text/javascript" src="http://cmais.com.br/portal/js/fancybox/jquery.fancybox-1.3.4.pack.js" ></script>
+      <link rel="stylesheet" href="http://cmais.com.br/portal/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+      <!--/FANCYBOX-->     
+     
+
+      <!-- BARRA SITE -->
+      <div id="barra-site">
+        
+        
+        <div class="topo-programa">
+          
+          <h2>
+            <a href="http://cmais.com.br/castelo" style="text-decoration: none;">
+          
+                <img src="http://cmais.com.br/portal/images/capaPrograma/castelo/img-logo-castelo.png" class="logo" alt="Castelo Ra Tim Bum" />
+              
+            </a>
+          </h2>
+          
+
+					<?php include_partial_from_folder('sites/castelo','global/like', array('uri' => $uri)) ?>          
+          
+          
+        </div>
+
+        <!-- box-topo -->
+        <div class="box-topo grid3">
+
+           <?php include_partial_from_folder('sites/castelo','global/menu', array('siteSections' => $siteSections, 'section' => $section)) ?>
+         
+        </div>
+        <!-- /box-topo -->
+        
+        
+      </div>
+      <!-- /BARRA SITE -->
+
+      <!-- MIOLO -->
+      <div id="miolo">
+        
+        
+
+        <!-- CONTEUDO PAGINA -->
+        <div id="conteudo-pagina">
+          
+          <!-- CAPA -->
+          <div class="capa grid3">
+            
+           <!--tudo-->            
+           <div class="tudo">
+						<!-- GALERIA DE FOTOS -->
+						<div class="container galeriaNew" style="float: left; margin-bottom: 10px; width: 640px;">
+						  <div id="featured">
+				      <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
+				      <?php if(count($related)>0): ?>
+				      	<?php foreach($related as $d): ?>
+						    <img src="<?php echo $d->retriveImageUrlByImageUsage('image-6') ?>" alt="<?php echo $d->getTitle() ?>" data-thumb="<?php echo $d->retriveImageUrlByImageUsage('image-1') ?>" data-caption="#html<?php echo $d->getSlug() ?>" />
+			          <?php endforeach; ?>
+			        <?php endif; ?>
+						  </div>
+						
+						  <!-- THUMBNAILS -->
+				      <?php $related = $asset->retriveRelatedAssetsByAssetTypeId(2); ?>
+				      <?php if(count($related)>0): ?>
+				      	<?php foreach($related as $d): ?>
+				      		<?php $related_content = $d->retriveRelatedAssetsByAssetTypeId(1); ?>
+						  <span class="orbit-caption" id="html<?php echo $d->getSlug() ?>">
+						    <span class="espaco">
+				          <?php echo $d->getDescription() ?><?php if($d->AssetImage->getHeadline()!="") echo "<br>".$d->AssetImage->getHeadline() ?><?php if($d->AssetImage->getAuthor()!="") echo "<br>Foto: ".$d->AssetImage->getAuthor() ?>
+				          <?php if(count($related_content)>0): ?>
+				          <br /><a href="<?php echo $related_content[0]->retriveUrl()?>" target="_blank">Saiba mais</a>
+				          <?php endif; ?>
+						    </span>
+						  </span>
+			          <?php endforeach; ?>
+			        <?php endif; ?>
+						  <!-- THUMBNAILS -->
+						</div>
+						<!-- /GALERIA DE FOTOS -->
+			
+      </div>
+        <!-- /CONTEUDO PAGINA -->
+        
+        
+        
+      </div>
+      <!-- /MIOLO -->
+      
+    </div>
+    <!-- /CAPA SITE -->
+    
+</div>   
+<!--/CASTELO-->
