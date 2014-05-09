4868998e44bccd69f124cab555912223db32c427
georgiacatarina <georgia.catarina@gmail.com>
2014-05-09 17:56:02 -0300

    fancy

--- apps/frontend/templates/sites/castelo/episodiosSuccess.php
+++ apps/frontend/templates/sites/castelo/episodiosSuccess.php
@@ -2,6 +2,10 @@
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/secoes/todos-videos.css" type="text/css" />
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/castelo/geral.css" type="text/css" />
 <link rel="stylesheet" href="http://cmais.com.br/portal/css/tvcultura/sites/castelo/interna.css" type="text/css" />
+ <!--FANCYBOX-->
+<script type="text/javascript" src="http://cmais.com.br/portal/js/fancybox/jquery.fancybox-1.3.4.pack.js" ></script>
+<link rel="stylesheet" href="http://cmais.com.br/portal/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+<!--/FANCYBOX-->     
 
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'section' => $section)) ?>
