c4ebf47bfac16ddfb25f4d4341f3c3590ca2158d
doljak <jedoljak@gmail.com>
2014-05-12 11:39:21 -0300

    u

--- apps/frontend/templates/sites/castelo/videoSuccess.php
+++ apps/frontend/templates/sites/castelo/videoSuccess.php
@@ -70,7 +70,7 @@ $section = Doctrine_Query::create()
         <div id="conteudo-pagina">
 					
 					<?php include_partial_from_folder('sites/castelo','global/asset-2c-video', array('asset' => $asset, 'ipad' => $ipad)) ?>
-					<?php $pager->getResults(); ?> 
+					<?php $pager = $section->Pager->getResults(); ?> 
 					<?php include_partial_from_folder('sites/castelo','global/display-videos-paginated', array('pager' => $pager)) ?>       	
           
         </div>
