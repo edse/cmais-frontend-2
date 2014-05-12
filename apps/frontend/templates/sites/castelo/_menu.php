e9021624379d7471c04ee75b231adf61eeb32aec
doljak <jedoljak@gmail.com>
2014-05-12 12:39:23 -0300

    u

--- apps/frontend/templates/sites/castelo/_menu.php
+++ apps/frontend/templates/sites/castelo/_menu.php
@@ -1,5 +1,5 @@
           <?php if(count($siteSections) > 0): ?>
-            <?php echo $section->Site->getFacebookUrl() ?>
+            <?php echo $asset->Site->getFacebookUrl(). ">>>>>>" ?>
           <!-- menu interna -->
           <ul class="menu-interna grid3">
             <?php foreach($siteSections as $s): ?>
