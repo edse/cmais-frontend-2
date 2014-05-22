9738d60e01ad265fd78019734be60de6c0909787
doljak <jedoljak@gmail.com>
2014-05-22 16:51:59 -0300

    u

--- apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
+++ apps/frontend/templates/sites/central-de-relacionamento/indexSuccess.php
@@ -840,9 +840,7 @@ $(document).ready(function(){
               		$("#form4 .control-group select").each(function(index){
               		  if(index>=5){
               		    $(this).parent().append('<i class=" icon-question-sign example" data-toggle="tooltip" data-placement="right" title="'+datas[index - 5]+'" data-original-title="'+datas[index]+'"></i>');
-                			$(".example").tooltip({
-                      delay: { show: 500, hide: 200000000 }
-                    });
+                			$(".example").tooltip();
               			}
               		})
 
