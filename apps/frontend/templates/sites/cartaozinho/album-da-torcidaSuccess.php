98e7edfb0055e5dfb84e7c2ed49d3a303cd6a1f3
georgiacatarina <georgia.catarina@gmail.com>
2014-05-30 16:46:29 -0300

    share

--- apps/frontend/templates/sites/cartaozinho/album-da-torcidaSuccess.php
+++ apps/frontend/templates/sites/cartaozinho/album-da-torcidaSuccess.php
@@ -137,7 +137,6 @@
                                     	<img src="http://cmais.com.br/portal/images/capaPrograma/cartaozinho/borda_figurinha.png" <?php echo $estilo[$j]?> >
                                     </div>
                                     <div class="legenda"><?php echo $displays["enviados"][$fig]->getTitle(); ?></div>
-                                    <div class="fb-share-button" data-href="<?php// echo $displays["enviados"][$fig]->Asset->retriveImageUrlByImageUsage('image-9'); ?>" data-type="button_count"></div>
                                   </div>
                                 </a>
                               </li>
@@ -458,6 +457,7 @@
 
 //console.log($(this).attr("aria-label"));
 	$(".fancybox2").fancybox({
+		$(".fancybox-inner").append('<div class="fb-share-button" data-href="<?php echo $displays["enviados"][$fig]->Asset->retriveImageUrlByImageUsage('image-9'); ?>" data-type="button_count"></div>')
 	 helpers : {
         title: {
             type: 'over'
