c0dc531a0d9656f5e6ef1e3f8abe22920b97e419
doljak <jedoljak@gmail.com>
2014-06-02 08:10:59 -0300

    u

--- apps/frontend/templates/sites/cmais/indexSuccess.php
+++ apps/frontend/templates/sites/cmais/indexSuccess.php
@@ -4,7 +4,7 @@
 <!--script type="text/javascript" src="http://cmais.com.br/portal/js/redirect_mobile.js"></script-->
 
 
-
+<?php $displays_videos = ""; ?>
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial_from_folder('blocks', 'global/menu', array('site' => $site, 'mainSite' => $mainSite, 'asset' => $asset, 'section' => $section)) ?>
 
