1538092cf20ee1a945d84680d87a907c145d186f
doljak <jedoljak@gmail.com>
2014-06-09 11:17:26 -0300

    u

--- apps/frontend/templates/blocks/_formCentralRelacionamento.php
+++ apps/frontend/templates/blocks/_formCentralRelacionamento.php
@@ -648,7 +648,7 @@
                 <?php
                 function getUrl() {
                   $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
-                  $url .= ( $_SERVER["SERVER_PORT"] !== 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
+                  //$url .= ( $_SERVER["SERVER_PORT"] !== 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
                   $url .= $_SERVER["REQUEST_URI"];
                   return $url;
                 }
--- web/crm-form/soap.php
+++ web/crm-form/soap.php
@@ -195,7 +195,7 @@ else if(($_REQUEST["step"]==2)&&($_REQUEST["f2_nome"]!="")){
       $telefone = substr($_REQUEST["f2_telefone"], 5, 9);
     }
     $arr = array(
-      'urlRequest'            =>  (isset($_REQUEST["urlRequest"])? $_REQUEST["urlRequest"] : "" ),          
+      'urlRequest'            =>  (isset($_REQUEST["urlRequest"])? $_REQUEST["f2_urlRequest"] : "" ),          
       'nome'                  =>  (isset($_REQUEST["f2_nome"]))? $_REQUEST["f2_nome"] : "", 
       'email'                 =>  (isset($_REQUEST["email"]))? $_REQUEST["email"] : "", 
       'cod_faixaetaria'       =>  (isset($_REQUEST["f2_cod_faixaetaria"]))? $_REQUEST["f2_cod_faixaetaria"] : "",
